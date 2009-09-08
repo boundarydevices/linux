e2df5a3e76dcc458cdf4d5e758e340eba3441bd8
Oliver Neukum <oliver@neukum.org>
2009-09-08 23:51:28 +0200

    USB: fix cdc-acm regression in open
    cdc-acm needs to set a flag during open to tell the
    tty layer that the device is initialized
    
    Signed-off-by: Oliver Neukum <oliver@neukum.org>
    Cc: Marcel Holtmann <marcel@holtmann.org>
    Cc: Paul Martin <pm@debian.org>
    Cc: stable <stable@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/class/cdc-acm.c
+++ drivers/usb/class/cdc-acm.c
@@ -596,6 +596,7 @@ static int acm_tty_open(struct tty_struct *tty, struct file *filp)
 
 	acm->throttle = 0;
 
+	tasklet_schedule(&acm->urb_task);
 	set_bit(ASYNCB_INITIALIZED, &acm->port.flags);
 	rv = tty_port_block_til_ready(&acm->port, tty, filp);
 	tasklet_schedule(&acm->urb_task);
