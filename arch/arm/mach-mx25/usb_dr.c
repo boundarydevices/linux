0b82680f068cbf7a452a31a5480e74be45dc387d
Huang Shijie <b32955@freescale.com>
2010-08-18 13:45:03 +0800

    ENGR00132249 USB:Fix dead loop when cat iomem
      Apply this fix for mx25.  Has been seperately cherry-picked for
    other platforms.
    
      The origin code reuse the otg_resource as the resource for udc
    device. This will cause a small bug, the following command will
    print endless log:
    	#cat /proc/iomem
    
      The reason of the bug is that reuse the otg_resources for the
    udc device will register the otg_resources twice which will make
    the ->child and ->parent of the otg_resources pointer to himself.
    So the above command will traverse in a dead loop for ever.
    
    Signed-off-by: Huang Shijie <b32955@freescale.com>

--- arch/arm/mach-mx25/usb_dr.c
+++ arch/arm/mach-mx25/usb_dr.c
@@ -39,7 +39,7 @@ static struct fsl_usb2_platform_data __maybe_unused dr_utmi_config = {
 };
 
 /*
- * resources
+ * OTG resources
  */
 static struct resource otg_resources[] = {
 	[0] = {
@@ -52,6 +52,22 @@ static struct resource otg_resources[] = {
 	       .flags = IORESOURCE_IRQ,
 	       },
 };
+
+/*
+ * UDC resources (same as OTG resource)
+ */
+static struct resource udc_resources[] = {
+	[0] = {
+	       .start = (u32)(USB_OTGREGS_BASE),
+	       .end   = (u32)(USB_OTGREGS_BASE + 0x1ff),
+	       .flags = IORESOURCE_MEM,
+	       },
+	[1] = {
+	       .start = MXC_INT_USB_OTG,
+	       .flags = IORESOURCE_IRQ,
+	       },
+};
+
 
 static u64 dr_udc_dmamask = ~(u32) 0;
 static void dr_udc_release(struct device *dev)
@@ -75,8 +91,8 @@ static struct platform_device __maybe_unused dr_udc_device = {
 		.dma_mask          = &dr_udc_dmamask,
 		.coherent_dma_mask = 0xffffffff,
 	},
-	.resource      = otg_resources,
-	.num_resources = ARRAY_SIZE(otg_resources),
+	.resource      = udc_resources,
+	.num_resources = ARRAY_SIZE(udc_resources),
 };
 
 static struct platform_device __maybe_unused dr_otg_device = {
