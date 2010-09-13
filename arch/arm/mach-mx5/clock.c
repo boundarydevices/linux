0481981acb9a83cc2c7d17e145338965d3a78edd
Shen Yong <b00984@freescale.com>
2010-09-13 11:33:47 +0800

    ENGR00127275 mx53: add clock depentent
    First, retore the ocram bit, pl301 bits to previous value 3,
    so most functions like fec, audio and video would not be blocked.
    Add clock dependency in the coming commits .
    
    Signed-off-by: Shen Yong <b00984@freescale.com>

--- arch/arm/mach-mx5/clock.c
+++ arch/arm/mach-mx5/clock.c
@@ -4589,12 +4589,12 @@ int __init mx53_clocks_init(unsigned long ckil, unsigned long osc, unsigned long
 		     3 << MXC_CCM_CCGRx_CG11_OFFSET, MXC_CCM_CCGR5);
 
 	__raw_writel(1 << MXC_CCM_CCGRx_CG0_OFFSET |
-				1 << MXC_CCM_CCGRx_CG1_OFFSET |
+				3 << MXC_CCM_CCGRx_CG1_OFFSET |
 				1 << MXC_CCM_CCGRx_CG4_OFFSET |
 				1 << MXC_CCM_CCGRx_CG8_OFFSET |
 				1 << MXC_CCM_CCGRx_CG9_OFFSET |
-				1 << MXC_CCM_CCGRx_CG12_OFFSET |
-				1 << MXC_CCM_CCGRx_CG13_OFFSET , MXC_CCM_CCGR6);
+				3 << MXC_CCM_CCGRx_CG12_OFFSET |
+				3 << MXC_CCM_CCGRx_CG13_OFFSET , MXC_CCM_CCGR6);
 
 	__raw_writel(0, MXC_CCM_CCGR7);
 
