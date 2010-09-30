d813a1d8d9c12570903ba1a17b589f89a8ddcde2
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2010-09-30 10:17:54 -0300

    [media] uvcvideo: Move to video_ioctl2
    Simplify ioctl handling by using video_ioctl2.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/usb/uvc/uvc_driver.c
+++ drivers/media/usb/uvc/uvc_driver.c
@@ -1761,6 +1761,7 @@ static int uvc_register_video(struct uvc_device *dev,
 	 */
 	vdev->v4l2_dev = &dev->vdev;
 	vdev->fops = &uvc_fops;
+	vdev->ioctl_ops = &uvc_ioctl_ops;
 	vdev->release = uvc_release;
 	vdev->prio = &stream->chain->prio;
 	set_bit(V4L2_FL_USE_FH_PRIO, &vdev->flags);
