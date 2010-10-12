393821f2871e8206b5a71017d52a366736483a13
Hu hui <b29976@freescale.com>
2010-10-12 14:05:13 +0800

    ENGR00132477-2 IMX USB:fix some wakeup releated issues
    Common Part
    move the is_b_host detecting from common code to the driver,so keep
    common code be same as the community
    
    Signed-off-by: Hu Hui <b29976@freescale.com>

--- drivers/usb/core/driver.c
+++ drivers/usb/core/driver.c
@@ -1313,13 +1313,6 @@ int usb_resume(struct device *dev, pm_message_t msg)
 	struct usb_device	*udev = to_usb_device(dev);
 	int			status;
 
-	/*
-	 * At otg mode, it should not call host resume for usb gadget device
-	 * Otherwise, this usb device can't be recognized as a gadget
-	 */
-	if (udev->bus->is_b_host)
-		return 0;
-
 	/* For PM complete calls, all we do is rebind interfaces */
 	if (msg.event == PM_EVENT_ON) {
 		if (udev->state != USB_STATE_NOTATTACHED)
