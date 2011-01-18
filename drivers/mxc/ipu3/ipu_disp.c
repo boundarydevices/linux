11e6a53b54774d58b2aec74f83609e19543df034
Eric Nelson <eric.nelson@boundarydevices.com>
2011-01-18 10:59:40 -0700

    Fix pixel clock polarity

--- drivers/mxc/ipu3/ipu_disp.c
+++ drivers/mxc/ipu3/ipu_disp.c
@@ -1147,7 +1147,7 @@ int32_t ipu_init_sync_panel(int disp, uint32_t pixel_clk,
 		return -EINVAL;
 	}
 
-	di_gen = DI_GEN_DI_PIXCLK_ACTH;
+	di_gen = sig.clk_pol ? DI_GEN_DI_PIXCLK_ACTH : 0 ;
 
 	if (sig.interlaced) {
 		if (g_ipu_hw_rev >= 2) {
