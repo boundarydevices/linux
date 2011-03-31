e00348c38d2e59925781b2b0d486a1c1801fc732
Eric Nelson <eric.nelson@boundarydevices.com>
2011-03-31 14:58:31 -0700

    ep0700m01 touch screen: change names to match driver and proc entry

--- drivers/input/touchscreen/ep0700m01.c
+++ drivers/input/touchscreen/ep0700m01.c
@@ -59,7 +59,7 @@ struct ep0700m01_ts {
 	unsigned gp;
 	struct proc_dir_entry *procentry;
 };
-static const char *client_name = "ep0700m01-ts";
+static const char *client_name = "ep0700m01";
 
 struct ep0700m01_ts* gts;
 
@@ -183,7 +183,7 @@ static inline int ts_register(struct ep0700m01_ts *ts)
 		return -ENOMEM;
 	}
 	ts->idev = idev;
-	idev->name      = "i2c_touch";
+	idev->name      = procentryname ;
 	idev->id.product = ts->client->addr;
 	idev->open      = ts_open;
 	idev->close     = ts_close;
@@ -483,7 +483,7 @@ static void __exit ts_exit(void)
 }
 
 MODULE_AUTHOR("Troy Kisky <troy.kisky@boundarydevices.com>");
-MODULE_DESCRIPTION("I2C interface for ep0700m01 touch screen controller.");
+MODULE_DESCRIPTION("I2C interface for Emerging Display Technologies ep0700m01 touch screen controller.");
 MODULE_LICENSE("GPL");
 
 module_init(ts_init)
