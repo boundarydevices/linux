7b086756d805c83ccddae02fcc8885222818c7f6
Lily Zhang <r58066@freescale.com>
2011-04-14 14:18:43 +0800

    ENGR00142005-2 ARM: imx: Add LDB device
    Add LDB device into i.MX platform
    
    Signed-off-by: Lily Zhang <r58066@freescale.com>

---
+++ arch/arm/plat-mxc/devices/platform-imx_ldb.c
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2011 Freescale Semiconductor, Inc. All Rights Reserved.
+ */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <mach/hardware.h>
+#include <mach/devices-common.h>
+#include <linux/clk.h>
+
+#define imx_ldb_data_entry_single(soc, size)	\
+	{								\
+		.iobase = soc ## _IOMUXC_BASE_ADDR,			\
+		.iosize = size,						\
+	}
+
+#ifdef CONFIG_SOC_IMX53
+const struct imx_ldb_data imx53_ldb_data __initconst =
+			imx_ldb_data_entry_single(MX53, SZ_4K);
+#endif
+
+struct platform_device *__init imx_add_ldb(
+		const struct imx_ldb_data *data,
+		struct fsl_mxc_ldb_platform_data *pdata)
+{
+	struct resource res[] = {
+		{
+			.start = data->iobase,
+			.end = data->iobase + data->iosize - 1,
+			.flags = IORESOURCE_MEM,
+		},
+	};
+
+	return imx_add_platform_device("mxc_ldb", -1,
+			res, ARRAY_SIZE(res), pdata, sizeof(*pdata));
+}
+
