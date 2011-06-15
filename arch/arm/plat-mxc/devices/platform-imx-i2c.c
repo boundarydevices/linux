7bd96d6bc88519e25d97419f28a7c44691a28c54
Jason Liu <r64343@freescale.com>
2011-06-16 01:16:17 +0800

    ENGR00139200 MX6Q: Add I2C3,I2C2 device
    Add I2C3 device and MAX7310
    Add I2C2 device
    
    Signed-off-by: Lily Zhang <r58066@freescale.com>
    Signed-off-by: Jason Liu <r64343@freescale.com>
    Signed-off-by: Frank Li <Frank.Li@freescale.com>

--- arch/arm/plat-mxc/devices/platform-imx-i2c.c
+++ arch/arm/plat-mxc/devices/platform-imx-i2c.c
@@ -98,6 +98,16 @@ const struct imx_imx_i2c_data imx53_imx_i2c_data[] __initconst = {
 };
 #endif /* ifdef CONFIG_SOC_IMX51 */
 
+#ifdef CONFIG_SOC_IMX6Q
+const struct imx_imx_i2c_data imx6q_imx_i2c_data[] __initconst = {
+#define imx6q_imx_i2c_data_entry(_id, _hwid)				\
+	imx_imx_i2c_data_entry(MX6Q, _id, _hwid, SZ_4K)
+	imx6q_imx_i2c_data_entry(0, 1),
+	imx6q_imx_i2c_data_entry(1, 2),
+	imx6q_imx_i2c_data_entry(2, 3),
+};
+#endif /* ifdef CONFIG_SOC_IMX6Q */
+
 struct platform_device *__init imx_add_imx_i2c(
 		const struct imx_imx_i2c_data *data,
 		const struct imxi2c_platform_data *pdata)
