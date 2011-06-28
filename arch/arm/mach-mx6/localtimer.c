782c865f31dcd9ab22f918bedf96be17bf6c3e1c
Zeng Zhaoming <b32542@freescale.com>
2011-06-28 10:23:16 +0800

    ENGR00139229-2 MX6: Bring up i.MX6 sabreauto with Quad cores
    MSL code for bring up MX6 sabreauto board with Quad core.
    Enable cpu core local timer, add reset and enable cpu core control,
    and enable it in default config.
    
    Merged from testbuild:imx6_bringup branch.
    
    Signed-off-by: Anson Huang <b20788@freescale.com>
    Signed-off-by: Jason Liu <r64343@freescale.com>
    Signed-off-by: Zeng Zhaoming <b32542@freescale.com>
    
    Merged-by: Zeng Zhaoming <b32542@freescale.com>

---
+++ arch/arm/mach-mx6/localtimer.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2011 Freescale Semiconductor, Inc. All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <linux/init.h>
+#include <linux/smp.h>
+#include <linux/clockchips.h>
+#include <asm/smp_twd.h>
+#include <asm/localtimer.h>
+#include <mach/irqs.h>
+#include <mach/hardware.h>
+
+
+/*
+ * Setup the local clock events for a CPU.
+ */
+void __cpuinit local_timer_setup(struct clock_event_device *evt)
+{
+	evt->irq = IRQ_LOCALTIMER;
+	twd_timer_setup(evt);
+}
