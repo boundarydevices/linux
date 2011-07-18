757b9401efd4214bad4789115941079a9b745a7e
Tony Lin <tony.lin@freescale.com>
2011-07-18 13:46:23 +0800

    ENGR00153221 [MX6Q uSDHC]workaround for TC intr coming ealier than DMA intr
    if TC interrupt bit is set but DMA interrupt bit is clear, read status register
    again in case DMA interrupt will come in next time cycle
    
    Signed-off-by: Tony Lin <tony.lin@freescale.com>

--- drivers/mmc/host/sdhci-esdhc-imx.c
+++ drivers/mmc/host/sdhci-esdhc-imx.c
@@ -99,6 +99,17 @@ static u32 esdhc_readl_le(struct sdhci_host *host, int reg)
 			val |= SDHCI_CARD_PRESENT;
 	}
 
+	if (reg == SDHCI_INT_STATUS && cpu_is_mx6q())
+		/*
+		 * on mx6q, there is low possibility that
+		 * DATA END interrupt comes ealier than DMA
+		 * END interrupt which is conflict with standard
+		 * host controller spec. In this case, read the
+		 * status register again will workaround this issue.
+		 */
+		if ((val & SDHCI_INT_DATA_END) && \
+			!(val & SDHCI_INT_DMA_END))
+			val = readl(host->ioaddr + reg);
 	return val;
 }
 
