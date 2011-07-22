133b433fc41314847d8a3c142389f7e6d2320201
Huang Shijie <b32955@freescale.com>
2011-07-22 10:28:03 +0800

    ENGR00139247-4 ARM: add APBH-DMA arch code for imx6q
    add the arch code for APBH-DMA.
    
    Signed-off-by: Huang Shijie <b32955@freescale.com>

--- arch/arm/mach-mx6/devices-imx6q.h
+++ arch/arm/mach-mx6/devices-imx6q.h
@@ -32,6 +32,9 @@ imx6q_anatop_thermal_imx_data __initconst;
 #define imx6q_add_anatop_thermal_imx(id, pdata)	\
 	imx_add_anatop_thermal_imx(&imx6q_anatop_thermal_imx_data, pdata)
 
+extern const struct imx_dma_data imx6q_dma_data __initconst;
+#define imx6q_add_dma()	imx_add_dma(&imx6q_dma_data);
+
 #define imx6q_add_gpmi(platform_data)	imx_add_gpmi(platform_data);
 
 extern const struct imx_fec_data imx6q_fec_data __initconst;
