53bd43e879c51d30c5669c07005812a23442f93c
Lionel Xu <Lionel.Xu@freescale.com>
2011-07-25 21:44:45 +0800

    ENGR00153651-1 ESAI: Prepare MSL support for esai/cs42888 audio codec driver
    1) Add machine specific code for esai/cs42888 driver support, including pad
        control, clk setting, i2c setting, etc.
    2) Enable audio support in default config.
    
    Signed-off-by: Lionel Xu <R63889@freescale.com>

---
+++ arch/arm/plat-mxc/devices/platform-imx-esai.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2011 Freescale Semiconductor, Inc. All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ */
+
+#include <mach/hardware.h>
+#include <mach/devices-common.h>
+
+#define MX6Q_DMA_REQ_ESAI_RX            23
+#define MX6Q_DMA_REQ_ESAI_TX            24
+
+#define imx_imx_esai_data_entry(soc, _id, _size)			\
+	[_id] = {							\
+		.id = _id,						\
+		.iobase = ESAI1_BASE_ADDR,		\
+		.iosize = _size,					\
+		.irq = MXC_INT_ESAI,			\
+		.dmatx = soc ## _DMA_REQ_ESAI ## _TX,		\
+		.dmarx = soc ## _DMA_REQ_ESAI ## _RX,		\
+	}
+
+#ifdef CONFIG_SOC_IMX6Q
+const struct imx_imx_esai_data imx6q_imx_esai_data[] __initconst = {
+#define imx6q_imx_esai_data_entry(_id)				\
+	imx_imx_esai_data_entry(MX6Q, _id, SZ_4K)
+	imx6q_imx_esai_data_entry(0),
+};
+#endif /* ifdef CONFIG_SOC_IMX6Q */
+
+struct platform_device *__init imx_add_imx_esai(
+		const struct imx_imx_esai_data *data,
+		const struct imx_esai_platform_data *pdata)
+{
+	struct resource res[] = {
+		{
+			.start = data->iobase,
+			.end = data->iobase + data->iosize - 1,
+			.flags = IORESOURCE_MEM,
+		}, {
+			.start = data->irq,
+			.end = data->irq,
+			.flags = IORESOURCE_IRQ,
+		},
+#define DMARES(_name) {							\
+	.name = #_name,							\
+	.start = data->dma ## _name,					\
+	.end = data->dma ## _name,					\
+	.flags = IORESOURCE_DMA,					\
+}
+		DMARES(tx),
+		DMARES(rx),
+	};
+
+	return imx_add_platform_device("imx-esai", data->id,
+			res, ARRAY_SIZE(res),
+			pdata, sizeof(*pdata));
+}
