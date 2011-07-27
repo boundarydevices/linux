29c7d99fcadef76e9afeee256962627047da3713
Jason Chen <b02280@freescale.com>
2011-07-27 14:17:40 +0800

    ENGR00153762 imx6q MSL: increase DMA zone size to max 184
    when we need enable 1080p 32bpp display and play a 1080p h264 clip,
    it may meet memory allocation fail issue. This patch fix this issue.
    
    Signed-off-by: Jason Chen <b02280@freescale.com>

--- arch/arm/plat-mxc/include/mach/memory.h
+++ arch/arm/plat-mxc/include/mach/memory.h
@@ -63,8 +63,10 @@
 #define CONSISTENT_DMA_SIZE SZ_4M
 #else
 
-#if defined(CONFIG_ARCH_MX5) || defined(CONFIG_ARCH_MX6)
+#if defined(CONFIG_ARCH_MX5)
 #define CONSISTENT_DMA_SIZE     (96 * SZ_1M)
+#elif defined(CONFIG_ARCH_MX6)
+#define CONSISTENT_DMA_SIZE     (184 * SZ_1M)
 #else
 #define CONSISTENT_DMA_SIZE     (32 * SZ_1M)
 #endif
