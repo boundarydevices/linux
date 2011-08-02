2830baec62492050b35a1c150a611bb1adc510eb
Anson Huang <b20788@freescale.com>
2011-08-02 17:24:15 +0800

    ENGR00154056-2 [MX6]Enable dormant mode in suspend
    1. Enable dormant mode in suspend, which means arm
    core will be powered off when enter wfi, the latest
    command for stop mode and dormant mode are as below:
    
    echo standby > /sys/power/state
    	-> stop mode with arm core power on
    echo mem > /sys/power/state
    	-> stop mode with arm core power off
    
    2. Remove all iram related code in suspend.
    
    Signed-off-by: Anson Huang <b20788@freescale.com>

--- arch/arm/mach-mx6/pm.c
+++ arch/arm/mach-mx6/pm.c
@@ -29,22 +29,30 @@
 #include <asm/mach/map.h>
 #include <mach/hardware.h>
 #include <mach/imx-pm.h>
-#ifdef CONFIG_ARCH_MX61
+#include <asm/hardware/cache-l2x0.h>
+#include <asm/hardware/gic.h>
+#ifdef CONFIG_ARCH_MX6Q
 #include <mach/iomux-mx6q.h>
 #endif
 #include "crm_regs.h"
+#include "src-reg.h"
 
-#define SCU_CTRL_OFFSET		0x00
-#define GPC_IMR1_OFFSET		0x08
-#define GPC_IMR2_OFFSET		0x0c
-#define GPC_IMR3_OFFSET		0x10
-#define GPC_IMR4_OFFSET		0x14
+#define SCU_CTRL_OFFSET				0x00
+#define GPC_IMR1_OFFSET				0x08
+#define GPC_IMR2_OFFSET				0x0c
+#define GPC_IMR3_OFFSET				0x10
+#define GPC_IMR4_OFFSET				0x14
+#define GPC_PGC_CPU_PDN_OFFSET		0x2a0
 #define GPC_PGC_CPU_PUPSCR_OFFSET	0x2a4
 #define GPC_PGC_CPU_PDNSCR_OFFSET	0x2a8
-#define UART_UCR3_OFFSET		0x88
-#define UART_USR1_OFFSET		0x94
-#define UART_UCR3_AWAKEN		(1 << 4)
-#define UART_USR1_AWAKE			(1 << 4)
+#define UART_UCR3_OFFSET			0x88
+#define UART_USR1_OFFSET			0x94
+#define UART_UCR3_AWAKEN			(1 << 4)
+#define UART_USR1_AWAKE				(1 << 4)
+#define LOCAL_TWD_LOAD_OFFSET		0x0
+#define LOCAL_TWD_COUNT_OFFSET		0x4
+#define LOCAL_TWD_CONTROL_OFFSET	0x8
+#define LOCAL_TWD_INT_OFFSET		0xc
 static struct clk *cpu_clk;
 static struct pm_platform_data *pm_data;
 
@@ -52,20 +60,23 @@ static struct pm_platform_data *pm_data;
 static int org_freq;
 extern int set_cpu_freq(int wp);
 #endif
-
-extern void mx6q_suspend(u32 m4if_addr);
+extern void mx6q_suspend(suspend_state_t state);
 static struct device *pm_dev;
 struct clk *gpc_dvfs_clk;
-void *suspend_iram_base;
-void (*suspend_in_iram)(void *sdclk_iomux_addr) = NULL;
-void __iomem *suspend_param1;
 
 static void __iomem *scu_base;
 static void __iomem *gpc_base;
-static u32 ccm_clpcr, scu_ctrl;
-static u32 gpc_imr[4], gpc_cpu_pup, gpc_cpu_pdn;
+static void __iomem *src_base;
+static void __iomem *local_twd_base;
+static void __iomem *pl310_base;
+static void __iomem *gic_dist_base;
+static void __iomem *gic_cpu_base;
 
-static void mx6_suspend_store()
+static u32 ccm_clpcr, scu_ctrl;
+static u32 gpc_imr[4], gpc_cpu_pup, gpc_cpu_pdn, gpc_cpu;
+static u32 local_timer[4];
+
+static void mx6_suspend_store(void)
 {
 	/* save some settings before suspend */
 	ccm_clpcr = __raw_readl(MXC_CCM_CLPCR);
@@ -76,9 +87,16 @@ static void mx6_suspend_store()
 	gpc_imr[3] = __raw_readl(gpc_base + GPC_IMR4_OFFSET);
 	gpc_cpu_pup = __raw_readl(gpc_base + GPC_PGC_CPU_PUPSCR_OFFSET);
 	gpc_cpu_pdn = __raw_readl(gpc_base + GPC_PGC_CPU_PDNSCR_OFFSET);
+	gpc_cpu = __raw_readl(gpc_base + GPC_PGC_CPU_PDN_OFFSET);
+#ifdef CONFIG_LOCAL_TIMERS
+	local_timer[0] = __raw_readl(local_twd_base + LOCAL_TWD_LOAD_OFFSET);
+	local_timer[1] = __raw_readl(local_twd_base + LOCAL_TWD_COUNT_OFFSET);
+	local_timer[2] = __raw_readl(local_twd_base + LOCAL_TWD_CONTROL_OFFSET);
+	local_timer[3] = __raw_readl(local_twd_base + LOCAL_TWD_INT_OFFSET);
+#endif
 }
 
-static void mx6_suspend_restore()
+static void mx6_suspend_restore(void)
 {
 	/* restore settings after suspend */
 	__raw_writel(ccm_clpcr, MXC_CCM_CLPCR);
@@ -89,6 +107,13 @@ static void mx6_suspend_restore()
 	__raw_writel(gpc_imr[3], gpc_base + GPC_IMR4_OFFSET);
 	__raw_writel(gpc_cpu_pup, gpc_base + GPC_PGC_CPU_PUPSCR_OFFSET);
 	__raw_writel(gpc_cpu_pdn, gpc_base + GPC_PGC_CPU_PDNSCR_OFFSET);
+	__raw_writel(gpc_cpu, gpc_base + GPC_PGC_CPU_PDN_OFFSET);
+#ifdef CONFIG_LOCAL_TIMERS
+	__raw_writel(local_timer[0], local_twd_base + LOCAL_TWD_LOAD_OFFSET);
+	__raw_writel(local_timer[1], local_twd_base + LOCAL_TWD_COUNT_OFFSET);
+	__raw_writel(local_timer[2], local_twd_base + LOCAL_TWD_CONTROL_OFFSET);
+	__raw_writel(local_timer[3], local_twd_base + LOCAL_TWD_INT_OFFSET);
+#endif
 }
 static int mx6_suspend_enter(suspend_state_t state)
 {
@@ -96,16 +121,16 @@ static int mx6_suspend_enter(suspend_state_t state)
 
 	switch (state) {
 	case PM_SUSPEND_MEM:
-		mxc_cpu_lp_set(STOP_POWER_OFF);
+		mxc_cpu_lp_set(ARM_POWER_OFF);
 		break;
 	case PM_SUSPEND_STANDBY:
-		mxc_cpu_lp_set(WAIT_UNCLOCKED_POWER_OFF);
+		mxc_cpu_lp_set(STOP_POWER_OFF);
 		break;
 	default:
 		return -EINVAL;
 	}
 
-	if (state == PM_SUSPEND_MEM) {
+	if (state == PM_SUSPEND_MEM || state == PM_SUSPEND_STANDBY) {
 		if (pm_data && pm_data->suspend_enter)
 			pm_data->suspend_enter();
 
@@ -113,14 +138,42 @@ static int mx6_suspend_enter(suspend_state_t state)
 		flush_cache_all();
 		outer_cache.flush_all();
 
-		suspend_in_iram(suspend_param1);
+		/* for dormant mode, we need to disable l2 cache */
+		if (state == PM_SUSPEND_MEM)
+			outer_cache.disable();
+
+		/* mx6q mmdc can enter self-refresh when ARM enter wfi
+		 * , so no need to run the code in the iram */
+		mx6q_suspend(state);
+
+		if (state == PM_SUSPEND_MEM) {
+			/* need to re-init gic */
+			gic_init(0, 29, gic_dist_base, gic_cpu_base);
+
+#ifdef CONFIG_LOCAL_TIMERS
+			gic_enable_ppi(IRQ_LOCALTIMER);
+#endif
+			/* if we use gpt as system timer, we need to
+			 * enable gpt timer here, and even LOCAL_TIMERS
+			 * defined, but if we only boot up a core, then
+			 * kernel will still use GPT as timer */
+			__raw_writel(1 << (MXC_INT_GPT % 32),
+					gic_dist_base + GIC_DIST_ENABLE_SET +
+					(MXC_INT_GPT / 32) * 4);
+
+			flush_cache_all();
+			/* init l2 cache, pl310 */
+			l2x0_init(pl310_base, 0x0, ~0x00000000);
+		}
 
 		mx6_suspend_restore();
+
 		if (pm_data && pm_data->suspend_exit)
 			pm_data->suspend_exit();
 	} else {
 			cpu_do_idle();
 	}
+
 	return 0;
 }
 
@@ -191,10 +244,13 @@ static struct platform_driver mx6_pm_driver = {
 
 static int __init pm_init(void)
 {
-	unsigned long iram_paddr, cpaddr;
-
 	scu_base = IO_ADDRESS(SCU_BASE_ADDR);
 	gpc_base = IO_ADDRESS(GPC_BASE_ADDR);
+	src_base = IO_ADDRESS(SRC_BASE_ADDR);
+	pl310_base = IO_ADDRESS(L2_BASE_ADDR);
+	gic_dist_base = IO_ADDRESS(IC_DISTRIBUTOR_BASE_ADDR);
+	gic_cpu_base = IO_ADDRESS(IC_INTERFACES_BASE_ADDR);
+	local_twd_base = IO_ADDRESS(LOCAL_TWD_ADDR);
 
 	pr_info("Static Power Management for Freescale i.MX6\n");
 
@@ -204,23 +260,6 @@ static int __init pm_init(void)
 	}
 
 	suspend_set_ops(&mx6_suspend_ops);
-	/* Move suspend routine into iRAM */
-	cpaddr = (unsigned long)iram_alloc(SZ_4K, &iram_paddr);
-	/* Need to remap the area here since we want the memory region
-		 to be executable. */
-	suspend_iram_base = __arm_ioremap(iram_paddr, SZ_4K,
-					  MT_HIGH_VECTORS);
-	pr_info("cpaddr = %x suspend_iram_base=%x\n",
-		(unsigned int)cpaddr, (unsigned int)suspend_iram_base);
-
-	/*
-	 * Need to run the suspend code from IRAM as the DDR needs
-	 * to be put into self refresh mode manually.
-	 */
-	memcpy((void *)cpaddr, mx6q_suspend, SZ_4K);
-
-	suspend_in_iram = (void *)suspend_iram_base;
-
 	cpu_clk = clk_get(NULL, "cpu_clk");
 	if (IS_ERR(cpu_clk)) {
 		printk(KERN_DEBUG "%s: failed to get cpu_clk\n", __func__);
@@ -230,7 +269,6 @@ static int __init pm_init(void)
 
 	return 0;
 }
-
 
 static void __exit pm_cleanup(void)
 {
