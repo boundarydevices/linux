1e6c55598fd26f8fbec2c5a32b663d10df38c973
Tony Lin <tony.lin@freescale.com>
2011-08-11 17:35:20 +0800

    ENGR00139261 [MX6Q]support 8 bit MMC and eMMC DDR mode
    enable 8 bit MMC mode according to mmc stack.
    enable eMMC DDR mode according to mmc stack, but change
    sdhci a little, since sdhci does not support DDR mode so
    far.
    
    Signed-off-by: Tony Lin <tony.lin@freescale.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -1089,6 +1089,7 @@ void mmc_power_off(struct mmc_host *host)
 	host->ios.power_mode = MMC_POWER_OFF;
 	host->ios.bus_width = MMC_BUS_WIDTH_1;
 	host->ios.timing = MMC_TIMING_LEGACY;
+	host->ios.ddr = MMC_SDR_MODE;
 	mmc_set_ios(host);
 
 	mmc_host_clk_release(host);
