32a002073901aaae9f8bf19028e515fd91b775e7
Tony Lin <tony.lin@freescale.com>
2011-08-22 19:50:19 +0800

    ENGR00139260-2 [ath6k wifi]add some delay after resuming
    add some delay after resuming.
    
    Signed-off-by: Tony Lin <tony.lin@freescale.com>

--- drivers/staging/ath6kl/os/linux/ar6000_pm.c
+++ drivers/staging/ath6kl/os/linux/ar6000_pm.c
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2004-2010 Atheros Communications Inc.
+ * Copyright (c) 2004-2011 Atheros Communications Inc.
  * All rights reserved.
  *
  * 
@@ -267,7 +267,11 @@ int ar6000_resume_ev(void *context)
         AR_DEBUG_PRINTF(ATH_DEBUG_ERR, ("Strange SDIO bus power mode!!\n"));
         break;
     }
-    return 0;
+#ifdef CONFIG_HAS_WAKELOCK
+    wake_unlock(&ar6k_suspend_wake_lock);
+#endif
+    mdelay(100);
+    return A_OK;
 }
 
 void ar6000_check_wow_status(struct ar6_softc *ar, struct sk_buff *skb, bool isEvent)
