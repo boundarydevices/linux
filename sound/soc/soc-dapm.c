7dd0a784922ab26bf136135f9547de9a8d4748c2
Peter Ujfalusi <peter.ujfalusi@ti.com>
2011-08-26 16:33:52 +0300

    ASoC: soc-dapm: Fix parameter comment for snd_soc_dapm_free
    We have dapm_context instead of codec parameter.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -2781,7 +2781,7 @@ EXPORT_SYMBOL_GPL(snd_soc_dapm_ignore_suspend);
 
 /**
  * snd_soc_dapm_free - free dapm resources
- * @card: SoC device
+ * @dapm: DAPM context
  *
  * Free all dapm widgets and resources.
  */
