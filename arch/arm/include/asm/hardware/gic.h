ae4dc788acb72762b9cec03e99cfb86242adb03d
Rob Herring <rob.herring@calxeda.com>
2011-09-28 21:27:52 -0500

    ARM: gic: add OF based initialization
    This adds ARM gic interrupt controller initialization using device tree
    data.
    
    The initialization function is intended to be called by of_irq_init
    function like this:
    
    const static struct of_device_id irq_match[] = {
    	{ .compatible = "arm,cortex-a9-gic", .data = gic_of_init, },
    	{}
    };
    
    static void __init init_irqs(void)
    {
    	of_irq_init(irq_match);
    }
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>
    Reviewed-by: Jamie Iles <jamie@jamieiles.com>
    Tested-by: Thomas Abraham <thomas.abraham@linaro.org>
    Acked-by: Grant Likely <grant.likely@secretlab.ca>

--- arch/arm/include/asm/hardware/gic.h
+++ arch/arm/include/asm/hardware/gic.h
@@ -66,6 +66,7 @@ struct gic_cpu_state {
 };
 
 void gic_init(unsigned int, unsigned int, void __iomem *, void __iomem *);
+int gic_of_init(struct device_node *node, struct device_node *parent);
 void gic_secondary_init(unsigned int);
 void gic_cascade_irq(unsigned int gic_nr, unsigned int irq);
 void gic_raise_softirq(const struct cpumask *mask, unsigned int irq);
