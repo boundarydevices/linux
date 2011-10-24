f5e95104cbab29f691fe2315cde8280fd8e9ab04
Mahesh Mahadevan <r9aadq@freescale.com>
2011-10-24 09:46:13 -0500

    ENGR00160701 Fix the Micrel PHY driver
    Received a fix from Micrel to fix their driver. Without this
    patch the Micrel PHY does not get recognized during boot
    
    Signed-off-by: Mahesh Mahadevan <r9aadq@freescale.com>

--- drivers/net/phy/micrel.c
+++ drivers/net/phy/micrel.c
@@ -116,7 +116,7 @@ static int ks8051_config_init(struct phy_device *phydev)
 
 static struct phy_driver ks8737_driver = {
 	.phy_id		= PHY_ID_KS8737,
-	.phy_id_mask	= 0x00fffff0,
+	.phy_id_mask	= 0x00ffffff,
 	.name		= "Micrel KS8737",
 	.features	= (PHY_BASIC_FEATURES | SUPPORTED_Pause),
 	.flags		= PHY_HAS_MAGICANEG | PHY_HAS_INTERRUPT,
@@ -130,7 +130,7 @@ static struct phy_driver ks8737_driver = {
 
 static struct phy_driver ks8041_driver = {
 	.phy_id		= PHY_ID_KS8041,
-	.phy_id_mask	= 0x00fffff0,
+	.phy_id_mask	= 0x00ffffff,
 	.name		= "Micrel KS8041",
 	.features	= (PHY_BASIC_FEATURES | SUPPORTED_Pause
 				| SUPPORTED_Asym_Pause),
@@ -145,7 +145,7 @@ static struct phy_driver ks8041_driver = {
 
 static struct phy_driver ks8051_driver = {
 	.phy_id		= PHY_ID_KS8051,
-	.phy_id_mask	= 0x00fffff0,
+	.phy_id_mask	= 0x00ffffff,
 	.name		= "Micrel KS8051",
 	.features	= (PHY_BASIC_FEATURES | SUPPORTED_Pause
 				| SUPPORTED_Asym_Pause),
@@ -161,7 +161,7 @@ static struct phy_driver ks8051_driver = {
 static struct phy_driver ks8001_driver = {
 	.phy_id		= PHY_ID_KS8001,
 	.name		= "Micrel KS8001 or KS8721",
-	.phy_id_mask	= 0x00fffff0,
+	.phy_id_mask	= 0x00ffffff,
 	.features	= (PHY_BASIC_FEATURES | SUPPORTED_Pause),
 	.flags		= PHY_HAS_MAGICANEG | PHY_HAS_INTERRUPT,
 	.config_init	= kszphy_config_init,
@@ -174,7 +174,7 @@ static struct phy_driver ks8001_driver = {
 
 static struct phy_driver ksz9021_driver = {
 	.phy_id		= PHY_ID_KSZ9021,
-	.phy_id_mask	= 0x000fff10,
+	.phy_id_mask	= 0x00ffffff,
 	.name		= "Micrel KSZ9021 Gigabit PHY",
 	.features	= (PHY_GBIT_FEATURES | SUPPORTED_Pause
 				| SUPPORTED_Asym_Pause),
@@ -240,11 +240,11 @@ MODULE_AUTHOR("David J. Choi");
 MODULE_LICENSE("GPL");
 
 static struct mdio_device_id __maybe_unused micrel_tbl[] = {
-	{ PHY_ID_KSZ9021, 0x000fff10 },
-	{ PHY_ID_KS8001, 0x00fffff0 },
-	{ PHY_ID_KS8737, 0x00fffff0 },
-	{ PHY_ID_KS8041, 0x00fffff0 },
-	{ PHY_ID_KS8051, 0x00fffff0 },
+	{ PHY_ID_KSZ9021, 0x00ffffff },
+	{ PHY_ID_KS8001, 0x00ffffff },
+	{ PHY_ID_KS8737, 0x00ffffff },
+	{ PHY_ID_KS8041, 0x00ffffff },
+	{ PHY_ID_KS8051, 0x00ffffff },
 	{ }
 };
 
