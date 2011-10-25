2eb269a6357eb93c1dbf9922c8f0a7d11ef19906
Alan Tull <alan.tull@freescale.com>
2011-10-25 15:41:55 -0500

    ENGR00160860-2 hdmi audio driver
    Audio driver for i.Mx built-in HDMI Transmitter.
    
    * Uses HDMI Transmitter's built-in DMA.
    * Adds IEC958-style digital audio header info to the raw audio.
    * Gets pixel clock from the IPU driver and calculates clock
      regenerator values (cts and N).
    * Move ipu_id, and disp_id from the HDMI's platform data to the
      HDMI mfd's platform data. Saves them in the hdmi mfd.
    * Add mfd functionality to update the clock regenerator values
      when the hdmi changes the pixel clock rate or when requested
      from the audio driver with a new audio sample rate.
    
    Signed-off-by: Alan Tull <alan.tull@freescale.com>

---
+++ sound/soc/imx/imx-hdmi.h
@@ -0,0 +1,89 @@
+/*
+ * MXC HDMI ALSA Soc Codec Driver
+ *
+ * Copyright (C) 2011 Freescale Semiconductor, Inc.
+ */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef __IMX_HDMI_H
+#define __IMX_HDMI_H
+
+#define DRV_NAME "imx-hdmi"
+
+struct imx_hdmi {
+	int irq;
+	struct platform_device *pdev;
+	struct platform_device *soc_platform_pdev;
+};
+
+#define MXC_HDMI_RATES_PLAYBACK			(SNDRV_PCM_RATE_32000 | \
+						 SNDRV_PCM_RATE_44100 | \
+						 SNDRV_PCM_RATE_48000 | \
+						 SNDRV_PCM_RATE_88200 | \
+						 SNDRV_PCM_RATE_96000)
+
+#define MXC_HDMI_FORMATS_PLAYBACK		(SNDRV_PCM_FMTBIT_S16_LE | \
+						 SNDRV_PCM_FMTBIT_S24_LE)
+
+typedef union {
+	uint64_t  U;
+	struct {
+		unsigned consumer:1;
+		unsigned linear_pcm:1;
+		unsigned copyright:1;
+		unsigned pre_emphasis:3;
+		unsigned mode:2;
+
+		unsigned category_code:8;
+
+		unsigned source:4;
+		unsigned channel:4;
+
+		unsigned sample_freq:4;
+		unsigned clock_acc:2;
+		unsigned reserved0:2;
+
+		unsigned word_length:4;
+		unsigned org_sample_freq:4;
+
+		unsigned cgms_a:2;
+		unsigned reserved1:6;
+
+		unsigned reserved2:8;
+
+		unsigned reserved3:8;
+	} B;
+	unsigned char status[8];
+} hdmi_audio_header_t;
+
+typedef union {
+	uint32_t  U;
+	struct {
+		unsigned data:24;
+		unsigned v:1;
+		unsigned u:1;
+		unsigned c:1;
+		unsigned p:1;
+		unsigned b:1;
+		unsigned reserved:3;
+	} B;
+} hdmi_audio_dma_data_t;
+
+extern hdmi_audio_header_t iec_header;
+
+#endif /* __IMX_HDMI_H */
