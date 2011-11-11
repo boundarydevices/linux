72ac27b17cc0761886f619c8a098753979011967
Gary Zhang <b13634@freescale.com>
2011-11-11 13:19:13 +0800

    ENGR00161608 SSI: support 24bit samplerate
    Add 24bit sample rate playback support for SSI and sgtl5000
    codec driver.
    
    Signed-off-by: Gary Zhang <b13634@freescale.com>

--- sound/soc/imx/imx-ssi.c
+++ sound/soc/imx/imx-ssi.c
@@ -53,6 +53,9 @@
 #include "imx-ssi.h"
 
 #define SSI_SACNT_DEFAULT (SSI_SACNT_AC97EN | SSI_SACNT_FV)
+#define IMX_SSI_FORMATS \
+	(SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S20_3LE | \
+	SNDRV_PCM_FMTBIT_S24_LE)
 
 /*
  * SSI Network Mode or TDM slots configuration.
@@ -465,13 +468,13 @@ static struct snd_soc_dai_driver imx_ssi_dai = {
 		.channels_min = 1,
 		.channels_max = 2,
 		.rates = SNDRV_PCM_RATE_8000_96000,
-		.formats = SNDRV_PCM_FMTBIT_S16_LE,
+		.formats = IMX_SSI_FORMATS,
 	},
 	.capture = {
 		.channels_min = 1,
 		.channels_max = 2,
 		.rates = SNDRV_PCM_RATE_8000_96000,
-		.formats = SNDRV_PCM_FMTBIT_S16_LE,
+		.formats = IMX_SSI_FORMATS,
 	},
 	.ops = &imx_ssi_pcm_dai_ops,
 };
