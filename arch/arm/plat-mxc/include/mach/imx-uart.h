d436a2da3c529642e1e4950c4095be094a7e659b
Lionel Xu <Lionel.Xu@freescale.com>
2011-11-16 16:57:27 +0800

    ENGR00139276-1 MX6 BT: Add basic bluetooth support to mx6 platform
    Add basic bluetooth support to mx6q platform:
    1) Enable bluetooth driver by default
    2) Configure uart2 which is used by bluetooth
    
    Signed-off-by: Lionel Xu <Lionel.Xu@freescale.com>

--- arch/arm/plat-mxc/include/mach/imx-uart.h
+++ arch/arm/plat-mxc/include/mach/imx-uart.h
@@ -21,6 +21,7 @@
 
 #define IMXUART_HAVE_RTSCTS (1<<0)
 #define IMXUART_IRDA        (1<<1)
+#define IMXUART_USE_DCEDTE  (1<<2)
 
 struct imxuart_platform_data {
 	int (*init)(struct platform_device *pdev);
