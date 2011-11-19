62c92ba4c66a1dacd654917e419cfc16396940dd
Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
2011-11-19 14:37:57 +0100

    PM / Suspend: Fix bug in suspend statistics update
    After commit 2a77c46de1e3dace73745015635ebbc648eca69c
    (PM / Suspend: Add statistics debugfs file for suspend to RAM)
    a missing pair of braces inside the state_store() function causes even
    invalid arguments to suspend to be wrongly treated as failed suspend
    attempts. Fix this.
    
    [rjw: Put the hash/subject of the buggy commit into the changelog.]
    
    Signed-off-by: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -295,7 +295,7 @@ static ssize_t state_store(struct kobject *kobj, struct kobj_attribute *attr,
 		if (*s && len == strlen(*s) && !strncmp(buf, *s, len))
 			break;
 	}
-	if (state < PM_SUSPEND_MAX && *s)
+	if (state < PM_SUSPEND_MAX && *s) {
 #ifdef CONFIG_EARLYSUSPEND
 		if (state == PM_SUSPEND_ON || valid_state(state)) {
 			error = 0;
@@ -309,6 +309,7 @@ static ssize_t state_store(struct kobject *kobj, struct kobj_attribute *attr,
 		} else
 			suspend_stats.success++;
 #endif
+	}
 #endif
 
  Exit:
