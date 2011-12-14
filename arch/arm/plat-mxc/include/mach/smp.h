298e7e67d721652e9c52c892bb237868790b57cb
Jason Liu <jason.hui@linaro.org>
2011-12-14 23:17:23 +0800

    fix the smp boot error
    Signed-off-by: Jason Liu <jason.hui@linaro.org>

--- arch/arm/plat-mxc/include/mach/smp.h
+++ arch/arm/plat-mxc/include/mach/smp.h
@@ -14,15 +14,5 @@
 
 /* Needed for secondary core boot */
 extern void mx6_secondary_startup(void);
-/*extern u32 mx6_modify_auxcoreboot0(u32 set_mask, u32 clear_mask);
-extern void mx6_auxcoreboot_addr(u32 cpu_addr);
-extern u32 mx6_read_auxcoreboot0(void);*/
 
-/*
- * We use Soft IRQ1 as the IPI
- */
-static inline void smp_cross_call(const struct cpumask *mask, int ipi)
-{
-	gic_raise_softirq(mask, ipi);
-}
 #endif
