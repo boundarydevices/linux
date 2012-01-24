8e46f7d92999249e2c38067b7777110633bcc244
Nancy Chen <Nancy.Chen@freescale.com>
2012-01-24 16:11:28 -0600

    ENGR00172676 MX6Q: Add suport for i.MX 6Q Sabre Smart Device
    Add suport for i.MX 6Quad SABRE Smart Device.
    Rename to SABRESD.
    
    Signed-off-by: Tony Lin <tony.lin@freescale.com>
    Signed-off-by: Nancy Chen <Nancy.Chen@freescale.com>

--- arch/arm/plat-mxc/include/mach/uncompress.h
+++ arch/arm/plat-mxc/include/mach/uncompress.h
@@ -68,6 +68,7 @@ static inline void flush(void)
 #define MX53_UART1_BASE_ADDR	0x53fbc000
 #define MX6Q_UART4_BASE_ADDR    0x021f0000
 #define MX6Q_UART2_BASE_ADDR    0x021e8000
+#define MX6Q_UART1_BASE_ADDR    0x02020000
 
 static __inline__ void __arch_decomp_setup(unsigned long arch_id)
 {
@@ -126,6 +127,9 @@ static __inline__ void __arch_decomp_setup(unsigned long arch_id)
 		break;
 	case MACH_TYPE_MX6Q_SABRELITE:
 		uart_base = MX6Q_UART2_BASE_ADDR;
+		break;
+	case MACH_TYPE_MX6Q_SABRESD:
+		uart_base = MX6Q_UART1_BASE_ADDR;
 		break;
 	case MACH_TYPE_MX6Q_SABREAUTO:
 		uart_base = MX6Q_UART4_BASE_ADDR;
