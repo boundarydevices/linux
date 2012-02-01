1f5a1728d76d3af116fcb71862baa0497898e64e
Huang Shijie <b32955@freescale.com>
2012-02-01 16:39:46 +0800

    ENGR00173731-5 MX6Q/ARCH :rename gpmi-nfc to gpmi-nand
    rename the gpmi-nfc to gpmi-nand.
    
    Signed-off-by: Huang Shijie <b32955@freescale.com>

--- arch/arm/plat-mxc/devices/platform-imx-gpmi-nfc.c
+++ arch/arm/plat-mxc/devices/platform-imx-gpmi-nfc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Freescale Semiconductor, Inc. All Rights Reserved.
+ * Copyright (C) 2011-2012 Freescale Semiconductor, Inc. All Rights Reserved.
  */
 
 /*
@@ -17,9 +17,9 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+#include <linux/mtd/gpmi-nand.h>
 #include <asm/sizes.h>
 #include <mach/hardware.h>
-#include <mach/gpmi-nfc.h>
 #include <mach/devices-common.h>
 
 #ifdef CONFIG_SOC_IMX50
@@ -67,42 +67,42 @@ imx_add_gpmi(const struct gpmi_nfc_platform_data *platform_data)
 
 #ifdef CONFIG_SOC_IMX6Q
 struct platform_device *__init
-imx_add_gpmi(const struct gpmi_nfc_platform_data *platform_data)
+imx_add_gpmi(const struct gpmi_nand_platform_data *platform_data)
 {
 	struct resource res[] = {
 	{ /* GPMI */
-		 .name  = GPMI_NFC_GPMI_REGS_ADDR_RES_NAME,
+		 .name  = GPMI_NAND_GPMI_REGS_ADDR_RES_NAME,
 		 .flags = IORESOURCE_MEM,
 		 .start = MX6Q_GPMI_BASE_ADDR,
 		 .end   = MX6Q_GPMI_BASE_ADDR + SZ_8K - 1,
 	}, {
-		 .name  = GPMI_NFC_GPMI_INTERRUPT_RES_NAME,
+		 .name  = GPMI_NAND_GPMI_INTERRUPT_RES_NAME,
 		 .flags = IORESOURCE_IRQ,
 		 .start	= MXC_INT_RAWNAND_GPMI,
 		 .end	= MXC_INT_RAWNAND_GPMI,
 	}, { /* BCH */
-		 .name  = GPMI_NFC_BCH_REGS_ADDR_RES_NAME,
+		 .name  = GPMI_NAND_BCH_REGS_ADDR_RES_NAME,
 		 .flags = IORESOURCE_MEM,
 		 .start = MX6Q_BCH_BASE_ADDR,
 		 .end   = MX6Q_BCH_BASE_ADDR + SZ_8K - 1,
 	}, {
-		 .name  = GPMI_NFC_BCH_INTERRUPT_RES_NAME,
+		 .name  = GPMI_NAND_BCH_INTERRUPT_RES_NAME,
 		 .flags = IORESOURCE_IRQ,
 		 .start	= MXC_INT_RAWNAND_BCH,
 		 .end	= MXC_INT_RAWNAND_BCH,
 	}, { /* DMA */
-		 .name  = GPMI_NFC_DMA_CHANNELS_RES_NAME,
+		 .name  = GPMI_NAND_DMA_CHANNELS_RES_NAME,
 		 .flags = IORESOURCE_DMA,
 		 .start	= MX6Q_DMA_CHANNEL_AHB_APBH_GPMI0,
 		 .end	= MX6Q_DMA_CHANNEL_AHB_APBH_GPMI7,
 	}, {
-		 .name  = GPMI_NFC_DMA_INTERRUPT_RES_NAME,
+		 .name  = GPMI_NAND_DMA_INTERRUPT_RES_NAME,
 		 .flags = IORESOURCE_IRQ,
 		 .start	= MXC_INT_APBHDMA_DMA,
 		 .end	= MXC_INT_APBHDMA_DMA,
 	}, };
 
-	return imx_add_platform_device_dmamask("imx6q-gpmi-nfc", 0,
+	return imx_add_platform_device_dmamask("imx6q-gpmi-nand", 0,
 			res, ARRAY_SIZE(res), platform_data,
 			sizeof(*platform_data), DMA_BIT_MASK(32));
 }
