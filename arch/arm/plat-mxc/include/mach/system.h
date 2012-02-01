a81f58ba465f71fe338c102a5941a9a3820e33dc
Huang Shijie <b32955@freescale.com>
2012-02-01 16:12:33 +0800

    ENGR00173731-4 MX6Q/ARCH : add mxs_reset_block()
    add mxs_reset_block() for mx6q.
    In order to keep the same code as the community, I reduce the
    parameters to one.
    
    Signed-off-by: Huang Shijie <b32955@freescale.com>

--- arch/arm/plat-mxc/include/mach/system.h
+++ arch/arm/plat-mxc/include/mach/system.h
@@ -1,7 +1,7 @@
 /*
  *  Copyright (C) 1999 ARM Limited
  *  Copyright (C) 2000 Deep Blue Solutions Ltd
- *  Copyright 2004-2011 Freescale Semiconductor, Inc. All Rights Reserved.
+ *  Copyright 2004-2012 Freescale Semiconductor, Inc. All Rights Reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,5 +25,4 @@ extern void mx5_cpu_lp_set(enum mxc_cpu_pwr_mode mode);
 void arch_idle(void);
 
 void arch_reset(char mode, const char *cmd);
-int mxs_reset_block(void __iomem *hwreg, int just_enable);
 #endif /* __ASM_ARCH_MXC_SYSTEM_H__ */
