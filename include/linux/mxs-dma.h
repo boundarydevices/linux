9863553c012eb39b962dd34297de550a02b04f1c
Huang Shijie <b32955@freescale.com>
2012-02-06 10:47:02 +0800

    ENGR00173731-1 mxs-dma : rename the dma.h for mxs-dma
    Move the header to a more common place.
    The mxs dma engine is not only used in mx23/mx28, but also used
    in mx50/mx6q.  It will also be used in the future chips.
    
    rename it to mxs-dma.h
    
    Acked-by: Lily Zhang <r58066@freescale.com>
    Signed-off-by: Huang Shijie <b32955@freescale.com>

--- include/linux/mxs-dma.h
+++ include/linux/mxs-dma.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 Freescale Semiconductor, Inc. All Rights Reserved.
+ * Copyright 2011-2012 Freescale Semiconductor, Inc. All Rights Reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
