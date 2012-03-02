560760de1b664f0983c176194db3837535f6bf5a
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-03-02 16:18:30 +0000

    ASoC: core: Split the union for CODEC/platform in the DAI
    There's now core code which falls back to global CODEC operations for
    DAI calls that needs to be able to tell if it's dealing with a CPU or
    CODEC DAI and given the small number of DAIs in a typical system and
    overall memory usage pattern saving a pointer per DAI is really not
    worth the effort.
    
    Reported-by: Ian Lartey <ian@opensource.wolfsonmicro.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Liam Girdwood <lrg@ti.com>

--- include/sound/soc-dai.h
+++ include/sound/soc-dai.h
@@ -253,10 +253,9 @@ struct snd_soc_dai {
 	unsigned int rate;
 
 	/* parent platform/codec */
-	union {
-		struct snd_soc_platform *platform;
-		struct snd_soc_codec *codec;
-	};
+	struct snd_soc_platform *platform;
+	struct snd_soc_codec *codec;
+
 	struct snd_soc_card *card;
 
 	struct list_head list;
