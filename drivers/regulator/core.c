ff6a25ec655895f309710dc0a30c95b617460b02
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-03-28 21:36:38 +0100

    regulator: core: Complain if we can't reenable a supply
    When cleaning up after a failed bulk_disable() we try to reenable any
    supplies that we did manage to disable - complain if we fail to do that
    when we try.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -2582,7 +2582,7 @@ int regulator_bulk_disable(int num_consumers,
 			   struct regulator_bulk_data *consumers)
 {
 	int i;
-	int ret;
+	int ret, r;
 
 	for (i = num_consumers - 1; i >= 0; --i) {
 		ret = regulator_disable(consumers[i].consumer);
@@ -2594,8 +2594,12 @@ int regulator_bulk_disable(int num_consumers,
 
 err:
 	pr_err("Failed to disable %s: %d\n", consumers[i].supply, ret);
-	for (++i; i < num_consumers; ++i)
-		regulator_enable(consumers[i].consumer);
+	for (++i; i < num_consumers; ++i) {
+		r = regulator_enable(consumers[i].consumer);
+		if (r != 0)
+			pr_err("Failed to reename %s: %d\n",
+			       consumers[i].supply, r);
+	}
 
 	return ret;
 }
