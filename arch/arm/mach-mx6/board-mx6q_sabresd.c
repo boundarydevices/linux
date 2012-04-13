5327f3f9288fc2682650b5afda44e2512ce865f3
Xinyu Chen <xinyu.chen@freescale.com>
2012-04-13 10:40:19 +0800

    ENGR00179594 mx6dl sabresd: register second framebuffer device
    Though mx6dl only has one IPU, it can still support two DIs.
    So two LVDS or LVDS+HDMI should be supported.
    
    Signed-off-by: Xinyu Chen <xinyu.chen@freescale.com>

--- arch/arm/mach-mx6/board-mx6q_sabresd.c
+++ arch/arm/mach-mx6/board-mx6q_sabresd.c
@@ -1534,13 +1534,10 @@ static void __init mx6_sabresd_board_init(void)
 	imx6q_add_mxc_hdmi_core(&hdmi_core_data);
 
 	imx6q_add_ipuv3(0, &ipu_data[0]);
-	if (cpu_is_mx6q()) {
+	if (cpu_is_mx6q())
 		imx6q_add_ipuv3(1, &ipu_data[1]);
-		for (i = 0; i < ARRAY_SIZE(sabresd_fb_data); i++)
-			imx6q_add_ipuv3fb(i, &sabresd_fb_data[i]);
-	} else
-		for (i = 0; i < (ARRAY_SIZE(sabresd_fb_data) + 1) / 2; i++)
-			imx6q_add_ipuv3fb(i, &sabresd_fb_data[i]);
+	for (i = 0; i < ARRAY_SIZE(sabresd_fb_data); i++)
+		imx6q_add_ipuv3fb(i, &sabresd_fb_data[i]);
 
 	imx6q_add_vdoa();
 	imx6q_add_lcdif(&lcdif_data);
