54f630743816bd77c2e2f8f00c9c3fbf28ab5e4e
Huang Shijie <b32955@freescale.com>
2012-06-05 17:26:35 +0800

    ENGR00212492 GPU: fix a macro error
    The default .config defines the GPU as a module:
      "CONFIG_MXC_GPU_VIV=m"
    
    In this case, we actually can not find the CONFIG_MXC_GPU_VIV.
    We should find CONFIG_MXC_GPU_VIV_MODULE instead.
    
    Signed-off-by: Huang Shijie <b32955@freescale.com>

--- arch/arm/mach-mx6/board-mx6q_sabrelite.c
+++ arch/arm/mach-mx6/board-mx6q_sabrelite.c
@@ -1202,7 +1202,7 @@ static struct sys_timer mx6_sabrelite_timer = {
 
 static void __init mx6q_sabrelite_reserve(void)
 {
-#ifdef CONFIG_MXC_GPU_VIV
+#if defined(CONFIG_MXC_GPU_VIV) || defined(CONFIG_MXC_GPU_VIV_MODULE)
 	phys_addr_t phys;
 
 	if (imx6q_gpu_pdata.reserved_mem_size) {
