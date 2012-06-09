feafc9862648e7261fa2e3f8db589e0979b4a323
Troy Kisky <troy.kisky@boundarydevices.com>
2012-06-08 17:33:57 -0700

    mx6q_sabrelite: add pcie support

--- arch/arm/mach-mx6/board-mx6q_sabrelite.c
+++ arch/arm/mach-mx6/board-mx6q_sabrelite.c
@@ -1258,6 +1258,13 @@ static struct mipi_csi2_platform_data mipi_csi2_pdata = {
 	.pixel_clk = "emi_clk",
 };
 
+static const struct imx_pcie_platform_data pcie_data  __initconst = {
+	.pcie_pwr_en	= -EINVAL,
+	.pcie_rst	= -EINVAL, //MX6Q_SABRELITE_CAP_TCH_INT1,
+	.pcie_wake_up	= -EINVAL,
+	.pcie_dis	= -EINVAL,
+};
+
 /*!
  * Board specific initialization.
  */
@@ -1411,6 +1418,7 @@ static void __init mx6_sabrelite_board_init(void)
 	clk_set_rate(clko2, rate);
 	clk_enable(clko2);
 
+	imx6q_add_pcie(&pcie_data);
 #ifdef CONFIG_WL12XX_PLATFORM_DATA
 	if (nitrogen6w) {
 		imx6q_add_mxc_pwm_backlight(0, &mx6_sabrelite_pwm0_backlight_data);
