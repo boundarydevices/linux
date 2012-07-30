5b91c6b56fce81df3afde913516d5e1f163f201c
Ryan QIAN <b32804@freescale.com>
2012-07-30 09:24:50 +0800

    ENGR00218754: mx6: fix build warning for sd pad configuration
    - fix build warning about uninitialization of sd_pads_50mhz, sd_pads_100mhz,
    and sd_pads_200mhz.
    
    affected soc:
    - mx6q arm2/sabreauto/sabrelite
    - mx6sl arm2
    
    Signed-off-by: Ryan QIAN <b32804@freescale.com>

--- arch/arm/mach-mx6/board-mx6sl_arm2.c
+++ arch/arm/mach-mx6/board-mx6sl_arm2.c
@@ -146,9 +146,9 @@ static int plt_sd_pad_change(unsigned int index, int clock)
 	/* LOW speed is the default state of SD pads */
 	static enum sd_pad_mode pad_mode = SD_PAD_MODE_LOW_SPEED;
 
-	iomux_v3_cfg_t *sd_pads_200mhz;
-	iomux_v3_cfg_t *sd_pads_100mhz;
-	iomux_v3_cfg_t *sd_pads_50mhz;
+	iomux_v3_cfg_t *sd_pads_200mhz = NULL;
+	iomux_v3_cfg_t *sd_pads_100mhz = NULL;
+	iomux_v3_cfg_t *sd_pads_50mhz = NULL;
 
 	u32 sd_pads_200mhz_cnt;
 	u32 sd_pads_100mhz_cnt;
