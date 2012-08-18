ff91bdb0d3a0c1433ef31e2f6eeb2bc229564c07
Yuxi Sun <b36102@freescale.com>
2012-08-18 13:16:44 +0800

    ENGR00220705 v4l2 overlay: Cancel the work structure schedule at overlay stop
    Add flush_work_sync and cancel_work_sync at the overlay stop to
    prevent moving data by DMA even when the space of those data address
    is freed.
    
    Signed-off-by: Yuxi Sun <b36102@freescale.com

--- drivers/media/video/mxc/capture/ipu_fg_overlay_sdc.c
+++ drivers/media/video/mxc/capture/ipu_fg_overlay_sdc.c
@@ -508,6 +508,8 @@ static int foreground_stop(void *private)
 	}
 #endif
 
+	flush_work_sync(&cam->csi_work_struct);
+	cancel_work_sync(&cam->csi_work_struct);
 	ipu_csi_enable_mclk_if(cam->ipu, CSI_MCLK_VF, cam->csi, false, false);
 
 	if (cam->vf_bufs_vaddr[0]) {
