ad29adf4cdc6d0a68247697256fd6ef603b38658
Xinyu Chen <xinyu.chen@freescale.com>
2012-11-08 14:22:19 +0800

    ENGR00230334-3 mx5x: refine the mxc_reset_block function declare
    The mxc_reset_block function declaration is
    different between mx6 and mx5 arch.
    
    Acked-by: Lily Zhang
    Signed-off-by: Xinyu Chen <xinyu.chen@freescale.com>

--- arch/arm/plat-mxc/include/mach/common.h
+++ arch/arm/plat-mxc/include/mach/common.h
@@ -89,7 +89,11 @@ extern void mx51_efikamx_reset(void);
 extern int mx53_revision(void);
 extern int mx50_revision(void);
 extern int mx53_display_revision(void);
+#ifdef CONFIG_ARCH_MX5
 extern int mxs_reset_block(void __iomem *, int);
+#else
+extern int mxs_reset_block(void __iomem *);
+#endif
 extern void early_console_setup(unsigned long base, struct clk *clk);
 extern void mx6_cpu_regulator_init(void);
 extern int mx6q_sabreauto_init_pfuze100(u32 int_gpio);
