98a5129e246eb69f4122b962a52017ba9c376925
Sheng Nan <b38800@freescale.com>
2012-12-05 10:22:35 +0800

    ENGR00235665: mxc_v4l2_capture: add YV12 format support in camera driver
    Android CTS verifier have a must requirement for YV12 format. Since IPUv3
    common driver has supported IPU_PIX_FMT_YVU420P pixel format, add the
    support of YV12 format in mxc_v4l2_capture.
    
    Signed-off-by: Sheng Nan <b38800@freescale.com>

--- drivers/media/video/mxc/capture/mxc_v4l2_capture.c
+++ drivers/media/video/mxc/capture/mxc_v4l2_capture.c
@@ -366,6 +366,7 @@ static inline int valid_mode(u32 palette)
 		(palette == V4L2_PIX_FMT_UYVY) ||
 		(palette == V4L2_PIX_FMT_YUYV) ||
 		(palette == V4L2_PIX_FMT_YUV420) ||
+		(palette == V4L2_PIX_FMT_YVU420) ||
 		(palette == V4L2_PIX_FMT_NV12));
 }
 
@@ -881,6 +882,7 @@ static int mxc_v4l2_s_fmt(cam_data *cam, struct v4l2_format *f)
 			bytesperline = f->fmt.pix.width * 2;
 			break;
 		case V4L2_PIX_FMT_YUV420:
+		case V4L2_PIX_FMT_YVU420:
 			size = f->fmt.pix.width * f->fmt.pix.height * 3 / 2;
 			bytesperline = f->fmt.pix.width;
 			break;
