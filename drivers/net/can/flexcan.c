2018c19f98635d29482ef56b5de5feb4d520c118
Dong Aisheng <b29396@freescale.com>
2012-12-10 10:32:56 +0800

    ENGR00240071-1 net: flexcan: using per clock for clock rate calculation
    The per clock is used for the output clock.
    
    Signed-off-by: Dong Aisheng <b29396@freescale.com>

--- drivers/net/can/flexcan.c
+++ drivers/net/can/flexcan.c
@@ -1014,7 +1014,6 @@ static int __devinit flexcan_probe(struct platform_device *pdev)
 			err = PTR_ERR(clk_ipg);
 			goto failed_clock;
 		}
-		clock_freq = clk_get_rate(clk_ipg);
 
 		clk_per = devm_clk_get(&pdev->dev, "per");
 		if (IS_ERR(clk_per)) {
@@ -1022,6 +1021,8 @@ static int __devinit flexcan_probe(struct platform_device *pdev)
 			err = PTR_ERR(clk_per);
 			goto failed_clock;
 		}
+
+		clock_freq = clk_get_rate(clk_per);
 	}
 
 	mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
