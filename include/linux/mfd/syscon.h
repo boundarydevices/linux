c6d5a55a4389e988af222883a0e267c54a098b21
Dong Aisheng <b29396@freescale.com>
2012-12-26 16:26:07 +0800

    ENGR00240071-9 mfd: syscon: add empty functions for !CONFIG_MFD_SYSCON build
    Add empty syscon_* APIs for !CONFIG_MFD_SYSCON build.
    
    Signed-off-by: Dong Aisheng <b29396@freescale.com>

--- include/linux/mfd/syscon.h
+++ include/linux/mfd/syscon.h
@@ -15,9 +15,29 @@
 #ifndef __LINUX_MFD_SYSCON_H__
 #define __LINUX_MFD_SYSCON_H__
 
+#ifdef CONFIG_MFD_SYSCON
 extern struct regmap *syscon_node_to_regmap(struct device_node *np);
 extern struct regmap *syscon_regmap_lookup_by_compatible(const char *s);
 extern struct regmap *syscon_regmap_lookup_by_phandle(
 					struct device_node *np,
 					const char *property);
+#else
+static inline struct regmap *syscon_node_to_regmap(struct device_node *np)
+{
+	return ERR_PTR(-ENODEV);
+}
+
+static inline struct regmap *syscon_regmap_lookup_by_compatible(const char *s)
+{
+	return ERR_PTR(-ENODEV);
+}
+
+static inline struct regmap *syscon_regmap_lookup_by_phandle(
+					struct device_node *np,
+					const char *property)
+{
+	return ERR_PTR(-ENODEV);
+}
+#endif
+
 #endif /* __LINUX_MFD_SYSCON_H__ */
