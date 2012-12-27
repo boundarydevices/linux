485bf63f9f09fc1453fdd412e706667bd2ed20c4
Sandor Yu <R01008@freescale.com>
2012-12-27 11:32:05 +0800

    ENGR00238357 MX6x Change HDMI default output RGB
    Change HDMI default output RGB
    
    Signed-off-by: Sandor Yu <R01008@freescale.com>

--- drivers/video/mxc_hdmi.c
+++ drivers/video/mxc_hdmi.c
@@ -1920,13 +1920,6 @@ static void mxc_hdmi_setup(struct mxc_hdmi *hdmi, unsigned long event)
 	hdmi->hdmi_data.enc_in_format = RGB;
 
 	hdmi->hdmi_data.enc_out_format = RGB;
-	/*DVI mode not support non-RGB */
-	if (!hdmi->hdmi_data.video_mode.mDVI) {
-		if (hdmi->edid_cfg.cea_ycbcr444)
-			hdmi->hdmi_data.enc_out_format = YCBCR444;
-		else if (hdmi->edid_cfg.cea_ycbcr422)
-			hdmi->hdmi_data.enc_out_format = YCBCR422_8BITS;
-	}
 
 	/* IPU not support depth color output */
 	hdmi->hdmi_data.enc_color_depth = 8;
