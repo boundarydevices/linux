34c7a9ed48f475dafbcbc7ce7821d83c53bcc179
Troy Kisky <troy.kisky@boundarydevices.com>
2013-01-21 13:21:37 -0700

    pcie: force gen1 speed
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- arch/arm/mach-mx6/pcie.c
+++ arch/arm/mach-mx6/pcie.c
@@ -1030,6 +1030,13 @@ static int __devinit imx_pcie_pltfm_probe(struct platform_device *pdev)
 	}
 
 	pr_info("PCIE: %s start link up.\n", __func__);
+	/*
+	 * Force to GEN1 because of PCIE2USB storage stress tests
+	 * would be failed when GEN2 is enabled
+	 */
+	writel(((readl(dbi_base + LNK_CAP) & 0xfffffff0) | 0x1),
+			dbi_base + LNK_CAP);
+
 	/* start link up */
 	imx_pcie_clrset(IOMUXC_GPR12_APP_LTSSM_ENABLE, 1 << 10, IOMUXC_GPR12);
 
