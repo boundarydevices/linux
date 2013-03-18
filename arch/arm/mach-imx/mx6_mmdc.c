294f667a5514a292ae27d3f601918d7a28ab0732
Anson Huang <b20788@freescale.com>
2013-03-18 17:00:36 -0400

    ENGR00254627-2: imx: improve coding style
    improve coding style:
    1. clean up comments;
    2. use tab instead of space.
    
    Signed-off-by: Anson Huang <b20788@freescale.com>

--- arch/arm/mach-imx/mx6_mmdc.c
+++ arch/arm/mach-imx/mx6_mmdc.c
@@ -50,7 +50,8 @@ void __iomem *iomux_base;
 void __iomem *gic_dist_base;
 void __iomem *gic_cpu_base;
 
-void (*mx6_change_ddr_freq)(u32 freq, void *ddr_settings, bool dll_mode, void* iomux_offsets) = NULL;
+void (*mx6_change_ddr_freq)(u32 freq, void *ddr_settings,
+	bool dll_mode, void *iomux_offsets) = NULL;
 
 extern unsigned int ddr_low_rate;
 extern unsigned int ddr_med_rate;
@@ -59,7 +60,8 @@ extern int low_bus_freq_mode;
 extern int audio_bus_freq_mode;
 extern int mmdc_med_rate;
 extern void __iomem *ccm_base;
-extern void mx6_ddr_freq_change(u32 freq, void *ddr_settings, bool dll_mode, void *iomux_offsets);
+extern void mx6_ddr_freq_change(u32 freq, void *ddr_settings,
+	bool dll_mode, void *iomux_offsets);
 
 static void *ddr_freq_change_iram_base;
 static int ddr_settings_size;
@@ -69,7 +71,7 @@ static volatile bool wait_for_ddr_freq_update;
 static int curr_ddr_rate;
 
 #define MIN_DLL_ON_FREQ		333000000
-#define MAX_DLL_OFF_FREQ		125000000
+#define MAX_DLL_OFF_FREQ	125000000
 
 unsigned long ddr3_dll_mx6q[][2] = {
 	{0x0c, 0x0},
@@ -155,10 +157,12 @@ int can_change_ddr_freq(void)
 }
 
 
-/* Each active core apart from the one changing the DDR frequency will execute
-  * this function. The rest of the cores have to remain in WFE state until the frequency
-  * is changed.
-  */
+/*
+ * each active core apart from the one changing
+ * the DDR frequency will execute this function.
+ * the rest of the cores have to remain in WFE
+ * state until the frequency is changed.
+ */
 irqreturn_t wait_in_wfe_irq(int irq, void *dev_id)
 {
 	u32 me = smp_processor_id();
@@ -173,7 +177,7 @@ irqreturn_t wait_in_wfe_irq(int irq, void *dev_id)
 	return IRQ_HANDLED;
 }
 
-/* Change the DDR frequency. */
+/* change the DDR frequency. */
 int update_ddr_freq(int ddr_rate)
 {
 	int i, j;
@@ -203,7 +207,8 @@ int update_ddr_freq(int ddr_rate)
 			iram_ddr_settings[i + 1][1] =
 					normal_mmdc_settings[i][1];
 		}
-		for (j = 0, i = ARRAY_SIZE(ddr3_dll_mx6q); i < iram_ddr_settings[0][0]; j++, i++) {
+		for (j = 0, i = ARRAY_SIZE(ddr3_dll_mx6q);
+			i < iram_ddr_settings[0][0]; j++, i++) {
 			iram_ddr_settings[i + 1][0] =
 					ddr3_400[j][0];
 			iram_ddr_settings[i + 1][1] =
@@ -218,7 +223,7 @@ int update_ddr_freq(int ddr_rate)
 		}
 	}
 
-	/* Ensure that all Cores are in WFE. */
+	/* ensure that all Cores are in WFE. */
 	local_irq_disable();
 
 	me = smp_processor_id();
@@ -228,23 +233,25 @@ int update_ddr_freq(int ddr_rate)
 	for_each_online_cpu(cpu) {
 		*((char *)(&online_cpus) + (u8)cpu) = 0xff;
 		if (cpu != me) {
-			/* Set the interrupt to be pending in the GIC. */
+			/* set the interrupt to be pending in the GIC. */
 			reg = 1 << (irq_used[cpu] % 32);
-			writel_relaxed(reg, gic_dist_base + GIC_DIST_PENDING_SET + (irq_used[cpu] / 32) * 4);
+			writel_relaxed(reg, gic_dist_base + GIC_DIST_PENDING_SET
+				+ (irq_used[cpu] / 32) * 4);
 		}
 	}
 	while (cpus_in_wfe != online_cpus)
 		udelay(5);
 
 	/* Now we can change the DDR frequency. */
-	mx6_change_ddr_freq(ddr_rate, iram_ddr_settings, dll_off, iram_iomux_settings);
+	mx6_change_ddr_freq(ddr_rate, iram_ddr_settings,
+		dll_off, iram_iomux_settings);
 
 	curr_ddr_rate = ddr_rate;
 
 	/* DDR frequency change is done . */
 	wait_for_ddr_freq_update = false;
 
-	/* Wake up all the cores. */
+	/* wake up all the cores. */
 	sev();
 
 	*((char *)(&cpus_in_wfe) + (u8)me) = 0;
@@ -294,17 +301,20 @@ int init_mmdc_settings(void)
 	gic_cpu_base = of_iomap(node, 1);
 	WARN(!gic_cpu_base, "unable to map gic cpu registers\n");
 
-	ddr_settings_size = ARRAY_SIZE(ddr3_dll_mx6q) + ARRAY_SIZE(ddr3_calibration);
+	ddr_settings_size = ARRAY_SIZE(ddr3_dll_mx6q) +
+		ARRAY_SIZE(ddr3_calibration);
 
 	normal_mmdc_settings = kmalloc((ddr_settings_size * 8), GFP_KERNEL);
 	memcpy(normal_mmdc_settings, ddr3_dll_mx6q, sizeof(ddr3_dll_mx6q));
-	memcpy(((char *)normal_mmdc_settings + sizeof(ddr3_dll_mx6q)), ddr3_calibration, sizeof(ddr3_calibration));
+	memcpy(((char *)normal_mmdc_settings + sizeof(ddr3_dll_mx6q)),
+		ddr3_calibration, sizeof(ddr3_calibration));
 
-	/* Store the original DDR settings at boot. */
+	/* store the original DDR settings at boot. */
 	for (i = 0; i < ddr_settings_size; i++) {
-		/*Writes via command mode register cannot be read back.
-		 * Hence hardcode them in the initial static array.
-		 * This may require modification on a per customer basis.
+		/*
+		 * writes via command mode register cannot be read back.
+		 * hence hardcode them in the initial static array.
+		 * this may require modification on a per customer basis.
 		 */
 		if (normal_mmdc_settings[i][0] != 0x1C)
 			normal_mmdc_settings[i][1] =
@@ -320,7 +330,8 @@ int init_mmdc_settings(void)
 		return -EINVAL;
 	}
 
-	/* Store the size of the array in iRAM also,
+	/*
+	 * store the size of the array in iRAM also,
 	 * increase the size by 8 bytes.
 	 */
 	of_property_read_u32(node, "iram_data1_base", &iram_paddr);
@@ -342,12 +353,12 @@ int init_mmdc_settings(void)
 	iram_iomux_settings = ioremap(iram_paddr, iram_size);
 	if (iram_iomux_settings == NULL) {
 			printk(KERN_DEBUG
-			"%s: failed to allocate iRAM memory for iomuxr settings\n",
+			"%s: failed to allocate iRAM for iomux settings\n",
 			__func__);
 			return ENOMEM;
 	}
 
-	/* Store the IOMUX settings at boot. */
+	/* store the IOMUX settings at boot. */
 	for (i = 0; i < iomux_settings_size; i++) {
 		iomux_offsets_mx6q[i][1] =
 			__raw_readl(iomux_base
@@ -357,11 +368,13 @@ int init_mmdc_settings(void)
 	}
 	irq_used = irqs_used_mx6q;
 
-	/* Allocate IRAM for the DDR freq change code. */
+	/* allocate IRAM for the DDR freq change code. */
 	of_property_read_u32(node, "iram_code_base", &iram_paddr);
 	of_property_read_u32(node, "iram_code_size", &iram_size);
-	/* Need to remap the area here since we want the memory region
-		 to be executable. */
+	/*
+	 * need to remap the area here since we want
+	 * the memory region to be executable.
+	 */
 	ddr_freq_change_iram_base = __arm_ioremap(iram_paddr,
 						iram_size, MT_MEMORY_NONCACHED);
 	mx6_change_ddr_freq = (void *)fncpy(ddr_freq_change_iram_base,
@@ -370,18 +383,22 @@ int init_mmdc_settings(void)
 	curr_ddr_rate = ddr_normal_rate;
 
 	for_each_online_cpu(cpu) {
-		/* Set up a reserved interrupt to get all the active cores into a WFE state
-		  * before changing the DDR frequency.
-		  */
-		err = request_irq(irq_used[cpu], wait_in_wfe_irq, IRQF_PERCPU, "mmdc_1",
-				  NULL);
+		/*
+		 * set up a reserved interrupt to get all
+		 * the active cores into a WFE state
+		 * before changing the DDR frequency.
+		 */
+		err = request_irq(irq_used[cpu], wait_in_wfe_irq,
+			IRQF_PERCPU, "mmdc_1", NULL);
 		if (err) {
-			printk(KERN_ERR "MMDC: Unable to attach to %ld,err = %d\n", irq_used[cpu], err);
+			printk(KERN_ERR "MMDC: Unable to attach to %ld, err = %d\n",
+				irq_used[cpu], err);
 			return err;
 		}
 		err = irq_set_affinity(irq_used[cpu], cpumask_of(cpu));
 		if (err) {
-			printk(KERN_ERR "MMDC: unable to set irq affinity irq=%ld,\n", irq_used[cpu]);
+			printk(KERN_ERR "MMDC: unable to set irq affinity irq=%ld,\n",
+				irq_used[cpu]);
 			return err;
 		}
 	}
