a5d24252efd66ea0454c1ab5609da8814c41cf6c
Mark Brown <broonie@opensource.wolfsonmicro.com>
2013-03-27 13:08:44 +0000

    regmap: async: Add missing return
    Let's only write once...
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- drivers/base/regmap/regmap.c
+++ drivers/base/regmap/regmap.c
@@ -1057,6 +1057,8 @@ int _regmap_raw_write(struct regmap *map, unsigned int reg,
 			kfree(async->work_buf);
 			kfree(async);
 		}
+
+		return ret;
 	}
 
 	trace_regmap_hw_write_start(map->dev, reg,
