1eecdedab7cc15f2a024ee8d786bac18cad866db
Victoria Milhoan (b42089) <vicki.milhoan@freescale.com>
2013-03-28 23:40:18 -0700

    ENGR00256763-25: Add support for external SEQ IN/SEQ OUT lengths
    This patch adds functions for the external storage of SEQ IN and SEQ
    OUT lengths.  The original community patch is located at
    http://www.mail-archive.com/linux-crypto@vger.kernel.org/msg07262.html
    for reference.
    
    Signed-off-by: Victoria Milhoan (b42089) <vicki.milhoan@freescale.com>
    Signed-off-by: Steve Cornelius <steve.cornelius@freescale.com>
    Signed-off-by: Terry Lv <r65388@freescale.com>

--- drivers/crypto/caam/caamalg.c
+++ drivers/crypto/caam/caamalg.c
@@ -65,7 +65,7 @@
 #define CAAM_MAX_IV_LENGTH		16
 
 /* length of descriptors text */
-#define DESC_JOB_IO_LEN			(CAAM_CMD_SZ * 3 + CAAM_PTR_SZ * 3)
+#define DESC_JOB_IO_LEN			(CAAM_CMD_SZ * 5 + CAAM_PTR_SZ * 3)
 
 #define DESC_AEAD_BASE			(4 * CAAM_CMD_SZ)
 #define DESC_AEAD_ENC_LEN		(DESC_AEAD_BASE + 16 * CAAM_CMD_SZ)
