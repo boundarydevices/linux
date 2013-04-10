63695046831b93dcbedca5ce1d7c992c43987dd6
Eric Nelson <eric.nelson@boundarydevices.com>
2013-04-10 11:46:42 -0700

    oc: init USB device controller
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- arch/arm/mach-mx6/board-mx6_oc.c
+++ arch/arm/mach-mx6/board-mx6_oc.c
@@ -586,6 +586,7 @@ static void __init imx6_oc_init_usb(void)
 	mxc_iomux_set_gpr_register(1, 13, 1, 1);
 
 	mx6_set_otghost_vbus_func(imx6_oc_usbotg_vbus);
+	mx6_usb_dr_init();
 }
 
 /* HW Initialization, if return 0, initialization is successful. */
