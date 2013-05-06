77f3ec611804c32215ffe7dcfd2517ac35365a13
Eric Nelson <eric.nelson@boundarydevices.com>
2013-05-06 10:27:02 -0700

    Revert "ipu_common: delay request_irq, check for handler not NULL"
    This reverts commit 4878f925a1ad9939a570cf14ac6dd9c4954e8f7b.

--- drivers/mxc/ipu3/ipu_common.c
+++ drivers/mxc/ipu3/ipu_common.c
@@ -263,6 +263,19 @@ static int __devinit ipu_probe(struct platform_device *pdev)
 		goto failed_get_res;
 	}
 
+	ret = request_irq(ipu->irq_sync, ipu_sync_irq_handler, 0,
+			  pdev->name, ipu);
+	if (ret) {
+		dev_err(ipu->dev, "request SYNC interrupt failed\n");
+		goto failed_req_irq_sync;
+	}
+	ret = request_irq(ipu->irq_err, ipu_err_irq_handler, 0,
+			  pdev->name, ipu);
+	if (ret) {
+		dev_err(ipu->dev, "request ERR interrupt failed\n");
+		goto failed_req_irq_err;
+	}
+
 	ipu_base = res->start;
 	/* base fixup */
 	if (g_ipu_hw_rev == 4)	/* IPUv3H */
@@ -347,29 +360,12 @@ static int __devinit ipu_probe(struct platform_device *pdev)
 	if (!plat_data->bypass_reset)
 		clk_disable(ipu->ipu_clk);
 
-	if (request_irq(ipu->irq_sync, ipu_irq_handler, 0, pdev->name, ipu) != 0) {
-		dev_err(ipu->dev, "request SYNC interrupt failed\n");
-		ret = -EBUSY;
-		goto failed_req_irq_sync;
-	}
-	/* Some platforms have 2 IPU interrupts */
-	if (ipu->irq_err >= 0) {
-		if (request_irq
-		    (ipu->irq_err, ipu_irq_handler, 0, pdev->name, ipu) != 0) {
-			dev_err(ipu->dev, "request ERR interrupt failed\n");
-			ret = -EBUSY;
-			goto failed_req_irq_err;
-		}
-	}
 	register_ipu_device(ipu, pdev->id);
 
 	ipu->online = true;
 
 	return ret;
 
-failed_req_irq_err:
-	free_irq(ipu->irq_sync, ipu);
-failed_req_irq_sync:
 failed_clk_setup:
 	iounmap(ipu->cm_reg);
 	iounmap(ipu->ic_reg);
@@ -390,11 +386,8 @@ failed_clk_setup:
 failed_ioremap:
 	free_irq(ipu->irq_err, ipu);
 failed_req_irq_err:
-	if (ipu->irq_sync)
-		free_irq(ipu->irq_sync, ipu);
+	free_irq(ipu->irq_sync, ipu);
 failed_req_irq_sync:
-	if (ipu->irq_sync)
-		free_irq(ipu->irq_err, ipu);
 failed_get_res:
 	return ret;
 }
@@ -2412,8 +2405,8 @@ static irqreturn_t ipu_sync_irq_handler(int irq, void *desc)
 			bit = --line;
 			int_stat &= ~(1UL << line);
 			line += (int_reg[i] - 1) * 32;
-			if (ipu->irq_list[line].handler)
-				result |= ipu->irq_list[line].handler(line,
+			result |=
+			    ipu->irq_list[line].handler(line,
 						       ipu->irq_list[line].
 						       dev_id);
 			if (ipu->irq_list[line].flags & IPU_IRQF_ONESHOT) {
