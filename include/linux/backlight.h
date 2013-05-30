b48b097319587422195adc3f8b2b3b0c067943cc
Liu Ying <Ying.Liu@freescale.com>
2013-05-30 13:56:17 +0800

    ENGR00264855 backlight: Support backlight shared by multiple fbs
    One backlight device may shared by multiple framebuffers.
    We don't hope blanking one of the framebuffers may turn the
    backlight off for all the other framebuffers, since they are
    likely active to show display content. This patch adds logic
    to record each framebuffer's backlight usage to determine the
    backlight device use count and whether the backlight should be
    turned on or off.
    
    Signed-off-by: Liu Ying <Ying.Liu@freescale.com>
    (cherry picked from commit 37cbf741e4dff1f757f3ade6bb861d9a2af70693)

--- include/linux/backlight.h
+++ include/linux/backlight.h
@@ -9,6 +9,7 @@
 #define _LINUX_BACKLIGHT_H
 
 #include <linux/device.h>
+#include <linux/fb.h>
 #include <linux/mutex.h>
 #include <linux/notifier.h>
 
@@ -101,6 +102,11 @@ struct backlight_device {
 	struct notifier_block fb_notif;
 
 	struct device dev;
+
+	/* Multiple framebuffers may share one backlight device */
+	bool fb_bl_on[FB_MAX];
+
+	int use_count;
 };
 
 static inline void backlight_update_status(struct backlight_device *bd)
