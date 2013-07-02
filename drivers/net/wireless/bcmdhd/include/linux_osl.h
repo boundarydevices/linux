50ec58c5968e63e1c358ba4181cac04807e72796
Eric Nelson <eric.nelson@boundarydevices.com>
2013-07-02 10:28:00 -0700

    Add Broadcom DHD driver from package bcmdhd.5.90.195.26.1.6.1
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- drivers/net/wireless/bcmdhd/include/linux_osl.h
+++ drivers/net/wireless/bcmdhd/include/linux_osl.h
@@ -163,7 +163,7 @@ extern int osl_error(int bcmerror);
 
 
 
-#define OSL_SYSUPTIME()		((uint32)jiffies_to_msecs(jiffies))
+#define OSL_SYSUPTIME()		((uint32)jiffies * (1000 / HZ))
 #define	printf(fmt, args...)	printk(fmt , ## args)
 #include <linux/kernel.h>	
 #include <linux/string.h>	
