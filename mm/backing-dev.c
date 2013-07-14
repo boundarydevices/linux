baaa9cbb2c6ef29e6f812ea82887cb2ade48cdbe
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-07-14 16:05:51 -0700

    sysfs.h: add __ATTR_RW() macro
    A number of parts of the kernel created their own version of this, might
    as well have the sysfs core provide it instead.
    
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Tested-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/backing-dev.c
+++ mm/backing-dev.c
@@ -204,8 +204,6 @@ static ssize_t max_ratio_store(struct device *dev,
 }
 BDI_SHOW(max_ratio, bdi->max_ratio)
 
-#define __ATTR_RW(attr) __ATTR(attr, 0644, attr##_show, attr##_store)
-
 static struct device_attribute bdi_dev_attrs[] = {
 	__ATTR_RW(read_ahead_kb),
 	__ATTR_RW(min_ratio),
