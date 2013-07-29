db9ae2910b584b46fc7b232ef2ed82a8501f5551
Troy Kisky <troy.kisky@boundarydevices.com>
2013-07-29 14:55:26 -0700

    mxc_hdmi: always add non-interlaced modes

--- drivers/video/mxc/mxc_hdmi.c
+++ drivers/video/mxc/mxc_hdmi.c
@@ -1828,8 +1828,8 @@ static void mxc_hdmi_edid_rebuild_modelist(struct mxc_hdmi *hdmi)
 		 */
 		mode = &hdmi->fbi->monspecs.modedb[i];
 
-		if (!(mode->vmode & FB_VMODE_INTERLACED) &&
-				(mxc_edid_mode_to_vic(mode) != 0)) {
+		if (!(mode->vmode & FB_VMODE_INTERLACED)  /* &&
+				(mxc_edid_mode_to_vic(mode) != 0) */) {
 
 			dev_dbg(&hdmi->pdev->dev, "Added mode %d:", i);
 			dev_dbg(&hdmi->pdev->dev,
@@ -2832,6 +2832,7 @@ err_out_class:
 err_out_chrdev:
 	unregister_chrdev(hdmi_major, "mxc_hdmi");
 ealloc:
+	dev_err(&pdev->dev, "ret=%d\n", ret);
 	return ret;
 }
 
