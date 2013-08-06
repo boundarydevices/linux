3f1363212e68c7daca03cb21d1c6c63fdd0f38c0
Sandor Yu <R01008@freescale.com>
2013-08-06 17:04:22 +0800

    ENGR00274026 MX6 Clock: Replace imx_clk_mux2 function with imx_clk_mux_flags
    The function of imx_clk_mux2 is cover by imx_clk_mux_flags.
    Remove imx_clk_mux2 and replace with imx_clk_mux_flags function.
    
    Signed-off-by: Sandor Yu <R01008@freescale.com>

--- arch/arm/mach-imx/clk.h
+++ arch/arm/mach-imx/clk.h
@@ -94,13 +94,6 @@ static inline struct clk *imx_clk_mux(const char *name, void __iomem *reg,
 			width, 0, &imx_ccm_lock);
 }
 
-static inline struct clk *imx_clk_mux2(const char *name, void __iomem *reg,
-		u8 shift, u8 width, const char **parents, int num_parents)
-{
-	return clk_register_mux(NULL, name, parents, num_parents, CLK_SET_RATE_PARENT,
-			reg, shift,	width, 0, &imx_ccm_lock);
-}
-
 static inline struct clk *imx_clk_mux_flags(const char *name,
 		void __iomem *reg, u8 shift, u8 width, const char **parents,
 		int num_parents, unsigned long flags)
