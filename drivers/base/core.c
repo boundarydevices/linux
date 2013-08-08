744035b5f97795557c9bc173610d03ee7f4d8f47
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-08-08 15:22:55 -0700

    driver core: bus_type: add dev_groups
    attribute groups are much more flexible than just a list of attributes,
    due to their support for visibility of the attributes, and binary
    attributes. Add dev_groups to struct bus_type which should be used
    instead of dev_attrs.
    
    dev_attrs will be removed from the structure soon.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Huang Shijie <b32955@freescale.com>

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -461,8 +461,7 @@ static void device_remove_bin_attributes(struct device *dev,
 			device_remove_bin_file(dev, &attrs[i]);
 }
 
-static int device_add_groups(struct device *dev,
-			     const struct attribute_group **groups)
+int device_add_groups(struct device *dev, const struct attribute_group **groups)
 {
 	int error = 0;
 	int i;
@@ -481,8 +480,8 @@ static int device_add_groups(struct device *dev,
 	return error;
 }
 
-static void device_remove_groups(struct device *dev,
-				 const struct attribute_group **groups)
+void device_remove_groups(struct device *dev,
+			  const struct attribute_group **groups)
 {
 	int i;
 
