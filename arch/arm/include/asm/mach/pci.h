8ca48c6a984e4299ebc071f104cd71a4bdb98bc6
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-08-09 22:27:12 +0200

    ARM: pci: add ->add_bus() and ->remove_bus() hooks to hw_pci
    Some PCI drivers may need to adjust the pci_bus structure after it has
    been allocated by the Linux PCI core. The PCI core allows
    architectures to implement the pcibios_add_bus() and
    pcibios_remove_bus() for this purpose. This commit therefore extends
    the hw_pci and pci_sys_data structures of the ARM PCI core to allow
    PCI drivers to register ->add_bus() and ->remove_bus() in hw_pci,
    which will get called when a bus is added or removed from the system.
    
    This will be used for example by the Marvell PCIe driver to connect a
    particular PCI bus with its corresponding MSI chip to handle Message
    Signaled Interrupts.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Reviewed-by: Thierry Reding <thierry.reding@gmail.com>
    Acked-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Tested-by: Daniel Price <daniel.price@gmail.com>
    Tested-by: Thierry Reding <thierry.reding@gmail.com>
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- arch/arm/include/asm/mach/pci.h
+++ arch/arm/include/asm/mach/pci.h
@@ -35,6 +35,8 @@ struct hw_pci {
 					  resource_size_t start,
 					  resource_size_t size,
 					  resource_size_t align);
+	void		(*add_bus)(struct pci_bus *bus);
+	void		(*remove_bus)(struct pci_bus *bus);
 };
 
 /*
@@ -62,6 +64,8 @@ struct pci_sys_data {
 					  resource_size_t start,
 					  resource_size_t size,
 					  resource_size_t align);
+	void		(*add_bus)(struct pci_bus *bus);
+	void		(*remove_bus)(struct pci_bus *bus);
 	void		*private_data;	/* platform controller private data	*/
 };
 
