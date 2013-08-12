8636141ad1ef1cc40f79b7fad2bd58191f860514
Shengjiu Wang <b02247@freescale.com>
2013-08-12 14:08:08 +0800

    ENGR00274585-3 ASoC: fsl: add asrc p2p driver.
    add asrc p2p driver.
    add bindings document of asrc p2p.
    
    Signed-off-by: Shengjiu Wang <b02247@freescale.com>

---
+++ sound/soc/fsl/fsl_asrc.h
@@ -0,0 +1,48 @@
+/*
+ * fsl_asrc.h - ALSA ASRC interface
+ *
+ * Copyright (C) 2013 Freescale Semiconductor, Inc.  This file is licensed
+ * under the terms of the GNU General Public License version 2.  This
+ * program is licensed "as is" without any warranty of any kind, whether
+ * express or implied.
+ */
+
+#ifndef _FSL_ASRC_P2P_H
+#define _FSL_ASRC_P2P_H
+
+#include <linux/mxc_asrc.h>
+#include <sound/dmaengine_pcm.h>
+#include <linux/platform_data/dma-imx.h>
+
+enum peripheral_device_type {
+	UNKNOWN,
+	SSI1,
+	SSI2,
+	SSI3,
+	ESAI,
+};
+
+struct fsl_asrc_p2p {
+	int output_rate;
+	int output_width;
+	enum asrc_pair_index asrc_index;
+	enum peripheral_device_type per_dev;
+	struct asrc_p2p_ops asrc_ops;
+
+	struct snd_dmaengine_dai_dma_data dma_params_rx;
+	struct snd_dmaengine_dai_dma_data dma_params_tx;
+	struct imx_dma_data filter_data_tx;
+	struct imx_dma_data filter_data_rx;
+
+	struct dma_async_tx_descriptor  *asrc_p2p_desc;
+	struct dma_chan                 *asrc_p2p_dma_chan;
+	struct imx_dma_data              asrc_p2p_dma_data;
+	struct platform_device *soc_platform_pdev;
+
+	int dmarx[3];
+	int dmatx[3];
+
+	char name[32];
+};
+
+#endif
