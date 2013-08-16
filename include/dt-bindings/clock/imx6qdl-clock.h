650f32032534c5436d467684d22210077dce965f
Liu Ying <Ying.Liu@freescale.com>
2013-08-16 13:44:42 +0800

    ENGR00275483-1 ARM: imx6q: clk: add video 27m clock
    This patch adds the fixed factor video 27m clock in
    the imx6q clock driver. This clock's parent clock
    is the pll3_pfd1_540m clock.
    
    Conflicts:
    
    	Documentation/devicetree/bindings/clock/imx6q-clock.txt
    	arch/arm/mach-imx/clk-imx6q.c
    
    Signed-off-by: Liu Ying <Ying.Liu@freescale.com>
    (cherry picked from commit 922c6a032581cfe0aff45d7495fd7816b55c2f39)

--- include/dt-bindings/clock/imx6qdl-clock.h
+++ include/dt-bindings/clock/imx6qdl-clock.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Freescale Semiconductor, Inc.
+ * Copyright (C) 2014 Freescale Semiconductor, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -221,6 +221,7 @@
 #define IMX6QDL_CLK_LDB_DI1_DIV_7		210
 #define IMX6QDL_CLK_LDB_DI0_DIV_SEL		211
 #define IMX6QDL_CLK_LDB_DI1_DIV_SEL		212
-#define IMX6QDL_CLK_END				213
+#define IMX6QDL_CLK_VIDEO_27M			213
+#define IMX6QDL_CLK_END				214
 
 #endif /* __DT_BINDINGS_CLOCK_IMX6QDL_H */
