f480d5214aa92332b6071801aae63f04954e7d1d
Xianzhong <b07117@freescale.com>
2013-08-23 20:36:45 +0800

    ENGR00274478 fix gpu memory multi-lock failure
    this issue cause system boot with multi-user switch on JB4.3,
    
    root cause is gpu memory cannot be multi-locked in same process,
    gpu memory lock reference is added to allow multi-lock in kernel driver.
    
    Signed-off-by: Xianzhong <b07117@freescale.com>
    Acked-by: Jason Liu

--- drivers/mxc/gpu-viv/hal/os/linux/kernel/gc_hal_kernel_os.c
+++ drivers/mxc/gpu-viv/hal/os/linux/kernel/gc_hal_kernel_os.c
@@ -375,9 +375,10 @@ _CreateMdlMap(
         return gcvNULL;
     }
 
-    mdlMap->pid     = ProcessID;
-    mdlMap->vmaAddr = gcvNULL;
-    mdlMap->vma     = gcvNULL;
+    mdlMap->pid       = ProcessID;
+    mdlMap->vmaAddr   = gcvNULL;
+    mdlMap->vma       = gcvNULL;
+    mdlMap->reference = 0;
 
     mdlMap->next    = Mdl->maps;
     Mdl->maps       = mdlMap;
@@ -4431,6 +4432,8 @@ gckOS_LockPages(
 
         up_write(&current->mm->mmap_sem);
     }
+
+#if 0
     else
     {
         /* mdlMap->vmaAddr != gcvNULL means current process has already locked this node. */
@@ -4439,6 +4442,7 @@ gckOS_LockPages(
         gcmkFOOTER_ARG("*status=%d, mdlMap->vmaAddr=%x", gcvSTATUS_MEMORY_LOCKED, mdlMap->vmaAddr);
         return gcvSTATUS_MEMORY_LOCKED;
     }
+#endif
 
     /* Convert pointer to MDL. */
     *Logical = mdlMap->vmaAddr;
@@ -4448,6 +4452,9 @@ gckOS_LockPages(
     gcmkASSERT((PAGE_SIZE / 4096) >= 1);
 
     *PageCount = mdl->numPages * (PAGE_SIZE / 4096);
+
+    /* Increase reference count. */
+    mdlMap->reference++;
 
     MEMORY_UNLOCK(Os);
 
@@ -4715,6 +4722,11 @@ gckOS_UnlockPages(
     {
         if ((mdlMap->vmaAddr != gcvNULL) && (_GetProcessID() == mdlMap->pid))
         {
+            if (--mdlMap->reference > 0)
+            {
+                continue;
+            }
+
             _UnmapUserLogical(mdlMap->pid, mdlMap->vmaAddr, mdl->numPages * PAGE_SIZE);
             mdlMap->vmaAddr = gcvNULL;
         }
