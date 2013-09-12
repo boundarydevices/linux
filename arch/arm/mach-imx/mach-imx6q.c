da4a080a12375cecc2d9dff4680ec9d93c9a02ba
Robin Gong <b38343@freescale.com>
2013-09-12 16:34:01 +0800

    ENGR00279402-2 ARM: imx6q: imx6sl: System reset by checking the source setting
    Check the source setting in dts file to support different WDOG reset event.
    Correct imx6sl_restart instead of mxc_restart.
    
    Signed-off-by: Robin Gong <b38343@freescale.com>

--- arch/arm/mach-imx/mach-imx6q.c
+++ arch/arm/mach-imx/mach-imx6q.c
@@ -45,6 +45,13 @@ static void imx6q_restart(char mode, const char *cmd)
 {
 	struct device_node *np;
 	void __iomem *wdog_base;
+	u32 wdog_source = 1; /* use WDOG1 default */
+	unsigned int value;
+
+	np = of_find_compatible_node(NULL, NULL, "fsl,imx6q-gpc");
+	if (np)
+		of_property_read_u32(np, "fsl,wdog-reset", &wdog_source);
+	pr_info("Use WDOG%d as reset source\n", wdog_source);
 
 	np = of_find_compatible_node(NULL, NULL, "fsl,imx6q-wdt");
 	wdog_base = of_iomap(np, 0);
@@ -53,10 +60,27 @@ static void imx6q_restart(char mode, const char *cmd)
 
 	imx_src_prepare_restart();
 
+	if (wdog_source == 2) {
+		/*
+		 * Some boards  use WDOG2 to reset external pmic in bypass mode,
+		 * so do WDOG2 reset here. Do not set SRS,since we will
+		 * trigger external POR later. Use WDOG1 to reset in ldo-enable
+		 * mode.
+		 */
+		np = of_find_compatible_node(np, NULL, "fsl,imx6q-wdt");
+		wdog_base = of_iomap(np, 0);
+		if (!wdog_base) {
+			pr_warn("Not found wdt2, please check your dts!\n");
+			goto soft;
+		}
+		value = 0x14;
+	} else {
+		value = (1 << 2);
+	}
 	/* enable wdog */
-	writew_relaxed(1 << 2, wdog_base);
+	writew_relaxed(value, wdog_base);
 	/* write twice to ensure the request will not get ignored */
-	writew_relaxed(1 << 2, wdog_base);
+	writew_relaxed(value, wdog_base);
 
 	/* wait for reset to assert ... */
 	mdelay(500);
