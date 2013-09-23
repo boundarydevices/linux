dbf45927ac7e149c28a3aac4bfa7768acbbb8e35
Peter Chen <peter.chen@freescale.com>
2013-09-23 11:20:08 +0800

    ENGR00320792-3 usb: gadget: mark init as late_initcall
    Since we introduce -EPROBE_DEFER for udc driver, it will be
    probed at late_initcall if it is defered. When the gadget
    is built in, it will return "couldn't find an available UDC"
    at such case. That's the problem we met at below link:
    
    http://marc.info/?l=linux-usb&m=137706435611447&w=2
    
    We have no driver's probe at gadget driver, so we can't return
    -EPROBE_DEFER. And it is also not suitable to defer udc_bind_to_driver
    if the udc is not found temporarily, since it is hard to decide the
    return value for usb_gadget_probe_driver.
    
    Due to above reasons, mark gadget's init as late_initcall may be a
    moderate solution.
    
    Signed-off-by: Peter Chen <peter.chen@freescale.com>

--- drivers/usb/gadget/ncm.c
+++ drivers/usb/gadget/ncm.c
@@ -196,7 +196,7 @@ static int __init init(void)
 {
 	return usb_composite_probe(&ncm_driver);
 }
-module_init(init);
+late_initcall(init);
 
 static void __exit cleanup(void)
 {
--- drivers/usb/gadget/zero.c
+++ drivers/usb/gadget/zero.c
@@ -418,7 +418,7 @@ static int __init init(void)
 {
 	return usb_composite_probe(&zero_driver);
 }
-module_init(init);
+late_initcall(init);
 
 static void __exit cleanup(void)
 {
