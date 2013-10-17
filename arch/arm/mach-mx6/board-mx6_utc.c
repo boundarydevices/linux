b15341fa5bcde7bf1a84ab28008045ea66010cdb
Eric Nelson <eric.nelson@boundarydevices.com>
2013-10-17 10:49:25 -0700

    utc: allow reserved v4l2_output memory
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- arch/arm/mach-mx6/board-mx6_utc.c
+++ arch/arm/mach-mx6/board-mx6_utc.c
@@ -1241,6 +1241,12 @@ static void __init reserve(void)
 			memblock_remove(phys, fb_data[i].res_size[0]);
 			fb_data[i].res_base[0] = phys;
 		}
+	if (vout_mem.res_msize) {
+		phys = memblock_alloc_base(vout_mem.res_msize,
+					   SZ_4K, SZ_1G);
+		memblock_remove(phys, vout_mem.res_msize);
+		vout_mem.res_mbase = phys;
+	}
 }
 
 /*
