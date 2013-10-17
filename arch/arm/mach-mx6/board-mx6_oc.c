13201aac847c3f9015c54a338c795fbe22c9e883
Eric Nelson <eric.nelson@boundarydevices.com>
2013-10-17 10:47:40 -0700

    oc: allow reserved v4l2_output memory
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- arch/arm/mach-mx6/board-mx6_oc.c
+++ arch/arm/mach-mx6/board-mx6_oc.c
@@ -762,6 +762,15 @@ static struct fsl_mxc_capture_platform_data capture_data = {
 };
 
 
+struct imx_vout_mem {
+       resource_size_t res_mbase;
+       resource_size_t res_msize;
+};
+
+static struct imx_vout_mem vout_mem __initdata = {
+       .res_msize = 0,
+};
+
 static void oc_suspend_enter(void)
 {
 	/* suspend preparation */
@@ -982,6 +991,13 @@ static void __init fixup_mxc_board(struct machine_desc *desc, struct tag *tags,
 				str += 7;
 				imx6_gpu_pdata.reserved_mem_size = memparse(str, &str);
 			}
+			/* VPU reserved memory */
+			str = t->u.cmdline.cmdline;
+			str = strstr(str, "vpumem=");
+			if (str != NULL) {
+				str += 7;
+				vout_mem.res_msize = memparse(str, &str);
+			}
 			break;
 		}
 	}
@@ -1003,6 +1019,7 @@ static void __init mx6_oc_board_init(void)
 	struct clk *clko2;
 	struct clk *new_parent;
 	int rate;
+	struct platform_device *voutdev;
 	int ret = gpio_request_array(mx6_oc_init_gpios,
 			ARRAY_SIZE(mx6_oc_init_gpios));
 
@@ -1039,7 +1056,15 @@ static void __init mx6_oc_board_init(void)
 
 	imx6q_add_vdoa();
 	imx6q_add_ldb(&ldb_data);
-	imx6q_add_v4l2_output(0);
+	voutdev = imx6q_add_v4l2_output(0);
+	if (vout_mem.res_msize && voutdev) {
+		dma_declare_coherent_memory(&voutdev->dev,
+                                            vout_mem.res_mbase,
+                                            vout_mem.res_mbase,
+                                            vout_mem.res_msize,
+                                            (DMA_MEMORY_MAP |
+                                             DMA_MEMORY_EXCLUSIVE));
+	}
 	imx6q_add_v4l2_capture(0, &capture_data);
 	imx6q_add_imx_snvs_rtc();
 
@@ -1165,6 +1190,12 @@ static void __init mx6_oc_reserve(void)
 		imx6_gpu_pdata.reserved_mem_base = phys;
 	}
 #endif
+	if (vout_mem.res_msize) {
+		phys = memblock_alloc_base(vout_mem.res_msize,
+					   SZ_4K, SZ_1G);
+		memblock_remove(phys, vout_mem.res_msize);
+		vout_mem.res_mbase = phys;
+	}
 }
 
 /*
