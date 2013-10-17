1f865021bba83c3bbf78d9a00fb69aea3ef51d27
Nicolin Chen <b42378@freescale.com>
2013-10-17 18:24:29 +0800

    ENGR00283079-1 ASoC: fsl: Implement different DMA buffer sizes
    Each CPU DAI driver has its own defined DMA buffer size, so this patch just
    drops the original one that uses a fixed size for all drivers and implements
    a different DMA buffer size to each driver.
    
    Acked-by: Wang Shengjiu <b02247@freescale.com>
    Signed-off-by: Nicolin Chen <b42378@freescale.com>
    (cherry picked from commit d2589c6bcdfbcac420999e15d36ecd48a7df2742)

--- sound/soc/fsl/imx-pcm-dma.c
+++ sound/soc/fsl/imx-pcm-dma.c
@@ -55,10 +55,27 @@ static const struct snd_dmaengine_pcm_config imx_dmaengine_pcm_config = {
 	.prealloc_buffer_size = IMX_DEFAULT_DMABUF_SIZE,
 };
 
-int imx_pcm_dma_init(struct platform_device *pdev)
+int imx_pcm_dma_init(struct platform_device *pdev, size_t size)
 {
+	struct snd_dmaengine_pcm_config *config;
+	struct snd_pcm_hardware *pcm_hardware;
+
+	config = devm_kzalloc(&pdev->dev,
+			sizeof(struct snd_dmaengine_pcm_config), GFP_KERNEL);
+	*config = imx_dmaengine_pcm_config;
+	if (size)
+		config->prealloc_buffer_size = size;
+
+	pcm_hardware = devm_kzalloc(&pdev->dev,
+			sizeof(struct snd_pcm_hardware), GFP_KERNEL);
+	*pcm_hardware = imx_pcm_hardware;
+	if (size)
+		pcm_hardware->buffer_bytes_max = size;
+
+	config->pcm_hardware = pcm_hardware;
+
 	return devm_snd_dmaengine_pcm_register(&pdev->dev,
-		&imx_dmaengine_pcm_config,
+		config,
 		SND_DMAENGINE_PCM_FLAG_COMPAT);
 }
 EXPORT_SYMBOL_GPL(imx_pcm_dma_init);
