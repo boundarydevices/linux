50eaddc1f356ca2ae949900423e54816f88c5e66
Nicolin Chen <b42378@freescale.com>
2013-10-17 18:24:29 +0800

    ENGR00283079-1 ASoC: fsl: Implement different DMA buffer sizes
    Each CPU DAI driver has its own defined DMA buffer size, so this patch just
    drops the original one that uses a fixed size for all drivers and implements
    a different DMA buffer size to each driver.
    
    Acked-by: Wang Shengjiu <b02247@freescale.com>
    Signed-off-by: Nicolin Chen <b42378@freescale.com>
    (cherry picked from commit d2589c6bcdfbcac420999e15d36ecd48a7df2742)

--- sound/soc/fsl/imx-pcm.h
+++ sound/soc/fsl/imx-pcm.h
@@ -20,6 +20,7 @@
  */
 #define IMX_DEFAULT_DMABUF_SIZE (256 * 1024)
 #define IMX_SSI_DMABUF_SIZE	(64 * 1024)
+#define IMX_SAI_DMABUF_SIZE	(64 * 1024)
 #define IMX_SPDIF_DMABUF_SIZE	(64 * 1024)
 #define IMX_ESAI_DMABUF_SIZE	(256 * 1024)
 #define IMX_ASRC_DMABUF_SIZE	(256 * 1024)
@@ -43,9 +44,9 @@ struct imx_pcm_fiq_params {
 };
 
 #if IS_ENABLED(CONFIG_SND_SOC_IMX_PCM_DMA)
-int imx_pcm_dma_init(struct platform_device *pdev);
+int imx_pcm_dma_init(struct platform_device *pdev, size_t size);
 #else
-static inline int imx_pcm_dma_init(struct platform_device *pdev)
+static inline int imx_pcm_dma_init(struct platform_device *pdev, size_t size)
 {
 	return -ENODEV;
 }
