ffa92e7f1437acc2e54bc14d3d2694aa9a8ab7aa
Peter Chen <peter.chen@freescale.com>
2013-10-21 16:50:35 +0800

    ENGR00286426-19 usb: chipidea: imx: Enable runtime pm support for imx6
    Currently, only imx6 SoC series add wakeup logic, so only enable
    runtime pm for imx6.
    
    Signed-off-by: Peter Chen <peter.chen@freescale.com>

--- drivers/usb/chipidea/ci_hdrc_imx.c
+++ drivers/usb/chipidea/ci_hdrc_imx.c
@@ -23,7 +23,8 @@
 #include "ci.h"
 #include "ci_hdrc_imx.h"
 
-#define CI_HDRC_IMX_IMX28_WRITE_FIX BIT(0)
+#define CI_HDRC_IMX_IMX28_WRITE_FIX		BIT(0)
+#define CI_HDRC_IMX_SUPPORT_RUNTIME_PM		BIT(1)
 
 struct ci_hdrc_imx_platform_flag {
 	unsigned int flags;
@@ -36,7 +37,17 @@ static const struct ci_hdrc_imx_platform_flag imx28_usb_data = {
 	.flags = CI_HDRC_IMX_IMX28_WRITE_FIX,
 };
 
+static const struct ci_hdrc_imx_platform_flag imx6q_usb_data = {
+	.flags = CI_HDRC_IMX_SUPPORT_RUNTIME_PM,
+};
+
+static const struct ci_hdrc_imx_platform_flag imx6sl_usb_data = {
+	.flags = CI_HDRC_IMX_SUPPORT_RUNTIME_PM,
+};
+
 static const struct of_device_id ci_hdrc_imx_dt_ids[] = {
+	{ .compatible = "fsl,imx6sl-usb", .data = &imx6sl_usb_data},
+	{ .compatible = "fsl,imx6q-usb", .data = &imx6q_usb_data},
 	{ .compatible = "fsl,imx28-usb", .data = &imx28_usb_data},
 	{ .compatible = "fsl,imx27-usb", .data = &imx27_usb_data},
 	{ /* sentinel */ }
@@ -147,6 +158,11 @@ static int ci_hdrc_imx_probe(struct platform_device *pdev)
 
 	if (imx_platform_flag->flags & CI_HDRC_IMX_IMX28_WRITE_FIX)
 		pdata.flags |= CI_HDRC_IMX28_WRITE_FIX;
+
+	if (imx_platform_flag->flags & CI_HDRC_IMX_SUPPORT_RUNTIME_PM)
+		pdata.flags |= CI_HDRC_SUPPORTS_RUNTIME_PM;
+		data->supports_runtime_pm = true;
+	}
 
 	if (data->usbmisc_data) {
 		ret = imx_usbmisc_init(data->usbmisc_data);
