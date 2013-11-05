d2c829f85fba63bf4846dfe53049556ead6a253f
Andrew Morton <akpm@linux-foundation.org>
2013-11-05 17:06:30 +1100

    mm-drop-actor-argument-of-do_generic_file_read-fix
    fix mm-drop-actor-argument-of-do_generic_file_read for linux-next changes
    
    Cc: "Kirill A. Shutemov" <kirill.shutemov@linux.intel.com>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Matthew Wilcox <willy@linux.intel.com>
    Cc: Wanpeng Li <liwanp@linux.vnet.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/filemap.c
+++ mm/filemap.c
@@ -1206,7 +1206,7 @@ page_ok:
 		 * "pos" here (the actor routine has to update the user buffer
 		 * pointers and the remaining count).
 		 */
-		ret = file_read_actor(desc, page, offset, nr);
+		ret = file_read_iter_actor(desc, page, offset, nr);
 		offset += ret;
 		index += offset >> PAGE_CACHE_SHIFT;
 		offset &= ~PAGE_CACHE_MASK;
