1401d4c84b71ba643fbbdbb17bab4c17aa6aaaf5
Dong Aisheng <b29396@freescale.com>
2013-11-08 18:50:07 +0800

    ENGR00286971-4 Revert "ENGR00278504-1 mmc: sdhci: add quirk for broken acmd23"
    The ACMD23 unwork issue is fixed now. so the former quirk to disable
    ACMD23 can be removed.
    
    This reverts commit fd27fce042bfd289eab6dbb7c98ab3adb48ca25b.
    
    Signed-off-by: Dong Aisheng <b29396@freescale.com>

--- include/linux/mmc/sdhci.h
+++ include/linux/mmc/sdhci.h
@@ -95,7 +95,6 @@ struct sdhci_host {
 /* The system physically doesn't support 1.8v, even if the host does */
 #define SDHCI_QUIRK2_NO_1_8_V				(1<<2)
 #define SDHCI_QUIRK2_PRESET_VALUE_BROKEN		(1<<3)
-#define SDHCI_QUIRK2_BROKEN_AUTO_CMD23                  (1<<4)
 
 	int irq;		/* Device IRQ */
 	void __iomem *ioaddr;	/* Mapped address */
