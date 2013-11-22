b8258ae3d0b8b708539232ba0a69d12f0842c8e8
Dong Aisheng <b29396@freescale.com>
2013-11-22 20:34:38 +0800

    ENGR00289406-1 mmc: sdhci: add quirk for get max timeout counter
    The max timeout counter of some SoCs like i.MX6 uSDHC may not be standard,
    add SDHCI_QUIRK2_NOSTD_TIMEOUT_COUNTER quirk to get the correct max timeout
    counter from platform specific code.
    Then we can calculate the correct max_discard_to value.
    
    Signed-off-by: Dong Aisheng <b29396@freescale.com>
    (cherry picked from commit 0469c7c4c683547f7923fb54882f681ad6d23ca8)

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -281,6 +281,7 @@ struct sdhci_ops {
 	unsigned int	(*get_max_clock)(struct sdhci_host *host);
 	unsigned int	(*get_min_clock)(struct sdhci_host *host);
 	unsigned int	(*get_timeout_clock)(struct sdhci_host *host);
+	unsigned int	(*get_max_timeout_counter)(struct sdhci_host *host);
 	int		(*platform_bus_width)(struct sdhci_host *host,
 					       int width);
 	void (*platform_send_init_74_clocks)(struct sdhci_host *host,
