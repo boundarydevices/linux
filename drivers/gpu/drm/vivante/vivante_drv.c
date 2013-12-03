9c1a6aa46197506a74dbcde0d9950378d7376406
Eric Nelson <eric.nelson@boundarydevices.com>
2013-12-03 13:33:16 -0700

    Add support to Vivante 4.6.9p12 GPU code
    In order to use the latest (and recommended) Vivante binaries,
    a suffix is needed.
    
    https://github.com/Freescale/meta-fsl-arm/blob/master/recipes-kernel/linux/linux-imx-3.0.35/drm-vivante-Add-00-sufix-in-returned-bus-Id.patch
    
    Note that this breaks compatibility with older userspaces.
    
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- drivers/gpu/drm/vivante/vivante_drv.c
+++ drivers/gpu/drm/vivante/vivante_drv.c
@@ -55,7 +55,7 @@
 
 #include "drm_pciids.h"
 
-static char platformdevicename[] = "Vivante GCCore";
+static char platformdevicename[] = "Vivante GCCore:00";
 static struct platform_device *pplatformdev;
 
 static const struct file_operations viv_driver_fops = {
