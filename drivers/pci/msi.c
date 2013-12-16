3bcef6ebbd3fd4d24115b1e72f1bcb04ee0c0d42
Alexander Gordeev <agordeev@redhat.com>
2013-12-16 09:34:56 +0100

    PCI/MSI: Return msix_capability_init() failure if populate_msi_sysfs() fails
    If populate_msi_sysfs() function failed msix_capability_init() must return
    the error code, but it returns the success instead.  This update fixes the
    described misbehaviour.
    
    Signed-off-by: Alexander Gordeev <agordeev@redhat.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Tejun Heo <tj@kernel.org>
    (cherry picked from commit c683bb76ce7686e41011d7df988266ce25e84ed4)

--- drivers/pci/msi.c
+++ drivers/pci/msi.c
@@ -756,7 +756,7 @@ static int msix_capability_init(struct pci_dev *dev,
 
 	ret = arch_setup_msi_irqs(dev, nvec, PCI_CAP_ID_MSIX);
 	if (ret)
-		goto error;
+		goto out_avail;
 
 	/*
 	 * Some devices require MSI-X to be enabled before we can touch the
@@ -769,10 +769,8 @@ static int msix_capability_init(struct pci_dev *dev,
 	msix_program_entries(dev, entries);
 
 	ret = populate_msi_sysfs(dev);
-	if (ret) {
-		ret = 0;
-		goto error;
-	}
+	if (ret)
+		goto out_free;
 
 	/* Set MSI-X enabled bits and unmask the function */
 	pci_intx_for_msi(dev, 0);
@@ -783,7 +781,7 @@ static int msix_capability_init(struct pci_dev *dev,
 
 	return 0;
 
-error:
+out_avail:
 	if (ret < 0) {
 		/*
 		 * If we had some success, report the number of irqs
@@ -800,6 +798,7 @@ error:
 			ret = avail;
 	}
 
+out_free:
 	free_msi_irqs(dev);
 
 	return ret;
