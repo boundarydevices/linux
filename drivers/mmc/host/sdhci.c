63f1013acf24b8df85e7c45dbb3f634a6f59ca47
Dong Aisheng <b29396@freescale.com>
2013-12-16 14:42:14 +0800

    ENGR00292140 mmc: sdhci: fix possible sleep in atomic in sdio_irq enable function
    The sdhci_runtime_pm_get API is able to sleep, so should not call it in
    sdhci_enable_sdio_irq_nolock which is executed with spin_lock_irqsave in
    sdhci_enable_sdio_irq.
    Move it out of spin lock to fix this issue.
    
    Signed-off-by: Dong Aisheng <b29396@freescale.com>
    (cherry picked from commit 2aa0640de623c8f4ef908210da30aeece30dbd91)

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1709,9 +1709,6 @@ static int sdhci_get_ro(struct mmc_host *mmc)
 
 static void sdhci_enable_sdio_irq_nolock(struct sdhci_host *host, int enable)
 {
-	if (enable)
-		sdhci_runtime_pm_get(host);
-
 	if (host->flags & SDHCI_DEVICE_DEAD)
 		goto out;
 
@@ -1730,9 +1727,6 @@ static void sdhci_enable_sdio_irq_nolock(struct sdhci_host *host, int enable)
 		sdhci_mask_irqs(host, SDHCI_INT_CARD_INT);
 out:
 	mmiowb();
-
-	if (!enable)
-		sdhci_runtime_pm_put(host);
 }
 
 static void sdhci_enable_sdio_irq(struct mmc_host *mmc, int enable)
@@ -1740,9 +1734,15 @@ static void sdhci_enable_sdio_irq(struct mmc_host *mmc, int enable)
 	struct sdhci_host *host = mmc_priv(mmc);
 	unsigned long flags;
 
+	if (enable)
+		sdhci_runtime_pm_get(host);
+
 	spin_lock_irqsave(&host->lock, flags);
 	sdhci_enable_sdio_irq_nolock(host, enable);
 	spin_unlock_irqrestore(&host->lock, flags);
+
+	if (!enable)
+		sdhci_runtime_pm_put(host);
 }
 
 static int sdhci_do_start_signal_voltage_switch(struct sdhci_host *host,
