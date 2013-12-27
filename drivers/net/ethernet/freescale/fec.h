180d2c57bbded3adb75c77315910b188f8824028
Troy Kisky <troy.kisky@boundarydevices.com>
2013-12-27 11:38:34 -0700

    net: fec: reduce interrupts
    By clearing the NAPI interrupts in the NAPI routine
    and not in the interrupt handler, we can reduce the
    number of interrupts. We also don't need any status
    variables as the registers are still valid.
    
    Also, notice that if budget pkts are received, the
    next call to fec_enet_rx_napi will now continue to
    receive the previously pending packets.
    
    To test that this actually reduces interrupts, try
    this command before/after patch
    
    cat /proc/interrupts |grep ether; \
    ping -s2800 192.168.0.201 -f -c1000 ; \
    cat /proc/interrupts |grep ether
    
    For me, before this patch is 2996 interrupts.
    After patch is 2010 interrupts.
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/net/ethernet/freescale/fec.h
+++ drivers/net/ethernet/freescale/fec.h
@@ -530,11 +530,7 @@ struct fec_enet_private {
 
 	unsigned int total_tx_ring_size;
 	unsigned int total_rx_ring_size;
-
-	unsigned long work_tx;
-	unsigned long work_rx;
-	unsigned long work_ts;
-	unsigned long work_mdio;
+	uint	events;
 
 	struct	platform_device *pdev;
 
