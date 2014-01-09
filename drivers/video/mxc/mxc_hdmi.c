6e740e42c3d9e5c77c2963a5849efb4670743264
Troy Kisky <troy.kisky@boundarydevices.com>
2014-01-09 14:29:52 -0700

    mxc_hdmi: enable overflow interrupt after initialization complete
    If enabled too early, a flood of interrupts can happen, and as
    console_lock is held, you cannot see any messages being printed.
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/video/mxc/mxc_hdmi.c
+++ drivers/video/mxc/mxc_hdmi.c
@@ -1269,9 +1269,6 @@ static void mxc_hdmi_phy_init(struct mxc_hdmi *hdmi)
 			|| (hdmi->blank != FB_BLANK_UNBLANK))
 		return;
 
-	if (!hdmi->hdmi_data.video_mode.mDVI)
-		hdmi_enable_overflow_interrupts();
-
 	/*check csc whether needed activated in HDMI mode */
 	cscon = (isColorSpaceConversion(hdmi) &&
 			!hdmi->hdmi_data.video_mode.mDVI);
@@ -1288,6 +1285,8 @@ static void mxc_hdmi_phy_init(struct mxc_hdmi *hdmi)
 	}
 
 	hdmi->phy_enabled = true;
+	if (!hdmi->hdmi_data.video_mode.mDVI)
+		hdmi_enable_overflow_interrupts();
 }
 
 static void hdmi_config_AVI(struct mxc_hdmi *hdmi)
