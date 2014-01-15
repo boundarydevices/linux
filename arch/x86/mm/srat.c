d022e35075e60acbea69f7743e08e5d940e0f851
Toshi Kani <toshi.kani@hp.com>
2014-01-15 16:54:26 +1100

    arch/x86/mm/srat.c: skip NUMA_NO_NODE while parsing SLIT
    When ACPI SLIT table has an I/O locality (i.e.  a locality unique to an
    I/O device), numa_set_distance() emits the warning message below.
    
     NUMA: Warning: node ids are out of bound, from=-1 to=-1 distance=10
    
    acpi_numa_slit_init() calls numa_set_distance() with pxm_to_node(), which
    assumes that all localities have been parsed with SRAT previously.  SRAT
    does not list I/O localities, where as SLIT lists all localities including
    I/Os.  Hence, pxm_to_node() returns NUMA_NO_NODE (-1) for an I/O locality.
     I/O localities are not supported and are ignored today, but emitting such
    warning message leads unnecessary confusion.
    
    Change acpi_numa_slit_init() to avoid calling numa_set_distance() with
    NUMA_NO_NODE.
    
    Signed-off-by: Toshi Kani <toshi.kani@hp.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/x86/mm/srat.c
+++ arch/x86/mm/srat.c
@@ -47,10 +47,16 @@ void __init acpi_numa_slit_init(struct acpi_table_slit *slit)
 {
 	int i, j;
 
-	for (i = 0; i < slit->locality_count; i++)
-		for (j = 0; j < slit->locality_count; j++)
+	for (i = 0; i < slit->locality_count; i++) {
+		if (pxm_to_node(i) == NUMA_NO_NODE)
+			continue;
+		for (j = 0; j < slit->locality_count; j++) {
+			if (pxm_to_node(j) == NUMA_NO_NODE)
+				continue;
 			numa_set_distance(pxm_to_node(i), pxm_to_node(j),
 				slit->entry[slit->locality_count * i + j]);
+		}
+	}
 }
 
 /* Callback for Proximity Domain -> x2APIC mapping */
