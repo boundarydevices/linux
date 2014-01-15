9a902bff845df185faa557cf0b0b05b3fdef5875
Andrew Morton <akpm@linux-foundation.org>
2014-01-15 16:54:47 +1100

    mm-show_mem-remove-show_mem_filter_page_count-fix
    fix parisc
    
    Cc: David Rientjes <rientjes@google.com>
    Cc: James Bottomley <jejb@parisc-linux.org>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Russell King <linux@arm.linux.org.uk>
    Cc: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/parisc/mm/init.c
+++ arch/parisc/mm/init.c
@@ -663,9 +663,6 @@ void show_mem(unsigned int filter)
 
 			total += zone->present_pages;
 			reserved = zone->present_pages - zone->managed_pages;
-
-			if (is_highmem_idx(zoneid))
-				highmem += zone->present_pages;
 		}
 		pgdat_resize_unlock(pgdat, &flags);
 	}
