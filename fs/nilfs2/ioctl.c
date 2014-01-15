45097424f8ca5474676e98d8a4567137c6636738
Wenliang Fan <fanwlexca@gmail.com>
2014-01-15 16:55:43 +1100

    fs/nilfs2: fix integer overflow in nilfs_ioctl_wrap_copy()
    The local variable 'pos' in nilfs_ioctl_wrap_copy function can overflow if
    a large number was passed to argv->v_index from userspace and the sum of
    argv->v_index and argv->v_nmembs exceeds the maximum value of __u64 type
    integer (= ~(__u64)0 = 18446744073709551615).
    
    Here, argv->v_index is a 64-bit width argument to specify the start
    position of target data items (such as segment number, checkpoint number,
    or virtual block address of nilfs), and argv->v_nmembs gives the total
    number of the items that userland programs (such as lssu, lscp, or
    cleanerd) want to get information about, which also gives the maximum
    element count of argv->v_base[] array.
    
    nilfs_ioctl_wrap_copy() calls dofunc() repeatedly and increments the
    position variable 'pos' at the end of each iteration if dofunc() itself
    didn't update 'pos':
    
          if (pos == ppos)
                  pos += n;
    
    This patch prevents the overflow here by rejecting pairs of a start
    position (argv->v_index) and a total count (argv->v_nmembs) which leads to
    the overflow.
    
    Signed-off-by: Wenliang Fan <fanwlexca@gmail.com>
    Cc: Vyacheslav Dubeyko <slava@dubeyko.com>
    Signed-off-by: Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/nilfs2/ioctl.c
+++ fs/nilfs2/ioctl.c
@@ -57,6 +57,9 @@ static int nilfs_ioctl_wrap_copy(struct the_nilfs *nilfs,
 	if (argv->v_size > PAGE_SIZE)
 		return -EINVAL;
 
+	if (argv->v_index > (~(__u64)0 - argv->v_nmembs))
+		return -EINVAL;
+
 	buf = (void *)__get_free_pages(GFP_NOFS, 0);
 	if (unlikely(!buf))
 		return -ENOMEM;
