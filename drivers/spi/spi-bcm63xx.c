e627c6820f5a1757a5fba499bb716dd455d314fd
Axel Lin <axel.lin@ingics.com>
2014-02-09 11:06:04 +0800

    spi: Use reinit_completion at appropriate places
    Calling init_completion() once is enough.
    For the rest of the iterations, call reinit_completion() instead.
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>
    (cherry picked from commit aa0fe82629f19efba5c870bc9be089a4f8056a75)
    (cherry picked from commit fd20fe148977afbdda2f3d90eb6226b436ab9e09)

--- drivers/spi/spi-bcm63xx.c
+++ drivers/spi/spi-bcm63xx.c
@@ -168,7 +168,7 @@ static int bcm63xx_txrx_bufs(struct spi_device *spi, struct spi_transfer *first,
 			       transfer_list);
 	}
 
-	init_completion(&bs->done);
+	reinit_completion(&bs->done);
 
 	/* Fill in the Message control register */
 	msg_ctl = (len << SPI_BYTE_CNT_SHIFT);
@@ -352,6 +352,7 @@ static int bcm63xx_spi_probe(struct platform_device *pdev)
 	}
 
 	bs = spi_master_get_devdata(master);
+	init_completion(&bs->done);
 
 	platform_set_drvdata(pdev, master);
 	bs->pdev = pdev;
