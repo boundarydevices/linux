eeb52d1f6ed85d4eda114f44c8609f91b78e5df7
Ranjani Vaidyanathan <Ranjani.Vaidyanathan@freescale.com>
2014-02-13 16:50:20 -0600

    ENGR00297285-1 [MX6x] Support IRAM page table when DDR is in self-refresh
    Whenever DDR is explicitly put into self-refresh, we need to ensure
    that no access are made to the DDR. All the bus masters excpet ARM
    are shutdown gracefully.
    The ARM core can continue to access the DDR due to:
    1. Speculative accesses
       This can be prevented by flushing the Branch Target Address Cache
    2. Aggressive Prefetching
       This can be minimized by adding nops.
    Apart from this the TLB architecture in ARM does not guarantee that
    an entry remains in the TLB unless its explicitly locked. Even if
    free slots are available an entry maybe evicted. So flushing the TLB
    does not guarantee a page table walk will not happen.
    
    The solution is to put a minimized page table in IRAM that can be used when
    DDR is in self-refresh. The IRAM page tables should have entries for IRAM,
    AIPS1 and AIPS2 as these entries will be needed by the code that puts DDR
    into self-refresh. It should not contain any entries that point to the DDR.
    
    This patch set accomplishes the following:
    1. Set the IRAM to be mapped as 1M sections in the high mem region.
       This makes it possible to create entries for the IRAM code in the IRAM page table.
    We need to ensure that both the DDR and IRAM page table have mapping for the IRAM code.
    2. Ensure the IRAM, AIPS1, AIPS2 have entries in the IRAM page table.
    3. Save TTBR1
    4. Set TTBR1 to point to the page tables stored in IRAM. Switch to using
    TTBR1 before DDR is put into self-refresh. Ensure the following settings:
        a. TTBCR.N = 1
         This means the 0-2G virtual address space is translated using TTBR0
         and 2G-4G is translated using TTBR1.
        b. Set TTBCR.PD0 = 1
          With this setting page table walks using TTBR0 are disabled.
    4. After the DDR has exited self-refresh, reset TTBCR to 0 (TTBR0 will
    be used for translations now).
    5. Restore TTBR1
    
    Even though TTBR1 is only used to decode the top 2G of virtual address
    space, ARM requires that we allocate the entire 16KB for the page table.
    To minimize IRAM/OCRAM required, we put the code in the bottom 8K and
    page table entries in the top 8K.
    This requires the low power code be optimized to occupy as little space
    as possible.
    
    This commit is cherry-picked:
    93ae491d9dbe34a91e2dd5832b02b0f0a390ddbe
    
    Signed-off-by: Ranjani Vaidyanathan <Ranjani.Vaidyanathan@freescale.com>

--- arch/arm/mach-imx/cpuidle-imx6sl.c
+++ arch/arm/mach-imx/cpuidle-imx6sl.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2013 Freescale Semiconductor, Inc.
+ * Copyright (C) 2012-2014 Freescale Semiconductor, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -20,36 +20,38 @@
 
 #include "common.h"
 #include "cpuidle.h"
+#include "hardware.h"
 
 extern u32 audio_bus_freq_mode;
 extern u32 ultra_low_bus_freq_mode;
 extern unsigned long reg_addrs[];
 extern void imx6sl_low_power_wfi(void);
 
+extern unsigned long save_ttbr1(void);
+extern void restore_ttbr1(unsigned long ttbr1);
+
 static void __iomem *iomux_base;
 static void *wfi_iram_base;
 
 void (*imx6sl_wfi_in_iram_fn)(void *wfi_iram_base,
-	void *iomux_addr, void *regs_addr, u32 audio_mode) = NULL;
+	void *iomux_addr, u32 audio_mode) = NULL;
 
-#define WFI_IN_IRAM_SIZE	0x1000
 
 static int imx6sl_enter_wait(struct cpuidle_device *dev,
 			    struct cpuidle_driver *drv, int index)
 {
 	imx6_set_lpm(WAIT_UNCLOCKED);
 	if (ultra_low_bus_freq_mode || audio_bus_freq_mode) {
-		/*
-		 * Flush the TLB, to ensure no TLB maintenance occurs
-		 * when DDR is in self-refresh.
-		 */
-		local_flush_tlb_all();
+		unsigned long ttbr1;
 		/*
 		 * Run WFI code from IRAM.
 		 * Drop the DDR freq to 1MHz and AHB to 3MHz
 		 * Also float DDR IO pads.
 		 */
-		imx6sl_wfi_in_iram_fn(wfi_iram_base, iomux_base, reg_addrs, audio_bus_freq_mode);
+		ttbr1 = save_ttbr1();
+		imx6sl_wfi_in_iram_fn(wfi_iram_base, iomux_base,
+					audio_bus_freq_mode);
+		restore_ttbr1(ttbr1);
 	} else {
 		imx6sl_set_wait_clk(true);
 		cpu_do_idle();
@@ -83,10 +85,8 @@ static struct cpuidle_driver imx6sl_cpuidle_driver = {
 
 int __init imx6sl_cpuidle_init(void)
 {
-	struct platform_device *ocram_dev;
 	unsigned int iram_paddr;
 	struct device_node *node;
-	struct gen_pool *iram_pool;
 
 	node = of_find_compatible_node(NULL, NULL, "fsl,imx6sl-iomuxc");
 	if (!node) {
@@ -96,50 +96,13 @@ int __init imx6sl_cpuidle_init(void)
 	iomux_base = of_iomap(node, 0);
 	WARN(!iomux_base, "unable to map iomux registers\n");
 
-	node = NULL;
-	node = of_find_compatible_node(NULL, NULL, "mmio-sram");
-	if (!node) {
-		pr_err("%s: failed to find ocram node\n",
-			__func__);
-		return -EINVAL;
-	}
-
-	ocram_dev = of_find_device_by_node(node);
-	if (!ocram_dev) {
-		pr_err("failed to find ocram device!\n");
-		return -EINVAL;
-	}
-
-	iram_pool = dev_get_gen_pool(&ocram_dev->dev);
-	if (!iram_pool) {
-		pr_err("iram pool unavailable!\n");
-		return -EINVAL;
-	}
-	/*
-	 * Allocate IRAM memory when ARM executes WFI in
-	 * ultra_low_power_mode.
-	 */
-	wfi_iram_base = (void *)gen_pool_alloc(iram_pool,
-						WFI_IN_IRAM_SIZE);
-	if (!wfi_iram_base) {
-		pr_err("Cannot alloc iram for wfi code!\n");
-		return -ENOMEM;
-	}
-
-	iram_paddr = gen_pool_virt_to_phys(iram_pool,
-				(unsigned long)wfi_iram_base);
-	/*
-	 * Need to remap the area here since we want
-	 * the memory region to be executable.
-	 */
-	wfi_iram_base = __arm_ioremap(iram_paddr,
-					WFI_IN_IRAM_SIZE,
-					MT_MEMORY_NONCACHED);
-	if (!wfi_iram_base)
-		pr_err("wfi_ram_base NOT remapped\n");
+	iram_paddr = MX6SL_WFI_IRAM_ADDR;
+	/* Calculate the virtual address of the code */
+	wfi_iram_base = (void *)IMX_IO_P2V(MX6Q_IRAM_TLB_BASE_ADDR) +
+				(iram_paddr - MX6Q_IRAM_TLB_BASE_ADDR);
 
 	imx6sl_wfi_in_iram_fn = (void *)fncpy(wfi_iram_base,
-		&imx6sl_low_power_wfi, WFI_IN_IRAM_SIZE);
+		&imx6sl_low_power_wfi, MX6SL_WFI_IRAM_CODE_SIZE);
 
 	return cpuidle_register(&imx6sl_cpuidle_driver, NULL);
 }
