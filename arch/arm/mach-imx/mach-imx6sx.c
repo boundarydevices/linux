d9ccbb28d31b8214b0809ff6b6848311190c0a9f
Fugang Duan <B38611@freescale.com>
2014-02-15 12:43:26 +0800

    ENGR00299323-13 ARM:imx:imx6sx: add enet init for imx6sx platform
    - Init GPR1 register to select enet1 and enet2 refrence clock from
      internal PLL.
    - Add enet MAC address checking from fuse.
    - Add some phy fixup, set RGMII IO voltage to 1.8V.
    
    Signed-off-by: Fugang Duan <B38611@freescale.com>

--- arch/arm/mach-imx/mach-imx6sx.c
+++ arch/arm/mach-imx/mach-imx6sx.c
@@ -14,6 +14,7 @@
 #include <linux/of_address.h>
 #include <linux/of_platform.h>
 #include <linux/opp.h>
+#include <linux/phy.h>
 #include <linux/regmap.h>
 #include <linux/mfd/syscon.h>
 #include <linux/mfd/syscon/imx6q-iomuxc-gpr.h>
@@ -24,6 +25,67 @@
 #include "common.h"
 #include "cpuidle.h"
 #include "hardware.h"
+
+static void __init imx6sx_enet_clk_sel(void)
+{
+	struct regmap *gpr;
+
+	/*
+	 * By default, set enet RGMII TX_CLK from phy CLK_25M pin
+	 * From external via ENETn_TX_CLK pin:
+	 * 	GPR1[14:13] = 11b, GPR1[18:17] = 00b
+	 * From internal PLL:
+	 * 	GPR1[14:13] = 00b
+	 */
+	gpr = syscon_regmap_lookup_by_compatible("fsl,imx6sx-iomuxc-gpr");
+	if (!IS_ERR(gpr)) {
+		regmap_update_bits(gpr, IOMUXC_GPR1,
+				IMX6SX_GPR1_FEC_CLOCK_MUX_SEL_MASK, 0);
+		regmap_update_bits(gpr, IOMUXC_GPR1,
+				IMX6SX_GPR1_FEC_CLOCK_PAD_DIR_MASK, 0);
+	} else
+		pr_err("failed to find fsl,imx6sx-iomux-gpr regmap\n");
+}
+
+static int ar8031_phy_fixup(struct phy_device *dev)
+{
+	u16 val;
+
+	/* Set RGMII IO voltage to 1.8V */
+	phy_write(dev, 0x1d, 0x1f);
+	phy_write(dev, 0x1e, 0x8);
+
+	/* disable phy AR8031 SmartEEE function. */
+	phy_write(dev, 0xd, 0x3);
+	phy_write(dev, 0xe, 0x805d);
+	phy_write(dev, 0xd, 0x4003);
+	val = phy_read(dev, 0xe);
+	val &= ~(0x1 << 8);
+	phy_write(dev, 0xe, val);
+
+	/* introduce tx clock delay */
+	phy_write(dev, 0x1d, 0x5);
+	val = phy_read(dev, 0x1e);
+	val |= 0x0100;
+	phy_write(dev, 0x1e, val);
+
+	return 0;
+}
+
+#define PHY_ID_AR8031   0x004dd074
+static void __init imx6sx_enet_phy_init(void)
+{
+	if (IS_BUILTIN(CONFIG_PHYLIB))
+		phy_register_fixup_for_uid(PHY_ID_AR8031, 0xffffffff,
+			ar8031_phy_fixup);
+}
+
+static inline void imx6sx_enet_init(void)
+{
+	imx6_enet_mac_init("fsl,imx6sx-fec");
+	imx6sx_enet_phy_init();
+	imx6sx_enet_clk_sel();
+}
 
 static void __init imx6sx_init_machine(void)
 {
@@ -37,6 +99,7 @@ static void __init imx6sx_init_machine(void)
 
 	of_platform_populate(NULL, of_default_bus_match_table, NULL, parent);
 
+	imx6sx_enet_init();
 	imx_anatop_init();
 	imx6_pm_init();
 }
