6d3cc68fa38672401d615d0160eee510dd54355a
Fang Hui <b31070@freescale.com>
2014-02-20 17:33:02 +0800

    MA-5035 On 3.10.17 kernel, Camera can't preview in most resolutions
    when queue buffer, just compare input size with cam->v2f.fmt.pix.sizeimage,
    don't with PAGE_ALIGN(cam->v2f.fmt.pix.sizeimage).
    
    Signed-off-by: Fang Hui <b31070@freescale.com>

--- drivers/media/platform/mxc/capture/mxc_v4l2_capture.c
+++ drivers/media/platform/mxc/capture/mxc_v4l2_capture.c
@@ -349,7 +349,7 @@ static int mxc_v4l2_prepare_bufs(cam_data *cam, struct v4l2_buffer *buf)
 	pr_debug("In MVC:mxc_v4l2_prepare_bufs\n");
 
 	if (buf->index < 0 || buf->index >= FRAME_NUM || buf->length <
-			PAGE_ALIGN(cam->v2f.fmt.pix.sizeimage)) {
+			cam->v2f.fmt.pix.sizeimage) {
 		pr_err("ERROR: v4l2 capture: mxc_v4l2_prepare_bufs buffers "
 			"not allocated,index=%d, length=%d\n", buf->index,
 			buf->length);
