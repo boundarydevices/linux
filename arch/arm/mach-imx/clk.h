e04615819197d4e8f6f4033f4dac453c40a324d9
Huang Shijie <b32955@freescale.com>
2014-02-25 13:14:26 +0800

    ENGR00300430-12 ARM: clk: add a new helper which can re-parent the clock
    The clock for qspi may be different when different NOR flashes are connected
    to the board.
    
    So the IMX6SX_CLK_QSPI1_SEL/IMX6SX_CLK_QSPI2_SEL should have the re-parent
    capability.
    
    This patch adds a new helper to register the clock which needs the
    re-parent capability.
    
    Signed-off-by: Huang Shijie <b32955@freescale.com>

--- arch/arm/mach-imx/clk.h
+++ arch/arm/mach-imx/clk.h
@@ -105,6 +105,16 @@ static inline struct clk *imx_clk_mux_flags(const char *name,
 			&imx_ccm_lock);
 }
 
+/* we can use this helper to register the clock which needs the re-parent. */
+static inline struct clk *imx_clk_mux_flags_reparent(const char *name,
+		void __iomem *reg, u8 shift, u8 width, const char **parents,
+		int num_parents, unsigned long flags)
+{
+	return clk_register_mux(NULL, name, parents, num_parents,
+			flags, reg, shift, width, 0,
+			&imx_ccm_lock);
+}
+
 static inline struct clk *imx_clk_fixed_factor(const char *name,
 		const char *parent, unsigned int mult, unsigned int div)
 {
