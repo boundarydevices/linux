237701a87121fd1e50f20aa69f7ce6c3d97f8a24
Troy Kisky <troy.kisky@boundarydevices.com>
2014-03-04 19:24:08 -0700

    mxc_v4l2_capture: add mxc_camera_common_lock/unlock, fix device name

--- drivers/media/platform/mxc/capture/mxc_v4l2_capture.h
+++ drivers/media/platform/mxc/capture/mxc_v4l2_capture.h
@@ -260,4 +260,7 @@ struct sensor_data {
 };
 
 void set_mclk_rate(uint32_t *p_mclk_freq, uint32_t csi);
+void mxc_camera_common_lock(void);
+void mxc_camera_common_unlock(void);
+
 #endif				/* __MXC_V4L2_CAPTURE_H__ */
