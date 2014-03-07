6376acf8654f1038bc5c5e507a0361cadeacf00b
Troy Kisky <troy.kisky@boundarydevices.com>
2014-03-06 19:17:08 -0700

    add new API ipu_channel_request/disable/free

--- drivers/media/platform/mxc/capture/ipu_csi_enc.c
+++ drivers/media/platform/mxc/capture/ipu_csi_enc.c
@@ -161,9 +161,9 @@ static int csi_enc_setup(cam_data *cam)
 	}
 #endif
 
-	err = ipu_init_channel(cam->ipu, chan, &params);
-	if (err != 0) {
-		printk(KERN_ERR "ipu_init_channel %d\n", err);
+	err = ipu_channel_request(cam->ipu, chan, &params, &cam->ipu_chan);
+	if (err) {
+		pr_err("%s: ipu_channel_request %d\n", __func__, err);
 		return err;
 	}
 
@@ -285,16 +285,15 @@ static int csi_enc_disabling_tasks(void *private)
 {
 	cam_data *cam = (cam_data *) private;
 	int err = 0;
-	ipu_channel_t chan = (cam->csi == 0) ? CSI_MEM0 : CSI_MEM1;
 #ifdef CONFIG_MXC_MIPI_CSI2
 	void *mipi_csi2_info;
 	int ipu_id;
 	int csi_id;
 #endif
 
-	err = ipu_disable_channel(cam->ipu, chan, true);
+	err = ipu_channel_disable(cam->ipu_chan, true);
 
-	ipu_uninit_channel(cam->ipu, chan, NULL);
+	ipu_channel_free(&cam->ipu_chan);
 
 	if (cam->dummy_frame.vaddress != 0) {
 		dma_free_coherent(0, cam->dummy_frame.buffer.length,
