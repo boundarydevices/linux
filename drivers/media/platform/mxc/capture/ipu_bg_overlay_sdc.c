12ae19603ff5ef9f7cb7d8a6b65db4b0ebacb4a4
Troy Kisky <troy.kisky@boundarydevices.com>
2014-03-07 13:08:38 -0700

    mipi: reduce code, pair up mipi_csi2_pixelclk_enable/disable

--- drivers/media/platform/mxc/capture/ipu_bg_overlay_sdc.c
+++ drivers/media/platform/mxc/capture/ipu_bg_overlay_sdc.c
@@ -126,12 +126,6 @@ static int csi_enc_setup(cam_data *cam)
 	int err = 0, sensor_protocol = 0;
 	ipu_channel_t chan;
 
-#ifdef CONFIG_MXC_MIPI_CSI2
-	void *mipi_csi2_info;
-	int ipu_id;
-	int csi_id;
-#endif
-
 	if (!cam) {
 		printk(KERN_ERR "cam private is NULL\n");
 		return -ENXIO;
@@ -160,36 +154,9 @@ static int csi_enc_setup(cam_data *cam)
 		printk(KERN_ERR "sensor protocol unsupported\n");
 		return -EINVAL;
 	}
-
-#ifdef CONFIG_MXC_MIPI_CSI2
-	mipi_csi2_info = mipi_csi2_get_info();
-
-	if (mipi_csi2_info) {
-		if (mipi_csi2_get_status(mipi_csi2_info)) {
-			ipu_id = mipi_csi2_get_bind_ipu(mipi_csi2_info);
-			csi_id = mipi_csi2_get_bind_csi(mipi_csi2_info);
-
-			if (cam->ipu == ipu_get_soc(ipu_id)
-				&& cam->csi == csi_id) {
-				params.csi_mem.mipi_en = true;
-				params.csi_mem.mipi_vc =
-				mipi_csi2_get_virtual_channel(mipi_csi2_info);
-				params.csi_mem.mipi_id =
-				mipi_csi2_get_datatype(mipi_csi2_info);
-
-				mipi_csi2_pixelclk_enable(mipi_csi2_info);
-			} else {
-				params.csi_mem.mipi_en = false;
-				params.csi_mem.mipi_vc = 0;
-				params.csi_mem.mipi_id = 0;
-			}
-		} else {
-			params.csi_mem.mipi_en = false;
-			params.csi_mem.mipi_vc = 0;
-			params.csi_mem.mipi_id = 0;
-		}
-	}
-#endif
+	err = cam_mipi_csi2_enable(cam, &params.csi_mem.mipi);
+	if (err)
+		return err;
 
 	if (cam->vf_bufs_vaddr[0]) {
 		dma_free_coherent(cam->dev, cam->vf_bufs_size[0],
@@ -378,12 +345,8 @@ static int bg_overlay_start(void *private)
 static int bg_overlay_stop(void *private)
 {
 	int err = 0;
+	int err2 = 0;
 	cam_data *cam = (cam_data *) private;
-#ifdef CONFIG_MXC_MIPI_CSI2
-	void *mipi_csi2_info;
-	int ipu_id;
-	int csi_id;
-#endif
 
 	if (cam->overlay_active == false)
 		return 0;
@@ -394,21 +357,7 @@ static int bg_overlay_stop(void *private)
 
 	csi_buffer_num = 0;
 
-#ifdef CONFIG_MXC_MIPI_CSI2
-	mipi_csi2_info = mipi_csi2_get_info();
-
-	if (mipi_csi2_info) {
-		if (mipi_csi2_get_status(mipi_csi2_info)) {
-			ipu_id = mipi_csi2_get_bind_ipu(mipi_csi2_info);
-			csi_id = mipi_csi2_get_bind_csi(mipi_csi2_info);
-
-			if (cam->ipu == ipu_get_soc(ipu_id)
-				&& cam->csi == csi_id)
-				mipi_csi2_pixelclk_disable(mipi_csi2_info);
-		}
-	}
-#endif
-
+	err2 = cam_mipi_csi2_disable(cam);
 	flush_work(&cam->csi_work_struct);
 	cancel_work_sync(&cam->csi_work_struct);
 
@@ -440,7 +389,7 @@ static int bg_overlay_stop(void *private)
 	}
 
 	cam->overlay_active = false;
-	return err;
+	return err ? err : err2;
 }
 
 /*!
