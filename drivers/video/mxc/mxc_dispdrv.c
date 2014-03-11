d70b886b0bf17bc4d3a2675cb124c123b293bda0
Liu Ying <Ying.Liu@freescale.com>
2014-03-11 14:15:33 +0800

    ENGR00302472-9 video: mxc disp: Drop post_init callback
    Since there is no encoder driver using the post_init
    callback, let's drop it.
    
    Signed-off-by: Liu Ying <Ying.Liu@freescale.com>

--- drivers/video/mxc/mxc_dispdrv.c
+++ drivers/video/mxc/mxc_dispdrv.c
@@ -18,8 +18,6 @@
  * A display device driver could call mxc_dispdrv_register(drv) in its dev_probe() function.
  * Move all dev_probe() things into mxc_dispdrv_driver->init(), init() function should init
  * and feedback setting;
- * Necessary deferred operations can be done in mxc_dispdrv_driver->post_init(),
- * after dev_id and disp_id pass usage check;
  * Move all dev_remove() things into mxc_dispdrv_driver->deinit();
  * Move all dev_suspend() things into fb_notifier for SUSPEND, if there is;
  * Move all dev_resume() things into fb_notifier for RESUME, if there is;
