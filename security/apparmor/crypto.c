20d9b0a26f248dd3b6929f6dc1c609f23908b719
Vinícius Tinti <viniciustinti@gmail.com>
2014-04-04 18:18:00 -0300

    apparmor: LLVMLinux: Remove VLAIS
    Replaced the use of a Variable Length Array In Struct (VLAIS) with a C99
    compliant equivalent. This is the original VLAIS struct.
    
    struct {
    	struct shash_desc shash;
    	char ctx[crypto_shash_descsize(apparmor_tfm)];
    } desc;
    
    This patch instead allocates the appropriate amount of memory using an
    char array.
    
    The new code can be compiled with both gcc and clang.
    
    struct shash_desc contains a flexible array member member ctx declared with
    CRYPTO_MINALIGN_ATTR, so sizeof(struct shash_desc) aligns the beginning
    of the array declared after struct shash_desc with long long.
    
    No trailing padding is required because it is not a struct type that can
    be used in an array.
    
    The CRYPTO_MINALIGN_ATTR is required so that desc is aligned with long long
    as would be the case for a struct containing a member with
    CRYPTO_MINALIGN_ATTR.
    
    Signed-off-by: Jan-Simon Möller <dl9pf@gmx.de>
    Signed-off-by: Behan Webster <behanw@converseincode.com>
    Signed-off-by: Vinícius Tinti <viniciustinti@gmail.com>
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>
    Acked-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/crypto.c
+++ security/apparmor/crypto.c
@@ -32,10 +32,9 @@ unsigned int aa_hash_size(void)
 int aa_calc_profile_hash(struct aa_profile *profile, u32 version, void *start,
 			 size_t len)
 {
-	struct {
-		struct shash_desc shash;
-		char ctx[crypto_shash_descsize(apparmor_tfm)];
-	} desc;
+	char desc[sizeof(struct shash_desc) +
+		crypto_shash_descsize(apparmor_tfm)] CRYPTO_MINALIGN_ATTR;
+	struct shash_desc *shash = (struct shash_desc *)desc;
 	int error = -ENOMEM;
 	u32 le32_version = cpu_to_le32(version);
 
@@ -46,19 +45,19 @@ int aa_calc_profile_hash(struct aa_profile *profile, u32 version, void *start,
 	if (!profile->hash)
 		goto fail;
 
-	desc.shash.tfm = apparmor_tfm;
-	desc.shash.flags = 0;
+	shash->tfm = apparmor_tfm;
+	shash->flags = 0;
 
-	error = crypto_shash_init(&desc.shash);
+	error = crypto_shash_init(shash);
 	if (error)
 		goto fail;
-	error = crypto_shash_update(&desc.shash, (u8 *) &le32_version, 4);
+	error = crypto_shash_update(shash, (u8 *) &le32_version, 4);
 	if (error)
 		goto fail;
-	error = crypto_shash_update(&desc.shash, (u8 *) start, len);
+	error = crypto_shash_update(shash, (u8 *) start, len);
 	if (error)
 		goto fail;
-	error = crypto_shash_final(&desc.shash, profile->hash);
+	error = crypto_shash_final(shash, profile->hash);
 	if (error)
 		goto fail;
 
