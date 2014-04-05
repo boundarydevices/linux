4a452ac52d393b203b731a7695dba2f6ba91269d
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2014-04-05 17:57:45 +0200

    ARM: imx: factor device tree timer initialization
    Signed-off-by: Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
    [shawn.guo: cherry-pick commit 876292d66791 from upstream]
    Signed-off-by: Shawn Guo <shawn.guo@freescale.com>
    
    Conflicts:
    	arch/arm/mach-imx/clk-imx6sl.c

--- arch/arm/mach-imx/clk-imx6sl.c
+++ arch/arm/mach-imx/clk-imx6sl.c
@@ -169,7 +169,6 @@ static void __init imx6sl_clocks_init(struct device_node *ccm_node)
 {
 	struct device_node *np;
 	void __iomem *base;
-	int irq;
 	int i;
 	int ret;
 
@@ -385,10 +384,7 @@ static void __init imx6sl_clocks_init(struct device_node *ccm_node)
 	imx6q_set_lpm(WAIT_CLOCKED);
 
 	np = of_find_compatible_node(NULL, NULL, "fsl,imx6sl-gpt");
-	base = of_iomap(np, 0);
-	WARN_ON(!base);
-	irq = irq_of_parse_and_map(np, 0);
-	mxc_timer_init(base, irq);
+	mxc_timer_init_dt(np);
 
 	/* Initialize Video PLLs to valid frequency (650MHz). */
 	clk_set_rate(clks[IMX6SL_CLK_PLL5_VIDEO_DIV], 650000000);
