12a3949ef4167bd3b88f3611c0aa6e083f4c9a0f
Troy Kisky <troy.kisky@boundarydevices.com>
2014-04-14 17:03:04 -0700

    sdhci-esdhc-imx: add vqmmc-1-8-v property to stop regulator errors

--- drivers/mmc/host/sdhci-esdhc-imx.c
+++ drivers/mmc/host/sdhci-esdhc-imx.c
@@ -1296,6 +1296,9 @@ sdhci_esdhc_imx_probe_dt(struct platform_device *pdev,
 	else
 		boarddata->support_vsel = true;
 
+	if (of_find_property(np, "vqmmc-1-8-v", NULL))
+		boarddata->vqmmc_18v = true;
+
 	if (of_property_read_u32(np, "fsl,delay-line", &boarddata->delay_line))
 		boarddata->delay_line = 0;
 
@@ -1526,6 +1529,9 @@ static int sdhci_esdhc_imx_probe(struct platform_device *pdev)
 		if (err)
 			goto disable_clk;
 	}
+
+	if (imx_data->boarddata.vqmmc_18v)
+		host->quirks2 |= SDHCI_QUIRK2_VQMMC_1_8_V;
 
 	if (of_id)
 		err = sdhci_esdhc_imx_probe_dt(pdev, host, imx_data);
