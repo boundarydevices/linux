1fbe6006fc28a9d32a89ff083b6168a3a0345bfc
Troy Kisky <troy.kisky@boundarydevices.com>
2014-04-14 17:03:04 -0700

    sdhci-esdhc-imx: add vqmmc-1-8-v property to stop regulator errors

--- include/linux/mmc/sdhci.h
+++ include/linux/mmc/sdhci.h
@@ -103,9 +103,10 @@ struct sdhci_host {
 #define SDHCI_QUIRK2_CARD_ON_NEEDS_BUS_ON		(1<<4)
 #define SDHCI_QUIRK2_NOSTD_TIMEOUT_COUNTER		(1<<5)
 /* Controller has a non-standard host control register */
-#define SDHCI_QUIRK2_BROKEN_HOST_CONTROL		(1<<5)
+#define SDHCI_QUIRK2_BROKEN_HOST_CONTROL		(1<<6)
 /* Controller does not support HS200 */
-#define SDHCI_QUIRK2_BROKEN_HS200			(1<<6)
+#define SDHCI_QUIRK2_BROKEN_HS200			(1<<7)
+#define SDHCI_QUIRK2_VQMMC_1_8_V			(1<<8)
 
 	int irq;		/* Device IRQ */
 	void __iomem *ioaddr;	/* Mapped address */
