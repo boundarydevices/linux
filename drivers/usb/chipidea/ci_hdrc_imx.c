7e03d72bf9b08d186d76cba0b64d02b63afabfc3
Troy Kisky <troy.kisky@boundarydevices.com>
2014-04-14 17:01:08 -0700

    ci_hdrc_imx: add reset-gpios
    ci_hdrc_imx: fix polarity of reset-gpios
    
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>
    
    ci_hdrc_imx: apply reset when module removed
    ci_hdrc_imx: reset gpio are allocated in correct state, no need to set again
    ci_hdrc_imx: allow up to 5 reset-gpios
    ci_hdrc_imx: add delay between gpio-reset releases to avoid power surge
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/usb/chipidea/ci_hdrc_imx.c
+++ drivers/usb/chipidea/ci_hdrc_imx.c
@@ -100,6 +100,11 @@ static const struct of_device_id ci_hdrc_imx_dt_ids[] = {
 };
 MODULE_DEVICE_TABLE(of, ci_hdrc_imx_dt_ids);
 
+struct gpio_desc {
+	int gpio;
+	int active_low;
+};
+
 struct ci_hdrc_imx_data {
 	struct usb_phy *phy;
 	struct platform_device *ci_pdev;
@@ -121,6 +126,7 @@ struct ci_hdrc_imx_data {
 	struct clk *clk_per;
 	/* --------------------------------- */
 	struct pm_qos_request pm_qos_req;
+	struct gpio_desc reset_gpios[5];
 };
 
 static char *imx_usb_charger_supplied_to[] = {
@@ -458,6 +464,37 @@ static int imx_usb_register_charger(struct usb_charger *charger,
 	return 0;
 }
 
+static int setup_reset_gpios(struct platform_device *pdev, struct ci_hdrc_imx_data *data)
+{
+	int err = 0;
+	int i;
+	int gpio;
+	enum of_gpio_flags flags;
+	struct device *dev = &pdev->dev;
+	struct device_node *np = dev->of_node;
+
+	for (i = 0 ; i < ARRAY_SIZE(data->reset_gpios); i++) {
+		if (i)
+			msleep(3);
+		data->reset_gpios[i].gpio = -1;
+		gpio = of_get_named_gpio_flags(np, "reset-gpios", i, &flags);
+		pr_info("%s:%d, flags %d\n", __func__, gpio, flags & OF_GPIO_ACTIVE_LOW);
+		if (!gpio_is_valid(gpio))
+			break;
+
+		data->reset_gpios[i].active_low = flags & OF_GPIO_ACTIVE_LOW;
+		err = devm_gpio_request_one(dev, gpio, flags & OF_GPIO_ACTIVE_LOW ?
+				GPIOF_OUT_INIT_HIGH : GPIOF_OUT_INIT_LOW,
+				"ehci_reset_gpio");
+		if (err) {
+			dev_err(dev, "can't request reset gpio %d", gpio);
+			break;
+		}
+		data->reset_gpios[i].gpio = gpio;
+	}
+	return err;
+}
+
 static int ci_hdrc_imx_probe(struct platform_device *pdev)
 {
 	struct ci_hdrc_imx_data *data;
@@ -483,6 +520,7 @@ static int ci_hdrc_imx_probe(struct platform_device *pdev)
 		return -ENOMEM;
 
 	platform_set_drvdata(pdev, data);
+	setup_reset_gpios(pdev, data);
 
 	data->data = imx_platform_flag;
 	pdata.flags |= imx_platform_flag->flags;
@@ -660,6 +698,7 @@ err_bus_freq:
 static int ci_hdrc_imx_remove(struct platform_device *pdev)
 {
 	struct ci_hdrc_imx_data *data = platform_get_drvdata(pdev);
+	int i;
 
 	if (data->supports_runtime_pm) {
 		pm_runtime_get_sync(&pdev->dev);
@@ -674,6 +713,11 @@ static int ci_hdrc_imx_remove(struct platform_device *pdev)
 	if (data->hsic_pad_regulator)
 		regulator_disable(data->hsic_pad_regulator);
 
+	for (i = 0 ; i < ARRAY_SIZE(data->reset_gpios); i++) {
+		if (!gpio_is_valid(data->reset_gpios[i].gpio))
+			break;
+		gpio_set_value(data->reset_gpios[i].gpio, data->reset_gpios[i].active_low ? 0 : 1);
+	}
 	return 0;
 }
 
