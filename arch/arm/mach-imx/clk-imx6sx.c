306115044be1722ec2a8ee05c0ed1e3dab8dafe8
Shawn Guo <shawn.guo@freescale.com>
2014-05-26 16:37:25 +0800

    ENGR00318063-5: ARM: imx: set CLK_SET_PARENT_GATE flag for glitchy mux clocks
    The mux clocks found on imx6 SoCs are all glitchy ones except
    pll1_sw_clk_sel, axi_sel, periph_clk_sel and periph2_clk_sel.  When
    switching parent clock of a glitchy mux without gating, a glitch could
    be generated and propagated into the downstream divider, and hence locks
    up the divider and results in no clock output.
    
    To avoid the situation, the parent switching should happen only when
    the clock is gated.  Add CLK_SET_PARENT_GATE flag for i.MX mux clocks,
    so that clock core will make that check during clk_set_parent() call.
    
    Since glitchless clocks do not need this flag, we create
    imx_clk_mux_glitchless() without this flag for them.  The periph_clk_sel
    and periph2_clk_sel are registered by imx_clk_busy_mux() which does not
    set this flag anyway, so they need no change.
    
    shawn.guo: cherry-pick commit 10bae51219b3 from imx_3.10.y
    
    Signed-off-by: Shawn Guo <shawn.guo@freescale.com>

--- arch/arm/mach-imx/clk-imx6sx.c
+++ arch/arm/mach-imx/clk-imx6sx.c
@@ -278,9 +278,9 @@ static void __init imx6sx_clocks_init(struct device_node *ccm_node)
 
 	/*                                                name                reg           shift   width   parent_names       num_parents */
 	clks[IMX6SX_CLK_STEP]               = imx_clk_mux("step",             base + 0xc,   8,      1,      step_sels,         ARRAY_SIZE(step_sels));
-	clks[IMX6SX_CLK_PLL1_SW]            = imx_clk_mux("pll1_sw",          base + 0xc,   2,      1,      pll1_sw_sels,      ARRAY_SIZE(pll1_sw_sels));
+	clks[IMX6SX_CLK_PLL1_SW]            = imx_clk_mux_glitchless("pll1_sw", base + 0xc, 2,      1,      pll1_sw_sels,      ARRAY_SIZE(pll1_sw_sels));
 	clks[IMX6SX_CLK_OCRAM_ALT_SEL]      = imx_clk_mux("ocram_alt_sel",    base + 0x14,  7,      1,      ocram_alt_sels,    ARRAY_SIZE(ocram_alt_sels));
-	clks[IMX6SX_CLK_OCRAM_SEL]          = imx_clk_mux("ocram_sel",        base + 0x14,  6,      1,      ocram_sels,        ARRAY_SIZE(ocram_sels));
+	clks[IMX6SX_CLK_OCRAM_SEL]          = imx_clk_mux_glitchless("ocram_sel", base + 0x14, 6,   1,      ocram_sels,        ARRAY_SIZE(ocram_sels));
 	clks[IMX6SX_CLK_PERIPH_PRE]         = imx_clk_mux("periph_pre",       base + 0x18,  18,     2,      periph_pre_sels,   ARRAY_SIZE(periph_pre_sels));
 	clks[IMX6SX_CLK_PERIPH2_PRE]        = imx_clk_mux("periph2_pre",      base + 0x18,  21,     2,      periph2_pre_sels,   ARRAY_SIZE(periph2_pre_sels));
 	clks[IMX6SX_CLK_PERIPH_CLK2_SEL]    = imx_clk_mux("periph_clk2_sel",  base + 0x18,  12,     2,      periph_clk2_sels,  ARRAY_SIZE(periph_clk2_sels));
--- arch/arm/mach-imx/clk.h
+++ arch/arm/mach-imx/clk.h
@@ -102,8 +102,8 @@ static inline struct clk *imx_clk_mux(const char *name, void __iomem *reg,
 		u8 shift, u8 width, const char **parents, int num_parents)
 {
 	return clk_register_mux(NULL, name, parents, num_parents,
-			CLK_SET_RATE_NO_REPARENT, reg, shift,
-			width, 0, &imx_ccm_lock);
+			CLK_SET_RATE_NO_REPARENT | CLK_SET_PARENT_GATE,
+			reg, shift, width, 0, &imx_ccm_lock);
 }
 
 static inline struct clk *imx_clk_mux_flags(const char *name,
@@ -111,7 +111,17 @@ static inline struct clk *imx_clk_mux_flags(const char *name,
 		int num_parents, unsigned long flags)
 {
 	return clk_register_mux(NULL, name, parents, num_parents,
-			flags, reg, shift, width, 0, &imx_ccm_lock);
+			flags | CLK_SET_PARENT_GATE, reg, shift, width, 0,
+			&imx_ccm_lock);
+}
+
+static inline struct clk *imx_clk_mux_glitchless(const char *name,
+		void __iomem *reg, u8 shift, u8 width, const char **parents,
+		int num_parents)
+{
+	return clk_register_mux(NULL, name, parents, num_parents,
+			0, reg, shift,
+			width, 0, &imx_ccm_lock);
 }
 
 static inline struct clk *imx_clk_fixed_factor(const char *name,
