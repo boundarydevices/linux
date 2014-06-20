827f2fe71e6222882930db7e89460087cb3bce5b
Li Jun <b47624@freescale.com>
2014-06-20 15:52:14 +0800

    ENGR00320099 usb: chipidea: add vbus glitch handling
    We add vbus glitch handling for both BSV rise and drop interruptes.
    If it is a vbus glitch (higher than BSV but cannot reach AVV), ignore it.
    
    Signed-off-by: Peter Chen <peter.chen@freescale.com>
    Signed-off-by: Li Jun <b47624@freescale.com>

--- drivers/usb/chipidea/core.c
+++ drivers/usb/chipidea/core.c
@@ -457,7 +457,7 @@ static irqreturn_t ci_irq(int irq, void *data)
 	 * and disconnection events.
 	 */
 	if (ci->is_otg && (otgsc & OTGSC_BSVIE) && (otgsc & OTGSC_BSVIS)) {
-		ci->b_sess_valid_event = true;
+		ci->vbus_glitch_check_event = true;
 		hw_write_otgsc(ci, OTGSC_BSVIS, OTGSC_BSVIS);
 		ci_otg_queue_work(ci);
 		return IRQ_HANDLED;
