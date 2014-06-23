eeeb09c86010e72153fbe15b0b67eefd160a0344
Li Jun <b47624@freescale.com>
2014-06-23 15:39:35 +0800

    ENGR00319720-1 usb: gadget: add host_request_flag in usb_gadget for OTG
    This patch adds host_request_flag in usb_gadget to store host request
    information from application.
    
    Signed-off-by: Li Jun <b47624@freescale.com>

--- include/linux/usb/gadget.h
+++ include/linux/usb/gadget.h
@@ -562,6 +562,7 @@ struct usb_gadget {
 	unsigned			b_hnp_enable:1;
 	unsigned			a_hnp_support:1;
 	unsigned			a_alt_hnp_support:1;
+	unsigned			host_request_flag:1;
 	unsigned			quirk_ep_out_aligned_size:1;
 };
 #define work_to_gadget(w)	(container_of((w), struct usb_gadget, work))
