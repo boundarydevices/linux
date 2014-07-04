463ada98906cee6035426bba377b7f9d0d2f979c
Peter Chen <peter.chen@freescale.com>
2014-07-04 12:39:04 +0800

    ENGR00320792-1 Revert "usb: gadget: remove __init from gadget driver bind function"
    This reverts commit 6d9f0704e3acf4b754188817974247c1963c1493.
    
    Signed-off-by: Peter Chen <peter.chen@freescale.com>

--- drivers/usb/gadget/webcam.c
+++ drivers/usb/gadget/webcam.c
@@ -323,7 +323,7 @@ static const struct uvc_descriptor_header * const uvc_ss_streaming_cls[] = {
  * USB configuration
  */
 
-static int
+static int __init
 webcam_config_bind(struct usb_configuration *c)
 {
 	return uvc_bind_config(c, uvc_fs_control_cls, uvc_ss_control_cls,
@@ -345,7 +345,7 @@ webcam_unbind(struct usb_composite_dev *cdev)
 	return 0;
 }
 
-static int
+static int __init
 webcam_bind(struct usb_composite_dev *cdev)
 {
 	int ret;
