53874b2127c82e729e089938e291d3d65f837107
Eric Nelson <eric.nelson@boundarydevices.com>
2014-07-21 10:37:09 -0700

    sdhci: Allow platform_set_power callback
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -296,6 +296,7 @@ struct sdhci_ops {
 	void	(*platform_resume)(struct sdhci_host *host);
 	void    (*adma_workaround)(struct sdhci_host *host, u32 intmask);
 	void	(*platform_init)(struct sdhci_host *host);
+	void	(*platform_set_power)(struct sdhci_host *host, int on);
 };
 
 #ifdef CONFIG_MMC_SDHCI_IO_ACCESSORS
