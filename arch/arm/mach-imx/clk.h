f45d0b6dcedaf4f1195cee1cf488f6e6606bc5a4
Anson Huang <b20788@freescale.com>
2014-07-25 14:04:32 +0800

    ENGR00324429 ARM: imx: skip system bus related clks parent switch check
    Those system bus related mux only can be switched by busfreq driver,
    and in busfreq asm code, it already follow the flow that hardware design
    require, so no need to do flow check for these clk mux:
    
    i.MX6Q/DL: periph_clk2_sel and pre_periph_clk_sel;
    i.MX6SL: periph_clk2_sel, pre_periph_clk_sel,
    	periph2_clk2_sel and pre_periph2_clk_sel
    i.MX6SX: periph_clk2_sel, pre_periph_clk_sel,
    	periph2_clk2_sel and pre_periph2_clk_sel.
    
    Signed-off-by: Anson Huang <b20788@freescale.com>

--- arch/arm/mach-imx/clk.h
+++ arch/arm/mach-imx/clk.h
@@ -132,6 +132,14 @@ static inline struct clk *imx_clk_mux(const char *name, void __iomem *reg,
 			reg, shift, width, 0, &imx_ccm_lock);
 }
 
+static inline struct clk *imx_clk_mux_bus(const char *name, void __iomem *reg,
+		u8 shift, u8 width, const char **parents, int num_parents)
+{
+	return clk_register_mux(NULL, name, parents, num_parents,
+			CLK_SET_RATE_NO_REPARENT,
+			reg, shift, width, 0, &imx_ccm_lock);
+}
+
 static inline struct clk *imx_clk_mux_flags(const char *name,
 		void __iomem *reg, u8 shift, u8 width, const char **parents,
 		int num_parents, unsigned long flags)
