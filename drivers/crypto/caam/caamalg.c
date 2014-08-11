1d99d631a27ba039d15cbe3b072031005cbcc2bb
Troy Kisky <troy.kisky@boundarydevices.com>
2014-08-11 12:16:08 -0700

    caamalg: retry on dev_get_drvdata failure

--- drivers/crypto/caam/caamalg.c
+++ drivers/crypto/caam/caamalg.c
@@ -2351,6 +2351,14 @@ static int __init caam_algapi_init(void)
 
 	ctrldev = &pdev->dev;
 	priv = dev_get_drvdata(ctrldev);
+	if (!priv) {
+		dev_err(ctrldev, "dev_get_drvdata failed\n");
+		msleep(10);
+		priv = dev_get_drvdata(ctrldev);
+		if (!priv)
+			return -ENODEV;
+		dev_err(ctrldev, "dev_get_drvdata succeeded after pause\n");
+	}
 
 	INIT_LIST_HEAD(&priv->alg_list);
 
