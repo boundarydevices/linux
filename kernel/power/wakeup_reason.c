84f156c693215f1ab2ef2af30ddbe59c546de257
guoyin.chen <guoyin.chen@freescale.com>
2014-08-18 17:54:10 +0800

    ENGR00327503 android.permission.cts.FileSystemPermissionTest#testAllFilesInSysAreNotWritable
    Remove other write permission to pass this CTS case
    
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- kernel/power/wakeup_reason.c
+++ kernel/power/wakeup_reason.c
@@ -53,7 +53,7 @@ static ssize_t reason_show(struct kobject *kobj, struct kobj_attribute *attr,
 	return buf_offset;
 }
 
-static struct kobj_attribute resume_reason = __ATTR(last_resume_reason, 0666,
+static struct kobj_attribute resume_reason = __ATTR(last_resume_reason, 0664,
 		reason_show, NULL);
 
 static struct attribute *attrs[] = {
