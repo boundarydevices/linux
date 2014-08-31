e5b4ab4a3292da335b38c9ef0823f8d76c68ee4a
Eric Nelson <eric.nelson@boundarydevices.com>
2014-08-31 09:43:44 -0700

    arcotg: Allow forced FULLSPEED operation through Kconfig
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- drivers/usb/gadget/arcotg_udc.c
+++ drivers/usb/gadget/arcotg_udc.c
@@ -380,6 +380,12 @@ static int dr_controller_setup(struct fsl_udc *udc)
 	tmp |= USB_CMD_CTRL_RESET;
 	fsl_writel(tmp, &dr_regs->usbcmd);
 
+#ifdef CONFIG_GADGET_ARC_FORCE_FULLSPEED
+	portctrl = readl(&dr_regs->portsc1)
+			|PORTSCX_PORT_FORCE_FULL_SPEED;
+        fsl_writel(portctrl,&dr_regs->portsc1);
+#endif
+
 	/* Wait for reset to complete */
 	timeout = jiffies + FSL_UDC_RESET_TIMEOUT;
 	while (fsl_readl(&dr_regs->usbcmd) & USB_CMD_CTRL_RESET) {
@@ -3125,7 +3131,11 @@ static int __devinit fsl_udc_probe(struct platform_device *pdev)
 
 	/* Setup gadget structure */
 	udc_controller->gadget.ops = &fsl_gadget_ops;
+#ifdef USB_GADGET_DUALSPEED
 	udc_controller->gadget.is_dualspeed = 1;
+#else
+	udc_controller->gadget.is_dualspeed = 0;
+#endif
 	udc_controller->gadget.ep0 = &udc_controller->eps[0].ep;
 	INIT_LIST_HEAD(&udc_controller->gadget.ep_list);
 	udc_controller->gadget.speed = USB_SPEED_UNKNOWN;
