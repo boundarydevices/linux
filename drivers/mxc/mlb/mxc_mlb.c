ab3e171b9fe348f00c4c42e396cb5c753f0b720a
Luwei Zhou <b45643@freescale.com>
2014-09-03 14:24:50 +0800

    ENGR00329774-2 mxc: mlb: Remove the unnecessary code out.
    The regulator is not used by MLB driver. Remove the related code out.
    
    Signed-off-by: Luwei Zhou <b45643@freescale.com>

--- drivers/mxc/mlb/mxc_mlb.c
+++ drivers/mxc/mlb/mxc_mlb.c
@@ -34,7 +34,6 @@
 #include <linux/of_device.h>
 #include <linux/platform_device.h>
 #include <linux/poll.h>
-#include <linux/regulator/consumer.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
 #include <linux/spinlock.h>
@@ -371,9 +370,6 @@ struct mlb_data {
 	struct cdev cdev;
 	struct class *class;	/* device class */
 	dev_t firstdev;
-#ifdef CONFIG_REGULATOR
-	struct regulator *nvcc;
-#endif
 	void __iomem *membase;	/* mlb module base address */
 	struct gen_pool *iram_pool;
 	u32 iram_size;
@@ -2647,19 +2643,6 @@ static int mxc_mlb150_probe(struct platform_device *pdev)
 	}
 	drvdata->membase = mlb_base;
 
-#ifdef CONFIG_REGULATOR
-	drvdata->nvcc = devm_regulator_get(&pdev->dev, "reg_nvcc");
-	if (!IS_ERR(drvdata->nvcc)) {
-		regulator_set_voltage(drvdata->nvcc, 2500000, 2500000);
-		dev_err(&pdev->dev, "enalbe regulator\n");
-		ret = regulator_enable(drvdata->nvcc);
-		if (ret) {
-			dev_err(&pdev->dev, "vdd set voltage error\n");
-			goto err_dev;
-		}
-	}
-#endif
-
 	/* enable clock */
 	drvdata->clk_mlb3p = devm_clk_get(&pdev->dev, "mlb");
 	if (IS_ERR(drvdata->clk_mlb3p)) {
@@ -2714,12 +2697,6 @@ static int mxc_mlb150_remove(struct platform_device *pdev)
 
 	if (pdevinfo && atomic_read(&pdevinfo->opencnt))
 		clk_disable_unprepare(drvdata->clk_mlb3p);
-
-	/* disable mlb power */
-#ifdef CONFIG_REGULATOR
-	if (!IS_ERR(drvdata->nvcc))
-		regulator_disable(drvdata->nvcc);
-#endif
 
 	/* destroy mlb device class */
 	for (i = MLB_MINOR_DEVICES - 1; i >= 0; i--)
