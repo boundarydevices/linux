2c833800b8596a9315977f8f690695da2697529b
Andrew Morton <akpm@linux-foundation.org>
2014-09-05 16:59:02 +0100

    not-adding-modules-range-to-kcore-if-its-equal-to-vmcore-range-checkpatch-fixes
    ERROR: space prohibited after that open parenthesis '('
    #34: FILE: fs/proc/kcore.c:613:
    +	if ( (MODULES_VADDR != VMALLOC_START) &&
    
    ERROR: space prohibited before that close parenthesis ')'
    #35: FILE: fs/proc/kcore.c:614:
    +		(MODULES_END != VMALLOC_END) ) {
    
    total: 2 errors, 0 warnings, 12 lines checked
    
    ./patches/not-adding-modules-range-to-kcore-if-its-equal-to-vmcore-range.patch has style problems, please review.
    
    If any of these errors are false positives, please report
    them to the maintainer, see CHECKPATCH in MAINTAINERS.
    
    Please run checkpatch prior to sending patches
    
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Paul Gortmaker <paul.gortmaker@windriver.com>
    Cc: Xishi Qiu <qiuxishi@huawei.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/proc/kcore.c
+++ fs/proc/kcore.c
@@ -610,8 +610,7 @@ static void __init proc_kcore_text_init(void)
 struct kcore_list kcore_modules;
 static void __init add_modules_range(void)
 {
-	if ( (MODULES_VADDR != VMALLOC_START) &&
-		(MODULES_END != VMALLOC_END) ) {
+	if (MODULES_VADDR != VMALLOC_START && MODULES_END != VMALLOC_END) {
 		kclist_add(&kcore_modules, (void *)MODULES_VADDR,
 			MODULES_END - MODULES_VADDR, KCORE_VMALLOC);
 	}
