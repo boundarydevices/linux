c6fc217b9e86f85d1dcfb68793951e1802a24235
Shengjiu Wang <shengjiu.wang@freescale.com>
2014-09-16 13:43:59 +0800

    ENGR00331635-2: dma: imx-sdma: Add SAI script support
    This patch adds SAI script support to imx-sdma.
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@freescale.com>

--- include/linux/platform_data/dma-imx.h
+++ include/linux/platform_data/dma-imx.h
@@ -42,6 +42,7 @@ enum sdma_peripheral_type {
 	IMX_DMATYPE_SSI_DUAL,	/* SSI Dual FIFO */
 	IMX_DMATYPE_ASRC_SP,	/* Shared ASRC */
 	IMX_DMATYPE_HDMI,	/* HDMI Audio */
+	IMX_DMATYPE_SAI,        /* SAI Audio */
 };
 
 enum imx_dma_prio {
