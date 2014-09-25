523c84faef7e6d7976f660e6a92df86927b44c5a
Steve Cornelius <steve.cornelius@freescale.com>
2014-09-25 16:34:11 -0700

    MLK-9710-18 snvs - make SECVIO module device tree correct
    Converted the prototype 3.0.x SNVS Security Violation Handler
    subsystem to be device tree correct/compliant under 3.10 for ARM
    platforms. Also, separated out SNVS property detection so as to make
    it independent of CAAM, and corrected function namespace accordingly.
    
    Later releases of this subsystem are likely to be separate from the
    kernel's CAAM driver space.
    
    [<vicki.milhoan@freescale.com>: Edited to apply to latest 3.10 kernel]
    Signed-off-by: Steve Cornelius <steve.cornelius@freescale.com>
    Signed-off-by: Victoria Milhoan <vicki.milhoan@freescale.com>
    (cherry picked from commit c8c128086eae012ced0c96d66f21f36bcbd14f66)

--- drivers/crypto/caam/ctrl.c
+++ drivers/crypto/caam/ctrl.c
@@ -401,7 +401,6 @@ static int caam_probe(struct platform_device *pdev)
 	struct device_node *nprop, *np;
 	struct caam_ctrl __iomem *ctrl;
 	struct caam_full __iomem *topregs;
-	struct snvs_full __iomem *snvsregs;
 	struct caam_drv_private *ctrlpriv;
 #ifdef CONFIG_DEBUG_FS
 	struct caam_perfmon *perfmon;
@@ -429,17 +428,6 @@ static int caam_probe(struct platform_device *pdev)
 	/* topregs used to derive pointers to CAAM sub-blocks only */
 	topregs = (struct caam_full __iomem *)ctrl;
 
-	/* Get the IRQ of the controller (for security violations only) */
-	ctrlpriv->secvio_irq = irq_of_parse_and_map(nprop, 0);
-
-	/* Get SNVS register Page */
-	np = of_find_compatible_node(NULL, NULL, "fsl,imx6q-caam-snvs");
-
-	if (!np)
-		return -ENODEV;
-
-	snvsregs = of_iomap(np, 0);
-	ctrlpriv->snvs = snvsregs;
 	/* Get CAAM-SM node and of_iomap() and save */
 	np = of_find_compatible_node(NULL, NULL, "fsl,imx6q-caam-sm");
 
