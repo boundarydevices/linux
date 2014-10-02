17c43ebb309a0235178d2f931e458147ee703823
Troy Kisky <troy.kisky@boundarydevices.com>
2014-10-02 16:48:56 -0700

    fec_main: use mac set by u-boot before fuses

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -1656,7 +1656,18 @@ static void fec_get_mac(struct net_device *ndev)
 	iap = macaddr;
 
 	/*
-	 * 2) from device tree data
+	 * 2) FEC mac registers set by bootloader
+	 */
+	if (!is_valid_ether_addr(iap)) {
+		*((__be32 *) &tmpaddr[0]) =
+			cpu_to_be32(readl(fep->hwp + FEC_ADDR_LOW));
+		*((__be16 *) &tmpaddr[4]) =
+			cpu_to_be16(readl(fep->hwp + FEC_ADDR_HIGH) >> 16);
+		iap = &tmpaddr[0];
+	}
+
+	/*
+	 * 3) from device tree data
 	 */
 	if (!is_valid_ether_addr(iap)) {
 		struct device_node *np = fep->pdev->dev.of_node;
@@ -1668,7 +1679,7 @@ static void fec_get_mac(struct net_device *ndev)
 	}
 
 	/*
-	 * 3) from flash or fuse (via platform data)
+	 * 4) from flash or fuse (via platform data)
 	 */
 	if (!is_valid_ether_addr(iap)) {
 #ifdef CONFIG_M5272
@@ -1678,17 +1689,6 @@ static void fec_get_mac(struct net_device *ndev)
 		if (pdata)
 			iap = (unsigned char *)&pdata->mac;
 #endif
-	}
-
-	/*
-	 * 4) FEC mac registers set by bootloader
-	 */
-	if (!is_valid_ether_addr(iap)) {
-		*((__be32 *) &tmpaddr[0]) =
-			cpu_to_be32(readl(fep->hwp + FEC_ADDR_LOW));
-		*((__be16 *) &tmpaddr[4]) =
-			cpu_to_be16(readl(fep->hwp + FEC_ADDR_HIGH) >> 16);
-		iap = &tmpaddr[0];
 	}
 
 	/*
