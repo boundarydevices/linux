7d5c066e2f632346c07222ce123aa63e75aa9a48
Fugang Duan <b38611@freescale.com>
2014-10-14 16:44:57 +0800

    MLK-9691 net: fec: call .pm_qos_remove_request() in .ndo_stop() callback
    Call .pm_qos_remove_request() in .ndo_stop() callback to avoid kernel
    warning during enet open/close test.
    
    Signed-off-by: Fugang Duan <B38611@freescale.com>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -2776,6 +2776,7 @@ fec_enet_close(struct net_device *ndev)
 	fep->phy_dev = NULL;
 
 	fec_enet_clk_enable(ndev, false);
+	pm_qos_remove_request(&ndev->pm_qos_req);
 	pinctrl_pm_select_sleep_state(&fep->pdev->dev);
 	fec_enet_free_buffers(ndev);
 
