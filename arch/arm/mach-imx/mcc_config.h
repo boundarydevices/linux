826aec64bb91701a0d234cd45e970be39dc395be
Richard Zhu <richard.zhu@freescale.com>
2014-10-20 15:09:11 +0800

    MLK-9707-3 arm: mcc: mcc 2.0 updates common codes changes
    Common codes changes in the mcc 2.0 updates
    - common definitions are moved from mcc_config.h to mcc_common.h
    because that these definitions are common for the standalone mcc
    stack, and shared by different platforms, such as Linux, MQX.
    - re-define the common api _psp_core_num(), and _psp_node_num().
    Let them to be no platform dependency.
    - move the definition of the MCC_OS_USED in mcc_config.h
    - new add on mcc_config_linux.h file, contained the platform
    related macro definitions contained in mcc_config.h before.
    - add the related linux modifications into mcc_api.c/mcc_common.c
    when implement the mcc2.0 into linux BSP.
    - fix one potential bug that all the share memory operations should
    be protected by sema4.
    
    Acked-by: Shawn Guo
    Signed-off-by: Richard Zhu <richard.zhu@freescale.com>
    (cherry picked from commit 541325a16b6db73a86a5a86049145b0060805c7c)

--- arch/arm/mach-imx/mcc_config.h
+++ arch/arm/mach-imx/mcc_config.h
@@ -1,7 +1,6 @@
 /*
  * Copyright (C) 2014 Freescale Semiconductor, Inc.
  * Freescale IMX Linux-specific MCC implementation.
- * The main MCC configuration file
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -19,37 +18,10 @@
 #ifndef __MCC_CONFIG__
 #define __MCC_CONFIG__
 
+#include <linux/mcc_config_linux.h>
+#include <linux/mcc_common.h>
+
 /* used OS */
-#define MCC_OS_USED                    (MCC_MQX)
-
-/* base address of shared memory */
-#define MCC_BASE_ADDRESS               (BSP_SHARED_RAM_START)
-
-/* size (in bytes) and number of receive buffers */
-#define MCC_ATTR_NUM_RECEIVE_BUFFERS   (10)
-#define MCC_ATTR_BUFFER_SIZE_IN_BYTES  (1024)
-
-/* maximum number of receive endpoints (application specific setting),
- * do not assign it to a value greater than 255 ! */
-#define MCC_ATTR_MAX_RECEIVE_ENDPOINTS (5)
-
-/* size of the signal queue */
-#define MCC_MAX_OUTSTANDING_SIGNALS    (10)
-
-/* number of cores */
-#define MCC_NUM_CORES                  (2)
-
-/* core number */
-#define MCC_CORE_NUMBER                (_psp_core_num())
-
-/* semaphore number */
-#define MCC_SHMEM_SEMAPHORE_NUMBER      (1)
-#define MCC_PRINTF_SEMAPHORE_NUMBER     (2)
-#define MCC_I2C_SEMAPHORE_NUMBER        (3)
-#define MCC_RESERVED1_SEMAPHORE_NUMBER  (4)
-#define MCC_RESERVED2_SEMAPHORE_NUMBER  (5)
-
-/* data non-copy mechanisms enabled  */
-#define MCC_SEND_RECV_NOCOPY_API_ENABLED (1)
+#define MCC_OS_USED                    (MCC_LINUX)
 
 #endif /* __MCC_CONFIG__ */
