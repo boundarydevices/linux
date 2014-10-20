c0ebbc50b4c5051b99a5ea812b31c48f77d49e29
Richard Zhu <richard.zhu@freescale.com>
2014-10-20 14:34:34 +0800

    MLK-9707-2 mcc: mcc version 2.0 base common codes
    This is the base line of the mcc version 2.0.
    
    Acked-by: Shawn Guo
    Signed-off-by: Richard Zhu <richard.zhu@freescale.com>
    (cherry picked from commit cfd44c266e3b8a833ac624b86be627efbda6aaa9)

---
+++ arch/arm/mach-imx/mcc_config.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2014 Freescale Semiconductor, Inc.
+ * Freescale IMX Linux-specific MCC implementation.
+ * The main MCC configuration file
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __MCC_CONFIG__
+#define __MCC_CONFIG__
+
+/* used OS */
+#define MCC_OS_USED                    (MCC_MQX)
+
+/* base address of shared memory */
+#define MCC_BASE_ADDRESS               (BSP_SHARED_RAM_START)
+
+/* size (in bytes) and number of receive buffers */
+#define MCC_ATTR_NUM_RECEIVE_BUFFERS   (10)
+#define MCC_ATTR_BUFFER_SIZE_IN_BYTES  (1024)
+
+/* maximum number of receive endpoints (application specific setting),
+ * do not assign it to a value greater than 255 ! */
+#define MCC_ATTR_MAX_RECEIVE_ENDPOINTS (5)
+
+/* size of the signal queue */
+#define MCC_MAX_OUTSTANDING_SIGNALS    (10)
+
+/* number of cores */
+#define MCC_NUM_CORES                  (2)
+
+/* core number */
+#define MCC_CORE_NUMBER                (_psp_core_num())
+
+/* semaphore number */
+#define MCC_SHMEM_SEMAPHORE_NUMBER      (1)
+#define MCC_PRINTF_SEMAPHORE_NUMBER     (2)
+#define MCC_I2C_SEMAPHORE_NUMBER        (3)
+#define MCC_RESERVED1_SEMAPHORE_NUMBER  (4)
+#define MCC_RESERVED2_SEMAPHORE_NUMBER  (5)
+
+/* data non-copy mechanisms enabled  */
+#define MCC_SEND_RECV_NOCOPY_API_ENABLED (1)
+
+#endif /* __MCC_CONFIG__ */
