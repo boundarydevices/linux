3cb77dccfa0112e0db15bae602a3502d913ee1c4
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2014-11-07 13:39:51 +0000

    ASoC: cs47l24: Remove unused ana_tlv
    Change-Id: Ic8d1fb728d926baa619637f669ec97f2b200eaf4
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>

--- sound/soc/codecs/cs47l24.c
+++ sound/soc/codecs/cs47l24.c
@@ -100,7 +100,6 @@ static int cs47l24_virt_dsp_power_ev(struct snd_soc_dapm_widget *w,
 	return 0;
 }
 
-static DECLARE_TLV_DB_SCALE(ana_tlv, 0, 100, 0);
 static DECLARE_TLV_DB_SCALE(eq_tlv, -1200, 100, 0);
 static DECLARE_TLV_DB_SCALE(digital_tlv, -6400, 50, 0);
 static DECLARE_TLV_DB_SCALE(noise_tlv, 0, 600, 0);
