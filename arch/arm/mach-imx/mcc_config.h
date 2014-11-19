c762f1b51c8c666f626e1ceb547806e7e0d0fc85
Richard Zhu <Richard.Zhu@freescale.com>
2014-11-19 15:52:18 +0800

    MLK-9843-01 arm: mcc: set dual copyright of mcc common codes
    set the dual bsd/gpl copyright of the mcc common codes
    implemented in linux bsp release.
    add 2015.
    
    Signed-off-by: Richard Zhu <Richard.Zhu@freescale.com>
    (cherry picked from commit d7f8c4eff50ba78b82484597bd301c4a87dfe082)

--- arch/arm/mach-imx/mcc_config.h
+++ arch/arm/mach-imx/mcc_config.h
@@ -1,18 +1,16 @@
 /*
- * Copyright (C) 2014 Freescale Semiconductor, Inc.
- * Freescale IMX Linux-specific MCC implementation.
+ * This is the main MCC configuration file
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
+ * Copyright (C) 2014-2015 Freescale Semiconductor, Inc. All Rights Reserved.
  *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>.
+ * SPDX-License-Identifier: GPL-2.0+ and/or BSD-3-Clause
+ * The GPL-2.0+ license for this file can be found in the COPYING.GPL file
+ * included with this distribution or at
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ * The BSD-3-Clause License for this file can be found in the COPYING.BSD file
+ * included with this distribution or at
+ * http://opensource.org/licenses/BSD-3-Clause
  */
 
 #ifndef __MCC_CONFIG__
