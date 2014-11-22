67ad9ef9636cf614d0a8fa433ab95aad33e52d64
Troy Kisky <troy.kisky@boundarydevices.com>
2014-11-21 19:14:50 -0700

    mxc_lcdif: add LSA40AT9001 panel

--- drivers/video/mxc/mxc_lcdif.c
+++ drivers/video/mxc/mxc_lcdif.c
@@ -48,7 +48,50 @@ static struct fb_videomode lcdif_modedb[] = {
 	FB_SYNC_CLK_LAT_FALL,
 	FB_VMODE_NONINTERLACED,
 	0,},
-
+	{
+	 /* Okaya 480x272 */
+	 "okaya_480x272", 60, 480, 272, 97786,
+	 .left_margin = 2, .right_margin = 1,
+	 .upper_margin = 3, .lower_margin = 2,
+	 .hsync_len = 41, .vsync_len = 10,
+	 .sync = FB_SYNC_CLK_LAT_FALL,
+	 .vmode = FB_VMODE_NONINTERLACED,
+	 .flag = 0,},
+	{
+		/* 1280x120 @ 60 Hz , pixel clk @ 32MHz */
+		"TRULY-1U",
+		60, /* refresh rate in Hz */
+		1280, /* xres in pixels */
+		120, /* yres in pixels */
+		45000, /* pixel clock in picoseconds (dot clock or just clock) */
+		54, /* left_margin (Horizontal Back Porch) in pixel clock units */
+		54, /* right_margin (Horizontal Front Porch) in pixel clock units */
+		3, /* upper_margin (Vertical Back Porch) in pixel clock units */
+		3, /* lower_margin (Vertical Front Porch) in pixel clock units */
+		3, /* hsync_len (Hsync pulse width) */
+		3, /* vsync_len (Vsync pulse width) */
+		0, /* sync (Polarity on the Data Enable) */
+		FB_VMODE_NONINTERLACED, /* vmode (Video Mode) */
+		0, /* flags */
+	},
+	{
+	/* 800x600 @ 60 Hz , pixel clk @ 40MHz */
+	"LSA40AT9001", 60, 800, 600, 1000000000 / (800+10+46+210) * 1000 / (600+1+23+12) / 60,
+	.left_margin = 46, .right_margin = 210,
+	.upper_margin = 23, .lower_margin = 12,
+	.hsync_len = 10, .vsync_len = 1,
+	.sync = FB_SYNC_CLK_LAT_FALL,
+	.vmode = FB_VMODE_NONINTERLACED,
+	.flag = 0,},
+	{
+	/* 480x800 @ 57 Hz , pixel clk @ 27MHz */
+	"LB043", 57, 480, 800, 25000,
+	.left_margin = 40, .right_margin = 60,
+	.upper_margin = 10, .lower_margin = 10,
+	.hsync_len = 20, .vsync_len = 10,
+	.sync = FB_SYNC_CLK_LAT_FALL,
+	.vmode = FB_VMODE_NONINTERLACED,
+	.flag = 0,},
 	/*
 	 * BT656/BT1120 mode
 	 *
@@ -122,32 +165,6 @@ static struct fb_videomode lcdif_modedb[] = {
 	  FB_SYNC_HOR_HIGH_ACT | FB_SYNC_VERT_HIGH_ACT,
 	  FB_VMODE_INTERLACED,
 	  FB_MODE_IS_DETAILED,},
-	{
-	 /* Okaya 480x272 */
-	 "okaya_480x272", 60, 480, 272, 97786,
-	 .left_margin = 2, .right_margin = 1,
-	 .upper_margin = 3, .lower_margin = 2,
-	 .hsync_len = 41, .vsync_len = 10,
-	 .sync = FB_SYNC_CLK_LAT_FALL,
-	 .vmode = FB_VMODE_NONINTERLACED,
-	 .flag = 0,},
-	{
-		/* 1280x120 @ 60 Hz , pixel clk @ 32MHz */
-		"TRULY-1U",
-		60, /* refresh rate in Hz */
-		1280, /* xres in pixels */
-		120, /* yres in pixels */
-		45000, /* pixel clock in picoseconds (dot clock or just clock) */
-		54, /* left_margin (Horizontal Back Porch) in pixel clock units */
-		54, /* right_margin (Horizontal Front Porch) in pixel clock units */
-		3, /* upper_margin (Vertical Back Porch) in pixel clock units */
-		3, /* lower_margin (Vertical Front Porch) in pixel clock units */
-		3, /* hsync_len (Hsync pulse width) */
-		3, /* vsync_len (Vsync pulse width) */
-		0, /* sync (Polarity on the Data Enable) */
-		FB_VMODE_NONINTERLACED, /* vmode (Video Mode) */
-		0, /* flags */
-	}
 };
 static int lcdif_modedb_sz = ARRAY_SIZE(lcdif_modedb);
 
