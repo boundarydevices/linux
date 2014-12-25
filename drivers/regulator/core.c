df691a62a6917321c9b85b0b6e9e56f1c01ace5d
Richard Zhu <Richard.Zhu@freescale.com>
2014-12-25 14:28:06 +0800

    MLK-10058-2 regulator: consumer: define pre_xxx event macros
    Some gpc operations are mandatory required when
    iMX6SX PCIe PHY is powered on/off.
    use the notify framwork to encapsulate the
    pre-operations in gpc driver
    - add two pre-xxx macros into consumer.h
    - kick off the pre-xxx events in enable/disalbe call back.
    
    Signed-off-by: Richard Zhu <Richard.Zhu@freescale.com>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -1750,6 +1750,7 @@ static int _regulator_do_enable(struct regulator_dev *rdev)
 {
 	int ret, delay;
 
+	_notifier_call_chain(rdev, REGULATOR_EVENT_PRE_ENABLE, NULL);
 	/* Query before enabling in case configuration dependent.  */
 	ret = _regulator_get_enable_time(rdev);
 	if (ret >= 0) {
@@ -1892,6 +1893,7 @@ static int _regulator_do_disable(struct regulator_dev *rdev)
 {
 	int ret;
 
+	_notifier_call_chain(rdev, REGULATOR_EVENT_PRE_DISABLE, NULL);
 	trace_regulator_disable(rdev_get_name(rdev));
 
 	if (rdev->ena_pin) {
