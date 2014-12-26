c1f3faf63b60f863a1cf41c770f9fac48583e415
Bai Ping <b51503@freescale.com>
2014-12-26 21:37:31 +0800

    MLK-10018-01 arm: imx: Implement busfreq notifier calls for busfreq
    Implement busfreq notifier calls used when busfreq entering low_bus_freq_mode.
    When the system lower the bus frequency, some modules can be affected by bus
    frequency change. Adding notifier call chains that allow driver affected by bus
    frequency can be notified before and after low_bus_freq_mode.
    
    Signed-off-by: Bai Ping <b51503@freescale.com>

--- include/linux/busfreq-imx6.h
+++ include/linux/busfreq-imx6.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2013 Freescale Semiconductor, Inc. All Rights Reserved.
+ * Copyright 2012-2015 Freescale Semiconductor, Inc. All Rights Reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -9,9 +9,15 @@
 #ifndef __ASM_ARCH_MXC_BUSFREQ_H__
 #define __ASM_ARCH_MXC_BUSFREQ_H__
 
+#include <linux/notifier.h>
 /*
  * This enumerates busfreq mode.
  */
+enum busfreq_event {
+	LOW_BUSFREQ_ENTER,
+	LOW_BUSFREQ_EXIT,
+};
+
 enum bus_freq_mode {
 	BUS_FREQ_HIGH,
 	BUS_FREQ_MED,
@@ -20,4 +26,6 @@ enum bus_freq_mode {
 };
 void request_bus_freq(enum bus_freq_mode mode);
 void release_bus_freq(enum bus_freq_mode mode);
+int register_busfreq_notifier(struct notifier_block *nb);
+int unregister_busfreq_notifier(struct notifier_block *nb);
 #endif
