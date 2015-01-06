05d96ac5e9917e2ec277323c9cad36f0cfe3be0b
Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>
2015-01-06 11:27:30 +0900

    switch: arizona: transit to button det for direct 4 pole plugin
    when 4 pole jack is plugged in directly without going via an antenna
    cable the state machine can directly transit to button detection state
    instead of transiting to a state which detects antenna and button.
    
    Change-Id: I1ffb9451b52d8096d80fdee4baacc51d17cbdb56
    Signed-off-by: Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>

--- drivers/switch/switch-arizona.c
+++ drivers/switch/switch-arizona.c
@@ -1464,7 +1464,7 @@ static int arizona_antenna_hp_reading(struct arizona_extcon_info *info, int val)
 
 	if (info->mic) {
 		arizona_extcon_report(info, BIT_HEADSET);
-		arizona_jds_set_state(info, &arizona_antenna_button_det);
+		arizona_jds_set_state(info, &arizona_micd_button);
 	} else {
 		arizona_extcon_report(info, BIT_HEADSET_NO_MIC);
 		arizona_jds_set_state(info, NULL);
