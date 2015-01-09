94e3438da4fda71ae8097a69f55080e1b23cec91
Mathias Nyman <mathias.nyman@linux.intel.com>
2015-01-09 16:06:32 +0200

    xhci: Add completion code to the debug ouput of unhandled transfer events
    Helps debugging to know the unhandled event type.
    Also make the debug message grepable
    
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -2374,8 +2374,8 @@ static int handle_tx_event(struct xhci_hcd *xhci,
 			status = 0;
 			break;
 		}
-		xhci_warn(xhci, "ERROR Unknown event condition, HC probably "
-				"busted\n");
+		xhci_warn(xhci, "ERROR Unknown event condition %u, HC probably busted\n",
+			  trb_comp_code);
 		goto cleanup;
 	}
 
