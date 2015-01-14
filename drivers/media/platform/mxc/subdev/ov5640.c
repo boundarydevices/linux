926cf03f322f74123347b05d2abc4fd089ffb3a1
Sandor Yu <R01008@freescale.com>
2015-01-14 15:34:22 +0800

    MLK-10092-1 ov5640: Rename compatible string from ov564x to ov5640
    There are two version ov5640 driver,
    one is written with v4l2 int-device architecture,
    and the other is written with v4l2 subdev architecture.
    Rename subdev ov5640 compatible string from ov5640x to ov5640
    to distinguish with ov5640 int-device driver.
    so ov564x is used for int-device architecture and
    ov5640 is used for subdev architecture.
    
    Signed-off-by: Sandor Yu <R01008@freescale.com>

--- drivers/media/platform/mxc/subdev/ov5640.c
+++ drivers/media/platform/mxc/subdev/ov5640.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2014 Freescale Semiconductor, Inc. All Rights Reserved.
+ * Copyright (C) 2012-2015 Freescale Semiconductor, Inc. All Rights Reserved.
  */
 
 /*
@@ -598,7 +598,6 @@ static s32 ov5640_write_reg(u16 reg, u8 val);
 
 static const struct i2c_device_id ov5640_id[] = {
 	{"ov5640", 0},
-	{"ov564x", 0},
 	{},
 };
 
