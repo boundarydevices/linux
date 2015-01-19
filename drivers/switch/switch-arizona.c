5ae88d900610d5c73c9f258687190a8b1b6f8025
Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>
2015-01-19 10:00:51 +0000

    switch: arizona: call hpdet_cb on antenna jack plugin
    hpdet_cb function is called to convey the headphone impedance
    when only antenna is plugged in we convey a sufficiently large
    number to indicate open circuit
    
    Change-Id: I5c0eef1d485b3a24765dc0b6cceede77fa748a65
    Signed-off-by: Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>

--- drivers/switch/switch-arizona.c
+++ drivers/switch/switch-arizona.c
@@ -1463,6 +1463,9 @@ static int arizona_antenna_mic_reading(struct arizona_extcon_info *info, int val
 		info->antenna_debounce = 0;
 		info->antenna_count = 0;
 		info->mic = false;
+		/* Use a sufficiently large number to indicate open circuit */
+		if (arizona->pdata.hpdet_cb)
+			arizona->pdata.hpdet_cb(ARIZONA_HP_Z_OPEN);
 		arizona_extcon_report(info, BIT_ANTENNA);
 		arizona_jds_set_state(info, &arizona_antenna_oc_det);
 	} else {
