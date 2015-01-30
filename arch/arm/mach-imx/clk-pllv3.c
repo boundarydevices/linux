bed6276a2dbb419ae6e5170772247702e2f9f899
Ranjani Vaidyanathan <Ranjani.Vaidyanathan@freescale.com>
2015-01-30 13:47:35 -0600

    MLK-9961-1 Revert "MLK-10091 arm: imx: check pll1 enable when changing arm_podf"
    This reverts commit 8cc908a0168afb677b5de6405579b681b6f595db.
    
    Signed-off-by: Ranjani Vaidyanathan <Ranjani.Vaidyanathan@freescale.com>

--- arch/arm/mach-imx/clk-pllv3.c
+++ arch/arm/mach-imx/clk-pllv3.c
@@ -26,10 +26,7 @@
 
 #define BM_PLL_POWER		(0x1 << 12)
 #define BM_PLL_LOCK		(0x1 << 31)
-#define BM_PLL_ENABLE		(0x1 << 13)
-#define BM_PLL_BYPASS		(0x1 << 16)
 
-static void __iomem *pll_sys_base;
 /**
  * struct clk_pllv3 - IMX PLL clock version 3
  * @clk_hw:	 clock source
@@ -338,7 +335,6 @@ struct clk *imx_clk_pllv3(enum imx_pllv3_type type, const char *name,
 	switch (type) {
 	case IMX_PLLV3_SYS:
 		ops = &clk_pllv3_sys_ops;
-		pll_sys_base = base;
 		break;
 	case IMX_PLLV3_USB:
 		ops = &clk_pllv3_ops;
@@ -369,19 +365,4 @@ struct clk *imx_clk_pllv3(enum imx_pllv3_type type, const char *name,
 		kfree(pll);
 
 	return clk;
-}
-
-void imx_enable_pll_arm(bool enable)
-{
-	static u32 saved_pll_arm;
-	u32 val;
-
-	if (enable) {
-		saved_pll_arm = val = readl_relaxed(pll_sys_base);
-		val |= BM_PLL_ENABLE;
-		val |= BM_PLL_BYPASS;
-		writel_relaxed(val, pll_sys_base);
-	} else {
-		writel_relaxed(saved_pll_arm, pll_sys_base);
-	}
 }
