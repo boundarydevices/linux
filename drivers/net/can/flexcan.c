5056cf08e40933a8dbb9940e1530126650ace558
Dong Aisheng <b29396@freescale.com>
2015-02-11 15:12:44 +0800

    MLK-10211 can: flexcan: fix resume failed issue
    We can observe the following error during system resume.
    dpm_run_callback(): platform_pm_resume+0x0/0x4c returns -110
    PM: Device 2090000.can failed to resume: error -110
    
    It is caused by the flexcan is in disable state and the clock is disabled
    by default while the CAN is not up. During resume, the calling of
    flexcan_chip_enable will obviously fail due to no clock.
    
    The issue only happened when CAN device is not up. If CAN is up, clock is
    there and no issue will happen.
    
    Because the CAN is always in disabled state by default if it's not up.
    We don't have to disable it again during suspend and enable it after
    resume. This patch simply the suspend/resume to only enter/exit disabled
    mode if CAN is up.
    
    Signed-off-by: Dong Aisheng <b29396@freescale.com>
    (cherry picked from commit d976d7c9d408c1275075d5870ccf6cb425720434)
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>

--- drivers/net/can/flexcan.c
+++ drivers/net/can/flexcan.c
@@ -1563,8 +1563,6 @@ static int __maybe_unused flexcan_suspend(struct device *device)
 		} else {
 			err = flexcan_chip_disable(priv);
 		}
-	} else {
-		err = flexcan_chip_disable(priv);
 	}
 	priv->can.state = CAN_STATE_SLEEPING;
 
@@ -1588,8 +1586,6 @@ static int __maybe_unused flexcan_resume(struct device *device)
 		} else {
 			err = flexcan_chip_enable(priv);
 		}
-	} else {
-		err = flexcan_chip_enable(priv);
 	}
 
 	return err;
