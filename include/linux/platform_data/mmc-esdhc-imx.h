1c3dac9dbc851fff1fd1dd6719dc6a603cbfa979
Haibo Chen <haibo.chen@freescale.com>
2015-02-27 18:35:30 +0800

    MLK-10237-4 mmc: sdhci-esdhc-imx: add tuning-step setting support
    tuning-step is the delay cell steps in tuning procedure. This patch
    add the tuning-step setting in driver, so that user can set the
    tuning-step value in dts.
    
    e.g.
    tuning-step = <2>;
    
    this example set the tuning-step as value 2.
    
    This patch also set the tuning-step of i.MX7D as 2, so that the tuning
    procedure can execute successfully.
    
    Signed-off-by: Haibo Chen <haibo.chen@freescale.com>

--- include/linux/platform_data/mmc-esdhc-imx.h
+++ include/linux/platform_data/mmc-esdhc-imx.h
@@ -46,5 +46,6 @@ struct esdhc_platform_data {
 	unsigned int f_max;
 	bool support_vsel;
 	unsigned int delay_line;
+	unsigned int tuning_step;	/* The delay cell steps in tuning procedure */
 };
 #endif /* __ASM_ARCH_IMX_ESDHC_H */
