1a47d69c0f950ca1e9ce400b7ea3f656a5a224e5
Troy Kisky <troy.kisky@boundarydevices.com>
2015-03-05 20:33:27 -0700

    max77823_fuelgauge: cleanup

--- include/linux/battery/fuelgauge/max77823_fuelgauge.h
+++ include/linux/battery/fuelgauge/max77823_fuelgauge.h
@@ -31,67 +31,52 @@
 
 #include <linux/regulator/machine.h>
 
-#if defined(CONFIG_FUELGAUGE_MAX77823_VOLTAGE_TRACKING)
 #define MAX77823_REG_STATUS		0x00
 #define MAX77823_REG_VALRT_TH		0x01
 #define MAX77823_REG_TALRT_TH		0x02
 #define MAX77823_REG_SALRT_TH		0x03
-#define MAX77823_REG_VCELL			0x09
+#define REMCAP_REP_REG			0x05
+#define SOCREP_REG			0x06
 #define MAX77823_REG_TEMPERATURE	0x08
+#define MAX77823_REG_VCELL		0x09
+#define CURRENT_REG			0x0A
+#define AVG_CURRENT_REG			0x0B
+#define REMCAP_MIX_REG			0x0F
+#define CYCLES_REG			0x17
+#define REMCAP_AV_REG			0x1F
 #define MAX77823_REG_AVGVCELL		0x19
 #define MAX77823_REG_CONFIG		0x1D
 #define MAX77823_REG_VERSION		0x21
 #define MAX77823_REG_LEARNCFG		0x28
-#define MAX77823_REG_FILTERCFG	0x29
+#define MAX77823_REG_FILTERCFG		0x29
 #define MAX77823_REG_MISCCFG		0x2B
 #define MAX77823_REG_CGAIN		0x2E
+#define FULLCAPREP_REG			0x35
 #define MAX77823_REG_RCOMP		0x38
 #define MAX77823_REG_VFOCV		0xFB
 #define MAX77823_REG_SOC_VF		0xFF
-#endif
+
+#define PRINT_COUNT	10
 
 #if defined(CONFIG_FUELGAUGE_MAX77823_COULOMB_COUNTING) || \
 	defined(CONFIG_FUELGAUGE_MAX77843)
-#define PRINT_COUNT	10
 
-#define STATUS_REG				0x00
-#define VALRT_THRESHOLD_REG	0x01
-#define TALRT_THRESHOLD_REG	0x02
-#define SALRT_THRESHOLD_REG	0x03
-#define REMCAP_REP_REG			0x05
-#define SOCREP_REG				0x06
-#define TEMPERATURE_REG		0x08
-#define VCELL_REG				0x09
-#define CURRENT_REG				0x0A
-#define AVG_CURRENT_REG		0x0B
-#define SOCMIX_REG				0x0D
-#define SOCAV_REG				0x0E
-#define REMCAP_MIX_REG			0x0F
-#define FULLCAP_REG				0x10
+#define SOCMIX_REG			0x0D
+#define SOCAV_REG			0x0E
+#define FULLCAP_REG			0x10
 #define TIME_TO_EMPTY_REG		0x11
-#define FULLCAPREP_REG			0x35
-#define RFAST_REG				0x15
-#define AVR_TEMPERATURE_REG	0x16
-#define CYCLES_REG				0x17
+#define RFAST_REG			0x15
+#define AVR_TEMPERATURE_REG		0x16
 #define DESIGNCAP_REG			0x18
-#define AVR_VCELL_REG			0x19
 #define TIME_TO_FULL_REG		0x20
-#define CONFIG_REG				0x1D
-#define REMCAP_AV_REG			0x1F
-#define FULLCAP_NOM_REG		0x23
-#define MISCCFG_REG				0x2B
+#define FULLCAP_NOM_REG			0x23
 #define QRTABLE20_REG			0x32
-#define RCOMP_REG				0x38
-#define FSTAT_REG				0x3D
+#define FSTAT_REG			0x3D
 #define QRTABLE30_REG			0x42
-#define DQACC_REG				0x45
-#define DPACC_REG				0x46
-#define OCV_REG					0xEE
-#define VFOCV_REG				0xFB
-#define VFSOC_REG				0xFF
+#define DQACC_REG			0x45
+#define DPACC_REG			0x46
+#define OCV_REG				0xEE
 
-#define LOW_BATT_COMP_RANGE_NUM	5
-#define LOW_BATT_COMP_LEVEL_NUM	2
 #define MAX_LOW_BATT_CHECK_CNT	10
 
 enum {
@@ -115,6 +100,10 @@ enum {
 	NEGATIVE,
 };
 
+#endif
+
+#define LOW_BATT_COMP_RANGE_NUM	5
+#define LOW_BATT_COMP_LEVEL_NUM	2
 enum {
 	RANGE = 0,
 	SLOPE,
@@ -131,7 +120,7 @@ struct max77823_fuelgauge_battery_data_t {
 	u16 QResidual20;
 	u16 QResidual30;
 	u16 Capacity;
-	u16 low_battery_comp_voltage;
+	u32 low_battery_comp_voltage;
 	s32 low_battery_table[CURRENT_RANGE_MAX_NUM][TABLE_MAX];
 	s32 temp_adjust_table[TEMP_RANGE_MAX_NUM][TABLE_MAX];
 	u8	*type_str;
@@ -159,6 +148,8 @@ struct max77823_fuelgauge_info {
 	int full_check_flag;
 	bool is_first_check;
 };
+#if defined(CONFIG_FUELGAUGE_MAX77823_COULOMB_COUNTING) || \
+	defined(CONFIG_FUELGAUGE_MAX77843)
 
 /* FullCap learning setting */
 #define VFFULLCAP_CHECK_INTERVAL	300 /* sec */
@@ -173,13 +164,15 @@ struct max77823_fuelgauge_info {
 /* soc should be 0.1% unit */
 #define POWER_OFF_SOC_HIGH_MARGIN	20
 #define POWER_OFF_VOLTAGE_HIGH_MARGIN	3500
-#define POWER_OFF_VOLTAGE_LOW_MARGIN	3400
 
 /* FG recovery handler */
 /* soc should be 0.1% unit */
 #define STABLE_LOW_BATTERY_DIFF	30
 #define STABLE_LOW_BATTERY_DIFF_LOWBATT	10
 #define LOW_BATTERY_SOC_REDUCE_UNIT	10
+#endif
+
+#define POWER_OFF_VOLTAGE_LOW_MARGIN	3400
 
 struct max77823_fuelgauge_data {
 	struct device           *dev;
@@ -218,5 +211,4 @@ struct max77823_fuelgauge_data {
 
 	int fg_irq;
 };
-#endif
 #endif /* __MAX77823_FUELGAUGE_H */
