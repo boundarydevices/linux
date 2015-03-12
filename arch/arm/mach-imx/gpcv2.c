7bb478f29e820cecfc151834202453ea67f182e7
Anson Huang <b20788@freescale.com>
2015-03-12 16:32:44 +0800

    MLK-10415-4 ARM: imx: enable mega/fast mix off for imx7d
    When Mega/Fast mix is off in DSM, we need to do DDR retention,
    by default, Mega/Fast mix will be off in DSM, unless there
    is any device inside Mega/Fast mix domain enabled as wakeup source.
    
    Signed-off-by: Anson Huang <b20788@freescale.com>

--- arch/arm/mach-imx/gpcv2.c
+++ arch/arm/mach-imx/gpcv2.c
@@ -29,10 +29,11 @@
 #define GPC_LPCR_M4		0x8
 #define GPC_SLPCR		0x14
 #define GPC_PGC_ACK_SEL_A7	0x24
-#define GPC_MISC_OFFSET		0x2c
+#define GPC_MISC		0x2c
 #define GPC_IMR1_CORE0		0x30
 #define GPC_IMR1_CORE1		0x40
 #define GPC_SLOT0_CFG		0xb0
+#define GPC_PGC_CPU_MAPPING	0xec
 #define GPC_CPU_PGC_SW_PUP_REQ	0xf0
 #define GPC_CPU_PGC_SW_PDN_REQ	0xfc
 #define GPC_GTOR		0x124
@@ -46,6 +47,7 @@
 #define GPC_PGC_USB_OTG2_PHY	0xcc0
 #define GPC_PGC_USB_HSIC_PHY	0xd00
 
+#define BM_LPCR_A7_BSC_IRQ_SRC_A7_WAKEUP	0x70000000
 #define BM_LPCR_A7_BSC_CPU_CLK_ON_LPM		0x4000
 #define BM_LPCR_A7_BSC_LPM1			0xc
 #define BM_LPCR_A7_BSC_LPM0			0x3
@@ -53,17 +55,31 @@
 #define BP_LPCR_A7_BSC_LPM0			0
 #define BM_LPCR_M4_MASK_DSM_TRIGGER		0x80000000
 #define BM_SLPCR_EN_DSM				0x80000000
+#define BM_SLPCR_RBC_EN				0x40000000
 #define BM_SLPCR_VSTBY				0x4
 #define BM_SLPCR_SBYOS				0x2
 #define BM_SLPCR_BYPASS_PMIC_READY		0x1
 
 #define BM_LPCR_A7_AD_EN_C1_PUP			0x800
+#define BM_LPCR_A7_AD_EN_C1_IRQ_PUP		0x400
 #define BM_LPCR_A7_AD_EN_C0_PUP			0x200
+#define BM_LPCR_A7_AD_EN_C0_IRQ_PUP		0x100
 #define BM_LPCR_A7_AD_EN_PLAT_PDN		0x10
 #define BM_LPCR_A7_AD_EN_C1_PDN			0x8
+#define BM_LPCR_A7_AD_EN_C1_WFI_PDN		0x4
 #define BM_LPCR_A7_AD_EN_C0_PDN			0x2
+#define BM_LPCR_A7_AD_EN_C0_WFI_PDN		0x1
 
 #define BM_CPU_PGC_SW_PDN_PUP_REQ_CORE1_A7	0x2
+
+#define BM_GPC_PGC_ACK_SEL_A7_DUMMY_PUP_ACK	0x80000000
+#define BM_GPC_PGC_ACK_SEL_A7_DUMMY_PDN_ACK	0x8000
+
+#define BP_LPCR_A7_BSC_IRQ_SRC			28
+
+#define MAX_SLOT_NUMBER				10
+#define A7_LPM_WAIT				0x5
+#define A7_LPM_STOP				0xa
 
 enum imx_gpc_slot {
 	CORE0_A7,
@@ -81,6 +97,7 @@ enum imx_gpc_slot {
 static void __iomem *gpc_base;
 static u32 gpcv2_wake_irqs[IMR_NUM];
 static u32 gpcv2_saved_imrs[IMR_NUM];
+static u32 gpcv2_mf_irqs[IMR_NUM];
 static DEFINE_SPINLOCK(gpcv2_lock);
 
 void imx_gpcv2_set_slot_ack(u32 index, enum imx_gpc_slot m_core,
@@ -88,7 +105,7 @@ void imx_gpcv2_set_slot_ack(u32 index, enum imx_gpc_slot m_core,
 {
 	u32 val;
 
-	if (index > 9)
+	if (index >= MAX_SLOT_NUMBER)
 		pr_err("Invalid slot index!\n");
 	/* set slot */
 	writel_relaxed((mode + 1) << (m_core * 2), gpc_base +
@@ -106,28 +123,34 @@ void imx_gpcv2_set_slot_ack(u32 index, enum imx_gpc_slot m_core,
 
 void imx_gpcv2_set_lpm_mode(u32 cpu, enum mxc_cpu_pwr_mode mode)
 {
-	u32 val1 = readl_relaxed(gpc_base + GPC_LPCR_A7_BSC);
-	u32 val2 = readl_relaxed(gpc_base + GPC_SLPCR);
+	unsigned long flags;
+	u32 val1, val2;
 
-	if (cpu == 0)
-		val1 &= ~BM_LPCR_A7_BSC_LPM0;
-	if (cpu == 1)
-		val1 &= ~BM_LPCR_A7_BSC_LPM1;
+	spin_lock_irqsave(&gpcv2_lock, flags);
 
-	val2 &= ~(BM_SLPCR_EN_DSM | BM_SLPCR_VSTBY |
+	val1 = readl_relaxed(gpc_base + GPC_LPCR_A7_BSC);
+	val2 = readl_relaxed(gpc_base + GPC_SLPCR);
+
+	/* core 0/1's LPM settings must be same */
+	val1 &= ~(BM_LPCR_A7_BSC_LPM0 | BM_LPCR_A7_BSC_LPM1);
+
+	val1 |= BM_LPCR_A7_BSC_CPU_CLK_ON_LPM;
+
+	val2 &= ~(BM_SLPCR_EN_DSM | BM_SLPCR_VSTBY | BM_SLPCR_RBC_EN |
 		BM_SLPCR_SBYOS | BM_SLPCR_BYPASS_PMIC_READY);
 
 	switch (mode) {
 	case WAIT_CLOCKED:
 		break;
 	case WAIT_UNCLOCKED:
-		val1 |= 0x1 << (BP_LPCR_A7_BSC_LPM0 + cpu * 2);
+		val1 |= A7_LPM_WAIT << BP_LPCR_A7_BSC_LPM0;
 		val1 &= ~BM_LPCR_A7_BSC_CPU_CLK_ON_LPM;
 		break;
 	case STOP_POWER_OFF:
-		val1 |= 0x2 << (BP_LPCR_A7_BSC_LPM0 + cpu * 2);
+		val1 |= A7_LPM_STOP << BP_LPCR_A7_BSC_LPM0;
 		val1 &= ~BM_LPCR_A7_BSC_CPU_CLK_ON_LPM;
 		val2 |= BM_SLPCR_EN_DSM;
+		val2 |= BM_SLPCR_RBC_EN;
 		val2 |= BM_SLPCR_SBYOS;
 		val2 |= BM_SLPCR_VSTBY;
 		val2 |= BM_SLPCR_BYPASS_PMIC_READY;
@@ -138,13 +161,16 @@ void imx_gpcv2_set_lpm_mode(u32 cpu, enum mxc_cpu_pwr_mode mode)
 
 	writel_relaxed(val1, gpc_base + GPC_LPCR_A7_BSC);
 	writel_relaxed(val2, gpc_base + GPC_SLPCR);
+	spin_unlock_irqrestore(&gpcv2_lock, flags);
 }
 
-void imx_gpcv2_set_plat_power_gate_by_lpm(void)
+void imx_gpcv2_set_plat_power_gate_by_lpm(bool pdn)
 {
 	u32 val = readl_relaxed(gpc_base + GPC_LPCR_A7_AD);
 
-	val |= BM_LPCR_A7_AD_EN_PLAT_PDN;
+	val &= ~BM_LPCR_A7_AD_EN_PLAT_PDN;
+	if (pdn)
+		val |= BM_LPCR_A7_AD_EN_PLAT_PDN;
 
 	writel_relaxed(val, gpc_base + GPC_LPCR_A7_AD);
 }
@@ -171,45 +197,135 @@ void imx_gpcv2_set_core1_pdn_pup_by_software(bool pdn)
 	imx_gpcv2_set_m_core_pgc(false, GPC_PGC_C1);
 }
 
-void imx_gpcv2_set_cpu_power_gate_by_lpm(u32 cpu)
+void imx_gpcv2_set_cpu_power_gate_by_wfi(u32 cpu, bool pdn)
 {
+	unsigned long flags;
+	u32 val;
+
+	spin_lock_irqsave(&gpcv2_lock, flags);
+	val = readl_relaxed(gpc_base + GPC_LPCR_A7_AD);
+
+	if (cpu == 0) {
+		if (pdn) {
+			imx_gpcv2_set_m_core_pgc(true, GPC_PGC_C0);
+			val |= BM_LPCR_A7_AD_EN_C0_WFI_PDN |
+				BM_LPCR_A7_AD_EN_C0_IRQ_PUP;
+		} else {
+			imx_gpcv2_set_m_core_pgc(false, GPC_PGC_C0);
+			val &= ~(BM_LPCR_A7_AD_EN_C0_WFI_PDN |
+				BM_LPCR_A7_AD_EN_C0_IRQ_PUP);
+		}
+	}
+	if (cpu == 1) {
+		if (pdn) {
+			imx_gpcv2_set_m_core_pgc(true, GPC_PGC_C1);
+			val |= BM_LPCR_A7_AD_EN_C1_WFI_PDN |
+				BM_LPCR_A7_AD_EN_C1_IRQ_PUP;
+		} else {
+			imx_gpcv2_set_m_core_pgc(false, GPC_PGC_C1);
+			val &= ~(BM_LPCR_A7_AD_EN_C1_WFI_PDN |
+				BM_LPCR_A7_AD_EN_C1_IRQ_PUP);
+		}
+	}
+	writel_relaxed(val, gpc_base + GPC_LPCR_A7_AD);
+	spin_unlock_irqrestore(&gpcv2_lock, flags);
+}
+
+void imx_gpcv2_set_cpu_power_gate_by_lpm(u32 cpu, bool pdn)
+{
+	unsigned long flags;
 	u32 val = readl_relaxed(gpc_base + GPC_LPCR_A7_AD);
 
-	if (cpu == 0)
-		val |= BM_LPCR_A7_AD_EN_C0_PDN | BM_LPCR_A7_AD_EN_C0_PUP;
-	if (cpu == 1)
-		val |= BM_LPCR_A7_AD_EN_C1_PDN | BM_LPCR_A7_AD_EN_C1_PUP;
+	spin_lock_irqsave(&gpcv2_lock, flags);
+	if (cpu == 0) {
+		if (pdn)
+			val |= BM_LPCR_A7_AD_EN_C0_PDN |
+				BM_LPCR_A7_AD_EN_C0_PUP;
+		else
+			val &= ~(BM_LPCR_A7_AD_EN_C0_PDN |
+				BM_LPCR_A7_AD_EN_C0_PUP);
+	}
+	if (cpu == 1) {
+		if (pdn)
+			val |= BM_LPCR_A7_AD_EN_C1_PDN |
+				BM_LPCR_A7_AD_EN_C1_PUP;
+		else
+			val &= ~(BM_LPCR_A7_AD_EN_C1_PDN |
+				BM_LPCR_A7_AD_EN_C1_PUP);
+	}
 
 	writel_relaxed(val, gpc_base + GPC_LPCR_A7_AD);
+	spin_unlock_irqrestore(&gpcv2_lock, flags);
+}
+
+void imx_gpcv2_set_mix_phy_gate_by_lpm(u32 pdn_index, u32 pup_index)
+{
+	/* set power down slot */
+	writel_relaxed(1 << (FAST_MEGA_MIX * 2),
+		gpc_base + GPC_SLOT0_CFG + pdn_index * 4);
+
+	/* set power up slot */
+	writel_relaxed(1 << (FAST_MEGA_MIX * 2 + 1),
+		gpc_base + GPC_SLOT0_CFG + pup_index * 4);
+}
+
+unsigned int imx_gpcv2_is_mf_mix_off(void)
+{
+	return readl_relaxed(gpc_base + GPC_PGC_FM);
+}
+
+static void imx_gpcv2_mf_mix_off(void)
+{
+	int i;
+
+	for (i = 0; i < IMR_NUM; i++)
+		if ((gpcv2_wake_irqs[i] & gpcv2_mf_irqs[i]) != 0)
+			return;
+
+	pr_info("Turn off Mega/Fast mix in DSM\n");
+	imx_gpcv2_set_mix_phy_gate_by_lpm(1, 5);
+	imx_gpcv2_set_m_core_pgc(true, GPC_PGC_FM);
 }
 
 void imx_gpcv2_pre_suspend(bool arm_power_off)
 {
 	void __iomem *reg_imr1 = gpc_base + GPC_IMR1_CORE0;
-	int i;
+	int i, cpu = 0;
 
 	if (arm_power_off) {
-		/* both core0 and core1's lpm need to be set */
-		imx_gpcv2_set_lpm_mode(0, STOP_POWER_OFF);
-		imx_gpcv2_set_lpm_mode(1, STOP_POWER_OFF);
+		/* all CPU's lpm need to be set */
+		for_each_possible_cpu(cpu)
+			imx_gpcv2_set_lpm_mode(cpu, STOP_POWER_OFF);
 		/* enable core0 power down/up with low power mode */
-		imx_gpcv2_set_cpu_power_gate_by_lpm(0);
+		imx_gpcv2_set_cpu_power_gate_by_lpm(0, true);
 		/* enable plat power down with low power mode */
-		imx_gpcv2_set_plat_power_gate_by_lpm();
+		imx_gpcv2_set_plat_power_gate_by_lpm(true);
 
-		/* enable cpu0 power down in slot0 */
+		/*
+		 * To avoid confuse, we use slot 0~4 for power down,
+		 * slot 5~9 for power up.
+		 *
+		 * Power down slot sequence:
+		 * Slot0 -> CORE0
+		 * Slot1 -> Mega/Fast MIX
+		 * Slot2 -> SCU
+		 *
+		 * Power up slot sequence:
+		 * Slot5 -> Mega/Fast MIX
+		 * Slot6 -> SCU
+		 * Slot7 -> CORE0
+		 */
 		imx_gpcv2_set_slot_ack(0, CORE0_A7, false, false);
-		/* enable scu power down in slot1, ack for last one */
-		imx_gpcv2_set_slot_ack(1, SCU_A7, false, true);
+		imx_gpcv2_set_slot_ack(2, SCU_A7, false, true);
 
-		/* enable scu power up in slot5 */
-		imx_gpcv2_set_slot_ack(5, SCU_A7, true, false);
-		/* enable core0 power up in slot5, ack for last one */
-		imx_gpcv2_set_slot_ack(6, CORE0_A7, true, true);
+		imx_gpcv2_mf_mix_off();
+
+		imx_gpcv2_set_slot_ack(6, SCU_A7, true, false);
+		imx_gpcv2_set_slot_ack(7, CORE0_A7, true, true);
 
 		/* enable core0, scu */
-		writel_relaxed(0x1, gpc_base + GPC_PGC_C0);
-		writel_relaxed(0x1, gpc_base + GPC_PGC_SCU);
+		imx_gpcv2_set_m_core_pgc(true, GPC_PGC_C0);
+		imx_gpcv2_set_m_core_pgc(true, GPC_PGC_SCU);
 	}
 
 	for (i = 0; i < IMR_NUM; i++) {
@@ -221,14 +337,25 @@ void imx_gpcv2_pre_suspend(bool arm_power_off)
 void imx_gpcv2_post_resume(void)
 {
 	void __iomem *reg_imr1 = gpc_base + GPC_IMR1_CORE0;
-	int i;
+	int i, cpu = 0;
 
 	for (i = 0; i < IMR_NUM; i++)
 		writel_relaxed(gpcv2_saved_imrs[i], reg_imr1 + i * 4);
 
-	/* both core0 and core1's lpm need to be set */
-	imx_gpcv2_set_lpm_mode(0, WAIT_CLOCKED);
-	imx_gpcv2_set_lpm_mode(1, WAIT_CLOCKED);
+	/* all cpu's lpm need to be set */
+	for_each_possible_cpu(cpu)
+		imx_gpcv2_set_lpm_mode(cpu, WAIT_CLOCKED);
+	imx_gpcv2_set_cpu_power_gate_by_lpm(0, false);
+	imx_gpcv2_set_plat_power_gate_by_lpm(false);
+
+	imx_gpcv2_set_m_core_pgc(false, GPC_PGC_C0);
+	imx_gpcv2_set_m_core_pgc(false, GPC_PGC_SCU);
+	imx_gpcv2_set_m_core_pgc(false, GPC_PGC_FM);
+	for (i = 0; i < MAX_SLOT_NUMBER; i++)
+		writel_relaxed(0x0, gpc_base + GPC_SLOT0_CFG + i * 0x4);
+	writel_relaxed(BM_GPC_PGC_ACK_SEL_A7_DUMMY_PUP_ACK |
+		BM_GPC_PGC_ACK_SEL_A7_DUMMY_PDN_ACK,
+		gpc_base + GPC_PGC_ACK_SEL_A7);
 }
 
 static int imx_gpcv2_irq_set_wake(struct irq_data *d, unsigned int on)
@@ -315,10 +442,31 @@ void __init imx_gpcv2_init(void)
 		writel_relaxed(~0, gpc_base + GPC_IMR1_CORE1 + i * 4);
 	}
 
-	writel_relaxed(0x0, gpc_base + GPC_LPCR_A7_BSC);
+	/* Read supported wakeup source in M/F domain */
+	if (cpu_is_imx7d()) {
+		of_property_read_u32_index(np, "fsl,mf-mix-wakeup-irq", 0,
+			&gpcv2_mf_irqs[0]);
+		of_property_read_u32_index(np, "fsl,mf-mix-wakeup-irq", 1,
+			&gpcv2_mf_irqs[1]);
+		of_property_read_u32_index(np, "fsl,mf-mix-wakeup-irq", 2,
+			&gpcv2_mf_irqs[2]);
+		of_property_read_u32_index(np, "fsl,mf-mix-wakeup-irq", 3,
+			&gpcv2_mf_irqs[3]);
+		if (!(gpcv2_mf_irqs[0] | gpcv2_mf_irqs[1] |
+			gpcv2_mf_irqs[2] | gpcv2_mf_irqs[3]))
+			pr_info("No wakeup source in Mega/Fast domain found!\n");
+	}
+
+	/* only external IRQs to wake up LPM and core 0/1 */
+	writel_relaxed(0x2 << BP_LPCR_A7_BSC_IRQ_SRC,
+		gpc_base + GPC_LPCR_A7_BSC);
 	/* mask m4 dsm trigger */
 	writel_relaxed(readl_relaxed(gpc_base + GPC_LPCR_M4) |
 		BM_LPCR_M4_MASK_DSM_TRIGGER, gpc_base + GPC_LPCR_M4);
+	/* set mega/fast mix in A7 domain */
+	writel_relaxed(0x1, gpc_base + GPC_PGC_CPU_MAPPING);
+	/* init A7 basic setting register */
+	writel_relaxed(0x0, gpc_base + GPC_LPCR_A7_BSC);
 
 	/* Register GPC as the secondary interrupt controller behind GIC */
 	gic_arch_extn.irq_mask = imx_gpcv2_irq_mask;
