bce271ceb8a999f9ef642e5abcbab73c58400498
zhang sanshan <b51434@freescale.com>
2015-03-15 18:41:09 +0800

    MA-6386 [Android_MX6Q/DL/SX_SD/ARD]Fastboot: 'reboot fastboot' doesn't work in console. 100%
    If we use a invalid cmd to reboot system,such as "reboot 112233"
    The console will show:"Restarting system with command '112233'."
    Actually it only execute command "reboot"
    
    Signed-off-by: zhang sanshan <b51434@freescale.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -403,10 +403,7 @@ void kernel_restart(char *cmd)
 	kernel_restart_prepare(cmd);
 	migrate_to_reboot_cpu();
 	syscore_shutdown();
-	if (!cmd)
-		printk(KERN_EMERG "Restarting system.\n");
-	else
-		printk(KERN_EMERG "Restarting system with command '%s'.\n", cmd);
+	printk(KERN_EMERG "Restarting system.\n");
 	kmsg_dump(KMSG_DUMP_RESTART);
 	machine_restart(cmd);
 }
