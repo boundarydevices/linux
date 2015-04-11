465e411b9564268e5cabea37a4c22040ea9e67db
Troy Kisky <troy.kisky@boundarydevices.com>
2015-04-10 17:11:56 -0700

    max77823_fuelgauge: remove fg_read_avsoc

--- drivers/battery/max77823_fuelgauge.c
+++ drivers/battery/max77823_fuelgauge.c
@@ -507,14 +507,6 @@ static int fg_read_vfsoc(struct max77823_fuelgauge_data *fuelgauge)
 	return fg_read_percent(fuelgauge, MAX77823_REG_SOC_VF);
 }
 
-#ifdef CONFIG_FUELGAUGE_MAX77823_COULOMB_COUNTING
-/* soc should be 0.1% unit */
-static int fg_read_avsoc(struct max77823_fuelgauge_data *fuelgauge)
-{
-	return fg_read_percent(fuelgauge, SOCAV_REG);
-}
-#endif
-
 /* soc should be 0.1% unit */
 static int fg_read_soc(struct max77823_fuelgauge_data *fuelgauge)
 {
@@ -1203,7 +1195,7 @@ static bool fuelgauge_recovery_handler(struct max77823_fuelgauge_data *fuelgauge
 			fuelgauge->info.is_low_batt_alarm = false;
 		} else {
 			temperature = fg_read_temp(fuelgauge);
-			avsoc = fg_read_avsoc(fuelgauge);
+			avsoc = fg_read_percent(fuelgauge, SOCAV_REG);
 
 			if ((fuelgauge->info.soc > avsoc) ||
 				(temperature < 0)) {
