5a72ac30cc16461c94c54052e34003cf4879472f
Troy Kisky <troy.kisky@boundarydevices.com>
2015-05-07 13:15:54 -0700

    serial: imx: use subsys_initcall

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -2442,7 +2442,7 @@ static void __exit imx_serial_exit(void)
 	uart_unregister_driver(&imx_reg);
 }
 
-module_init(imx_serial_init);
+subsys_initcall(imx_serial_init);
 module_exit(imx_serial_exit);
 
 MODULE_AUTHOR("Sascha Hauer");
