fb3e34ada5df9cf91f6fe2abed928207e07ac84a
Troy Kisky <troy.kisky@boundarydevices.com>
2015-05-20 12:41:18 -0700

    pci-imx6: fix pci hang issue with imx6sx when u-boot doesn't enable pci

--- drivers/pci/host/pci-imx6.c
+++ drivers/pci/host/pci-imx6.c
@@ -422,6 +422,12 @@ static int imx_pcie_abort_handler(unsigned long addr,
 }
 #endif
 
+#define GPC_CNTR 0
+#define reg32_write(addr, val) __raw_writel(val, addr)
+#define reg32_read(addr)       __raw_readl(addr)
+#define reg32setbit(addr, bitpos) \
+        reg32_write((addr), (reg32_read((addr)) | (1<<(bitpos))))
+
 static void imx_pcie_assert_core_reset(struct imx_pcie *imx_pcie)
 {
 	struct pcie_port *pp = &imx_pcie->pp;
@@ -430,6 +436,10 @@ static void imx_pcie_assert_core_reset(struct imx_pcie *imx_pcie)
 
 	switch (imx_pcie->variant) {
 	case IMX6SX:
+	{
+		struct device_node *gpc_np;
+		void __iomem *gpc_reg;
+
 		regmap_update_bits(imx_pcie->iomuxc_gpr, IOMUXC_GPR12,
 				   IMX6SX_GPR12_PCIE_TEST_POWERDOWN,
 				   IMX6SX_GPR12_PCIE_TEST_POWERDOWN);
@@ -437,7 +447,16 @@ static void imx_pcie_assert_core_reset(struct imx_pcie *imx_pcie)
 		regmap_update_bits(imx_pcie->iomuxc_gpr, IOMUXC_GPR5,
 				   IMX6SX_GPR5_PCIE_BTNRST_RESET,
 				   IMX6SX_GPR5_PCIE_BTNRST_RESET);
+		/* map GPC register  */
+		gpc_np = of_find_compatible_node(NULL, NULL, "fsl,imx6q-gpc");
+		gpc_reg = of_iomap(gpc_np, 0);
+		if (gpc_reg)
+			reg32setbit(gpc_reg + GPC_CNTR, 7);
+		else
+			dev_err(pp->dev, "ioremap failed with gpc base\n");
+		iounmap(gpc_reg);
 		break;
+	}
 	case IMX6QP:
 		regmap_update_bits(imx_pcie->iomuxc_gpr, IOMUXC_GPR1,
 				   IMX6Q_GPR1_PCIE_SW_RST,
