9168a980005e3370207079aff5ae4fdca89be142
Li Jun <jun.li@freescale.com>
2015-06-17 16:34:25 +0800

    MLK-11123-7 usb: common: add API to set otg features by device tree
    Check property of usb hardware to get otg version and if SRP, HNP and ADP
    are disabled.
    
    Signed-off-by: Li Jun <jun.li@freescale.com>

--- include/linux/usb/of.h
+++ include/linux/usb/of.h
@@ -16,6 +16,8 @@ enum usb_dr_mode of_usb_get_dr_mode(struct device_node *np);
 enum usb_device_speed of_usb_get_maximum_speed(struct device_node *np);
 bool of_usb_host_tpl_support(struct device_node *np);
 bool of_usb_otg_adp_support(struct device_node *np);
+void of_usb_set_otg_caps(struct device_node *np,
+			struct usb_otg_caps *otg_caps);
 #else
 static inline enum usb_dr_mode of_usb_get_dr_mode(struct device_node *np)
 {
@@ -34,6 +36,11 @@ static inline bool of_usb_host_tpl_support(struct device_node *np)
 static inline bool of_usb_otg_srp_support(struct device_node *np)
 {
 	return false;
+}
+static inline void of_usb_set_otg_caps(struct device_node *np,
+				struct usb_otg_caps *otg_caps)
+{
+
 }
 #endif
 
