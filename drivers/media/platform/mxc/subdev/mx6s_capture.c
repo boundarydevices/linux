e4d7d263181f502743b4b255139edcbbb47d8259
Troy Kisky <troy.kisky@boundarydevices.com>
2015-06-17 10:11:33 -0700

    mx6s_capture: don't fail so easily on mx6s_vidioc_s_fmt_vid_cap
    Modifications made for vadc in order for the gstreamer plugin
    to set the stream settings.
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/media/platform/mxc/subdev/mx6s_capture.c
+++ drivers/media/platform/mxc/subdev/mx6s_capture.c
@@ -1289,8 +1289,8 @@ static int mx6s_vidioc_g_input(struct file *file, void *priv, unsigned int *i)
 
 static int mx6s_vidioc_s_input(struct file *file, void *priv, unsigned int i)
 {
-	if (i > 0)
-		return -EINVAL;
+//	if (i > 0)
+//		return -EINVAL;
 
 	return 0;
 }
@@ -1426,7 +1426,6 @@ static int mx6s_vidioc_try_fmt_vid_cap(struct file *file, void *priv,
 
 	pix->sizeimage = fmt->bpp * pix->height * pix->width;
 	pix->bytesperline = fmt->bpp * pix->width;
-
 	return ret;
 }
 
@@ -1438,13 +1437,29 @@ static int mx6s_vidioc_s_fmt_vid_cap(struct file *file, void *priv,
 				    struct v4l2_format *f)
 {
 	struct mx6s_csi_dev *csi_dev = video_drvdata(file);
+	struct v4l2_subdev *sd = csi_dev->sd;
 	int ret;
+	struct v4l2_frmsizeenum fsize;
+
+	memset(&fsize, 0, sizeof(fsize));
+	v4l2_subdev_call(sd, video, enum_framesizes, &fsize);
+	if (!f->fmt.pix.width)
+		f->fmt.pix.width = fsize.discrete.width;
+	if (!f->fmt.pix.height)
+		f->fmt.pix.height = fsize.discrete.height;
 
 	ret = mx6s_vidioc_try_fmt_vid_cap(file, csi_dev, f);
-	if (ret < 0)
-		return ret;
+	if (ret < 0) {
+		f->fmt.pix.pixelformat = fsize.pixel_format;
+		f->fmt.pix.width = fsize.discrete.width;
+		f->fmt.pix.height = fsize.discrete.height;
+		ret = mx6s_vidioc_try_fmt_vid_cap(file, csi_dev, f);
+		if (ret < 0)
+			return -EINVAL;
+	}
 
 	csi_dev->fmt           = format_by_fourcc(f->fmt.pix.pixelformat);
+	csi_dev->pix.pixelformat = f->fmt.pix.pixelformat;
 	csi_dev->pix.width     = f->fmt.pix.width;
 	csi_dev->pix.height    = f->fmt.pix.height;
 	csi_dev->pix.sizeimage = f->fmt.pix.sizeimage;
