01ce1d48f480a615377f0ca14952dba0746b0d80
Li Jun <jun.li@freescale.com>
2015-06-18 10:54:26 +0800

    MLK-11123-15 usb: otg: remove bcdOTG from usb_otg_descriptor
    Remove bcdOTG from usb_otg_descriptor since we introduce usb_otg20_descriptor
    for OTG 2.0 or above, and usb_otg_descriptor is only for OTG 1.x.
    
    Signed-off-by: Li Jun <jun.li@freescale.com>

--- include/uapi/linux/usb/ch9.h
+++ include/uapi/linux/usb/ch9.h
@@ -674,12 +674,6 @@ struct usb_otg_descriptor {
 	__u8  bDescriptorType;
 
 	__u8  bmAttributes;	/* support for HNP, SRP, etc */
-	__le16 bcdOTG;		/* OTG and EH supplement release number
-				 * in binary-coded decimal (i.e. 2.0 is 0200H).
-				 * This field identifies the release of the
-				 * OTG and EH supplement with which the device
-				 * and its descriptors are compliant.
-				 */
 } __attribute__ ((packed));
 
 /* USB_DT_OTG (from OTG 2.0 supplement) */
