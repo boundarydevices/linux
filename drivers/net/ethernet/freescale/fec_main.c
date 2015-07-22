45431f336a2698725be18be2459cafaf5595b621
Fugang Duan <b38611@freescale.com>
2015-07-22 12:42:40 +0800

    MLK-11274 net: fec: add mii bus up_failed flag to reflect the real status
    Add mii bus up_failed flag to reflect the real mii bus status.
    
    Signed-off-by: Fugang Duan <B38611@freescale.com>
    Reported-and-tested-by: Zhang Sanshan <B51434@freescale.com>
    (cherry picked from commit: ea348e597501d44841a28d8ee099361e89d63520)
    TODO: checkpatch warnings
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -2967,12 +2967,14 @@ fec_enet_open(struct net_device *ndev)
 
 	device_set_wakeup_enable(&ndev->dev, fep->wol_flag &
 				 FEC_WOL_FLAG_ENABLE);
+	fep->miibus_up_failed = false;
 
 	return 0;
 
 err_enet_mii_probe:
 	fec_enet_free_buffers(ndev);
 err_enet_alloc:
+	fep->miibus_up_failed = true;
 	if (!fep->mii_bus_share)
 		fec_enet_clk_enable(ndev, false);
 clk_enable:
@@ -3779,7 +3781,7 @@ static int __maybe_unused fec_suspend(struct device *dev)
 			enable_irq_wake(fep->wake_irq);
 		}
 		fec_enet_clk_enable(ndev, false);
-	} else if (fep->mii_bus_share && !ndev->phydev) {
+	} else if (fep->mii_bus_share && fep->miibus_up_failed && !ndev->phydev) {
 		fec_enet_clk_enable(ndev, false);
 		pinctrl_pm_select_sleep_state(&fep->pdev->dev);
 	}
@@ -3837,6 +3839,8 @@ static int __maybe_unused fec_resume(struct device *dev)
 		phy_start(ndev->phydev);
 	} else if (fep->mii_bus_share && !ndev->phydev) {
 		pinctrl_pm_select_default_state(&fep->pdev->dev);
+		fep->miibus_up_failed = true;
+		/* And then recovery mii bus */
 		fec_restore_mii_bus(ndev);
 	}
 	rtnl_unlock();
