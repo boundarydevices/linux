96dd3a2339f7b18cb2fa8d13a5e01ab6e8edf8b2
Haoran Wang <b50027@freescale.com>
2015-07-31 11:02:53 +0800

    MA-5957 Remove RTL8821AS logs
    Remove RTL8821AS remain logs.
    
    Signed-off-by: Haoran Wang <b50027@freescale.com>

--- drivers/net/wireless/rtlwifi/rtl8821as/include/autoconf.h
+++ drivers/net/wireless/rtlwifi/rtl8821as/include/autoconf.h
@@ -99,13 +99,13 @@
  * Debug Related Config
  *
  ***********************************************************/
-#define CONFIG_DEBUG /* DBG_871X, etc... */
+/* #define CONFIG_DEBUG */ /* DBG_871X, etc... */
 //#define CONFIG_DEBUG_RTL871X /* RT_TRACE, RT_PRINT_DATA, _func_enter_, _func_exit_ */
-#define CONFIG_PROC_DEBUG
-#define DBG_CONFIG_ERROR_DETECT
+/* #define CONFIG_PROC_DEBUG */
+/* #define DBG_CONFIG_ERROR_DETECT */
 //#define DBG_CONFIG_ERROR_RESET
 
-#define DBG 1
+#define DBG 0
 
 //#define DBG_IO
 //#define DBG_DELAY_OS
