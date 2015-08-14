7f0ef203119b244704ba7e9ac2ed9a6802c13c9f
Fancy Fang <chen.fang@freescale.com>
2015-08-14 14:28:31 +0800

    MLK-11362 video: mxsfb: add busfreq support for this module
    Add busfreq support for this framebuffer driver.
    
    Signed-off-by: Fancy Fang <chen.fang@freescale.com>
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>

--- drivers/video/fbdev/mxsfb.c
+++ drivers/video/fbdev/mxsfb.c
@@ -39,6 +39,7 @@
  * the required value in the imx_fb_videomode structure.
  */
 
+#include <linux/busfreq-imx.h>
 #include <linux/console.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -1539,6 +1540,7 @@ static void mxsfb_shutdown(struct platform_device *pdev)
 #ifdef CONFIG_PM_RUNTIME
 static int mxsfb_runtime_suspend(struct device *dev)
 {
+	release_bus_freq(BUS_FREQ_HIGH);
 	dev_dbg(dev, "mxsfb busfreq high release.\n");
 
 	return 0;
@@ -1546,6 +1548,7 @@ static int mxsfb_runtime_suspend(struct device *dev)
 
 static int mxsfb_runtime_resume(struct device *dev)
 {
+	request_bus_freq(BUS_FREQ_HIGH);
 	dev_dbg(dev, "mxsfb busfreq high request.\n");
 
 	return 0;
