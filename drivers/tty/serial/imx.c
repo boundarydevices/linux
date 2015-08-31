efd3e6dd25ddd868857d495503249f1788dcd82d
Fugang Duan <b38611@freescale.com>
2015-08-31 11:39:15 +0800

    MLK-11456-01 tty: serial: imx: add pinctrl sleep mode support
    For LPSR mode, UART iomux settings will be lost after resume,
    so add pinctrl sleep mode support.
    
    Signed-off-by: Anson Huang <b20788@freescale.com>
    Signed-off-by: Fugang Duan <B38611@freescale.com>
    Vipul: Fixes for 4.19 kernel.
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -28,6 +28,7 @@
 #include <linux/slab.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
+#include <linux/pinctrl/consumer.h>
 #include <linux/io.h>
 #include <linux/dma-mapping.h>
 
@@ -2465,6 +2466,8 @@ static int imx_uart_suspend_noirq(struct device *dev)
 
 	clk_disable(sport->clk_ipg);
 
+	pinctrl_pm_select_sleep_state(dev);
+
 	return 0;
 }
 
@@ -2472,6 +2475,8 @@ static int imx_uart_resume_noirq(struct device *dev)
 {
 	struct imx_port *sport = dev_get_drvdata(dev);
 	int ret;
+
+	pinctrl_pm_select_default_state(dev);
 
 	ret = clk_enable(sport->clk_ipg);
 	if (ret)
