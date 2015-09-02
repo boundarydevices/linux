95f849d642e522e91ce27f25e0493f05c6df2d27
Anson Huang <b20788@freescale.com>
2015-09-03 01:05:48 +0800

    MLK-11492 ARM: imx: keep weak 2p5 on for USB vbus wakeup
    Since i.MX6SX, if USB vbus wake up is enabled, weak 2P5
    needs to be on even if the DRAM is LPDDR2, previously, we need
    to set stop_mode_config to keep 2P5 on, so enter DSM,
    if USB vbus wakeup is enabled, we need to keep weak 2P5 on.
    
    Signed-off-by: Anson Huang <b20788@freescale.com>
    (cherry picked from commit 1ca4dffee79055ea95c59e27bab50bc5080310f5)
    Signed-off-by: Peter Chen <peter.chen@freescale.com>
    Signed-off-by: Arulpandiyan Vadivel <arulpandiyan_vadivel@mentor.com>

--- arch/arm/mach-imx/anatop.c
+++ arch/arm/mach-imx/anatop.c
@@ -109,7 +109,8 @@ void imx_anatop_pre_suspend(void)
 		return;
 	}
 
-	if (imx_mmdc_get_ddr_type() == IMX_DDR_TYPE_LPDDR2)
+	if ((imx_mmdc_get_ddr_type() == IMX_DDR_TYPE_LPDDR2) &&
+		!imx_gpc_usb_wakeup_enabled())
 		imx_anatop_enable_2p5_pulldown(true);
 	else
 		imx_anatop_enable_weak2p5(true);
@@ -133,7 +134,8 @@ void imx_anatop_post_resume(void)
 		return;
 	}
 
-	if (imx_mmdc_get_ddr_type() == IMX_DDR_TYPE_LPDDR2)
+	if ((imx_mmdc_get_ddr_type() == IMX_DDR_TYPE_LPDDR2) &&
+		!imx_gpc_usb_wakeup_enabled())
 		imx_anatop_enable_2p5_pulldown(false);
 	else
 		imx_anatop_enable_weak2p5(false);
