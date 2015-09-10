7cb9d881d27e96c15c8961f8564d7b3d9bae1a4b
Troy Kisky <troy.kisky@boundarydevices.com>
2015-09-10 11:38:41 -0700

    tc358743_h2c: fix compile bug
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/media/platform/mxc/capture/tc358743_h2c.c
+++ drivers/media/platform/mxc/capture/tc358743_h2c.c
@@ -38,7 +38,7 @@
 #include <linux/mutex.h>
 #include <linux/mipi_csi2.h>
 #include <media/v4l2-chip-ident.h>
-#include <media/v4l2-int-device.h>
+#include "v4l2-int-device.h"
 #include <sound/core.h>
 #include <sound/pcm.h>
 #include <sound/soc.h>
