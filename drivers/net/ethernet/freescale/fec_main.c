16f4f41cc5518198a3159907ce02dc769318077c
Fugang Duan <b38611@freescale.com>
2015-10-15 17:38:29 +0800

    MLK-11704 net: fec: fix MDIO read/write timeout when ethx down/up after suspend/resume
    This reverts commit 12fee3d24574bd084e8b6fa1dcaf84a084298c9a.
    
    The commit:12fee3d24574 intruduce MDIO read/write timeout when Ethernet
    down and up after Mega/Fast off. The patch just revert the patch.
    
    Signed-off-by: Fugang Duan <B38611@freescale.com>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -2967,6 +2967,7 @@ fec_enet_close(struct net_device *ndev)
 	}
 
 	phy_disconnect(ndev->phydev);
+	ndev->phydev = NULL;
 
 	if (fep->quirks & FEC_QUIRK_ERR006687)
 		imx6q_cpuidle_fec_irqs_unused();
