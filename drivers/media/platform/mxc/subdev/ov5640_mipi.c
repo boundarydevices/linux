9e2dc2e50caf927d5ed09b27d7ec870c98c3e90e
Gary Bisson <gary.bisson@boundarydevices.com>
2015-11-18 16:46:33 +0100

    subdev: ov5640_mipi: add pwm option for mclk
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- drivers/media/platform/mxc/subdev/ov5640_mipi.c
+++ drivers/media/platform/mxc/subdev/ov5640_mipi.c
@@ -22,6 +22,7 @@
 #include <linux/i2c.h>
 #include <linux/of_gpio.h>
 #include <linux/pinctrl/consumer.h>
+#include <linux/pwm.h>
 #include <linux/regulator/consumer.h>
 #include <linux/v4l2-mediabus.h>
 #include <media/v4l2-device.h>
@@ -1788,6 +1789,7 @@ static int ov5640_probe(struct i2c_client *client,
 			const struct i2c_device_id *id)
 {
 	struct pinctrl *pinctrl;
+	struct pwm_device *pwm;
 	struct device *dev = &client->dev;
 	int retval;
 	u8 chip_id_high, chip_id_low;
@@ -1863,6 +1865,13 @@ static int ov5640_probe(struct i2c_client *client,
 	ov5640_data.streamcap.capturemode = 0;
 	ov5640_data.streamcap.timeperframe.denominator = DEFAULT_FPS;
 	ov5640_data.streamcap.timeperframe.numerator = 1;
+
+	pwm = pwm_get(dev, NULL);
+	if (!IS_ERR(pwm)) {
+		dev_info(dev, "found pwm%d, period=%d\n", pwm->pwm, pwm->period);
+		pwm_config(pwm, pwm->period >> 1, pwm->period);
+		pwm_enable(pwm);
+	}
 
 	ov5640_regulator_enable(&client->dev);
 
