d1369a78bdbbb273e954dc11e0ab8993b60d86d7
Troy Kisky <troy.kisky@boundarydevices.com>
2016-01-20 11:24:45 -0700

    net: fec: rename dirty_tx to pending_tx
    dirty_tx always pointed to the last entry that was
    trasmitted. pending_tx always points to the next entry
    to be transmitted. This should be a little more efficient.
    
    This will allow 1 more entry in the queue to be used.
    That is, ring_size -1 entries instead of ring_size -2.
    
    This also allows the removal of fec_enet_get_prevdesc.
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/net/ethernet/freescale/fec.h
+++ drivers/net/ethernet/freescale/fec.h
@@ -540,6 +540,10 @@ enum {
 	XDP_STATS_TOTAL,
 };
 
+/* bd.cur points to the currently available buffer.
+ * pending_tx tracks the current buffer that is being sent by the
+ * controller.  When bd.cur and pending_tx are equal, nothing is pending.
+ */
 struct fec_enet_priv_tx_q {
 	struct bufdesc_prop bd;
 	unsigned char *tx_bounce[TX_RING_SIZE];
@@ -548,7 +552,7 @@ struct fec_enet_priv_tx_q {
 	unsigned short tx_stop_threshold;
 	unsigned short tx_wake_threshold;
 
-	struct bufdesc	*dirty_tx;
+	struct bufdesc	*pending_tx;
 	char *tso_hdrs;
 	dma_addr_t tso_hdrs_dma;
 };
@@ -572,13 +576,7 @@ struct fec_stop_mode_gpr {
 	u8 bit;
 };
 
-/* The FEC buffer descriptors track the ring buffers.  The rx_bd_base and
- * tx_bd_base always point to the base of the buffer descriptors.  The
- * cur_rx and cur_tx point to the currently available buffer.
- * The dirty_tx tracks the current buffer that is being sent by the
- * controller.  The cur_tx and dirty_tx are equal under both completely
- * empty and completely full conditions.  The empty/ready indicator in
- * the buffer descriptor determines the actual condition.
+/* The FEC buffer descriptors track the ring buffers.
  */
 struct fec_enet_private {
 	/* Hardware registers of the FEC device */
