6e68c5159065b76a97d3e30a069b12d318c521cc
Haibo Chen <haibo.chen@nxp.com>
2016-04-05 17:43:38 +0800

    MLK-12617 mmc: Fix compile error when CONFIG_MMC=m
    When CONFIG_MMC=m, compile error shows up
    
    ERROR: "of_alias_max_index" [drivers/mmc/core/mmc_core.ko] undefined!
    ERROR: "mmc_get_reserved_index" [drivers/mmc/card/mmc_block.ko] undefined!
    ERROR: "mmc_first_nonreserved_index" [drivers/mmc/card/mmc_block.ko] undefined!
    make[1]: *** [__modpost] Error 1
    make: *** [modules] Error 2
    make: *** Waiting for unfinished jobs....
    
    This patch export the upper three symbol for module runtime load.
    
    Signed-off-by: Haibo Chen <haibo.chen@nxp.com>
    (cherry picked from commit 3b2520f17d427b8fa8db37a6d9a4311f20c29036)

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -2705,6 +2705,7 @@ int mmc_first_nonreserved_index(void)
 {
 	return __mmc_max_reserved_idx + 1;
 }
+EXPORT_SYMBOL(mmc_first_nonreserved_index);
 
 /**
  * mmc_get_reserved_index() - get the index reserved for this host
@@ -2716,6 +2717,7 @@ int mmc_get_reserved_index(struct mmc_host *host)
 {
 	return of_alias_get_id(host->parent->of_node, "mmc");
 }
+EXPORT_SYMBOL(mmc_get_reserved_index);
 
 static void mmc_of_reserve_idx(void)
 {
