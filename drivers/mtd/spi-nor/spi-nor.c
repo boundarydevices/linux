1451706043637255b867de668988bbc1829740e4
Troy Kisky <troy.kisky@boundarydevices.com>
2016-05-10 15:59:44 -0700

    spi-nor: add ISSI is25wp032/64/128
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -875,6 +875,13 @@ static const struct flash_info spi_nor_ids[] = {
 
 	/* ISSI */
 	{ "is25cd512", INFO(0x7f9d20, 0, 32 * 1024,   2, SECT_4K) },
+	{ "is25lp032", INFO(0x9d6016, 0, 64 * 1024,  64, SECT_4K) },
+	{ "is25lp064", INFO(0x9d6017, 0, 64 * 1024, 128, SECT_4K) },
+	{ "is25lp128", INFO(0x9d6018, 0, 64 * 1024, 256, SECT_4K) },
+
+	{ "is25wp032", INFO(0x9d7016, 0, 64 * 1024,  64, SECT_4K) },
+	{ "is25wp064", INFO(0x9d7017, 0, 64 * 1024, 128, SECT_4K) },
+	{ "is25wp128", INFO(0x9d7018, 0, 64 * 1024, 256, SECT_4K) },
 
 	/* Macronix */
 	{ "mx25l512e",   INFO(0xc22010, 0, 64 * 1024,   1, SECT_4K) },
