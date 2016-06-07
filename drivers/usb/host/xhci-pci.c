4862b4583bc8b7e0421af89963289850732a1356
Christian Lamparter <chunkeey@gmail.com>
2016-06-08 00:14:57 +0200

    usb: xhci: handle uPD720201 and uPD720202 w/o ROM
    This patch adds a firmware check for the uPD720201K8-711-BAC-A
    and uPD720202K8-711-BAA-A variant. Both of these chips are listed
    in Renesas' R19UH0078EJ0500 Rev.5.00 "User's Manual: Hardware" as
    devices which need a firmware in order to work as they do not have
    support to load the firmware from an external ROM.
    
    Currently, the xhci-pci driver is unable to initialize the hcd in
    this case. Instead it will wait for 30 seconds and cause a timeout
    in xhci_handshake() and fails.
    
    [	5.116990] xhci_hcd 0000:45:00.0: new USB bus registered ...
    [	32.335215] xhci_hcd 0000:45:00.0: can't setup: -110
    [	32.340179] xhci_hcd 0000:45:00.0: USB bus 2 deregistered
    [	32.345587] xhci_hcd 0000:45:00.0: init 0000:45:00.0 fail, -110
    [	32.351496] xhci_hcd: probe of 0000:45:00.0 failed with error -110
    
    With the firmware loader and the correct firmware in place,
    the device will now initialize successfully:
    
    [	5.384776] xhci_hcd 0000:45:00.0: xHCI Host Controller
    [	5.390028] xhci_hcd 0000:45:00.0: new USB bus registered ...
    [	5.403017] xhci_hcd 0000:45:00.0: hcc params 0x014051cf ...
    [	5.412217] hub 2-0:1.0: USB hub found
    [	5.416123] hub 2-0:1.0: 2 ports detected
    [	5.420494] xhci_hcd 0000:45:00.0: xHCI Host Controller
    [	5.425730] xhci_hcd 0000:45:00.0: new USB bus registered...
    [	5.441771] hub 3-0:1.0: USB hub found
    [	5.445692] hub 3-0:1.0: 2 ports detected
    ...
    The firmware image can be extracted from the Windows driver.
    A viable source is the "PP2U-E" (USB3.0 Host to PCIe Adapter)'s
    "Firmware download (ver 2.0.1.3) Jun 15, 2012" file. It contains
    the K2013080.mem file which needs to be placed in /lib/firmware.
    
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/usb/host/xhci-pci.c
+++ drivers/usb/host/xhci-pci.c
@@ -27,6 +27,7 @@
 
 #include "xhci.h"
 #include "xhci-trace.h"
+#include "xhci-renesas.h"
 
 #define SSIC_PORT_NUM		2
 #define SSIC_PORT_CFG2		0x880c
@@ -260,6 +261,12 @@ static int xhci_pci_probe(struct pci_dev *dev, const struct pci_device_id *id)
 	struct hc_driver *driver;
 	struct usb_hcd *hcd;
 
+	if (IS_ENABLED(CONFIG_USB_XHCI_RENESAS_FW_LOADING)) {
+		/* Check if this device is a RENESAS uPD720201/2 device. */
+		retval = renesas_check_if_fw_dl_is_needed(dev);
+		if (retval)
+			return retval;
+	}
 	driver = (struct hc_driver *)id->driver_data;
 
 	/* Prevent runtime suspending between USB-2 and USB-3 initialization */
@@ -444,6 +451,13 @@ static int xhci_pci_resume(struct usb_hcd *hcd, bool hibernated)
 
 	if (xhci->quirks & XHCI_PME_STUCK_QUIRK)
 		xhci_pme_quirk(hcd);
+
+	if (IS_ENABLED(CONFIG_USB_XHCI_RENESAS_FW_LOADING)) {
+		/* Check if this device is a RENESAS uPD720201/2 device. */
+		retval = renesas_check_if_fw_dl_is_needed(pdev);
+		if (retval)
+			return retval;
+	}
 
 	retval = xhci_resume(xhci, hibernated);
 	return retval;
