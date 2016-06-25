58ba7020922f0bbf731d7eed7dbb1237b4e83e52
Troy Kisky <troy.kisky@boundarydevices.com>
2016-06-24 20:01:17 -0700

    brcmfmac: bcmsdh: fix compile warning
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/bcmsdh.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/bcmsdh.c
@@ -22,11 +22,11 @@
 #include <linux/sched.h>
 #include <linux/completion.h>
 #include <linux/scatterlist.h>
+#include <linux/mmc/host.h>
 #include <linux/mmc/sdio.h>
 #include <linux/mmc/core.h>
 #include <linux/mmc/sdio_func.h>
 #include <linux/mmc/card.h>
-#include <linux/mmc/host.h>
 #include <linux/pm_runtime.h>
 #include <linux/suspend.h>
 #include <linux/errno.h>
