17bf67e22fe91add0042ca2f9cbc78f9ca6de432
tip-bot for Irina Tirdea <irina.tirdea@intel.com>
2016-09-10 20:57:37 +0300

    Input: goodix - add support for gt9157
    Goodix touchscreen GT9157 has the same basic functionality
    as GT911 touchscreen. This is based on Goodix datasheets
    for GT911 and GT9157 and on Goodix driver gt9xx.c for
    Android (publicly available in Android kernel trees for
    various devices).
    
    Signed-off-by: Irina Tirdea <irina.tirdea@intel.com>
    Acked-by: Rob Herring <robh@kernel.org>
    Reviewed-by: Bastien Nocera <hadess@hadess.net>

--- drivers/input/touchscreen/goodix.c
+++ drivers/input/touchscreen/goodix.c
@@ -257,6 +257,7 @@ static const struct goodix_chip_data *goodix_get_chip_data(u16 id)
 	case 911:
 	case 9271:
 	case 9110:
+	case 9157:
 	case 927:
 	case 928:
 		return &gt911_chip_data;
@@ -1357,6 +1358,7 @@ static const struct of_device_id goodix_of_match[] = {
 	{ .compatible = "goodix,gt911" },
 	{ .compatible = "goodix,gt9110" },
 	{ .compatible = "goodix,gt912" },
+	{ .compatible = "goodix,gt9157" },
 	{ .compatible = "goodix,gt927" },
 	{ .compatible = "goodix,gt9271" },
 	{ .compatible = "goodix,gt928" },
