00414cda1b3dbb573393367c82f241eca3a0c5ac
Troy Kisky <troy.kisky@boundarydevices.com>
2016-12-27 19:24:02 -0700

    pinctrl-imx: allow no mux register for imx51
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/pinctrl/freescale/pinctrl-imx.c
+++ drivers/pinctrl/freescale/pinctrl-imx.c
@@ -210,23 +210,19 @@ static int imx_pmx_set(struct pinctrl_dev *pctldev, unsigned selector,
 		pin_id = pin->pin;
 		pin_reg = &info->pin_regs[pin_id];
 
-		if (pin_reg->mux_reg == -1) {
-			dev_dbg(ipctl->dev, "Pin(%s) does not support mux function\n",
-				info->pins[pin_id].name);
-			continue;
+		if (pin_reg->mux_reg != -1) {
+			if (info->flags & SHARE_MUX_CONF_REG) {
+				u32 reg;
+				reg = readl(ipctl->base + pin_reg->mux_reg);
+				reg &= ~info->mux_mask;
+				reg |= (pin->mux_mode << mux_shift);
+				writel(reg, ipctl->base + pin_reg->mux_reg);
+			} else {
+				writel(pin->mux_mode, ipctl->base + pin_reg->mux_reg);
+			}
+			dev_dbg(ipctl->dev, "write: offset 0x%x val 0x%x\n",
+				pin_reg->mux_reg, pin->mux_mode);
 		}
-
-		if (info->flags & SHARE_MUX_CONF_REG) {
-			u32 reg;
-			reg = readl(ipctl->base + pin_reg->mux_reg);
-			reg &= ~info->mux_mask;
-			reg |= (pin->mux_mode << mux_shift);
-			writel(reg, ipctl->base + pin_reg->mux_reg);
-		} else {
-			writel(pin->mux_mode, ipctl->base + pin_reg->mux_reg);
-		}
-		dev_dbg(ipctl->dev, "write: offset 0x%x val 0x%x\n",
-			pin_reg->mux_reg, pin->mux_mode);
 
 		/*
 		 * If the select input value begins with 0xff, it's a quirky
