74a54407289182ced474f40d7cae7a6c0c619b50
Ulf Hansson <ulf.hansson@linaro.org>
2017-01-13 14:14:16 +0100

    mmc: core: Don't use extern declarations of public mmc functions
    Using extern when declaring functions in the public header, core.h, is
    redundant. Let's just remove the use of it.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Reviewed-by: Shawn Lin <shawn.lin@rock-chips.com>
    (cherry picked from commit 23888bfe9bd730d42e326cc5f6f1188beb948f97)

--- include/linux/mmc/core.h
+++ include/linux/mmc/core.h
@@ -158,14 +158,15 @@ struct mmc_request {
 struct mmc_card;
 struct mmc_async_req;
 
-extern struct mmc_async_req *mmc_start_req(struct mmc_host *,
-					   struct mmc_async_req *,
-					   enum mmc_blk_status *);
-extern void mmc_wait_for_req(struct mmc_host *, struct mmc_request *);
-extern int mmc_wait_for_cmd(struct mmc_host *, struct mmc_command *, int);
+struct mmc_async_req *mmc_start_req(struct mmc_host *host,
+				struct mmc_async_req *areq,
+				enum mmc_blk_status *ret_stat);
+void mmc_wait_for_req(struct mmc_host *host, struct mmc_request *mrq);
+int mmc_wait_for_cmd(struct mmc_host *host, struct mmc_command *cmd,
+		int retries);
 
-extern int mmc_hw_reset(struct mmc_host *host);
-extern void mmc_set_data_timeout(struct mmc_data *, const struct mmc_card *);
+int mmc_hw_reset(struct mmc_host *host);
+void mmc_set_data_timeout(struct mmc_data *data, const struct mmc_card *card);
 
 int mmc_first_nonreserved_index(void);
 int mmc_get_reserved_index(struct mmc_host *host);
