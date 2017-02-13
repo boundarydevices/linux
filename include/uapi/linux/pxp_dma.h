d72a3aaa79a2dc2e046897d0c849fd65df74e416
Robby Cai <robby.cai@nxp.com>
2017-02-13 18:01:25 +0800

    MLK-13917 pxp: fix build error for pxp library in user space
    Fix following build error by changing type to '__u64'.
    
    include/uapi/linux/pxp_dma.h:230:2: error: unknown type name 'u64'
       u64 lut_sels;
       ^~~
    
    Signed-off-by: Robby Cai <robby.cai@nxp.com>
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>

--- include/uapi/linux/pxp_dma.h
+++ include/uapi/linux/pxp_dma.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013-2015 Freescale Semiconductor, Inc. All Rights Reserved.
+ * Copyright 2017 NXP
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
