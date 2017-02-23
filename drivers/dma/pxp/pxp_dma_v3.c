78b33323aba41d03bf29215198d42bcf9997485e
Robby Cai <robby.cai@nxp.com>
2017-02-23 20:50:06 +0800

    MLK-13198 pxp: imx7d: fix error histogram status report issue
    The cause is the legacy process involves PXP AS engine accidentally to
    process data, which results to one pixel at (0,0) to be changed. This
    will cause an incorrect histogram calculation.
    
    Signed-off-by: Robby Cai <robby.cai@nxp.com>
    (Vipul: Fixed merge conflicts)
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>

--- drivers/dma/pxp/pxp_dma_v3.c
+++ drivers/dma/pxp/pxp_dma_v3.c
@@ -1520,14 +1520,34 @@ static int pxp_config(struct pxps *pxp, struct pxp_channel *pxp_chan)
 		if (pxp->devdata && pxp->devdata->pxp_wfe_a_process)
 			pxp->devdata->pxp_wfe_a_process(pxp);
 
-		break;
-	case PXP_OP_WFE_B:
-		pxp_wfe_b_configure(pxp);
-		pxp_wfe_b_process(pxp);
-		break;
-	default:
-		pr_err("Invalid pxp operation type passed\n");
-		return -EINVAL;
+		return 0;
+	}
+
+	/* Configure PxP regs */
+	pxp_set_ctrl(pxp);
+	pxp_set_s0param(pxp);
+	pxp_set_s0crop(pxp);
+	pxp_set_scaling(pxp);
+	ol_nr = pxp_conf_data->layer_nr - 2;
+
+	if (ol_nr == 0) {
+		/* disable AS engine */
+		__raw_writel(BF_PXP_OUT_AS_ULC_X(1) |
+				BF_PXP_OUT_AS_ULC_Y(1),
+				pxp->base + HW_PXP_OUT_AS_ULC);
+
+		__raw_writel(BF_PXP_OUT_AS_LRC_X(0) |
+				BF_PXP_OUT_AS_LRC_Y(0),
+				pxp->base + HW_PXP_OUT_AS_LRC);
+	} else {
+		while (ol_nr > 0) {
+			i = pxp_conf_data->layer_nr - 2 - ol_nr;
+			pxp_set_oln(i, pxp);
+			pxp_set_olparam(i, pxp);
+			/* only the color key in higher overlay will take effect. */
+			pxp_set_olcolorkey(i, pxp);
+			ol_nr--;
+		}
 	}
 
 	return 0;
