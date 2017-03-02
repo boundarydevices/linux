260c6d8c9b6c0a9185c54a0b274da290460410d1
Yun Cai <yun.cai@amlogic.com>
2017-03-02 14:30:32 +0800

    clocktree: update misc clk
    PD#138714: update misc clock for gxl
    
    Change-Id: I9cf067ba8c39fbcfa5fe17671f3bd6a9ec890263
    Signed-off-by: Yun Cai <yun.cai@amlogic.com>

--- drivers/amlogic/clk/clk_misc.c
+++ drivers/amlogic/clk/clk_misc.c
@@ -26,6 +26,7 @@
 #include "clkc.h"
 #include "gxl.h"
 
+/* cts_vdin_meas_clk */
 const char *meas_parent_names[] = { "xtal", "fclk_div4",
 	"fclk_div3", "fclk_div5", "vid_pll_clk", "vid2_pll_clk"};
 
@@ -77,12 +78,266 @@ static struct clk_hw *vdin_meas_clk_hws[] = {
 [CLKID_VDIN_MEAS_GATE - CLKID_VDIN_MEAS_MUX] = &vdin_meas_gate.hw,
 };
 
+/* cts_amclk */
+const char *amclk_parent_names[] = { "ddr_pll_clk", "mpll0", "mpll1", "mpll2"};
+
+static struct clk_mux amclk_mux = {
+	.reg = (void *)HHI_AUD_CLK_CNTL,
+	.mask = 0x3,
+	.shift = 9,
+	.lock = &clk_lock,
+	.hw.init = &(struct clk_init_data){
+		.name = "amclk_mux",
+		.ops = &clk_mux_ops,
+		.parent_names = amclk_parent_names,
+		.num_parents = 4,
+		.flags = (CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED),
+	},
+};
+
+static struct clk_divider amclk_div = {
+	.reg = (void *)HHI_AUD_CLK_CNTL,
+	.shift = 0,
+	.width = 8,
+	.lock = &clk_lock,
+	.hw.init = &(struct clk_init_data){
+		.name = "amclk_div",
+		.ops = &clk_divider_ops,
+		.parent_names = (const char *[]){ "amclk_mux" },
+		.num_parents = 1,
+		.flags = (CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED),
+	},
+};
+
+static struct clk_gate amclk_gate = {
+	.reg = (void *)HHI_AUD_CLK_CNTL,
+	.bit_idx = 8,
+	.lock = &clk_lock,
+	.hw.init = &(struct clk_init_data) {
+		.name = "amclk_gate",
+		.ops = &clk_gate_ops,
+		.parent_names = (const char *[]){ "amclk_div" },
+		.num_parents = 1,
+		.flags = (CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED),
+	},
+};
+
+static struct clk_hw *amclk_hws[] = {
+[CLKID_AMCLK_MUX - CLKID_AMCLK_MUX] = &amclk_mux.hw,
+[CLKID_AMCLK_DIV - CLKID_AMCLK_MUX] = &amclk_div.hw,
+[CLKID_AMCLK_GATE - CLKID_AMCLK_MUX] = &amclk_gate.hw,
+};
+
+/* cts_pdm */
+const char *pdm_parent_names[] = { "amclk_composite",
+	"mpll0", "mpll1", "mpll2"};
+
+static struct clk_mux pdm_mux = {
+	.reg = (void *)HHI_AUD_CLK_CNTL3,
+	.mask = 0x3,
+	.shift = 17,
+	.lock = &clk_lock,
+	.hw.init = &(struct clk_init_data){
+		.name = "pdm_mux",
+		.ops = &clk_mux_ops,
+		.parent_names = pdm_parent_names,
+		.num_parents = 4,
+		.flags = (CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED),
+	},
+};
+
+static struct clk_divider pdm_div = {
+	.reg = (void *)HHI_AUD_CLK_CNTL3,
+	.shift = 0,
+	.width = 16,
+	.lock = &clk_lock,
+	.hw.init = &(struct clk_init_data){
+		.name = "pdm_div",
+		.ops = &clk_divider_ops,
+		.parent_names = (const char *[]){ "pdm_mux" },
+		.num_parents = 1,
+		.flags = (CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED),
+	},
+};
+
+static struct clk_gate pdm_gate = {
+	.reg = (void *)HHI_AUD_CLK_CNTL3,
+	.bit_idx = 16,
+	.lock = &clk_lock,
+	.hw.init = &(struct clk_init_data) {
+		.name = "pdm_gate",
+		.ops = &clk_gate_ops,
+		.parent_names = (const char *[]){ "pdm_div" },
+		.num_parents = 1,
+		.flags = (CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED),
+	},
+};
+
+static struct clk_hw *pdm_hws[] = {
+[CLKID_PDM_MUX - CLKID_PDM_MUX] = &pdm_mux.hw,
+[CLKID_PDM_DIV - CLKID_PDM_MUX] = &pdm_div.hw,
+[CLKID_PDM_GATE - CLKID_PDM_MUX] = &pdm_gate.hw,
+};
+
+/* cts_clk_i958 */
+const char *i958_parent_names[] = { "NULL", "mpll0", "mpll1", "mpll2"};
+
+static struct clk_mux i958_mux = {
+	.reg = (void *)HHI_AUD_CLK_CNTL2,
+	.mask = 0x3,
+	.shift = 25,
+	.lock = &clk_lock,
+	.hw.init = &(struct clk_init_data){
+		.name = "i958_mux",
+		.ops = &clk_mux_ops,
+		.parent_names = i958_parent_names,
+		.num_parents = 4,
+		.flags = (CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED),
+	},
+};
+
+static struct clk_divider i958_div = {
+	.reg = (void *)HHI_AUD_CLK_CNTL2,
+	.shift = 16,
+	.width = 8,
+	.lock = &clk_lock,
+	.hw.init = &(struct clk_init_data){
+		.name = "i958_div",
+		.ops = &clk_divider_ops,
+		.parent_names = (const char *[]){ "i958_mux" },
+		.num_parents = 1,
+		.flags = (CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED),
+	},
+};
+
+static struct clk_gate i958_gate = {
+	.reg = (void *)HHI_AUD_CLK_CNTL2,
+	.bit_idx = 24,
+	.lock = &clk_lock,
+	.hw.init = &(struct clk_init_data) {
+		.name = "i958_gate",
+		.ops = &clk_gate_ops,
+		.parent_names = (const char *[]){ "i958_div" },
+		.num_parents = 1,
+		.flags = (CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED),
+	},
+};
+
+static struct clk_hw *i958_hws[] = {
+[CLKID_I958_MUX - CLKID_I958_MUX] = &i958_mux.hw,
+[CLKID_I958_DIV - CLKID_I958_MUX] = &i958_div.hw,
+[CLKID_I958_GATE - CLKID_I958_MUX] = &i958_gate.hw,
+};
+
+/* cts_pcm_mclk */
+const char *pcm_mclk_parent_names[] = { "mpll0", "fclk_div4",
+	"fclk_div3", "fclk_div5"};
+
+static struct clk_mux pcm_mclk_mux = {
+	.reg = (void *)HHI_PCM_CLK_CNTL,
+	.mask = 0x3,
+	.shift = 10,
+	.lock = &clk_lock,
+	.hw.init = &(struct clk_init_data){
+		.name = "pcm_mclk_mux",
+		.ops = &clk_mux_ops,
+		.parent_names = pcm_mclk_parent_names,
+		.num_parents = 4,
+		.flags = (CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED),
+	},
+};
+
+static struct clk_divider pcm_mclk_div = {
+	.reg = (void *)HHI_PCM_CLK_CNTL,
+	.shift = 0,
+	.width = 9,
+	.lock = &clk_lock,
+	.hw.init = &(struct clk_init_data){
+		.name = "pcm_mclk_div",
+		.ops = &clk_divider_ops,
+		.parent_names = (const char *[]){ "pcm_mclk_mux" },
+		.num_parents = 1,
+		.flags = (CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED),
+	},
+};
+
+static struct clk_gate pcm_mclk_gate = {
+	.reg = (void *)HHI_PCM_CLK_CNTL,
+	.bit_idx = 9,
+	.lock = &clk_lock,
+	.hw.init = &(struct clk_init_data) {
+		.name = "pcm_mclk_gate",
+		.ops = &clk_gate_ops,
+		.parent_names = (const char *[]){ "pcm_mclk_div" },
+		.num_parents = 1,
+		.flags = (CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED),
+	},
+};
+
+static struct clk_hw *pcm_mclk_hws[] = {
+[CLKID_PCM_MCLK_MUX - CLKID_PCM_MCLK_MUX] = &pcm_mclk_mux.hw,
+[CLKID_PCM_MCLK_DIV - CLKID_PCM_MCLK_MUX] = &pcm_mclk_div.hw,
+[CLKID_PCM_MCLK_GATE - CLKID_PCM_MCLK_MUX] = &pcm_mclk_gate.hw,
+};
+
+/* cts_pcm_sclk */
+static struct clk_divider pcm_sclk_div = {
+	.reg = (void *)HHI_PCM_CLK_CNTL,
+	.shift = 16,
+	.width = 5,
+	.lock = &clk_lock,
+	.hw.init = &(struct clk_init_data){
+		.name = "pcm_sclk_div",
+		.ops = &clk_divider_ops,
+		.parent_names = (const char *[]){ "pcm_mclk_composite" },
+		.num_parents = 1,
+		.flags = (CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED),
+	},
+};
+
+static struct clk_gate pcm_sclk_gate = {
+	.reg = (void *)HHI_PCM_CLK_CNTL,
+	.bit_idx = 22,
+	.lock = &clk_lock,
+	.hw.init = &(struct clk_init_data) {
+		.name = "pcm_sclk_gate",
+		.ops = &clk_gate_ops,
+		.parent_names = (const char *[]){ "pcm_sclk_div" },
+		.num_parents = 1,
+		.flags = (CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED),
+	},
+};
+
 void amlogic_init_misc(void)
 {
 	/* cts_vdin_meas_clk */
 	vdin_meas_mux.reg = clk_base + (u64)(vdin_meas_mux.reg);
 	vdin_meas_div.reg = clk_base + (u64)(vdin_meas_div.reg);
 	vdin_meas_gate.reg = clk_base + (u64)(vdin_meas_gate.reg);
+
+	/* cts_amclk */
+	amclk_mux.reg = clk_base + (u64)(amclk_mux.reg);
+	amclk_div.reg = clk_base + (u64)(amclk_div.reg);
+	amclk_gate.reg = clk_base + (u64)(amclk_gate.reg);
+
+	/* cts_pdm */
+	pdm_mux.reg = clk_base + (u64)(pdm_mux.reg);
+	pdm_div.reg = clk_base + (u64)(pdm_div.reg);
+	pdm_gate.reg = clk_base + (u64)(pdm_gate.reg);
+
+	/* cts_clk_i958 */
+	i958_mux.reg = clk_base + (u64)(i958_mux.reg);
+	i958_div.reg = clk_base + (u64)(i958_div.reg);
+	i958_gate.reg = clk_base + (u64)(i958_gate.reg);
+
+	/* cts_pclk_mclk */
+	pcm_mclk_mux.reg = clk_base + (u64)(pcm_mclk_mux.reg);
+	pcm_mclk_div.reg = clk_base + (u64)(pcm_mclk_div.reg);
+	pcm_mclk_gate.reg = clk_base + (u64)(pcm_mclk_gate.reg);
+
+	/* cts_pclk_sclk */
+	pcm_sclk_div.reg = clk_base + (u64)(pcm_sclk_div.reg);
+	pcm_sclk_gate.reg = clk_base + (u64)(pcm_sclk_gate.reg);
 
 	clks[CLKID_VDIN_MEAS_COMP] = clk_register_composite(NULL,
 		"vdin_meas_composite",
@@ -96,6 +351,64 @@ void amlogic_init_misc(void)
 	if (IS_ERR(clks[CLKID_VDIN_MEAS_COMP]))
 		pr_err("%s: %d clk_register_composite vdin_meas_composite error\n",
 		__func__, __LINE__);
+
+	clks[CLKID_AMCLK_COMP] = clk_register_composite(NULL,
+		"amclk_composite",
+		amclk_parent_names, 4,
+		amclk_hws[CLKID_AMCLK_MUX - CLKID_AMCLK_MUX],
+		&clk_mux_ops,
+		amclk_hws[CLKID_AMCLK_DIV - CLKID_AMCLK_MUX],
+		&clk_divider_ops,
+		amclk_hws[CLKID_AMCLK_GATE - CLKID_AMCLK_MUX],
+		&clk_gate_ops, 0);
+	if (IS_ERR(clks[CLKID_AMCLK_COMP]))
+		pr_err("%s: %d clk_register_composite amclk_composite error\n",
+		__func__, __LINE__);
+
+	clks[CLKID_PDM_COMP] = clk_register_composite(NULL,
+		"pdm_composite",
+		pdm_parent_names, 4,
+		pdm_hws[CLKID_PDM_MUX - CLKID_PDM_MUX],
+		&clk_mux_ops,
+		pdm_hws[CLKID_PDM_DIV - CLKID_PDM_MUX],
+		&clk_divider_ops,
+		pdm_hws[CLKID_PDM_GATE - CLKID_PDM_MUX],
+		&clk_gate_ops, 0);
+	if (IS_ERR(clks[CLKID_PDM_COMP]))
+		pr_err("%s: %d clk_register_composite pdm_composite error\n",
+		__func__, __LINE__);
+
+	clks[CLKID_I958_COMP] = clk_register_composite(NULL,
+		"i958_composite",
+		i958_parent_names, 4,
+		i958_hws[CLKID_I958_MUX - CLKID_I958_MUX],
+		&clk_mux_ops,
+		i958_hws[CLKID_I958_DIV - CLKID_I958_MUX],
+		&clk_divider_ops,
+		i958_hws[CLKID_I958_GATE - CLKID_I958_MUX],
+		&clk_gate_ops, 0);
+	if (IS_ERR(clks[CLKID_I958_COMP]))
+		pr_err("%s: %d clk_register_composite i958_composite error\n",
+		__func__, __LINE__);
+
+	clks[CLKID_PCM_MCLK_COMP] = clk_register_composite(NULL,
+		"pcm_mclk_composite",
+		pcm_mclk_parent_names, 4,
+		pcm_mclk_hws[CLKID_PCM_MCLK_MUX - CLKID_PCM_MCLK_MUX],
+		&clk_mux_ops,
+		pcm_mclk_hws[CLKID_PCM_MCLK_DIV - CLKID_PCM_MCLK_MUX],
+		&clk_divider_ops,
+		pcm_mclk_hws[CLKID_PCM_MCLK_GATE - CLKID_PCM_MCLK_MUX],
+		&clk_gate_ops, 0);
+	if (IS_ERR(clks[CLKID_PCM_MCLK_COMP]))
+		pr_err("%s: %d clk_register_composite pcm_mclk_composite error\n",
+		__func__, __LINE__);
+
+	clks[CLKID_PCM_SCLK_DIV] = clk_register(NULL, &pcm_sclk_div.hw);
+	WARN_ON(IS_ERR(clks[CLKID_PCM_SCLK_DIV]));
+
+	clks[CLKID_PCM_SCLK_GATE] = clk_register(NULL, &pcm_sclk_gate.hw);
+	WARN_ON(IS_ERR(clks[CLKID_PCM_SCLK_GATE]));
 
 	pr_info("%s: register meson misc clk\n", __func__);
 };
