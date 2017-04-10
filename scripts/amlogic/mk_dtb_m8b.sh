ac50358ec2eb0dfcd48d3eb657db03c460d0d0bc
Peipeng Zhao <peipeng.zhao@amlogic.com>
2017-04-10 20:07:29 +0800

    dts: add m400 dts
    PD#141217: add m400 dts
    m400 is A111 development board with SLC Nand
    
    Change-Id: I47a3528df70f656c7d631d9c35cbca87dc702f97
    Signed-off-by: Peipeng Zhao <peipeng.zhao@amlogic.com>

--- scripts/amlogic/mk_dtb_m8b.sh
+++ scripts/amlogic/mk_dtb_m8b.sh
@@ -5,3 +5,7 @@ export CROSS_COMPILE=/opt/gcc-linaro-6.3.1-2017.02-x86_64_arm-linux-gnueabihf/bi
 
 make ARCH=arm meson8b_m200.dtb \
 	|| echo "Compile dtb Fail !!"
+
+
+make ARCH=arm meson8b_m400.dtb \
+	|| echo "Compile dtb Fail !!"
