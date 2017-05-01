ed82db3cddaa036ed61de87e11c34638980291ce
Troy Kisky <troy.kisky@boundarydevices.com>
2017-05-01 11:51:38 -0700

    clk-imx6q: fix disable_anatop_clocks when ddr based on PFD0 or PFD2/2
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/clk/imx/clk-imx6q.c
+++ drivers/clk/imx/clk-imx6q.c
@@ -467,15 +467,20 @@ static void __init init_ipu_clk(void __iomem *anatop_base)
 static void disable_anatop_clocks(void __iomem *anatop_base)
 {
 	unsigned int reg;
+	struct clk *parent = clk_get_parent(hws[IMX6QDL_CLK_PERIPH_PRE]->clk);
 
 	/* Make sure PLL2 PFDs 0-2 are gated */
 	reg = readl_relaxed(anatop_base + CCM_ANALOG_PFD_528);
+	reg |= PFD1_CLKGATE;				/* Disable PFD1 */
+
 	/* Cannot gate PFD2 if pll2_pfd2_396m is the parent of MMDC clock */
-	if (clk_get_parent(hws[IMX6QDL_CLK_PERIPH_PRE]->clk) ==
-	    hws[IMX6QDL_CLK_PLL2_PFD2_396M]->clk)
-		reg |= PFD0_CLKGATE | PFD1_CLKGATE;
-	else
-		reg |= PFD0_CLKGATE | PFD1_CLKGATE | PFD2_CLKGATE;
+	if (parent == hws[IMX6QDL_CLK_PLL2_PFD0_352M]->clk) {
+		reg |= PFD2_CLKGATE;			/* Disable PFD2 */
+	} else {
+		reg |= PFD0_CLKGATE;			/* Disable PFD0 */
+		if (parent == hws[IMX6QDL_CLK_PLL2_BUS]->clk)
+			reg |= PFD2_CLKGATE;		/* Disable PFD2 */
+	}
 	writel_relaxed(reg, anatop_base + CCM_ANALOG_PFD_528);
 
 	/* Make sure PLL3 PFDs 0-3 are gated */
