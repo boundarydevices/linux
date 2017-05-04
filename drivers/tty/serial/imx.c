9ddc41e2cc7f847048bef6acb61e2c4f844fbde3
Troy Kisky <troy.kisky@boundarydevices.com>
2017-05-04 11:48:08 -0700

    serial: tty: imx: fix problem with MLK-11782 tty: serial: imx: align tx path with kernel 3.14
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -550,14 +550,19 @@ static void dma_tx_callback(void *data)
 	struct scatterlist *sgl = &sport->tx_sgl[0];
 	struct circ_buf *xmit = &sport->port.state->xmit;
 	unsigned long flags;
+	unsigned long temp;
 	unsigned pending;
 
 	dma_unmap_sg(sport->port.dev, sgl, sport->dma_tx_nents, DMA_TO_DEVICE);
 
+	spin_lock_irqsave(&sport->port.lock, flags);
+	temp = readl(sport->port.membase + UCR1);
+	temp &= ~UCR1_TDMAEN;
+	writel(temp, sport->port.membase + UCR1);
+
 	sport->dma_is_txing = 0;
 
 	/* update the stat */
-	spin_lock_irqsave(&sport->port.lock, flags);
 	xmit->tail = (xmit->tail + sport->tx_bytes) & (UART_XMIT_SIZE - 1);
 	sport->port.icount.tx += sport->tx_bytes;
 	pending = uart_circ_chars_pending(xmit);
