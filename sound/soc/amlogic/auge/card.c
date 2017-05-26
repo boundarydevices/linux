b3a48d09e068e619f58d031bdf0a950b2df5e4e1
Peipeng Zhao <peipeng.zhao@amlogic.com>
2017-05-26 17:43:37 +0800

    dts: S400: change tdm-b for audio capture
    PD#145484: audio: support snd_soc_dai_set_sysclk for mutli codec
    
    Change-Id: I3a2e24e1321e9251029e0abe79e97a9852a68450
    Signed-off-by: Peipeng Zhao <peipeng.zhao@amlogic.com>

--- sound/soc/amlogic/auge/card.c
+++ sound/soc/amlogic/auge/card.c
@@ -151,14 +151,14 @@ static int aml_card_hw_params(struct snd_pcm_substream *substream,
 				      struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *codec_dai = rtd->codec_dai;
 	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
 	struct aml_card_data *priv = snd_soc_card_get_drvdata(rtd->card);
 	struct aml_dai_props *dai_props =
 		aml_priv_to_props(priv, rtd->num);
 	unsigned int mclk, mclk_fs = 0;
-	int ret = 0;
+	int i = 0, ret = 0;
 
+	pr_info("%s ..numcodec:%d\n", __func__, rtd->num_codecs);
 	if (priv->mclk_fs)
 		mclk_fs = priv->mclk_fs;
 	else if (dai_props->mclk_fs)
@@ -166,11 +166,16 @@ static int aml_card_hw_params(struct snd_pcm_substream *substream,
 
 	if (mclk_fs) {
 		mclk = params_rate(params) * mclk_fs;
-		ret = snd_soc_dai_set_sysclk(codec_dai, 0, mclk,
-					     SND_SOC_CLOCK_IN);
-		if (ret && ret != -ENOTSUPP)
-			goto err;
 
+		for (i = 0; i < rtd->num_codecs; i++) {
+			struct snd_soc_dai *codec_dai = rtd->codec_dais[i];
+
+			ret = snd_soc_dai_set_sysclk(codec_dai, 0, mclk,
+				SND_SOC_CLOCK_IN);
+
+			if (ret && ret != -ENOTSUPP)
+				goto err;
+		}
 		ret = snd_soc_dai_set_sysclk(cpu_dai, 0, mclk,
 					     SND_SOC_CLOCK_OUT);
 		if (ret && ret != -ENOTSUPP)
