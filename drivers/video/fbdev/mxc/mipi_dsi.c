8ad201b3d9fbebfabb760dd98f3ecb11d94c9af5
Troy Kisky <troy.kisky@boundarydevices.com>
2017-06-13 17:18:55 -0700

    video: mipi_dsi: fix MIPI_DSI_TMR_LINE_CFG settings
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/video/fbdev/mxc/mipi_dsi.c
+++ drivers/video/fbdev/mxc/mipi_dsi.c
@@ -35,8 +35,6 @@
 #include "mipi_dsi.h"
 
 #define DISPDRV_MIPI			"mipi_dsi"
-#define ROUND_UP(x)			((x)+1)
-#define NS2PS_RATIO			(1000)
 #define NUMBER_OF_CHUNKS		(0x8)
 #define NULL_PKT_SIZE			(0x8)
 #define PHY_BTA_MAXTIME			(0xd00)
@@ -334,6 +332,7 @@ static void mipi_dsi_dphy_init(struct mipi_dsi_info *mipi_dsi,
 static void mipi_dsi_enable_controller(struct mipi_dsi_info *mipi_dsi,
 				bool init)
 {
+	uint64_t	val64;
 	u32		val = 0;
 	u32		lane_byte_clk_period;
 	struct  fb_videomode *mode = mipi_dsi->mode;
@@ -379,19 +378,40 @@ static void mipi_dsi_enable_controller(struct mipi_dsi_info *mipi_dsi,
 		mipi_dsi_write_register(mipi_dsi, MIPI_DSI_CMD_MODE_CFG,
 				MIPI_DSI_CMD_MODE_CFG_EN_LOWPOWER);
 
-		 /* mipi lane byte clk period in ns unit */
-		lane_byte_clk_period = NS2PS_RATIO /
-				(lcd_config->max_phy_clk / BITS_PER_BYTE);
-		val  = ROUND_UP(mode->hsync_len * mode->pixclock /
-				NS2PS_RATIO / lane_byte_clk_period)
-				<< DSI_TME_LINE_CFG_HSA_TIME_SHIFT;
-		val |= ROUND_UP(mode->left_margin * mode->pixclock /
-				NS2PS_RATIO / lane_byte_clk_period)
-				<< DSI_TME_LINE_CFG_HBP_TIME_SHIFT;
-		val |= ROUND_UP((mode->left_margin + mode->right_margin +
+		 /* mipi lane byte clk period in 1/256 ps unit */
+		val64 = (8000000ULL * 256) + lcd_config->max_phy_clk - 1;
+		do_div(val64, lcd_config->max_phy_clk);
+		lane_byte_clk_period = (u32)val64;
+
+		val64 = (uint64_t)mode->hsync_len * mode->pixclock
+				* 256 + lane_byte_clk_period - 1;
+		do_div(val64, lane_byte_clk_period);
+		if (val64 >= (1 << 9)) {
+			dev_err(&mipi_dsi->pdev->dev, "hsync(%d) too large\n", (u32)val64);
+			val64 = (1 << 9) - 1;
+		}
+		val = (u32)val64 << DSI_TME_LINE_CFG_HSA_TIME_SHIFT;
+
+		val64 = (uint64_t)mode->left_margin * mode->pixclock
+				* 256 + lane_byte_clk_period - 1;
+		do_div(val64, lane_byte_clk_period);
+		if (val64 >= (1 << 9)) {
+			dev_err(&mipi_dsi->pdev->dev, "left_margin(%d) too large\n", (u32)val64);
+			val64 = (1 << 9) - 1;
+		}
+		val |= (u32)val64 << DSI_TME_LINE_CFG_HBP_TIME_SHIFT;
+
+		val64 = (uint64_t)(mode->left_margin + mode->right_margin +
 				mode->hsync_len + mode->xres) * mode->pixclock
-				/ NS2PS_RATIO / lane_byte_clk_period)
-				<< DSI_TME_LINE_CFG_HLINE_TIME_SHIFT;
+				* 256 + lane_byte_clk_period - 1;
+		do_div(val64, lane_byte_clk_period);
+		if (val64 >= (1 << 14)) {
+			dev_err(&mipi_dsi->pdev->dev, "total(%d) too large\n", (u32)val64);
+			val64 = (1 << 14) - 1;
+		}
+		val |= (u32)val64 << DSI_TME_LINE_CFG_HLINE_TIME_SHIFT;
+
+		pr_debug("%s:LINE_CFG=%x\n", __func__, val);
 		mipi_dsi_write_register(mipi_dsi, MIPI_DSI_TMR_LINE_CFG, val);
 
 		val = ((mode->vsync_len & DSI_VTIMING_CFG_VSA_LINES_MASK)
@@ -402,6 +422,7 @@ static void mipi_dsi_enable_controller(struct mipi_dsi_info *mipi_dsi,
 				<< DSI_VTIMING_CFG_VFP_LINES_SHIFT);
 		val |= ((mode->yres & DSI_VTIMING_CFG_V_ACT_LINES_MASK)
 				<< DSI_VTIMING_CFG_V_ACT_LINES_SHIFT);
+		pr_debug("%s:VTIMING_CFG=%x\n", __func__, val);
 		mipi_dsi_write_register(mipi_dsi, MIPI_DSI_VTIMING_CFG, val);
 
 		val = ((PHY_BTA_MAXTIME & DSI_PHY_TMR_CFG_BTA_TIME_MASK)
