4ffc517c7714f7d8c124ce6c0593f2081aafed90
Shengjiu Wang <shengjiu.wang@freescale.com>
2017-06-15 13:07:20 +0800

    MLK-15068: ASoC: fsl_ssi: fix the noise issue with S20_3LE Mono bitsream
    In master mode, clock of S20_3LE mono bistream is calculated by formula
    "2 * params_width * params_rate", and this clock can't be divided from
    clock soure, so switch to use the "2 * params_physical_width * params_rate"
    formula to fix this issue.
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@freescale.com>
    (cherry picked from commit 52a91e870b8cc95cbceeda205761134e25626863)
    
    Vipul: while rebasing this patch on v4.19, get merge conflicts.
    To fix these conflicts, use commit
    b0a7043d5c2c ("ASoC: fsl_ssi: Caculate bit clock rate using slot number and width")
    (Vipul: Fixed merge conflicts)
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>
    Signed-off-by: Srikanth Krishnakar <Srikanth_Krishnakar@mentor.com>

--- sound/soc/fsl/fsl_ssi.c
+++ sound/soc/fsl/fsl_ssi.c
@@ -690,6 +690,7 @@ static int fsl_ssi_set_bclk(struct snd_pcm_substream *substream,
 	unsigned int freq;
 	bool baudclk_is_used;
 	int ret;
+	snd_pcm_format_t sample_format = params_format(hw_params);
 
 	/* Override slots and slot_width if being specifically set... */
 	if (ssi->slots)
@@ -700,6 +701,15 @@ static int fsl_ssi_set_bclk(struct snd_pcm_substream *substream,
 
 	/* Generate bit clock based on the slot number and slot width */
 	freq = slots * slot_width * params_rate(hw_params);
+
+	if (params_channels(hw_params) == 1) {
+		freq = 2 * params_width(hw_params) *
+				params_rate(hw_params);
+
+		if (sample_format == SNDRV_PCM_FORMAT_S20_3LE)
+			freq = 2 * params_physical_width(hw_params) *
+						params_rate(hw_params);
+	}
 
 	/* Don't apply it to any non-baudclk circumstance */
 	if (IS_ERR(ssi->baudclk))
