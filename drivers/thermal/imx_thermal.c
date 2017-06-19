282722ad136956bebd8f5f0c6a053b739cecd075
Bai Ping <ping.bai@nxp.com>
2017-06-19 17:01:23 +0800

    MLK-15075 thermal: imx: fix temp read failure on i.mx7d
    On i.MX7D, if the system enter LPSR mode, the tempmon module
    will be power down, so the regiter's value is lost, so we need
    to save the registers before suspend and restore the register after
    resume back.
    
    Signed-off-by: Bai Ping <ping.bai@nxp.com>
    [Arul: Fix merge conflicts]
    Signed-off-by: Arulpandiyan Vadivel <arulpandiyan_vadivel@mentor.com>

--- drivers/thermal/imx_thermal.c
+++ drivers/thermal/imx_thermal.c
@@ -240,6 +240,7 @@ struct imx_thermal_data {
 
 static struct imx_thermal_data *imx_thermal_data;
 static int skip_finish_check;
+static u32 imx7_lpsr_save[2];
 
 static void imx_set_panic_temp(struct imx_thermal_data *data,
 			       int panic_temp)
@@ -1033,6 +1034,16 @@ static int imx_thermal_suspend(struct device *dev)
 		     data->socdata->measure_temp_mask);
 	regmap_write(map, data->socdata->sensor_ctrl + REG_SET,
 		     data->socdata->power_down_mask);
+
+	/*
+	 * Save the temp sensor registers of i.MX7D as the tempmon
+	 * will lost power in LPSR mode
+	 */
+	if (data->socdata->version == TEMPMON_IMX7) {
+		regmap_read(map, data->socdata->sensor_ctrl, &imx7_lpsr_save[0]);
+		regmap_read(map, data->socdata->high_alarm_ctrl, &imx7_lpsr_save[1]);
+	}
+
 	data->mode = THERMAL_DEVICE_DISABLED;
 	clk_disable_unprepare(data->thermal_clk);
 
@@ -1048,6 +1059,16 @@ static int imx_thermal_resume(struct device *dev)
 	ret = clk_prepare_enable(data->thermal_clk);
 	if (ret)
 		return ret;
+
+	/*
+	 * restore the temp sensor registers of i.MX7D as the tempmon
+	 * will lost power in LPSR mode
+	 */
+	if (data->socdata->version == TEMPMON_IMX7) {
+		regmap_write(map, data->socdata->sensor_ctrl, imx7_lpsr_save[0]);
+		regmap_write(map, data->socdata->high_alarm_ctrl, imx7_lpsr_save[1]);
+	}
+
 	/* Enabled thermal sensor after resume */
 	regmap_write(map, data->socdata->sensor_ctrl + REG_CLR,
 		     data->socdata->power_down_mask);
