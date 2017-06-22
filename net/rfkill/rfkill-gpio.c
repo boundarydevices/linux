56d7c5e03a549d2a29177e98797d0c08d48f9c28
Troy Kisky <troy.kisky@boundarydevices.com>
2017-06-21 17:48:50 -0700

    rfkill-gpio: add pulse-on-gpio support
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- net/rfkill/rfkill-gpio.c
+++ net/rfkill/rfkill-gpio.c
@@ -16,6 +16,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+#include <linux/delay.h>
 #include <linux/gpio.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
@@ -34,6 +35,8 @@ struct rfkill_gpio_data {
 	enum rfkill_type	type;
 	struct gpio_desc	*reset_gpio;
 	struct gpio_desc	*shutdown_gpio;
+	struct gpio_desc	*pulse_on_gpio;
+	unsigned		pulse_duration;
 
 	struct rfkill		*rfkill_dev;
 	struct clk		*clk;
@@ -54,6 +57,12 @@ static int rfkill_gpio_set_power(void *data, bool blocked)
 	} else {
 		gpiod_set_value_cansleep(rfkill->reset_gpio, 0);
 		gpiod_set_value_cansleep(rfkill->shutdown_gpio, 0);
+		if (rfkill->pulse_on_gpio) {
+			gpiod_set_value_cansleep(rfkill->pulse_on_gpio, 1);
+			msleep(rfkill->pulse_duration);
+			pr_info("%s:msleep %d\n", __func__, rfkill->pulse_duration);
+			gpiod_set_value_cansleep(rfkill->pulse_on_gpio, 0);
+		}
 	}
 
 	if (blocked && !IS_ERR(rfkill->clk) && rfkill->clk_enabled)
@@ -153,6 +162,13 @@ static int rfkill_gpio_probe(struct platform_device *pdev)
 		return PTR_ERR(gpio);
 
 	rfkill->shutdown_gpio = gpio;
+
+	gpio = devm_gpiod_get(&pdev->dev, "pulse-on", GPIOD_OUT_LOW);
+	if (!IS_ERR(gpio))
+		rfkill->pulse_on_gpio = gpio;
+
+	ret = of_property_read_u32(pdev->dev.of_node, "pulse-duration",
+			&rfkill->pulse_duration);
 
 	/* Make sure at-least one GPIO is defined for this instance */
 	if (!rfkill->reset_gpio && !rfkill->shutdown_gpio) {
