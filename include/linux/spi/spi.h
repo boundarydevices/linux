c28be5842ab2572e30df6f72ed1158ff5430c2df
Troy Kisky <troy.kisky@boundarydevices.com>
2017-06-27 16:31:39 -0700

    spi: add pinctrl/max_freq for read
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- include/linux/spi/spi.h
+++ include/linux/spi/spi.h
@@ -168,6 +168,7 @@ struct spi_device {
 	struct spi_controller	*controller;
 	struct spi_controller	*master;	/* compatibility layer */
 	u32			max_speed_hz;
+	u32			max_read_speed_hz;
 	u8			chip_select;
 	u8			bits_per_word;
 	bool			rt;
@@ -504,6 +505,7 @@ struct spi_controller {
 	/* limits on transfer speed */
 	u32			min_speed_hz;
 	u32			max_speed_hz;
+	u32			max_read_speed_hz;
 
 	/* other constraints relevant to this driver */
 	u16			flags;
@@ -672,6 +674,9 @@ struct spi_controller {
 
 	/* Interrupt enable state during PTP system timestamping */
 	unsigned long		irq_flags;
+	struct pinctrl		*pinctrl;
+	struct pinctrl_state	*pins_write;
+	struct pinctrl_state	*pins_read;
 };
 
 static inline void *spi_controller_get_devdata(struct spi_controller *ctlr)
@@ -948,6 +953,7 @@ struct spi_transfer {
 #define	SPI_NBITS_SINGLE	0x01 /* 1bit transfer */
 #define	SPI_NBITS_DUAL		0x02 /* 2bits transfer */
 #define	SPI_NBITS_QUAD		0x04 /* 4bits transfer */
+	unsigned	read_setup:1;
 	u8		bits_per_word;
 	struct spi_delay	delay;
 	struct spi_delay	cs_change_delay;
