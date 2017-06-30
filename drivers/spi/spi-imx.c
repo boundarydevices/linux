f7b92bbb564553486802b7ffe60564828db28a9c
Troy Kisky <troy.kisky@boundarydevices.com>
2017-06-30 14:49:06 -0700

    spi-imx: call dmaengine_slave_config in probe to prevent dma error
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/spi/spi-imx.c
+++ drivers/spi/spi-imx.c
@@ -104,6 +104,7 @@ struct spi_imx_data {
 	unsigned int spi_bus_clk;
 
 	unsigned int bits_per_word;
+	unsigned int prev_width;
 	unsigned int spi_drctl;
 
 	unsigned int count, remainder;
@@ -1315,6 +1316,8 @@ static int spi_imx_setupxfer(struct spi_device *spi,
 				 struct spi_transfer *t)
 {
 	struct spi_imx_data *spi_imx = spi_controller_get_devdata(spi->controller);
+	int bits_per_word;
+	int width;
 
 	if (!t)
 		return 0;
@@ -1337,9 +1340,10 @@ static int spi_imx_setupxfer(struct spi_device *spi,
 		spi_imx->usedma = 0;
 
 	if (spi_imx->slave_mode && spi_imx->usedma)
-		spi_imx->bits_per_word = 32;
+		bits_per_word = 32;
 	else
-		spi_imx->bits_per_word = t->bits_per_word;
+		bits_per_word = t->bits_per_word;
+	spi_imx->bits_per_word = bits_per_word;
 
 	/*
 	 * Initialize the functions for transfer. To transfer non byte-aligned
@@ -1348,23 +1352,28 @@ static int spi_imx_setupxfer(struct spi_device *spi,
 	 */
 	if (spi_imx->dynamic_burst && !spi_imx->slave_mode &&
 	    !(spi->mode & SPI_CS_WORD) &&
-	    (spi_imx->bits_per_word == 8 ||
-	    spi_imx->bits_per_word == 16 ||
-	    spi_imx->bits_per_word == 32)) {
+	    (bits_per_word == 8 ||
+	    bits_per_word == 16 ||
+	    bits_per_word == 32)) {
 		spi_imx->rx = spi_imx_buf_rx_swap;
 		spi_imx->tx = spi_imx_buf_tx_swap;
 
 	} else {
-		if (spi_imx->bits_per_word <= 8) {
+		if (bits_per_word <= 8) {
 			spi_imx->rx = spi_imx_buf_rx_u8;
 			spi_imx->tx = spi_imx_buf_tx_u8;
-		} else if (spi_imx->bits_per_word <= 16) {
+			width = DMA_SLAVE_BUSWIDTH_1_BYTE;
+		} else if (bits_per_word <= 16) {
 			spi_imx->rx = spi_imx_buf_rx_u16;
 			spi_imx->tx = spi_imx_buf_tx_u16;
+			width = DMA_SLAVE_BUSWIDTH_2_BYTES;
 		} else {
 			spi_imx->rx = spi_imx_buf_rx_u32;
 			spi_imx->tx = spi_imx_buf_tx_u32;
+			width = DMA_SLAVE_BUSWIDTH_4_BYTES;
 		}
+		spi_imx->rx_config.src_addr_width = width;
+		spi_imx->tx_config.dst_addr_width = width;
 	}
 
 	spi_imx->rx_only = ((t->tx_buf == NULL)
@@ -1428,6 +1437,18 @@ static int spi_imx_sdma_init(struct device *dev, struct spi_imx_data *spi_imx,
 	spi_imx->controller->flags = SPI_CONTROLLER_MUST_RX |
 					 SPI_CONTROLLER_MUST_TX;
 
+
+	/*
+	 * I have no idea why this is needed, but a dma error
+	 * happens on 1st dma without it
+	 */
+	spi_imx->tx_config.dst_addr_width = DMA_SLAVE_BUSWIDTH_1_BYTE;
+	ret = dmaengine_slave_config(controller->dma_tx,
+			&spi_imx->tx_config);
+	if (ret) {
+		dev_err(spi_imx->dev, "error(%d) in TX dma configuration.\n", ret);
+		goto err;
+	}
 	return 0;
 err:
 	spi_imx_sdma_exit(spi_imx);
@@ -1548,11 +1569,12 @@ static int spi_imx_dma_transfer(struct spi_imx_data *spi_imx,
 		timeout = wait_for_completion_timeout(&spi_imx->dma_tx_completion,
 							transfer_timeout);
 		if (!timeout) {
-			dev_err(spi_imx->dev, "I/O Error in DMA TX:%x %x %x %x\n",
+			dev_err(spi_imx->dev, "I/O Error in DMA TX:%x %x %x %x %x\n",
 				transfer->len,
 				readl(spi_imx->base + MX51_ECSPI_STAT),
 				readl(spi_imx->base + MX51_ECSPI_TESTREG),
-				readl(spi_imx->base + MX51_ECSPI_DMA));
+				readl(spi_imx->base + MX51_ECSPI_DMA),
+				readl(spi_imx->base + MX51_ECSPI_CTRL));
 			dmaengine_terminate_all(controller->dma_tx);
 			dmaengine_terminate_all(controller->dma_rx);
 			return -ETIMEDOUT;
