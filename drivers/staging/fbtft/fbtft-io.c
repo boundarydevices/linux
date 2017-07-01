85046978c05dc1c748c357443722c81d5e819b13
Troy Kisky <troy.kisky@boundarydevices.com>
2017-07-01 13:35:33 -0700

    fbtft-bus: optional workqueues, bits_per_word
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/staging/fbtft/fbtft-io.c
+++ drivers/staging/fbtft/fbtft-io.c
@@ -22,8 +22,8 @@ int fbtft_write_spi(struct fbtft_par *par, void *buf, size_t len)
 	}
 
 	spi_message_init(&m);
-	if (par->txbuf.dma && buf == par->txbuf.buf) {
-		t.tx_dma = par->txbuf.dma;
+	if (par->txbuf[0].dma && buf == par->txbuf[0].buf) {
+		t.tx_dma = par->txbuf[0].dma;
 		m.is_dma_mapped = 1;
 	}
 	spi_message_add_tail(&t, &m);
