8602923f7a80a262060ae8de718de47582e19f4b
Adriana Reus <adriana.reus@nxp.com>
2017-07-07 11:47:25 +0300

    MLK-15936 clk: imx7d: Fix OCRAM_CLK parent clock
    The parent of OCRAM_CLK should be axi_main_root_clk
    and not axi_post_div.
    
    before:
    
    axi_src                         1       1       332307692       0 0
      axi_cg                        1       1       332307692       0 0
        axi_pre_div                 1       1       332307692       0 0
          axi_post_div              2       2       332307692       0 0
            ocram_clk               1       1       332307692       0 0
            main_axi_root_clk       1       1       332307692       0 0
              pxp_axi_clk           0       0       332307692       0 0
    
    after:
    
    axi_src                         1       1       332307692       0 0
      axi_cg                        1       1       332307692       0 0
        axi_pre_div                 1       1       332307692       0 0
          axi_post_div              1       1       332307692       0 0
            main_axi_root_clk       2       2       332307692       0 0
              pxp_axi_clk           0       0       332307692       0 0
              ocram_clk             1       1       332307692       0 0
    
    Signed-off-by: Adriana Reus <adriana.reus@nxp.com>

--- drivers/clk/imx/clk-imx7d.c
+++ drivers/clk/imx/clk-imx7d.c
@@ -791,7 +791,7 @@ static void __init imx7d_clocks_init(struct device_node *ccm_node)
 	clks[IMX7D_MAIN_AXI_ROOT_CLK] = imx_clk_gate4("main_axi_root_clk", "axi_post_div", base + 0x4040, 0);
 	clks[IMX7D_DISP_AXI_ROOT_CLK] = imx_clk_gate4("disp_axi_root_clk", "disp_axi_post_div", base + 0x4050, 0);
 	clks[IMX7D_ENET_AXI_ROOT_CLK] = imx_clk_gate4("enet_axi_root_clk", "enet_axi_post_div", base + 0x4060, 0);
-	clks[IMX7D_OCRAM_CLK] = imx_clk_gate4("ocram_clk", "axi_post_div", base + 0x4110, 0);
+	clks[IMX7D_OCRAM_CLK] = imx_clk_gate4("ocram_clk", "main_axi_root_clk", base + 0x4110, 0);
 	clks[IMX7D_OCRAM_S_CLK] = imx_clk_gate4("ocram_s_clk", "ahb_root_clk", base + 0x4120, 0);
 	clks[IMX7D_CAAM_CLK] = imx_clk_gate4("caam_clk", "ipg_root_clk", base + 0x4240, 0);
 	clks[IMX7D_DRAM_ROOT_CLK] = imx_clk_gate4("dram_root_clk", "dram_post_div", base + 0x4130, 0);
