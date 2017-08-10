6fc4a91bfb85da56885e3e1255cc359b8d6727f7
Robin Gong <yibin.gong@nxp.com>
2017-08-10 17:03:45 +0800

    MLK-16202-01 driver: regulator: add enable/disable for switch for pfuze100
    Add enable/disable support for switch regulator on pfuze100.
    
    Signed-off-by: Robin Gong <yibin.gong@nxp.com>
    Signed-off-by: Bai Ping <ping.bai@nxp.com>
    (Vipul: Fixed merge conflicts)
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>

--- drivers/regulator/pfuze100-regulator.c
+++ drivers/regulator/pfuze100-regulator.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0+
 //
 // Copyright (C) 2011-2013 Freescale Semiconductor, Inc. All Rights Reserved.
+// Copyright 2017 NXP
 
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -178,6 +179,9 @@ static const struct regulator_ops pfuze100_fixed_regulator_ops = {
 };
 
 static const struct regulator_ops pfuze100_sw_regulator_ops = {
+	.enable = regulator_enable_regmap,
+	.disable = regulator_disable_regmap,
+	.is_enabled = regulator_is_enabled_regmap,
 	.list_voltage = regulator_list_voltage_linear,
 	.set_voltage_sel = regulator_set_voltage_sel_regmap,
 	.get_voltage_sel = regulator_get_voltage_sel_regmap,
@@ -236,6 +240,8 @@ static const struct regulator_ops pfuze100_swb_regulator_ops = {
 			.vsel_reg = (base) + PFUZE100_VOL_OFFSET,	\
 			.vsel_mask = 0x3f,	\
 			.enable_reg = (base) + PFUZE100_MODE_OFFSET,	\
+			.enable_val = 0xc,	\
+			.disable_val = 0x0,	\
 			.enable_mask = 0xf,	\
 		},	\
 		.stby_reg = (base) + PFUZE100_STANDBY_OFFSET,	\
