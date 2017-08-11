49216eb801168356eb066f0e8d2e996c4bab1fe9
Fancy Fang <chen.fang@nxp.com>
2017-08-11 18:44:03 +0800

    MLK-16197-2 video: fbdev: dcss: init dcss irqs earlier
    Initialize the DCSS interrupt system earlier to make
    it enabled before any cfifo commit.
    
    Signed-off-by: Fancy Fang <chen.fang@nxp.com>
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>

--- drivers/video/fbdev/mxc/imx_dcss.c
+++ drivers/video/fbdev/mxc/imx_dcss.c
@@ -3247,6 +3247,8 @@ static int dcss_probe(struct platform_device *pdev)
 	/* Pull DCSS out of resets */
 	writel(0xffffffff, info->blkctl_base + 0x0);
 
+	dcss_interrupts_init(info);
+
 	/* register channel 0: graphic */
 	ret = dcss_register_one_ch(0, info);
 	if (ret)
@@ -3256,8 +3258,6 @@ static int dcss_probe(struct platform_device *pdev)
 	ret = dcss_register_one_fb(&info->chans.chan_info[0]);
 	if (ret)
 		goto unregister_ch0;
-
-	dcss_interrupts_init(info);
 
 	/* enable encoder if exists */
 	dcss_enable_encoder(info);
