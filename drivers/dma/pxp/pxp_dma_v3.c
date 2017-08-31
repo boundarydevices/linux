085d99e7bf721318994d5f0851453381fb7b52d3
Guoniu.Zhou <guoniu.zhou@nxp.com>
2017-08-31 19:00:47 +0800

    MLK-16348: PxP: fix background issue
    There was no pxp background register setting, so the
    background we see always black.
    
    Signed-off-by: Guoniu.Zhou <guoniu.zhou@nxp.com>
    Reviewed-by: Robby Cai <robby.cai@nxp.com>

--- drivers/dma/pxp/pxp_dma_v3.c
+++ drivers/dma/pxp/pxp_dma_v3.c
@@ -3381,6 +3381,9 @@ config:
 		break;
 	}
 
+	__raw_writel(proc_data->bgcolor,
+		     pxp->base + HW_PXP_PS_BACKGROUND_0);
+
 	if (proc_data->lut_transform && pxp_is_v3(pxp))
 		set_mux(&path_ctrl0);
 
