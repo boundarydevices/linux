0c58413ecba734f1eb89a9f8f495a95bf77f6534
Guoniu.Zhou <guoniu.zhou@nxp.com>
2017-09-05 17:58:02 +0800

    MLK-16374-2: PxP: add new format support for as and out buffer
    1) add PXP_PIX_FMT_BGRA32 format support for AS buffer
    2) add PXP_PIX_FMT_BGRA32 format support for OUT buffer
    
    Signed-off-by: Guoniu.Zhou <guoniu.zhou@nxp.com>
    Reviewed-by: Fancy Fang <chen.fang@nxp.com>
    (cherry picked from commit 4eeefd54cdbb8d1d3ffd64bd444ea7b759101a35)
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>

--- drivers/dma/pxp/pxp_dma_v3.c
+++ drivers/dma/pxp/pxp_dma_v3.c
@@ -1292,6 +1292,7 @@ static uint32_t pxp_parse_as_fmt(uint32_t format)
 	uint32_t fmt_ctrl;
 
 	switch (format) {
+	case PXP_PIX_FMT_BGRA32:
 	case PXP_PIX_FMT_ARGB32:
 		fmt_ctrl = BV_PXP_AS_CTRL_FORMAT__ARGB8888;
 		break;
@@ -1336,6 +1337,7 @@ static uint32_t pxp_parse_out_fmt(uint32_t format)
 	uint32_t fmt_ctrl;
 
 	switch (format) {
+	case PXP_PIX_FMT_BGRA32:
 	case PXP_PIX_FMT_ARGB32:
 		fmt_ctrl = BV_PXP_OUT_CTRL_FORMAT__ARGB8888;
 		break;
@@ -1889,6 +1891,7 @@ static bool fmt_as_support(uint32_t format)
 	case PXP_PIX_FMT_ARGB32:
 	case PXP_PIX_FMT_RGBA32:
 	case PXP_PIX_FMT_XRGB32:
+	case PXP_PIX_FMT_BGRA32:
 	case PXP_PIX_FMT_ARGB555:
 	case PXP_PIX_FMT_ARGB444:
 	case PXP_PIX_FMT_RGBA555:
@@ -1907,6 +1910,7 @@ static bool fmt_out_support(uint32_t format)
 	switch (format) {
 	case PXP_PIX_FMT_ARGB32:
 	case PXP_PIX_FMT_XRGB32:
+	case PXP_PIX_FMT_BGRA32:
 	case PXP_PIX_FMT_RGB24:
 	case PXP_PIX_FMT_ARGB555:
 	case PXP_PIX_FMT_ARGB444:
