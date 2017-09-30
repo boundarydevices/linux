486c9721db59f0c3383b7bf82a562abd36e4eef9
Alagu Sankar <alagusankar@silex-india.com>
2017-09-30 23:07:41 +0530

    ath10k_sdio: reduce transmit msdu count
    Reduce the transmit MSDU count for SDIO, to match with the descriptors
    as used by the firmware. This also acts as a high watermark level for
    transmit. Too many packets to the firmware results in transmit overflow
    interrupt.
    
    Signed-off-by: Alagu Sankar <alagusankar@silex-india.com>

--- drivers/net/wireless/ath/ath10k/hw.h
+++ drivers/net/wireless/ath/ath10k/hw.h
@@ -582,6 +582,7 @@ ath10k_rx_desc_get_l3_pad_bytes(struct ath10k_hw_params *hw,
 #define TARGET_TLV_NUM_TDLS_VDEVS		1
 #define TARGET_TLV_NUM_TIDS			((TARGET_TLV_NUM_PEERS) * 2)
 #define TARGET_TLV_NUM_MSDU_DESC		(1024 + 32)
+#define TARGET_TLV_NUM_MSDU_DESC_HL		64
 #define TARGET_TLV_NUM_WOW_PATTERNS		22
 
 /* Target specific defines for QCA9377 high latency firmware */
