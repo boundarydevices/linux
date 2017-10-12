2335009e9f4c1f54da2e12757a9575384ff4347c
Troy Kisky <troy.kisky@boundarydevices.com>
2017-10-12 14:16:15 -0700

    tty: serial: imx: wait until a character is about to be transmitted before enabling the RS485 transmitter
    For some reason, imx_start_tx is called when there is nothing to send.
    Stop using this to enable the transmitter as it interfers with
    half-duplex reception.
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -589,6 +589,11 @@ static inline int imx_uart_transmit_buffer(struct imx_port *sport)
 	struct circ_buf *xmit = &sport->port.state->xmit;
 
 	if (sport->port.x_char) {
+		if (!sport->txing && sport->txen_mask) {
+			imx_set_gpios(sport, sport->txen_mask, sport->txen_levels, 1);
+			sport->txing = 1;
+		}
+
 		/* Send next char */
 		imx_uart_writel(sport, sport->port.x_char, URTX0);
 		sport->port.icount.tx++;
@@ -599,6 +604,12 @@ static inline int imx_uart_transmit_buffer(struct imx_port *sport)
 		imx_uart_stop_tx(&sport->port);
 		return 0;
 	}
+
+	if (!sport->txing && sport->txen_mask) {
+		imx_set_gpios(sport, sport->txen_mask, sport->txen_levels, 1);
+		sport->txing = 1;
+	}
+
 	if (sport->dma_is_enabled) {
 		u32 ucr1, ucr4;
 		/*
@@ -696,7 +707,7 @@ static void imx_uart_dma_tx(struct imx_port *sport)
 
 	sport->tx_bytes = uart_circ_chars_pending(xmit);
 
-	if (uart_tx_stopped(&sport->port)) {
+	if (!sport->tx_bytes || uart_tx_stopped(&sport->port)) {
 		sport->dma_is_txing = 0;
 		if ((sport->port.rs485.flags & SER_RS485_ENABLED) && sport->txing) {
 			ucr4 = imx_uart_readl(sport, UCR4);
@@ -705,6 +716,11 @@ static void imx_uart_dma_tx(struct imx_port *sport)
 		}
 		return;
 	}
+	if (!sport->txing && sport->txen_mask) {
+		imx_set_gpios(sport, sport->txen_mask, sport->txen_levels, 1);
+		sport->txing = 1;
+	}
+
 	if (xmit->tail < xmit->head || xmit->head == 0) {
 		sport->dma_tx_nents = 1;
 		sg_init_one(sgl, xmit->buf + xmit->tail, sport->tx_bytes);
@@ -767,10 +783,6 @@ static void imx_uart_start_tx(struct uart_port *port)
 		imx_uart_writel(sport, ucr1, UCR1);
 	}
 
-	if (!sport->txing && sport->txen_mask) {
-		imx_set_gpios(sport, sport->txen_mask, sport->txen_levels, 1);
-		sport->txing = 1;
-	}
 	/*
 	 * We cannot simply do nothing here if sport->tx_state == SEND already
 	 * because UCR1_TXMPTYEN might already have been cleared in
