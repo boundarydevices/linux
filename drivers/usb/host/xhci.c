7e52cd32ea613069ba7f5cb0e60ff754963c421b
Peter Chen <peter.chen@nxp.com>
2017-11-09 16:58:40 +0800

    MLK-16735 usb: host: add XHCI_CDNS_HOST flag
    The NXP Cadence XHCI host has the same issue with Intel's,
    it is triggered by reboot test, the test case is described
    at this jira ticket.
    
    BuildInfo:
    - SCFW 8dcff26, IMX-MKIMAGE ea027c4b, ATF
    - U-Boot 2017.03-imx_v2017.03_4.9.51_imx8_beta1+g6dc7b0f
    
    Acked-by: Jun Li <jun.li@nxp.com>
    Signed-off-by: Peter Chen <peter.chen@nxp.com>
    (cherry picked from commit 5e353132931b9dc6e05f6bea1281ae35dd6a59d8)

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -197,7 +197,7 @@ int xhci_reset(struct xhci_hcd *xhci, u64 timeout_us)
 	 * Without this delay, the subsequent HC register access,
 	 * may result in a system hang very rarely.
 	 */
-	if (xhci->quirks & XHCI_INTEL_HOST)
+	if (xhci->quirks & (XHCI_INTEL_HOST | XHCI_CDNS_HOST))
 		udelay(1000);
 
 	ret = xhci_handshake(&xhci->op_regs->command, CMD_RESET, 0, timeout_us);
