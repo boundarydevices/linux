2f65c3fdddbfcd83a6bbbdb53042cab49139dd4d
Guoniu.Zhou <guoniu.zhou@nxp.com>
2017-11-10 11:01:46 +0800

    MLK-16695-2: mipi_csi: Add pm suspend and resume support
    Add power manager suspend and resume support for ISI
    
    Reviewed-by: Sandor Yu <sandor.yu@nxp.com>
    Signed-off-by: Guoniu.Zhou <guoniu.zhou@nxp.com>

--- drivers/media/platform/imx8/mxc-isi-core.h
+++ drivers/media/platform/imx8/mxc-isi-core.h
@@ -155,6 +155,11 @@ enum mxc_isi_m2m_in_fmt {
 	MXC_ISI_M2M_IN_FMT_YUV422_1P10P,
 };
 
+enum mxc_isi_power_state {
+	MXC_ISI_PM_SUSPENDED = 0x01,
+	MXC_ISI_PM_POWERED = 0x02,
+};
+
 struct mxc_isi_fmt {
 	char	*name;
 	u32 mbus_code;
