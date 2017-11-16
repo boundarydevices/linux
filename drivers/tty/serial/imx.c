aa655f2bf7dac7ca4c274ac0cfdfa0f7efc278cf
Fugang Duan <fugang.duan@nxp.com>
2017-11-16 16:31:13 +0800

    tty: serial: imx: add busfreq and pm qos support for DMA mode
    Add busfreq and pm qos support for DMA mode.
    
    DMA mode cannot work stablely at low busfreq mode, so request
    high busfreq once DMA is enabled.
    
    Signed-off-by: Fugang Duan <fugang.duan@nxp.com>

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -30,6 +30,8 @@
 #include <linux/dma-mapping.h>
 
 #include <asm/irq.h>
+#include <linux/busfreq-imx.h>
+#include <linux/pm_qos.h>
 #include <linux/platform_data/dma-imx.h>
 
 #include "serial_mctrl_gpio.h"
@@ -238,6 +240,8 @@ struct imx_port {
 	enum imx_tx_state	tx_state;
 	struct hrtimer		trigger_start_tx;
 	struct hrtimer		trigger_stop_tx;
+
+	struct pm_qos_request   pm_qos_req;
 };
 
 struct imx_port_ucrs {
@@ -1287,6 +1291,9 @@ static void imx_uart_dma_exit(struct imx_port *sport)
 		dma_release_channel(sport->dma_chan_tx);
 		sport->dma_chan_tx = NULL;
 	}
+
+	cpu_latency_qos_remove_request(&sport->pm_qos_req);
+	release_bus_freq(BUS_FREQ_HIGH);
 }
 
 static int imx_uart_dma_init(struct imx_port *sport)
@@ -1294,6 +1301,10 @@ static int imx_uart_dma_init(struct imx_port *sport)
 	struct dma_slave_config slave_config = {};
 	struct device *dev = sport->port.dev;
 	int ret;
+
+	/* request high bus for DMA mode */
+	request_bus_freq(BUS_FREQ_HIGH);
+	cpu_latency_qos_add_request(&sport->pm_qos_req, 0);
 
 	/* Prepare for RX : */
 	sport->dma_chan_rx = dma_request_slave_channel(dev, "rx");
