3f6ae8d27d7bce7747d6e386d34d89f26608fcc7
Troy Kisky <troy.kisky@boundarydevices.com>
2017-11-16 11:46:13 -0700

    ARM: imx: clk-imx6q: always call imx6q_mmdc_ch1_mask_handshake
    MMDC CH1 is not used on i.MX6Q, so the handshake needed to change the parent
    of periph2_sel or the divider of mmdc_ch1_axi_podf will never succeed.
    Disable the handshake mechanism to allow changing the frequency of mmdc_ch1_axi,
    allowing to use it as a possible source for the LDB DI clock.
    
    The ldb driver can set the parent clock, and hardware bug being checked for
    is irrelevant.
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/clk/imx/clk-imx6q.c
+++ drivers/clk/imx/clk-imx6q.c
@@ -254,6 +254,7 @@ static bool pll6_bypassed(struct device_node *node)
 	return false;
 }
 
+#define CCM_CCDR		0x04
 #define CCM_CCSR		0x0c
 #define CCM_CS2CDR		0x2c
 #define CCM_CSCDR3		0x3c
@@ -262,10 +263,20 @@ static bool pll6_bypassed(struct device_node *node)
 
 #define ANATOP_PLL3_PFD		0xf0
 
+#define CCDR_MMDC_CH1_MASK		BIT(16)
 #define CCSR_PLL3_SW_CLK_SEL		BIT(0)
 
 #define CS2CDR_LDB_DI0_CLK_SEL_SHIFT	9
 #define CS2CDR_LDB_DI1_CLK_SEL_SHIFT	12
+
+static void __init imx6q_mmdc_ch1_mask_handshake(void __iomem *ccm_base)
+{
+	unsigned int reg;
+
+	reg = readl_relaxed(ccm_base + CCM_CCDR);
+	reg |= CCDR_MMDC_CH1_MASK;
+	writel_relaxed(reg, ccm_base + CCM_CCDR);
+}
 
 /*
  * The only way to disable the MMDC_CH1 clock is to move it to pll3_sw_clk
@@ -667,6 +678,7 @@ static void __init imx6q_clocks_init(struct device_node *ccm_node)
 	base = of_iomap(np, 0);
 	ccm_base = base;
 	WARN_ON(!base);
+	imx6q_mmdc_ch1_mask_handshake(base);
 
 	/*                                              name                reg       shift width parent_names     num_parents */
 	hws[IMX6QDL_CLK_STEP]             = imx_clk_hw_mux("step",	            base + 0xc,  8,  1, step_sels,	   ARRAY_SIZE(step_sels));
