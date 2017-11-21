678e2235d1f470e5bafcb1bcfdb6afcfc838cacd
Fugang Duan <fugang.duan@nxp.com>
2017-11-21 13:56:50 +0800

    MLK-16896 tty: serial: lpuart: flush transmit/receive fifo/buffer
    Although .startup() alreadly do transmit/receive fifo/buffer flush,
    but switch the baud rate may introduce dirty data on fifo, in such
    case, user will call tcflush() to clean up buffer and fifo. So driver
    also ensure HW fifo is cleaned up.
    
    The patch add hw fifo/buffer flush in .flush_buffer() callback.
    
    Signed-off-by: Fugang Duan <fugang.duan@nxp.com>

--- drivers/tty/serial/fsl_lpuart.c
+++ drivers/tty/serial/fsl_lpuart.c
@@ -480,6 +480,8 @@ static int lpuart_dma_tx_request(struct uart_port *port)
 static void lpuart_flush_buffer(struct uart_port *port)
 {
 	struct lpuart_port *sport = container_of(port, struct lpuart_port, port);
+	u32 val;
+
 	if (sport->lpuart_dma_tx_use) {
 		if (sport->dma_tx_in_progress) {
 			dma_unmap_sg(sport->port.dev, &sport->tx_sgl[0],
@@ -487,6 +489,16 @@ static void lpuart_flush_buffer(struct uart_port *port)
 			sport->dma_tx_in_progress = false;
 		}
 		dmaengine_terminate_all(sport->dma_tx_chan);
+	}
+
+	if (sport->lpuart32) {
+		val = lpuart32_read(sport->port.membase + UARTFIFO);
+		val |= UARTFIFO_TXFLUSH | UARTFIFO_RXFLUSH;
+		lpuart32_write(val, sport->port.membase + UARTFIFO);
+	} else {
+		val = readb(sport->port.membase + UARTPFIFO);
+		val |= UARTCFIFO_TXFLUSH | UARTCFIFO_RXFLUSH;
+		writeb(val, sport->port.membase + UARTCFIFO);
 	}
 }
 
