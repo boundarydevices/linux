bc81095b2fdb9e12586fe6bd8f5f98e842334654
Peter Chen <peter.chen@nxp.com>
2017-12-21 09:06:40 +0800

    MLK-17261-2 usb: chipidea: imx: do not call hsic callback for non-hsic controller
    With this judgement, the non-hsic controller will access wrong registers,
    and below error message will be showed:
    "usbmisc_imx 2184800.usbmisc: index is error for usbmisc"
    
    Fixes: 113be1516160 ("MLK-16715-6 usb: chipidea: imx:
    	add HSIC support for controllers from imx7d")
    Reported-by: Peng Fan <peng.fan@nxp.com>
    Acked-by: Jun Li <jun.li@nxp.com>
    Signed-off-by: Peter Chen <peter.chen@nxp.com>
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>
    Signed-off-by: Srikanth Krishnakar <Srikanth_Krishnakar@mentor.com>

--- drivers/usb/chipidea/ci_hdrc_imx.c
+++ drivers/usb/chipidea/ci_hdrc_imx.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
  * Copyright 2012-2016 Freescale Semiconductor, Inc.
+ * Copyright 2017 NXP
  * Copyright (C) 2012 Marek Vasut <marex@denx.de>
  * on behalf of DENX Software Engineering GmbH
  */
