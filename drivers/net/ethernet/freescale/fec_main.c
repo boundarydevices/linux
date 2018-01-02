35b65d15fd6dcf5ec30c207d33cb54d69aad0390
Fugang Duan <fugang.duan@nxp.com>
2018-01-02 18:29:16 +0800

    MLK-17400-01 net: fec: defer probe if regulator is not ready
    Defer probe if regulator is not ready. E.g. some regulator is fixed
    regulator controlled by i2c expander gpio, the i2c device may be probed
    after the driver, then it should handle the case of defer probe error.
    
    Signed-off-by: Fugang Duan <fugang.duan@nxp.com>
    Acked-by: Gao Pan <pandy.gao@nxp.com>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -3771,7 +3771,6 @@ fec_probe(struct platform_device *pdev)
 		if (ret) {
 			dev_err(&pdev->dev,
 				"Failed to enable phy regulator: %d\n", ret);
-			clk_disable_unprepare(fep->clk_ipg);
 			goto failed_regulator;
 		}
 	} else {
@@ -3827,10 +3826,8 @@ fec_probe(struct platform_device *pdev)
 	if (!of_get_property(np, "fsl,mii-exclusive", NULL))
 		fep->quirks |= FEC_QUIRK_SINGLE_MDIO;
 	ret = fec_enet_mii_init(pdev);
-	if (ret) {
-		dev_id = 0;
+	if (ret)
 		goto failed_mii_init;
-	}
 
 	/* Carrier starts down, phylib will bring it up */
 	netif_carrier_off(ndev);
@@ -3869,9 +3866,9 @@ failed_init:
 	if (fep->reg_phy)
 		regulator_disable(fep->reg_phy);
 failed_reset:
-	pm_runtime_put(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
 failed_regulator:
+	clk_disable_unprepare(fep->clk_ahb);
 failed_clk_ahb:
 	clk_disable_unprepare(fep->clk_ipg);
 failed_clk_ipg:
