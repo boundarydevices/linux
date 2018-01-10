7170c52677447d1412965ec87163b69cd7ba5366
Ranjani Vaidyanathan <Ranjani.Vaidyanathan@nxp.com>
2018-01-10 11:51:26 -0600

    MLK-21052-08 clk: imx: Add CLK_SET_PARENT_NOCACHE
    Implement a CLK_SET_PARENT_NOCACHE flag in clk core for imx8 clk
    implementation where the parent needs to be restore after PM domain is
    up.
    
    Reviewed-by: Anson Huang <anson.huang@nxp.com>
    Signed-off-by: Dong Aisheng <aisheng.dong@nxp.com>
    Signed-off-by: Ranjani Vaidyanathan <Ranjani.Vaidyanathan@nxp.com>
    (cherry picked from commit 87e997822c050fc7dc027a863c92f1f0b4816515)
    [Leonard: split clk core part]
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -2215,7 +2215,8 @@ static int clk_core_set_parent_nolock(struct clk_core *core,
 	if (!core)
 		return 0;
 
-	if (core->parent == parent)
+	if ((core->parent == parent) &&
+		!(core->flags & CLK_SET_PARENT_NOCACHE))
 		return 0;
 
 	/* verify ops for for multi-parent clks */
