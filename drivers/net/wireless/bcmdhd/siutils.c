b3b72c12915bd5dbb4be50e4f3d90e20a19a681c
Gao Pan <pandy.gao@nxp.com>
2018-01-23 14:15:37 +0800

    MLK-17443 wireless: bcmdhd: fix build warning
    case1: statement "if (bustype != BUSTYPE(bustype))"
    equals "if (bustype != bustype)" when BCMBUSTYPE is
    not defined. Some compilers treat it as a build warning.
    
    case2: fix misleadingly indented of dhd_os_sdunlock(dhdp)
    
    Signed-off-by: Gao Pan <pandy.gao@nxp.com>
    Tested-by: Peng Fan <peng.fan@nxp.com>

--- drivers/net/wireless/bcmdhd/siutils.c
+++ drivers/net/wireless/bcmdhd/siutils.c
@@ -400,11 +400,6 @@ si_doattach(si_info_t *sii, uint devid, osl_t *osh, void *regs,
 	}
 
 	sih->bustype = bustype;
-	if (bustype != BUSTYPE(bustype)) {
-		SI_ERROR(("si_doattach: bus type %d does not match configured bus type %d\n",
-			bustype, BUSTYPE(bustype)));
-		return NULL;
-	}
 
 	/* bus/core/clk setup for register access */
 	if (!si_buscore_prep(sii, bustype, devid, sdh)) {
