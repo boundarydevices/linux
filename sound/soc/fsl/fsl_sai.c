0f13af43352c29e9547ea237c15f2c3255510031
Viorel Suman <viorel.suman@nxp.com>
2018-01-29 16:04:44 +0200

    MLK-16224-6: ASoC: fsl_sai: fix DSD suspend/resume
    With the existing implementation the SAI pinctrl state is restored to
    default after resume - this breaks DSD playback after resume.
    Restore DSD pinctrl state in snd_soc_dai_driver resume callback.
    
    Signed-off-by: Viorel Suman <viorel.suman@nxp.com>
    Reviewed-by: Shengjiu Wang <shengjiu.wang@nxp.com>

--- sound/soc/fsl/fsl_sai.c
+++ sound/soc/fsl/fsl_sai.c
@@ -872,6 +872,23 @@ static int fsl_sai_dai_probe(struct snd_soc_dai *cpu_dai)
 	return 0;
 }
 
+static int fsl_sai_dai_resume(struct snd_soc_component *component)
+{
+	struct fsl_sai *sai = snd_soc_component_get_drvdata(component);
+	int ret;
+
+	if (sai->is_dsd && !IS_ERR_OR_NULL(sai->pins_dsd)) {
+		ret = pinctrl_select_state(sai->pinctrl, sai->pins_dsd);
+		if (ret) {
+			dev_err(&sai->pdev->dev,
+				"failed to set proper pins state: %d\n", ret);
+			return ret;
+		}
+	}
+
+	return 0;
+}
+
 static struct snd_soc_dai_driver fsl_sai_dai_template = {
 	.probe = fsl_sai_dai_probe,
 	.playback = {
@@ -897,6 +914,7 @@ static struct snd_soc_dai_driver fsl_sai_dai_template = {
 
 static const struct snd_soc_component_driver fsl_component = {
 	.name           = "fsl-sai",
+	.resume  = fsl_sai_dai_resume,
 };
 
 static struct reg_default fsl_sai_reg_defaults_ofs0[] = {
