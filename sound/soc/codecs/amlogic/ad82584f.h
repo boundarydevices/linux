5574b659236121833f78d32387c77f6188dac4d5
Xing Wang <xing.wang@amlogic.com>
2018-02-02 15:03:56 +0800

    audio: codec: add ad82584f driver
    PD#156734: audio: codec: add ad82584f driver
    
    Change-Id: I1e2d8b452e5560c84c60d965e7a916fa0ff03bf4
    Signed-off-by: Xing Wang <xing.wang@amlogic.com>

---
+++ sound/soc/codecs/amlogic/ad82584f.h
@@ -0,0 +1,20 @@
+#ifndef _AD82584F_H
+#define _AD82584F_H
+
+#define MVOL                             0x03
+#define C1VOL                            0x04
+#define C2VOL                            0x05
+#define CFADDR                           0x1d
+#define A1CF1                            0x1e
+#define A1CF2                            0x1f
+#define A1CF3                            0x20
+#define CFUD                             0x2d
+
+#define AD82584F_REGISTER_COUNT		 0x86
+#define AD82584F_RAM_TABLE_COUNT         0x80
+
+struct ad82584f_platform_data {
+	int reset_pin;
+};
+
+#endif
