54a0de943887e1be7dd156539cb4ee368f82ce1c
Leonard Crestez <leonard.crestez@nxp.com>
2018-02-09 16:35:59 +0200

    MLK-17583-2: arm: imx: Fix imx6sl cpuidle build fail without CONFIG_SOC_IMX6SLL
    Fixes commit 91558864ab21 ("MLK-13344-05 ARM: imx: Add cpuidle support on imx6sll")
    
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>
    Reviewed-by: Jacky Bai <ping.bai@nxp.com>

--- arch/arm/mach-imx/cpuidle.h
+++ arch/arm/mach-imx/cpuidle.h
@@ -14,7 +14,6 @@
 extern int imx5_cpuidle_init(void);
 extern int imx6q_cpuidle_init(void);
 extern int imx6sl_cpuidle_init(void);
-extern int imx6sll_cpuidle_init(void);
 extern int imx6sx_cpuidle_init(void);
 extern int imx6ul_cpuidle_init(void);
 extern int imx7d_cpuidle_init(void);
@@ -30,10 +29,6 @@ static inline int imx6q_cpuidle_init(void)
 	return 0;
 }
 static inline int imx6sl_cpuidle_init(void)
-{
-	return 0;
-}
-static inline int imx6sll_cpuidle_init(void)
 {
 	return 0;
 }
@@ -57,4 +52,10 @@ static inline int imx7ulp_cpuidle_init(void)
 {
 	return 0;
 }
+#endif
+
+#if defined(CONFIG_CPU_IDLE) && defined (CONFIG_SOC_IMX6SLL)
+extern int imx6sll_cpuidle_init(void);
+#else
+static inline int imx6sll_cpuidle_init(void) { return 0; }
 #endif
