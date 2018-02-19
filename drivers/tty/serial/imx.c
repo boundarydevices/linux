9cbb8e2043e17f9b14ba4b7a27a1ea22d5d80f64
phil eichinger <phil@zankapfel.net>
2018-02-19 10:24:15 +0100

    serial: imx: fix bogus dev_err
    Only one of the two is really required, not both:
    * have_rtscts or
    * have_rtsgpio
    
    In imx_rs485_config() this is done correctly, so RS485 is working,
    just the error message is false.
    
    Signed-off-by: Phil Eichinger <phil@zankapfel.net>
    Reviewed-by: Fabio Estevam <fabio.estevam@nxp.com>
    Fixes: b8f3bff057b0 ("serial: imx: Support common rs485 binding for RTS polarity"
    Acked-by: Uwe Kleine-KÃ¶nig <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -2442,7 +2442,7 @@ static int serial_imx_probe(struct platform_device *pdev)
 	}
 
 	if (sport->port.rs485.flags & SER_RS485_ENABLED &&
-	    (!sport->have_rtscts || !sport->have_rtsgpio))
+	    (!sport->have_rtscts && !sport->have_rtsgpio))
 		dev_err(&pdev->dev, "no RTS control, disabling rs485\n");
 
 	imx_rs485_config(&sport->port, &sport->port.rs485);
