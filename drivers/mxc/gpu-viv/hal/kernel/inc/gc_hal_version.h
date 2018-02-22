7e22ae91802a5f9c40d8908a64b1d2df3cdf47f5
Xianzhong <xianzhong.li@nxp.com>
2018-02-22 19:23:23 +0800

    MGS-3655: gpu-viv: integrate 6.2.4.p1 official release
    update gpu kernel version with 6.2.4.p1 official release.
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- drivers/mxc/gpu-viv/hal/kernel/inc/gc_hal_version.h
+++ drivers/mxc/gpu-viv/hal/kernel/inc/gc_hal_version.h
@@ -62,8 +62,8 @@
 
 #define gcvVERSION_PATCH        4
 
-#define gcvVERSION_BUILD     148482
+#define gcvVERSION_BUILD     150331
 
-#define gcvVERSION_STRING    "6.2.4.p1.148482"
+#define gcvVERSION_STRING    "6.2.4.p1.150331"
 
 #endif /* __gc_hal_version_h_ */
