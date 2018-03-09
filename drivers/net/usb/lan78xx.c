b5041eae36bd52ada8df9a84e06a86fa07ca358a
Phil Elwell <phil@raspberrypi.org>
2018-03-09 12:01:00 +0000

    net: lan78xx: Read initial EEE status from DT
    Add two new DT properties:
    * microchip,eee-enabled  - a boolean to enable EEE
    * microchip,tx-lpi-timer - time in microseconds to wait before entering
                               low power state
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.org>
    
    Squashed:
    
    lan78xx: Move enabling of EEE into PHY init code
    
    Enable EEE mode as soon as possible after connecting to the PHY, and
    before phy_start. This avoids a second link negotiation, which speeds
    up booting and stops the interface failing to become ready.
    
    See: https://github.com/raspberrypi/linux/issues/2437
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.org>
    
    lan78xx: EEE support is now a PHY property
    
    Now that EEE support is a property of the PHY, use the PHY's DT node
    when querying the EEE-related properties.
    
    See: https://github.com/raspberrypi/linux/issues/2882
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.org>

--- drivers/net/usb/lan78xx.c
+++ drivers/net/usb/lan78xx.c
@@ -2410,6 +2410,22 @@ static int lan78xx_phy_init(struct lan78xx_net *dev)
 	mii_adv_to_linkmode_adv_t(fc, mii_adv);
 	linkmode_or(phydev->advertising, fc, phydev->advertising);
 
+	if (of_property_read_bool(phydev->mdio.dev.of_node,
+				  "microchip,eee-enabled")) {
+		struct ethtool_eee edata;
+		memset(&edata, 0, sizeof(edata));
+		edata.cmd = ETHTOOL_SEEE;
+		edata.advertised = ADVERTISED_1000baseT_Full |
+				   ADVERTISED_100baseT_Full;
+		edata.eee_enabled = true;
+		edata.tx_lpi_enabled = true;
+		if (of_property_read_u32(dev->udev->dev.of_node,
+					 "microchip,tx-lpi-timer",
+					 &edata.tx_lpi_timer))
+			edata.tx_lpi_timer = 600; /* non-aggressive */
+		(void)lan78xx_set_eee(dev->net, &edata);
+	}
+
 	if (phydev->mdio.dev.of_node) {
 		u32 reg;
 		int len;
