cad6bfd7a1a885383e42ed42278e14064ce4e6b7
Laurentiu Palcu <laurentiu.palcu@nxp.com>
2018-03-22 17:08:10 -0500

    MLK-17703-2: drm: change HDR metadata infoframe structure
    According to ANSI-CTA-861-G specification:
     * EOTF is 8 bit, not 16;
     * metadata type is 8 bit, not 16;
     * There's no "Minimum Content Light Level"
    
    This patch will change the HDR metadata structures to reflect that. Also, this
    will fix problems seen on some TVs that were rejecting HDR metadata because
    it's size was too big (more than 26 bytes).
    
    Signed-off-by: Laurentiu Palcu <laurentiu.palcu@nxp.com>
    CC: Sandor Yu <sandor.yu@nxp.com>

--- include/uapi/drm/drm_mode.h
+++ include/uapi/drm/drm_mode.h
@@ -531,8 +531,8 @@ enum supported_eotf_type {
 
 /* HDR Metadata */
 struct hdr_static_metadata {
-	uint16_t eotf;
-	uint16_t type;
+	uint8_t eotf;
+	uint8_t type;
 	uint16_t display_primaries_x[3];
 	uint16_t display_primaries_y[3];
 	uint16_t white_point_x;
@@ -541,7 +541,6 @@ struct hdr_static_metadata {
 	uint16_t min_mastering_display_luminance;
 	uint16_t max_fall;
 	uint16_t max_cll;
-	uint16_t min_cll;
 };
 
 #define DRM_MODE_PAGE_FLIP_EVENT 0x01
