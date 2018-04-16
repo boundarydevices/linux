4b3dd8343e79f389d408592b200cd0684f0c3f73
Gary Bisson <gary.bisson@boundarydevices.com>
2018-04-16 17:17:43 +0200

    usb: dwc3: add basic vbus regulator control
    usb: dwc3: factor out dwc3_set_vbus
    
    Also make sure to check regulator status before enable/disable to avoid
    unbalanced enable/disable warnings.
    
    usb: dwc3: core: turn off vbus when nothing attached
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/usb/dwc3/drd.c
+++ drivers/usb/dwc3/drd.c
@@ -384,11 +384,7 @@ void dwc3_otg_update(struct dwc3 *dwc, bool ignore_idstatus)
 		if (ret) {
 			dev_err(dwc->dev, "failed to initialize host\n");
 		} else {
-			if (dwc->usb2_phy)
-				otg_set_vbus(dwc->usb2_phy->otg, true);
-			if (dwc->usb2_generic_phy)
-				phy_set_mode(dwc->usb2_generic_phy,
-					     PHY_MODE_USB_HOST);
+			dwc3_set_vbus(dwc, true);
 		}
 		break;
 	case DWC3_OTG_ROLE_DEVICE:
@@ -398,11 +394,8 @@ void dwc3_otg_update(struct dwc3 *dwc, bool ignore_idstatus)
 		dwc3_event_buffers_setup(dwc);
 		spin_unlock_irqrestore(&dwc->lock, flags);
 
-		if (dwc->usb2_phy)
-			otg_set_vbus(dwc->usb2_phy->otg, false);
-		if (dwc->usb2_generic_phy)
-			phy_set_mode(dwc->usb2_generic_phy,
-				     PHY_MODE_USB_DEVICE);
+		dwc3_set_vbus(dwc, false);
+
 		ret = dwc3_gadget_init(dwc);
 		if (ret)
 			dev_err(dwc->dev, "failed to initialize peripheral\n");
