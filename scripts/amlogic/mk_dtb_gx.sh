c198fa6e330ee76a2c09925024ab32bd7578c160
Yan Wang <yan.wang@amlogic.com>
2018-04-23 15:37:16 +0800

    dts: g12b: initial add g12b
    PD#165090: initial add g12b
    
    initial G12B dts for bringup.
    
    Change-Id: If7c390b2139055f637fb17fb4e3ce0acd71a6f17
    Signed-off-by: Yan Wang <yan.wang@amlogic.com>

--- scripts/amlogic/mk_dtb_gx.sh
+++ scripts/amlogic/mk_dtb_gx.sh
@@ -30,4 +30,6 @@ make ARCH=arm64 axg_s420_v03.dtb || echo "Compile dtb Fail !!"
 
 make ARCH=arm64 g12a_pxp.dtb || echo "Compile dtb Fail!!"
 
-make ARCH=arm64 g12a_skt.dtb || echo "Compile dtb Fail!!"
+make ARCH=arm64 g12a_s905d2_skt.dtb || echo "Compile dtb Fail!!"
+
+make ARCH=arm64 g12b_pxp.dtb || echo "Compile dtb Fail!!"
