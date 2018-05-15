10025a4ab781a2079d64bf08445a97a95c313d33
Troy Kisky <troy.kisky@boundarydevices.com>
2018-05-14 18:59:37 -0700

    Input: goodix: poll after every sleep
    There is no guarantee we will run immediately after sleep
    expires. Make sure we poll at least once after 10ms is up.
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/input/touchscreen/goodix.c
+++ drivers/input/touchscreen/goodix.c
@@ -316,8 +316,10 @@ static int goodix_ts_read_input_report(struct goodix_ts_data *ts, u8 *data)
 				return 0;
 		}
 
+		if (time_after(jiffies, max_timeout))
+			break;
 		usleep_range(1000, 2000); /* Poll every 1 - 2 ms */
-	} while (time_before(jiffies, max_timeout));
+	} while (1);
 
 	/*
 	 * The Goodix panel will send spurious interrupts after a
