de93612e48f124d6689a0310803d0f9c593940ac
Troy Kisky <troy.kisky@boundarydevices.com>
2018-07-03 12:17:44 -0700

    mxc_v4l2_capture: return -ENOIOCTLCMD for unsupported ioctls
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/media/platform/mxc/capture/mxc_v4l2_capture.c
+++ drivers/media/platform/mxc/capture/mxc_v4l2_capture.c
@@ -2526,7 +2526,7 @@ static long mxc_v4l_do_ioctl(struct file *file,
 	case VIDIOC_S_FREQUENCY:
 	default:
 		pr_debug("   case default or not supported\n");
-		retval = -EINVAL;
+		retval = -ENOIOCTLCMD;
 		break;
 	}
 
