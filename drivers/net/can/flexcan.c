feca7c1a0caf51a8e13761154ef6d5315326a9dd
Peter Chen <peter.chen@nxp.com>
2018-07-24 16:39:08 +0800

    MLK-19029 net: can: flexcan: fix the build warning
    linux-imx/drivers/net/can/flexcan.c: In function ‘flexcan_probe’:
    linux-imx/drivers/net/can/flexcan.c:1506:2: warning: label
    ‘failed_register’ defined but not used [-Wunused-label]
      failed_register:
        ^~~~~~~~~~~~~~~
    
    Reviewed-by: Dong Aisheng <aisheng.dong@nxp.com>
    Signed-off-by: Peter Chen <peter.chen@nxp.com>
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>

--- drivers/net/can/flexcan.c
+++ drivers/net/can/flexcan.c
@@ -1543,7 +1543,6 @@ static int flexcan_probe(struct platform_device *pdev)
  failed_rpm_disable:
 	pm_runtime_disable(&pdev->dev);
  failed_offload:
- failed_register:
 	free_candev(dev);
 
 	return err;
