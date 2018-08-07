7152bce7ca274fd6b9cfe9ac0db22feda27dcaae
Victor Wan <victor.wan@amlogic.com>
2018-08-07 14:45:33 +0800

    Revert "BACKPORT: dma-buf/sync_file: Allow multiple sync_files to wrap a single dma-fence"
    This reverts commit 74fa0af47306baf118b32b7acdba4ae98eda7a7d.
    
    The commit 74fa0af caused GPU driver loading failed.
    
    Change-Id: I255eb6b7a26e3b00e4f11489ece49896ff004d79

--- include/linux/sync_file.h
+++ include/linux/sync_file.h
@@ -40,13 +40,12 @@ struct sync_file {
 #endif
 
 	wait_queue_head_t	wq;
-	unsigned long		flags;
 
 	struct fence		*fence;
 	struct fence_cb cb;
 };
 
-#define POLL_ENABLED 0
+#define POLL_ENABLED FENCE_FLAG_USER_BITS
 
 struct sync_file *sync_file_create(struct fence *fence);
 struct fence *sync_file_get_fence(int fd);
