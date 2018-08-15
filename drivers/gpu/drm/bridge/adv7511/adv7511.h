1e6de787ea8bd5425094e0d3b71c4dcf2ba46d6f
Fancy Fang <chen.fang@nxp.com>
2018-08-15 10:45:27 +0800

    MLK-19252-2 drm/bridge: adv7511: realize mode_fixup for lanes fixup
    In the current implementation, the data lanes fixup is done in
    the Bridge's mode_set() function which is too late to give the
    upper layer Bridge a chance to check this change to be supported
    or not and a chance to refuse this commit if it does not support
    the requested data lanes number.
    
    Signed-off-by: Fancy Fang <chen.fang@nxp.com>
    (cherry picked from commit c97f9febd7e42d7865921e2bb56648ebb13c0761)
    (cherry picked from commit 36f25495a5c30e66e033cfd3d46d4693f50fc8f7)
    (cherry picked from commit de7e9c83c7503255c512b5c78165ba7924c7269e)

--- drivers/gpu/drm/bridge/adv7511/adv7511.h
+++ drivers/gpu/drm/bridge/adv7511/adv7511.h
@@ -404,6 +404,8 @@ static inline int adv7511_cec_init(struct device *dev, struct adv7511 *adv7511)
 #ifdef CONFIG_DRM_I2C_ADV7533
 void adv7533_dsi_power_on(struct adv7511 *adv);
 void adv7533_dsi_power_off(struct adv7511 *adv);
+void adv7533_mode_set(struct adv7511 *adv, struct drm_display_mode *mode);
+bool adv7533_mode_fixup(struct adv7511 *adv, struct drm_display_mode *mode);
 int adv7533_patch_registers(struct adv7511 *adv);
 int adv7533_patch_cec_registers(struct adv7511 *adv);
 int adv7533_attach_dsi(struct adv7511 *adv);
@@ -421,6 +423,12 @@ static inline void adv7533_dsi_power_off(struct adv7511 *adv)
 static inline void adv7533_mode_set(struct adv7511 *adv,
 				    struct drm_display_mode *mode)
 {
+}
+
+static inline bool adv7533_mode_fixup(struct adv7511 *adv,
+				      struct drm_display_mode *mode)
+{
+	return true;
 }
 
 static inline int adv7533_patch_registers(struct adv7511 *adv)
