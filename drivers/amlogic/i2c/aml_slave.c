e11c1a802aafd78c6a54d57035c0b039e7d34f7e
Yingyuan Zhu <yingyuan.zhu@amlogic.com>
2018-08-29 10:49:05 +0800

    meson: i2c: fix coverity warning
    PD#172722: meson: i2c: fix coverity warning
    
    When the function "devm_ioremap_resource" returns
    an error,the previously applied memory resource
    "slave" is not released.
    This causes resource leak.
    
    Change-Id: I2dfb7fab007977e1ae57e714ae489fbf80ec7103
    Signed-off-by: Yingyuan Zhu <yingyuan.zhu@amlogic.com>

--- drivers/amlogic/i2c/aml_slave.c
+++ drivers/amlogic/i2c/aml_slave.c
@@ -182,7 +182,8 @@ static int i2c_slave_probe(struct platform_device *pdev)
 	if (!strcmp(i2c_auto_test_mode, "open"))
 		i2c_auto_test_flag = 1;
 
-	slave =	kzalloc(sizeof(struct aml_i2c_slave), GFP_KERNEL);
+	slave =	devm_kzalloc(&pdev->dev, sizeof(struct aml_i2c_slave),
+		GFP_KERNEL);
 
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 
@@ -246,7 +247,6 @@ static int i2c_slave_remove(struct platform_device *pdev)
 	class_destroy(&slave->cls);
 	mutex_destroy(slave->lock);
 	free_irq(slave->irq, slave);
-	kzfree(slave);
 	return 0;
 }
 static int i2c_slave_suspend(struct device *dev)
