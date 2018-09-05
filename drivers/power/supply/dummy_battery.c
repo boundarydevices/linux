53493b756482a45edcde15d8573924bc28433ec3
Zhang Bo <bo.zhang@nxp.com>
2018-09-05 18:25:06 +0800

    MA-20973-5 dummy_battery: Add dummy battery driver
    Cherry-pick following two patches from android-things git.
    Combine other two patches.
    
    commit 8477e9491f8654a31e233aae11ba9c1684c28541
    Author: Dave Rim <davidrim@google.com>
    Date:   Thu Oct 12 14:48:39 2017 +0900
    
        Create dummy battery for devices with no battery
    
        Creates a dummy battery module for android devices with no battery
    
        Bug: 63166743, Bug: 64996847
        Test: com.android.server.cts.BatteryStatsValidationTest#testBleScans
        Change-Id: I21e0968e8b43ee35c21b77ada204069f2df58773
    
    commit b067cacece5ebcc4f3f0b7efda4b7940c723706a
    Author: Dave Rim <davidrim@google.com>
    Date:   Thu Oct 19 11:41:25 2017 +0900
    
        Set dummy battery status to charging
    
        CtsJobSchedulerTestCases requires status charging
    
        Bug: 67625790
        Test: cts-tradefed run commandAndExit cts -m CtsJobSchedulerTestCases -t
              android.jobscheduler.cts.TimingConstraintsTest#testJobParameters_unexpiredDeadline
        Change-Id: I26463cf8adafbd8baec960ee06e1c7f836f92ed9
    
    commit b9fe212e0527d778153018f41cb679f22fada288
    Author: Dandan Sun <dandan.sun@nxp.com>
    Date:   Tue Feb 15 16:13:32 2022 +0800
    
        MA-13705 fake detailed property value for dummy battery
    
        add below node /sys/devices/virtual/power_supply/battery/ which is used
        in BatteryMonitor. /sys/class/power_supply linked to the former link.
        Many cts can't pass if do not add this sys node.
    
        Change-Id: I432fa4ebb62d19aed2a94348bd62d76beafe4ecf
        Signed-off-by: Dandan Sun <dandan.sun@nxp.com>
    
    commit d19d7eb571a4aa140a24e70d0213cdd7f3e2134e
    Author: Dandan Sun <dandan.sun@nxp.com>
    Date:   Tue Feb 15 16:21:37 2022 +0800
    
        MA-17389 Set dummy_battery_property writeable for charger mode
    
        override dummy_power_desc[] to implement dummy_usb_set_property,
        dummy_usb_property_is_writeable, dummy_battery_set_property,
        dummy_battery_property_is_writeable to overlay POWER_SUPPLY_PROP_ONLINE,
    
        can echo 0 or 1 to /sys/class/power_supply/battery/online and
        /sys/class/power_supply/usb/online, when both set to 0, equals to
        unplug power_supply device, charger_online will be set to false,
        system will shut down by HandlePowerSupplyState when in charger mode.
    
        Change-Id: If4b710ba72e42524770e4a961ded4908e70afa5f
        Signed-off-by: Dandan Sun <dandan.sun@nxp.com>
    
    Change-Id: If41f51806c7c1a8ac7c5980f00be7a11a54dec7d
    Signed-off-by: Zhang Bo <bo.zhang@nxp.com>
    Signed-off-by: Dandan Sun <dandan.sun@nxp.com>

---
+++ drivers/power/supply/dummy_battery.c
@@ -0,0 +1,257 @@
+/*
+ * Dummy power supply driver for android
+ *
+ * Copyright (C) 2017 Google, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/power_supply.h>
+#include <linux/errno.h>
+#include <linux/slab.h>
+
+#define DUMMY_POWER_NUM 2
+#define POWER_SUPPLY_PROP_CAPACITY_VALUE 85
+#define POWER_SUPPLY_PROP_VOLTAGE_NOW_VALUE 3600
+#define POWER_SUPPLY_PROP_CURRENT_NOW_VALUE 400000
+#define POWER_SUPPLY_PROP_CYCLE_COUNT_VALUE 32
+#define POWER_SUPPLY_PROP_TEMP_VALUE 350
+#define POWER_SUPPLY_PROP_CHARGE_FULL_VALUE 4000000
+#define POWER_SUPPLY_PROP_CHARGE_COUNTER_VALUE 1900000
+#define POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT_MAX_VALUE 500000
+#define POWER_SUPPLY_PROP_CONSTANT_CHARGE_VOLTAGE_MAX_VALUE 5000000
+
+struct dummy_usb_property{
+    int online;
+};
+struct dummy_usb_property *usb_property;
+
+struct dummy_battery_property{
+    int status;
+    int health;
+    int present;
+    int technology;
+    int capacity;
+    int voltage_now;
+    int current_now;
+    int cycle_count;
+    int temp;
+    int charge_full;
+    int charge_counter;
+    int constant_charge_current_max;
+    int constant_charge_voltage_max;
+    int online;
+};
+struct dummy_battery_property *battery_property;
+
+static enum power_supply_property dummy_battery_props[] = {
+    POWER_SUPPLY_PROP_STATUS,
+    POWER_SUPPLY_PROP_HEALTH,
+    POWER_SUPPLY_PROP_PRESENT,
+    POWER_SUPPLY_PROP_TECHNOLOGY,
+    POWER_SUPPLY_PROP_CAPACITY,
+    POWER_SUPPLY_PROP_VOLTAGE_NOW,
+    POWER_SUPPLY_PROP_CURRENT_NOW,
+    POWER_SUPPLY_PROP_CYCLE_COUNT,
+    POWER_SUPPLY_PROP_TEMP,
+    POWER_SUPPLY_PROP_CHARGE_FULL,
+    POWER_SUPPLY_PROP_CHARGE_COUNTER,
+    POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT_MAX,
+    POWER_SUPPLY_PROP_CONSTANT_CHARGE_VOLTAGE_MAX,
+    POWER_SUPPLY_PROP_ONLINE,
+};
+
+static enum power_supply_property dummy_usb_props[] = {
+    POWER_SUPPLY_PROP_ONLINE,
+};
+
+static int dummy_usb_get_property(struct power_supply *psy,
+                                  enum power_supply_property psp,
+                                  union power_supply_propval *val)
+{
+    int ret = 0;
+    switch (psp) {
+        case POWER_SUPPLY_PROP_ONLINE:
+            val->intval = usb_property->online;
+            break;
+        default:
+            ret = -EINVAL;
+            break;
+    }
+    return ret;
+}
+
+static int dummy_usb_set_property(struct power_supply *psy,
+                                  enum power_supply_property psp,
+                                  const union power_supply_propval *val)
+{
+    int ret = 0;
+    switch (psp) {
+        case POWER_SUPPLY_PROP_ONLINE:
+            usb_property->online = val->intval;
+            break;
+        default:
+            ret = -EINVAL;
+            break;
+    }
+    return ret;
+}
+
+static int dummy_usb_property_is_writeable(struct power_supply *psy,
+                                  enum power_supply_property psp)
+{
+    return psp == POWER_SUPPLY_PROP_ONLINE;
+}
+
+static int dummy_battery_get_property(struct power_supply *psy,
+                                      enum power_supply_property psp,
+                                      union power_supply_propval *val)
+{
+    int ret = 0;
+    switch (psp) {
+        case POWER_SUPPLY_PROP_STATUS:
+            val->intval = POWER_SUPPLY_STATUS_CHARGING;
+            break;
+        case POWER_SUPPLY_PROP_HEALTH:
+            val->intval = POWER_SUPPLY_HEALTH_GOOD;
+            break;
+        case POWER_SUPPLY_PROP_PRESENT:
+            val->intval = true;
+            break;
+        case POWER_SUPPLY_PROP_TECHNOLOGY:
+            val->intval = POWER_SUPPLY_TECHNOLOGY_LION;
+            break;
+        case POWER_SUPPLY_PROP_CAPACITY:
+            val->intval = POWER_SUPPLY_PROP_CAPACITY_VALUE;
+            break;
+        case POWER_SUPPLY_PROP_VOLTAGE_NOW:
+            val->intval = POWER_SUPPLY_PROP_VOLTAGE_NOW_VALUE;
+            break;
+        case POWER_SUPPLY_PROP_CURRENT_NOW:
+            val->intval = POWER_SUPPLY_PROP_CURRENT_NOW_VALUE;
+            break;
+        case POWER_SUPPLY_PROP_CYCLE_COUNT:
+            val->intval = POWER_SUPPLY_PROP_CYCLE_COUNT_VALUE;
+            break;
+        case POWER_SUPPLY_PROP_TEMP:
+            val->intval = POWER_SUPPLY_PROP_TEMP_VALUE;
+            break;
+        case POWER_SUPPLY_PROP_CHARGE_FULL:
+            val->intval = POWER_SUPPLY_PROP_CHARGE_FULL_VALUE;
+            break;
+        case POWER_SUPPLY_PROP_CHARGE_COUNTER:
+            val->intval = POWER_SUPPLY_PROP_CHARGE_COUNTER_VALUE;
+            break;
+        case POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT_MAX:
+            val->intval = POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT_MAX_VALUE;
+            break;
+        case POWER_SUPPLY_PROP_CONSTANT_CHARGE_VOLTAGE_MAX:
+            val->intval = POWER_SUPPLY_PROP_CONSTANT_CHARGE_VOLTAGE_MAX_VALUE;
+            break;
+        case POWER_SUPPLY_PROP_ONLINE:
+            val->intval = battery_property->online;
+            break;
+        default:
+            ret = -EINVAL;
+            break;
+    }
+    return ret;
+}
+
+static int dummy_battery_set_property(struct power_supply *psy,
+                                  enum power_supply_property psp,
+                                  const union power_supply_propval *val)
+{
+    int ret = 0;
+    switch (psp) {
+        case POWER_SUPPLY_PROP_ONLINE:
+            battery_property->online = val->intval;
+            break;
+        default:
+            ret = -EINVAL;
+            break;
+    }
+    return ret;
+}
+
+static int dummy_battery_property_is_writeable(struct power_supply *psy,
+                                  enum power_supply_property psp)
+{
+    return psp == POWER_SUPPLY_PROP_ONLINE;
+}
+
+static struct power_supply *dummy_power_supplies[DUMMY_POWER_NUM];
+static const struct power_supply_desc dummy_power_desc[] = {
+    {
+        .properties = dummy_battery_props,
+        .num_properties = ARRAY_SIZE(dummy_battery_props),
+        .set_property = dummy_battery_set_property,
+        .get_property = dummy_battery_get_property,
+        .name = "battery",
+        .property_is_writeable = dummy_battery_property_is_writeable,
+        .type = POWER_SUPPLY_TYPE_BATTERY,
+    },
+    {
+        .properties = dummy_usb_props,
+        .num_properties = ARRAY_SIZE(dummy_usb_props),
+        .set_property = dummy_usb_set_property,
+        .get_property = dummy_usb_get_property,
+        .name = "usb",
+        .property_is_writeable = dummy_usb_property_is_writeable,
+        .type = POWER_SUPPLY_TYPE_MAINS,
+    }
+};
+
+static int __init dummy_power_init(void)
+{
+    struct power_supply_config psy_cfg = {};
+    int i;
+    int ret;
+
+    usb_property = (struct dummy_usb_property*)kmalloc(sizeof(struct dummy_usb_property),GFP_KERNEL);
+    usb_property->online = 1;
+
+    battery_property = (struct dummy_battery_property*)kmalloc(sizeof(struct dummy_battery_property),GFP_KERNEL);
+    battery_property->online = 1;
+
+    for (i = 0; i < ARRAY_SIZE(dummy_power_supplies); i++) {
+        dummy_power_supplies[i] = power_supply_register(NULL,
+                                                        &dummy_power_desc[i],
+                                                        &psy_cfg);
+        if (IS_ERR(dummy_power_supplies[i])) {
+            ret = PTR_ERR(dummy_power_supplies[i]);
+            goto failed;
+        }
+    }
+    return 0;
+failed:
+    while (--i >= 0)
+        power_supply_unregister(dummy_power_supplies[i]);
+    return ret;
+}
+
+static void __exit dummy_power_exit(void)
+{
+    int i;
+    for (i = 0; i < ARRAY_SIZE(dummy_power_supplies); i++) {
+        power_supply_unregister(dummy_power_supplies[i]);
+    }
+    kfree(usb_property);
+    kfree(battery_property);
+}
+
+module_init(dummy_power_init);
+module_exit(dummy_power_exit);
+
+MODULE_DESCRIPTION("Power supply driver for Android devices with no battery");
+MODULE_AUTHOR("Dave Rim davidrim@google.com");
+MODULE_LICENSE("GPL");
