432c99e27ca9edca5f531081b241de56edbeb95d
Troy Kisky <troy.kisky@boundarydevices.com>
2018-09-13 12:14:49 -0700

    spi-imx: bus_num=-1 if no device tree, from spi: imx: Using existing properties for chipselects
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/spi/spi-imx.c
+++ drivers/spi/spi-imx.c
@@ -1358,7 +1358,7 @@ static int spi_imx_probe(struct platform_device *pdev)
 	platform_set_drvdata(pdev, master);
 
 	master->bits_per_word_mask = SPI_BPW_RANGE_MASK(1, 32);
-	master->bus_num = pdev->id;
+	master->bus_num = np ? -1 : pdev->id;
 
 	spi_imx = spi_master_get_devdata(master);
 	spi_imx->bitbang.master = master;
