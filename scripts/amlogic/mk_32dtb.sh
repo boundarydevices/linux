fd9dacf2d13ec352ea05d74f53e97aaabbe0458a
Jianxin Pan <jianxin.pan@amlogic.com>
2018-09-14 16:53:47 +0800

    dts: arch32: sync dtb between aarch64 and aarch32
    PD#173683: remove unnecessary difference between aarch32 and aarch64
    
    Change-Id: I9a1248e3d38dadf9cc6c4bb5e0a6f8c5dbd24a0a
    Signed-off-by: Jianxin Pan <jianxin.pan@amlogic.com>

--- scripts/amlogic/mk_32dtb.sh
+++ scripts/amlogic/mk_32dtb.sh
@@ -3,8 +3,16 @@
 export CROSS_COMPILE=/opt/gcc-linaro-6.3.1-2017.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 
 make ARCH=arm meson64_a32_defconfig
-make ARCH=arm txl_t962_p321.dtb || echo "Compile dtb Fail !!"
-make ARCH=arm txlx_t962x_r311_1g.dtb || echo "Compile dtb Fail !!"
+
+make ARCH=arm gxl_p212_1g.dtb
+make ARCH=arm gxl_p212_2g.dtb
+make ARCH=arm meson8b_m200.dtb
+make ARCH=arm meson8b_m400.dtb
+make ARCH=arm meson8b_skt.dtb
+make ARCH=arm txl_t962_p321.dtb
+make ARCH=arm txlx_t962e_r321.dtb
+make ARCH=arm txlx_t962x_r311_1g.dtb
+make ARCH=arm txlx_t962x_r311_2g.dtb
 
 
 
