fb55590cd40dcc57c54b346ac06faf9a08973ee8
Chi-Hsien Lin <chi-hsien.lin@cypress.com>
2018-09-17 04:51:08 -0500

    brcmfmac: introduce module parameter to configure default PM mode
    Add module parameter max_pm to allow using PM_MAX as default power
    management mode. Default PM mode is set to PM_MAX when max_pm=1, and is
    set to PM_FAST when max_pm=0 or max_pm is not set.
    
    Signed-off-by: Chi-Hsien Lin <chi-hsien.lin@cypress.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.h
@@ -39,6 +39,7 @@ extern struct brcmf_mp_global_t brcmf_mp_global;
  * @roamoff: Firmware roaming off?
  * @eap_restrict: Not allow data tx/rx until 802.1X auth succeeds
  * @sdio_wq_highpri: Tasks submitted to SDIO workqueue will run immediately.
+ * @default_pm: default power management (PM) mode.
  * @ignore_probe_fail: Ignore probe failure.
  * @trivial_ccode_map: Assume firmware uses ISO3166 country codes with rev 0
  * @country_codes: If available, pointer to struct for translating country codes
@@ -52,6 +53,7 @@ struct brcmf_mp_device {
 	bool		iapp;
 	bool		eap_restrict;
 	bool		sdio_wq_highpri;
+	int		default_pm;
 	bool		ignore_probe_fail;
 	bool		trivial_ccode_map;
 	struct brcmfmac_pd_cc *country_codes;
