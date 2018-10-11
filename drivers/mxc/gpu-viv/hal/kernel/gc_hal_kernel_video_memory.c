1ebc77309cdbd9e306e7b04c67a016400d4a3f57
Richard Liu <xuegang.liu@nxp.com>
2018-10-12 00:56:56 +0800

    MA-12874 [#imx-1203] Fix random kernel panic in viv_gem_prime_import
    Fix random kernel panic in viv_gem_prime_import when run
    android CTS or monkey test. The panic reason is dmabuf size
    no meet page alignment request. Do buffer size page alignment
    when exported dmabuf from vidmem to fix the issue.
    
    Test case:
    while true;do monkey --pct-syskeys 0 -v 100000;done
    
    Kernel panic stack:
    [ 140.322048] PC is at drm_gem_private_object_init+0x58/0x5c
    [ 140.327532] LR is at viv_gem_prime_import+0xdc/0x11c
    [ 140.332497] pc : [<ffff000008718a00>] lr : [<ffff000008b7d6f0>] pstate: 40000145
    [ 140.339889] sp : ffff00000d45ba40
    [ 140.343199] x29: ffff00000d45ba40 x28: 000000000000000c
    [ 140.348507] x27: ffff00000d45bd58 x26: ffff8008ed08e200
    [ 140.353815] x25: ffff8008f6eab5b8 x24: 000000000000000c
    [ 140.359123] x23: ffff00000d45bd58 x22: ffff8008a34a4000
    [ 140.364431] x21: ffff8008f6eab000 x20: ffff000009c1a000
    [ 140.369739] x19: ffff80083d7ffa00 x18: 0000f1d2ac2ac8aa
    [ 140.375047] x17: 0000f1d2c70690e0 x16: ffff0000082add48
    [ 140.380356] x15: 0000000000000000 x14: 0000000000000000
    [ 140.385663] x13: 0000000000000001 x12: 00000000ffffffff
    [ 140.390971] x11: 0000f1d2ac2ac4f0 x10: 0000f1d2ac2ac4a8
    [ 140.396280] x9 : 0000000000000000 x8 : ffff80083d7ffb00
    [ 140.401587] x7 : 0000000000000000 x6 : 000000000000003f
    [ 140.406896] x5 : 0000000000000040 x4 : 0000000000000000
    [ 140.412204] x3 : 0000000000000040 x2 : 0000000000002040
    [ 140.417512] x1 : ffff80083d7ffa00 x0 : ffff8008f6eab000
    
    Change-Id: I787b0bcfd4e5e79996eda073fd1a12924c97d170
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>

--- drivers/mxc/gpu-viv/hal/kernel/gc_hal_kernel_video_memory.c
+++ drivers/mxc/gpu-viv/hal/kernel/gc_hal_kernel_video_memory.c
@@ -2947,6 +2947,7 @@ gckVIDMEM_NODE_Export(
         {
             physical = node->VidMem.memory->physical;
             bytes = node->VidMem.bytes;
+            bytes = bytes & (~(PAGE_SIZE -1));
         }
         else
         {
