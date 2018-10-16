8559e62c9c6b306927ee62b36119acf841a3166a
Troy Kisky <troy.kisky@boundarydevices.com>
2018-10-16 13:17:35 -0700

    video: fbdev: mxc: mipi_dsi: remove panel check
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/video/fbdev/mxc/mipi_dsi.h
+++ drivers/video/fbdev/mxc/mipi_dsi.h
@@ -144,8 +144,4 @@ void mipid_rm68191_get_lcd_videomode(struct fb_videomode **mode, int *size,
 int mipid_rm68191_lcd_setup(struct mipi_dsi_info *mipi_dsi);
 #endif
 
-#ifndef CONFIG_FB_MXC_TRULY_WVGA_SYNC_PANEL
-#error "Please configure MIPI LCD panel, we cannot find one!"
-#endif
-
 #endif
