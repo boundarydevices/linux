19b679cfeb5cb5d63f105eab67bd4e99a2215262
Roy Pledge <roy.pledge@nxp.com>
2018-10-25 16:55:53 -0400

    soc: fsl: dpio: Fix order restoration API for QBMan 5.0
    The mechanism for indicating to HW that a frame was dropped
    when performing HW order restoration changed in QBMan 5.0 to
    use a management command instead of a special enqueue command.
    This patch implements that change when running on a QBMan 5.0
    and above device.
    
    Signed-off-by: Roy Pledge <roy.pledge@nxp.com>

--- drivers/soc/fsl/dpio/qbman-portal.c
+++ drivers/soc/fsl/dpio/qbman-portal.c
@@ -19,6 +19,7 @@
 /* QBMan portal management command codes */
 #define QBMAN_MC_ACQUIRE       0x30
 #define QBMAN_WQCHAN_CONFIGURE 0x46
+#define QBMAN_MC_ORP           0x63
 
 /* CINH register offsets */
 #define QBMAN_CINH_SWP_EQCR_PI      0x800
@@ -1905,4 +1906,55 @@ void qbman_swp_get_irq_coalescing(struct qbman_swp *p, u32 *irq_threshold,
 		*irq_threshold = p->irq_threshold;
 	if (irq_holdoff)
 		*irq_holdoff = p->irq_holdoff;
+}
+
+struct qbman_orp_cmd_desc {
+	u8 verb;
+	u8 reserved;
+	u8 cid;
+	u8 reserved2;
+	u16 orpid;
+	u16 seqnum;
+	u8 reserved3[56];
+};
+
+struct qbman_orp_cmd_rslt {
+	u8 verb;
+	u8 rslt;
+	u8 cid;
+	u8 reserved1[61];
+};
+
+int qbman_orp_drop(struct qbman_swp *s, u16 orpid, u16 seqnum)
+{
+	struct qbman_orp_cmd_desc *p;
+	struct qbman_orp_cmd_rslt *r;
+	void *resp;
+
+	p = (struct qbman_orp_cmd_desc *)qbman_swp_mc_start(s);
+	if (!p)
+		return -EBUSY;
+
+	p->cid = 0x7;
+	p->orpid = cpu_to_le16(orpid);
+	p->seqnum = cpu_to_le16(seqnum);
+
+	resp = qbman_swp_mc_complete(s, p, QBMAN_MC_ORP);
+	if (!resp) {
+		pr_err("qbman: Drop sequence num %d orpid 0x%x failed, no response\n",
+		       seqnum, orpid);
+		return -EIO;
+	}
+	r = (struct qbman_orp_cmd_rslt *)resp;
+	/* Decode the outcome */
+	WARN_ON((r->verb & QBMAN_RESPONSE_VERB_MASK) != QBMAN_MC_ORP);
+
+	/* Determine success or failure */
+	if (r->rslt != QBMAN_MC_RSLT_OK) {
+		pr_err("Drop seqnum %d of prpid 0x%x failed, code=0x%02x\n",
+		       seqnum, orpid, r->rslt);
+		return -EIO;
+	}
+
+	return 0;
 }
