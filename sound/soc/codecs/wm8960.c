5650b950f495debd0f97f559fac056760c7af0d0
Vipul Kumar <vipul_kumar@mentor.com>
2018-11-30 15:13:34 +0530

    ASoC: wm8960: fixed compilation error
    As per commit e075fc17b708 ("ASoC: wm8960: replace codec to component")
    made changes to fix the compilation error.
    
    sound/soc/codecs/wm8960.c: In function ‘wm8960_i2c_probe’:
    sound/soc/codecs/wm8960.c:1438:8: error: implicit declaration of function ‘snd_soc_register_codec’; did you mean ‘snd_soc_register_dai’? [-Werror=implicit-function-declaration]
      ret = snd_soc_register_codec(&i2c->dev,
            ^~~~~~~~~~~~~~~~~~~~~~
            snd_soc_register_dai
    sound/soc/codecs/wm8960.c:1439:5: error: ‘soc_codec_dev_wm8960’ undeclared (first use in this function); did you mean ‘soc_component_dev_wm8960’?
        &soc_codec_dev_wm8960, &wm8960_dai, 1);
         ^~~~~~~~~~~~~~~~~~~~
         soc_component_dev_wm8960
    sound/soc/codecs/wm8960.c:1439:5: note: each undeclared identifier is reported only once for each function it appears in
    cc1: some warnings being treated as errors
    scripts/Makefile.build:305: recipe for target 'sound/soc/codecs/wm8960.o' failed
    make[3]: *** [sound/soc/codecs/wm8960.o] Error 1
    scripts/Makefile.build:546: recipe for target 'sound/soc/codecs' failed
    make[2]: *** [sound/soc/codecs] Error 2
    
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>
    Signed-off-by: Srikanth Krishnakar <Srikanth_Krishnakar@mentor.com>

--- sound/soc/codecs/wm8960.c
+++ sound/soc/codecs/wm8960.c
@@ -1435,8 +1435,8 @@ static int wm8960_i2c_probe(struct i2c_client *i2c,
 
 	pm_runtime_enable(&i2c->dev);
 
-	ret = snd_soc_register_codec(&i2c->dev,
-			&soc_codec_dev_wm8960, &wm8960_dai, 1);
+	ret = devm_snd_soc_register_component(&i2c->dev,
+			&soc_component_dev_wm8960, &wm8960_dai, 1);
 
 	return ret;
 }
