43eb4c09ebef7e43b79d52d4b7c641990ea835ee
zhang sanshan <pete.zhang@nxp.com>
2018-12-19 12:29:11 +0800

    Revert "MA-13786[Android] change the gfp_mask when md alloc reserved_buffers"
    This reverts commit 92408a1cc559e473e434365641b409d20220269f.
    
    Change-Id: I0dd1b0fa0fa5541b804b6a5110ebd8095f5bdcb7

--- drivers/md/dm-bufio.c
+++ drivers/md/dm-bufio.c
@@ -1730,7 +1730,7 @@ struct dm_bufio_client *dm_bufio_client_create(struct block_device *bdev, unsign
 	mutex_unlock(&dm_bufio_clients_lock);
 
 	while (c->need_reserved_buffers) {
-		struct dm_buffer *b = alloc_buffer(c, GFP_NOWAIT | __GFP_NORETRY | __GFP_NOMEMALLOC | __GFP_NOWARN);
+		struct dm_buffer *b = alloc_buffer(c, GFP_KERNEL);
 
 		if (!b) {
 			r = -ENOMEM;
