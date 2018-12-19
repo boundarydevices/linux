7036314549ed6e0625eae2a060b177c9c89fb1b1
Han Xu <han.xu@nxp.com>
2018-12-19 15:34:19 +0530

    MLK-10985: mtd: nand: support NAND on i.MX6UL
    support i.MX6UL GPMI NAND driver and removed the unnecessary clock
    per1_bch.
    
    Signed-off-by: Han Xu <b45815@freescale.com>
    
    During 4.14 rebase fixed for upstream moving clocks to gpmi_devdata
    
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>

--- drivers/mtd/nand/raw/gpmi-nand/gpmi-nand.h
+++ drivers/mtd/nand/raw/gpmi-nand/gpmi-nand.h
@@ -13,7 +13,7 @@
 #include <linux/dma-mapping.h>
 #include <linux/dmaengine.h>
 
-#define GPMI_CLK_MAX 5 /* MX6Q needs five clocks */
+#define GPMI_CLK_MAX 4 /* MX6Q needs four clocks */
 struct resources {
 	void __iomem  *gpmi_regs;
 	void __iomem  *bch_regs;
@@ -75,6 +75,7 @@ enum gpmi_type {
 	IS_MX6QP,
 	IS_MX6SX,
 	IS_MX7D,
+	IS_MX6UL,
 };
 
 struct gpmi_devdata {
@@ -206,7 +207,9 @@ void gpmi_copy_bits(u8 *dst, size_t dst_bit_off,
 #define GPMI_IS_MX6QP(x)	((x)->devdata->type == IS_MX6QP)
 #define GPMI_IS_MX6SX(x)	((x)->devdata->type == IS_MX6SX)
 #define GPMI_IS_MX7D(x)		((x)->devdata->type == IS_MX7D)
+#define GPMI_IS_MX6UL(x)	((x)->devdata->type == IS_MX6UL)
 
 #define GPMI_IS_MX6(x)		(GPMI_IS_MX6Q(x) || GPMI_IS_MX6QP(x) || \
-				 GPMI_IS_MX6SX(x) || GPMI_IS_MX7D(x))
+				 GPMI_IS_MX6SX(x) || GPMI_IS_MX6UL(x) || \
+				 GPMI_IS_MX7D(x))
 #endif
