f4bf3c6baec2ab096a67caa765f55910e6659c9c
fang hui <hui.fang@nxp.com>
2019-01-03 22:10:21 +0800

    MA-13882 [Android Pi] Enable wm8960 on car image
    The car audio will route different streams to different sound cards.
    Besides cs42888, enable wm8960 to make car image have 2 sound cards.
    
    To save boot time, if it's car image, don't sleep 100ms on codec probe.
    The 100ms sleep is not mentioned in the codec spec, maybe it's added by
    experience. Tested 10 cycle of unplugging and plugging power, each time
    play ok from wm8960.
    
    Change-Id: I6c3cca2c85bd419edc1edef987376f4096f24956
    Signed-off-by: fang hui <hui.fang@nxp.com>

--- sound/soc/codecs/wm8960.c
+++ sound/soc/codecs/wm8960.c
@@ -137,6 +137,7 @@ struct wm8960_priv {
 	int freq_in;
 	bool is_stream_in_use[2];
 	struct wm8960_data pdata;
+	bool quick_probe;
 };
 
 #define wm8960_reset(c)	regmap_write(c, WM8960_RESET, 0)
@@ -932,7 +933,8 @@ static int wm8960_set_bias_level_out3(struct snd_soc_component *component,
 
 			/* Enable & ramp VMID at 2x50k */
 			snd_soc_component_update_bits(component, WM8960_POWER1, 0x80, 0x80);
-			msleep(100);
+			if(wm8960->quick_probe == false)
+				msleep(100);
 
 			/* Enable VREF */
 			snd_soc_component_update_bits(component, WM8960_POWER1, WM8960_VREF,
@@ -1378,6 +1380,7 @@ static int wm8960_i2c_probe(struct i2c_client *i2c,
 	struct wm8960_priv *wm8960;
 	int ret;
 	int repeat_reset = 10;
+	struct device_node *np = i2c->dev.of_node;
 
 	wm8960 = devm_kzalloc(&i2c->dev, sizeof(struct wm8960_priv),
 			      GFP_KERNEL);
@@ -1398,6 +1401,11 @@ static int wm8960_i2c_probe(struct i2c_client *i2c,
 		memcpy(&wm8960->pdata, pdata, sizeof(struct wm8960_data));
 	else if (i2c->dev.of_node)
 		wm8960_set_pdata_from_of(i2c, &wm8960->pdata);
+
+	if (of_property_read_bool(np, "quick-probe"))
+		wm8960->quick_probe = true;
+
+	dev_info(&i2c->dev, "%s(), quick probe %d\n", __func__, wm8960->quick_probe);
 
 	do {
 		ret = wm8960_reset(wm8960->regmap);
