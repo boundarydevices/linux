aa6ab25009144c773d456906b37234952549115c
Bai Ping <b51503@freescale.com>
2019-01-04 11:52:49 +0530

    MLK-9777 cpufreq: imx6: fix the high bus count mismatch
    Normally, the system is booting up with higher cpufreq. In the
    cpufreq set_target_index we will release the high bus mode if
    the target cpu frequency is the lowest. It will release the high
    bus mode and dcrease the high_bus_count.This will lead to a wrong
    release of high bus mode. So, in the cpufreq_init function, if the
    original frequency is not the lowest, we need request high busfreq.
    
    Signed-off-by: Bai Ping <b51503@freescale.com>
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>

--- drivers/cpufreq/imx6q-cpufreq.c
+++ drivers/cpufreq/imx6q-cpufreq.c
@@ -240,10 +240,17 @@ static int imx6q_cpufreq_init(struct cpufreq_policy *policy)
 	int ret;
 
 	policy->clk = clks[ARM].clk;
+	policy->cur = clk_get_rate(clks[ARM].clk) / 1000;
 	ret = cpufreq_generic_init(policy, freq_table, transition_latency);
 	policy->suspend_freq = max_freq;
+	if (ret) {
+		dev_err(cpu_dev, "imx6 cpufreq init failed!\n");
+		return ret;
+	}
+	if (policy->cur > freq_table[0].frequency)
+		request_bus_freq(BUS_FREQ_HIGH);
 
-	return ret;
+	return 0;
 }
 
 static int imx6q_cpufreq_exit(struct cpufreq_policy *policy)
