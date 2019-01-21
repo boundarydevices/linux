15291ff2fcf90ccb2d0934fabd494a76e107a1dd
Olivier Masse <olivier.masse@nxp.com>
2019-01-21 11:45:53 +0100

    MMIOT-30 MA-13967-1 Add secure_ion.h
    secure_ion.h added in drivers includes to expose it in user space
    
    Change-Id: I3adeafc18323e751fccf954bf48e42f3ae34a40c
    Signed-off-by: Olivier Masse <olivier.masse@nxp.com>

---
+++ drivers/staging/android/uapi/secure_ion.h
@@ -0,0 +1,25 @@
+/*
+ * drivers/staging/android/uapi/secure_ion.h
+ *
+ * Copyright (C) 2019 NXP, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+
+#ifndef _LINUX_SECURE_ION_H
+#define _LINUX_SECURE_ION_H
+#define DWL_ION_ENCODED_BUFFER_OPTEE_HEAP_MASK         4
+#define DWL_ION_ENCODED_BUFFER_OPTEE_HEAP_NAME         "optee"
+#define DWL_ION_DECODED_BUFFER_DISPLAY_HEAP_MASK       2
+#define DWL_ION_DECODED_BUFFER_DISPLAY_HEAP_NAME       "display"
+#endif
+
