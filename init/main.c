a7811498baaec8918687324ec39ea7820778478d
Troy Kisky <troy.kisky@boundarydevices.com>
2019-02-18 01:39:32 -0800

    print last of command line again
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- init/main.c
+++ init/main.c
@@ -550,6 +550,7 @@ asmlinkage __visible void __init start_kernel(void)
 	page_alloc_init();
 
 	pr_notice("Kernel command line: %s\n", boot_command_line);
+	pr_notice("Kernel command line: %s\n", &boot_command_line[900]);
 	parse_early_param();
 	after_dashes = parse_args("Booting kernel",
 				  static_command_line, __start___param,
