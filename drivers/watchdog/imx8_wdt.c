d83596f41c8c5042b048144bb302aa17bd3e19c5
Robin Gong <yibin.gong@nxp.com>
2019-02-22 18:43:35 +0800

    MLK-20965 watchdog: imx8_wdt: remove watchdog action set
    The default watchdog action is partition reset now, so no need kernel
    to take care. Besides, scfw full test case may set other watchdog
    action but kernel may set it back later to default partition reset
    which scfw wouldn't expect, so avoid touching watchdog action.
    Please modify scfw code in case changing watchdog action to board
    reset.
    
    Signed-off-by: Robin Gong <yibin.gong@nxp.com>
    Reviewed-by: Anson Huang <anson.huang@nxp.com>
    (cherry picked from commit d9189e251995cc91efcac62a8d1c59ff32cdb8dc)
    (cherry picked from commit 13445b41c617042b309fb4cc5159a6d6ab9182fe)

--- drivers/watchdog/imx8_wdt.c
+++ drivers/watchdog/imx8_wdt.c
@@ -73,10 +73,8 @@ static int imx8_wdt_start(struct watchdog_device *wdog)
 			&res);
 	if (res.a0)
 		return res.a0;
-	/* TODO: change to SC_TIMER_WDOG_ACTION_PARTITION after SCFW support */
-	arm_smccc_smc(FSL_SIP_SRTC, FSL_SIP_SRTC_SET_WDOG_ACT,
-			SC_TIMER_WDOG_ACTION_PARTITION, 0, 0, 0, 0, 0, &res);
-	return res.a0;
+
+	return 0;
 }
 
 static int imx8_wdt_stop(struct watchdog_device *wdog)
