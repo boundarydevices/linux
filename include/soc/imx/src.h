102f052bba1a6a8d6d95d9985ac2fe820442d7f1
Leonard Crestez <leonard.crestez@nxp.com>
2019-03-01 21:00:06 +0200

    MLK-21052-09 soc: imx: Fix building without HAVE_IMX_SRC
    If support for imx_src_is_m4_enabled is not compiled then just return
    the default value "false".
    
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>

--- include/soc/imx/src.h
+++ include/soc/imx/src.h
@@ -1,6 +1,10 @@
 #ifndef __SOC_IMX_SRC_H
 #define __SOC_IMX_SRC_H
 
+#if defined(CONFIG_HAVE_IMX_SRC)
 bool imx_src_is_m4_enabled(void);
+#else
+static inline bool imx_src_is_m4_enabled(void) { return false; }
+#endif
 
 #endif /* __SOC_IMX_SRC_H */
