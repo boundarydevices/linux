77f4d65e9602f574aefeef7ce75577f4f972c741
ruixuan.li <ruixuan.li@amlogic.com>
2019-04-02 11:11:56 +0800

    tm2: emmc run hs200 busmode [1/1]
    PD#SWPL-5658
    
    Problem:
    emmc run high speed now
    
    Solution:
    modify dts
    
    Verify:
    passed on t962e2_ab319
    
    Change-Id: Iedef30bed9547e7f57c883077462f1762c55fda3
    Signed-off-by: ruixuan.li <ruixuan.li@amlogic.com>
    Signed-off-by: Luan Yuan <luan.yuan@amlogic.com>

--- drivers/amlogic/mmc/aml_sd_emmc_v3.c
+++ drivers/amlogic/mmc/aml_sd_emmc_v3.c
@@ -241,7 +241,7 @@ static int meson_mmc_clk_set_rate_v3(struct mmc_host *mmc,
 					host->mux_parent[0]);
 			if (ret)
 				pr_warn("set comp0 as mux_clk parent error\n");
-		} else if (((host->data->chip_type == MMC_CHIP_TL1)
+		} else if (((host->data->chip_type >= MMC_CHIP_TL1)
 				|| (host->data->chip_type == MMC_CHIP_G12B))
 				&& (clk_ios >= 166000000)) {
 			src0_clk = devm_clk_get(host->dev, "clkin2");
