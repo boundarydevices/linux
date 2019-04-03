ccd121acdb363e80a3cf00faaee1c9ff335e47a5
Guoniu.Zhou <guoniu.zhou@nxp.com>
2019-04-03 16:13:44 +0800

    MLK-20191-1: media: mxc: fix unsigned compare
    Fix coverity CID 17388, 17389 and 17363 issue
    Because the type of variable is unsigned, so compare with zero is no
    effect
    
    Signed-off-by: Guoniu.Zhou <guoniu.zhou@nxp.com>

--- drivers/media/platform/mxc/capture/mxc_v4l2_capture.c
+++ drivers/media/platform/mxc/capture/mxc_v4l2_capture.c
@@ -328,7 +328,7 @@ static int mxc_v4l2_buffer_status(cam_data *cam, struct v4l2_buffer *buf)
 {
 	pr_debug("In MVC:mxc_v4l2_buffer_status\n");
 
-	if (buf->index < 0 || buf->index >= FRAME_NUM) {
+	if (buf->index >= FRAME_NUM) {
 		pr_err("ERROR: v4l2 capture: mxc_v4l2_buffer_status buffers "
 		       "not allocated\n");
 		return -EINVAL;
@@ -348,7 +348,7 @@ static int mxc_v4l2_prepare_bufs(cam_data *cam, struct v4l2_buffer *buf)
 {
 	pr_debug("In MVC:mxc_v4l2_prepare_bufs\n");
 
-	if (buf->index < 0 || buf->index >= FRAME_NUM || buf->length <
+	if (buf->index >= FRAME_NUM || buf->length <
 			PAGE_ALIGN(cam->v2f.fmt.pix.sizeimage)) {
 		pr_err("ERROR: v4l2 capture: mxc_v4l2_prepare_bufs buffers "
 			"not allocated,index=%d, length=%d\n", buf->index,
