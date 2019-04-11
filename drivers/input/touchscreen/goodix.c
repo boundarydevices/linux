2b26b785117c197ebb1ab1bbf5e8009870750572
Troy Kisky <troy.kisky@boundarydevices.com>
2019-04-11 13:52:46 -0700

    Input: goodix: register the touchscreen after firmware loading is complete
    This prevents a deadlock waiting for completion in the open function.
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/input/touchscreen/goodix.c
+++ drivers/input/touchscreen/goodix.c
@@ -1343,7 +1343,6 @@ static int goodix_i2c_test(struct i2c_client *client)
  */
 static int goodix_configure_dev(struct goodix_ts_data *ts)
 {
-	int error;
 	int i;
 
 	ts->int_trigger_type = GOODIX_INT_TRIGGER;
@@ -1387,6 +1386,12 @@ static int goodix_configure_dev(struct goodix_ts_data *ts)
 
 	/* Read configuration and apply touchscreen parameters */
 	goodix_read_config(ts);
+	return 0;
+}
+
+static int goodix_finish_setup(struct goodix_ts_data *ts)
+{
+	int error;
 
 	/* Try overriding touchscreen parameters via device properties */
 	touchscreen_parse_properties(ts->input_dev, true, &ts->prop);
@@ -1491,6 +1496,11 @@ static void goodix_config_cb(const struct firmware *cfg, void *ctx)
 	/* Must not suspend immediately after device initialization */
 	pm_runtime_mark_last_busy(&ts->client->dev);
 	pm_request_autosuspend(&ts->client->dev);
+
+	release_firmware(cfg);
+	complete_all(&ts->firmware_loading_complete);
+	goodix_finish_setup(ts);
+	return;
 
 err_release_cfg:
 	release_firmware(cfg);
@@ -1624,6 +1634,9 @@ reset:
 		return 0;
 	} else {
 		error = goodix_configure_dev(ts);
+		if (error)
+			return error;
+		error = goodix_finish_setup(ts);
 		if (error)
 			return error;
 	}
