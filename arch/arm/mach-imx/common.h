8e453ad3b1d2ef4d707a363b8dfff7697f937734
Anson Huang <Anson.Huang@nxp.com>
2019-04-12 15:11:09 +0800

    ARM: imx: add i.MX7D bus-freq support
    Add i.MX7D bus-freq support, it supports High/Low/Audio bus
    mode.
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>

--- arch/arm/mach-imx/common.h
+++ arch/arm/mach-imx/common.h
@@ -88,6 +88,8 @@ void imx6_set_int_mem_clk_lpm(bool enable);
 void imx6sl_set_wait_clk(bool enter);
 int imx_mmdc_get_ddr_type(void);
 int imx7ulp_set_lpm(enum ulp_cpu_pwr_mode mode);
+void imx_busfreq_map_io(void);
+void imx7_pm_map_io(void);
 
 void imx_cpu_die(unsigned int cpu);
 int imx_cpu_kill(unsigned int cpu);
@@ -103,6 +105,12 @@ static inline void imx6_suspend(void __iomem *ocram_vbase) {}
 #endif
 
 void v7_cpu_resume(void);
+
+#ifdef CONFIG_HAVE_IMX_DDRC
+int imx_ddrc_get_ddr_type(void);
+#else
+static inline int imx_ddrc_get_ddr_type(void) { return 0; }
+#endif
 
 void imx6_pm_ccm_init(const char *ccm_compat);
 void imx6q_pm_init(void);
