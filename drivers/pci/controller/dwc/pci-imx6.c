393e00e2a130beaf6d9067fb5e77f334b4b60b89
Arulpandiyan Vadivel <arulpandiyan_vadivel@mentor.com>
2019-04-24 19:09:51 +0530

    Revert "PCI: imx6: Fix link training status detection in link up check"
    This reverts commit 2a031cab71cd9909fa659c96b6221d1fe33ad87f.
    
    Signed-off-by: Shrikant Bobade <Shrikant_Bobade@mentor.com>
    (cherry picked from commit 91d66f481121b2eab56f5d829a0002f6bb47953e)

--- drivers/pci/controller/dwc/pci-imx6.c
+++ drivers/pci/controller/dwc/pci-imx6.c
@@ -123,6 +123,8 @@ struct imx_pcie {
 
 #define PCIE_PHY_DEBUG_R0 (PL_OFFSET + 0x28)
 #define PCIE_PHY_DEBUG_R1 (PL_OFFSET + 0x2c)
+#define PCIE_PHY_DEBUG_R1_XMLH_LINK_IN_TRAINING	(1 << 29)
+#define PCIE_PHY_DEBUG_R1_XMLH_LINK_UP		(1 << 4)
 
 #define PCIE_PHY_CTRL (PL_OFFSET + 0x114)
 #define PCIE_PHY_CTRL_DATA_LOC 0
@@ -1651,6 +1653,12 @@ static const struct dw_pcie_host_ops imx_pcie_host_ops = {
 	.host_init = imx_pcie_host_init,
 };
 
+static int imx6_pcie_link_up(struct dw_pcie *pci)
+{
+	return dw_pcie_readl_dbi(pci, PCIE_PHY_DEBUG_R1) &
+			PCIE_PHY_DEBUG_R1_XMLH_LINK_UP;
+}
+
 static int imx_add_pcie_port(struct imx_pcie *imx_pcie,
 			      struct platform_device *pdev)
 {
@@ -1679,7 +1687,7 @@ static int imx_add_pcie_port(struct imx_pcie *imx_pcie,
 }
 
 static const struct dw_pcie_ops dw_pcie_ops = {
-	/* No special ops needed, but pcie-designware still expects this struct */
+	.link_up = imx6_pcie_link_up,
 };
 
 static ssize_t imx_pcie_bar0_addr_info(struct device *dev,
