1bda9c24ee45b99bb0fc961a71501b116aa9ba2b
Joakim Zhang <qiangqing.zhang@nxp.com>
2019-04-29 13:11:36 +0800

    MLK-21597 can: flexcan: fix deadlock when can't call flexcan_noirq_resume()
    There is a limitation about wakeup event mechanisim in driver:
    
    Other devices may suspend failed after system calling flexcan_suspend()
    and before calling flexcan_noirq_suspend(), the result is that
    flexcan_noirq_resume() will not be called. This will leave flexcan in
    stop mode, and with the current driver it can't recover from this even
    with a soft reboot, it requires a hard reboot.
    
    This patch is intends to fix the scenario.
    
    Reviewed-by: Fugang Duan <fugang.duan@nxp.com>
    Signed-off-by: Joakim Zhang <qiangqing.zhang@nxp.com>

--- drivers/net/can/flexcan.c
+++ drivers/net/can/flexcan.c
@@ -369,6 +369,7 @@ struct flexcan_priv {
 	sc_ipc_t ipc_handle;
 #endif
 	bool wakeup;
+	bool in_stop_mode;
 	int stby_gpio;
 	enum of_gpio_flags stby_gpio_flags;
 
@@ -1921,6 +1922,7 @@ static int __maybe_unused flexcan_suspend(struct device *device)
 		if (device_may_wakeup(device)) {
 			enable_irq_wake(dev->irq);
 			flexcan_enter_stop_mode(priv);
+			priv->in_stop_mode = true;
 		} else {
 			flexcan_chip_stop(dev);
 			ret = pm_runtime_force_suspend(device);
@@ -1945,6 +1947,11 @@ static int __maybe_unused flexcan_resume(struct device *device)
 		netif_start_queue(dev);
 
 		if (device_may_wakeup(device)) {
+			if (priv->in_stop_mode) {
+				disable_irq_wake(dev->irq);
+				flexcan_exit_stop_mode(priv);
+				priv->in_stop_mode = false;
+			}
 			flexcan_wake_mask_disable(priv);
 		} else {
 			pinctrl_pm_select_default_state(device);
@@ -1999,6 +2006,7 @@ static int __maybe_unused flexcan_noirq_resume(struct device *device)
 	if (netif_running(dev) && device_may_wakeup(device)) {
 		disable_irq_wake(dev->irq);
 		flexcan_exit_stop_mode(priv);
+		priv->in_stop_mode = false;
 	}
 
 	return 0;
