aa972dfa4124dcc39fe2833b6dc32e782238c491
ming_qian <ming.qian@nxp.com>
2019-05-07 13:09:19 +0800

    MLK-21655: VPU Decoder: driver supply an interface to get the firmware log
    get the firmware log:
    cat /sys/devices/platform/2c000000.vpu_decoder/fwlog
    
    Signed-off-by: ming_qian <ming.qian@nxp.com>

--- drivers/mxc/vpu_malone/vpu_b0.h
+++ drivers/mxc/vpu_malone/vpu_b0.h
@@ -216,6 +216,17 @@ struct queue_data {
 	struct vb2_data_req vb2_reqs[VPU_MAX_BUFFER];
 	enum QUEUE_TYPE type;
 };
+
+struct print_buf_desc {
+	u32 start_h_phy;
+	u32 start_h_vir;
+	u32 start_m;
+	u32 bytes;
+	u32 read;
+	u32 write;
+	char buffer[0];
+};
+
 struct vpu_ctx;
 struct vpu_dev {
 	struct device *generic_dev;
@@ -254,6 +265,8 @@ struct vpu_dev {
 	struct shared_addr shared_mem;
 	struct vpu_ctx *ctx[VPU_MAX_NUM_STREAMS];
 	struct dentry *debugfs_root;
+
+	struct print_buf_desc *print_buf;
 };
 
 struct vpu_statistic {
