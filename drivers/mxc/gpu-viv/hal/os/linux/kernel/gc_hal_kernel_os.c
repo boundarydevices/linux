dd841d1e4e51f168da1540e507f601c8659bfbbc
Xianzhong <xianzhong.li@nxp.com>
2019-05-17 01:03:55 +0800

    MGS-4716-2 [#imx-1412] set contiguous 4GB flag for CMA limit
    CMA limit shall not allocate non-contiguous 4GB memory
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- drivers/mxc/gpu-viv/hal/os/linux/kernel/gc_hal_kernel_os.c
+++ drivers/mxc/gpu-viv/hal/os/linux/kernel/gc_hal_kernel_os.c
@@ -3064,6 +3064,15 @@ gckOS_AllocatePagedMemoryEx(
         gcmkONERROR(gcvSTATUS_OUT_OF_MEMORY);
     }
 
+#if defined(CONFIG_ZONE_DMA32) && LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,37)
+    /* redirect DMA32 pool for CMA LIMIT request */
+    if (Flag & gcvALLOC_FLAG_CMA_LIMIT)
+    {
+        Flag &= ~gcvALLOC_FLAG_CMA_LIMIT;
+        Flag |= gcvALLOC_FLAG_4GB_ADDR | gcvALLOC_FLAG_CONTIGUOUS;
+    }
+#endif
+
     if (Os->allocatorLimitMarker && (Flag & gcvALLOC_FLAG_CMA_LIMIT))
     {
         Flag &= ~gcvALLOC_FLAG_CACHEABLE;
