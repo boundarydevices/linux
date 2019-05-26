d21f852ea8f46c2d21958103b063ab636797afe9
Jian Xu <jian.xu@amlogic.com>
2019-05-26 15:05:05 +0800

    audio: auge:  fix the samesource spdif clock recovery issue [1/1]
    PD#SWPL-3667
    
    Problem:
    after playback none-48K raw audio, the spdif clock
    is not recoved to 48K when tdm/spdif same source
    
    Solution:
    use the same clock source as tdm if samesource and
    config that when tdm hardware prepare.
    
    Verify:
    u212
    
    Change-Id: I2a847273ab0b920f6cee691ee0db006709ffde2a
    Signed-off-by: Jian Xu <jian.xu@amlogic.com>

---
+++ sound/soc/amlogic/auge/spdif.h
@@ -0,0 +1,32 @@
+/*
+ * sound/soc/amlogic/auge/spdif.h
+ *
+ * Copyright (C) 2017 Amlogic, Inc. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ */
+
+#ifndef __AML_SPDIF_H__
+#define __AML_SPDIF_H__
+#include <linux/clk.h>
+#if 0
+enum SPDIF_ID {
+	SPDIF_A,
+	SPDIF_B,
+	SPDIF_ID_CNT
+};
+#endif
+
+extern int spdif_set_audio_clk(int id,
+		struct clk *clk_src, int rate, int same);
+
+#endif
