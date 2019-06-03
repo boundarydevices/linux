17b7df8eb7dbf46582ae177d29011b8ffe478510
Li Jun <jun.li@nxp.com>
2019-06-03 18:50:46 +0800

    MLK-21928 usb: chipidea: add spin lock for all hard irq
    Currently ci->lock is only used for udc irq, to cover all
    hard irqs, add spin lock for ci_irq, meanwhile, add spin
    lock for low power mode and wakeup irq flag.
    
    Acked-by: Peter Chen <peter.chen@nxp.com>
    Signed-off-by: Li Jun <jun.li@nxp.com>

--- drivers/usb/chipidea/core.c
+++ drivers/usb/chipidea/core.c
@@ -586,17 +586,21 @@ static irqreturn_t ci_irq(int irq, void *data)
 	irqreturn_t ret = IRQ_NONE;
 	u32 otgsc = 0;
 
+	spin_lock(&ci->lock);
 	if (ci->in_lpm) {
 		/*
 		 * If we already have a wakeup irq pending there,
 		 * let's just return to wait resume finished firstly.
 		 */
-		if (ci->wakeup_int)
+		if (ci->wakeup_int) {
+			spin_unlock(&ci->lock);
 			return IRQ_HANDLED;
+		}
 
 		disable_irq_nosync(irq);
 		ci->wakeup_int = true;
 		pm_runtime_get(ci->dev);
+		spin_unlock(&ci->lock);
 		return IRQ_HANDLED;
 	}
 
@@ -606,8 +610,10 @@ static irqreturn_t ci_irq(int irq, void *data)
 		otgsc = hw_read_otgsc(ci, ~0);
 		if (ci_otg_is_fsm_mode(ci)) {
 			ret = ci_otg_fsm_irq(ci);
-			if (ret == IRQ_HANDLED)
+			if (ret == IRQ_HANDLED) {
+				spin_unlock(&ci->lock);
 				return ret;
+			}
 		}
 
 		/*
@@ -636,6 +642,7 @@ static irqreturn_t ci_irq(int irq, void *data)
 			hw_write_otgsc(ci, OTGSC_INT_STATUS_BITS,
 				otgsc & (OTGSC_IDIS | OTGSC_BSVIS));
 			ci_otg_queue_work(ci);
+			spin_unlock(&ci->lock);
 			return IRQ_HANDLED;
 		}
 	}
@@ -644,6 +651,7 @@ static irqreturn_t ci_irq(int irq, void *data)
 	if (ci->role != CI_ROLE_END)
 		ret = ci_role(ci)->irq(ci);
 
+	spin_unlock(&ci->lock);
 	return ret;
 }
 
@@ -1376,15 +1384,19 @@ static int ci_controller_resume(struct device *dev)
 		hw_wait_phy_stable();
 	}
 
+	spin_lock(&ci->lock);
 	ci->in_lpm = false;
 	if (ci->wakeup_int) {
 		ci->wakeup_int = false;
+		spin_unlock(&ci->lock);
 		pm_runtime_mark_last_busy(ci->dev);
 		pm_runtime_put_autosuspend(ci->dev);
 		enable_irq(ci->irq);
 		if (ci_otg_is_fsm_mode(ci))
 			ci_otg_fsm_wakeup_by_srp(ci);
 		ci_extcon_wakeup_int(ci);
+	} else {
+		spin_unlock(&ci->lock);
 	}
 
 	return 0;
--- drivers/usb/chipidea/udc.c
+++ drivers/usb/chipidea/udc.c
@@ -1866,12 +1866,9 @@ static irqreturn_t udc_irq(struct ci_hdrc *ci)
 	if (ci == NULL)
 		return IRQ_HANDLED;
 
-	spin_lock(&ci->lock);
-
 	if (ci->platdata->flags & CI_HDRC_REGS_SHARED) {
 		if (hw_read(ci, OP_USBMODE, USBMODE_CM) !=
 				USBMODE_CM_DC) {
-			spin_unlock(&ci->lock);
 			return IRQ_NONE;
 		}
 	}
@@ -1919,7 +1916,6 @@ static irqreturn_t udc_irq(struct ci_hdrc *ci)
 	} else {
 		retval = IRQ_NONE;
 	}
-	spin_unlock(&ci->lock);
 
 	return retval;
 }
