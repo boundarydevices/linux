3f4ff4e13d5e37a761c308e86f86a00e1fa71895
Li Jun <jun.li@nxp.com>
2019-06-03 18:50:46 +0800

    MLK-21928 usb: chipidea: add spin lock for all hard irq
    Currently ci->lock is only used for udc irq, to cover all
    hard irqs, add spin lock for ci_irq, meanwhile, add spin
    lock for low power mode and wakeup irq flag.
    
    Acked-by: Peter Chen <peter.chen@nxp.com>
    Signed-off-by: Li Jun <jun.li@nxp.com>

--- drivers/usb/chipidea/udc.c
+++ drivers/usb/chipidea/udc.c
@@ -2022,12 +2022,9 @@ static irqreturn_t udc_irq(struct ci_hdrc *ci)
 	if (ci == NULL)
 		return IRQ_HANDLED;
 
-	spin_lock(&ci->lock);
-
 	if (ci->platdata->flags & CI_HDRC_REGS_SHARED) {
 		if (hw_read(ci, OP_USBMODE, USBMODE_CM) !=
 				USBMODE_CM_DC) {
-			spin_unlock(&ci->lock);
 			return IRQ_NONE;
 		}
 	}
@@ -2075,7 +2072,6 @@ static irqreturn_t udc_irq(struct ci_hdrc *ci)
 	} else {
 		retval = IRQ_NONE;
 	}
-	spin_unlock(&ci->lock);
 
 	return retval;
 }
