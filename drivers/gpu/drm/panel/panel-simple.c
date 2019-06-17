a916db8b7ac131c67147050df99d778ad5cc2656
Troy Kisky <troy.kisky@boundarydevices.com>
2019-06-17 16:00:22 -0700

    gpu: drm: panel-simple: add S_IF_1_LANE/S_IF_4_LANES support
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -143,6 +143,7 @@ static int send_mipi_cmd_list(struct panel_simple *panel, struct mipi_cmd *mc)
 	int generic;
 	int match = 0;
 	int readval, matchval;
+	int skip = 0;
 
 	pr_debug("%s:%d\n", __func__, length);
 	if (!cmd || !length)
@@ -153,6 +154,15 @@ static int send_mipi_cmd_list(struct panel_simple *panel, struct mipi_cmd *mc)
 	while (1) {
 		len = *cmd++;
 		length--;
+		if (!length)
+			break;
+		if ((len >= S_IF_1_LANE) && (len <= S_IF_4_LANES)) {
+			int lane_match = 1 + len - S_IF_1_LANE;
+
+			if (lane_match != dsi->lanes)
+				skip = 1;
+			continue;
+		}
 		generic = len & 0x80;
 		len &= 0x7f;
 
@@ -166,43 +176,49 @@ static int send_mipi_cmd_list(struct panel_simple *panel, struct mipi_cmd *mc)
 				dev_err(&dsi->dev, "Unexpected end of data\n");
 				break;
 			}
-			if (generic)
-				ret = mipi_dsi_generic_write(dsi, cmd, len);
-			else
-				ret = mipi_dsi_dcs_write_buffer(dsi, cmd, len);
+			if (!skip) {
+				if (generic)
+					ret = mipi_dsi_generic_write(dsi, cmd, len);
+				else
+					ret = mipi_dsi_dcs_write_buffer(dsi, cmd, len);
+			}
 		} else if (len == S_MRPS) {
 				ret = mipi_dsi_set_maximum_return_packet_size(
 					dsi, cmd[0]);
 				len = 1;
 		} else if (len == S_DCS_READ) {
 			data[0] = 0;
-			ret =  mipi_dsi_dcs_read(dsi, cmd[0], data, 1);
-			pr_debug("Read DCS(%d): (%x) %x cmp %x\n",
-				ret, cmd[0], data[0], cmd[1]);
+			if (!skip) {
+				ret =  mipi_dsi_dcs_read(dsi, cmd[0], data, 1);
+				pr_debug("Read DCS(%d): (%x) %x cmp %x\n",
+					ret, cmd[0], data[0], cmd[1]);
+				if (data[0] != cmd[1])
+					match = -EINVAL;
+			}
 			len = 2;
-			if (data[0] != cmd[1])
-				match = -EINVAL;
 		} else if (len == S_DCS_READ4) {
 			data[0] = data[1] = data[2] = data[3] = 0;
-			if (generic) {
-				ret =  mipi_dsi_generic_read(dsi, cmd, 2, data, 4);
-				readval = data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
-				matchval = cmd[2] | (cmd[3] << 8) | (cmd[4] << 16) | (cmd[5] << 24);
-				pr_debug("Read GEN(%d): (%02x %02x) %08x cmp %08x\n",
-					ret, cmd[0], cmd[1], readval, matchval);
-				len = 6;
-			} else {
-				ret =  mipi_dsi_dcs_read(dsi, cmd[0], data, 4);
-				readval = data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
-				matchval = cmd[1] | (cmd[2] << 8) | (cmd[3] << 16) | (cmd[4] << 24);
-				pr_debug("Read DCS(%d): (%x) %x cmp %x\n",
-					ret, cmd[0], readval, matchval);
-				len = 5;
+			len = generic ? 6 : 5;
+			if (!skip) {
+				if (generic) {
+					ret =  mipi_dsi_generic_read(dsi, cmd, 2, data, 4);
+					readval = data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
+					matchval = cmd[2] | (cmd[3] << 8) | (cmd[4] << 16) | (cmd[5] << 24);
+					pr_debug("Read GEN(%d): (%02x %02x) %08x cmp %08x\n",
+						ret, cmd[0], cmd[1], readval, matchval);
+				} else {
+					ret =  mipi_dsi_dcs_read(dsi, cmd[0], data, 4);
+					readval = data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
+					matchval = cmd[1] | (cmd[2] << 8) | (cmd[3] << 16) | (cmd[4] << 24);
+					pr_debug("Read DCS(%d): (%x) %x cmp %x\n",
+						ret, cmd[0], readval, matchval);
+				}
+				if (readval != matchval)
+					match = -EINVAL;
 			}
-			if (readval != matchval)
-				match = -EINVAL;
 		} else if (len == S_DELAY) {
-			msleep(cmd[0]);
+			if (!skip)
+				msleep(cmd[0]);
 			len = 1;
 			if (length <= len)
 				break;
@@ -226,15 +242,17 @@ static int send_mipi_cmd_list(struct panel_simple *panel, struct mipi_cmd *mc)
 			}
 			return ret;
 		} else {
-			if (len == 6) {
-				pr_debug("Sent DCS (%d), (%d)%02x %02x: %02x %02x %02x %02x\n",
-					ret, len, cmd[0], cmd[1], cmd[2], cmd[3], cmd[4], cmd[5]);
-			} else if (len == 1) {
-				pr_debug("Sent DCS (%d), (%d)%02x\n",
-					ret, len, cmd[0]);
-			} else if (len) {
-				pr_debug("Sent DCS (%d), (%d)%02x %02x\n",
-					ret, len, cmd[0], cmd[1]);
+			if (!skip) {
+				if (len == 6) {
+					pr_debug("Sent DCS (%d), (%d)%02x %02x: %02x %02x %02x %02x\n",
+						ret, len, cmd[0], cmd[1], cmd[2], cmd[3], cmd[4], cmd[5]);
+				} else if (len == 1) {
+					pr_debug("Sent DCS (%d), (%d)%02x\n",
+						ret, len, cmd[0]);
+				} else if (len) {
+					pr_debug("Sent DCS (%d), (%d)%02x %02x\n",
+							ret, len, cmd[0], cmd[1]);
+				}
 			}
 		}
 		if (length < len) {
@@ -245,6 +263,7 @@ static int send_mipi_cmd_list(struct panel_simple *panel, struct mipi_cmd *mc)
 		length -= len;
 		if (!length)
 			break;
+		skip = 0;
 	}
 	return match;
 };
