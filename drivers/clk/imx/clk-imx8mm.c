342415117bbf3b6a835dea5a5f9574f22f2e300a
Peng Fan <peng.fan@nxp.com>
2019-06-19 18:18:51 +0800

    MLK-22049 imx8mm: fix audio pll setting
    The AUDIO PLL max support 650M, so the original clk settings violate
    spec. In order not to impact audio functionality, let's div the
    clk by 2.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Tested-by:  Viorel Suman <viorel.suman@nxp.com>
    Reviewed-by: Viorel Suman <viorel.suman@nxp.com>
    (cherry picked from commit a4b0c7c3fadc7903cb14ba1e9376967adead93b1)

--- drivers/clk/imx/clk-imx8mm.c
+++ drivers/clk/imx/clk-imx8mm.c
@@ -77,8 +77,8 @@ static const struct imx_int_pll_rate_table imx8mm_intpll_tbl[] = {
 };
 
 static const struct imx_int_pll_rate_table imx8mm_audiopll_tbl[] = {
-	PLL_1443X_RATE(786432000U, 262, 2, 2, 9437),
-	PLL_1443X_RATE(722534400U, 361, 3, 2, 17511),
+	PLL_1443X_RATE(393216000U, 262, 2, 3, 9437),
+	PLL_1443X_RATE(361267200U, 361, 3, 3, 17511),
 };
 
 static const struct imx_int_pll_rate_table imx8mm_videopll_tbl[] = {
