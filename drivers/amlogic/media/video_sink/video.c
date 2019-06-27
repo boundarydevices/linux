aec735f9b2fdc80ab49590367b508bf454bf350a
shuanglong.wang <shuanglong.wang@amlogic.com>
2019-06-27 16:08:49 +0800

    video: move a print info to debug control [1/1]
    PD#SWPL-10306
    
    Problem:
    omxpts is not update print too many
    
    Solution:
    move a print info to debug control, the condition to check
    whether omxpts is not set for while is not accurate
    
    Verify:
    verify by mainline
    
    Change-Id: I387cc6aee718182a8ff99838e9741443236dd1f9
    Signed-off-by: shuanglong.wang <shuanglong.wang@amlogic.com>

--- drivers/amlogic/media/video_sink/video.c
+++ drivers/amlogic/media/video_sink/video.c
@@ -6331,7 +6331,8 @@ static irqreturn_t vsync_isr_in(int irq, void *dev_id)
 		unsigned long delta1 = 0;
 
 		diff = system_time - omx_pts;
-		if (time_setomxpts > 0) {
+		if (time_setomxpts > 0
+			&& time_setomxpts_last > 0) {
 			/* time_setomxpts record hwc setomxpts time, */
 			/* when check  diff between pcr and  omx_pts, */
 			/* add compensation will let omx_pts and pcr */
@@ -6344,7 +6345,8 @@ static irqreturn_t vsync_isr_in(int irq, void *dev_id)
 				(4 * vsync_pts_inc * 1000 / 90)) {
 				time_setomxpts = 0;
 				time_setomxpts_last = 0;
-				pr_info("omxpts is not update for a while,do not need compenstate\n");
+				if (debug_flag & DEBUG_FLAG_PTS_TRACE)
+					pr_info("omxpts is not update for a while,do not need compenstate\n");
 			} else {
 				diff -=  delta1 * 90 / 1000;
 			}
