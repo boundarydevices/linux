97b739e771f2e04a9291fe63284d4b9b7de262ff
Shengjiu Wang <shengjiu.wang@nxp.com>
2019-07-16 13:31:44 +0800

    MLK-22022: ASoC: imx-si476x: Fix Failure at capture
    With command: arecord -D hw:$num,0 -f dat | aplay -D hw:0,0 -f dat &
    
    There is error:
    fsl-ssi-dai 202c000.ssi: invalid slot width: 32
    fsl-ssi-dai 202c000.ssi: failed to set dai tdm slot
    imx-tuner-si476x sound-fm: ASoC: machine hw_params failed: -22
    
    The reason is that slot width 32 is not valid and and set_sysclk
    api is removed by commit b0a7043d5c2c ("ASoC: fsl_ssi: Caculate bit clock
    rate using slot number and width").
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>
    Reviewed-by: Viorel Suman <viorel.suman@nxp.com>

--- sound/soc/fsl/imx-si476x.c
+++ sound/soc/fsl/imx-si476x.c
@@ -55,9 +55,6 @@ static int imx_si476x_hw_params(struct snd_pcm_substream *substream,
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
-	u32 channels = params_channels(params);
-	u32 rate = params_rate(params);
-	u32 bclk = rate * channels * 32;
 	int ret = 0;
 
 	/* set cpu DAI configuration */
@@ -67,19 +64,6 @@ static int imx_si476x_hw_params(struct snd_pcm_substream *substream,
 		dev_err(cpu_dai->dev, "failed to set dai fmt\n");
 		return ret;
 	}
-
-	ret = snd_soc_dai_set_tdm_slot(cpu_dai,
-			channels == 1 ? 1 : 0x3,
-			channels == 1 ? 1 : 0x3,
-			2, 32);
-	if (ret) {
-		dev_err(cpu_dai->dev, "failed to set dai tdm slot\n");
-		return ret;
-	}
-
-	ret = snd_soc_dai_set_sysclk(cpu_dai, 0, bclk, SND_SOC_CLOCK_OUT);
-	if (ret)
-		dev_err(cpu_dai->dev, "failed to set sysclk\n");
 
 	return ret;
 }
