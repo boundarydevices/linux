7b41bc7cdcb772d6cb19bb700cd3c4f4ed41ec1d
Robin Gong <yibin.gong@nxp.com>
2019-07-23 23:43:41 +0800

    MLK-22302-3: dmaengine: mxs-dma: fix build warning with CONFIG_PM_SLEEP=n
    Fix build waring with CONFIG_PM_SLEEP=n.
    
    Signed-off-by: Robin Gong <yibin.gong@nxp.com>
    Reviewed-by: Andy Duan <fugang.duan@nxp.com>
    (cherry picked from commit 3d4bb7eb025b18c8699e80a1a76226887120b198)

--- drivers/dma/mxs-dma.c
+++ drivers/dma/mxs-dma.c
@@ -947,6 +947,7 @@ static int mxs_dma_remove(struct platform_device *pdev)
 	return 0;
 }
 
+#ifdef CONFIG_PM_SLEEP
 static int mxs_dma_pm_suspend(struct device *dev)
 {
 	int ret;
@@ -967,6 +968,7 @@ static int mxs_dma_pm_resume(struct device *dev)
 
 	return 0;
 }
+#endif
 
 int mxs_dma_runtime_suspend(struct device *dev)
 {
