fab45c954348f839dda0f7066edb3c3b0ef21580
Fancy Fang <chen.fang@nxp.com>
2019-07-23 17:03:57 +0800

    MLK-22304-2 fbdev: mxsfb: fix build warnings if CONFIG_PM_SLEEP off
    The system suspend and resume hooks is related with CONFIG_PM_SLEEP
    instead of CONFIG_PM, so use CONFIG_PM_SLEEP to control the definitions
    for them. And after this, the related overlay suspend and resume should
    be defined under CONFIG_PM_SLEEP control. Below are the build warnings
    before this patch.
    
    drivers/video/fbdev/mxsfb.c:2463:12: warning: ‘mxsfb_resume’ defined but not used [-Wunused-function]
     static int mxsfb_resume(struct device *pdev)
                ^~~~~~~~~~~~
    drivers/video/fbdev/mxsfb.c:2444:12: warning: ‘mxsfb_suspend’ defined but not used [-Wunused-function]
     static int mxsfb_suspend(struct device *pdev)
                ^~~~~~~~~~~~~
    
    Signed-off-by: Fancy Fang <chen.fang@nxp.com>
    (cherry picked from commit d9ed772ccfeb42e8405bbf8c0f52ffe04f09b039)

--- drivers/video/fbdev/mxsfb.c
+++ drivers/video/fbdev/mxsfb.c
@@ -405,8 +405,6 @@ static const struct fb_bitfield def_rgb565[] = {
 };
 
 #ifdef CONFIG_FB_MXC_OVERLAY
-static u32 saved_as_ctrl;
-static u32 saved_as_next_buf;
 
 static const struct fb_bitfield def_argb555[] = {
 	[RED] = {
@@ -2314,6 +2312,10 @@ static void mxsfb_overlay_exit(struct mxsfb_info *fbi)
 	}
 }
 
+#ifdef CONFIG_PM_SLEEP
+static u32 saved_as_ctrl;
+static u32 saved_as_next_buf;
+
 static void mxsfb_overlay_resume(struct mxsfb_info *fbi)
 {
 	if (fbi->cur_blank != FB_BLANK_UNBLANK) {
@@ -2350,11 +2352,11 @@ static void mxsfb_overlay_suspend(struct mxsfb_info *fbi)
 		clk_disable_pix(fbi);
 	}
 }
+#endif
+
 #else
 static void mxsfb_overlay_init(struct mxsfb_info *fbi) {}
 static void mxsfb_overlay_exit(struct mxsfb_info *fbi) {}
-static void mxsfb_overlay_resume(struct mxsfb_info *fbi) {}
-static void mxsfb_overlay_suspend(struct mxsfb_info *fbi) {}
 #endif
 
 static int mxsfb_probe(struct platform_device *pdev)
@@ -2612,7 +2614,9 @@ static int mxsfb_runtime_resume(struct device *dev)
 
 	return 0;
 }
+#endif
 
+#ifdef CONFIG_PM_SLEEP
 static int mxsfb_suspend(struct device *pdev)
 {
 	struct mxsfb_info *host = dev_get_drvdata(pdev);
@@ -2647,12 +2651,6 @@ static int mxsfb_resume(struct device *pdev)
 
 	return 0;
 }
-#else
-#define	mxsfb_runtime_suspend	NULL
-#define	mxsfb_runtime_resume	NULL
-
-#define	mxsfb_suspend	NULL
-#define	mxsfb_resume	NULL
 #endif
 
 static const struct dev_pm_ops mxsfb_pm_ops = {
