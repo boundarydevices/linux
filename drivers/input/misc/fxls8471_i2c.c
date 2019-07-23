003873ba415582d8ef5497b23c5d363d78e33641
Clark Wang <xiaoning.wang@nxp.com>
2019-07-23 14:40:30 +0800

    MLK-22296-3 misc: fxls8471_i2c: Fix build warning when CONFIG_PM_SLEEP=n
    Fix build warning when CONFIG_PM_SLEEP=n, the warning message is:
    drivers/input/misc/fxls8471_i2c.c:77:12: warning: ‘fxls8471_i2c_resume’ defined but not used [-Wunused-function]
     static int fxls8471_i2c_resume(struct device *dev)
                ^~~~~~~~~~~~~~~~~~~
    drivers/input/misc/fxls8471_i2c.c:71:12: warning: ‘fxls8471_i2c_suspend’ defined but not used [-Wunused-function]
     static int fxls8471_i2c_suspend(struct device *dev)
                ^~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Clark Wang <xiaoning.wang@nxp.com>

--- drivers/input/misc/fxls8471_i2c.c
+++ drivers/input/misc/fxls8471_i2c.c
@@ -67,7 +67,7 @@ static int fxls8471_i2c_remove(struct i2c_client *client)
 	return fxls8471_driver_remove(&fxls8471_dev);
 }
 
-#ifdef CONFIG_PM
+#ifdef CONFIG_PM_SLEEP
 static int fxls8471_i2c_suspend(struct device *dev)
 {
 	struct i2c_client *client = to_i2c_client(dev);
