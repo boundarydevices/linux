7098dea6eef839aaa61109fc74ec6a9b16f9888e
Anson Huang <Anson.Huang@nxp.com>
2019-07-23 09:41:38 +0800

    MLK-21076-3 pinctrl: imx7d: Fix build warning when CONFIG_PM_SLEEP=n
    Fix below build warning when CONFIG_PM_SLEEP=n:
    
    drivers/pinctrl/freescale/pinctrl-imx7d.c:400:12: warning:
    'imx7d_pinctrl_resume' defined but not used [-Wunused-function]
     static int imx7d_pinctrl_resume(struct device *dev)
                ^~~~~~~~~~~~~~~~~~~~
    drivers/pinctrl/freescale/pinctrl-imx7d.c:394:12: warning:
     'imx7d_pinctrl_suspend' defined but not used [-Wunused-function]
    
     static int imx7d_pinctrl_suspend(struct device *dev)
                ^~~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>
    Reviewed-by: Jacky Bai <ping.bai@nxp.com>

--- drivers/pinctrl/freescale/pinctrl-imx7d.c
+++ drivers/pinctrl/freescale/pinctrl-imx7d.c
@@ -391,13 +391,13 @@ static int imx7d_pinctrl_probe(struct platform_device *pdev)
 	return imx_pinctrl_probe(pdev, pinctrl_info);
 }
 
-static int imx7d_pinctrl_suspend(struct device *dev)
+static int __maybe_unused imx7d_pinctrl_suspend(struct device *dev)
 {
 
        return imx_pinctrl_suspend(dev);
 }
 
-static int imx7d_pinctrl_resume(struct device *dev)
+static int __maybe_unused imx7d_pinctrl_resume(struct device *dev)
 {
 
        return imx_pinctrl_resume(dev);
