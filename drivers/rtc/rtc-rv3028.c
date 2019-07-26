515059b443341142c60552690990210a2a2f7c60
Troy Kisky <troy.kisky@boundarydevices.com>
2019-07-25 17:53:02 -0700

    rtc: rtc-rv3028: add backup-switchover-dsm
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/rtc/rtc-rv3028.c
+++ drivers/rtc/rtc-rv3028.c
@@ -80,6 +80,8 @@
 
 #define RV3028_BACKUP_TCE		BIT(5)
 #define RV3028_BACKUP_TCR_MASK		GENMASK(1,0)
+#define RV3028_BACKUP_BSM_MASK		0x0c
+#define RV3028_BACKUP_BSM_DSM		0x0c
 
 #define OFFSET_STEP_PPT			953674
 
@@ -877,6 +879,12 @@ static int rv3028_probe(struct i2c_client *client)
 		} else {
 			dev_warn(&client->dev, "invalid trickle resistor value\n");
 		}
+	}
+
+	if (device_property_present(&client->dev, "backup-switchover-dsm")) {
+		ret = regmap_update_bits(rv3028->regmap, RV3028_BACKUP,
+				RV3028_BACKUP_BSM_MASK,
+				RV3028_BACKUP_BSM_DSM);
 	}
 
 	ret = rtc_add_group(rv3028->rtc, &rv3028_attr_group);
