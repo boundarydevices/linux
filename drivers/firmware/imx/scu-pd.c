7a2e10ef4e0ea4bdbccef75ed4405201c573041a
Joakim Zhang <qiangqing.zhang@nxp.com>
2019-07-29 15:59:05 +0800

    firmware: scu-pd: add pd for CM41 SS
    Add pd for CM41 SS on imx8QM.
    
    Signed-off-by: Joakim Zhang <qiangqing.zhang@nxp.com>

--- drivers/firmware/imx/scu-pd.c
+++ drivers/firmware/imx/scu-pd.c
@@ -207,6 +207,10 @@ static const struct imx_sc_pd_range imx8qxp_scu_pd_ranges[] = {
 	{ "cm41-mu-a1", IMX_SC_R_M4_1_MU_1A, 1, false, 0},
 	{ "cm41-lpuart", IMX_SC_R_M4_1_UART, 1, false, 0},
 
+	/* CM41 SS */
+	{ "cm41_i2c", IMX_SC_R_M4_1_I2C, 1, false, 0 },
+	{ "cm41_intmux", IMX_SC_R_M4_1_INTMUX, 1, false, 0 },
+
 	/* IMAGE SS */
 	{ "img-jpegdec-mp", IMX_SC_R_MJPEG_DEC_MP, 1, false, 0 },
 	{ "img-jpegdec-s0", IMX_SC_R_MJPEG_DEC_S0, 4, true, 0 },
