d5315e9651fb730be669da2688547ece927e00ce
Troy Kisky <troy.kisky@boundarydevices.com>
2019-07-28 22:24:31 -0700

    pci: dwc: pci-imx6: sort headers
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/pci/controller/dwc/pci-imx6.c
+++ drivers/pci/controller/dwc/pci-imx6.c
@@ -17,9 +17,9 @@
 #include <linux/mfd/syscon/imx6q-iomuxc-gpr.h>
 #include <linux/mfd/syscon/imx7-iomuxc-gpr.h>
 #include <linux/module.h>
-#include <linux/of_gpio.h>
-#include <linux/of_device.h>
 #include <linux/of_address.h>
+#include <linux/of_device.h>
+#include <linux/of_gpio.h>
 #include <linux/of_irq.h>
 #include <linux/of_pci.h>
 #include <linux/pci.h>
@@ -31,9 +31,9 @@
 #include <linux/signal.h>
 #include <linux/types.h>
 #include <linux/interrupt.h>
-#include <linux/reset.h>
 #include <linux/busfreq-imx.h>
 #include <linux/regulator/consumer.h>
+#include <linux/reset.h>
 #include "../../pci.h"
 
 #include "pcie-designware.h"
