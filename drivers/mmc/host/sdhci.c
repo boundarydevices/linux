25c15a726ad34314339c0b12578b7111d3cd2ca3
Troy Kisky <troy.kisky@boundarydevices.com>
2019-07-30 10:51:21 -0700

    mmc: host: sdhci: limit adma segment to 64k-64 bytes to keep alignment
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -4228,7 +4228,9 @@ int sdhci_setup_host(struct sdhci_host *host)
 	 */
 	if (host->flags & SDHCI_USE_ADMA) {
 		if (host->quirks & SDHCI_QUIRK_BROKEN_ADMA_ZEROLEN_DESC) {
-			mmc->max_seg_size = 65535;
+#define MAX_ADMA_SEGMENT (SZ_64K - 64)
+			mmc->max_seg_size = MAX_ADMA_SEGMENT;
+
 			/*
 			 * send the ADMA limitation to IOMMU. In default,
 			 * the max segment size of IOMMU is 64KB, this exceed
@@ -4236,7 +4238,7 @@ int sdhci_setup_host(struct sdhci_host *host)
 			 */
 			dev->dma_parms = devm_kzalloc(dev,
 			                sizeof(*dev->dma_parms), GFP_KERNEL);
-			dma_set_max_seg_size(dev, SZ_64K - 1);
+			dma_set_max_seg_size(dev, MAX_ADMA_SEGMENT);
 		} else {
 			mmc->max_seg_size = 65536;
 		}
