18a73b85883b3925c0bc10fd1e95e7aa8d4af4a0
Troy Kisky <troy.kisky@boundarydevices.com>
2019-08-07 17:48:30 -0700

    squash i2c: i2c-core-base: don't restore on error if irq is in use already
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/i2c/i2c-core.c
+++ drivers/i2c/i2c-core.c
@@ -971,6 +971,7 @@ err_detach_pm_domain:
 		 * select a different trigger
 		 */
 		irq_set_irq_type(client->irq, IRQ_TYPE_NONE);
+		client->irq = 0;
 	}
 	dev_pm_domain_detach(&client->dev, true);
 err_clear_wakeup_irq:
