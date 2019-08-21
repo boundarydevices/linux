1f8d3a24ac0e4d0a68ebd6f3042bc0dffc509c06
Richard Liu <xuegang.liu@nxp.com>
2019-08-21 09:13:28 +0000

    MA-14981 [#imx-1655] Add evk_imx8mm 4GB DDR support
    Fix GPU memory allocation issue on 4GB DDR imx8mm_evk
    board.
    
    Change-Id: I39a1abcaa3e85935fec47862b86903d0a9b9a487
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>
    (cherry picked from commit 93c54100188bd2400b8be12e011aa4c1dc7ead01)

--- drivers/mxc/gpu-viv/hal/os/linux/kernel/platform/freescale/gc_hal_kernel_platform_imx.c
+++ drivers/mxc/gpu-viv/hal/os/linux/kernel/platform/freescale/gc_hal_kernel_platform_imx.c
@@ -1493,8 +1493,10 @@ _AdjustParam(
 {
     patch_param(Platform->device, Args);
 
-    if (of_find_compatible_node(NULL, NULL, "fsl,imx8mq-gpu") && ((Args->baseAddress + totalram_pages * PAGE_SIZE) > 0x100000000))
-        Platform->flagBits = gcvPLATFORM_FLAG_LIMIT_4G_ADDRESS;
+    if ((of_find_compatible_node(NULL, NULL, "fsl,imx8mq-gpu") ||
+	of_find_compatible_node(NULL, NULL, "fsl,imx8mm-gpu")) &&
+	((Args->baseAddress + totalram_pages * PAGE_SIZE) > 0x100000000))
+	    Platform->flagBits |= gcvPLATFORM_FLAG_LIMIT_4G_ADDRESS;
 
     return gcvSTATUS_OK;
 }
