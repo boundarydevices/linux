d6c8109f048e599513676e890e92ad11aa433983
Wujian sun <wujian.sun@nxp.com>
2019-08-26 10:13:59 +0800

    MGS-5014-1 [#imx-1575] gpu hang when do GLES sdk multiple stress test
    Revert the changes for disable feature gcvFEATURE_FENCE_64BIT in the
    kernel driver for 8MN_EVK board.
    
    This reverts commit 064f52faa7cc0724b171ec79609d9dcc2a513bea.
    
    Signed-off-by: Wujian sun <wujian.sun@nxp.com>

--- drivers/mxc/gpu-viv/hal/kernel/arch/gc_hal_kernel_hardware.c
+++ drivers/mxc/gpu-viv/hal/kernel/arch/gc_hal_kernel_hardware.c
@@ -1093,12 +1093,6 @@ _QueryFeatureDatabase(IN gckHARDWARE Hardware, IN gceFEATURE Feature)
 
     case gcvFEATURE_FENCE_64BIT:
         available = database->FENCE_64BIT;
-
-        if(_IsHardwareMatch(Hardware, gcv7000, 0x6203))
-        {
-            available = gcvFALSE;
-        }
-
         break;
 
     case gcvFEATURE_TEX_BASELOD:
