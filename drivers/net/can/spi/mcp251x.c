f2aa53872379194fe2e1c24e2a13aa1d06c1d34f
Marc Kleine-Budde <mkl@pengutronix.de>
2019-08-26 14:34:59 +0200

    can: mcp251x: remove deprecated board file setup example
    In the pre device-tree ARM aera there were board files that configured
    the system (instead of a device tree). A "struct spi_board_info" was
    used to describe the SPI bus.
    
    As new systems should be described via device trees, this patch removes
    the board setup example from the driver. The "struct
    mcp251x_platform_data" cannot be removed completely, as there are still
    some in-tree users of this file.
    
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/spi/mcp251x.c
+++ drivers/net/can/spi/mcp251x.c
@@ -29,28 +29,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  *
- *
- *
- * Your platform definition file should specify something like:
- *
- * static struct mcp251x_platform_data mcp251x_info = {
- *         .oscillator_frequency = 8000000,
- * };
- *
- * static struct spi_board_info spi_board_info[] = {
- *         {
- *                 .modalias = "mcp2510",
- *			// "mcp2515" or "mcp25625" depending on your controller
- *                 .platform_data = &mcp251x_info,
- *                 .irq = IRQ_EINT13,
- *                 .max_speed_hz = 2*1000*1000,
- *                 .chip_select = 2,
- *         },
- * };
- *
- * Please see mcp251x.h for a description of the fields in
- * struct mcp251x_platform_data.
- *
  */
 
 #include <linux/can/core.h>
