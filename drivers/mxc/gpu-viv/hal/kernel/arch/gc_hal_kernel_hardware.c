9904167b51a238db7e5312808aa5efef398c5ad4
Xianzhong <xianzhong.li@nxp.com>
2019-09-06 22:27:49 +0800

    MGS-5160 [#imx-1676] enable dummy draw to fix gpu hang and failures on 8MM
    Problems:
    - GPU hang when run Google Earth apk on 8MM EVK board
    - Android DEQP/SKQP CTS have random failures
    - Khronos ES20 CTS have random failures
    
    Analysis:
    GPU got stuck in shader module when process specific data format,
    this is caused by VSI GCNanoUltra Errata(HBN1286), which does not set
    the specific intermediate register to 0 in hardware reset sequence after power up,
    this wrong register will cause the unexpected result when process specific data type,
    wrong behavior will happen and may cause out of bound access in shader programming.
    
    Fix:
    GPU driver will submit the predefined command(dummy draw) with fake stream and shader,
    also set scissor with (0,0,0,0) to avoid draw out, no pixel output on hardware pipeline,
    this workaround can set the specific register to 0 as the effective SW remedy.
    
    Impact:
    No obvious functionality and performance impact with dummy draw workaround,
    it only takes several cycles in command fetch --> vertex shader --> primitive,
    and then cull out of reset of GPU pipelines.
    
    This patch can fix the same GPU problem for 7ULP.
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>
    (cherry picked from commit b14813c419b1f733c0945e99fc403b7a25774d24)

--- drivers/mxc/gpu-viv/hal/kernel/arch/gc_hal_kernel_hardware.c
+++ drivers/mxc/gpu-viv/hal/kernel/arch/gc_hal_kernel_hardware.c
@@ -1280,6 +1280,11 @@ _QueryFeatureDatabase(
 
     case gcvFEATURE_FE_NEED_DUMMYDRAW:
         available = database->FE_NEED_DUMMYDRAW;
+
+        if (_IsHardwareMatch(Hardware, gcv600, 0x4653))
+        {
+            available = gcvTRUE;
+        }
         break;
 
     case gcvFEATURE_DEC300_COMPRESSION:
