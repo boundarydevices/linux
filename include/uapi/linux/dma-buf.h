a51dda8feec03e8c675230786d64ee050944dbfd
Dong Aisheng <aisheng.dong@nxp.com>
2019-09-12 15:32:22 +0800

    dma: dma-buf.h: change DMA_BUF_IOCTL_PHYS NR to 10
    Change DMA_BUF_IOCTL_PHYS NR to 10 avoid conflicts with upstream
    in the future.
    
    Reviewed-by: Song Bing <bing.song@nxp.com>
    Signed-off-by: Dong Aisheng <aisheng.dong@nxp.com>
    (cherry picked from commit 36abba8f55078424e037e7f7877e9f22062cccbc)

--- include/uapi/linux/dma-buf.h
+++ include/uapi/linux/dma-buf.h
@@ -41,6 +41,6 @@ struct dma_buf_phys {
 
 #define DMA_BUF_BASE		'b'
 #define DMA_BUF_IOCTL_SYNC	_IOW(DMA_BUF_BASE, 0, struct dma_buf_sync)
-#define DMA_BUF_IOCTL_PHYS	_IOW(DMA_BUF_BASE, 1, struct dma_buf_phys)
+#define DMA_BUF_IOCTL_PHYS	_IOW(DMA_BUF_BASE, 10, struct dma_buf_phys)
 
 #endif
