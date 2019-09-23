d213ef03f0efc70105c889038141c5625ce0ab86
Liu Ying <victor.liu@nxp.com>
2019-09-23 10:30:58 +0800

    MLK-22653-3 drm/imx: dpu: crtc: Remove WARN_ON(!crtc->state->event)
    The DRM atomic core ensures crtc->state->event is not NULL when
    calling the ->atomic_disable() or the ->atomic_flush() callbacks.
    So, let's remove the unnecessary NULL check warning on it.
    
    Signed-off-by: Liu Ying <victor.liu@nxp.com>
    (cherry picked from commit 834bff532fe22a79708459ec976da0a74a2e97e2)

--- drivers/gpu/drm/imx/dpu/dpu-crtc.c
+++ drivers/gpu/drm/imx/dpu/dpu-crtc.c
@@ -313,8 +313,6 @@ static void dpu_crtc_atomic_disable(struct drm_crtc *crtc,
 
 	drm_crtc_vblank_off(crtc);
 
-	WARN_ON(!crtc->state->event);
-
 	if (crtc->state->event && !crtc->state->active) {
 		spin_lock_irq(&crtc->dev->event_lock);
 		drm_crtc_send_vblank_event(crtc, crtc->state->event);
@@ -773,8 +771,6 @@ static void dpu_crtc_atomic_flush(struct drm_crtc *crtc,
 					 "flush - FrameGen requests to read empty FIFO\n");
 			}
 		}
-
-		WARN_ON(!crtc->state->event);
 
 		if (crtc->state->event) {
 			spin_lock_irq(&crtc->dev->event_lock);
