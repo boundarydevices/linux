41f7b16712cbd8d5a1c8bed804a8b135f8b68ed7
Troy Kisky <troy.kisky@boundarydevices.com>
2019-09-30 13:30:26 -0700

    ov5640_mipi: add ov5640_reset_pwrdn
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/media/platform/mxc/capture/ov5640_mipi.c
+++ drivers/media/platform/mxc/capture/ov5640_mipi.c
@@ -1376,7 +1376,11 @@ static void ov5640_reset(struct ov5640 *sensor)
 	pr_debug("%s(mipi): reset released\n", __func__);
 	ov5640_update_slave_id(sensor);
 	mxc_camera_common_unlock();
+}
 
+static void ov5640_reset_pwrdn(struct ov5640 *sensor)
+{
+	ov5640_reset(sensor);
 	gpio_set_value(sensor->pwn_gpio, 1);
 }
 
@@ -1418,7 +1422,7 @@ static int power_up(struct ov5640 *sensor)
 			goto err4;
 		}
 	}
-	/* Make sure power on */
+	/* Make sure power is on */
 	ov5640_power_down(sensor, 0);
 	sensor->on = 1;
 	return 0;
@@ -2957,9 +2961,7 @@ static int ov5640_s_parm(struct ov5640 *sensor, struct v4l2_streamparm *a)
 	enum ov5640_mode orig_mode;
 	int ret = 0;
 
-	/* Make sure power on */
-	if (gpio_get_value(sensor->pwn_gpio))
-		ov5640_power_down(sensor, 0);
+	power_up(sensor);	/* Make sure power is on */
 
 	switch (a->type) {
 	/* This is the only case currently handled. */
@@ -3498,9 +3500,9 @@ static struct v4l2_int_device ov5640_int_device = {
 	},
 };
 
-static void _ov5640_reset(void)
+static void _ov5640_reset_pwrdn(void)
 {
-	ov5640_reset(ov5640_int_device.priv);
+	ov5640_reset_pwrdn(ov5640_int_device.priv);
 }
 
 static ssize_t show_reg(struct device *dev,
@@ -3630,7 +3632,7 @@ static int ov5640_probe(struct i2c_client *client,
 
 	clk_prepare_enable(sensor->s.sensor_clk);
 
-	sensor->s.io_init = _ov5640_reset;
+	sensor->s.io_init = _ov5640_reset_pwrdn;
 	sensor->i2c_client = client;
 	sensor->pix.pixelformat = V4L2_PIX_FMT_UYVY;
 	sensor->pix.width = 640;
@@ -3645,8 +3647,6 @@ static int ov5640_probe(struct i2c_client *client,
 
 	ov5640_reset(sensor);
 
-	ov5640_power_down(sensor, 0);
-
 	retval = ov5640_read_reg(sensor, OV5640_CHIP_ID_HIGH_BYTE, &chip_id_high);
 	if (retval < 0 || chip_id_high != 0x56)
 		goto err1;
@@ -3660,7 +3660,6 @@ static int ov5640_probe(struct i2c_client *client,
 	if (retval < 0)
 		pr_err("%s: error downloading autofocus firmware\n", __func__);
 
-	ov5640_power_down(sensor, 1);
 	power_down(sensor);
 
 	ov5640_int_device.priv = sensor;
