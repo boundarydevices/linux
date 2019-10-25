954ae2f9a449add1474a5973f260cdd3fbc458a5
Troy Kisky <troy.kisky@boundarydevices.com>
2019-10-25 09:34:24 -0700

    usb: gadget: composite: add usb_composite_probe_udc_name
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- include/linux/usb/composite.h
+++ include/linux/usb/composite.h
@@ -390,6 +390,7 @@ struct usb_composite_driver {
 };
 
 extern int usb_composite_probe(struct usb_composite_driver *driver);
+extern int usb_composite_probe_udc_name(struct usb_composite_driver *driver, char *udc_name);
 extern void usb_composite_unregister(struct usb_composite_driver *driver);
 
 /**
@@ -467,6 +468,7 @@ struct usb_composite_dev {
 	struct usb_request		*os_desc_req;
 
 	struct usb_configuration	*config;
+	void *private_data;
 
 	/* OS String is a custom (yet popular) extension to the USB standard. */
 	u8				qw_sign[OS_STRING_QW_SIGN_LEN];
