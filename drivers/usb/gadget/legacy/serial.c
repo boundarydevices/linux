a028e8eda57ec5f21e74cb2eb7cffd2ba52ea81f
Troy Kisky <troy.kisky@boundarydevices.com>
2019-10-25 10:30:35 -0700

    usb: gadget: legacy: serial: add binding to multiple udc's
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/usb/gadget/legacy/serial.c
+++ drivers/usb/gadget/legacy/serial.c
@@ -41,41 +41,23 @@ USB_GADGET_COMPOSITE_OPTIONS();
 
 #define STRING_DESCRIPTION_IDX		USB_GADGET_FIRST_AVAIL_IDX
 
-static struct usb_string strings_dev[] = {
-	[USB_GADGET_MANUFACTURER_IDX].s = "",
-	[USB_GADGET_PRODUCT_IDX].s = GS_VERSION_NAME,
-	[USB_GADGET_SERIAL_IDX].s = "",
-	[STRING_DESCRIPTION_IDX].s = NULL /* updated; f(use_acm) */,
-	{  } /* end of list */
+struct s_driver_desc {
+	struct usb_composite_driver driver;
+	struct usb_device_descriptor dev_desc;
+	struct usb_gadget_strings *dev_strings[2];
+	struct usb_gadget_strings stringtab_dev;
+	struct usb_string strings[STRING_DESCRIPTION_IDX + 2];
+	bool acm;
+	bool obex;
+	unsigned ports;
 };
 
-static struct usb_gadget_strings stringtab_dev = {
-	.language	= 0x0409,	/* en-us */
-	.strings	= strings_dev,
+struct gadget_serial {
+	struct usb_configuration config;
+	struct usb_function_instance *fi_serial[MAX_U_SERIAL_PORTS];
+	struct usb_function *f_serial[MAX_U_SERIAL_PORTS];
+	const struct usb_descriptor_header *otg_desc[2];
 };
-
-static struct usb_gadget_strings *dev_strings[] = {
-	&stringtab_dev,
-	NULL,
-};
-
-static struct usb_device_descriptor device_desc = {
-	.bLength =		USB_DT_DEVICE_SIZE,
-	.bDescriptorType =	USB_DT_DEVICE,
-	/* .bcdUSB = DYNAMIC */
-	/* .bDeviceClass = f(use_acm) */
-	.bDeviceSubClass =	0,
-	.bDeviceProtocol =	0,
-	/* .bMaxPacketSize0 = f(hardware) */
-	.idVendor =		cpu_to_le16(GS_VENDOR_ID),
-	/* .idProduct =	f(use_acm) */
-	.bcdDevice = cpu_to_le16(GS_VERSION_NUM),
-	/* .iManufacturer = DYNAMIC */
-	/* .iProduct = DYNAMIC */
-	.bNumConfigurations =	1,
-};
-
-static const struct usb_descriptor_header *otg_desc[2];
 
 /*-------------------------------------------------------------------------*/
 
@@ -85,16 +67,19 @@ MODULE_AUTHOR("Al Borchers");
 MODULE_AUTHOR("David Brownell");
 MODULE_LICENSE("GPL");
 
-static bool use_acm = true;
-module_param(use_acm, bool, 0);
+static bool use_acm[4] = {true, true, true, true};
+static unsigned num_acm;
+module_param_array(use_acm, bool, &num_acm, 0);
 MODULE_PARM_DESC(use_acm, "Use CDC ACM, default=yes");
 
-static bool use_obex = false;
-module_param(use_obex, bool, 0);
+static bool use_obex[4];
+static unsigned num_obex;
+module_param_array(use_obex, bool, &num_obex, 0);
 MODULE_PARM_DESC(use_obex, "Use CDC OBEX, default=no");
 
-static unsigned n_ports = 1;
-module_param(n_ports, uint, 0);
+static unsigned n_ports[4] = {1, 1, 1 ,1};
+static unsigned num_n_ports;
+module_param_array(n_ports, uint, &num_n_ports, 0);
 MODULE_PARM_DESC(n_ports, "number of ports to create, default=1");
 
 static bool enable = true;
@@ -127,21 +112,17 @@ static const struct kernel_param_ops enable_ops = {
 
 module_param_cb(enable, &enable_ops, &enable, 0644);
 
+static char *udc_name[4];
+static unsigned num_udc;
+module_param_array(udc_name, charp, &num_udc, 0);
+MODULE_PARM_DESC(udc_name, "udc to bind, default is 1st found");
+
 /*-------------------------------------------------------------------------*/
 
-static struct usb_configuration serial_config_driver = {
-	/* .label = f(use_acm) */
-	/* .bConfigurationValue = f(use_acm) */
-	/* .iConfiguration = DYNAMIC */
-	.bmAttributes	= USB_CONFIG_ATT_SELFPOWER,
-};
-
-static struct usb_function_instance *fi_serial[MAX_U_SERIAL_PORTS];
-static struct usb_function *f_serial[MAX_U_SERIAL_PORTS];
-
 static int serial_register_ports(struct usb_composite_dev *cdev,
-		struct usb_configuration *c, const char *f_name)
+		struct usb_configuration *c, const char *f_name, unsigned ports)
 {
+	struct gadget_serial *gs = cdev->private_data;
 	int i;
 	int ret;
 
@@ -149,21 +130,21 @@ static int serial_register_ports(struct usb_composite_dev *cdev,
 	if (ret)
 		goto out;
 
-	for (i = 0; i < n_ports; i++) {
+	for (i = 0; i < ports; i++) {
 
-		fi_serial[i] = usb_get_function_instance(f_name);
-		if (IS_ERR(fi_serial[i])) {
-			ret = PTR_ERR(fi_serial[i]);
+		gs->fi_serial[i] = usb_get_function_instance(f_name);
+		if (IS_ERR(gs->fi_serial[i])) {
+			ret = PTR_ERR(gs->fi_serial[i]);
 			goto fail;
 		}
 
-		f_serial[i] = usb_get_function(fi_serial[i]);
-		if (IS_ERR(f_serial[i])) {
-			ret = PTR_ERR(f_serial[i]);
+		gs->f_serial[i] = usb_get_function(gs->fi_serial[i]);
+		if (IS_ERR(gs->f_serial[i])) {
+			ret = PTR_ERR(gs->f_serial[i]);
 			goto err_get_func;
 		}
 
-		ret = usb_add_function(c, f_serial[i]);
+		ret = usb_add_function(c, gs->f_serial[i]);
 		if (ret)
 			goto err_add_func;
 	}
@@ -171,16 +152,16 @@ static int serial_register_ports(struct usb_composite_dev *cdev,
 	return 0;
 
 err_add_func:
-	usb_put_function(f_serial[i]);
+	usb_put_function(gs->f_serial[i]);
 err_get_func:
-	usb_put_function_instance(fi_serial[i]);
+	usb_put_function_instance(gs->fi_serial[i]);
 
 fail:
 	i--;
 	while (i >= 0) {
-		usb_remove_function(c, f_serial[i]);
-		usb_put_function(f_serial[i]);
-		usb_put_function_instance(fi_serial[i]);
+		usb_remove_function(c, gs->f_serial[i]);
+		usb_put_function(gs->f_serial[i]);
+		usb_put_function_instance(gs->fi_serial[i]);
 		i--;
 	}
 out:
@@ -189,22 +170,42 @@ out:
 
 static int gs_bind(struct usb_composite_dev *cdev)
 {
-	int			status;
+	struct usb_composite_driver *driver = cdev->driver;
+	struct s_driver_desc *sdd = container_of(driver, struct s_driver_desc, driver);
+	struct usb_string *s = sdd->strings;
+	struct gadget_serial *gs = cdev->private_data;
+	int status;
 
+	if (!gs) {
+		gs = kzalloc(sizeof(struct gadget_serial), GFP_KERNEL);
+		cdev->private_data = gs;
+		gs->config.bmAttributes	= USB_CONFIG_ATT_SELFPOWER;
+		if (sdd->acm) {
+			gs->config.label = "CDC ACM config";
+			gs->config.bConfigurationValue = 2;
+		} else if (sdd->obex) {
+			gs->config.label = "CDC OBEX config";
+			gs->config.bConfigurationValue = 3;
+		} else {
+			gs->config.label = "Generic Serial config";
+			gs->config.bConfigurationValue = 1;
+		}
+		s[STRING_DESCRIPTION_IDX].s = gs->config.label;
+	}
 	/* Allocate string descriptor numbers ... note that string
 	 * contents can be overridden by the composite_dev glue.
 	 */
 
-	status = usb_string_ids_tab(cdev, strings_dev);
+	status = usb_string_ids_tab(cdev, s);
 	if (status < 0)
 		goto fail;
-	device_desc.iManufacturer = strings_dev[USB_GADGET_MANUFACTURER_IDX].id;
-	device_desc.iProduct = strings_dev[USB_GADGET_PRODUCT_IDX].id;
-	status = strings_dev[STRING_DESCRIPTION_IDX].id;
-	serial_config_driver.iConfiguration = status;
+	sdd->dev_desc.iManufacturer = s[USB_GADGET_MANUFACTURER_IDX].id;
+	sdd->dev_desc.iProduct = s[USB_GADGET_PRODUCT_IDX].id;
+	status = s[STRING_DESCRIPTION_IDX].id;
+	gs->config.iConfiguration = status;
 
 	if (gadget_is_otg(cdev->gadget)) {
-		if (!otg_desc[0]) {
+		if (!gs->otg_desc[0]) {
 			struct usb_descriptor_header *usb_desc;
 
 			usb_desc = usb_otg_descriptor_alloc(cdev->gadget);
@@ -213,24 +214,24 @@ static int gs_bind(struct usb_composite_dev *cdev)
 				goto fail;
 			}
 			usb_otg_descriptor_init(cdev->gadget, usb_desc);
-			otg_desc[0] = usb_desc;
-			otg_desc[1] = NULL;
+			gs->otg_desc[0] = usb_desc;
+			gs->otg_desc[1] = NULL;
 		}
-		serial_config_driver.descriptors = otg_desc;
-		serial_config_driver.bmAttributes |= USB_CONFIG_ATT_WAKEUP;
+		gs->config.descriptors = gs->otg_desc;
+		gs->config.bmAttributes |= USB_CONFIG_ATT_WAKEUP;
 	}
 
 	/* register our configuration */
-	if (use_acm) {
-		status  = serial_register_ports(cdev, &serial_config_driver,
-				"acm");
+	if (sdd->acm) {
+		status  = serial_register_ports(cdev, &gs->config,
+				"acm", sdd->ports);
 		usb_ep_autoconfig_reset(cdev->gadget);
-	} else if (use_obex)
-		status = serial_register_ports(cdev, &serial_config_driver,
-				"obex");
+	} else if (sdd->obex)
+		status = serial_register_ports(cdev, &gs->config,
+				"obex", sdd->ports);
 	else {
-		status = serial_register_ports(cdev, &serial_config_driver,
-				"gser");
+		status = serial_register_ports(cdev, &gs->config,
+				"gser", sdd->ports);
 	}
 	if (status < 0)
 		goto fail1;
@@ -240,85 +241,178 @@ static int gs_bind(struct usb_composite_dev *cdev)
 
 	return 0;
 fail1:
-	kfree(otg_desc[0]);
-	otg_desc[0] = NULL;
+	kfree(gs->otg_desc[0]);
+	gs->otg_desc[0] = NULL;
 fail:
 	return status;
 }
 
 static int gs_unbind(struct usb_composite_dev *cdev)
 {
+	struct usb_composite_driver *driver = cdev->driver;
+	struct s_driver_desc *sdd = container_of(driver, struct s_driver_desc, driver);
+	struct gadget_serial *gs = cdev->private_data;
 	int i;
 
-	for (i = 0; i < n_ports; i++) {
-		usb_put_function(f_serial[i]);
-		usb_put_function_instance(fi_serial[i]);
+	if (!gs)
+		return 0;
+	for (i = 0; i < sdd->ports; i++) {
+		usb_put_function(gs->f_serial[i]);
+		usb_put_function_instance(gs->fi_serial[i]);
 	}
+	cdev->private_data = NULL;
 
-	kfree(otg_desc[0]);
-	otg_desc[0] = NULL;
-
+	kfree(gs->otg_desc[0]);
+	kfree(gs);
 	return 0;
 }
 
-static struct usb_composite_driver gserial_driver = {
-	.name		= "g_serial",
-	.dev		= &device_desc,
-	.strings	= dev_strings,
-	.max_speed	= USB_SPEED_SUPER,
-	.bind		= gs_bind,
-	.unbind		= gs_unbind,
+static struct s_driver_desc gserial_driver = {
+	.driver = {
+		.name		= "g_serial",
+		.max_speed	= USB_SPEED_SUPER,
+		.bind		= gs_bind,
+		.unbind		= gs_unbind,
+	},
+	.dev_desc = {
+		.bLength =		USB_DT_DEVICE_SIZE,
+		.bDescriptorType =	USB_DT_DEVICE,
+		/* .bcdUSB = DYNAMIC */
+		/* .bDeviceClass = f(use_acm) */
+		.bDeviceSubClass =	0,
+		.bDeviceProtocol =	0,
+		/* .bMaxPacketSize0 = f(hardware) */
+		.idVendor =		cpu_to_le16(GS_VENDOR_ID),
+		/* .idProduct =	f(use_acm) */
+		.bcdDevice = cpu_to_le16(GS_VERSION_NUM),
+		/* .iManufacturer = DYNAMIC */
+		/* .iProduct = DYNAMIC */
+		.bNumConfigurations =	1,
+	},
 };
+
+static struct s_driver_desc *g_sdd;
 
 static int switch_gserial_enable(bool do_enable)
 {
-	if (!serial_config_driver.label)
-		/* gserial_init() was not called, yet */
-		return 0;
-
-	if (do_enable)
-		return usb_composite_probe(&gserial_driver);
-
-	usb_composite_unregister(&gserial_driver);
-	return 0;
-}
-
-static int __init gserial_init(void)
-{
-	/* We *could* export two configs; that'd be much cleaner...
-	 * but neither of these product IDs was defined that way.
-	 */
-	if (use_acm) {
-		serial_config_driver.label = "CDC ACM config";
-		serial_config_driver.bConfigurationValue = 2;
-		device_desc.bDeviceClass = USB_CLASS_COMM;
-		device_desc.idProduct =
-				cpu_to_le16(GS_CDC_PRODUCT_ID);
-	} else if (use_obex) {
-		serial_config_driver.label = "CDC OBEX config";
-		serial_config_driver.bConfigurationValue = 3;
-		device_desc.bDeviceClass = USB_CLASS_COMM;
-		device_desc.idProduct =
-			cpu_to_le16(GS_CDC_OBEX_PRODUCT_ID);
-	} else {
-		serial_config_driver.label = "Generic Serial config";
-		serial_config_driver.bConfigurationValue = 1;
-		device_desc.bDeviceClass = USB_CLASS_VENDOR_SPEC;
-		device_desc.idProduct =
-				cpu_to_le16(GS_PRODUCT_ID);
-	}
-	strings_dev[STRING_DESCRIPTION_IDX].s = serial_config_driver.label;
+	int i;
+	struct s_driver_desc *sdd;
+	struct s_driver_desc *base = g_sdd;
+	int cnt = num_udc;
 
 	if (!enable)
 		return 0;
 
-	return usb_composite_probe(&gserial_driver);
+	if (!cnt || !base)
+		cnt = 1;
+	for (i = cnt - 1; i >= 0; i--) {
+		sdd = i ? &base[i - 1] : &gserial_driver;
+		if (do_enable)
+			usb_composite_probe(&sdd->driver);
+		else
+			usb_composite_unregister(&sdd->driver);
+	}
+	return 0;
+}
+
+void setup_strings(struct s_driver_desc *s)
+{
+	s->strings[USB_GADGET_MANUFACTURER_IDX].s = "";
+	s->strings[USB_GADGET_PRODUCT_IDX].s = GS_VERSION_NAME;
+	s->strings[USB_GADGET_SERIAL_IDX].s = "";
+
+	s->stringtab_dev.language = 0x0409;	/* en-us */
+	s->stringtab_dev.strings = s->strings;
+	s->dev_strings[0] = &s->stringtab_dev;
+	s->dev_strings[1] = NULL;
+	s->driver.strings = s->dev_strings;
+}
+
+void setup_desc(struct usb_device_descriptor *dev_desc, int acm, int obex)
+{
+	if (acm) {
+		dev_desc->bDeviceClass = USB_CLASS_COMM;
+		dev_desc->idProduct = cpu_to_le16(GS_CDC_PRODUCT_ID);
+	} else if (obex) {
+		dev_desc->bDeviceClass = USB_CLASS_COMM;
+		dev_desc->idProduct = cpu_to_le16(GS_CDC_OBEX_PRODUCT_ID);
+	} else {
+		dev_desc->bDeviceClass = USB_CLASS_VENDOR_SPEC;
+		dev_desc->idProduct = cpu_to_le16(GS_PRODUCT_ID);
+	}
+}
+
+void setup_driver_desc(struct s_driver_desc *sdd, int acm, int obex, unsigned ports)
+{
+	sdd->acm = acm;
+	sdd->obex = obex;
+	sdd->ports = ports;
+	sdd->driver.dev = &sdd->dev_desc;
+	setup_strings(sdd);
+	setup_desc(&sdd->dev_desc, acm, obex);
+}
+
+static int __init gserial_init(void)
+{
+	struct s_driver_desc *sdd = &gserial_driver;
+	static struct s_driver_desc *base = NULL;
+	bool acm, obex;
+	unsigned ports;
+	int ret = 0;
+	unsigned i;
+
+
+	if (!enable)
+		return 0;
+	/* We *could* export two configs; that'd be much cleaner...
+	 * but neither of these product IDs was defined that way.
+	 */
+
+	if (num_udc > ARRAY_SIZE(udc_name))
+		return -EINVAL;
+	if (num_udc) {
+		if (num_udc > 1) {
+			g_sdd = base = kmalloc(sizeof(*sdd) * (num_udc - 1), GFP_KERNEL);
+			for (i = 1; i < num_udc; i++) {
+				base[i - 1] = *sdd;
+			}
+		}
+		for (i = 0; i < num_udc; i++) {
+			acm = use_acm[num_acm ? ((i < num_acm) ? i : num_acm - 1) : 0];
+			obex = use_obex[num_obex ? ((i < num_obex) ? i : num_obex - 1) : 0];
+			ports = n_ports[num_n_ports ? ((i < num_n_ports) ? i : num_n_ports - 1) : 0];
+			setup_driver_desc(sdd, acm, obex, ports);
+			sdd->driver.gadget_driver.match_existing_only = 1;
+			ret = usb_composite_probe_udc_name(&sdd->driver, udc_name[i]);
+			sdd = &base[i];
+		}
+	} else {
+		acm = use_acm[0];
+		obex = use_obex[0];
+		ports = n_ports[0];
+		setup_driver_desc(sdd, acm, obex, ports);
+		ret = usb_composite_probe(&sdd->driver);
+	}
+	return ret;
 }
 module_init(gserial_init);
 
 static void __exit gserial_cleanup(void)
 {
-	if (enable)
-		usb_composite_unregister(&gserial_driver);
+	int i;
+	struct s_driver_desc *sdd;
+	struct s_driver_desc *base = g_sdd;
+	int cnt = num_udc;
+
+	if (!enable)
+		return;
+	g_sdd = NULL;
+	if (!cnt || !base)
+		cnt = 1;
+	for (i = cnt - 1; i >= 0; i--) {
+		sdd = i ? &base[i - 1] : &gserial_driver;
+		usb_composite_unregister(&sdd->driver);
+	}
+	kfree(base);
 }
 module_exit(gserial_cleanup);
