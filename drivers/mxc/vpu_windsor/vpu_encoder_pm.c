c8b549256e119e2cd9eb67dfdd428c1026e58635
Shijie Qin <shijie.qin@nxp.com>
2019-10-29 13:14:32 +0800

    MLK-22769 VPU Encoder: unify code between 4.19 and 5.x
    - Separate sc and mu contents to vpu_encoder_mu.h/.c
    - Separate pm_domain contents to vpu_encoder_pm.h/.c
      It unnecessary on 4.19, just create API for unify 5.4
    - Add kfifo for better hold received mu message
    - Sync v4l2 change
      Remove vidioc_enum_fmt_vid_cap_mplane from 5.3.0
      Remove v4l2_ioctl_g_crop(), v4l2_ioctl_g_selection could
      compatible with it from 4.14 or earlier
      vb2_qbuf() api changed from 4.20.0
      vb2_buffer_state type changed from 4.20.0
      Need set devices_caps when create video_device
    
    Signed-off-by: Shijie Qin <shijie.qin@nxp.com>
    Reviewed-by: Zhou Peng <eagle.zhou@nxp.com>
    Reviewed-by: ming_qian <ming.qian@nxp.com>
    (cherry picked from commit 4110874eb1aa2b31e5834537c0aba2ce67109212)

---
+++ drivers/mxc/vpu_windsor/vpu_encoder_pm.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2019 NXP
+ */
+
+/*
+ * The code contained herein is licensed under the GNU General Public
+ * License. You may obtain a copy of the GNU General Public License
+ * Version 2 or later at the following locations:
+ *
+ * http://www.opensource.org/licenses/gpl-license.html
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+/*
+ *
+ * @file vpu_encoder_pm.c
+ *
+ */
+
+#include "vpu_encoder_pm.h"
+
+int vpu_enc_attach_pm_domains(struct vpu_dev *dev)
+{
+	return 0;
+}
+
+void vpu_enc_detach_pm_domains(struct vpu_dev *dev)
+{
+
+}
