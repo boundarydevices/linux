611fa1406f6346b293395959aed37f00cc151e62
Ioana Ciornei <ioana.ciornei@nxp.com>
2019-10-29 22:48:55 +0200

    bus: fsl-mc: add autorescan sysfs
    Add the autorescan sysfs in order to enable/disable the DPRC IRQs on
    which automatic rescan of the bus is performed. This is important when
    dynamic creation of objects is needed to happen in a timely manner because
    object creation can be bundled together.
    
    Signed-off-by: Ioana Ciornei <ioana.ciornei@nxp.com>

--- drivers/bus/fsl-mc/dprc-driver.c
+++ drivers/bus/fsl-mc/dprc-driver.c
@@ -486,8 +486,9 @@ out:
 /*
  * Disable and clear interrupt for a given DPRC object
  */
-static int disable_dprc_irq(struct fsl_mc_device *mc_dev)
+int disable_dprc_irq(struct fsl_mc_device *mc_dev)
 {
+	struct fsl_mc_bus *mc_bus = to_fsl_mc_bus(mc_dev);
 	int error;
 	struct fsl_mc_io *mc_io = mc_dev->mc_io;
 
@@ -524,7 +525,16 @@ static int disable_dprc_irq(struct fsl_mc_device *mc_dev)
 		return error;
 	}
 
+	mc_bus->irq_enabled = 0;
+
 	return 0;
+}
+
+int get_dprc_irq_state(struct fsl_mc_device *mc_dev)
+{
+	struct fsl_mc_bus *mc_bus = to_fsl_mc_bus(mc_dev);
+
+	return mc_bus->irq_enabled;
 }
 
 static int register_dprc_irq_handler(struct fsl_mc_device *mc_dev)
@@ -553,8 +563,9 @@ static int register_dprc_irq_handler(struct fsl_mc_device *mc_dev)
 	return 0;
 }
 
-static int enable_dprc_irq(struct fsl_mc_device *mc_dev)
+int enable_dprc_irq(struct fsl_mc_device *mc_dev)
 {
+	struct fsl_mc_bus *mc_bus = to_fsl_mc_bus(mc_dev);
 	int error;
 
 	/*
@@ -581,6 +592,8 @@ static int enable_dprc_irq(struct fsl_mc_device *mc_dev)
 
 		return error;
 	}
+
+	mc_bus->irq_enabled = 1;
 
 	return 0;
 }
