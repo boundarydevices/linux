c3b30ac7e0c6df002b6e0516d60bc9671cc4d6a2
Liu Ying <victor.liu@nxp.com>
2019-11-26 18:22:13 +0800

    MLK-23116-16 drm/imx: dpu: plane: Add some non-native prefetch pfmts support for i.MX8QXP C0 SoC
    i.MX8QXP C0 SoC contains SoC design fix for ERR050060, which opens
    non-native prefetch pixel formats for driver to use.  With the previous
    patches in this serie, it should be save to add some such kind of
    pixel formats(RG24, BG24, NV16 and NV61) to the DPU plane.
    
    Reviewed-by: Sandor Yu <Sandor.yu@nxp.com>
    Signed-off-by: Liu Ying <victor.liu@nxp.com>
    (cherry picked from commit 3e2797be911261dc7f7683f14904999d42b777c9)
    (cherry picked from commit 78ef3b1b97670e1b007eb22c0dcfacf451894983)

--- drivers/gpu/drm/imx/dpu/dpu-plane.c
+++ drivers/gpu/drm/imx/dpu/dpu-plane.c
@@ -25,7 +25,7 @@
 #include "dpu-plane.h"
 #include "imx-drm.h"
 
-static const uint32_t dpu_formats[] = {
+static const uint32_t dpu_formats_b0[] = {
 	DRM_FORMAT_ARGB8888,
 	DRM_FORMAT_XRGB8888,
 	DRM_FORMAT_ABGR8888,
@@ -40,6 +40,31 @@ static const uint32_t dpu_formats[] = {
 	DRM_FORMAT_UYVY,
 	DRM_FORMAT_NV12,
 	DRM_FORMAT_NV21,
+};
+
+static const uint32_t dpu_formats_c0[] = {
+	/* formats supported by prefetch engine */
+	DRM_FORMAT_ARGB8888,
+	DRM_FORMAT_XRGB8888,
+	DRM_FORMAT_ABGR8888,
+	DRM_FORMAT_XBGR8888,
+	DRM_FORMAT_RGBA8888,
+	DRM_FORMAT_RGBX8888,
+	DRM_FORMAT_BGRA8888,
+	DRM_FORMAT_BGRX8888,
+	DRM_FORMAT_RGB565,
+
+	DRM_FORMAT_YUYV,
+	DRM_FORMAT_UYVY,
+	DRM_FORMAT_NV12,
+	DRM_FORMAT_NV21,
+
+	/* formats unsupported by prefetch engine */
+	DRM_FORMAT_RGB888,
+	DRM_FORMAT_BGR888,
+
+	DRM_FORMAT_NV16,
+	DRM_FORMAT_NV61,
 };
 
 static const uint64_t dpu_format_modifiers[] = {
@@ -140,8 +165,12 @@ static bool dpu_drm_plane_format_mod_supported(struct drm_plane *plane,
 		return false;
 
 	switch (format) {
+	case DRM_FORMAT_RGB888:
+	case DRM_FORMAT_BGR888:
 	case DRM_FORMAT_YUYV:
 	case DRM_FORMAT_UYVY:
+	case DRM_FORMAT_NV16:
+	case DRM_FORMAT_NV61:
 		return modifier == DRM_FORMAT_MOD_LINEAR;
 	case DRM_FORMAT_ARGB8888:
 	case DRM_FORMAT_XRGB8888:
@@ -225,7 +254,8 @@ drm_plane_state_to_uvbaseaddr(struct drm_plane_state *state, bool aux_source)
 static inline bool dpu_plane_fb_format_is_yuv(u32 fmt)
 {
 	return fmt == DRM_FORMAT_YUYV || fmt == DRM_FORMAT_UYVY ||
-	       fmt == DRM_FORMAT_NV12 || fmt == DRM_FORMAT_NV21;
+	       fmt == DRM_FORMAT_NV12 || fmt == DRM_FORMAT_NV21 ||
+	       fmt == DRM_FORMAT_NV16 || fmt == DRM_FORMAT_NV61;
 }
 
 static int dpu_plane_atomic_check(struct drm_plane *plane,
@@ -862,6 +892,8 @@ struct dpu_plane *dpu_plane_init(struct drm_device *drm,
 {
 	struct dpu_plane *dpu_plane;
 	struct drm_plane *plane;
+	const uint32_t *formats;
+	unsigned int format_count;
 	unsigned int zpos = dpu_plane_get_default_zpos(type);
 	int ret;
 
@@ -874,9 +906,17 @@ struct dpu_plane *dpu_plane_init(struct drm_device *drm,
 
 	plane = &dpu_plane->base;
 
+	if (imx8_get_soc_revision() == 0x11) {
+		formats = dpu_formats_b0;
+		format_count = ARRAY_SIZE(dpu_formats_b0);
+	} else {
+		formats = dpu_formats_c0;
+		format_count = ARRAY_SIZE(dpu_formats_c0);
+	}
+
 	ret = drm_universal_plane_init(drm, plane, possible_crtcs,
 				       &dpu_plane_funcs,
-				       dpu_formats, ARRAY_SIZE(dpu_formats),
+				       formats, format_count,
 				       dpu_format_modifiers,
 				       type, NULL);
 	if (ret)
