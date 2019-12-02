7c756a2ba4180a9d26411c839a1c2180bafd29d7
Haoran.Wang <elven.wang@nxp.com>
2019-12-02 15:57:08 +0800

    MA-15997-1 Add back Trusty OS GICv3 workaround
    Some SoC need the workaround for GICv3 to pass Trusty OS stress test.
    So add it back.
    
    If need this workaround, then add "use-gicv3-workaround" in the dts
    node.
    
    Change-Id: Icf4b27f7709b8237d9425ba3e2de3c8adda09aeb
    Signed-off-by: Haoran.Wang <elven.wang@nxp.com>
    
     Conflicts:
    	drivers/trusty/trusty.c
    
    Change-Id: Ic45862f69e010616e33c410b24efcd0181d31517

--- drivers/trusty/trusty.c
+++ drivers/trusty/trusty.c
@@ -57,6 +57,7 @@ struct trusty_state {
 	u16 ffa_local_id;
 	u16 ffa_remote_id;
 	struct mutex share_memory_msg_lock; /* protects share_memory_msg */
+	bool gicv3_workaround;
 };
 
 static inline unsigned long smc(unsigned long r0, unsigned long r1,
@@ -137,7 +138,13 @@ static unsigned long trusty_std_call_helper(struct device *dev,
 	struct trusty_state *s = platform_get_drvdata(to_platform_device(dev));
 
 	while (true) {
-		local_irq_disable();
+		/*
+		 * In GICv3, we don't use non-secure world generated interrupt
+		 * so no need disable IRQ here. Or the non-secure IRQ will never
+		 * be handle before the SMC process exited.
+		 */
+		if (!s->gicv3_workaround)
+			local_irq_disable();
 
 		/* tell Trusty scheduler what the current priority is */
 		if (s->trusty_sched_share_state) {
@@ -167,7 +174,8 @@ static unsigned long trusty_std_call_helper(struct device *dev,
 			 */
 			trusty_enqueue_nop(dev, NULL);
 		}
-		local_irq_enable();
+		if (!s->gicv3_workaround)
+			local_irq_enable();
 
 		if ((int)ret != SM_ERR_BUSY)
 			break;
@@ -1016,6 +1024,11 @@ static int trusty_probe(struct platform_device *pdev)
 		goto err_add_children;
 	}
 
+	if (of_find_property(s->dev->of_node, "use-gicv3-workaround", NULL)) {
+		s->gicv3_workaround = true;
+	} else {
+		s->gicv3_workaround = false;
+	}
 	return 0;
 
 err_add_children:
