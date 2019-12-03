ad8db3b461f227999f68f736bfb0b085b9e75f65
Liu Ying <victor.liu@nxp.com>
2019-12-03 14:35:30 +0800

    MLK-23116-11 gpu: imx: imx8_dprc: Support hard argument for dprc_disable()
    As we would support some non-native prefetch engine pixel formats
    soon later with some coming patches, hot-switch from native pixel
    formats to non-native ones(on-the-fly PRG bypass) is inevitable.
    Basically, dprc_disable() is called to do the switch.  If a follow-up
    modeset is done to enable the prefetch engine again, we see prefetch
    engine fail-over issue.  So, a reset is needed in dprc_disable()
    in the need_modeset case.  It appears that PRG reset is enough.
    
    Reviewed-by: Sandor Yu <Sandor.yu@nxp.com>
    Signed-off-by: Liu Ying <victor.liu@nxp.com>
    (cherry picked from commit 52450396e492b555cd965b046e8982c77cdcafee)
    (cherry picked from commit 8045c5046812a81e530d123d2ee78ef2c9376b8b)

--- drivers/gpu/imx/imx8_dprc.c
+++ drivers/gpu/imx/imx8_dprc.c
@@ -276,7 +276,7 @@ void dprc_enable(struct dprc *dprc)
 }
 EXPORT_SYMBOL_GPL(dprc_enable);
 
-void dprc_disable(struct dprc *dprc)
+void dprc_disable(struct dprc *dprc, bool hard)
 {
 	if (WARN_ON(!dprc))
 		return;
@@ -285,9 +285,9 @@ void dprc_disable(struct dprc *dprc)
 		dprc_write(dprc, SHADOW_LOAD_EN | SW_SHADOW_LOAD_SEL,
 								SYSTEM_CTRL0);
 
-	prg_disable(dprc->prgs[0]);
+	prg_disable(dprc->prgs[0], hard);
 	if (dprc->has_aux_prg)
-		prg_disable(dprc->prgs[1]);
+		prg_disable(dprc->prgs[1], hard);
 
 	if (!dprc->is_blit_chan) {
 		/* avoid garbage data in RTRAM after PRG bypass on-the-fly */
