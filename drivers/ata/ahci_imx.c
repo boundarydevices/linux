2bbb62cec3fac2213669121631597e93cd86085f
Richard Zhu <hongxing.zhu@nxp.com>
2019-12-04 17:29:02 +0800

    MLK-23051 ata: imx: update the copyright
    Update the copyright of this driver.
    
    Signed-off-by: Richard Zhu <hongxing.zhu@nxp.com>
    Acked-by: Fugang Duan <fugang.duan@nxp.com>
    (cherry picked from commit e8ed08124f12b5ec623db58bdc30ed20af69f040)

--- drivers/ata/ahci_imx.c
+++ drivers/ata/ahci_imx.c
@@ -1,5 +1,6 @@
 /*
  * copyright (c) 2013 Freescale Semiconductor, Inc.
+ * Copyright 2018-2019 NXP
  * Freescale IMX AHCI SATA platform driver
  *
  * based on the AHCI SATA platform driver by Jeff Garzik and Anton Vorontsov
