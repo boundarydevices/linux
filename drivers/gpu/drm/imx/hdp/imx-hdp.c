7665b238534a05b123af43e14419f340b9e27937
Oliver Brown <oliver.brown@nxp.com>
2019-12-05 14:11:41 -0600

    MLK-22756 drm: imx: hdp: EDID read needs to re-enabled on plug out
    The no_edid flag must be set back to false after a plugout event
    so the EDID read can be enabled for the nex plugin event
    
    Signed-off-by: Oliver Brown <oliver.brown@nxp.com>

--- drivers/gpu/drm/imx/hdp/imx-hdp.c
+++ drivers/gpu/drm/imx/hdp/imx-hdp.c
@@ -1442,6 +1442,9 @@ static void hotplug_work_func(struct work_struct *work)
 		/* Cable Disconnedted  */
 		DRM_INFO("HDMI/DP Cable Plug Out\n");
 		enable_irq(hdp->irq[HPD_IRQ_IN]);
+
+		/* Allow enable EDID read for next plug in event */
+		hdp->no_edid = false;
 	}
 }
 
