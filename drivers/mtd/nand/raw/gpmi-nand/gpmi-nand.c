97448837b660760dd20da10c51b6ea276791a6e2
Han Xu <han.xu@nxp.com>
2019-12-05 15:48:21 -0600

    LF-369: mtd: nand: raw: gpmi-nand: fixe gpmi-nand runtime pm issue
    gpmi_init function may return without marking the last_busy status for
    runtime, which causes gpmi doesn't go to auto suspend. Also fixed the
    issue that pinctrl set to wrong state and re-apply timing after system
    resume.
    
    Signed-off-by: Han Xu <han.xu@nxp.com>

--- drivers/mtd/nand/raw/gpmi-nand/gpmi-nand.c
+++ drivers/mtd/nand/raw/gpmi-nand/gpmi-nand.c
@@ -148,7 +148,7 @@ err_clk:
 static int gpmi_init(struct gpmi_nand_data *this)
 {
 	struct resources *r = &this->resources;
-	int ret;
+	int ret = 0;
 
 	ret = pm_runtime_get_sync(this->dev);
 	if (ret < 0) {
@@ -735,7 +735,7 @@ static int common_nfc_set_geometry(struct gpmi_nand_data *this)
 static int bch_set_geometry(struct gpmi_nand_data *this)
 {
 	struct resources *r = &this->resources;
-	int ret;
+	int ret = 0;
 
 	ret = common_nfc_set_geometry(this);
 	if (ret)
@@ -759,7 +759,6 @@ static int bch_set_geometry(struct gpmi_nand_data *this)
 	/* Set *all* chip selects to use layout 0. */
 	writel(0, r->bch_regs + HW_BCH_LAYOUTSELECT);
 
-	ret = 0;
 err_out:
 	pm_runtime_mark_last_busy(this->dev);
 	pm_runtime_put_autosuspend(this->dev);
@@ -2805,7 +2804,7 @@ static int gpmi_nand_probe(struct platform_device *pdev)
 	return 0;
 
 exit_nfc_init:
-	pm_runtime_put(&pdev->dev);
+	pm_runtime_dont_use_autosuspend(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
 	release_resources(this);
 exit_acquire_resources:
@@ -2819,7 +2818,6 @@ static int gpmi_nand_remove(struct platform_device *pdev)
 	struct nand_chip *chip = &this->nand;
 	int ret;
 
-	pm_runtime_put_sync(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
 
 	ret = mtd_device_unregister(nand_to_mtd(chip));
@@ -2852,7 +2850,7 @@ static int gpmi_pm_resume(struct device *dev)
 		return ret;
 	}
 
-	pinctrl_pm_select_sleep_state(dev);
+	pinctrl_pm_select_default_state(dev);
 
 	/* re-init the GPMI registers */
 	ret = gpmi_init(this);
@@ -2871,6 +2869,9 @@ static int gpmi_pm_resume(struct device *dev)
 		dev_err(this->dev, "Error setting BCH : %d\n", ret);
 		return ret;
 	}
+
+	/* re-apply the timing setting */
+	this->hw.must_apply_timings = true;
 
 	return 0;
 }
