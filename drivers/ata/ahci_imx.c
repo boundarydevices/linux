470dbbcbdfb29ce3ab46cdec3d5aa69bd616a07d
Li Yang <leoyang.li@nxp.com>
2019-12-06 16:44:42 -0600

    ata: ahci_imx: replace dev->bus_dma_mask with bus_dma_limit
    Update to deal with the upstream data structure change
    
    Fixes: a7ba70f178 ("dma-mapping: treat dev->bus_dma_mask as a DMA limit")
    
    Signed-off-by: Li Yang <leoyang.li@nxp.com>

--- drivers/ata/ahci_imx.c
+++ drivers/ata/ahci_imx.c
@@ -1243,8 +1243,8 @@ static int imx8_sata_probe(struct device *dev, struct imx_ahci_priv *imxpriv)
 	struct device_node *np = dev->of_node;
 	struct regmap_config regconfig = imx_sata_regconfig;
 
-	if (!(dev->bus_dma_mask)) {
-		dev->bus_dma_mask = DMA_BIT_MASK(32);
+	if (!(dev->bus_dma_limit)) {
+		dev->bus_dma_limit = DMA_BIT_MASK(32);
 		dev_info(dev, "imx8qm sata only supports 32bit dma.\n");
 	}
 	if (of_property_read_u32(np, "ext_osc", &imxpriv->ext_osc) < 0) {
