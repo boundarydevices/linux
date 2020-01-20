7f9bcef68d1c5acfd14de916fb68183d0da1c0cd
Troy Kisky <troy.kisky@boundarydevices.com>
2020-01-20 14:28:52 -0800

    soc: imx: gpc-psci: add DVFS for 1 regulator controlled by many clocks
    ie. VPU, imx8mm
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/soc/imx/gpc-psci.c
+++ drivers/soc/imx/gpc-psci.c
@@ -15,6 +15,7 @@
 
 #include <linux/arm-smccc.h>
 #include <linux/clk.h>
+#include <linux/clk-provider.h>
 #include <linux/cpumask.h>
 #include <linux/delay.h>
 #include <linux/io.h>
@@ -28,18 +29,27 @@
 #include <linux/platform_device.h>
 #include <linux/regulator/consumer.h>
 #include <linux/pm_domain.h>
+#include <linux/pm_opp.h>
 #include <soc/imx/fsl_sip.h>
 
 #define GPC_MAX_IRQS		(4 * 32)
 
+struct imx_gpc_pm_domain;
+
 struct imx_gpc_pm_domain {
-	const char name[30];
 	struct device *dev;
 	struct generic_pm_domain pd;
-	u32 gpc_domain_id;
+	struct imx_gpc_pm_domain *parent;
+	int gpc_domain_id;
 	struct clk **clks;
 	unsigned int num_clks;
 	struct regulator *reg;
+	struct regulator *dvfs_reg;
+	struct device_node **opp_np;
+	unsigned long u_volt;
+	unsigned long idle_uv;
+	unsigned long idle_uv_min;
+	unsigned long idle_uv_max;
 };
 
 enum imx_gpc_pm_domain_state {
@@ -196,6 +206,114 @@ static int __init imx_gpc_psci_init(struct device_node *node,
 }
 IRQCHIP_DECLARE(imx_gpc_psci, "fsl,imx8mq-gpc", imx_gpc_psci_init);
 
+#define genpd_status_on(genpd)		((genpd)->status == GPD_STATE_ACTIVE)
+
+struct uv_freq {
+	unsigned long uv;
+	unsigned long uv_min;
+	unsigned long uv_max;
+	unsigned long freq;
+};
+
+static void get_max_uv(struct imx_gpc_pm_domain *pd, struct uv_freq *uvf)
+{
+	struct clk **clks = pd->clks;
+	struct device_node **opp_np = pd->opp_np;
+	int i = 0;
+	struct dev_pm_opp *opp;
+
+	if (!opp_np || !clks || !pd->num_clks)
+		return;
+
+	while (clks[i] && opp_np[i]) {
+		struct uv_freq uv = {
+			.uv = 0,
+			.uv_min = 0,
+			.uv_max = 0,
+			.freq = 0,
+		};
+
+		uv.freq = clk_get_rate(clks[i]);
+		opp = dev_pm_opp_find_freq_ceil_np(pd->dev, opp_np[i],
+				&uv.freq);
+		if (opp) {
+			opp_return_volts(opp, &uv.uv, &uv.uv_min, &uv.uv_max);
+			pr_debug("%s: voltage=%ld freq=%ld, clk=%s\n", __func__,
+					uv.uv, uv.freq, __clk_get_name(clks[i]));
+			if (uvf->uv < uv.uv)
+				*uvf = uv;
+		}
+		i++;
+		if (i >= pd->num_clks)
+			break;
+	}
+}
+
+static void rescan_voltage(struct imx_gpc_pm_domain *parent, struct generic_pm_domain *skip)
+{
+	struct generic_pm_domain *genpd = &parent->pd;
+	struct generic_pm_domain *child_pd;
+	struct gpd_link *link;
+	struct uv_freq u_volt = {
+		.uv = 0,
+		.uv_min = 0,
+		.uv_max = 0,
+		.freq = 0,
+	};
+	int ret;
+
+	if (!parent->dvfs_reg)
+		return;
+
+	list_for_each_entry(link, &genpd->master_links, master_node) {
+		child_pd = link->slave;
+
+		if ((child_pd != skip) && genpd_status_on(child_pd))
+			get_max_uv(to_imx_gpc_pm_domain(child_pd), &u_volt);
+	}
+	get_max_uv(parent, &u_volt);
+
+	if (u_volt.uv && (parent->u_volt != u_volt.uv)) {
+		parent->u_volt = u_volt.uv;
+		ret = regulator_set_voltage_triplet(parent->dvfs_reg,
+				u_volt.uv_min,
+				u_volt.uv,
+				u_volt.uv_max);
+		pr_info("%s: voltage=%ld freq=%ld %s\n", __func__,
+				u_volt.uv, u_volt.freq, genpd->name);
+	}
+}
+
+static void check_voltage(struct imx_gpc_pm_domain *pd)
+{
+	struct imx_gpc_pm_domain *parent = pd->parent;
+	int ret;
+	struct uv_freq u_volt = {
+		.uv = 0,
+		.uv_min = 0,
+		.uv_max = 0,
+		.freq = 0,
+	};
+
+	if (!parent)
+		parent = pd;
+	if (!parent->dvfs_reg)
+		return;
+
+	get_max_uv(pd, &u_volt);
+
+	if (parent->u_volt < u_volt.uv) {
+		parent->u_volt = u_volt.uv;
+		ret = regulator_set_voltage_triplet(parent->dvfs_reg,
+				u_volt.uv_min,
+				u_volt.uv,
+				u_volt.uv_max);
+		pr_info("%s: voltage=%ld %ld %ld freq=%ld %s\n", __func__,
+			u_volt.uv_min, u_volt.uv, u_volt.uv_max, u_volt.freq,
+			parent->pd.name);
+	}
+}
+
 static int imx_gpc_pd_power_on(struct generic_pm_domain *domain)
 {
 	struct imx_gpc_pm_domain *pd = to_imx_gpc_pm_domain(domain);
@@ -210,6 +328,15 @@ static int imx_gpc_pd_power_on(struct generic_pm_domain *domain)
 			return ret;
 		}
 	}
+	check_voltage(pd);
+
+	if (pd->dvfs_reg) {
+		ret = regulator_enable(pd->dvfs_reg);
+		if (ret) {
+			dev_warn(pd->dev, "failed to power up the dvfs-reg(%d)\n", ret);
+			return ret;
+		}
+	}
 
 	/* enable the necessary clks needed by the power domain */
 	if (pd->num_clks) {
@@ -217,11 +344,12 @@ static int imx_gpc_pd_power_on(struct generic_pm_domain *domain)
 			clk_prepare_enable(pd->clks[index]);
 	}
 
-	mutex_lock(&gpc_pd_mutex);
-	arm_smccc_smc(FSL_SIP_GPC, FSL_SIP_CONFIG_GPC_PM_DOMAIN, pd->gpc_domain_id,
-		      GPC_PD_STATE_ON, 0, 0, 0, 0, &res);
-	mutex_unlock(&gpc_pd_mutex);
-
+	if (pd->gpc_domain_id >= 0) {
+		mutex_lock(&gpc_pd_mutex);
+		arm_smccc_smc(FSL_SIP_GPC, FSL_SIP_CONFIG_GPC_PM_DOMAIN, pd->gpc_domain_id,
+				GPC_PD_STATE_ON, 0, 0, 0, 0, &res);
+		mutex_unlock(&gpc_pd_mutex);
+	}
 	return 0;
 }
 
@@ -231,10 +359,37 @@ static int imx_gpc_pd_power_off(struct generic_pm_domain *domain)
 	struct arm_smccc_res res;
 	int index, ret = 0;
 
-	mutex_lock(&gpc_pd_mutex);
-	arm_smccc_smc(FSL_SIP_GPC, FSL_SIP_CONFIG_GPC_PM_DOMAIN, pd->gpc_domain_id,
-		      GPC_PD_STATE_OFF, 0, 0, 0, 0, &res);
-	mutex_unlock(&gpc_pd_mutex);
+	if (pd->gpc_domain_id >= 0) {
+		mutex_lock(&gpc_pd_mutex);
+		arm_smccc_smc(FSL_SIP_GPC, FSL_SIP_CONFIG_GPC_PM_DOMAIN, pd->gpc_domain_id,
+				GPC_PD_STATE_OFF, 0, 0, 0, 0, &res);
+		mutex_unlock(&gpc_pd_mutex);
+	}
+
+	/* disable the necessary clks when power domain on finished */
+	if (pd->num_clks) {
+		for (index = 0; index < pd->num_clks; index++)
+			clk_disable_unprepare(pd->clks[index]);
+	}
+
+	if (pd->dvfs_reg) {
+		ret = regulator_disable(pd->dvfs_reg);
+		if (ret)
+			dev_warn(pd->dev, "failed to power off the dvfs-reg(%d)\n", ret);
+	}
+	if (pd->opp_np && pd->parent)
+		rescan_voltage(pd->parent, domain);
+
+	if ((pd->u_volt != pd->idle_uv) && pd->dvfs_reg && pd->idle_uv) {
+		pd->u_volt = pd->idle_uv;
+		ret = regulator_set_voltage_triplet(pd->dvfs_reg,
+				pd->idle_uv_min,
+				pd->idle_uv,
+				pd->idle_uv_max);
+		pr_info("%s: voltage=%ld %s\n", __func__, pd->idle_uv,
+				pd->pd.name);
+
+	}
 
 	/* power off the external supply */
 	if (pd->reg) {
@@ -244,13 +399,6 @@ static int imx_gpc_pd_power_off(struct generic_pm_domain *domain)
 			return ret;
 		}
 	}
-
-	/* disable the necessary clks when power domain on finished */
-	if (pd->num_clks) {
-		for (index = 0; index < pd->num_clks; index++)
-			clk_disable_unprepare(pd->clks[index]);
-	}
-
 	return ret;
 };
 
@@ -298,11 +446,13 @@ static int imx8m_pd_clk_init(struct device_node *np,
 }
 
 static int imx8m_add_subdomain(struct device_node *parent,
-			       struct generic_pm_domain *parent_pd)
+			       struct generic_pm_domain *parent_pd,
+			       struct device *dev)
 {
 	struct device_node *child_node;
 	struct imx_gpc_pm_domain *child_domain;
 	int ret = 0;
+	struct device_node **opp_np;
 
 	/* add each of the child domain of parent */
 	for_each_child_of_node(parent, child_node) {
@@ -312,6 +462,7 @@ static int imx8m_add_subdomain(struct device_node *parent,
 		child_domain = kzalloc(sizeof(*child_domain), GFP_KERNEL);
 		if (!child_domain)
 			return -ENOMEM;
+		child_domain->dev = dev;
 
 		ret = of_property_read_string(child_node, "domain-name",
 					      &child_domain->pd.name);
@@ -322,13 +473,24 @@ static int imx8m_add_subdomain(struct device_node *parent,
 					   &child_domain->gpc_domain_id);
 		if (ret)
 			goto exit;
+		pr_debug("%s: 0x%x\n", __func__, child_domain->gpc_domain_id);
 
 		child_domain->pd.power_off = imx_gpc_pd_power_off;
 		child_domain->pd.power_on = imx_gpc_pd_power_on;
 		/* no reg for subdomains */
 		child_domain->reg = NULL;
+		child_domain->parent = to_imx_gpc_pm_domain(parent_pd);
 
 		imx8m_pd_clk_init(child_node, child_domain);
+
+		if (child_domain->num_clks && of_find_property(child_node, "operating-points-v2", NULL)) {
+			opp_np = kcalloc(child_domain->num_clks, sizeof(*opp_np), GFP_KERNEL);
+			ret = dev_pm_opp_of_add_table_np(dev, child_node,
+					opp_np, child_domain->num_clks);
+			child_domain->opp_np = opp_np;
+			if (ret && (ret != -ENODEV))
+				return ret;
+		}
 
 		/* power domains as off at boot */
 		pm_genpd_init(&child_domain->pd, NULL, true);
@@ -348,11 +510,15 @@ exit:
 	return ret;
 };
 
+static unsigned char idle_uv[] = "idle-microvolt";
+
 static int imx_gpc_pm_domain_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	struct device_node *np = dev->of_node;
 	struct imx_gpc_pm_domain *imx_pm_domain;
+	int vcount;
+	u32 microvolt[3];
 	int ret = 0;
 
 	if (!np) {
@@ -376,6 +542,7 @@ static int imx_gpc_pm_domain_probe(struct platform_device *pdev)
 		dev_err(dev, "get domain id failed\n");
 		return -EINVAL;
 	}
+	pr_debug("%s: 0x%x\n", __func__, imx_pm_domain->gpc_domain_id);
 
 	imx_pm_domain->reg = devm_regulator_get_optional(dev, "power");
 	if (IS_ERR(imx_pm_domain->reg)) {
@@ -383,6 +550,31 @@ static int imx_gpc_pm_domain_probe(struct platform_device *pdev)
 			return -EPROBE_DEFER;
 
 		imx_pm_domain->reg = NULL;
+	}
+
+	imx_pm_domain->dvfs_reg = devm_regulator_get_optional(dev, "dvfs");
+	if (IS_ERR(imx_pm_domain->dvfs_reg)) {
+		if (PTR_ERR(imx_pm_domain->dvfs_reg) == -EPROBE_DEFER)
+			return -EPROBE_DEFER;
+		imx_pm_domain->dvfs_reg = NULL;
+	}
+
+	vcount = of_property_count_u32_elems(np, idle_uv);
+	if ((vcount == 1) || (vcount == 3)) {
+		ret = of_property_read_u32_array(np, idle_uv, microvolt, vcount);
+		if (ret) {
+			dev_err(dev, "%s: error parsing %s: %d\n", __func__, idle_uv, ret);
+			return -EINVAL;
+		}
+		if (vcount == 1) {
+			imx_pm_domain->idle_uv = microvolt[0];
+			imx_pm_domain->idle_uv_min = imx_pm_domain->idle_uv;
+			imx_pm_domain->idle_uv_max = imx_pm_domain->idle_uv;
+		} else {
+			imx_pm_domain->idle_uv = microvolt[0];
+			imx_pm_domain->idle_uv_min = microvolt[1];
+			imx_pm_domain->idle_uv_max = microvolt[2];
+		}
 	}
 
 	imx8m_pd_clk_init(np, imx_pm_domain);
@@ -396,7 +588,7 @@ static int imx_gpc_pm_domain_probe(struct platform_device *pdev)
 				 &imx_pm_domain->pd);
 
 	/* add subdomain */
-	ret = imx8m_add_subdomain(np, &imx_pm_domain->pd);
+	ret = imx8m_add_subdomain(np, &imx_pm_domain->pd, dev);
 	if (ret)
 		dev_warn(dev, "please check the child power domain init\n");
 
