18c876324b26bbb2ed8921cef43a8cbc6b74db56
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2020-02-02 20:24:33 +0800

    PCI: mobiveil: ls_pcie_g4: add Workaround for A-011264
    Clearing the Bus Master Enable bit in Command register blocks all
    outbound transactions to be sent out in RC mode
    
    Description:
    According to PCI Express base specification, the Command register’s
    Bus Master Enable bit of a PCI Express RC controller can only
    control the forwarding of memory requests received at its root port
    in the upstream direction. In other words, clearing the Bus Master
    Enable bit must not block all outbound transactions to be sent out
    toward RC’s downstream devices. Due to this erratum, when the
    Command register’s Bus Master Enable bit is cleared, all the outbound
    transactions from the device’s internal bus masters, including but
    not limited to configuration read and write transactions, are
    terminated with the slave error (SLVERR) response status on the PCI
    Express RC controller’s internal AXI bus interface.
    
    Workaround:
    Software must program the PCI Express RC controller's Command
    register [Bus Master Enable] bit to 1b before issuing any
    outbound transaction, including but not limited to configuration
    read and write transactions.
    
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- drivers/pci/controller/mobiveil/pcie-mobiveil-host.c
+++ drivers/pci/controller/mobiveil/pcie-mobiveil-host.c
@@ -61,6 +61,12 @@ static void __iomem *mobiveil_pcie_map_bus(struct pci_bus *bus,
 	if (pci_is_root_bus(bus))
 		return pcie->csr_axi_slave_base + where;
 
+	/* Make sure the Master Enable bit not cleared */
+	value = mobiveil_csr_readl(pcie, PCI_COMMAND);
+	if (!(value & PCI_COMMAND_MASTER))
+		mobiveil_csr_writel(pcie, value | PCI_COMMAND_MASTER,
+				    PCI_COMMAND);
+
 	/*
 	 * EP config access (in Config/APIO space)
 	 * Program PEX Address base (31..16 bits) with appropriate value
