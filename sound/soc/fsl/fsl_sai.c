6fe425591edbb16ca867bfc71d7bdd38f7cb9075
Oleksandr Suvorov <oleksandr.suvorov@toradex.com>
2020-02-05 18:04:36 +0200

    ASoC: fsl_sai: Fix exiting path on probing failure
    commit d1520889782dff58610c0b6b54d4cf3211ceb690 upstream.
    
    If the imx-sdma driver is built as a module, the fsl-sai device doesn't
    disable on probing failure, which causes the warning in the next probing:
    
    ==================================================================
    fsl-sai 308a0000.sai: Unbalanced pm_runtime_enable!
    fsl-sai 308a0000.sai: Unbalanced pm_runtime_enable!
    fsl-sai 308a0000.sai: Unbalanced pm_runtime_enable!
    fsl-sai 308a0000.sai: Unbalanced pm_runtime_enable!
    fsl-sai 308a0000.sai: Unbalanced pm_runtime_enable!
    fsl-sai 308a0000.sai: Unbalanced pm_runtime_enable!
    ==================================================================
    
    Disabling the device properly fixes the issue.
    
    Fixes: 812ad463e089 ("ASoC: fsl_sai: Add support for runtime pm")
    Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@toradex.com>
    Link: https://lore.kernel.org/r/20200205160436.3813642-1-oleksandr.suvorov@toradex.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- sound/soc/fsl/fsl_sai.c
+++ sound/soc/fsl/fsl_sai.c
@@ -1631,12 +1631,24 @@ static int fsl_sai_probe(struct platform_device *pdev)
 	ret = devm_snd_soc_register_component(&pdev->dev, &fsl_component,
 			&fsl_sai_dai, 1);
 	if (ret)
-		return ret;
+		goto err_pm_disable;
 
-	if (sai->soc->imx)
-		return imx_pcm_platform_register(&pdev->dev);
-	else
-		return devm_snd_dmaengine_pcm_register(&pdev->dev, NULL, 0);
+	if (sai->soc->imx) {
+		ret = imx_pcm_platform_register(&pdev->dev);
+		if (ret)
+			goto err_pm_disable;
+	} else {
+		ret = devm_snd_dmaengine_pcm_register(&pdev->dev, NULL, 0);
+		if (ret)
+			goto err_pm_disable;
+	}
+
+	return ret;
+
+err_pm_disable:
+	pm_runtime_disable(&pdev->dev);
+
+	return ret;
 }
 
 static int fsl_sai_remove(struct platform_device *pdev)
