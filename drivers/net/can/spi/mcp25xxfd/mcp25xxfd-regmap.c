f5312cf3d5f5757fe025954d3d4866dc0d9ede58
Marc Kleine-Budde <mkl@pengutronix.de>
2020-02-05 09:21:21 +0100

    can: mcp25xxfd: add preliminary support for read/write-with-CRC
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/spi/mcp25xxfd/mcp25xxfd-regmap.c
+++ drivers/net/can/spi/mcp25xxfd/mcp25xxfd-regmap.c
@@ -110,6 +110,89 @@ static int mcp25xxfd_regmap_read(void *context,
 	return spi_sync_transfer(spi, xfer, ARRAY_SIZE(xfer));
 }
 
+static int mcp25xxfd_regmap_crc_gather_write(void *context,
+					     const void *reg, size_t reg_len,
+					     const void *val, size_t val_len)
+{
+	struct spi_device *spi = context;
+	struct mcp25xxfd_priv *priv = spi_get_drvdata(spi);
+	struct spi_transfer xfer[] = {
+		{
+			.tx_buf = &priv->crc_buf.addr,
+			.len = sizeof(priv->crc_buf.addr),
+		}, {
+			.tx_buf = val,
+			.len = val_len,
+		}, {
+			.rx_buf = &priv->crc_buf.crc,
+			.len = sizeof(priv->crc_buf.crc),
+		},
+	};
+	u16 crc;
+
+	priv->crc_buf.addr.cmd = *(__be16 *)reg;
+	priv->crc_buf.addr.len = val_len;
+
+	crc = mcp25xxfd_crc16_compute(xfer[0].tx_buf, xfer[0].len,
+				      xfer[1].rx_buf, xfer[1].len);
+	priv->crc_buf.crc = cpu_to_be16(crc);
+
+	return spi_sync_transfer(spi, xfer, ARRAY_SIZE(xfer));
+}
+
+static int mcp25xxfd_regmap_crc_write(void *context,
+				      const void *data, size_t count)
+{
+	struct spi_device *spi = context;
+	struct mcp25xxfd_priv *priv = spi_get_drvdata(spi);
+
+	return mcp25xxfd_regmap_crc_gather_write(context, data,
+						 sizeof(priv->crc_buf.addr),
+						 data + 4, count - 4);
+}
+
+static int mcp25xxfd_regmap_crc_read(void *context,
+				     const void *reg, size_t reg_len,
+				     void *val_buf, size_t val_len)
+{
+	struct spi_device *spi = context;
+	struct mcp25xxfd_priv *priv = spi_get_drvdata(spi);
+	struct spi_transfer xfer[] = {
+		{
+			.tx_buf = &priv->crc_buf.addr,
+			.len = sizeof(priv->crc_buf.addr),
+		}, {
+			.rx_buf = val_buf,
+			.len = val_len,
+		}, {
+			.rx_buf = &priv->crc_buf.crc,
+			.len = sizeof(priv->crc_buf.crc),
+		},
+	};
+	u16 crc_received, crc_calculated;
+	int err;
+
+	priv->crc_buf.addr.cmd = *(__be16 *)reg;
+	priv->crc_buf.addr.len = val_len;
+
+	err = spi_sync_transfer(spi, xfer, ARRAY_SIZE(xfer));
+	if (err)
+		return err;
+
+	crc_received = be16_to_cpu(priv->crc_buf.crc);
+	crc_calculated = mcp25xxfd_crc16_compute(xfer[0].tx_buf, xfer[0].len,
+						 xfer[1].rx_buf, xfer[1].len);
+	if (crc_received != crc_calculated) {
+		__dump(xfer[0].tx_buf, xfer[0].len);
+		__dump(xfer[1].rx_buf, xfer[1].len);
+		__dump(&crc_received, sizeof(crc_received));
+		__dump(&crc_calculated, sizeof(crc_calculated));
+		return -EBADMSG;
+	}
+
+	return 0;
+}
+
 static const struct regmap_range mcp25xxfd_reg_table_yes_range[] = {
 	regmap_reg_range(0x000, 0x2ec),	/* CAN FD Controller Module SFR */
 	regmap_reg_range(0x400, 0xbfc),	/* RAM */
@@ -145,10 +228,41 @@ static const struct regmap_bus mcp25xxfd_bus = {
 	.val_format_endian_default = REGMAP_ENDIAN_LITTLE,
 };
 
+static const struct regmap_config mcp25xxfd_regmap_crc = {
+	.name = "crc",
+	.reg_bits = 16,
+	.reg_stride = 4,
+	.pad_bits = 16,		/* keep data bits aligned */
+	.val_bits = 32,
+	.max_register = 0xffc,
+	.wr_table = &mcp25xxfd_reg_table,
+	.rd_table = &mcp25xxfd_reg_table,
+	.cache_type = REGCACHE_NONE,
+	.read_flag_mask = (__force unsigned long)
+		cpu_to_be16(MCP25XXFD_INSTRUCTION_READ_CRC),
+	.write_flag_mask = (__force unsigned long)
+		cpu_to_be16(MCP25XXFD_INSTRUCTION_WRITE_CRC),
+};
+
+static const struct regmap_bus mcp25xxfd_bus_crc = {
+	.write = mcp25xxfd_regmap_crc_write,
+	.gather_write = mcp25xxfd_regmap_crc_gather_write,
+	.read = mcp25xxfd_regmap_crc_read,
+	.reg_format_endian_default = REGMAP_ENDIAN_BIG,
+	.val_format_endian_default = REGMAP_ENDIAN_LITTLE,
+	.max_raw_read = 256 - 4,
+	.max_raw_write = 256 - 4,
+};
+
 int mcp25xxfd_regmap_init(struct mcp25xxfd_priv *priv)
 {
 	priv->map = devm_regmap_init(&priv->spi->dev, &mcp25xxfd_bus,
 				     priv->spi, &mcp25xxfd_regmap);
+	if (IS_ERR(priv->map))
+		return PTR_ERR(priv->map);
 
-	return PTR_ERR_OR_ZERO(priv->map);
+	priv->map_crc = devm_regmap_init(&priv->spi->dev, &mcp25xxfd_bus_crc,
+					 priv->spi, &mcp25xxfd_regmap_crc);
+	return PTR_ERR_OR_ZERO(priv->map_crc);
+
 }
