c6c9a3fe1d79e0c7edc3a34399fe2ee9b2f0252e
mario tesi <mario.tesi@st.com>
2020-02-07 17:48:55 +0100

    driver:iio:imu:st_lsm6dsr: Added support to temperature sensor
    Added support to internal temperature sensor
    Load Temperature samples in HW FIFO
    
    Signed-off-by: mario tesi <mario.tesi@st.com>

--- drivers/iio/imu/st_lsm6dsr/st_lsm6dsr.h
+++ drivers/iio/imu/st_lsm6dsr/st_lsm6dsr.h
@@ -1,7 +1,7 @@
 /*
  * STMicroelectronics st_lsm6dsr sensor driver
  *
- * Copyright 2019 STMicroelectronics Inc.
+ * Copyright 2020 STMicroelectronics Inc.
  *
  * Lorenzo Bianconi <lorenzo.bianconi@st.com>
  *
@@ -20,6 +20,15 @@
 #define ST_LSM6DSR_ODR_EXPAND(odr, uodr)	((odr * 1000000) + uodr)
 
 #define ST_LSM6DSR_DEV_NAME			"lsm6dsr"
+
+#define ST_LSM6DSR_REG_FIFO_CTRL4_ADDR		0x0a
+#define ST_LSM6DSR_REG_ODR_T_BATCH_MASK		GENMASK(5, 4)
+
+#define ST_LSM6DSR_REG_OUT_TEMP_L_ADDR		0x20
+
+#define ST_LSM6DSR_TEMP_GAIN			256
+#define ST_LSM6DSR_TEMP_FS_GAIN			(1000000 / ST_LSM6DSR_TEMP_GAIN)
+#define ST_LSM6DSR_TEMP_OFFSET			6400
 
 #define ST_LSM6DSR_SAMPLE_SIZE			6
 #define ST_LSM6DSR_TS_SAMPLE_SIZE		4
@@ -108,9 +117,10 @@ struct st_lsm6dsr_fs {
 	u8 val;
 };
 
-#define ST_LSM6DSR_FS_LIST_SIZE		5
-#define ST_LSM6DSR_FS_ACC_LIST_SIZE	4
-#define ST_LSM6DSR_FS_GYRO_LIST_SIZE	5
+#define ST_LSM6DSR_FS_LIST_SIZE			5
+#define ST_LSM6DSR_FS_ACC_LIST_SIZE		4
+#define ST_LSM6DSR_FS_GYRO_LIST_SIZE		5
+#define ST_LSM6DSR_FS_TEMP_LIST_SIZE		1
 struct st_lsm6dsr_fs_table_entry {
 	u8 size;
 	struct st_lsm6dsr_fs fs_avl[ST_LSM6DSR_FS_LIST_SIZE];
@@ -135,6 +145,7 @@ struct st_lsm6dsr_ext_dev_info {
 enum st_lsm6dsr_sensor_id {
 	ST_LSM6DSR_ID_GYRO,
 	ST_LSM6DSR_ID_ACC,
+	ST_LSM6DSR_ID_TEMP,
 	ST_LSM6DSR_ID_EXT0,
 	ST_LSM6DSR_ID_EXT1,
 	ST_LSM6DSR_ID_STEP_COUNTER,
@@ -154,17 +165,18 @@ enum st_lsm6dsr_sensor_id {
 static const enum st_lsm6dsr_sensor_id st_lsm6dsr_main_sensor_list[] = {
 	 [0] = ST_LSM6DSR_ID_GYRO,
 	 [1] = ST_LSM6DSR_ID_ACC,
-	 [2] = ST_LSM6DSR_ID_STEP_COUNTER,
-	 [3] = ST_LSM6DSR_ID_STEP_DETECTOR,
-	 [4] = ST_LSM6DSR_ID_SIGN_MOTION,
-	 [5] = ST_LSM6DSR_ID_GLANCE,
-	 [6] = ST_LSM6DSR_ID_MOTION,
-	 [7] = ST_LSM6DSR_ID_NO_MOTION,
-	 [8] = ST_LSM6DSR_ID_WAKEUP,
-	 [9] = ST_LSM6DSR_ID_PICKUP,
-	[10] = ST_LSM6DSR_ID_ORIENTATION,
-	[11] = ST_LSM6DSR_ID_WRIST_TILT,
-	[12] = ST_LSM6DSR_ID_TILT,
+	 [2] = ST_LSM6DSR_ID_TEMP,
+	 [3] = ST_LSM6DSR_ID_STEP_COUNTER,
+	 [4] = ST_LSM6DSR_ID_STEP_DETECTOR,
+	 [5] = ST_LSM6DSR_ID_SIGN_MOTION,
+	 [6] = ST_LSM6DSR_ID_GLANCE,
+	 [7] = ST_LSM6DSR_ID_MOTION,
+	 [8] = ST_LSM6DSR_ID_NO_MOTION,
+	 [9] = ST_LSM6DSR_ID_WAKEUP,
+	[10] = ST_LSM6DSR_ID_PICKUP,
+	[11] = ST_LSM6DSR_ID_ORIENTATION,
+	[12] = ST_LSM6DSR_ID_WRIST_TILT,
+	[13] = ST_LSM6DSR_ID_TILT,
 };
 
 enum st_lsm6dsr_fifo_mode {
@@ -204,6 +216,7 @@ struct st_lsm6dsr_sensor {
 	int odr;
 	int uodr;
 
+	u32 offset;
 	u8 std_samples;
 	u8 std_level;
 
