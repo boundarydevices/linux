3f939bc19226c45f3d76d102ab28cdaf0f85cd79
Franck LENORMAND <franck.lenormand@nxp.com>
2020-02-07 11:02:47 +0100

    LF-824: fw: imx: Add power domain for MU of the SECO
    Signed-off-by: Franck LENORMAND <franck.lenormand@nxp.com>
    Acked-by: Dong Aisheng <aisheng.dong@nxp.com>
    Acked-by: Leonard Crestez <leonard.crestez@nxp.com>

--- drivers/firmware/imx/scu-pd.c
+++ drivers/firmware/imx/scu-pd.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
  * Copyright (C) 2016 Freescale Semiconductor, Inc.
- * Copyright 2017-2018 NXP
+ * Copyright 2017-2018,2020 NXP
  *	Dong Aisheng <aisheng.dong@nxp.com>
  *
  * Implementation of the SCU based Power Domains
@@ -180,6 +180,9 @@ static const struct imx_sc_pd_range imx8qxp_scu_pd_ranges[] = {
 	{ "cm41-pid", IMX_SC_R_M4_1_PID0, 5, true, 0},
 	{ "cm41-mu-a1", IMX_SC_R_M4_1_MU_1A, 1, false, 0},
 	{ "cm41-lpuart", IMX_SC_R_M4_1_UART, 1, false, 0},
+
+	/* SECO SS */
+	{ "seco_mu", IMX_SC_R_SECO_MU_2, 3, true, 2},
 };
 
 static const struct imx_sc_pd_soc imx8qxp_scu_pd = {
