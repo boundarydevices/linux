6a3cfe44a09a903b88c165e372c37e4297f1b9ff
Double Lo <double.lo@cypress.com>
2020-02-26 04:21:22 -0600

    brcmfmac: add 54591 PCIE device
    Support 54591 PCIE device.
    
    Signed-off-by: Double Lo <double.lo@cypress.com>
    Signed-off-by: Chi-hsien Lin <chi-hsien.lin@cypress.com>

--- drivers/net/wireless/broadcom/brcm80211/include/brcm_hw_ids.h
+++ drivers/net/wireless/broadcom/brcm80211/include/brcm_hw_ids.h
@@ -57,6 +57,9 @@
 #define CY_CC_43439_CHIP_ID		43439
 #define CY_CC_43752_CHIP_ID		43752
 #define CY_CC_89459_CHIP_ID		0x4355
+#define CY_CC_54591_CHIP_ID		0x54591
+#define CY_CC_55572_CHIP_ID		0xd908
+#define CY_CC_55500_CHIP_ID		0xD8CC
 
 /* USB Device IDs */
 #define BRCM_USB_43143_DEVICE_ID	0xbd1e
@@ -93,6 +96,7 @@
 #define BRCM_PCIE_4378_DEVICE_ID	0x4425
 #define CY_PCIE_89459_DEVICE_ID         0x4415
 #define CY_PCIE_89459_RAW_DEVICE_ID     0x4355
+#define CY_PCIE_54591_DEVICE_ID         0x4417
 
 /* brcmsmac IDs */
 #define BCM4313_D11N2G_ID	0x4727	/* 4313 802.11n 2.4G device */
