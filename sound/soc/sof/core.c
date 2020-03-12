dcd58fce6ac90a5c1d823fe8a49c83a599f5eda1
Daniel Baluta <daniel.baluta@nxp.com>
2020-03-12 22:20:42 +0200

    MLK-23560-1: ASoC: SOF: Remove duplicated code
    This is most likely introduced by a rebase. One can notice that
    same lines are copied.
    
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

--- sound/soc/sof/core.c
+++ sound/soc/sof/core.c
@@ -243,14 +243,6 @@ static int sof_probe_continue(struct snd_sof_dev *sdev)
 	if (!sof_ops(sdev)->runtime_suspend || !sof_ops(sdev)->runtime_resume)
 		pm_runtime_get_noresume(sdev->dev);
 
-	/*
-	 * Some platforms in SOF, ex: BYT, may not have their platform PM
-	 * callbacks set. Increment the usage count so as to
-	 * prevent the device from entering runtime suspend.
-	 */
-	if (!sof_ops(sdev)->runtime_suspend || !sof_ops(sdev)->runtime_resume)
-		pm_runtime_get_noresume(sdev->dev);
-
 	if (plat_data->sof_probe_complete)
 		plat_data->sof_probe_complete(sdev->dev);
 
