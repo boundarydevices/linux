1161dcb379582dbfdbe6637a6bc29baee84fbeba
Jindong <jindong.yue@nxp.com>
2020-03-19 14:56:27 +0800

    MA-16715 merge aosp android-5.4-stable branch: fix build error
    Below patch changed one interface "drm_mode_create_colorspace_property",
    which being used.
    
    Revert "UPSTREAM: drm: Rename HDMI colorspace property creation function"
    
    This reverts commit 15dff3e222afcc8f4fe807d00ead35f6626298f2.
    
    Change-Id: Idab9b4fc98341ab1a47a48f61ff27ecb272e1283
    Signed-off-by: Jindong <jindong.yue@nxp.com>

--- drivers/gpu/drm/drm_connector.c
+++ drivers/gpu/drm/drm_connector.c
@@ -1706,6 +1706,7 @@ EXPORT_SYMBOL(drm_mode_create_aspect_ratio_property);
  * DOC: standard connector properties
  *
  * Colorspace:
+ *     drm_mode_create_colorspace_property - create colorspace property
  *     This property helps select a suitable colorspace based on the sink
  *     capability. Modern sink devices support wider gamut like BT2020.
  *     This helps switch to BT2020 mode if the BT2020 encoded video stream
@@ -1725,39 +1726,32 @@ EXPORT_SYMBOL(drm_mode_create_aspect_ratio_property);
  *      - This property is just to inform sink what colorspace
  *        source is trying to drive.
  *
- * Because between HDMI and DP have different colorspaces,
- * drm_mode_create_hdmi_colorspace_property() is used for HDMI connector and
- * drm_mode_create_dp_colorspace_property() is used for DP connector.
- */
-
-/**
- * drm_mode_create_hdmi_colorspace_property - create hdmi colorspace property
- * @connector: connector to create the Colorspace property on.
- *
  * Called by a driver the first time it's needed, must be attached to desired
- * HDMI connectors.
- *
- * Returns:
- * Zero on success, negative errono on failure.
+ * connectors.
  */
-int drm_mode_create_hdmi_colorspace_property(struct drm_connector *connector)
+int drm_mode_create_colorspace_property(struct drm_connector *connector)
 {
 	struct drm_device *dev = connector->dev;
+	struct drm_property *prop;
 
-	if (connector->colorspace_property)
+	if (connector->connector_type == DRM_MODE_CONNECTOR_HDMIA ||
+	    connector->connector_type == DRM_MODE_CONNECTOR_HDMIB) {
+		prop = drm_property_create_enum(dev, DRM_MODE_PROP_ENUM,
+						"Colorspace",
+						hdmi_colorspaces,
+						ARRAY_SIZE(hdmi_colorspaces));
+		if (!prop)
+			return -ENOMEM;
+	} else {
+		DRM_DEBUG_KMS("Colorspace property not supported\n");
 		return 0;
+	}
 
-	connector->colorspace_property =
-		drm_property_create_enum(dev, DRM_MODE_PROP_ENUM, "Colorspace",
-					 hdmi_colorspaces,
-					 ARRAY_SIZE(hdmi_colorspaces));
-
-	if (!connector->colorspace_property)
-		return -ENOMEM;
+	connector->colorspace_property = prop;
 
 	return 0;
 }
-EXPORT_SYMBOL(drm_mode_create_hdmi_colorspace_property);
+EXPORT_SYMBOL(drm_mode_create_colorspace_property);
 
 /**
  * drm_mode_create_dp_colorspace_property - create dp colorspace property
