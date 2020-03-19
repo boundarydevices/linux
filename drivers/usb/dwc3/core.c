1f1ec156058b18bd1b3ebb8ae080f4635685cbe9
Gary Bisson <gary.bisson@boundarydevices.com>
2020-03-19 11:17:10 +0100

    usb: dwc3: add reset gpio
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -27,6 +27,7 @@
 #include <linux/pinctrl/consumer.h>
 #include <linux/regulator/consumer.h>
 #include <linux/reset.h>
+#include <linux/gpio/consumer.h>
 
 #include <linux/usb/ch9.h>
 #include <linux/usb/gadget.h>
@@ -1589,6 +1590,7 @@ static int dwc3_probe(struct platform_device *pdev)
 	struct device		*dev = &pdev->dev;
 	struct resource		*res, dwc_res;
 	struct dwc3		*dwc;
+	struct gpio_desc	*reset_gpio;
 
 	int			ret;
 
@@ -1633,6 +1635,9 @@ static int dwc3_probe(struct platform_device *pdev)
 
 	dwc3_get_properties(dwc);
 
+	reset_gpio = devm_gpiod_get_optional(dev, "reset", GPIOD_OUT_HIGH);
+	if (IS_ERR(reset_gpio))
+		return PTR_ERR(reset_gpio);
 	dwc->reset = devm_reset_control_get_optional_shared(dev, NULL);
 	if (IS_ERR(dwc->reset))
 		return PTR_ERR(dwc->reset);
@@ -1686,6 +1691,8 @@ static int dwc3_probe(struct platform_device *pdev)
 		ret = -ENOMEM;
 		goto err2;
 	}
+
+	gpiod_set_value_cansleep(reset_gpio, 0);
 
 	ret = dwc3_get_dr_mode(dwc);
 	if (ret)
