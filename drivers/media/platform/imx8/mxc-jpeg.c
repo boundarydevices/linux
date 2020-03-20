228b5453121055f7dc43ff960669f5da13be94a0
Dong Aisheng <aisheng.dong@nxp.com>
2020-03-20 18:14:02 +0800

    LF-1162-14 media: jpeg: change to use VFL_TYPE_VIDEO
    The legacy one VFL_TYPE_GRABBER was removed since:
    dc42cf74d98a ("media: v4l2-dev.h: remove VFL_TYPE_GRABBER")
    
    Signed-off-by: Dong Aisheng <aisheng.dong@nxp.com>

--- drivers/media/platform/imx8/mxc-jpeg.c
+++ drivers/media/platform/imx8/mxc-jpeg.c
@@ -2175,7 +2175,7 @@ static int mxc_jpeg_probe(struct platform_device *pdev)
 	jpeg->dec_vdev->device_caps = V4L2_CAP_STREAMING |
 					V4L2_CAP_VIDEO_M2M_MPLANE;
 
-	ret = video_register_device(jpeg->dec_vdev, VFL_TYPE_GRABBER, -1);
+	ret = video_register_device(jpeg->dec_vdev, VFL_TYPE_VIDEO, -1);
 	if (ret) {
 		dev_err(dev, "failed to register video device\n");
 		goto err_vdev_register;
