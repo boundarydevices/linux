cde6e7be43ddc3593d59f77f135697b2f2d01794
Jindong <jindong.yue@nxp.com>
2020-03-29 10:27:17 +0800

    MA-16805 vehicle: integrate external nanopb-c into vehicle driver
    nanopb-c is cloned from external/nanopb-c.
    
    Signed-off-by: Jindong <jindong.yue@nxp.com>
    Change-Id: Ida639b3eac2e4d53d34d017a15dc4cd5c75d6758

---
+++ drivers/mxc/vehicle/nanopb-c/Makefile
@@ -0,0 +1,7 @@
+#
+# Makefile file pb_encode pb_decode pb_common
+#
+
+EXTRA_CFLAGS := -Wall -W -Wno-unused-parameter -DPB_SYSTEM_HEADER=\"pb_system.h\"
+obj-y	+= pb_decode.o pb_encode.o pb_common.o
+
---
+++ drivers/mxc/vehicle/nanopb-c/pb_system.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2018 NXP
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+#ifndef PB_SYSTEM_H
+#define PB_SYSTEM_H
+
+#include <linux/module.h>
+
+typedef u8 uint_least8_t;
+typedef s8 int_least8_t;
+typedef u8 uint_fast8_t;
+
+typedef u16 uint_least16_t;
+typedef s16 int_least16_t;
+
+#endif
