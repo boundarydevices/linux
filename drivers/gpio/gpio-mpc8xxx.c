3e8aa9e7346844b1dd8992a14a3ee1293bf61c4c
Song Hui <hui.song_1@nxp.com>
2020-04-08 10:45:44 +0800

    gpio: mpc8xxx: Add shutdown function.
    The shutdown function needed to make interrupt handler to be NULL
    when kexec execute.
    
    Signed-off-by: Song Hui <hui.song_1@nxp.com>

--- drivers/gpio/gpio-mpc8xxx.c
+++ drivers/gpio/gpio-mpc8xxx.c
@@ -432,6 +432,16 @@ static int mpc8xxx_remove(struct platform_device *pdev)
 	return 0;
 }
 
+static void mpc8xxx_shutdown(struct platform_device *pdev)
+{
+	struct mpc8xxx_gpio_chip *mpc8xxx_gc = platform_get_drvdata(pdev);
+
+	if (mpc8xxx_gc->irq) {
+		irq_set_chained_handler_and_data(mpc8xxx_gc->irqn, NULL, NULL);
+		irq_domain_remove(mpc8xxx_gc->irq);
+	}
+}
+
 #ifdef CONFIG_ACPI
 static const struct acpi_device_id gpio_acpi_ids[] = {
 	{"NXP0031",},
@@ -443,6 +453,7 @@ MODULE_DEVICE_TABLE(acpi, gpio_acpi_ids);
 static struct platform_driver mpc8xxx_plat_driver = {
 	.probe		= mpc8xxx_probe,
 	.remove		= mpc8xxx_remove,
+	.shutdown	= mpc8xxx_shutdown,
 	.driver		= {
 		.name = "gpio-mpc8xxx",
 		.of_match_table	= mpc8xxx_gpio_ids,
