f62b369b7ba388828d5d596c5977c1aed344769f
Gary Bisson <gary.bisson@boundarydevices.com>
2020-04-09 14:31:17 +0200

    squash Input: sitronix_i2c_touch: initial addition
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- drivers/input/touchscreen/sitronix_i2c_touch.c
+++ drivers/input/touchscreen/sitronix_i2c_touch.c
@@ -366,11 +366,9 @@ long	 sitronix_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	if (_IOC_TYPE(cmd) != SMT_IOC_MAGIC) return -ENOTTY;
 	if (_IOC_NR(cmd) > SMT_IOC_MAXNR) return -ENOTTY;
 	if (_IOC_DIR(cmd) & _IOC_READ)
-		err = !access_ok(VERIFY_WRITE,(void __user *)arg,\
-				 _IOC_SIZE(cmd));
+		err = !access_ok((void __user *)arg, _IOC_SIZE(cmd));
 	else if (_IOC_DIR(cmd) & _IOC_WRITE)
-		err =  !access_ok(VERIFY_READ,(void __user *)arg,\
-				  _IOC_SIZE(cmd));
+		err =  !access_ok((void __user *)arg, _IOC_SIZE(cmd));
 	if (err) return -EFAULT;
 
 	switch(cmd) {
