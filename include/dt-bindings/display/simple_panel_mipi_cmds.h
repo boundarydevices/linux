28f7b9cbeb3666a0e7920ccd84c185ed6cff7520
Troy Kisky <troy.kisky@boundarydevices.com>
2020-05-09 12:28:10 -0700

    gpu: drm: panel: panel-simple: add DCS_READ1-4/GEN_READ1-4
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- include/dt-bindings/display/simple_panel_mipi_cmds.h
+++ include/dt-bindings/display/simple_panel_mipi_cmds.h
@@ -6,9 +6,11 @@
 #define S_DCS_L5P	6
 #define S_DELAY		0x40
 #define S_MRPS		0x41
-#define S_DCS_READ	0x42
-#define S_DCS_READ4	0x47
 #define S_DCS_LENGTH	0x48
+#define S_DCS_READ1	0x49
+#define S_DCS_READ2	0x4a
+#define S_DCS_READ3	0x4b
+#define S_DCS_READ4	0x4c
 #define S_IF_1_LANE	0x51
 #define S_IF_2_LANES	0x52
 #define S_IF_3_LANES	0x53
@@ -20,11 +22,21 @@
 #define S_GEN_L3P	0x84
 #define S_GEN_L4P	0x85
 #define S_GEN_L5P	0x86
-#define S_GEN_READ4	0xc7
 #define S_GEN_LENGTH	0xc8
+#define S_GEN_READ1	0xc9
+#define S_GEN_READ2	0xca
+#define S_GEN_READ3	0xcb
+#define S_GEN_READ4	0xcc
 
 #define MRPS(len)			S_MRPS len			/* Set max return packet size */
-#define DCS_READ(cmd, compare)		S_DCS_READ cmd compare
+#define DCS_READ(cmd, c1)		S_DCS_READ1 cmd c1
+#define DCS_READ1(cmd, c1)		S_DCS_READ1 cmd c1
+#define DCS_READ2(cmd, c1, c2)		S_DCS_READ2 cmd c1 c2
+#define DCS_READ3(cmd, c1, c2, c3)	S_DCS_READ3 cmd c1 c2 c3
+#define DCS_READ4(cmd, c1, c2, c3, c4)	S_DCS_READ4 cmd c1 c2 c3 c4
+#define GEN_READ1(addrl, addrh, c1)	S_GEN_READ1 addrl addrh c1
+#define GEN_READ2(addrl, addrh, c1)	S_GEN_READ2 addrl addrh c1 c2
+#define GEN_READ3(addrl, addrh, c1)	S_GEN_READ3 addrl addrh c1 c2 c3
 #define GEN_READ4(addrl, addrh, c1, c2, c3, c4)	S_GEN_READ4 addrl addrh c1 c2 c3 c4
 #define DCS_S0P(cmd)			S_DCS_S0P cmd			/* DCS_Short_Write_NP */
 #define DCS_S1P(cmd, p1)		S_DCS_S1P cmd p1		/* DCS_Short_Write_1P */
