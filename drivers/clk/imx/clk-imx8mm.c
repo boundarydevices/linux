e611fd80fb6a3bcb065c71a9543b5e9c93bab219
Troy Kisky <troy.kisky@boundarydevices.com>
2020-05-15 16:17:31 -0700

    clk: imx: clk-imx8mm: use subsys_initcall to probe before gpio which uses clocks
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/clk/imx/clk-imx8mm.c
+++ drivers/clk/imx/clk-imx8mm.c
@@ -705,4 +705,15 @@ static struct platform_driver imx8mm_clk_driver = {
 		.of_match_table = of_match_ptr(imx8mm_clk_of_match),
 	},
 };
-module_platform_driver(imx8mm_clk_driver);
+
+static int __init imx8mm_clk_driver_init(void)
+{
+	return platform_driver_register(&imx8mm_clk_driver);
+}
+subsys_initcall(imx8mm_clk_driver_init);
+
+static void __exit imx8mm_clk_driver_exit(void)
+{
+	platform_driver_unregister(&imx8mm_clk_driver);
+}
+module_exit(imx8mm_clk_driver_exit);
