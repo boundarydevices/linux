a1c6e109db1fb95fabb4718b6916fa3505278a9c
Ji Luo <ji.luo@nxp.com>
2020-06-11 20:54:19 +0800

    MA-17363-4 Enable trusty for xen
    Add trusty related nodes to enable trusty on xen dts. Also remove
    the xen_domain() check or trusty won't work.
    
    Change-Id: Ibdf4281a1cb9e489517e199ff5df3cd0878b7b25
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

--- drivers/virtio/virtio_ring.c
+++ drivers/virtio/virtio_ring.c
@@ -252,8 +252,6 @@ static bool vring_use_dma_api(struct virtio_device *vdev)
 	 * the DMA API if we're a Xen guest, which at least allows
 	 * all of the sensible Xen configurations to work correctly.
 	 */
-	if (xen_domain())
-		return true;
 
 	return false;
 }
