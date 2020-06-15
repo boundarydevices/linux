e8efbf148ae1773a0ffa0f40c41e442010416414
zhang sanshan <pete.zhang@nxp.com>
2020-06-15 18:23:55 +0800

    MA-17324-1 [Android_10_8QXP-C0]CTS: system crash when executing the CtsCameraTestCases
    use list_for_each_entry_safe to delete list out_active/out_pending/out_discard in order.
    remove redundant "while" logic which remove mxc_isi_buffer from list
    out_active/out_pending/out_discard
    
    Change-Id: I328acc7828e1121087e6423cd0f17d77b589e38c
    Signed-off-by: zhang sanshan <pete.zhang@nxp.com>

--- drivers/staging/media/imx/imx8-isi-cap.c
+++ drivers/staging/media/imx/imx8-isi-cap.c
@@ -479,37 +479,19 @@ static void cap_vb2_stop_streaming(struct vb2_queue *q)
 
 	spin_lock_irqsave(&isi_cap->slock, flags);
 
-	while (!list_empty(&isi_cap->out_active)) {
-		buf = list_entry(isi_cap->out_active.next,
-				 struct mxc_isi_buffer, list);
-		list_del(&buf->list);
-		if (buf->discard)
-			continue;
-
-		vb2_buffer_done(&buf->v4l2_buf.vb2_buf, VB2_BUF_STATE_ERROR);
-	}
-
-	while (!list_empty(&isi_cap->out_pending)) {
-		buf = list_entry(isi_cap->out_pending.next,
-				 struct mxc_isi_buffer, list);
-		list_del(&buf->list);
-		vb2_buffer_done(&buf->v4l2_buf.vb2_buf, VB2_BUF_STATE_ERROR);
-	}
-
-	while (!list_empty(&isi_cap->out_discard)) {
-		buf = list_entry(isi_cap->out_discard.next,
-				 struct mxc_isi_buffer, list);
-		list_del(&buf->list);
-	}
-
 	list_for_each_entry_safe(buf, tmp, &isi_cap->out_active, list) {
-		list_del(&buf->list);
-		vb2_buffer_done(&buf->v4l2_buf.vb2_buf, VB2_BUF_STATE_ERROR);
+		list_del_init(&buf->list);
+		if (!buf->discard)
+			vb2_buffer_done(&buf->v4l2_buf.vb2_buf, VB2_BUF_STATE_ERROR);
 	}
 
 	list_for_each_entry_safe(buf, tmp, &isi_cap->out_pending, list) {
-		list_del(&buf->list);
+		list_del_init(&buf->list);
 		vb2_buffer_done(&buf->v4l2_buf.vb2_buf, VB2_BUF_STATE_ERROR);
+	}
+
+	list_for_each_entry_safe(buf, tmp, &isi_cap->out_discard, list) {
+		list_del_init(&buf->list);
 	}
 
 	INIT_LIST_HEAD(&isi_cap->out_active);
