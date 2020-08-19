8bb1d64dfd91e83e4a67e5d16222777b11fd73b5
Dong Aisheng <aisheng.dong@nxp.com>
2020-08-19 17:06:56 +0800

    LF-2225-1 fbdev: mxsfb_sii902x: drop the using of FBINFO_MISC_USEREVENT
    To cope with upstream API change:
    d88ca7e1a27e ("fbmem: pull fbcon_update_vcs() out of fb_set_var()")
    
    Signed-off-by: Dong Aisheng <aisheng.dong@nxp.com>

--- drivers/video/fbdev/mxc/mxsfb_sii902x.c
+++ drivers/video/fbdev/mxc/mxsfb_sii902x.c
@@ -32,6 +32,7 @@
 #include <linux/delay.h>
 #include <linux/errno.h>
 #include <linux/fb.h>
+#include <linux/fbcon.h>
 #include <linux/init.h>
 #include <linux/platform_device.h>
 #include <linux/regulator/consumer.h>
@@ -215,6 +216,7 @@ static void sii902x_cable_connected(void)
 	int i;
 	const struct fb_videomode *mode;
 	struct fb_videomode m;
+	int ret;
 
 	if (sii902x_read_edid(sii902x.fbi) < 0)
 		dev_err(&sii902x.client->dev,
@@ -266,9 +268,9 @@ static void sii902x_cable_connected(void)
 
 			sii902x.fbi->var.activate |= FB_ACTIVATE_FORCE;
 			console_lock();
-			sii902x.fbi->flags |= FBINFO_MISC_USEREVENT;
-			fb_set_var(sii902x.fbi, &sii902x.fbi->var);
-			sii902x.fbi->flags &= ~FBINFO_MISC_USEREVENT;
+			ret = fb_set_var(sii902x.fbi, &sii902x.fbi->var);
+			if (!ret)
+				fbcon_update_vcs(sii902x.fbi, sii902x.fbi->var.activate & FB_ACTIVATE_ALL);
 			console_unlock();
 		}
 		/* Power on sii902x */
