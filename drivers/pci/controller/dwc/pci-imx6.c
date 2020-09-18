3volutionE/au-linux-imx

pci-imx6: use ext_src_clk_enabled to keep pcie_ext_src clk reference count accurate
Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

drivers/pci/host/pci-imx6.c
@@ -77,6 +77,7 @@ struct imx_pcie {
 	struct clk		*pcie_phy;
 	struct clk		*pcie_per;
 	struct clk		*pcie;
+	u32			ext_src_clk_enabled;
 	struct clk		*pcie_ext_src;
 	struct regmap		*iomuxc_gpr;
 	enum imx_pcie_variants variant;
@@ -1258,6 +1259,8 @@ static void imx_pcie_init_phy(struct imx_pcie *imx_pcie)
 			if (ret)
 				dev_err(imx_pcie->pp.dev,
 					"unable to enable pcie_ext_src clock\n");
+			else
+				imx_pcie->ext_src_clk_enabled = 1;
 		} else {
 			regmap_update_bits(imx_pcie->iomuxc_gpr, val,
 					IMX8MQ_GPR_PCIE_REF_USE_PAD,
@@ -1525,7 +1528,10 @@ static void pci_imx_clk_disable(struct device *dev)
 		regmap_update_bits(imx_pcie->iomuxc_gpr, val,
 				IMX8MQ_GPR_PCIE_CLK_REQ_OVERRIDE_EN,
 				0);
-		clk_disable_unprepare(imx_pcie->pcie_ext_src);
+		if (imx_pcie->ext_src_clk_enabled) {
+			imx_pcie->ext_src_clk_enabled = 0;
+			clk_disable_unprepare(imx_pcie->pcie_ext_src);
+		}
 		break;
 	case IMX8QXP:
 	case IMX8QM:

boundarydevices/linux

drivers/pci/controller/dwc/pci-imx6.c
@@ -113,6 +113,7 @@ struct imx6_pcie {
 	struct clk		*pcie_aux;
 	struct clk		*phy_per;
 	struct clk		*misc_per;
+	u32			ext_src_clk_enabled;
 	struct clk		*pcie_ext_src;
 	struct regmap		*iomuxc_gpr;
 	struct regmap		*hsiomix;
@@ -1063,8 +1064,10 @@ static void imx6_pcie_clk_disable(struct imx6_pcie *imx6_pcie)
 	default:
 		break;
 	}
-	if (imx6_pcie->ext_osc && imx6_pcie->pcie_ext_src)
+	if (imx6_pcie->ext_src_clk_enabled) {
+		imx6_pcie->ext_src_clk_enabled = 0;
 		clk_disable_unprepare(imx6_pcie->pcie_ext_src);
+	}
 }
 
 #define GPC_CNTR 0
@@ -1447,6 +1450,7 @@ static void imx6_pcie_init_phy(struct imx6_pcie *imx6_pcie)
 	if (imx6_pcie->ext_osc && imx6_pcie->pcie_ext_src) {
 		int ret;
 
+		imx6_pcie->ext_src_clk_enabled = 1;
 		ret = clk_prepare_enable(imx6_pcie->pcie_ext_src);
 		if (ret)
 			dev_err(imx6_pcie->pci->dev,