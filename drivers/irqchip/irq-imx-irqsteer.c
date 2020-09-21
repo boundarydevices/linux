f7bcfe06828ef1b7ec9ef8e656126c7bfc44b175
Jindong <jindong.yue@nxp.com>
2020-09-21 15:52:53 +0800

    MA-17912 irqchip: irqsteer: Allow IMX_IRQSTEER to be loadable as module
    Allows imx irqsteer driver to be loaded as module.
    
    Signed-off-by: Jindong <jindong.yue@nxp.com>
    Reviewed-by: Fugang Duan <fugang.duan@nxp.com>

--- drivers/irqchip/irq-imx-irqsteer.c
+++ drivers/irqchip/irq-imx-irqsteer.c
@@ -10,6 +10,7 @@
 #include <linux/irqchip/chained_irq.h>
 #include <linux/irqdomain.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/of_irq.h>
 #include <linux/of_platform.h>
 #include <linux/spinlock.h>
@@ -393,6 +394,7 @@ static const struct of_device_id imx_irqsteer_dt_ids[] = {
 	{ .compatible = "fsl,imx-irqsteer", },
 	{},
 };
+MODULE_DEVICE_TABLE(of, imx_irqsteer_dt_ids);
 
 static struct platform_driver imx_irqsteer_driver = {
 	.driver = {
@@ -403,4 +405,5 @@ static struct platform_driver imx_irqsteer_driver = {
 	.probe = imx_irqsteer_probe,
 	.remove = imx_irqsteer_remove,
 };
-builtin_platform_driver(imx_irqsteer_driver);
+module_platform_driver(imx_irqsteer_driver);
+MODULE_LICENSE("GPL v2");
