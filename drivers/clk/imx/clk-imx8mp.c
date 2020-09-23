f78563d9f86c698d6c0ea9d9c01df789376743b7
Jacky Bai <ping.bai@nxp.com>
2020-09-23 14:11:49 +0800

    LF-2366 clk: imx: Add m4 enable check for imx8mp
    Check if Cortex-M is enabled to make sure the root
    clocks used by M4 are on by default.
    
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- drivers/clk/imx/clk-imx8mp.c
+++ drivers/clk/imx/clk-imx8mp.c
@@ -13,6 +13,7 @@
 #include <linux/platform_device.h>
 #include <linux/slab.h>
 #include <linux/types.h>
+#include <soc/imx/soc.h>
 
 #include "clk.h"
 #include "clk-blk-ctrl.h"
@@ -701,6 +702,8 @@ static int imx8mp_clocks_probe(struct platform_device *pdev)
 	struct device *dev = &pdev->dev;
 	struct device_node *np = dev->of_node;
 	void __iomem *anatop_base, *ccm_base;
+
+	check_m4_enabled();
 
 	np = of_find_compatible_node(NULL, NULL, "fsl,imx8mp-anatop");
 	anatop_base = of_iomap(np, 0);
