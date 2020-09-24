4cd6bb63ccc568000ded3103dd08f7dec1a23c05
Thinh Nguyen <Thinh.Nguyen@synopsys.com>
2020-09-24 01:21:18 -0700

    usb: dwc3: gadget: Check MPS of the request length
    commit ca3df3468eec87f6374662f7de425bc44c3810c1 upstream.
    
    When preparing for SG, not all the entries are prepared at once. When
    resume, don't use the remaining request length to calculate for MPS
    alignment. Use the entire request->length to do that.
    
    Cc: stable@vger.kernel.org
    Fixes: 5d187c0454ef ("usb: dwc3: gadget: Don't setup more than requested")
    Signed-off-by: Thinh Nguyen <Thinh.Nguyen@synopsys.com>
    Signed-off-by: Felipe Balbi <balbi@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
     Conflicts:
    	drivers/usb/dwc3/gadget.c

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1061,6 +1061,8 @@ static void dwc3_prepare_one_trb_sg(struct dwc3_ep *dep,
 	struct scatterlist *s;
 	int		i;
 	unsigned int length = req->request.length;
+	unsigned int maxp = usb_endpoint_maxp(dep->endpoint.desc);
+	unsigned int rem = length % maxp;
 	unsigned int remaining = req->request.num_mapped_sgs
 		- req->num_queued_sgs;
 
@@ -1072,9 +1074,6 @@ static void dwc3_prepare_one_trb_sg(struct dwc3_ep *dep,
 		length -= sg_dma_len(s);
 
 	for_each_sg(sg, s, remaining, i) {
-		unsigned int length = req->request.length;
-		unsigned int maxp = usb_endpoint_maxp(dep->endpoint.desc);
-		unsigned int rem = length % maxp;
 		unsigned int trb_length;
 		unsigned chain = true;
 
