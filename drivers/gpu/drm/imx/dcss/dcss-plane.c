cbbc8582af91ad93b64e0477d3e7e434731cf388
Laurentiu Palcu <laurentiu.palcu@oss.nxp.com>
2020-12-11 08:41:59 +0000

    LF-3024: drm/imx/dcss: allow primary plane to be less than crtc size
    Currently, the primary plane has to cover the entire CRTC area. But, there are
    situations when we want the primary plane to be smaller, to save bandwidth.
    Since the HW allows it, activate it in the driver as well.
    
    Signed-off-by: Laurentiu Palcu <laurentiu.palcu@oss.nxp.com>
    Reviewed-by: Robert Chiras <robert.chiras@nxp.com>
    Acked-by: Mirela Rabulea <mirela.rabulea@nxp.com>
    [ Liu Ying: Fix conflicts to upgrade to next-20210514 ]
    Signed-off-by: Liu Ying <victor.liu@nxp.com>

--- drivers/gpu/drm/imx/dcss/dcss-plane.c
+++ drivers/gpu/drm/imx/dcss/dcss-plane.c
@@ -235,7 +235,6 @@ static int dcss_plane_atomic_check(struct drm_plane *plane,
 	struct dcss_plane *dcss_plane = to_dcss_plane(plane);
 	struct dcss_dev *dcss = plane->dev->dev_private;
 	struct drm_framebuffer *fb = new_plane_state->fb;
-	bool is_primary_plane = plane->type == DRM_PLANE_TYPE_PRIMARY;
 	struct drm_gem_cma_object *cma_obj;
 	struct drm_crtc_state *crtc_state;
 	int hdisplay, vdisplay;
@@ -265,8 +264,7 @@ static int dcss_plane_atomic_check(struct drm_plane *plane,
 				       &min, &max);
 
 	ret = drm_atomic_helper_check_plane_state(new_plane_state, crtc_state,
-						  min, max, !is_primary_plane,
-						  false);
+						  min, max, true, false);
 	if (ret)
 		return ret;
 
