d1447f3e3d303e9c99061db25a44db8f835eaa2e
Peng Fan <peng.fan@nxp.com>
2020-12-11 11:42:38 +0800

    LF-3016-1 clk: imx8mp: check hws in pll_debug_init
    For inamte linux, hws is not initialzied, so it will trigger panic
    if not checking hws.
    
    Reviewed-by: Ye Li <ye.li@nxp.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- drivers/clk/imx/clk-imx8mp.c
+++ drivers/clk/imx/clk-imx8mp.c
@@ -1116,7 +1116,7 @@ static int __init pll_debug_init(void)
 {
 	struct dentry *root, *audio_pll1, *audio_pll2;
 
-	if (of_machine_is_compatible("fsl,imx8mp")) {
+	if (of_machine_is_compatible("fsl,imx8mp") && hws) {
 		/* create a root dir for audio pll monitor */
 		root = debugfs_create_dir("audio_pll_monitor", NULL);
 		audio_pll1 = debugfs_create_dir("audio_pll1", root);
