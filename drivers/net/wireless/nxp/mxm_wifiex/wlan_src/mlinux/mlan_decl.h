691f6197f662da2bc5d1c2be5bdfdd472fd39941
Sherry Sun <sherry.sun@nxp.com>
2021-01-13 20:29:51 +0800

    MLK-25280: net: wireless: nxp: mxm_wifiex: upgrade to mxm5x16215.p2 release
    Upgrade to mxm5x16215 verison:
    - None
    
    Signed-off-by: Sherry Sun <sherry.sun@nxp.com>
    Approved-by: yang.tian <yang.tian@nxp.com>
    (cherry picked from commit 03c91af0b7784450a38ebb1211ac73811b735f30)

--- drivers/net/wireless/nxp/mxm_wifiex/wlan_src/mlinux/mlan_decl.h
+++ drivers/net/wireless/nxp/mxm_wifiex/wlan_src/mlinux/mlan_decl.h
@@ -24,7 +24,7 @@
 #define _MLAN_DECL_H_
 
 /** MLAN release version */
-#define MLAN_RELEASE_VERSION "215.p1"
+#define MLAN_RELEASE_VERSION "215.p2"
 
 /** Re-define generic data types for MLAN/MOAL */
 /** Signed char (1-byte) */
