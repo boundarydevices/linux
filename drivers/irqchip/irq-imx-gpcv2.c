b4f599a2ce01afc7e5226e7b41e6c264dde87fe9
Troy Kisky <troy.kisky@boundarydevices.com>
2021-01-18 13:12:55 -0800

    irqchip: irq-imx-gpcv2: add fsl,imx8mp-gpc
    We cannot reuse "fsl,imx8mq-gpc" because we only want the irqchip driver
    and not the power domain driver.
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/irqchip/irq-imx-gpcv2.c
+++ drivers/irqchip/irq-imx-gpcv2.c
@@ -317,6 +317,7 @@ static const struct irq_domain_ops gpcv2_irqchip_data_domain_ops = {
 static const struct of_device_id gpcv2_of_match[] = {
 	{ .compatible = "fsl,imx7d-gpc",  .data = (const void *) 2 },
 	{ .compatible = "fsl,imx8mq-gpc", .data = (const void *) 4 },
+	{ .compatible = "fsl,imx8mp-gpc", .data = (const void *) 4 },
 	{ /* END */ }
 };
 
@@ -420,3 +421,4 @@ static int __init imx_gpcv2_irqchip_init(struct device_node *node,
 
 IRQCHIP_DECLARE(imx_gpcv2_imx7d, "fsl,imx7d-gpc", imx_gpcv2_irqchip_init);
 IRQCHIP_DECLARE(imx_gpcv2_imx8mq, "fsl,imx8mq-gpc", imx_gpcv2_irqchip_init);
+IRQCHIP_DECLARE(imx_gpcv2_imx8mp, "fsl,imx8mp-gpc", imx_gpcv2_irqchip_init);
