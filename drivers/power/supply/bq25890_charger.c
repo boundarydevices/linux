048b48ef1300b158f0355ccad8b72236ef03b9d6
Troy Kisky <troy.kisky@boundarydevices.com>
2021-02-04 10:46:14 -0800

    power: supply: bq25890_charger: add ti,iilim-current dts property
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/power/supply/bq25890_charger.c
+++ drivers/power/supply/bq25890_charger.c
@@ -654,6 +654,7 @@ static int bq25890_hw_init(struct bq25890_device *bq)
 		{F_BOOSTI,	 bq->init_data.boosti},
 		{F_BOOSTF,	 bq->init_data.boostf},
 		{F_EN_ILIM,	 bq->init_data.ilim_en},
+		{F_IILIM,	 bq->init_data.iilim},
 		{F_TREG,	 bq->init_data.treg},
 		{F_BATCMP,	 bq->init_data.rbatcomp},
 		{F_VCLAMP,	 bq->init_data.vclamp},
@@ -860,6 +861,7 @@ static int bq25890_fw_read_u32_props(struct bq25890_device *bq)
 		/* required properties */
 		{"ti,charge-current", false, TBL_ICHG, &init->ichg},
 		{"ti,battery-regulation-voltage", false, TBL_VREG, &init->vreg},
+		{"ti,iilim-current", false, TBL_IILIM, &init->iilim},
 		{"ti,termination-current", false, TBL_ITERM, &init->iterm},
 		{"ti,precharge-current", false, TBL_ITERM, &init->iprechg},
 		{"ti,minimum-sys-voltage", false, TBL_SYSVMIN, &init->sysvmin},
