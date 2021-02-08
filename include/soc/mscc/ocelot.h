7dd8725efb87d935ffb1192282bbd9c3024d544a
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-02-08 19:36:27 +0200

    net: dsa: felix: implement port flushing on .phylink_mac_link_down
    [ Upstream commit eb4733d7cffc547e08fe5a216e4f03663bb71108 ]
    
    There are several issues which may be seen when the link goes down while
    forwarding traffic, all of which can be attributed to the fact that the
    port flushing procedure from the reference manual was not closely
    followed.
    
    With flow control enabled on both the ingress port and the egress port,
    it may happen when a link goes down that Ethernet packets are in flight.
    In flow control mode, frames are held back and not dropped. When there
    is enough traffic in flight (example: iperf3 TCP), then the ingress port
    might enter congestion and never exit that state. This is a problem,
    because it is the egress port's link that went down, and that has caused
    the inability of the ingress port to send packets to any other port.
    This is solved by flushing the egress port's queues when it goes down.
    
    There is also a problem when performing stream splitting for
    IEEE 802.1CB traffic (not yet upstream, but a sort of multicast,
    basically). There, if one port from the destination ports mask goes
    down, splitting the stream towards the other destinations will no longer
    be performed. This can be traced down to this line:
    
    	ocelot_port_writel(ocelot_port, 0, DEV_MAC_ENA_CFG);
    
    which should have been instead, as per the reference manual:
    
    	ocelot_port_rmwl(ocelot_port, 0, DEV_MAC_ENA_CFG_RX_ENA,
    			 DEV_MAC_ENA_CFG);
    
    Basically only DEV_MAC_ENA_CFG_RX_ENA should be disabled, but not
    DEV_MAC_ENA_CFG_TX_ENA - I don't have further insight into why that is
    the case, but apparently multicasting to several ports will cause issues
    if at least one of them doesn't have DEV_MAC_ENA_CFG_TX_ENA set.
    
    I am not sure what the state of the Ocelot VSC7514 driver is, but
    probably not as bad as Felix/Seville, since VSC7514 uses phylib and has
    the following in ocelot_adjust_link:
    
    	if (!phydev->link)
    		return;
    
    therefore the port is not really put down when the link is lost, unlike
    the DSA drivers which use .phylink_mac_link_down for that.
    
    Nonetheless, I put ocelot_port_flush() in the common ocelot.c because it
    needs to access some registers from drivers/net/ethernet/mscc/ocelot_rew.h
    which are not exported in include/soc/mscc/ and a bugfix patch should
    probably not move headers around.
    
    Fixes: bdeced75b13f ("net: dsa: felix: Add PCS operations for PHYLINK")
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    
    Conflict with NXP change done manually
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- include/soc/mscc/ocelot.h
+++ include/soc/mscc/ocelot.h
@@ -708,6 +708,7 @@ struct ocelot_policer {
 /* I/O */
 u32 ocelot_port_readl(struct ocelot_port *port, u32 reg);
 void ocelot_port_writel(struct ocelot_port *port, u32 val, u32 reg);
+void ocelot_port_rmwl(struct ocelot_port *port, u32 val, u32 mask, u32 reg);
 u32 __ocelot_read_ix(struct ocelot *ocelot, u32 reg, u32 offset);
 void __ocelot_write_ix(struct ocelot *ocelot, u32 val, u32 reg, u32 offset);
 void __ocelot_rmw_ix(struct ocelot *ocelot, u32 val, u32 mask, u32 reg,
@@ -716,14 +717,6 @@ u32 __ocelot_target_read_ix(struct ocelot *ocelot, enum ocelot_target target,
 			    u32 reg, u32 offset);
 void __ocelot_target_write_ix(struct ocelot *ocelot, enum ocelot_target target,
 			      u32 val, u32 reg, u32 offset);
-
-static inline void ocelot_port_rmwl(struct ocelot_port *port, u32 val,
-				    u32 mask, u32 reg)
-{
-	u32 cur = ocelot_port_readl(port, reg);
-
-	ocelot_port_writel(port, (cur & (~mask)) | val, reg);
-};
 
 /* Hardware initialization */
 int ocelot_regfields_init(struct ocelot *ocelot,
@@ -744,6 +737,7 @@ int ocelot_get_sset_count(struct ocelot *ocelot, int port, int sset);
 int ocelot_get_ts_info(struct ocelot *ocelot, int port,
 		       struct ethtool_ts_info *info);
 void ocelot_set_ageing_time(struct ocelot *ocelot, unsigned int msecs);
+int ocelot_port_flush(struct ocelot *ocelot, int port);
 void ocelot_adjust_link(struct ocelot *ocelot, int port,
 			struct phy_device *phydev);
 int ocelot_port_vlan_filtering(struct ocelot *ocelot, int port, bool enabled,
