1071b5c9b4d37426141527e754f5aae97a2d4b78
Heiner Kallweit <hkallweit1@gmail.com>
2021-02-14 17:38:30 +0100

    r8169: fix resuming from suspend on RTL8105e if machine runs on battery
    commit d2a04370817fc7b0172dad2ef2decf907e1a304e upstream.
    
    Armin reported that after referenced commit his RTL8105e is dead when
    resuming from suspend and machine runs on battery. This patch has been
    confirmed to fix the issue.
    
    Fixes: e80bd76fbf56 ("r8169: work around power-saving bug on some chip versions")
    Reported-by: Armin Wolf <W_Armin@gmx.de>
    Tested-by: Armin Wolf <W_Armin@gmx.de>
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/realtek/r8169_main.c
+++ drivers/net/ethernet/realtek/r8169_main.c
@@ -3967,6 +3967,7 @@ static void rtl_pll_power_down(struct rtl8169_private *tp)
 
 	switch (tp->mac_version) {
 	case RTL_GIGA_MAC_VER_25 ... RTL_GIGA_MAC_VER_26:
+	case RTL_GIGA_MAC_VER_29 ... RTL_GIGA_MAC_VER_30:
 	case RTL_GIGA_MAC_VER_32 ... RTL_GIGA_MAC_VER_33:
 	case RTL_GIGA_MAC_VER_37:
 	case RTL_GIGA_MAC_VER_39:
@@ -3997,6 +3998,7 @@ static void rtl_pll_power_up(struct rtl8169_private *tp)
 {
 	switch (tp->mac_version) {
 	case RTL_GIGA_MAC_VER_25 ... RTL_GIGA_MAC_VER_26:
+	case RTL_GIGA_MAC_VER_29 ... RTL_GIGA_MAC_VER_30:
 	case RTL_GIGA_MAC_VER_32 ... RTL_GIGA_MAC_VER_33:
 	case RTL_GIGA_MAC_VER_37:
 	case RTL_GIGA_MAC_VER_39:
