6b66cc053a3188ce44041d9c344fa6582e82e059
Kurt Lee <kurt.lee@cypress.com>
2021-03-09 02:18:13 -0600

    brcmfmac: Fix connecting enterprise AP failure
    Use alternative to check AKM-DPP enabled thus this change won't let
    status un-sync on use_fwsup between driver and firmware
    
    Signed-off-by: Kurt Lee <kurt.lee@cypress.com>
    Signed-off-by: Chung-Hsien Hsu <chung-hsien.hsu@infineon.com>
    Signed-off-by: Chi-hsien Lin <chi-hsien.lin@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
@@ -2024,6 +2024,7 @@ brcmf_set_key_mgmt(struct net_device *ndev, struct cfg80211_connect_params *sme)
 			break;
 		case WLAN_AKM_SUITE_DPP:
 			val = WFA_AUTH_DPP;
+			profile->use_fwsup = BRCMF_PROFILE_FWSUP_NONE;
 			break;
 		default:
 			bphy_err(drvr, "invalid akm suite (%d)\n",
@@ -2359,8 +2360,6 @@ brcmf_cfg80211_connect(struct wiphy *wiphy, struct net_device *ndev,
 				brcmf_dbg(INFO, "using PSK offload\n");
 				profile->use_fwsup = BRCMF_PROFILE_FWSUP_PSK;
 			}
-		} else {
-			profile->use_fwsup = BRCMF_PROFILE_FWSUP_NONE;
 		}
 
 		if (profile->use_fwsup != BRCMF_PROFILE_FWSUP_NONE) {
