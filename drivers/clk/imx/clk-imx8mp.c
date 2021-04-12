ae66b39b23e50bd14626d890cced1bbc64d32c7a
Jindong Yue <jindong.yue@nxp.com>
2021-04-12 17:10:25 +0800

    MA-17597 clk: imx8mp: remove __init in imx_clk_init_on
    Fix below section mismatch build warnings:
      The function imx8mp_clocks_probe() references
      the function __init imx_clk_init_on().
      This is often because imx8mp_clocks_probe lacks a __init
      annotation or the annotation of imx_clk_init_on is wrong.
    
      FATAL: modpost: Section mismatches detected.
      Set CONFIG_SECTION_MISMATCH_WARN_ONLY=y to allow them.
    
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-by: Jacky Bai <ping.bai@nxp.com>

--- drivers/clk/imx/clk-imx8mp.c
+++ drivers/clk/imx/clk-imx8mp.c
@@ -687,7 +687,7 @@ static const char * const imx8mp_dram_core_sels[] = {"dram_pll_out", "dram_alt_r
 static struct clk_hw **hws;
 static struct clk_hw_onecell_data *clk_hw_data;
 
-static int __init imx_clk_init_on(struct device_node *np,
+static int imx_clk_init_on(struct device_node *np,
 				  struct clk_hw * const clks[])
 {
 	u32 *array;
