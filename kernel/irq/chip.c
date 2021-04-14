b57dd0d2b4c75da5f16b9cfd17a3e53efba203b0
zhang sanshan <pete.zhang@nxp.com>
2021-04-14 14:39:33 +0800

    Revert "MA-17422-1 irq: export necessary APIs to support user built as module"
    This reverts commit 3a9860ca4718b4a0dc0857636a4c05d9c70fd743.

--- kernel/irq/chip.c
+++ kernel/irq/chip.c
@@ -1618,7 +1618,6 @@ int irq_chip_pm_get(struct irq_data *data)
 
 	return 0;
 }
-EXPORT_SYMBOL_GPL(irq_chip_pm_get);
 
 /**
  * irq_chip_pm_put - Disable power for an IRQ chip
@@ -1637,4 +1636,3 @@ int irq_chip_pm_put(struct irq_data *data)
 
 	return (retval < 0) ? retval : 0;
 }
-EXPORT_SYMBOL_GPL(irq_chip_pm_put);
