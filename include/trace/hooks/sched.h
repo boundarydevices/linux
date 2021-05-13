16151a687e7e346d3991fc411f8f8dc3b9347caa
Liangliang Li <liliangliang@vivo.com>
2021-05-13 15:01:26 +0800

    ANDROID: vendor_hooks: Add hooks to dup_task_struct
    Add hook to dup_task_struct for vendor data fields initialisation.
    
    Bug: 188004638
    
    Change-Id: I4b58604ee822fb8d1e0cc37bec72e820e7318427
    Signed-off-by: Liangliang Li <liliangliang@vivo.com>
    (cherry picked from commit f66d96b14aab5051fdf6b5054d87362c17a7b365)
    (cherry picked from commit bafafe0ec46160573bef46d3d0f5d6c65fadaa3b)

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -313,6 +313,10 @@ DECLARE_RESTRICTED_HOOK(android_rvh_find_new_ilb,
 	TP_PROTO(struct cpumask *nohz_idle_cpus_mask, int *new_ilb),
 	TP_ARGS(nohz_idle_cpus_mask, new_ilb), 1);
 
+DECLARE_HOOK(android_vh_dup_task_struct,
+	TP_PROTO(struct task_struct *tsk, struct task_struct *orig),
+	TP_ARGS(tsk, orig));
+
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_SCHED_H */
