2df270b515856b85dc069ec91bb5458bf3681e47
Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>
2021-06-23 17:05:02 +0200

    rpmsg: char: Introduce __rpmsg_chrdev_create_eptdev function
    Introduce the __rpmsg_chrdev_create_eptdev internal function that returns
    the rpmsg_eptdev context structure.
    
    This patch prepares the introduction of a rpmsg channel device for the
    char device. The rpmsg device will need a reference to the context.
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>

--- drivers/rpmsg/rpmsg_char.c
+++ drivers/rpmsg/rpmsg_char.c
@@ -322,8 +322,9 @@ static void rpmsg_eptdev_release_device(struct device *dev)
 	kfree(eptdev);
 }
 
-int rpmsg_chrdev_eptdev_create(struct rpmsg_device *rpdev, struct device *parent,
-			       struct rpmsg_channel_info chinfo)
+static struct rpmsg_eptdev *__rpmsg_chrdev_eptdev_create(struct rpmsg_device *rpdev,
+							 struct device *parent,
+							 struct rpmsg_channel_info chinfo)
 {
 	struct rpmsg_eptdev *eptdev;
 	struct device *dev;
@@ -331,7 +332,7 @@ int rpmsg_chrdev_eptdev_create(struct rpmsg_device *rpdev, struct device *parent
 
 	eptdev = kzalloc(sizeof(*eptdev), GFP_KERNEL);
 	if (!eptdev)
-		return -ENOMEM;
+		return ERR_PTR(-ENOMEM);
 
 	dev = &eptdev->dev;
 	eptdev->rpdev = rpdev;
@@ -379,7 +380,19 @@ free_eptdev:
 	put_device(dev);
 	kfree(eptdev);
 
-	return ret;
+	return ERR_PTR(ret);
+}
+
+int rpmsg_chrdev_eptdev_create(struct rpmsg_device *rpdev, struct device *parent,
+			       struct rpmsg_channel_info chinfo)
+{
+	struct rpmsg_eptdev *eptdev;
+
+	eptdev = __rpmsg_chrdev_eptdev_create(rpdev, parent, chinfo);
+	if (IS_ERR(eptdev))
+		return PTR_ERR(eptdev);
+
+	return 0;
 }
 EXPORT_SYMBOL(rpmsg_chrdev_eptdev_create);
 
