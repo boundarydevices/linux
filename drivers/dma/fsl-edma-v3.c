90e70f64ad46016942671bb29090f02c05e5f9a5
Robin Gong <yibin.gong@nxp.com>
2021-07-07 01:59:11 +0800

    LF-4141: dmaengine: fsl-edma-v3: fix Coverity issue
    Fix 'Coverity Issue:16750978 Dereference null return value'
    
    Signed-off-by: Robin Gong <yibin.gong@nxp.com>
    Reviewed-by: Shengjiu Wang <shengjiu.wang@nxp.com>
    (cherry picked from commit 7b5f35a73cf0761ac189d35c0c40d86735645e03)

--- drivers/dma/fsl-edma-v3.c
+++ drivers/dma/fsl-edma-v3.c
@@ -1052,6 +1052,9 @@ static int fsl_edma3_probe(struct platform_device *pdev)
 	int len, chans;
 	int ret, i;
 
+	if (!of_id)
+		return -EINVAL;
+
 	ret = of_property_read_u32(np, "dma-channels", &chans);
 	if (ret) {
 		dev_err(&pdev->dev, "Can't get dma-channels.\n");
