8610716f16f44dacc86a36fd75d56546a62978f8
Zhipeng Wang <zhipeng.wang_1@nxp.com>
2021-07-14 16:50:38 +0800

    MA-19269 gki: Fix the issue of disagrees about version of symbol
    Revert "net: sched: remove qdisc->empty for lockless qdisc"
    
    This patch solves a network performance problem of layerscape,
    and do not affect the imx platform.
    
    Because this patch modifies the structure and causes disagrees about
    version of symbol, revert it.
    
    This reverts commit 1ff4e088bb6e6d5ade37184ceb89ea03e45c6cb4.
    
    Change-Id: I7c6761a4dd0eb67c7f58039e8c882f5061f2ddfe

--- include/net/sch_generic.h
+++ include/net/sch_generic.h
@@ -117,6 +117,8 @@ struct Qdisc {
 	spinlock_t		busylock ____cacheline_aligned_in_smp;
 	spinlock_t		seqlock;
 
+	/* for NOLOCK qdisc, true if there are no enqueued skbs */
+	bool			empty;
 	struct rcu_head		rcu;
 
 	/* private data */
@@ -163,7 +165,7 @@ static inline bool qdisc_is_percpu_stats(const struct Qdisc *q)
 static inline bool qdisc_is_empty(const struct Qdisc *qdisc)
 {
 	if (qdisc_is_percpu_stats(qdisc))
-		return nolock_qdisc_is_empty(qdisc);
+		return READ_ONCE(qdisc->empty);
 	return !READ_ONCE(qdisc->q.qlen);
 }
 
@@ -171,7 +173,7 @@ static inline bool qdisc_run_begin(struct Qdisc *qdisc)
 {
 	if (qdisc->flags & TCQ_F_NOLOCK) {
 		if (spin_trylock(&qdisc->seqlock))
-			return true;
+			goto nolock_empty;
 
 		/* Paired with smp_mb__after_atomic() to make sure
 		 * STATE_MISSED checking is synchronized with clearing
@@ -205,7 +207,12 @@ static inline bool qdisc_run_begin(struct Qdisc *qdisc)
 		/* Retry again in case other CPU may not see the new flag
 		 * after it releases the lock at the end of qdisc_run_end().
 		 */
-		return spin_trylock(&qdisc->seqlock);
+		if (!spin_trylock(&qdisc->seqlock))
+			return false;
+
+nolock_empty:
+		WRITE_ONCE(qdisc->empty, false);
+		return true;
 	} else if (qdisc_is_running(qdisc)) {
 		return false;
 	}
