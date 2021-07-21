2df5369d345df34108fc9c4c3ed5581ccd918115
Zhang Peng <peng.zhang_8@nxp.com>
2021-07-21 10:47:52 +0800

    LF-4230-1: ASoC: SOF: Support SOF on 8ULP platform
    Add SOF driver to support DSP on imx8ulp platform.
    HIFI4 DSP is on LAVD domain for audio. HIFI4 memory region is mapping to kernel side,
    as fllow, need to careful handle this.
    ----------------------------------------------------------------------
    | DDR                        |         DSP            | attr         |
    ----------------------------------------------------------------------
    | 0x90000000 - 0x9fffffff    |0x30000000 - 0x3fffffff | non-cachable |
    ----------------------------------------------------------------------
    | 0x80000000 - 0x8fffffff    |0x0c000000 - 0x13ffffff | cachable     |
    ----------------------------------------------------------------------
    
    Signed-off-by: Zhang Peng <peng.zhang_8@nxp.com>

--- sound/soc/sof/sof-of-dev.c
+++ sound/soc/sof/sof-of-dev.c
@@ -15,6 +15,7 @@
 extern struct snd_sof_dsp_ops sof_imx8_ops;
 extern struct snd_sof_dsp_ops sof_imx8x_ops;
 extern struct snd_sof_dsp_ops sof_imx8m_ops;
+extern struct snd_sof_dsp_ops sof_imx8ulp_ops;
 
 /* platform specific devices */
 #if IS_ENABLED(CONFIG_SND_SOC_SOF_IMX8)
@@ -42,6 +43,16 @@ static struct sof_dev_desc sof_of_imx8mp_desc = {
 	.default_fw_filename = "sof-imx8m.ri",
 	.nocodec_tplg_filename = "sof-imx8-nocodec.tplg",
 	.ops = &sof_imx8m_ops,
+};
+#endif
+
+#if IS_ENABLED(CONFIG_SND_SOC_SOF_IMX8ULP)
+static struct sof_dev_desc sof_of_imx8ulp_desc = {
+	.default_fw_path = "imx/sof",
+	.default_tplg_path = "imx/sof-tplg",
+	.default_fw_filename = "sof-imx8ulp.ri",
+	.nocodec_tplg_filename = "sof-imx8ulp-nocodec.tplg",
+	.ops = &sof_imx8ulp_ops,
 };
 #endif
 
@@ -155,6 +166,9 @@ static const struct of_device_id sof_of_ids[] = {
 #endif
 #if IS_ENABLED(CONFIG_SND_SOC_SOF_IMX8M)
 	{ .compatible = "fsl,imx8mp-dsp", .data = &sof_of_imx8mp_desc},
+#endif
+#if IS_ENABLED(CONFIG_SND_SOC_SOF_IMX8ULP)
+	{ .compatible = "fsl,imx8ulp-dsp", .data = &sof_of_imx8ulp_desc},
 #endif
 	{ }
 };
