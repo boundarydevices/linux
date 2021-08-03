36252b0756618ef153c3c6fddfe2bd7f7e7c971d
Haibo Chen <haibo.chen@nxp.com>
2021-08-03 11:46:40 +0800

    LF-4246 input: touchscreen: goodix: do not check FW cfg when it not support load fw
    After commit d3a9723141d3 ("LF-4184-2 input: goodix: do not update
    touch device firmware"), driver use RST and INT pad do not load fw
    in default. So in resume, also need to check whether it support
    load fw from device, otherwise resume will failed.
    
    Reviewed-by: Clark Wang <xiaoning.wang@nxp.com>
    Signed-off-by: Haibo Chen <haibo.chen@nxp.com>

--- drivers/input/touchscreen/goodix.c
+++ drivers/input/touchscreen/goodix.c
@@ -1358,9 +1358,11 @@ static int __maybe_unused goodix_resume(struct device *dev)
 			return error;
 		}
 
-		error = goodix_send_cfg(ts, ts->config, ts->chip->config_len);
-		if (error)
-			return error;
+		if (ts->load_cfg_from_disk) {
+			error = goodix_send_cfg(ts, ts->config, ts->chip->config_len);
+			if (error)
+				return error;
+		}
 	}
 
 	error = goodix_request_irq(ts);
