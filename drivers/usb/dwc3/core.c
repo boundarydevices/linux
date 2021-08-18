5fe231c8507bac5838e74ee09febf563cae89539
Gary Bisson <gary.bisson@boundarydevices.com>
2021-08-18 17:58:34 +0200

    Revert "LF-3751 usb: dwc3: core: set port cap dir to be device mode for disconnect"
    This reverts commit 9be37f25615b0f039832972c1adbff9a0838cece.

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -105,18 +105,14 @@ static int dwc3_get_dr_mode(struct dwc3 *dwc)
 
 void dwc3_set_prtcap(struct dwc3 *dwc, u32 mode)
 {
-	u32 reg, reg_mode;
+	u32 reg;
 
-	/* Set PRTCAPDIR to be device mode for disconnect */
-	if (mode != DWC3_GCTL_PRTCAP_HOST)
-		reg_mode = DWC3_GCTL_PRTCAP_DEVICE;
-	else
-		reg_mode = DWC3_GCTL_PRTCAP_HOST;
-
-	reg = dwc3_readl(dwc->regs, DWC3_GCTL);
-	reg &= ~(DWC3_GCTL_PRTCAPDIR(DWC3_GCTL_PRTCAP_OTG));
-	reg |= DWC3_GCTL_PRTCAPDIR(reg_mode);
-	dwc3_writel(dwc->regs, DWC3_GCTL, reg);
+	if (mode != DWC3_GCTL_PRTCAP_NONE) {
+		reg = dwc3_readl(dwc->regs, DWC3_GCTL);
+		reg &= ~(DWC3_GCTL_PRTCAPDIR(DWC3_GCTL_PRTCAP_OTG));
+		reg |= DWC3_GCTL_PRTCAPDIR(mode);
+		dwc3_writel(dwc->regs, DWC3_GCTL, reg);
+	}
 
 	dwc->current_dr_role = mode;
 }
