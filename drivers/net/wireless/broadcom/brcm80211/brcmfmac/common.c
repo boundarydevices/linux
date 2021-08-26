e08e4743349e0a9baafc0d8c3b1010f68d1a9a80
Ian Lin <ian.lin-ee@infineon.com>
2021-08-25 23:19:24 -0500

    Revert "non-upstream: make setting SDIO workqueue WQ_HIGHPRI a module parameter"
    This reverts commit 6e3e95205de5c5c84504efb2f5bb847fab2a0547.
    Kernel.org do not accept to add parameter.
    That fix is handle by new commit 41b637ba.
    
    Signed-off-by: Ian Lin <ian.lin-ee@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.c
@@ -74,10 +74,6 @@ static int brcmf_eap_restrict;
 module_param_named(eap_restrict, brcmf_eap_restrict, int, 0400);
 MODULE_PARM_DESC(eap_restrict, "Block non-802.1X frames until auth finished");
 
-static int brcmf_sdio_wq_highpri;
-module_param_named(sdio_wq_highpri, brcmf_sdio_wq_highpri, int, 0);
-MODULE_PARM_DESC(sdio_wq_highpri, "SDIO workqueue is set to high priority");
-
 static int brcmf_max_pm;
 module_param_named(max_pm, brcmf_max_pm, int, 0);
 MODULE_PARM_DESC(max_pm, "Use max power management mode by default");
@@ -485,7 +481,6 @@ struct brcmf_mp_device *brcmf_get_module_param(struct device *dev,
 	settings->roamoff = !!brcmf_roamoff;
 	settings->iapp = !!brcmf_iapp_enable;
 	settings->eap_restrict = !!brcmf_eap_restrict;
-	settings->sdio_wq_highpri = !!brcmf_sdio_wq_highpri;
 	settings->default_pm = !!brcmf_max_pm ? PM_MAX : PM_FAST;
 #ifdef DEBUG
 	settings->ignore_probe_fail = !!brcmf_ignore_probe_fail;
