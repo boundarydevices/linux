c99119a545b9af8e59f27d3c61737600efc063e7
Christian König <christian.koenig@amd.com>
2021-08-30 09:22:06 +0200

    UPSTREAM: drm/ttm: add back a reference to the bdev to the res manager
    It is simply a lot cleaner to have this around instead of adding
    the device throughout the call chain.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Huang Rui <ray.huang@amd.com>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220124122514.1832-3-christian.koenig@amd.com
    (cherry picked from commit 3f268ef06f8cf3c481dbd5843d564f5170c6df54)
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- include/drm/ttm/ttm_resource.h
+++ include/drm/ttm/ttm_resource.h
@@ -108,11 +108,11 @@ struct ttm_resource_manager_func {
  * @gpu_offset: If used, the GPU offset of the first managed page of
  * fixed memory or the first managed location in an aperture.
  * @size: Size of the managed region.
+ * @bdev: ttm device this manager belongs to
  * @func: structure pointer implementing the range manager. See above
  * @move_lock: lock for move fence
- * static information. bdev::driver::io_mem_free is never used.
- * @lru: The lru list for this memory type.
  * @move: The fence of the last pipelined move operation.
+ * @lru: The lru list for this memory type.
  *
  * This structure is used to identify and manage memory types for a device.
  */
@@ -122,20 +122,21 @@ struct ttm_resource_manager {
 	 */
 	bool use_type;
 	bool use_tt;
+	struct ttm_device *bdev;
 	uint64_t size;
 	const struct ttm_resource_manager_func *func;
 	spinlock_t move_lock;
+
+	/*
+	 * Protected by @move_lock.
+	 */
+	struct dma_fence *move;
 
 	/*
 	 * Protected by the global->lru_lock.
 	 */
 
 	struct list_head lru[TTM_MAX_BO_PRIORITY];
-
-	/*
-	 * Protected by @move_lock.
-	 */
-	struct dma_fence *move;
 };
 
 /**
@@ -274,6 +275,7 @@ bool ttm_resource_compat(struct ttm_resource *res,
 			 struct ttm_placement *placement);
 
 void ttm_resource_manager_init(struct ttm_resource_manager *man,
+			       struct ttm_device *bdev,
 			       unsigned long p_size);
 
 int ttm_resource_manager_evict_all(struct ttm_device *bdev,
