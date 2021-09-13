c78b6bdb15e0530c937165e1a3e2c2989fc24a21
yan <yan-wyb@foxmail.com>
2021-09-13 14:11:06 +0800

    ETH: setup mac address from command line
    Signed-off-by: yan <yan-wyb@foxmail.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
@@ -21,6 +21,40 @@
 
 #ifdef CONFIG_OF
 
+#if defined (CONFIG_DWMAC_MESON) || defined (CONFIG_DWMAC_ROCKCHIP)
+static u8 DEFMAC[] = {0, 0, 0, 0, 0, 0};
+static unsigned int g_mac_addr_setup;
+static unsigned char chartonum(char c)
+{
+   if (c >= '0' && c <= '9')
+       return c - '0';
+   if (c >= 'A' && c <= 'F')
+       return (c - 'A') + 10;
+   if (c >= 'a' && c <= 'f')
+       return (c - 'a') + 10;
+
+   return 0;
+}
+
+static int __init mac_addr_set(char *line)
+{
+   unsigned char mac[6];
+   int i = 0;
+
+   for (i = 0; i < 6 && line[0] != '\0' && line[1] != '\0'; i++) {
+       mac[i] = chartonum(line[0]) << 4 | chartonum(line[1]);
+       line += 3;
+   }
+   memcpy(DEFMAC, mac, 6);
+   pr_info("uboot setup mac-addr: %02x:%02x:%02x:%02x:%02x:%02x\n",
+   DEFMAC[0], DEFMAC[1], DEFMAC[2], DEFMAC[3], DEFMAC[4], DEFMAC[5]);
+   g_mac_addr_setup++;
+   return 0;
+}
+__setup("mac=", mac_addr_set);
+#endif
+
+
 /**
  * dwmac1000_validate_mcast_bins - validates the number of Multicast filter bins
  * @dev: struct device of the platform device
@@ -405,6 +439,17 @@ stmmac_probe_config_dt(struct platform_device *pdev, u8 *mac)
 	if (!plat)
 		return ERR_PTR(-ENOMEM);
 
+#if defined (CONFIG_DWMAC_MESON) || defined (CONFIG_DWMAC_ROCKCHIP)
+   if (g_mac_addr_setup)   /*so uboot mac= is first priority.*/
+       mac = (unsigned char *)DEFMAC;
+   else
+	   rc = of_get_mac_address(np, mac);
+       if (rc) {
+           if (rc == -EPROBE_DEFER)
+               return ERR_PTR(rc);
+           eth_zero_addr(mac);
+       }   
+#else
 	rc = of_get_mac_address(np, mac);
 	if (rc) {
 		if (rc == -EPROBE_DEFER)
@@ -412,6 +457,8 @@ stmmac_probe_config_dt(struct platform_device *pdev, u8 *mac)
 
 		eth_zero_addr(mac);
 	}
+
+#endif
 
 	phy_mode = device_get_phy_mode(&pdev->dev);
 	if (phy_mode < 0)
