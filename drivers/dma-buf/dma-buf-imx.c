edab6ee5f96e99c5aada4ad87f7ff0a2cc722e3e
zhang sanshan <pete.zhang@nxp.com>
2021-09-27 10:52:32 +0800

    MA-19505 device memory leakage in allocator process
    there are two leakages
    first leakage: it do ot release device when dma_buf_attach failed.
    second leakage: it still have another lekage after add device_del, because
    it have below call chain: device_add -> device_private_init -> kzalloc(sizeof(*dev->p), GFP_KERNEL);
    the private data is not free even call device_del.
    the private data is free in device_release. device_release is called when call put_device and the reference count is 0.
    
    But why need add dma_buf_ioc_phy_release? please reference implement of device_release, it will
    have many warning log printed if have no release callback.
    
    leakage 1:
    unreferenced object 0xffffff80d39b6580 (size 128):
      comm "allocator@4.0-s", pid 457, jiffies 4294896528 (age 793.972s)
      hex dump (first 32 bytes):
        64 6d 61 5f 70 68 79 00 00 00 00 00 00 00 00 00  dma_phy.........
        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
      backtrace:
        [<000000002cd4059f>] __kmalloc_track_caller+0x2d0/0x4fc
        [<00000000104f292d>] kstrdup_const+0x90/0xcc
        [<000000000ac65f13>] kvasprintf_const+0xc0/0xf4
        [<00000000bba667f4>] kobject_set_name_vargs+0x5c/0xec
        [<000000006f897da3>] dev_set_name+0x58/0x84
        [<0000000071fc9fa3>] ion_ioctl+0xf8/0x1b0 [dma_buf_imx]
        [<00000000c68c1a93>] __arm64_sys_ioctl+0xa8/0x110
        [<000000007724b0fd>] el0_svc_common.llvm.8308473301766836847+0xbc/0x1bc
        [<00000000791c61e7>] do_el0_svc+0x28/0x94
        [<000000000da0740c>] el0_svc+0x14/0x24
        [<00000000bc882d06>] el0_sync_handler+0x6c/0xb4
        [<00000000642d7e1a>] el0_sync+0x1c0/0x200
    
    leakage 2:
    unreferenced object 0xffff000018a6b500 (size 256):
      comm "composer@2.4-se", pid 347, jiffies 4294905403 (age 655.560s)
      hex dump (first 32 bytes):
        00 00 00 00 00 00 00 00 08 b5 a6 18 00 00 ff ff  ................
        08 b5 a6 18 00 00 ff ff 64 dd 9e 10 00 80 ff ff  ........d.......
      backtrace:
        [<00000000aca21ba5>] kmem_cache_alloc_trace+0x220/0x43c
        [<000000009de144fc>] device_add+0xd0/0x944
        [<00000000d72c7974>] dma_buf_ioctl+0x108/0x450
        [<00000000c925a9d1>] __arm64_sys_ioctl+0x94/0xd0
        [<00000000b63afad0>] el0_svc_common+0xa4/0x180
        [<00000000878774a8>] do_el0_svc+0x28/0x7c
        [<0000000032461d6c>] el0_svc+0x14/0x24
        [<00000000c784be6c>] el0_sync_handler+0x6c/0xb4
        [<0000000036c88b8b>] el0_sync+0x1c0/0x200
    
    Change-Id: Iefd9c3e09dfb59fc607d80502a59607676a78725
    Signed-off-by: zhang sanshan <pete.zhang@nxp.com>

--- drivers/dma-buf/dma-buf-imx.c
+++ drivers/dma-buf/dma-buf-imx.c
@@ -21,6 +21,11 @@ static struct class *dmabuf_class;
 static struct cdev cdev;
 static DEFINE_MUTEX(dev_lock);
 
+static void dma_buf_ioc_phy_release(struct device *dev)
+{
+	return;
+}
+
 long ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
 	switch (cmd) {
@@ -32,6 +37,7 @@ long ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		struct dma_buf_attachment *attachment = NULL;
 		struct sg_table *sgt = NULL;
 		struct dmabuf_imx_phys_data data;
+		int ret;
 
 		// copy dmabuf_imx_phys_data from user space to kernel space
 		if (copy_from_user(&data, (void __user *)arg,
@@ -50,10 +56,18 @@ long ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		dev.coherent_dma_mask = DMA_BIT_MASK(64);
 		dev.dma_mask = &dev.coherent_dma_mask;
 		dev.parent = NULL;
+		dev.release = dma_buf_ioc_phy_release;
 		dev_set_name(&dev, "dma_phy");
-		device_add(&dev);
+		ret = device_add(&dev);
+		if (ret < 0) {
+			put_device(&dev);
+			return ret;
+		}
 		attachment = dma_buf_attach(dmabuf, &dev);
 		if (!attachment || IS_ERR(attachment)) {
+			device_del(&dev);
+			put_device(&dev);
+			dma_buf_put(dmabuf);
 			mutex_unlock(&dev_lock);
 			return -EFAULT;
 		}
@@ -71,6 +85,7 @@ long ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		dma_buf_put(dmabuf);
 
 		device_del(&dev);
+		put_device(&dev);
 		mutex_unlock(&dev_lock);
 		// put the phys address to user space through dmabuf_imx_phys_data
 		if (copy_to_user((void __user *)arg, &data,
