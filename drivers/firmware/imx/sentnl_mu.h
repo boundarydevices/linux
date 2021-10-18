61c49b7c2ff696f37ca9a0500bf31b1ab776851a
Pankaj Gupta <pankaj.gupta@nxp.com>
2021-10-19 03:22:43 +0530

    LF-4778 sentnl-mu: rename the structure
    Rename the string:
    - s4_ to sentnl_
    - s400 to sentnl
    - muap to mu
    - s400_api to priv
    - S400 to SENTNL
    eg.
    - s4_mu_device_ctx to sentnl_mu_device_ctx
    - s4_out_buffer_desc/sentnl_obuf_desc
    - s400_api_msg/sentnl_api_msg/
    - s4_shared_mem/sentnl_shared_mem
    - s4_muap_id/sentnl_mu_id
    - s400_api_export/sentnl_priv_export
    
    Signed-off-by: Pankaj Gupta <pankaj.gupta@nxp.com>
    Acked-by: Alice Guo <alice.guo@nxp.com>

--- drivers/firmware/imx/sentnl_mu.h
+++ drivers/firmware/imx/sentnl_mu.h
@@ -6,6 +6,9 @@
 #ifndef SENTNL_MU_H
 #define SENTNL_MU_H
 
+#include <linux/miscdevice.h>
+#include <linux/semaphore.h>
+
 /* macro to log operation of a misc device */
 #define miscdev_dbg(p_miscdev, fmt, va_args...)                                \
 	({                                                                     \
@@ -48,7 +51,7 @@
 #define DEFAULT_MESSAGING_TAG_COMMAND           (0x17u)
 #define DEFAULT_MESSAGING_TAG_RESPONSE          (0xe1u)
 
-struct s4_out_buffer_desc {
+struct sentnl_obuf_desc {
 	u8 *out_ptr;
 	u8 *out_usr_ptr;
 	u32 out_size;
@@ -61,7 +64,7 @@ enum mu_device_status_t {
 	MU_OPENED
 };
 
-struct s4_shared_mem {
+struct sentnl_shared_mem {
 	dma_addr_t dma_addr;
 	u32 size;
 	u32 pos;
@@ -69,9 +72,9 @@ struct s4_shared_mem {
 };
 
 /* Private struct for each char device instance. */
-struct s4_mu_device_ctx {
+struct sentnl_mu_device_ctx {
 	struct device *dev;
-	struct imx_s400_api *s400_muap_priv;
+	struct sentnl_mu_priv *priv;
 	struct miscdevice miscdev;
 
 	enum mu_device_status_t status;
@@ -81,13 +84,13 @@ struct s4_mu_device_ctx {
 	u32 pending_hdr;
 	struct list_head pending_out;
 
-	struct s4_shared_mem secure_mem;
-	struct s4_shared_mem non_secure_mem;
+	struct sentnl_shared_mem secure_mem;
+	struct sentnl_shared_mem non_secure_mem;
 
 	u32 temp_cmd[MAX_MESSAGE_SIZE];
 	u32 temp_resp[MAX_RECV_SIZE];
 	u32 temp_resp_size;
-	struct notifier_block s4_notify;
+	struct notifier_block sentnl_notify;
 };
 
 struct container_hdr {
@@ -114,16 +117,10 @@ struct image_info {
 	u8 iv[IV_MAX_LEN];
 } __packed;
 
-struct s4_muap_auth_image {
+struct sentnl_auth_image {
 	struct container_hdr chdr;
 	struct image_info img_info[MAX_IMG_COUNT];
 	u32 resp;
-};
-
-struct s4_read_info {
-	u32 cmd_id;
-	u32 size;
-	u32 resp[8];
 };
 
 /* Header of the messages exchange with the SENTINEL */
@@ -134,14 +131,14 @@ struct mu_hdr {
 	u8 tag;
 }  __packed;
 
-struct s400_api_msg {
+struct sentnl_api_msg {
 	u32 header; /* u8 Tag; u8 Command; u8 Size; u8 Ver; */
-	u32 data[S400_MSG_DATA_NUM];
+	u32 data[SENTNL_MSG_DATA_NUM];
 };
 
-struct imx_s400_api {
-	struct s4_mu_device_ctx *cmd_receiver_dev;
-	struct s4_mu_device_ctx *waiting_rsp_dev;
+struct sentnl_mu_priv {
+	struct sentnl_mu_device_ctx *cmd_receiver_dev;
+	struct sentnl_mu_device_ctx *waiting_rsp_dev;
 	/*
 	 * prevent parallel access to the MU registers
 	 * e.g. a user trying to send a command while the other one is
@@ -154,16 +151,16 @@ struct imx_s400_api {
 	 */
 	struct mutex mu_cmd_lock;
 	struct device *dev;
-	u32 s4_muap_id;
+	u32 sentnl_mu_id;
 	u8 cmd_tag;
 	u8 rsp_tag;
 
 	struct mbox_client tx_cl, rx_cl;
 	struct mbox_chan *tx_chan, *rx_chan;
-	struct s400_api_msg tx_msg, rx_msg;
+	struct sentnl_api_msg tx_msg, rx_msg;
 	struct completion done;
 	spinlock_t lock;
 };
 
-int get_imx_s400_api(struct imx_s400_api **export);
+int get_sentnl_mu_priv(struct sentnl_mu_priv **export);
 #endif
