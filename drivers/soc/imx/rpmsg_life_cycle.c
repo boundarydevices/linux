boundarydevices/linux

commit d32b5fb023d3b5c6aaca2ce6fad1cd39d98db9e1

LF-4707 soc: imx: Add rpmsg life cycle support on imx8ulp
Add rpmsg life cycle support on i.MX8ULP for APD side low power mode
state coordination with M core by sending rpmsg to M4 core. Currently,
only need to send nofity to M core for reboot/poweroff case. APD side
need to send rpmsg to M core side to let it know that APD side will
enter 'DPD' HW mode or do reboot. For both cases, M core side need to
re-init the rpmsg service when APD out of reset.

Signed-off-by: Jacky Bai <ping.bai@nxp.com>
Reviewed-by: Robin Gong <yibin.gong@nxp.com>

drivers/soc/imx/Kconfig
@@ -42,4 +42,14 @@ config IMX8M_PM_DOMAINS
 	depends on PM
 	select PM_GENERIC_DOMAINS
 
+config RPMSG_LIFE_CYCLE
+	tristate "i.MX8ULP Rpmsg Life Cycle Support"
+	depends on ARCH_MXC || COMPILE_TEST
+	depends on RPMSG
+	default ARCH_MXC
+	help
+	  If you say yes here you get supoort for the rpmsg life cycle support on
+	  i.MX8ULP for low power mode state coordination between A core & M core to
+	  make sure A core can be put into Low power mode without risk by sending
+	  notify to M core.
 endmenu

drivers/soc/imx/rpmsg_life_cycle.c
@@ -1,0 +1,116 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2021 NXP
+ */
+
+#include <linux/cpu.h>
+#include <linux/imx_rpmsg.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/reboot.h>
+#include <linux/rpmsg.h>
+
+#define PM_RPMSG_TYPE		0
+
+struct pm_rpmsg_data {
+	struct imx_rpmsg_head header;
+	u8 data;
+	u8 reserved;
+} __packed;
+
+enum pm_rpmsg_cmd {
+	PM_RPMSG_MODE,
+};
+
+enum pm_rpmsg_power_mode {
+	PM_RPMSG_SHUTDOWN = 7,
+};
+
+static struct rpmsg_device *life_cycle_rpdev;
+
+static int rpmsg_life_cycle_notifier(struct notifier_block *nb,
+		unsigned long action, void *unused)
+{
+	int ret, cpu;
+	struct pm_rpmsg_data msg;
+
+	/* return early if it is RESTART case */
+	if (action == SYS_RESTART)
+		return NOTIFY_DONE;
+
+	/*
+	 * unplug the non-boot cpu to make sure A35 cluster can be
+	 * put into DPD mode without risk.
+	 */
+
+	for_each_online_cpu(cpu) {
+		if (cpu == cpumask_first(cpu_online_mask))
+			continue;
+		ret = remove_cpu(cpu);
+		if (ret) {
+			pr_info("unplug the non-boot cpu failed:%d\n", ret);
+			return NOTIFY_BAD;
+		}
+	}
+
+	msg.header.cate = IMX_RMPSG_LIFECYCLE;
+	msg.header.major = IMX_RMPSG_MAJOR;
+	msg.header.minor = IMX_RMPSG_MINOR;
+	msg.header.type = PM_RPMSG_TYPE;
+	msg.header.cmd = PM_RPMSG_MODE;
+	msg.data = PM_RPMSG_SHUTDOWN;
+
+	/* No ACK from M core */
+	ret = rpmsg_send(life_cycle_rpdev->ept, &msg, sizeof(struct pm_rpmsg_data));
+
+	if (ret) {
+		pr_info("rpmsg send failed:%d\n", ret);
+		return NOTIFY_BAD;
+	}
+
+	return NOTIFY_DONE;
+};
+
+static struct notifier_block rpmsg_life_cycle_nb = {
+	.notifier_call = rpmsg_life_cycle_notifier,
+};
+
+static int rpmsg_life_cycle_cb(struct rpmsg_device *rpdev, void *data, int len,
+				void *priv, u32 src)
+{
+	return 0;
+}
+
+static int rpmsg_life_cycle_probe(struct rpmsg_device *rpdev)
+{
+
+	life_cycle_rpdev = rpdev;
+
+	dev_info(&rpdev->dev, "new channel: 0x%x -> 0x%x!\n",
+			rpdev->src, rpdev->dst);
+
+	return register_reboot_notifier(&rpmsg_life_cycle_nb);
+}
+
+static struct rpmsg_device_id rpmsg_life_cycle_id_table[] = {
+	{ .name = "rpmsg-life-cycle-channel" },
+	{ },
+};
+
+static struct rpmsg_driver rpmsg_life_cycle_driver = {
+	.drv.name = 	"rpmsg_life_cycle",
+	.drv.owner	= THIS_MODULE,
+	.id_table	= rpmsg_life_cycle_id_table,
+	.probe		= rpmsg_life_cycle_probe,
+	.callback	= rpmsg_life_cycle_cb,
+};
+
+static int __init rpmsg_life_cycle_init(void)
+{
+	return register_rpmsg_driver(&rpmsg_life_cycle_driver);
+};
+module_init(rpmsg_life_cycle_init);
+
+MODULE_AUTHOR("NXP Semiconductor");
+MODULE_DESCRIPTION("NXP rpmsg life cycle driver");
+MODULE_LICENSE("GPL v2");

drivers/soc/imx/Makefile
@@ -9,3 +9,4 @@ obj-$(CONFIG_IMX8M_BUSFREQ) += busfreq-imx8mq.o
 obj-y	+= mu/
 obj-${CONFIG_SECVIO_SC} += secvio/
 obj-$(CONFIG_IMX8M_PM_DOMAINS) += imx8m_pm_domains.o
+obj-$(CONFIG_RPMSG_LIFE_CYCLE) += rpmsg_life_cycle.o

commit 9382a451edaddfe93a3c16a214efe279858ec989

LF-4784 soc: imx: Fix build break for 32bit nosmp config
Fix below build break when using 32bit ARM & NOSMP config

  CC      drivers/soc/imx/rpmsg_life_cycle.o
drivers/soc/imx/rpmsg_life_cycle.c: In function 'rpmsg_life_cycle_notifier':
drivers/soc/imx/rpmsg_life_cycle.c:49:9: error: implicit declaration of
function 'remove_cpu' [-Werror=implicit-function-declaration]
   49 |   ret = remove_cpu(cpu);
      |         ^~~~~~~~~~
cc1: some warnings being treated as errors

Fixes: d32b5fb023d3 ("LF-4707 soc: imx: Add rpmsg life cycle support on imx8ulp")

Signed-off-by: Jacky Bai <ping.bai@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

drivers/soc/imx/Kconfig
@@ -46,7 +46,7 @@ config RPMSG_LIFE_CYCLE
 	tristate "i.MX8ULP Rpmsg Life Cycle Support"
 	depends on ARCH_MXC || COMPILE_TEST
 	depends on RPMSG
-	default ARCH_MXC
+	default ARCH_MXC && ARM64
 	help
 	  If you say yes here you get supoort for the rpmsg life cycle support on
 	  i.MX8ULP for low power mode state coordination between A core & M core to

drivers/soc/imx/rpmsg_life_cycle.c
@@ -31,7 +31,10 @@ static struct rpmsg_device *life_cycle_rpdev;
 static int rpmsg_life_cycle_notifier(struct notifier_block *nb,
 		unsigned long action, void *unused)
 {
-	int ret, cpu;
+	int ret;
+#ifdef CONFIG_HOTPLUG_CPU
+	int cpu;
+#endif
 	struct pm_rpmsg_data msg;
 
 	/* return early if it is RESTART case */
@@ -43,6 +46,7 @@ static int rpmsg_life_cycle_notifier(struct notifier_block *nb,
 	 * put into DPD mode without risk.
 	 */
 
+#ifdef CONFIG_HOTPLUG_CPU
 	for_each_online_cpu(cpu) {
 		if (cpu == cpumask_first(cpu_online_mask))
 			continue;
@@ -52,7 +56,7 @@ static int rpmsg_life_cycle_notifier(struct notifier_block *nb,
 			return NOTIFY_BAD;
 		}
 	}
-
+#endif
 	msg.header.cate = IMX_RMPSG_LIFECYCLE;
 	msg.header.major = IMX_RMPSG_MAJOR;
 	msg.header.minor = IMX_RMPSG_MINOR;