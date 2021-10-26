3e0c19506c8f2a5544edf996d19275c78759ab6f
Fang Hui <hui.fang@nxp.com>
2021-10-27 01:05:44 +0800

    MA-19458-2 Revert "MA-19299-2 android.media.cts.NativeImageReaderTest#testTakePictures failed on 8qm"
    With MA-19458-1 patch previous delay optimization patch is not needed anymore.
    This reverts commit 9c551cdb80bc11cf8612036500a60b9660b05c80.
    
    Change-Id: I9208cf97fb7e0bbfe030f021fcf29560a808e681
    Signed-off-by: Fang Hui <hui.fang@nxp.com>

--- drivers/staging/media/imx/imx8-isi-hw.c
+++ drivers/staging/media/imx/imx8-isi-hw.c
@@ -709,12 +709,7 @@ void mxc_isi_channel_enable(struct mxc_isi_dev *mxc_isi, bool m2m_enabled)
 	}
 
 	dump_isi_regs(mxc_isi);
-
-	int delay_ms = 300;
-	if (mxc_isi->isi_cap && (mxc_isi->isi_cap->dst_f.width <= 640) && (mxc_isi->isi_cap->dst_f.height <= 480))
-		delay_ms = 100;
-
-	msleep(delay_ms);
+	msleep(300);
 }
 EXPORT_SYMBOL_GPL(mxc_isi_channel_enable);
 
