efe3b70fda81ab9659a7a45893416ce0693065cc
Pankaj Gupta <pankaj.gupta@nxp.com>
2021-11-17 18:04:07 +0530

    Revert "MLK25252: firmware: imx: register device users"
    This reverts commit 2f15a3502066bd6a634ac919495d9c7f4eeafbb5.

--- drivers/firmware/imx/s400-api.c
+++ drivers/firmware/imx/s400-api.c
@@ -11,7 +11,6 @@
 #include <linux/firmware/imx/s400-api.h>
 #include <linux/init.h>
 #include <linux/mailbox_client.h>
-#include <linux/miscdevice.h>
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
 #include <linux/of_platform.h>
@@ -19,46 +18,11 @@
 #include <linux/slab.h>
 #include <linux/sys_soc.h>
 
-/* macro to log operation of a misc device */
-#define miscdev_dbg(p_miscdev, fmt, va_args...)                                \
-	({                                                                     \
-		struct miscdevice *_p_miscdev = p_miscdev;                     \
-		dev_dbg((_p_miscdev)->parent, "%s: " fmt, (_p_miscdev)->name,  \
-		##va_args);                                                    \
-	})
-
-#define miscdev_info(p_miscdev, fmt, va_args...)                               \
-	({                                                                     \
-		struct miscdevice *_p_miscdev = p_miscdev;                     \
-		dev_info((_p_miscdev)->parent, "%s: " fmt, (_p_miscdev)->name, \
-		##va_args);                                                    \
-	})
-
-#define miscdev_err(p_miscdev, fmt, va_args...)                                \
-	({                                                                     \
-		struct miscdevice *_p_miscdev = p_miscdev;                     \
-		dev_err((_p_miscdev)->parent, "%s: " fmt, (_p_miscdev)->name,  \
-		##va_args);                                                    \
-	})
-/* macro to log operation of a device context */
-#define devctx_dbg(p_devctx, fmt, va_args...) \
-	miscdev_dbg(&((p_devctx)->miscdev), fmt, ##va_args)
-#define devctx_info(p_devctx, fmt, va_args...) \
-	miscdev_info(&((p_devctx)->miscdev), fmt, ##va_args)
-#define devctx_err(p_devctx, fmt, va_args...) \
-	miscdev_err((&(p_devctx)->miscdev), fmt, ##va_args)
-
-#define MSG_TAG(x)			(((x) & 0xff000000) >> 24)
-#define MSG_COMMAND(x)			(((x) & 0x00ff0000) >> 16)
-#define MSG_SIZE(x)			(((x) & 0x0000ff00) >> 8)
-#define MSG_VER(x)			((x) & 0x000000ff)
-#define RES_STATUS(x)			((x) & 0x000000ff)
-#define MAX_DATA_SIZE_PER_USER		(65 * 1024)
-#define S4_DEFAULT_MUAP_INDEX		(0)
-#define S4_MUAP_DEFAULT_MAX_USERS	(4)
-
-#define DEFAULT_MESSAGING_TAG_COMMAND           (0x17u)
-#define DEFAULT_MESSAGING_TAG_RESPONSE          (0xe1u)
+#define MSG_TAG(x)		(((x) & 0xff000000) >> 24)
+#define MSG_COMMAND(x)		(((x) & 0x00ff0000) >> 16)
+#define MSG_SIZE(x)		(((x) & 0x0000ff00) >> 8)
+#define MSG_VER(x)		((x) & 0x000000ff)
+#define RES_STATUS(x)		((x) & 0x000000ff)
 
 struct imx_s400_api *s400_api_export;
 
@@ -235,190 +199,52 @@ struct device *imx_soc_device_register(void)
 	return soc_device_to_device(dev);
 }
 
-/* Char driver setup */
-static const struct file_operations s4_muap_fops = {
-	.open		= NULL,
-	.owner		= THIS_MODULE,
-	.release	= NULL,
-	.unlocked_ioctl = NULL,
-};
-
-/* interface for managed res to free a mailbox channel */
-static void if_mbox_free_channel(void *mbox_chan)
-{
-	mbox_free_channel(mbox_chan);
-}
-
-/* interface for managed res to unregister a char device */
-static void if_misc_deregister(void *miscdevice)
-{
-	misc_deregister(miscdevice);
-}
-
-static int s4_mu_request_channel(struct device *dev,
-				 struct mbox_chan **chan,
-				 struct mbox_client *cl,
-				 const char *name)
-{
-	struct mbox_chan *t_chan;
-	int ret = 0;
-
-	t_chan = mbox_request_channel_byname(cl, name);
-	if (IS_ERR(t_chan)) {
-		ret = PTR_ERR(t_chan);
-		if (ret != -EPROBE_DEFER)
-			dev_err(dev,
-				"Failed to request chan %s ret %d\n", name,
-				ret);
-		goto exit;
-	}
-
-	ret = devm_add_action(dev, if_mbox_free_channel, t_chan);
-	if (ret) {
-		dev_err(dev, "failed to add devm removal of mbox %s\n", name);
-		goto exit;
-	}
-
-	*chan = t_chan;
-
-exit:
-	return ret;
-}
-
 static int s400_api_probe(struct platform_device *pdev)
 {
-	struct s4_mu_device_ctx *dev_ctx;
 	struct device *dev = &pdev->dev;
-	struct imx_s400_api *priv;
-	struct device_node *np;
-	int max_nb_users = 0;
-	char *devname;
+	struct of_phandle_args mboxargs;
+	struct imx_s400_api *s400_api;
+	struct mbox_client cl;
 	struct device *soc;
-	int ret;
-	int i;
+	int err;
 
-	priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
-	if (!priv) {
-		ret = -ENOMEM;
-		dev_err(dev, "Fail allocate mem for private data\n");
-		goto exit;
-	}
-	priv->dev = dev;
-	dev_set_drvdata(dev, priv);
+	if (of_parse_phandle_with_args(dev->of_node, "mboxes", "#mbox-cells",
+				       0, &mboxargs))
+		return -ENODEV;
 
-	/*
-	 * Get the address of MU to be used for communication with the SCU
-	 */
-	np = pdev->dev.of_node;
-	if (!np) {
-		dev_err(dev, "Cannot find MU User entry in device tree\n");
-		ret = -ENOTSUPP;
-		goto exit;
-	}
+	if (!of_device_is_compatible(mboxargs.np, "fsl,imx8ulp-mu-s4"))
+		return -ENODEV;
 
-	/* Initialize the mutex. */
-	mutex_init(&priv->mu_cmd_lock);
-	mutex_init(&priv->mu_lock);
+	s400_api = devm_kzalloc(dev, sizeof(*s400_api), GFP_KERNEL);
+	if (!s400_api)
+		return -ENOMEM;
 
-	/* TBD */
-	priv->cmd_receiver_dev = NULL;
-	priv->waiting_rsp_dev = NULL;
+	cl.dev		= dev;
+	cl.tx_block	= false;
+	cl.knows_txdone	= true;
+	cl.rx_callback	= s400_api_receive_message;
 
-	ret = of_property_read_u32(np, "fsl,s4_muap_id", &priv->s4_muap_id);
-	if (ret) {
-		dev_warn(dev, "%s: Not able to read mu_id", __func__);
-		priv->s4_muap_id = S4_DEFAULT_MUAP_INDEX;
+	s400_api->tx_cl = cl;
+	s400_api->rx_cl = cl;
+
+	s400_api->tx_chan = mbox_request_channel_byname(&s400_api->tx_cl, "tx");
+	if (IS_ERR(s400_api->tx_chan)) {
+		err = PTR_ERR(s400_api->tx_chan);
+		dev_err(dev, "failed to request tx chan: %d\n", err);
+		return err;
 	}
 
-	ret = of_property_read_u32(np, "fsl,fsl,s4muap_max_users", &max_nb_users);
-	if (ret) {
-		dev_warn(dev, "%s: Not able to read mu_max_user", __func__);
-		max_nb_users = S4_MUAP_DEFAULT_MAX_USERS;
+	s400_api->rx_chan = mbox_request_channel_byname(&s400_api->rx_cl, "rx");
+	if (IS_ERR(s400_api->rx_chan)) {
+		err = PTR_ERR(s400_api->rx_chan);
+		dev_err(dev, "failed to request rx chan: %d\n", err);
+		goto free_tx;
 	}
 
-	ret = of_property_read_u8(np, "fsl,cmd_tag", &priv->cmd_tag);
-	if (ret)
-		priv->cmd_tag = DEFAULT_MESSAGING_TAG_COMMAND;
+	init_completion(&s400_api->done);
+	spin_lock_init(&s400_api->lock);
 
-	ret = of_property_read_u8(np, "fsl,rsp_tag", &priv->rsp_tag);
-	if (ret)
-		priv->rsp_tag = DEFAULT_MESSAGING_TAG_RESPONSE;
-
-	/* Mailbox client configuration */
-	priv->tx_cl.dev			= dev;
-	priv->tx_cl.tx_block		= false;
-	priv->tx_cl.knows_txdone	= true;
-	priv->tx_cl.rx_callback		= s400_api_receive_message;
-
-	priv->rx_cl.dev			= dev;
-	priv->rx_cl.tx_block		= false;
-	priv->rx_cl.knows_txdone	= true;
-	priv->rx_cl.rx_callback		= s400_api_receive_message;
-
-	ret = s4_mu_request_channel(dev, &priv->tx_chan, &priv->tx_cl, "tx");
-	if (ret) {
-		if (ret != -EPROBE_DEFER)
-			dev_err(dev, "Failed to request tx channel\n");
-
-		goto exit;
-	}
-
-	ret = s4_mu_request_channel(dev, &priv->rx_chan, &priv->rx_cl, "rx");
-	if (ret) {
-		if (ret != -EPROBE_DEFER)
-			dev_err(dev, "Failed to request rx channel\n");
-
-		goto exit;
-	}
-
-	/* Create users */
-	for (i = 0; i < max_nb_users; i++) {
-		dev_ctx = devm_kzalloc(dev, sizeof(*dev_ctx), GFP_KERNEL);
-		if (!dev_ctx) {
-			ret = -ENOMEM;
-			dev_err(dev,
-				"Fail to allocate memory for device context\n");
-			goto exit;
-		}
-
-		dev_ctx->dev = dev;
-		dev_ctx->status = MU_FREE;
-		dev_ctx->s400_muap_priv = priv;
-		/* Default value invalid for an header. */
-		init_waitqueue_head(&dev_ctx->wq);
-
-		INIT_LIST_HEAD(&dev_ctx->pending_out);
-		sema_init(&dev_ctx->fops_lock, 1);
-
-		devname = devm_kasprintf(dev, GFP_KERNEL, "s4muap%d_ch%d",
-					 priv->s4_muap_id, i);
-		if (!devname) {
-			ret = -ENOMEM;
-			dev_err(dev,
-				"Fail to allocate memory for misc dev name\n");
-			goto exit;
-		}
-
-		dev_ctx->miscdev.name = devname;
-		dev_ctx->miscdev.minor = MISC_DYNAMIC_MINOR;
-		dev_ctx->miscdev.fops = &s4_muap_fops;
-		dev_ctx->miscdev.parent = dev;
-		ret = misc_register(&dev_ctx->miscdev);
-		if (ret) {
-			dev_err(dev, "failed to register misc device %d\n",
-				ret);
-			goto exit;
-		}
-
-		ret = devm_add_action(dev, if_misc_deregister,
-				      &dev_ctx->miscdev);
-
-	}
-
-	init_completion(&priv->done);
-	spin_lock_init(&priv->lock);
-
-	s400_api_export = priv;
+	s400_api_export = s400_api;
 
 	soc = imx_soc_device_register();
 	if (IS_ERR(soc)) {
@@ -426,11 +252,13 @@ static int s400_api_probe(struct platform_device *pdev)
 		return PTR_ERR(soc);
 	}
 
-	dev_set_drvdata(dev, priv);
+	dev_set_drvdata(dev, s400_api);
 	return devm_of_platform_populate(dev);
 
-exit:
-	return ret;
+free_tx:
+	mbox_free_channel(s400_api->tx_chan);
+
+	return err;
 }
 
 static int s400_api_remove(struct platform_device *pdev)
