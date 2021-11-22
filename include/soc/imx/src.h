3b95c2fd0d81e360964f8026bfab00237668ae98
Jindong Yue <jindong.yue@nxp.com>
2021-11-22 16:17:06 +0800

    MA-19811-6 soc: imx: Implement empty imx_src_is_m4_enabled()
    imx_src_is_m4_enabled() is used widely in imx common
    clk drivers, but only mscale family implements it
    in soc-imx8m driver.
    Add a empty implementation for other SoCs to
    fix undefined build error.
    
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Change-Id: Ib7d8dedfaf7f05f7c361c6397b97b9d8e2fc91ee

--- include/soc/imx/src.h
+++ include/soc/imx/src.h
@@ -1,6 +1,10 @@
 #ifndef __SOC_IMX_SRC_H
 #define __SOC_IMX_SRC_H
 
+#if IS_ENABLED(CONFIG_SOC_IMX8M)
 bool imx_src_is_m4_enabled(void);
+#else
+static bool imx_src_is_m4_enabled(void) { return false; }
+#endif
 
 #endif /* __SOC_IMX_SRC_H */
