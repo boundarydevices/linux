b0a5d12afdcc404746e51f65f3c51478e5f481ef
Robby Cai <robby.cai@nxp.com>
2021-12-06 15:24:02 +0800

    LF-2859-1 media: mxc_capture: Fix Coverity Issue: 17678 Dereference before null check
    Fix issue for dereference before NULL check
    
    Signed-off-by: Robby Cai <robby.cai@nxp.com>
    Reviewed-by: G.N. Zhou <guoniu.zhou@nxp.com>

--- drivers/media/platform/mxc/capture/ipu_bg_overlay_sdc.c
+++ drivers/media/platform/mxc/capture/ipu_bg_overlay_sdc.c
@@ -124,7 +124,7 @@ static int csi_enc_setup(cam_data *cam)
 	ipu_channel_params_t params;
 	u32 pixel_fmt;
 	int err = 0, sensor_protocol = 0;
-	ipu_channel_t chan = (cam->csi == 0) ? CSI_MEM0 : CSI_MEM1;
+	ipu_channel_t chan;
 
 #ifdef CONFIG_MXC_MIPI_CSI2
 	void *mipi_csi2_info;
@@ -136,6 +136,8 @@ static int csi_enc_setup(cam_data *cam)
 		printk(KERN_ERR "cam private is NULL\n");
 		return -ENXIO;
 	}
+
+	chan = (cam->csi == 0) ? CSI_MEM0 : CSI_MEM1;
 
 	memset(&params, 0, sizeof(ipu_channel_params_t));
 	params.csi_mem.csi = cam->csi;
