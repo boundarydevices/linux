98d64bd1fb94cb611e6d4c3a0fc989c6850ed99b
Colin Foster <colin.foster@in-advantage.com>
2021-12-07 09:00:29 -0800

    net: dsa: ocelot: felix: add interface for custom regmaps
    Add an interface so that non-mmio regmaps can be used
    
    Signed-off-by: Colin Foster <colin.foster@in-advantage.com>
    Reviewed-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 242bd0c10bbdcea4c8bfd1f40973d145dd4ae120)
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- drivers/net/dsa/ocelot/felix.c
+++ drivers/net/dsa/ocelot/felix.c
@@ -1197,7 +1197,7 @@ static int felix_init_structs(struct felix *felix, int num_phys_ports)
 		res.start += felix->switch_base;
 		res.end += felix->switch_base;
 
-		target = ocelot_regmap_init(ocelot, &res);
+		target = felix->info->init_regmap(ocelot, &res);
 		if (IS_ERR(target)) {
 			dev_err(ocelot->dev,
 				"Failed to map device memory space\n");
@@ -1234,7 +1234,7 @@ static int felix_init_structs(struct felix *felix, int num_phys_ports)
 		res.start += felix->switch_base;
 		res.end += felix->switch_base;
 
-		target = ocelot_regmap_init(ocelot, &res);
+		target = felix->info->init_regmap(ocelot, &res);
 		if (IS_ERR(target)) {
 			dev_err(ocelot->dev,
 				"Failed to map memory space for port %d\n",
