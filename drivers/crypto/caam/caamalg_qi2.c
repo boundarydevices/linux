boundarydevices/linux

commit c3eb2dcfb0db9f807971aaee3b7821450c9f1638

crypto: caam - added support for TLS1.1 & TLS1.2
Enabled the support for TLS1.2 algorithms offload

        - aes-128-cbc-hmac-sha256
        - aes-256-cbc-hmac-sha256

Enabled the support of TLS1.1 algorithms offload

        - aes-128-cbc-hmac-sha1
        - aes-256-cbc-hmac-sha1

Support for the obsolete TLS1.0 is removed from the caam
driver.

Signed-off-by: Arun Pathak <arun.pathak@nxp.com>
Signed-off-by: Pankaj Gupta <pankaj.gupta@nxp.com>

drivers/crypto/caam/caamalg_qi2.c
@@ -3262,8 +3262,8 @@ static struct caam_aead_alg driver_aeads[] = {
 	{
 		.aead = {
 			.base = {
-				.cra_name = "tls10(hmac(sha1),cbc(aes))",
-				.cra_driver_name = "tls10-hmac-sha1-cbc-aes-caam-qi2",
+				.cra_name = "tls11(hmac(sha1),cbc(aes))",
+				.cra_driver_name = "tls11-hmac-sha1-cbc-aes-caam-qi2",
 				.cra_blocksize = AES_BLOCK_SIZE,
 			},
 			.setkey = tls_setkey,
@@ -3272,6 +3272,26 @@ static struct caam_aead_alg driver_aeads[] = {
 			.decrypt = tls_decrypt,
 			.ivsize = AES_BLOCK_SIZE,
 			.maxauthsize = SHA1_DIGEST_SIZE,
+		},
+		.caam = {
+			.class1_alg_type = OP_ALG_ALGSEL_AES | OP_ALG_AAI_CBC,
+			.class2_alg_type = OP_ALG_ALGSEL_SHA256 |
+					   OP_ALG_AAI_HMAC_PRECOMP,
+		},
+	},
+	{
+		.aead = {
+			.base = {
+				.cra_name = "tls12(hmac(sha256),cbc(aes))",
+				.cra_driver_name = "tls12-hmac-sha256-cbc-aes-caam-qi2",
+				.cra_blocksize = AES_BLOCK_SIZE,
+			},
+			.setkey = tls_setkey,
+			.setauthsize = tls_setauthsize,
+			.encrypt = tls_encrypt,
+			.decrypt = tls_decrypt,
+			.ivsize = AES_BLOCK_SIZE,
+			.maxauthsize = SHA256_DIGEST_SIZE,
 		},
 		.caam = {
 			.class1_alg_type = OP_ALG_ALGSEL_AES | OP_ALG_AAI_CBC,

drivers/crypto/caam/caamalg_desc.c
@@ -650,11 +650,21 @@ void cnstr_shdsc_tls_encap(u32 * const desc, struct alginfo *cdata,
 	 * Compute the index (in bytes) for the LOAD with destination of
 	 * Class 1 Data Size Register and for the LOAD that generates padding
 	 */
-	if (adata->key_inline) {
+	if (adata->key_inline && cdata->key_inline) {
 		idx_ld_datasz = DESC_TLS10_ENC_LEN + adata->keylen_pad +
 				cdata->keylen - 4 * CAAM_CMD_SZ;
 		idx_ld_pad = DESC_TLS10_ENC_LEN + adata->keylen_pad +
 			     cdata->keylen - 2 * CAAM_CMD_SZ;
+	} else if (adata->key_inline && !cdata->key_inline) {
+		idx_ld_datasz = DESC_TLS10_ENC_LEN + adata->keylen_pad +
+				CAAM_PTR_SZ - 4 * CAAM_CMD_SZ;
+		idx_ld_pad = DESC_TLS10_ENC_LEN + adata->keylen_pad +
+			     CAAM_PTR_SZ - 2 * CAAM_CMD_SZ;
+	} else if (adata->key_inline && !cdata->key_inline) {
+		idx_ld_datasz = DESC_TLS10_ENC_LEN + cdata->keylen +
+				CAAM_PTR_SZ - 4 * CAAM_CMD_SZ;
+		idx_ld_pad = DESC_TLS10_ENC_LEN + cdata->keylen +
+			     CAAM_PTR_SZ - 2 * CAAM_CMD_SZ;
 	} else {
 		idx_ld_datasz = DESC_TLS10_ENC_LEN + 2 * CAAM_PTR_SZ -
 				4 * CAAM_CMD_SZ;
@@ -698,11 +708,14 @@ void cnstr_shdsc_tls_encap(u32 * const desc, struct alginfo *cdata,
 	append_operation(desc, cdata->algtype | OP_ALG_AS_INITFINAL |
 			 OP_ALG_ENCRYPT);
 
-	/* payloadlen = input data length - (assoclen + ivlen) */
-	append_math_sub_imm_u32(desc, REG0, SEQINLEN, IMM, assoclen + ivsize);
+	/* payloadlen = input data length
+	 * - (assoclen + ivsize + xplicit ivsize)
+	 */
+	append_math_sub_imm_u32(desc, REG0, SEQINLEN,
+				IMM, assoclen + 2 * ivsize);
 
-	/* math1 = payloadlen + icvlen */
-	append_math_add_imm_u32(desc, REG1, REG0, IMM, authsize);
+	/* math1 = payloadlen + icvlen + xplicit ivsize */
+	append_math_add_imm_u32(desc, REG1, REG0, IMM, authsize + ivsize);
 
 	/* padlen = block_size - math1 % block_size */
 	append_math_and_imm_u32(desc, REG3, REG1, IMM, blocksize - 1);
@@ -741,6 +754,11 @@ void cnstr_shdsc_tls_encap(u32 * const desc, struct alginfo *cdata,
 	/* read assoc for authentication */
 	append_seq_fifo_load(desc, assoclen, FIFOLD_CLASS_CLASS2 |
 			     FIFOLD_TYPE_MSG);
+
+	/* read xplicit iv in case of >TL10 */
+	append_seq_fifo_load(desc, ivsize, FIFOLD_CLASS_CLASS1 |
+				     FIFOLD_TYPE_MSG);
+
 	/* insnoop payload */
 	append_seq_fifo_load(desc, 0, FIFOLD_CLASS_BOTH | FIFOLD_TYPE_MSG |
 			     FIFOLD_TYPE_LAST2 | FIFOLDST_VLF);

drivers/crypto/caam/caamalg_desc.h
@@ -18,7 +18,7 @@
 #define DESC_QI_AEAD_GIVENC_LEN		(DESC_AEAD_GIVENC_LEN + 3 * CAAM_CMD_SZ)
 
 #define DESC_TLS_BASE			(4 * CAAM_CMD_SZ)
-#define DESC_TLS10_ENC_LEN		(DESC_TLS_BASE + 29 * CAAM_CMD_SZ)
+#define DESC_TLS10_ENC_LEN		(DESC_TLS_BASE + 30  * CAAM_CMD_SZ)
 
 /* Note: Nonce is counted in cdata.keylen */
 #define DESC_AEAD_CTR_RFC3686_LEN	(4 * CAAM_CMD_SZ)

drivers/crypto/caam/caamalg_qi.c
@@ -2870,8 +2870,8 @@ static struct caam_aead_alg driver_aeads[] = {
 	{
 		.aead = {
 			.base = {
-				.cra_name = "tls10(hmac(sha1),cbc(aes))",
-				.cra_driver_name = "tls10-hmac-sha1-cbc-aes-caam-qi",
+				.cra_name = "tls11(hmac(sha1),cbc(aes))",
+				.cra_driver_name = "tls11-hmac-sha1-cbc-aes-caam-qi",
 				.cra_blocksize = AES_BLOCK_SIZE,
 			},
 			.setkey = tls_setkey,
@@ -2884,6 +2884,26 @@ static struct caam_aead_alg driver_aeads[] = {
 		.caam = {
 			.class1_alg_type = OP_ALG_ALGSEL_AES | OP_ALG_AAI_CBC,
 			.class2_alg_type = OP_ALG_ALGSEL_SHA1 |
+					   OP_ALG_AAI_HMAC_PRECOMP,
+		},
+	},
+	{
+		.aead = {
+			.base = {
+				.cra_name = "tls12(hmac(sha256),cbc(aes))",
+				.cra_driver_name = "tls12-hmac-sha256-cbc-aes-caam-qi",
+				.cra_blocksize = AES_BLOCK_SIZE,
+			},
+			.setkey = tls_setkey,
+			.setauthsize = tls_setauthsize,
+			.encrypt = tls_encrypt,
+			.decrypt = tls_decrypt,
+			.ivsize = AES_BLOCK_SIZE,
+			.maxauthsize = SHA256_DIGEST_SIZE,
+		},
+		.caam = {
+			.class1_alg_type = OP_ALG_ALGSEL_AES | OP_ALG_AAI_CBC,
+			.class2_alg_type = OP_ALG_ALGSEL_SHA256 |
 					   OP_ALG_AAI_HMAC_PRECOMP,
 		}
 	}

commit 53921fa111d17b10ded3a3d45281928204d37120

LF-5355 crypto: caam/qi2 - fix typo in tls algorithm registration
Fix the typos when trying to register tls algorithms:
-"tls11(hmac(sha1),cbc(aes))" uses OP_ALG_ALGSEL_SHA256
instead of OP_ALG_ALGSEL_SHA1
-"tls12(hmac(sha256),cbc(aes))" uses OP_ALG_ALGSEL_SHA1
instead of OP_ALG_ALGSEL_SHA256

Fixes: c3eb2dcfb0db ("crypto: caam - added support for TLS1.1 & TLS1.2")
Verified-by: Pankaj Gupta <pankaj.gupta@nxp.com>
Signed-off-by: Pankaj Gupta <pankaj.gupta@nxp.com>
Signed-off-by: Horia Geantă <horia.geanta@nxp.com>

drivers/crypto/caam/caamalg_qi2.c
@@ -3275,7 +3275,7 @@ static struct caam_aead_alg driver_aeads[] = {
 		},
 		.caam = {
 			.class1_alg_type = OP_ALG_ALGSEL_AES | OP_ALG_AAI_CBC,
-			.class2_alg_type = OP_ALG_ALGSEL_SHA256 |
+			.class2_alg_type = OP_ALG_ALGSEL_SHA1 |
 					   OP_ALG_AAI_HMAC_PRECOMP,
 		},
 	},
@@ -3295,7 +3295,7 @@ static struct caam_aead_alg driver_aeads[] = {
 		},
 		.caam = {
 			.class1_alg_type = OP_ALG_ALGSEL_AES | OP_ALG_AAI_CBC,
-			.class2_alg_type = OP_ALG_ALGSEL_SHA1 |
+			.class2_alg_type = OP_ALG_ALGSEL_SHA256 |
 					   OP_ALG_AAI_HMAC_PRECOMP,
 		},
 	},