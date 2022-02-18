b4d5412b2a4327e9873a8cd1eb396831f668da62
Dong Aisheng <aisheng.dong@nxp.com>
2022-02-18 17:10:23 +0800

    Revert "drm/bridge: adv7511: Register and attach our DSI device at probe"
    This reverts commit 864c49a31d6b84fd20748a003523fc2181fd54b1.
    
    This patch caused adv7511 can't work anymore on 8mq/mm/mn/qxp.
    Revert it first and give owner more time to find a better fix.
    
    Signed-off-by: Dong Aisheng <aisheng.dong@nxp.com>

--- drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
+++ drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
@@ -991,6 +991,9 @@ static int adv7511_bridge_attach(struct drm_bridge *bridge,
 			return ret;
 	}
 
+	if (adv->type == ADV7533 || adv->type == ADV7535)
+		ret = adv7533_attach_dsi(adv);
+
 	if (adv->i2c_main->irq)
 		regmap_write(adv->regmap, ADV7511_REG_INT_ENABLE(0),
 			     ADV7511_INT0_HPD);
@@ -1419,18 +1422,8 @@ static int adv7511_probe(struct i2c_client *i2c)
 	drm_bridge_add(&adv7511->bridge);
 
 	adv7511_audio_init(dev, adv7511);
-
-	if (adv7511->type == ADV7533 || adv7511->type == ADV7535) {
-		ret = adv7533_attach_dsi(adv7511);
-		if (ret)
-			goto err_unregister_audio;
-	}
-
 	return 0;
 
-err_unregister_audio:
-	adv7511_audio_exit(adv7511);
-	drm_bridge_remove(&adv7511->bridge);
 err_unregister_cec:
 	cec_unregister_adapter(adv7511->cec_adap);
 	i2c_unregister_device(adv7511->i2c_cec);
