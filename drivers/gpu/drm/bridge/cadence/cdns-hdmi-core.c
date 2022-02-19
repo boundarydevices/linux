1a9f299337b19fe845e4df2a3f1421c3936c3706
Troy Kisky <troy.kisky@boundarydevices.com>
2022-02-18 16:52:09 -0800

    gpu: drm: bridge: cadence: cdns-hdmi-core: set cec phys_addr from edid
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/gpu/drm/bridge/cadence/cdns-hdmi-core.c
+++ drivers/gpu/drm/bridge/cadence/cdns-hdmi-core.c
@@ -29,6 +29,8 @@
 #include <linux/mutex.h>
 #include <linux/of_device.h>
 
+#include <media/cec-notifier.h>
+
 #include "cdns-mhdp-hdcp.h"
 #include "cdns-hdcp-common.h"
 
@@ -303,6 +305,7 @@ static int cdns_hdmi_connector_get_modes(struct drm_connector *connector)
 			 edid->header[4], edid->header[5],
 			 edid->header[6], edid->header[7]);
 		drm_connector_update_edid_property(connector, edid);
+		cec_notifier_set_phys_addr_from_edid(mhdp->hdmi.cec.notifier, edid);
 		num_modes = drm_add_edid_modes(connector, edid);
 		mhdp->hdmi.hdmi_type = drm_detect_hdmi_monitor(edid) ?
 						MODE_HDMI_1_4 : MODE_DVI;
@@ -599,6 +602,7 @@ static void hotplug_work_func(struct work_struct *work)
 		/* force mode set for cable replugin to recovery HDMI2.0 video modes */
 		mhdp->force_mode_set = true;
 		enable_irq(mhdp->irq[IRQ_IN]);
+		cec_notifier_phys_addr_invalidate(mhdp->hdmi.cec.notifier);
 	}
 }
 
