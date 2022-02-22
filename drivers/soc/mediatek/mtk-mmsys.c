8b13a29dd6aa0982f2c1b379ccf38305cbda5d6a
Yongqiang Niu <yongqiang.niu@mediatek.com>
2022-02-22 13:28:01 +0800

    UPSTREAM: soc: mediatek: mmsys: add mt8186 mmsys routing table
    Add new routing table for MT8186.
    In MT8186, there are two routing pipelines for internal and external
    display.
    
    Internal display: OVL0->RDMA0->COLOR0->CCORR0->AAL0->GAMMA->POSTMASK0->
    		  DITHER->DSI0
    External display: OVL_2L0->RDMA1->DPI0
    
    Signed-off-by: Yongqiang Niu <yongqiang.niu@mediatek.com>
    Signed-off-by: Rex-BC Chen <rex-bc.chen@mediatek.com>

--- drivers/soc/mediatek/mtk-mmsys.c
+++ drivers/soc/mediatek/mtk-mmsys.c
@@ -17,6 +17,7 @@
 #include "mtk-mmsys.h"
 #include "mt8167-mmsys.h"
 #include "mt8183-mmsys.h"
+#include "mt8186-mmsys.h"
 #include "mt8192-mmsys.h"
 #include "mt8365-mmsys.h"
 
@@ -59,6 +60,12 @@ static const struct mtk_mmsys_driver_data mt8183_mmsys_driver_data = {
 	.mdp_routes = mmsys_mt8183_mdp_routing_table,
 	.mdp_num_routes = ARRAY_SIZE(mmsys_mt8183_mdp_routing_table),
 	.mdp_isp_ctrl = mmsys_mt8183_mdp_isp_ctrl_table,
+};
+
+static const struct mtk_mmsys_driver_data mt8186_mmsys_driver_data = {
+	.clk_driver = "clk-mt8186-mm",
+	.routes = mmsys_mt8186_routing_table,
+	.num_routes = ARRAY_SIZE(mmsys_mt8186_routing_table),
 };
 
 static const struct mtk_mmsys_driver_data mt8192_mmsys_driver_data = {
@@ -402,6 +409,10 @@ static const struct of_device_id of_match_mtk_mmsys[] = {
 	{
 		.compatible = "mediatek,mt8183-mmsys",
 		.data = &mt8183_mmsys_driver_data,
+	},
+	{
+		.compatible = "mediatek,mt8186-mmsys",
+		.data = &mt8186_mmsys_driver_data,
 	},
 	{
 		.compatible = "mediatek,mt8192-mmsys",
