fc177f96c08336708d7166d64e693afd8ef947dd
Peng Fan <peng.fan@nxp.com>
2022-02-28 10:09:06 +0800

    clk: imx: add i.MX93 composite clk
    i.MX93 CCM ROOT clock has a mux, gate and divider in one register, here
    is to combine all these into one composite clk and simplify clk tree.
    i.MX93 CCM is a new IP compared with i.MX8M, so introduce a new file.
    
    Reviewed-by: Abel Vesa <abel.vesa@nxp.com>
    Reviewed-by: Stephen Boyd <sboyd@kernel.org>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Link: https://lore.kernel.org/r/20220228020908.2810346-4-peng.fan@oss.nxp.com
    Signed-off-by: Abel Vesa <abel.vesa@nxp.com>

--- drivers/clk/imx/clk.h
+++ drivers/clk/imx/clk.h
@@ -668,6 +668,15 @@ struct clk_hw *imx8m_clk_hw_composite_flags(const char *name,
 #define imx8m_clk_composite_critical(name, parent_names, reg) \
 	__imx8m_clk_composite(name, parent_names, reg, CLK_IS_CRITICAL)
 
+struct clk_hw *imx93_clk_composite_flags(const char *name,
+					 const char * const *parent_names,
+					 int num_parents,
+					 void __iomem *reg,
+					 unsigned long flags);
+#define imx93_clk_composite(name, parent_names, num_parents, reg) \
+	imx93_clk_composite_flags(name, parent_names, num_parents, reg, \
+				  CLK_SET_RATE_NO_REPARENT | CLK_OPS_PARENT_ENABLE)
+
 struct clk_hw *imx_clk_hw_divider_gate(const char *name, const char *parent_name,
 		unsigned long flags, void __iomem *reg, u8 shift, u8 width,
 		u8 clk_divider_flags, const struct clk_div_table *table,
