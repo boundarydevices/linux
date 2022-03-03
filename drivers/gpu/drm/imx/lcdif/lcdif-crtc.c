ac4efeabc06cf0a4055de047df61c21108eacc16
Troy Kisky <troy.kisky@boundarydevices.com>
2022-03-02 16:28:18 -0800

    gpu: drm: imx: lcdif: setup plane before enable, so that enable can actually enable it
    move drm_atomic_helper_commit_planes call
    before drm_atomic_helper_commit_modeset_enables
    
    so that enable can do the enable. This allows the backlight
    to be turned on after the display is running and prevents flashing
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/gpu/drm/imx/lcdif/lcdif-crtc.c
+++ drivers/gpu/drm/imx/lcdif/lcdif-crtc.c
@@ -195,10 +195,7 @@ static void lcdif_crtc_atomic_enable(struct drm_crtc *crtc,
 	/* config LCDIF output bus format */
 	lcdif_set_bus_fmt(lcdif, imx_crtc_state->bus_format);
 
-	/* defer the lcdif controller enable to plane update,
-	 * since until then the lcdif config is complete to
-	 * enable the controller to run actually.
-	 */
+	lcdif_enable_controller(lcdif, use_i80);
 }
 
 static void lcdif_crtc_atomic_disable(struct drm_crtc *crtc,
@@ -277,6 +274,7 @@ static int lcdif_enable_vblank(struct drm_crtc *crtc)
 	struct lcdif_crtc *lcdif_crtc = to_lcdif_crtc(crtc);
 	struct lcdif_soc *lcdif = dev_get_drvdata(lcdif_crtc->dev->parent);
 
+	pm_runtime_get_sync(lcdif_crtc->dev->parent);
 	lcdif_vblank_irq_enable(lcdif);
 	enable_irq(lcdif_crtc->vbl_irq);
 
@@ -290,6 +288,7 @@ static void lcdif_disable_vblank(struct drm_crtc *crtc)
 
 	disable_irq_nosync(lcdif_crtc->vbl_irq);
 	lcdif_vblank_irq_disable(lcdif);
+	pm_runtime_put(lcdif_crtc->dev->parent);
 }
 
 static const struct drm_crtc_funcs lcdif_crtc_funcs = {
