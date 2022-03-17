ffd6b6a047e85830f767e624a49bbe7c85601666
Gary Bisson <gary.bisson@boundarydevices.com>
2022-03-17 12:02:21 +0100

    mxc: hantro*: fix build with gcc
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- drivers/mxc/hantro/hantrodec.c
+++ drivers/mxc/hantro/hantrodec.c
@@ -298,8 +298,9 @@ static u32 secure_regs_g2[5] = {HWIF_DEC_RSY_BASE,
 static bool check_secure_regs(u32 target, int id)
 {
 	int array_size = id == 0 ? 2 : 5;
+	size_t i;
 
-	for (size_t i = 0; i < array_size; i++) {
+	for (i = 0; i < array_size; i++) {
 		if (id == 0)
 			comp_secure_regs(g1);
 		else
