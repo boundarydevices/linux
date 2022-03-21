87d3243f9e0a13a082bffd678ef2fecdf392d1c5
Nicolas Belin <nbelin@baylibre.com>
2022-03-21 16:15:24 +0100

    sound: soc: mediatek: Use the common set_card_codec_info function
    Use the common set_card_codec_info function in order to avoid the
    replications.
    
    Signed-off-by: Nicolas Belin <nbelin@baylibre.com>

--- sound/soc/mediatek/mt8365/mt8365-evk.c
+++ sound/soc/mediatek/mt8365/mt8365-evk.c
@@ -19,6 +19,7 @@
 #include <sound/soc.h>
 #include <sound/pcm_params.h>
 #include "mt8365-afe-common.h"
+#include "../common/mtk-soundcard-driver.h"
 
 #define PREFIX	"mediatek,"
 
@@ -468,74 +469,6 @@ static struct snd_soc_card mt8365_evk_card = {
 	.dapm_routes = mt8365_evk_routes,
 	.num_dapm_routes = ARRAY_SIZE(mt8365_evk_routes),
 };
-
-static int set_card_codec_info(struct snd_soc_card *card)
-{
-	struct snd_soc_dai_link_component *dai_link_codecs, *dlc;
-	struct device_node *dl_node, *c_node;
-	struct device *dev = card->dev;
-	struct of_phandle_args args;
-	const char *dai_link_name;
-	int num_codecs;
-	int ret, i;
-
-	/* Loop over all the dai link sub nodes*/
-	for_each_child_of_node(dev->of_node, dl_node) {
-		if (of_property_read_string(dl_node, "dai-link-name",
-					    &dai_link_name))
-			return -EINVAL;
-
-		num_codecs = of_get_child_count(dl_node);
-		/* Allocate the snd_soc_dai_link_component array that will be
-		 * used to dynamically add the list of codecs to the static
-		 * snd_soc_dai_link array.
-		 */
-		dai_link_codecs = devm_kcalloc(dev, num_codecs,
-					       sizeof(*dai_link_codecs),
-					       GFP_KERNEL);
-		if (!dai_link_codecs)
-			return -ENOMEM;
-
-		dlc = dai_link_codecs;
-
-		/* Loop over all the codec sub nodes for this dai link */
-		for_each_child_of_node(dl_node, c_node) {
-			/* Retrieve the node and the dai_name that are used
-			 * by the soundcard.
-			 */
-			ret = of_parse_phandle_with_args(c_node, "sound-dai",
-							 "#sound-dai-cells", 0,
-							 &args);
-			if (ret) {
-				if (ret != -EPROBE_DEFER)
-					dev_err(dev,
-						"can't parse dai %d\n", ret);
-				return ret;
-			}
-			dlc->of_node = args.np;
-			ret =  snd_soc_get_dai_name(&args, &dlc->dai_name);
-			if (ret) {
-				of_node_put(c_node);
-				return ret;
-			}
-
-			dlc++;
-		}
-
-		/* Update the snd_soc_dai_link static array with the codecs
-		 * we have just found.
-		 */
-		for (i = 0; i < card->num_links; i++) {
-			if (!strcmp(dai_link_name, card->dai_link[i].name)) {
-				card->dai_link[i].num_codecs = num_codecs;
-				card->dai_link[i].codecs = dai_link_codecs;
-				break;
-			}
-		}
-	}
-
-	return 0;
-}
 
 static int mt8365_evk_dev_probe(struct platform_device *pdev)
 {
