8e57377ece109e4d931c6514571d3c4df9567752
Mike Christie <michael.christie@oracle.com>
2022-04-07 19:13:12 -0500

    scsi: iscsi: Fix NOP handling during conn recovery
    [ Upstream commit 44ac97109e42f87b1a34954704b81b6c8eca80c4 ]
    
    If a offload driver doesn't use the xmit workqueue, then when we are doing
    ep_disconnect libiscsi can still inject PDUs to the driver. This adds a
    check for if the connection is bound before trying to inject PDUs.
    
    Link: https://lore.kernel.org/r/20220408001314.5014-9-michael.christie@oracle.com
    Tested-by: Manish Rangankar <mrangankar@marvell.com>
    Reviewed-by: Lee Duncan <lduncan@suse.com>
    Reviewed-by: Chris Leech <cleech@redhat.com>
    Signed-off-by: Mike Christie <michael.christie@oracle.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/scsi/libiscsi.h
+++ include/scsi/libiscsi.h
@@ -55,6 +55,11 @@ enum {
 /* Connection suspend "bit" */
 #define ISCSI_SUSPEND_BIT		1
 
+/* Connection flags */
+#define ISCSI_CONN_FLAG_SUSPEND_TX	BIT(0)
+#define ISCSI_CONN_FLAG_SUSPEND_RX	BIT(1)
+#define ISCSI_CONN_FLAG_BOUND		BIT(2)
+
 #define ISCSI_ITT_MASK			0x1fff
 #define ISCSI_TOTAL_CMDS_MAX		4096
 /* this must be a power of two greater than ISCSI_MGMT_CMDS_MAX */
