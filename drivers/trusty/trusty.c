b40dfda0532be25e299814e87348b5e2ac266878
Ji Luo <ji.luo@nxp.com>
2022-04-11 09:20:49 +0800

    MA-20199 Make the trusty core driver probe later
    With the pkvm support, the FF-A share instructions will trap to
    the FF-A handler in EL2 before they are routed to EL3. But as
    the trusty core driver (which will issue SMC_FCZ_FFA_RXTX_MAP)
    probes at subsys_initcall() which is early than the kvm driver
    probe, which causes the mailbox mapping in kvm failed and all
    following memory share failed.
    
    This issue will impact platforms without GKI support, this commit
    moves the trusty core driver probe to module_init() so it can
    probe when kvm drivers ready.
    
    Change-Id: I4182eadf879b5cb3819cbbf816f98a7c79b2b45b
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

--- drivers/trusty/trusty.c
+++ drivers/trusty/trusty.c
@@ -1106,7 +1106,7 @@ static void __exit trusty_driver_exit(void)
 	platform_driver_unregister(&trusty_driver);
 }
 
-subsys_initcall(trusty_driver_init);
+module_init(trusty_driver_init);
 module_exit(trusty_driver_exit);
 
 #define CREATE_TRACE_POINTS
