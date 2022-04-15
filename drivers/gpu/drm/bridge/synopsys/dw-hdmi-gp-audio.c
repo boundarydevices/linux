95b713d1de669408fd402e03836844629cfb636d
Troy Kisky <troy.kisky@boundarydevices.com>
2022-04-15 10:20:26 -0700

    gpu: drm: bridge: synopsys: dw-hdmi-gp-audio: check return value from get_eld
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/gpu/drm/bridge/synopsys/dw-hdmi-gp-audio.c
+++ drivers/gpu/drm/bridge/synopsys/dw-hdmi-gp-audio.c
@@ -118,9 +118,14 @@ static int audio_get_eld(struct device *dev, void *data,
 			 u8 *buf, size_t len)
 {
 	struct snd_dw_hdmi *dw = dev_get_drvdata(dev);
+	u8 *eld;
 
-	memcpy(buf, dw->data.get_eld(dw->data.hdmi),
-	       min_t(size_t, MAX_ELD_BYTES, len));
+	eld = dw->data.get_eld(dw->data.hdmi);
+	if (eld)
+		memcpy(buf, eld, min_t(size_t, MAX_ELD_BYTES, len));
+	else
+		/* Pass an empty ELD if connector not available */
+		memset(buf, 0, len);
 
 	return 0;
 }
