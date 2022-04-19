19a223d63d7f03a1711eb20e4b85b03793d9d42f
Jindong Yue <jindong.yue@nxp.com>
2022-04-19 09:22:44 +0800

    MA-19046-1 dma: mxs-dma: Add module license and description
    Module license string is required for loading it as a module.
    
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-by: Han Xu <han.xu@nxp.com>

--- drivers/dma/mxs-dma.c
+++ drivers/dma/mxs-dma.c
@@ -963,3 +963,6 @@ static struct platform_driver mxs_dma_driver = {
 	.probe = mxs_dma_probe,
 };
 module_platform_driver(mxs_dma_driver);
+
+MODULE_DESCRIPTION("MXS DMA driver");
+MODULE_LICENSE("GPL");
