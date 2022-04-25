8ea94cc163c7d43f87e8184a93edf2df590a3524
Fuad Tabba <tabba@google.com>
2022-04-25 10:26:34 +0000

    ANDROID: KVM: arm64: Move pstate reset value definitions to kvm_arm.h
    Move the macro defines of the pstate reset values to a shared
    header to be used by hyp in future patches.
    
    No functional change intended.
    
    Bug: 357781595
    Change-Id: I31925b7175496ac1d8acff1f0618af07103adaef
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- arch/arm64/kvm/reset.c
+++ arch/arm64/kvm/reset.c
@@ -34,18 +34,6 @@
 static u32 __ro_after_init kvm_ipa_limit;
 unsigned int __ro_after_init kvm_host_sve_max_vl;
 
-/*
- * ARMv8 Reset Values
- */
-#define VCPU_RESET_PSTATE_EL1	(PSR_MODE_EL1h | PSR_A_BIT | PSR_I_BIT | \
-				 PSR_F_BIT | PSR_D_BIT)
-
-#define VCPU_RESET_PSTATE_EL2	(PSR_MODE_EL2h | PSR_A_BIT | PSR_I_BIT | \
-				 PSR_F_BIT | PSR_D_BIT)
-
-#define VCPU_RESET_PSTATE_SVC	(PSR_AA32_MODE_SVC | PSR_AA32_A_BIT | \
-				 PSR_AA32_I_BIT | PSR_AA32_F_BIT)
-
 unsigned int __ro_after_init kvm_sve_max_vl;
 
 int __init kvm_arm_init_sve(void)
