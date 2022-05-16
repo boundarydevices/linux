3fc3afd0bddac3a9b33f80b7adeb76ed09721fb8
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2022-05-16 16:16:11 +0200

    FIXUP: drm/mediatek: dpi: implement a CK/DE pol toggle in board config
    Do the same for mt8365
    
    Fixes: f89090c1ca88 ("drm/mediatek: dpi: implement a CK/DE pol toggle in board config")
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- drivers/gpu/drm/mediatek/mtk_dpi.c
+++ drivers/gpu/drm/mediatek/mtk_dpi.c
@@ -870,6 +870,7 @@ static const struct mtk_dpi_conf mt8365_conf = {
 	.max_clock_khz = 150000,
 	.output_fmts = mt8183_output_fmts,
 	.num_output_fmts = ARRAY_SIZE(mt8183_output_fmts),
+	.is_ck_de_pol = true,
 	.limit = &mtk_dpi_limit,
 };
 
