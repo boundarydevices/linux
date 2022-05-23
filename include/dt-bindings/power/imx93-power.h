c5cb7fdc910dcc778efefdbc6f79f2ee97043cde
Peng Fan <peng.fan@nxp.com>
2022-05-23 16:49:59 +0800

    MLK-25894-1 dt-bindings: soc: add bindings for i.MX93 SRC
    Add bindings for i.MX93 System Reset Controller(SRC). SRC supports
    resets and power gating for mixes.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Jacky Bai <ping.bai@nxp.com>

---
+++ include/dt-bindings/power/imx93-power.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: (GPL-2.0 OR MIT) */
+/*
+ *  Copyright 2022 NXP
+ */
+
+#ifndef __DT_BINDINGS_IMX93_POWER_H__
+#define __DT_BINDINGS_IMX93_POWER_H__
+
+#define IMX93_POWER_DOMAIN_MEDIAMIX		0
+
+#endif
