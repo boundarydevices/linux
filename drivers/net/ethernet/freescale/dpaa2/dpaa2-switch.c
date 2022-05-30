ebcc64da04bb5d1a3b2b14abea201913240860e6
Ioana Ciornei <ioana.ciornei@nxp.com>
2022-05-30 12:50:28 +0300

    dpaa2-switch: cleanup the egress flood of an unused FDB
    In case a switch interface is joining a bond device or a bridge one, its
    FDB might change. In case this happens, we have to recreate the egress
    flood setup of the FDB that we just left.
    For this to happen, keep track of the old FDB and just call
    dpaa2_switch_fdb_set_egress_flood() on it.
    
    Signed-off-by: Ioana Ciornei <ioana.ciornei@nxp.com>

--- drivers/net/ethernet/freescale/dpaa2/dpaa2-switch.c
+++ drivers/net/ethernet/freescale/dpaa2/dpaa2-switch.c
@@ -2113,6 +2113,7 @@ static int dpaa2_switch_port_bridge_join(struct net_device *netdev,
 {
 	struct ethsw_port_priv *port_priv = netdev_priv(netdev);
 	struct ethsw_core *ethsw = port_priv->ethsw_data;
+	struct dpaa2_switch_fdb *old_fdb = port_priv->fdb;
 	struct ethsw_port_priv *other_port_priv;
 	struct net_device *other_dev;
 	struct list_head *iter;
@@ -2145,6 +2146,11 @@ static int dpaa2_switch_port_bridge_join(struct net_device *netdev,
 
 	/* Setup the egress flood policy (broadcast, unknown unicast) */
 	err = dpaa2_switch_fdb_set_egress_flood(ethsw, port_priv->fdb->fdb_id);
+	if (err)
+		goto err_egress_flood;
+
+	/* Recreate the egress flood domain of the FDB that we just left. */
+	err = dpaa2_switch_fdb_set_egress_flood(ethsw, old_fdb->fdb_id);
 	if (err)
 		goto err_egress_flood;
 
@@ -2356,6 +2362,7 @@ static int dpaa2_switch_port_bond_join(struct net_device *netdev,
 {
 	struct ethsw_port_priv *port_priv = netdev_priv(netdev);
 	struct ethsw_core *ethsw = port_priv->ethsw_data;
+	struct dpaa2_switch_fdb *old_fdb = port_priv->fdb;
 	struct ethsw_port_priv *other_port_priv;
 	struct net_device *other_dev;
 	struct list_head *iter;
@@ -2384,6 +2391,11 @@ static int dpaa2_switch_port_bond_join(struct net_device *netdev,
 	err = dpaa2_switch_fdb_set_egress_flood(ethsw, port_priv->fdb->fdb_id);
 	if (err)
 		goto err_egress_flood;
+
+	/* Recreate the egress flood domain of the FDB that we just left. */
+	err = dpaa2_switch_fdb_set_egress_flood(ethsw, old_fdb->fdb_id);
+	if (err)
+		return err;
 
 	/* Setup the port_priv->lag pointer for this switch port */
 	err = dpaa2_switch_port_set_lag_group(port_priv, bond_dev);
