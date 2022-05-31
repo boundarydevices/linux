80c01be05151747f76d1671f1f911eef40a90916
Biwen Li <biwen.li@nxp.com>
2022-05-31 17:50:21 +0800

    drivers: tty: support i.MX SRTM protocol(for uart)
    resolved Conflicts:
    	drivers/tty/rpmsg_tty.c
    Signed-off-by: Biwen Li <biwen.li@nxp.com>
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- drivers/tty/rpmsg_tty.c
+++ drivers/tty/rpmsg_tty.c
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (C) 2021 STMicroelectronics - All Rights Reserved
+ * Copyright 2023 NXP
  *
  * The rpmsg tty driver implements serial communication on the RPMsg bus to makes
  * possible for user-space programs to send and receive rpmsg messages as a standard
  * tty protocol.
  *
- * The remote processor can instantiate a new tty by requesting a "rpmsg-tty" RPMsg service.
- * The "rpmsg-tty" service is directly used for data exchange. No flow control is implemented yet.
+ * The remote processor can instantiate a new tty by requesting a "srtm-uart-channel" or "rpmsg-tty" RPMsg service.
+ * The "srtm-uart-channel" or "rpmsg-tty" service is directly used for data exchange. No flow control is implemented yet.
  */
 
 #define pr_fmt(fmt)		KBUILD_MODNAME ": " fmt
@@ -17,6 +18,43 @@
 #include <linux/slab.h>
 #include <linux/tty.h>
 #include <linux/tty_flip.h>
+#include <linux/rpmsg/imx_srtm.h>
+#include <linux/of.h>
+
+/*
+ * The srtm (simplified real time message) protocol for uart:
+ *
+ *   +---------------+-------------------------------+
+ *   |  Byte Offset  |            Content            |
+ *   +---------------+---+---+---+---+---+---+---+---+
+ *   |       0       |           Category            |
+ *   +---------------+---+---+---+---+---+---+---+---+
+ *   |     1 ~ 2     |           Version             |
+ *   +---------------+---+---+---+---+---+---+---+---+
+ *   |       3       |             Type              |
+ *   +---------------+---+---+---+---+---+---+---+---+
+ *   |       4       |           Command             |
+ *   +---------------+---+---+---+---+---+---+---+---+
+ *   |       5       |           Priority            |
+ *   +---------------+---+---+---+---+---+---+---+---+
+ *   |       6       |           Reserved1           |
+ *   +---------------+---+---+---+---+---+---+---+---+
+ *   |       7       |           Reserved2           |
+ *   +---------------+---+---+---+---+---+---+---+---+
+ *   |       8       |           Reserved3           |
+ *   +---------------+---+---+---+---+---+---+---+---+
+ *   |       9       |           Reserved4           |
+ *   +---------------+---+---+---+---+---+---+---+---+
+ *   |       10      |           UART BUS ID         |
+ *   +---------------+---+---+---+---+---+---+---+---+
+ *   |       11    |         reserved/ret code       |
+ *   +---------------+---+---+---+---+---+---+---+---+
+ *   |    12 ~ 13    |           Flags               |
+ *   +---------------+---+---+---+---+---+---+---+---+
+ *   |    14 ~ 495   |           Data                |
+ *   +---------------+---+---+---+---+---+---+---+---+
+ * UART BUS ID: real uart instance, such as: the soc has UART0(UART BUS ID = 0), UART1(UART BUS ID = 1), UART2(UART BUS ID = 2), ... , UARTx(UART BUS ID = x)
+ */
 
 #define RPMSG_TTY_NAME	"ttyRPMSG"
 #define MAX_TTY_RPMSG	32
@@ -29,18 +67,72 @@ static struct tty_driver *rpmsg_tty_driver;
 struct rpmsg_tty_port {
 	struct tty_port		port;	 /* TTY port data */
 	int			id;	 /* TTY rpmsg index */
+	int			bus_id; /* used for srtm uart protocol, which real uart will be used, such as, uart0, uart1, uart2, uart3... */
+	int			flags; /* used for srtm uart protocol */
+	u16			srtm_uart_msg_data_max_sz; /* used for srtm uart protocol */
+	bool			use_srtm_uart_protocol; /* used for srtm uart protocol */
 	struct rpmsg_device	*rpdev;	 /* rpmsg device */
+};
+
+struct srtm_uart_msg_header {
+	struct imx_srtm_head common;
+	u8 bus_id; /* The bus_id is used when send data from acore to mcore; The bus_id is useless when acore received data that from mcore*/
+	union {
+		u8 reserved; /* used in request packet */
+		u8 retCode; /* used in response packet */
+	};
+	u16 flags;
+} __packed __aligned[1];
+
+struct srtm_uart_msg {
+	struct srtm_uart_msg_header header;
+	/* srtm uart Payload Start */
+	u8 data[1];
+} __packed __aligned(1);
+
+struct imx_srtm_uart_data_structure
+{
+	bool use_srtm_uart_protocol;
+};
+
+const static struct imx_srtm_uart_data_structure imx_srtm_uart_data = {
+	.use_srtm_uart_protocol = true,
+};
+
+const static struct imx_srtm_uart_data_structure rpmsg_tty_data = {
+	.use_srtm_uart_protocol = false,
 };
 
 static int rpmsg_tty_cb(struct rpmsg_device *rpdev, void *data, int len, void *priv, u32 src)
 {
 	struct rpmsg_tty_port *cport = dev_get_drvdata(&rpdev->dev);
+	struct srtm_uart_msg *msg = (struct srtm_uart_msg *)data;
 	int copied;
+	u8 *payload = data;
+	int payload_len = len;
 
-	if (!len)
+	if (!payload_len)
 		return -EINVAL;
-	copied = tty_insert_flip_string(&cport->port, data, len);
-	if (copied != len)
+	if (cport->use_srtm_uart_protocol) {
+		if (payload_len < (sizeof(struct srtm_uart_msg)))
+			return -EINVAL;
+		payload_len -= (sizeof(struct srtm_uart_msg) - 1); /* 1: msg->data[0] */
+
+		if (msg->header.common.type != IMX_SRTM_TYPE_RESPONSE && msg->header.common.type != IMX_SRTM_TYPE_NOTIFY) {
+			return -EINVAL;
+		}
+
+		if (payload_len > cport->srtm_uart_msg_data_max_sz) {
+			dev_err(&rpdev->dev,
+			"%s failed: data length greater than %d, len=%d\n",
+			__func__, cport->srtm_uart_msg_data_max_sz, payload_len);
+			return -EINVAL;
+		}
+		payload = &msg->data[0];
+	}
+
+	copied = tty_insert_flip_string(&cport->port, payload, payload_len);
+	if (copied != payload_len)
 		dev_err_ratelimited(&rpdev->dev, "Trunc buffer: available space is %d\n", copied);
 	tty_flip_buffer_push(&cport->port);
 
@@ -78,26 +170,52 @@ static ssize_t rpmsg_tty_write(struct tty_struct *tty, const u8 *buf,
 {
 	struct rpmsg_tty_port *cport = tty->driver_data;
 	struct rpmsg_device *rpdev;
-	int msg_max_size, msg_size;
+	int msg_max_size, msg_size = -1;
+	struct srtm_uart_msg *msg = NULL;
 	int ret;
+	void *data = NULL;
+	int data_len = 0;
 
 	rpdev = cport->rpdev;
 
-	msg_max_size = rpmsg_get_mtu(rpdev->ept);
+	msg_max_size = cport->use_srtm_uart_protocol ? (cport->srtm_uart_msg_data_max_sz) : (rpmsg_get_mtu(rpdev->ept));
 	if (msg_max_size < 0)
 		return msg_max_size;
 
 	msg_size = min_t(unsigned int, len, msg_max_size);
 
+	if (cport->use_srtm_uart_protocol) {
+		data_len = msg_size + sizeof(struct srtm_uart_msg) - 1; /* srtm uart msg header + data len */
+		msg = kmalloc(data_len, GFP_KERNEL);
+		if (!msg)
+			return -ENOMEM;
+
+		memset(msg, 0, data_len);
+		msg->header.common.cate = IMX_SRTM_CATEGORY_UART;
+		msg->header.common.major = IMX_SRTM_VER_UART;
+		msg->header.common.minor = IMX_SRTM_VER_UART >> 8;
+		msg->header.common.type = IMX_SRTM_TYPE_NOTIFY;
+		msg->header.common.cmd = IMX_SRTM_UART_COMMAND_SEND;
+		msg->header.common.reserved[0] = IMX_SRTM_UART_PRIORITY;
+		msg->header.bus_id = cport->bus_id & 0xFF;
+		msg->header.flags = cport->flags & 0xFFFF;
+		memcpy(&msg->data[0], buf, msg_size);
+		data = (void *)msg;
+	} else {
+		data = (void *)buf;
+		data_len = msg_size;
+	}
 	/*
 	 * Use rpmsg_trysend instead of rpmsg_send to send the message so the caller is not
 	 * hung until a rpmsg buffer is available. In such case rpmsg_trysend returns -ENOMEM.
 	 */
-	ret = rpmsg_trysend(rpdev->ept, (void *)buf, msg_size);
+	ret = rpmsg_trysend(rpdev->ept, data, data_len);
 	if (ret) {
 		dev_dbg_ratelimited(&rpdev->dev, "rpmsg_send failed: %d\n", ret);
-		return ret;
+		msg_size = ret;
 	}
+
+	kfree(msg);
 
 	return msg_size;
 }
@@ -106,8 +224,10 @@ static unsigned int rpmsg_tty_write_room(struct tty_struct *tty)
 {
 	struct rpmsg_tty_port *cport = tty->driver_data;
 	int size;
+	struct rpmsg_device *rpdev;
 
-	size = rpmsg_get_mtu(cport->rpdev->ept);
+	rpdev = cport->rpdev;
+	size = cport->use_srtm_uart_protocol ? (cport->srtm_uart_msg_data_max_sz) : (rpmsg_get_mtu(rpdev->ept));
 	if (size < 0)
 		return 0;
 
@@ -173,7 +293,10 @@ static int rpmsg_tty_probe(struct rpmsg_device *rpdev)
 	struct rpmsg_tty_port *cport;
 	struct device *dev = &rpdev->dev;
 	struct device *tty_dev;
+	struct device_node *np;
+	struct imx_srtm_uart_data_structure *srtm_uart_data = NULL;
 	int ret;
+	char buf[64];
 
 	cport = rpmsg_tty_alloc_cport();
 	if (IS_ERR(cport))
@@ -181,6 +304,23 @@ static int rpmsg_tty_probe(struct rpmsg_device *rpdev)
 
 	tty_port_init(&cport->port);
 	cport->port.ops = &rpmsg_tty_port_ops;
+
+	cport->rpdev = rpdev;
+
+	srtm_uart_data = (struct imx_srtm_uart_data_structure *)rpdev->id.driver_data;
+	if (srtm_uart_data && srtm_uart_data->use_srtm_uart_protocol == true) {
+		cport->bus_id = 0xFF; /* mcore directly print the data that received from acore */
+		cport->use_srtm_uart_protocol = true;
+		snprintf(buf, sizeof(buf), "uart-rpbus-%d", cport->id);
+		np = of_find_node_by_name(NULL, buf);
+		if (np && of_device_is_compatible(np, "fsl,uart-rpbus") && of_device_is_available(np)) {
+			of_property_read_u32(np, "bus_id", &cport->bus_id); /* mcore will use the id as uart instance, then write data to the real uart instance */
+			of_property_read_u32(np, "flags", &cport->flags);
+		}
+		cport->srtm_uart_msg_data_max_sz = rpmsg_get_mtu(rpdev->ept) - (sizeof(struct srtm_uart_msg) - 1); /* 1: data[1] of struct srtm_uart_msg */
+	} else {
+		cport->use_srtm_uart_protocol = false;
+	}
 
 	tty_dev = tty_port_register_device(&cport->port, rpmsg_tty_driver,
 					   cport->id, dev);
@@ -190,9 +330,7 @@ static int rpmsg_tty_probe(struct rpmsg_device *rpdev)
 		return ret;
 	}
 
-	cport->rpdev = rpdev;
-
-	dev_set_drvdata(dev, cport);
+	dev_set_drvdata(dev, (void *)cport);
 
 	dev_dbg(dev, "New channel: 0x%x -> 0x%x: " RPMSG_TTY_NAME "%d\n",
 		rpdev->src, rpdev->dst, cport->id);
@@ -215,7 +353,8 @@ static void rpmsg_tty_remove(struct rpmsg_device *rpdev)
 }
 
 static struct rpmsg_device_id rpmsg_driver_tty_id_table[] = {
-	{ .name	= "rpmsg-tty" },
+	{ .name	= "srtm-uart-channel", .driver_data = (kernel_ulong_t)&imx_srtm_uart_data, },
+	{ .name	= "rpmsg-tty", .driver_data = (kernel_ulong_t)&rpmsg_tty_data, },
 	{ },
 };
 MODULE_DEVICE_TABLE(rpmsg, rpmsg_driver_tty_id_table);
@@ -284,5 +423,6 @@ module_init(rpmsg_tty_init);
 module_exit(rpmsg_tty_exit);
 
 MODULE_AUTHOR("Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>");
+MODULE_AUTHOR("Biwen Li <biwen.li@nxp.com>");
 MODULE_DESCRIPTION("remote processor messaging tty driver");
 MODULE_LICENSE("GPL v2");
