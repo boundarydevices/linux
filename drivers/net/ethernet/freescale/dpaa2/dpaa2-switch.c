4726cdce07411f4f12c901bc7d9d8111c8e35c42
Ioana Ciornei <ioana.ciornei@nxp.com>
2022-06-08 15:27:42 +0300

    dpaa2-switch: say that the bond device is offloaded as a bridge port
    We have to let the switchdev framework know if a specific bridge port is
    offloaded or not, even if that port is bond device.
    For this to happen, create the dpaa2_switch_port_to_bridge_port function
    which will determine the bridge port corresponding to a particulat dpaa2
    switch interface and use it in the switchdev_bridge_port_offload call.
    
    Signed-off-by: Ioana Ciornei <ioana.ciornei@nxp.com>

--- drivers/net/ethernet/freescale/dpaa2/dpaa2-switch.c
+++ drivers/net/ethernet/freescale/dpaa2/dpaa2-switch.c
@@ -2086,6 +2086,17 @@ static int dpaa2_switch_port_attr_set_event(struct net_device *netdev,
 static struct notifier_block dpaa2_switch_port_switchdev_nb;
 static struct notifier_block dpaa2_switch_port_switchdev_blocking_nb;
 
+static struct net_device *dpaa2_switch_port_to_bridge_port(struct ethsw_port_priv *port_priv)
+{
+	if (!port_priv->fdb->bridge_dev)
+		return NULL;
+
+	if (port_priv->lag)
+		return port_priv->lag->bond_dev;
+
+	return port_priv->netdev;
+}
+
 static int dpaa2_switch_port_bridge_join(struct net_device *netdev,
 					 struct net_device *upper_dev,
 					 struct netlink_ext_ack *extack)
@@ -2094,7 +2105,7 @@ static int dpaa2_switch_port_bridge_join(struct net_device *netdev,
 	struct ethsw_core *ethsw = port_priv->ethsw_data;
 	struct dpaa2_switch_fdb *old_fdb = port_priv->fdb;
 	struct ethsw_port_priv *other_port_priv;
-	struct net_device *other_dev;
+	struct net_device *other_dev, *brport_dev;
 	struct list_head *iter;
 	bool learn_ena;
 	int err;
@@ -2133,7 +2144,8 @@ static int dpaa2_switch_port_bridge_join(struct net_device *netdev,
 	if (err)
 		goto err_egress_flood;
 
-	err = switchdev_bridge_port_offload(netdev, netdev, port_priv,
+	brport_dev = dpaa2_switch_port_to_bridge_port(port_priv);
+	err = switchdev_bridge_port_offload(brport_dev, netdev, port_priv,
 					    &dpaa2_switch_port_switchdev_nb,
 					    &dpaa2_switch_port_switchdev_blocking_nb,
 					    false, extack);
