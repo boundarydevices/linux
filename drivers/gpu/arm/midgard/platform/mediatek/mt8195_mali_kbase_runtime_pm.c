c34cacc2598de1dcef00da9495d5e62d64585c28
Andy.Hsieh <Andy.Hsieh@mediatek.com>
2022-06-09 16:38:02 +0800

    gpu/arm: midgard: mediatek: Enable platform-specific devfreq_ops
    The original Mali devfreq flow is to set clock rate directly,
    while for MTK it has a specific clock setting flow. The config
    CONFIG_MALI_VALHALL_DEVFREQ is wrong, use CONFIG_MALI_DEVFREQ
    instead. Then, we can use correct clock setting flow.
    
    Signed-off-by: Andy.Hsieh <Andy.Hsieh@mediatek.com>

--- drivers/gpu/arm/midgard/platform/mediatek/mt8195_mali_kbase_runtime_pm.c
+++ drivers/gpu/arm/midgard/platform/mediatek/mt8195_mali_kbase_runtime_pm.c
@@ -414,7 +414,7 @@ static int mali_mfgsys_init(struct kbase_device *kbdev, struct mfg_base *mfg)
 				i, err);
 			return err;
 		}
-#ifdef CONFIG_MALI_VALHALL_DEVFREQ
+#ifdef CONFIG_MALI_DEVFREQ
 		kbdev->current_voltages[i] = volt;
 #endif
 	}
@@ -430,7 +430,7 @@ static int mali_mfgsys_init(struct kbase_device *kbdev, struct mfg_base *mfg)
 	return 0;
 }
 
-#ifdef CONFIG_MALI_VALHALL_DEVFREQ
+#ifdef CONFIG_MALI_DEVFREQ
 static void voltage_range_check(struct kbase_device *kbdev,
 				unsigned long *voltages)
 {
@@ -513,7 +513,7 @@ static int platform_init(struct kbase_device *kbdev)
 		return err;
 	}
 
-#ifdef CONFIG_MALI_VALHALL_DEVFREQ
+#ifdef CONFIG_MALI_DEVFREQ
 	kbdev->devfreq_ops.set_frequency = set_frequency;
 	kbdev->devfreq_ops.voltage_range_check = voltage_range_check;
 #endif
