77278e2dd68427fd89eff4a938b2c32290cd14d9
Dong Aisheng <aisheng.dong@nxp.com>
2022-06-17 17:30:11 +0800

    regmap: add option to disable debugfs
    The regmap core will create debugfs by default for each instance.
    However, it's fairly possible that some devices may not work properly
    with debugfs to dump registers due to it may be in suspend state
    (e.g. Power is off).
    
    Current regmap core does not support runtime pm for MMIO bus.
    Although there have been several retries [1] in community but finally
    didn't get accepted.
    
    This patch adds an option to allow drivers to claim no debugfs required
    due to possible platform limitations.
    
    1. Link: https://lkml.iu.edu/hypermail/linux/kernel/1204.0/01646.html
    Signed-off-by: Dong Aisheng <aisheng.dong@nxp.com>

--- include/linux/regmap.h
+++ include/linux/regmap.h
@@ -282,6 +282,7 @@ typedef void (*regmap_unlock)(void *);
  * @disable_locking: This regmap is either protected by external means or
  *                   is guaranteed not to be accessed from multiple threads.
  *                   Don't use any locking mechanisms.
+ * @disable_debugfs: Optional, don't create debugfs entries for this regmap.
  * @lock:	  Optional lock callback (overrides regmap's default lock
  *		  function, based on spinlock or mutex).
  * @unlock:	  As above for unlocking.
@@ -385,6 +386,7 @@ struct regmap_config {
 	bool (*readable_noinc_reg)(struct device *dev, unsigned int reg);
 
 	bool disable_locking;
+	bool disable_debugfs;
 	regmap_lock lock;
 	regmap_unlock unlock;
 	void *lock_arg;
