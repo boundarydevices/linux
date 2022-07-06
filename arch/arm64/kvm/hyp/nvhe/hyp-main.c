d238908806d9b51441ddf72aa8008bcec09b8467
Quentin Perret <qperret@google.com>
2022-07-06 18:16:55 +0000

    ANDROID: KVM: arm64: Allow non-RWX permissions in __pkvm_host_share_guest()
    In preparation for allowing RO mappings in non-protected stage-2
    page-tables with pKVM, refactor the __pkvm_host_share_guest() path to
    accept a caller-specified permission.
    
    Bug: 357781595
    Change-Id: Ib598bba312facb8e09bf995d0a671c74d521fec9
    Signed-off-by: Quentin Perret <qperret@google.com>

--- arch/arm64/kvm/hyp/nvhe/hyp-main.c
+++ arch/arm64/kvm/hyp/nvhe/hyp-main.c
@@ -1052,6 +1052,7 @@ static void handle___pkvm_host_map_guest(struct kvm_cpu_context *host_ctxt)
 {
 	DECLARE_REG(u64, pfn, host_ctxt, 1);
 	DECLARE_REG(u64, gfn, host_ctxt, 2);
+	DECLARE_REG(enum kvm_pgtable_prot, prot, host_ctxt, 3);
 	struct pkvm_hyp_vcpu *hyp_vcpu;
 	int ret = -EINVAL;
 
@@ -1070,7 +1071,7 @@ static void handle___pkvm_host_map_guest(struct kvm_cpu_context *host_ctxt)
 	if (pkvm_hyp_vcpu_is_protected(hyp_vcpu))
 		ret = __pkvm_host_donate_guest(pfn, gfn, hyp_vcpu);
 	else
-		ret = __pkvm_host_share_guest(pfn, gfn, hyp_vcpu);
+		ret = __pkvm_host_share_guest(pfn, gfn, hyp_vcpu, prot);
 out:
 	cpu_reg(host_ctxt, 1) =  ret;
 }
