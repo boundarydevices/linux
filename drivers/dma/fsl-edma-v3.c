05e491ae0ecaf7b31b13d802afba8664267f0708
Sherry Sun <sherry.sun@nxp.com>
2022-07-20 17:48:19 +0800

    LF-6209-1 Revert "LF-4508-1 dmaengine: fsl-edma-v3: add ESDA for eeop+cyclic"
    Since the EOP feature still has some issues in the edma-v3 driver, from
    long term perspective, edma-v3 driver need to restructure the code to
    better support EOP case.
    But now, let's disable the EOP feature temporary to unblock some uart
    and Android bluetooth test cases.
    
    This reverts commit 523c23e21e1a9ee80f2ce7b98d1b4390d765e2b7.
    Signed-off-by: Sherry Sun <sherry.sun@nxp.com>
    Reviewed-by: Jun Li <jun.li@nxp.com>
    Tested-by: Clark Wang <xiaoning.wang@nxp.com>
    Tested-by: Shengjiu Wang <shengjiu.wang@nxp.com>
    Tested-by: Junmeng Li <junmeng.li_1@nxp.com>
    Tested-by: Ondrej Lutera <ondrej.lutera@nxp.com>

--- drivers/dma/fsl-edma-v3.c
+++ drivers/dma/fsl-edma-v3.c
@@ -98,8 +98,8 @@
 #define EDMA_TCD_CSR_D_REQ		BIT(3)
 #define EDMA_TCD_CSR_E_SG		BIT(4)
 #define EDMA_TCD_CSR_E_LINK		BIT(5)
-#define EDMA_TCD_CSR_EEOP		BIT(6)
-#define EDMA_TCD_CSR_ESDA		BIT(7)
+#define EDMA_TCD_CSR_ACTIVE		BIT(6)
+#define EDMA_TCD_CSR_DONE		BIT(7)
 
 #define FSL_EDMA_BUSWIDTHS	(BIT(DMA_SLAVE_BUSWIDTH_1_BYTE) | \
 				BIT(DMA_SLAVE_BUSWIDTH_2_BYTES) | \
@@ -130,11 +130,6 @@ struct fsl_edma3_hw_tcd {
 	__le32	dlast_sga;
 	__le16	csr;
 	__le16	biter;
-	/*
-	 * Store Dest Address if EDMA_TCD_CSR_ESDA enabled which's used as
-	 * 'eeop + cyclic'.
-	 */
-	__le32  sda;
 };
 
 struct fsl_edma3_sw_tcd {
@@ -188,7 +183,6 @@ struct fsl_edma3_desc {
 	struct fsl_edma3_chan		*echan;
 	bool				iscyclic;
 	unsigned int			n_tcds;
-	u32				curidx;
 	struct fsl_edma3_sw_tcd		tcd[];
 };
 
@@ -445,16 +439,8 @@ static size_t fsl_edma3_desc_residue(struct fsl_edma3_chan *fsl_chan,
 	else
 		cur_addr = readl(addr + EDMA_TCD_DADDR);
 
-	/* skip the tcds before curidx in cyclic case */
-	if (edesc->iscyclic)
-		len -= edesc->curidx * le32_to_cpu(edesc->tcd[0].vtcd->nbytes)
-			* le16_to_cpu(edesc->tcd[0].vtcd->biter);
-
 	/* figure out the finished and calculate the residue */
-	for (i = edesc->curidx; i < fsl_chan->edesc->n_tcds; i++) {
-		if (dir == DMA_DEV_TO_MEM)
-			cur_addr = le32_to_cpu(edesc->tcd[i].vtcd->sda);
-
+	for (i = 0; i < fsl_chan->edesc->n_tcds; i++) {
 		size = le32_to_cpu(edesc->tcd[i].vtcd->nbytes)
 			* le16_to_cpu(edesc->tcd[i].vtcd->biter);
 		if (dir == DMA_MEM_TO_DEV)
@@ -463,15 +449,8 @@ static size_t fsl_edma3_desc_residue(struct fsl_edma3_chan *fsl_chan,
 			dma_addr = le32_to_cpu(edesc->tcd[i].vtcd->daddr);
 
 		len -= size;
-		if (cur_addr > dma_addr && cur_addr <= dma_addr + size) {
+		if (cur_addr >= dma_addr && cur_addr < dma_addr + size) {
 			len += dma_addr + size - cur_addr;
-			/*
-			 * mark curidx as the start point in the next time
-			 * fsl_edma3_desc_residue(called by fsl_edma3_tx_status
-			 * commonly per cyclic interrupt/callback)
-			 */
-			if (edesc->iscyclic)
-				edesc->curidx = (i + 1) % edesc->n_tcds;
 			break;
 		}
 	}
@@ -547,13 +526,11 @@ static void fsl_edma3_set_tcd_regs(struct fsl_edma3_chan *fsl_chan,
 
 static inline
 void fsl_edma3_fill_tcd(struct fsl_edma3_chan *fsl_chan,
-			struct fsl_edma3_sw_tcd *sw_tcd, u32 src, u32 dst,
+			struct fsl_edma3_hw_tcd *tcd, u32 src, u32 dst,
 			u16 attr, u16 soff, u32 nbytes, u32 slast, u16 citer,
 			u16 biter, u16 doff, u32 dlast_sga, bool major_int,
 			bool disable_req, bool enable_sg)
 {
-	struct fsl_edma3_hw_tcd *tcd = sw_tcd->vtcd;
-	u32 slast_sda = sw_tcd->ptcd + offsetof(struct fsl_edma3_hw_tcd, sda);
 	u16 csr = 0;
 
 	/*
@@ -583,11 +560,7 @@ void fsl_edma3_fill_tcd(struct fsl_edma3_chan *fsl_chan,
 	}
 
 	tcd->nbytes = cpu_to_le32(EDMA_TCD_NBYTES_NBYTES(nbytes));
-
-	if (fsl_chan->is_rxchan)
-		tcd->slast = cpu_to_le32(EDMA_TCD_SLAST_SLAST(slast_sda));
-	else
-		tcd->slast = cpu_to_le32(EDMA_TCD_SLAST_SLAST(slast));
+	tcd->slast = cpu_to_le32(EDMA_TCD_SLAST_SLAST(slast));
 
 	tcd->citer = cpu_to_le16(EDMA_TCD_CITER_CITER(citer));
 	tcd->doff = cpu_to_le16(EDMA_TCD_DOFF_DOFF(doff));
@@ -605,7 +578,7 @@ void fsl_edma3_fill_tcd(struct fsl_edma3_chan *fsl_chan,
 		csr |= EDMA_TCD_CSR_E_SG;
 
 	if (fsl_chan->is_rxchan)
-		csr |= EDMA_TCD_CSR_EEOP | EDMA_TCD_CSR_ESDA;
+		csr |= EDMA_TCD_CSR_ACTIVE;
 
 	if (fsl_chan->is_sw)
 		csr |= EDMA_TCD_CSR_START;
@@ -717,13 +690,11 @@ static struct dma_async_tx_descriptor *fsl_edma3_prep_dma_cyclic(
 			major_int = false;
 		}
 
-		fsl_edma3_fill_tcd(fsl_chan, &fsl_desc->tcd[i], src_addr,
+		fsl_edma3_fill_tcd(fsl_chan, fsl_desc->tcd[i].vtcd, src_addr,
 				dst_addr, fsl_chan->fsc.attr, soff, nbytes, 0,
 				iter, iter, doff, last_sg, major_int, false, true);
 		dma_buf_next += period_len;
 	}
-
-	fsl_desc->curidx = 0;
 
 	return vchan_tx_prep(&fsl_chan->vchan, &fsl_desc->vdesc, flags);
 }
@@ -794,13 +765,13 @@ static struct dma_async_tx_descriptor *fsl_edma3_prep_slave_sg(
 		iter = sg_dma_len(sg) / nbytes;
 		if (i < sg_len - 1) {
 			last_sg = fsl_desc->tcd[(i + 1)].ptcd;
-			fsl_edma3_fill_tcd(fsl_chan, &fsl_desc->tcd[i],
+			fsl_edma3_fill_tcd(fsl_chan, fsl_desc->tcd[i].vtcd,
 					src_addr, dst_addr, fsl_chan->fsc.attr,
 					soff, nbytes, 0, iter, iter, doff,
 					last_sg, false, false, true);
 		} else {
 			last_sg = 0;
-			fsl_edma3_fill_tcd(fsl_chan, &fsl_desc->tcd[i],
+			fsl_edma3_fill_tcd(fsl_chan, fsl_desc->tcd[i].vtcd,
 					src_addr, dst_addr, fsl_chan->fsc.attr,
 					soff, nbytes, 0, iter, iter, doff,
 					last_sg, true, true, false);
@@ -840,7 +811,7 @@ static struct dma_async_tx_descriptor *fsl_edma3_prep_memcpy(
 	fsl_chan->is_sw = true;
 
 	/* To match with copy_align and max_seg_size so 1 tcd is enough */
-	fsl_edma3_fill_tcd(fsl_chan, &fsl_desc->tcd[0], dma_src, dma_dst,
+	fsl_edma3_fill_tcd(fsl_chan, fsl_desc->tcd[0].vtcd, dma_src, dma_dst,
 			EDMA_TCD_ATTR_SSIZE_64BYTE | EDMA_TCD_ATTR_DSIZE_64BYTE,
 			64, len, 0, 1, 1, 64, 0, true, true, false);
 
