5af3477e7fb2015066f68dfe5766e8d74d5b234a
Jacky Bai <ping.bai@nxp.com>
2022-07-27 14:21:57 +0800

    LF-6681-05 soc: imx: update lpm code to support sys dvfs on imx8ulp
    Update the lpm code to support system dvfs for core/bus frequency
    scaling and voltage change when sys_dvfs_enabled is true.
    
    if sys_dvfs_enabled is false, only do ddr frequency scaling just as before
    
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>

--- drivers/soc/imx/imx8ulp_lpm.c
+++ drivers/soc/imx/imx8ulp_lpm.c
@@ -22,9 +22,18 @@ static struct clk *dram_sel;
 static struct clk *dram_div;
 static struct clk *pll4;
 static struct clk *frosc;
+static struct clk *spll2;
+static struct clk *a35_sel;
+static struct clk *nic_sel;
+static struct clk *lpav_axi_sel;
+static struct clk *nic_old_parent;
+static struct clk *a35_old_parent;
+static struct clk *lpav_old_parent;
+static struct clk *pll4;
 
 static bool lpm_enabled = false;
 static bool bypass_enabled = false;
+static bool sys_dvfs_enabled = false;
 static struct device *imx8ulp_lpm_dev;
 static int num_fsp;
 
@@ -37,7 +46,7 @@ static int scaling_dram_freq(unsigned int fsp_index)
 
 	/* need to check the return value ?*/ 
 	arm_smccc_smc(FSL_SIP_DDR_DVFS, fsp_index, num_cpus,
-		0, 0, 0, 0, 0, &res);
+		sys_dvfs_enabled, 0, 0, 0, 0, &res);
 
 	local_irq_enable();
 
@@ -54,6 +63,91 @@ static int scaling_dram_freq(unsigned int fsp_index)
 	return 0;
 }
 
+static void sys_freq_scaling(bool enter)
+{
+	int ret;
+	if (enter) {
+		if (sys_dvfs_enabled) {
+			/*scaling down APD side NIC frequency, switch to fro 192MHz */
+			nic_old_parent = clk_get_parent(nic_sel);
+			lpav_old_parent = clk_get_parent(lpav_axi_sel);
+			a35_old_parent = clk_get_parent(a35_sel);
+
+			ret = clk_set_parent(nic_sel, frosc);
+			if (ret)
+				pr_err("failed to change nic clock parent:%d\n", ret);
+
+			ret = clk_set_parent(lpav_axi_sel, frosc);
+			if (ret)
+				pr_err("failed to change lpav axi clock parent:%d\n", ret);
+
+			/*
+			 * scaling down the A35 core frequency, switch to fro 192MHz,
+			 * then, change SPLL2 frequency to 500MHz.
+			 */
+			ret = clk_set_parent(a35_sel, frosc);
+			if (ret)
+				pr_err("failed to change a35 clock parent:%d\n", ret);
+
+			/* change SPLL2 to UD 500MHz */
+			ret = clk_set_rate(spll2, 500000000);
+			if (ret)
+				pr_err("failed to set spll2 frequency:%d\n", ret);
+
+			/* switch A35 from frosc to spll2 */
+			ret = clk_set_parent(a35_sel, a35_old_parent);
+			if (ret)
+				pr_err("failed to change a35 clock parent back:%d\n", ret);
+		}
+
+		/*
+		 * scaling down the ddr frequency and change the BUCK3
+		 * voltage to ND 1.0V if system level dvfs is enabled.
+		 */
+		scaling_dram_freq(DDR_FSP_LOW);
+
+		pr_info("DDR enter low frequency mode\n");
+	} else {
+		/* prepare enable PLL4 first */
+		clk_prepare_enable(pll4);
+		/*
+		 * exit LPM mode, increase the BUCK3 voltage to OD if system level
+		 * dvfs enabled, scaling up the DDR frequency
+		 */
+		scaling_dram_freq(DDR_FSP_HIGH);
+
+		if (sys_dvfs_enabled) {
+			ret = clk_set_parent(a35_sel, frosc);
+			if (ret)
+				pr_err("failed to change a35 clock parent:%d\n", ret);
+
+			/* change SPLL2 to OD 960MHz */
+			ret = clk_set_rate(spll2, 960000000);
+			if (ret)
+				pr_err("failed to set spll2 frequency:%d\n", ret);
+
+			/* switch A35 from frosc to spll2 */
+			ret = clk_set_parent(a35_sel, a35_old_parent);
+			if (ret)
+				pr_err("failed to change a35 clock parent back:%d\n", ret);
+
+			/* scaling up the NIC frequency */
+			ret = clk_set_parent(nic_sel, nic_old_parent);
+			if (ret)
+				pr_err("failed to change nic clock parent:%d\n", ret);
+
+			/* scaling up lpav axi frequency */
+			ret = clk_set_parent(lpav_axi_sel, lpav_old_parent);
+			if (ret)
+				pr_err("failed to change lpav axi clock parent:%d\n", ret);
+		}
+
+		/* unprepare pll4 after clock tree info is correct */
+		clk_disable_unprepare(pll4);
+
+		pr_info("DDR Exit from low frequency mode\n");
+	}
+}
 static ssize_t lpm_enable_show(struct device *dev, struct device_attribute *attr,
 				char *buf)
 {
@@ -74,18 +168,14 @@ static ssize_t lpm_enable_store(struct device *dev, struct device_attribute *att
 		pr_info("DDR DFS only support with both F1 & F2 enabled\n");
 
 
-	if (strncmp(buf, "1", 1) == 0) {
-		scaling_dram_freq(DDR_FSP_LOW);
+	if ((strncmp(buf, "1", 1) == 0) && !lpm_enabled) {
+		sys_freq_scaling(true);
 
 		lpm_enabled = true;
-		pr_info("DDR enter low frequency mode\n");
 	} else if (strncmp(buf, "0", 1) == 0) {
-		if (lpm_enabled) {
-			/* exit LPM mode */
-			scaling_dram_freq(DDR_FSP_HIGH);
+		if (lpm_enabled)
+			sys_freq_scaling(false);
 
-			pr_info("DDR Exit from low frequency mode\n");
-		}
 		lpm_enabled = false;
 	}
 
@@ -102,9 +192,9 @@ static int imx8ulp_lpm_pm_notify(struct notifier_block *nb, unsigned long event,
 		return NOTIFY_OK;
 
 	if (event == PM_SUSPEND_PREPARE)
-		scaling_dram_freq(DDR_FSP_HIGH);
+		sys_freq_scaling(false);
 	else if (event == PM_POST_SUSPEND)
-		scaling_dram_freq(DDR_FSP_LOW);
+		sys_freq_scaling(true);
 
 	return NOTIFY_OK;
 }
@@ -132,12 +222,26 @@ static int imx8ulp_lpm_probe(struct platform_device *pdev)
 	if (num_fsp != DDR_DFS_FSP_NUM_MIN)
 		return -ENODEV;
 
+	/*
+	 * check if system level dvfs is enabled, only when this is enabled,
+	 * we can do system level frequency scaling and voltage change dynamically
+	 */
+	sys_dvfs_enabled = of_property_read_bool(pdev->dev.of_node, "sys-dvfs-enabled");
+
 	/* get the necessary clocks */
 	dram_sel = devm_clk_get(&pdev->dev, "ddr_sel");
 	dram_div = devm_clk_get(&pdev->dev, "ddr_div");
 	pll4 = devm_clk_get(&pdev->dev, "pll4");
 	frosc = devm_clk_get(&pdev->dev, "frosc");
-	if (IS_ERR(dram_sel) || IS_ERR(dram_div) || IS_ERR(pll4) || IS_ERR(frosc))
+	/* below clock is used for system level od/nd mode swithing */
+	nic_sel = devm_clk_get(&pdev->dev, "nic_sel");
+	a35_sel = devm_clk_get(&pdev->dev, "a35_sel");
+	spll2 = devm_clk_get(&pdev->dev, "spll2");
+	lpav_axi_sel = devm_clk_get(&pdev->dev, "lpav_axi_sel");
+	pll4 = devm_clk_get(&pdev->dev, "pll4");
+	if (IS_ERR(dram_sel) || IS_ERR(dram_div) || IS_ERR(pll4) || IS_ERR(frosc) ||
+	    IS_ERR(nic_sel) || IS_ERR(a35_sel) || IS_ERR(spll2) || IS_ERR(lpav_axi_sel) ||
+	    IS_ERR(pll4))
 		dev_err(&pdev->dev, "Get clocks failed\n");
 
 	/* create the sysfs file */
