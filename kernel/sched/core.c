e1d313a431d2c0588f2404486c32057fdd278aa2
Stephen Dickey <quic_dickey@quicinc.com>
2022-07-29 12:38:11 -0700

    ANDROID: GKI: vendor code needs __balance_callbacks access
    Vendor module code needs to ensure that pushes to the balance
    callback queue are handled prior to releasing rq locks.
    Export __balance_callbacks to facilitate implementation of Qualcomm's
    proprietary load tracking scheme, WALT via stated vendor module.
    
    Bug: 241223356
    Change-Id: I86a3f5cb6cc7b97d87bd2db3e11cb7a8d99d4aef
    Signed-off-by: Stephen Dickey <quic_dickey@quicinc.com>
    Signed-off-by: Maria Yu <quic_aiquny@quicinc.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5122,10 +5122,11 @@ struct balance_callback *splice_balance_callbacks(struct rq *rq)
 	return __splice_balance_callbacks(rq, true);
 }
 
-static void __balance_callbacks(struct rq *rq)
+void __balance_callbacks(struct rq *rq)
 {
 	do_balance_callbacks(rq, __splice_balance_callbacks(rq, false));
 }
+EXPORT_SYMBOL_GPL(__balance_callbacks);
 
 void balance_callbacks(struct rq *rq, struct balance_callback *head)
 {
