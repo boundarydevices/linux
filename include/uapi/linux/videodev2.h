09ecf267073ff7c3517f3526210214b07e4c3303
Xu Mao <mao.xu@nxp.com>
2022-08-03 17:38:11 +0800

    [PATCH] media: videobuf2: add V4L2_BUF_FLAG_HEADERS_ONLY flag
     By setting the V4L2_BUF_FLAG_HEADERS_ONLY flag,
     hint the vb2 only contains stream header,
     but does not contain any frame data.
    
     This flag needs to be used when header mode is set to
     V4L2_MPEG_VIDEO_HEADER_MODE_SEPARATE.
    
    Change-Id: Id6cf30cd59109f31845c3ad74562703c4a8fe390
    Signed-off-by: Ming Qian <ming.qian@nxp.com>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -1155,6 +1155,8 @@ static inline __u64 v4l2_timeval_to_ns(const struct timeval *tv)
 #define V4L2_BUF_FLAG_TSTAMP_SRC_SOE		0x00010000
 /* mem2mem encoder/decoder */
 #define V4L2_BUF_FLAG_LAST			0x00100000
+/* Buffer only contains codec header */
+#define V4L2_BUF_FLAG_HEADERS_ONLY		0x00200000
 /* request_fd is valid */
 #define V4L2_BUF_FLAG_REQUEST_FD		0x00800000
 
