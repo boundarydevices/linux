4b6cfe4c6136c04bac6042fe0b84a199678ebdc9
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2022-09-20 06:32:16 +0000

    BACKPORT: ASoC: soc.h: remove num_cpus/codecs
    Current rtd has both dai_link pointer (A) and num_cpus/codecs (B).
    
    (A)	rtd->dai_link	= dai_link;
    (B)	rtd->num_cpus	= dai_link->num_cpus;
    (B)	rtd->num_codecs	= dai_link->num_codecs;
    
    But, we can get num_cpus/codecs (B) via dai_link (A).
    This means we don't need to keep num_cpus/codecs on rtd.
    This patch removes these.
    
    Change-Id: I3ac372c6373d7758a0e38b4792b4325b6c1cf51b
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://lore.kernel.org/r/87sfkmv9n3.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/soc-generic-dmaengine-pcm.c
+++ sound/soc/soc-generic-dmaengine-pcm.c
@@ -50,7 +50,7 @@ int snd_dmaengine_pcm_prepare_slave_config(struct snd_pcm_substream *substream,
 	struct snd_dmaengine_dai_dma_data *dma_data;
 	int ret;
 
-	if (rtd->num_cpus > 1) {
+	if (rtd->dai_link->num_cpus > 1) {
 		dev_err(rtd->dev,
 			"%s doesn't support Multi CPU yet\n", __func__);
 		return -EINVAL;
@@ -111,7 +111,7 @@ dmaengine_pcm_set_runtime_hwparams(struct snd_soc_component *component,
 	struct snd_dmaengine_dai_dma_data *dma_data;
 	struct snd_pcm_hardware hw;
 
-	if (rtd->num_cpus > 1) {
+	if (rtd->dai_link->num_cpus > 1) {
 		dev_err(rtd->dev,
 			"%s doesn't support Multi CPU yet\n", __func__);
 		return -EINVAL;
@@ -184,7 +184,7 @@ static struct dma_chan *dmaengine_pcm_compat_request_channel(
 	struct snd_dmaengine_dai_dma_data *dma_data;
 	dma_filter_fn fn = NULL;
 
-	if (rtd->num_cpus > 1) {
+	if (rtd->dai_link->num_cpus > 1) {
 		dev_err(rtd->dev,
 			"%s doesn't support Multi CPU yet\n", __func__);
 		return NULL;
