99302591152615d191106d9fb9fbe13541ae4d6d
Kshitiz Varshney <kshitiz.varshney@nxp.com>
2022-10-08 15:37:31 +0200

    DCP: Trusted keys backed by DCP
    This patch adds trusted hardware key support for DCP crypto driver.
    
    Signed-off-by: Kshitiz Varshney <kshitiz.varshney@nxp.com>
    Reviewed by: Pankaj Gupta <pankaj.gupta@nxp.com>

---
+++ include/keys/trusted_dcp.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-3.0-only */
+/*
+ * Copyright (C) 2022 NXP
+ * Author:- Kshitiz Varshney <kshitiz.varshney@nxp.com>
+ */
+
+#ifndef __DCP_TRUSTED_KEY_H__
+#define __DCP_TRUSTED_KEY_H__
+
+
+extern struct trusted_key_ops trusted_key_dcp_ops;
+
+#endif
