a9184230dffd04f8c9159028519f6369553f5481
Carter Chen <carter.chen@infineon.com>
2022-10-13 06:23:22 -0500

    non-upstream: adding vendor_cmds are with IFX_OUI
    Fixes: SWLINUX-2939
    
    Signed-off-by: Carter Chen <carter.chen@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/vendor_ifx.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/vendor_ifx.h
@@ -22,17 +22,43 @@
  * @IFX_VENDOR_SCMD_MAX: This acts as a the tail of cmds list.
  *      Make sure it located at the end of the list.
  */
+#define SCMD(_CMD)	IFX_VENDOR_SCMD_##_CMD
+#define IFX_SUBCMD(_CMD, _FLAGS, _POLICY, _FN) \
+	{	\
+		.vendor_id = OUI_IFX,	\
+		.subcmd = SCMD(_CMD)	\
+	},	\
+	.flags = (_FLAGS),	\
+	.policy = (_POLICY),	\
+	.doit = (_FN)
+
 enum ifx_nl80211_vendor_subcmds {
-	/* TODO: IFX Vendor subcmd enum IDs between 1-15 are reserved
+	/* TODO: IFX Vendor subcmd enum IDs between 2-15 are reserved
 	 * to be filled later with BRCM Vendor subcmds that are
 	 * already used by IFX.
+	 *
+	 * @IFX_VENDOR_SCMD_DCMD: equivilent to SCMD_PRIV_STR in DHD.
+	 *
+	 * @IFX_VENDOR_SCMD_FRAMEBURST: align ID to DHD.
 	 */
-	IFX_VENDOR_SCMD_UNSPEC		= 0,
-	/* Reserved 1-5 */
-	IFX_VENDOR_SCMD_FRAMEBURST	= 6,
-	/* Reserved 7-15 */
-	IFX_VENDOR_SCMD_DCMD		= 16,
-	IFX_VENDOR_SCMD_MAX		= 17
+	/* Reserved 2-5 */
+	SCMD(UNSPEC)		= 0,
+	SCMD(DCMD)		= 1,
+	SCMD(RSV2)		= 2,
+	SCMD(RSV3)		= 3,
+	SCMD(RSV4)		= 4,
+	SCMD(RSV5)		= 5,
+	SCMD(FRAMEBURST)	= 6,
+	SCMD(RSV7)		= 7,
+	SCMD(RSV8)		= 8,
+	SCMD(RSV9)		= 9,
+	SCMD(RSV10)		= 10,
+	SCMD(RSV11)		= 11,
+	SCMD(RSV12)		= 12,
+	SCMD(RSV13)		= 13,
+	SCMD(RSV14)		= 14,
+	SCMD(RSV15)		= 15,
+	SCMD(MAX)		= 16
 };
 
 /* enum ifx_vendor_attr - IFX nl80211 vendor attributes
