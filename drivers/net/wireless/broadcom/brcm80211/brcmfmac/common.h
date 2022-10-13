b5776766277bc102d2d5d35dd8fdb8c4c1fb40e9
Gokul Sivakumar <gokulkumar.sivakumar@infineon.com>
2022-10-13 04:41:42 -0500

    brcmfmac: introduce a module parameter to disable the 6GHz Operation
    Add a kernel module parameter "disable_6ghz" to disable the operation of
    the STA and SoftAP in the 6GHz band. By default, this module parameter will
    be set to zero, so 6GHz operation will be disabled during kernel module
    load only when explicity setting "disable_6ghz=1".
    
    Signed-off-by: Gokul Sivakumar <gokulkumar.sivakumar@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.h
@@ -42,6 +42,7 @@ extern struct brcmf_mp_global_t brcmf_mp_global;
  * @ignore_probe_fail: Ignore probe failure.
  * @trivial_ccode_map: Assume firmware uses ISO3166 country codes with rev 0
  * @fw_ap_select: Allow FW to select AP.
+ * @disable_6ghz: Disable 6GHz operation
  * @country_codes: If available, pointer to struct for translating country codes
  * @bus: Bus specific platform data. Only SDIO at the mmoment.
  */
@@ -56,6 +57,7 @@ struct brcmf_mp_device {
 	bool		ignore_probe_fail;
 	bool		trivial_ccode_map;
 	bool		fw_ap_select;
+	bool		disable_6ghz;
 	struct brcmfmac_pd_cc *country_codes;
 	const char	*board_type;
 	unsigned char	mac[ETH_ALEN];
