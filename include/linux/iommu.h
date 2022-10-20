43905b4095b55c8b94a95ad00b41ab77c0c82b35
Zhipeng Wang <zhipeng.wang_1@nxp.com>
2022-10-20 18:10:01 +0800

    MA-20763-5 Revert "iommu: Introduce a callback to struct iommu_resv_region"
    This reverts commit 5011a6fbfb3b36b8b304a1a87a55788d7265325c.
    
    It breaks the Android GKI kernel abi, and is not needed for Android
    devices, so revert it for now.
    
    Change-Id: Ifd4ea3266a7658e16eb3b1b180c3159efffb5819

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -145,7 +145,6 @@ enum iommu_resv_type {
  * @length: Length of the region in bytes
  * @prot: IOMMU Protection flags (READ/WRITE/...)
  * @type: Type of the reserved region
- * @free: Callback to free associated memory allocations
  */
 struct iommu_resv_region {
 	struct list_head	list;
@@ -153,7 +152,6 @@ struct iommu_resv_region {
 	size_t			length;
 	int			prot;
 	enum iommu_resv_type	type;
-	void (*free)(struct device *dev, struct iommu_resv_region *region);
 };
 
 struct iommu_iort_rmr_data {
