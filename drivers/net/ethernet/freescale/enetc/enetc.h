f4123c2a941c0837048768a86f66e7c21018e77f
Zhipeng Wang <zhipeng.wang_1@nxp.com>
2022-11-01 13:14:30 +0800

    MA-20860 Revert several patches related to enetc from 13764483abd4 to 393a61ecb9db
    The patches are only needed on a Layerscape
    platform and break the ABI, so revert them.
    
    This reverts commit 13764483abd4b8777cbf0549cab54f740852e8c4.
    
    Change-Id: I72371c76aed8600ad1addf8a66914c4047948780

--- drivers/net/ethernet/freescale/enetc/enetc.h
+++ drivers/net/ethernet/freescale/enetc/enetc.h
@@ -25,7 +25,6 @@ struct enetc_tx_swbd {
 	union {
 		struct sk_buff *skb;
 		struct xdp_frame *xdp_frame;
-		struct xdp_buff *xsk_buff;
 	};
 	dma_addr_t dma;
 	struct page *page;	/* valid only if is_xdp_tx */
@@ -38,7 +37,6 @@ struct enetc_tx_swbd {
 	u8 is_eof:1;
 	u8 is_xdp_tx:1;
 	u8 is_xdp_redirect:1;
-	u8 is_xsk:1;
 	u8 qbv_en:1;
 };
 
@@ -51,11 +49,10 @@ struct enetc_tx_swbd {
 	(SKB_WITH_OVERHEAD(ENETC_RXB_TRUESIZE) - XDP_PACKET_HEADROOM)
 
 struct enetc_rx_swbd {
-	struct xdp_buff *xsk_buff;
-	struct page *page;
 	dma_addr_t dma;
-	enum dma_data_direction dir;
+	struct page *page;
 	u16 page_offset;
+	enum dma_data_direction dir;
 	u16 len;
 };
 
@@ -82,7 +79,6 @@ struct enetc_ring_stats {
 
 struct enetc_xdp_data {
 	struct xdp_rxq_info rxq;
-	struct xsk_buff_pool *xsk_pool;
 	struct bpf_prog *prog;
 	int xdp_tx_in_flight;
 };
@@ -90,7 +86,6 @@ struct enetc_xdp_data {
 #define ENETC_RX_RING_DEFAULT_SIZE	2048
 #define ENETC_TX_RING_DEFAULT_SIZE	2048
 #define ENETC_DEFAULT_TX_WORK		(ENETC_TX_RING_DEFAULT_SIZE / 2)
-#define ENETC_XSK_TX_BATCH		ENETC_DEFAULT_TX_WORK
 
 struct enetc_bdr {
 	struct device *dev; /* for DMA mapping */
@@ -342,7 +337,6 @@ enum enetc_active_offloads {
 
 enum enetc_flags_bit {
 	ENETC_TX_ONESTEP_TSTAMP_IN_PROGRESS = 0,
-	ENETC_DOWN,
 };
 
 /* interrupt coalescing modes */
@@ -430,10 +424,9 @@ struct net_device_stats *enetc_get_stats(struct net_device *ndev);
 void enetc_set_features(struct net_device *ndev, netdev_features_t features);
 int enetc_ioctl(struct net_device *ndev, struct ifreq *rq, int cmd);
 int enetc_setup_tc_mqprio(struct net_device *ndev, void *type_data);
-int enetc_setup_bpf(struct net_device *ndev, struct netdev_bpf *bpf);
+int enetc_setup_bpf(struct net_device *dev, struct netdev_bpf *xdp);
 int enetc_xdp_xmit(struct net_device *ndev, int num_frames,
 		   struct xdp_frame **frames, u32 flags);
-int enetc_xsk_wakeup(struct net_device *dev, u32 queue, u32 flags);
 
 /* ethtool */
 void enetc_set_ethtool_ops(struct net_device *ndev);
