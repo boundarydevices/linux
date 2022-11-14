fancyfly/linux-test

MLK-11395-3 regulator: consumer: add new event macros
Add two new regulator events macro 'REGULATOR_EVENT_PRE_DO_ENABLE'
and 'REGULATOR_EVENT_PRE_DO_DISABLE', since some gpc operations
should be required when MIPI PHY is powered on/off.

Signed-off-by: Fancy Fang <chen.fang@freescale.com>

include/linux/regulator/consumer.h
@@ -119,6 +119,8 @@ struct regmap;
 #define REGULATOR_EVENT_ABORT_VOLTAGE_CHANGE	0x200
 #define REGULATOR_EVENT_PRE_DISABLE		0x400
 #define REGULATOR_EVENT_ABORT_DISABLE		0x800
+#define REGULATOR_EVENT_PRE_DO_ENABLE		0x1000
+#define REGULATOR_EVENT_PRE_DO_DISABLE		0x2000
 
 /**
  * struct pre_voltage_change_data - Data sent with PRE_VOLTAGE_CHANGE event

boundarydevices/linux

Signed-off-by: Arulpandiyan Vadivel <arulpandiyan_vadivel@mentor.com>

include/linux/regulator/consumer.h
@@ -129,6 +129,8 @@ struct regulator_dev;
 #define REGULATOR_EVENT_OVER_VOLTAGE_WARN	0x8000
 #define REGULATOR_EVENT_OVER_TEMP_WARN		0x10000
 #define REGULATOR_EVENT_WARN_MASK		0x1E000
+#define REGULATOR_EVENT_PRE_DO_ENABLE		0x20000
+#define REGULATOR_EVENT_PRE_DO_DISABLE		0x40000
 
 /*
  * Regulator errors that can be queried using regulator_get_error_flags