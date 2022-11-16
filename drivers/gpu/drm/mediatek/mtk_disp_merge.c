8d76536b79a8395564ff740492cd280640c72c28
Stuart Lee <stuart.lee@mediatek.com>
2022-11-16 13:51:39 +0800

    GENIO: drm/mediatek: fix external display flashing while layer off
    Fix external display flashing while layer off:
    Only need to do VDO SW reset while cmdq is not support.
    e.g, crtc disabled.
    No need to do sw reset while layer off.
    Add cmdq-pkt as judgement condition while call vdo sw reset.
    
    Change-Id: I0851ad49bb64c88a98534b0e9540dba2b316e87a
    Signed-off-by: Stuart Lee <stuart.lee@mediatek.com>

--- drivers/gpu/drm/mediatek/mtk_disp_merge.c
+++ drivers/gpu/drm/mediatek/mtk_disp_merge.c
@@ -105,7 +105,7 @@ void mtk_merge_stop_cmdq(struct device *dev, struct cmdq_pkt *cmdq_pkt)
 	mtk_ddp_write(cmdq_pkt, 0, &priv->cmdq_reg, priv->regs,
 		      DISP_REG_MERGE_CTRL);
 
-	if (priv->async_clk)
+	if (priv->async_clk && !cmdq_pkt)
 		reset_control_reset(priv->reset_ctl);
 }
 
