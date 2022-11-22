2613ae08a11706b2111132a298d06ac8f3ec2fbe
Gary Bisson <gary.bisson@boundarydevices.com>
2022-11-22 14:21:15 +0100

    Revert "MA-19859 Gate off core and bus clock slice"
    This reverts commit ff4e284bff7aefb4b1e1a0aa6b1c0e23bdae3c48.

--- drivers/clk/imx/clk-composite-8m.c
+++ drivers/clk/imx/clk-composite-8m.c
@@ -220,7 +220,6 @@ struct clk_hw *imx8m_clk_hw_composite_flags(const char *name,
 	struct clk_mux *mux = NULL;
 	const struct clk_ops *divider_ops;
 	const struct clk_ops *mux_ops;
-	u32 val;
 
 	mux = kzalloc(sizeof(*mux), GFP_KERNEL);
 	if (!mux)
@@ -243,34 +242,18 @@ struct clk_hw *imx8m_clk_hw_composite_flags(const char *name,
 		div->width = PCG_CORE_DIV_WIDTH;
 		divider_ops = &clk_divider_ops;
 		mux_ops = &imx8m_clk_composite_mux_ops;
-		if (!(flags & CLK_IS_CRITICAL) && !(imx_src_is_m4_enabled() && m4_lpa_required(name))) {
-			val = readl(reg);
-			val &= ~BIT(PCG_CGC_SHIFT);
-			writel(val, reg);
-		}
 	} else if (composite_flags & IMX_COMPOSITE_BUS) {
 		div->shift = PCG_PREDIV_SHIFT;
 		div->width = PCG_PREDIV_WIDTH;
 		divider_ops = &imx8m_clk_composite_divider_ops;
 		mux_ops = &imx8m_clk_composite_mux_ops;
-		if (!(flags & CLK_IS_CRITICAL) && !(imx_src_is_m4_enabled() && m4_lpa_required(name))) {
-			val = readl(reg);
-			val &= ~BIT(PCG_CGC_SHIFT);
-			writel(val, reg);
-		}
 	} else {
 		div->shift = PCG_PREDIV_SHIFT;
 		div->width = PCG_PREDIV_WIDTH;
 		divider_ops = &imx8m_clk_composite_divider_ops;
 		mux_ops = &clk_mux_ops;
-		if (!(composite_flags & IMX_COMPOSITE_FW_MANAGED)){
+		if (!(composite_flags & IMX_COMPOSITE_FW_MANAGED))
 			flags |= CLK_SET_PARENT_GATE;
-			if (!(flags & CLK_IS_CRITICAL) && !(imx_src_is_m4_enabled() && m4_lpa_required(name))) {
-				val = readl(reg);
-				val &= ~BIT(PCG_CGC_SHIFT);
-				writel(val, reg);
-			}
-		}
 	}
 
 	div->lock = &imx_ccm_lock;
