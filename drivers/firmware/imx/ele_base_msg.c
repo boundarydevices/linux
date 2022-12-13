6bc54657e1fb6c34a446dd745535b951eceacd5d
Gaurav Jain <gaurav.jain@nxp.com>
2022-12-13 14:42:02 +0530

    LF-7786-2: firmware: imx: ele: add base api support to save and restore IMEM
    during suspend this commands must be sent with EXPORT to save the IMEM.
    during resume this commands must be sent with IMPORT to retrieve the IMEM.
    
    Signed-off-by: Gaurav Jain <gaurav.jain@nxp.com>
    Reviewed-by: Pankaj Gupta <pankaj.gupta@nxp.com>
    Reviewed-by: Varun Sethi <v.sethi@nxp.com>

--- drivers/firmware/imx/ele_base_msg.c
+++ drivers/firmware/imx/ele_base_msg.c
@@ -33,6 +33,18 @@ static int plat_fill_cmd_msg_hdr(struct mu_hdr *hdr, uint8_t cmd, uint32_t len)
 	return err;
 }
 
+static u32 plat_add_msg_crc(uint32_t *msg, uint32_t msg_len)
+{
+	uint32_t i;
+	uint32_t crc = 0;
+	uint32_t nb_words = msg_len / (uint32_t)sizeof(uint32_t);
+
+	for (i = 0; i < nb_words - 1; i++)
+		crc ^= *(msg + i);
+
+	return crc;
+}
+
 int imx_ele_msg_send_rcv(struct ele_mu_priv *priv)
 {
 	unsigned int wait;
@@ -175,6 +187,47 @@ int ele_ping(void)
 	return -EAGAIN;
 }
 EXPORT_SYMBOL_GPL(ele_ping);
+
+int ele_service_swap(phys_addr_t addr, u32 addr_size, u16 flag)
+{
+	struct ele_mu_priv *priv;
+	int ret;
+	unsigned int tag, command, size, ver, status;
+
+	ret = get_ele_mu_priv(&priv);
+	if (ret)
+		return ret;
+
+	ret = plat_fill_cmd_msg_hdr((struct mu_hdr *)&priv->tx_msg.header,
+				    ELE_SERVICE_SWAP_REQ, 24);
+	if (ret)
+		return ret;
+
+	priv->tx_msg.data[0] = flag;
+	priv->tx_msg.data[1] = addr_size;
+	priv->tx_msg.data[2] = 0x0;
+	priv->tx_msg.data[3] = lower_32_bits(addr);
+	priv->tx_msg.data[4] = plat_add_msg_crc((uint32_t *)&priv->tx_msg, 24);
+	ret = imx_ele_msg_send_rcv(priv);
+	if (ret < 0)
+		return ret;
+
+	tag = MSG_TAG(priv->rx_msg.header);
+	command = MSG_COMMAND(priv->rx_msg.header);
+	size = MSG_SIZE(priv->rx_msg.header);
+	ver = MSG_VER(priv->rx_msg.header);
+	status = RES_STATUS(priv->rx_msg.data[0]);
+	if (tag == 0xe1 && command == ELE_SERVICE_SWAP_REQ && size == 0x03 &&
+	    ver == 0x06 && status == 0xd6) {
+		if (flag == ELE_IMEM_EXPORT)
+			return priv->rx_msg.data[1];
+		else
+			return 0;
+	}
+
+	return -EINVAL;
+}
+EXPORT_SYMBOL_GPL(ele_service_swap);
 
 int ele_get_info(phys_addr_t addr, u32 data_size)
 {
--- include/linux/firmware/imx/ele_base_msg.h
+++ include/linux/firmware/imx/ele_base_msg.h
@@ -24,6 +24,7 @@
 #define OTP_UNIQ_ID			0x01
 #define OTFAD_CONFIG			0x2
 #define ELE_GET_INFO_REQ                0xDA
+#define ELE_SERVICE_SWAP_REQ		0xDF
 #define GET_INFO_DATA                   0x17
 #define ELE_START_RNG_REQ		0xA3
 #define ELE_GET_TRNG_STATE_REQ		0xA4
@@ -39,11 +40,13 @@
 #define ELE_OEM_CNTN_AUTH_REQ_SIZE	3
 #define ELE_VERIFY_IMAGE_REQ_SIZE	2
 #define ELE_REL_CONTAINER_REQ_SIZE	1
-
+#define ELE_IMEM_EXPORT			0x1
+#define ELE_IMEM_IMPORT			0x2
 
 int read_common_fuse(uint16_t fuse_index, u32 *value);
 int ele_ping(void);
 int ele_get_info(phys_addr_t addr, u32 data_size);
+int ele_service_swap(phys_addr_t addr, u32 addr_size, u16 flag);
 int ele_start_rng(void);
 int ele_get_trng_state(void);
 
