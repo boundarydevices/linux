0ec44c3a4b34088e77dc01167c5e5ae552d5ab4a
Ming Zhou <ming.zhou@nxp.com>
2023-01-13 12:42:26 +0800

    MA-20518: add ioctl DMABUF_GET_HEAP_NAME
    add this ioctl so that userspace can know which heap buffer was alloccated from.
    
    Signed-off-by: Ming Zhou <ming.zhou@nxp.com>
    Change-Id: Ia89604574fbc5d1caa7af8e15e8b9a45080e52a9

--- include/uapi/linux/dma-buf-imx.h
+++ include/uapi/linux/dma-buf-imx.h
@@ -25,7 +25,12 @@ struct dmabuf_imx_phys_data {
 	__u32 dmafd;
 	__u64 phys;
 };
+struct dmabuf_imx_heap_name {
+	__u32 dmafd;
+	__u8 name[32];
+};
 
 #define DMABUF_GET_PHYS   _IOWR('M', 32, struct dmabuf_imx_phys_data)
+#define DMABUF_GET_HEAP_NAME   _IOWR('M', 33, struct dmabuf_imx_heap_name)
 
 #endif
