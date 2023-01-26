09b7910468ee5cb1fa893e10a543efc6b4f7c0b3
Fuad Tabba <tabba@google.com>
2023-01-26 10:14:05 +0000

    ANDROID: KVM: arm64: Ensure that SME is trapped if not supported in pKVM
    Do not rely on trapping being set only by the host for SME when
    running in protected mode.
    
    Bug: 357781595
    Change-Id: I76a090a3df43405b2f7728ade030035c659195e7
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- arch/arm64/kvm/hyp/nvhe/pkvm.c
+++ arch/arm64/kvm/hyp/nvhe/pkvm.c
@@ -96,6 +96,8 @@ static void pvm_init_traps_aa64pfr1(struct kvm_vcpu *vcpu)
 	const u64 feature_ids = pvm_read_id_reg(vcpu, SYS_ID_AA64PFR1_EL1);
 	u64 hcr_set = 0;
 	u64 hcr_clear = 0;
+	u64 cptr_set = 0;
+	u64 cptr_clear = 0;
 
 	/* Memory Tagging: Trap and Treat as Untagged if not supported. */
 	if (!FIELD_GET(ARM64_FEATURE_MASK(ID_AA64PFR1_EL1_MTE), feature_ids)) {
@@ -103,8 +105,17 @@ static void pvm_init_traps_aa64pfr1(struct kvm_vcpu *vcpu)
 		hcr_clear |= HCR_DCT | HCR_ATA;
 	}
 
+	/* No SME supprot in KVM. */
+	BUG_ON(FIELD_GET(ARM64_FEATURE_MASK(ID_AA64PFR1_EL1_SME), feature_ids));
+	if (has_hvhe())
+		cptr_clear |= CPACR_ELx_SMEN;
+	else
+		cptr_set |= CPTR_EL2_TSM;
+
 	vcpu->arch.hcr_el2 |= hcr_set;
 	vcpu->arch.hcr_el2 &= ~hcr_clear;
+	vcpu->arch.cptr_el2 |= cptr_set;
+	vcpu->arch.cptr_el2 &= ~cptr_clear;
 }
 
 /*
