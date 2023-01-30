5b88c14270420dc650edffc36268830af03b3055
parkeryang <Parker.Yang@mediatek.com>
2023-01-30 18:39:39 +0800

    GENIO: ASoC: mediatek: mt8195: add multi-in dai
    Add MT8195_AFE_IO_MULTI_IN1,2 in DAI enum
    Add multi-in dai register in platform driver
    
    Change-Id: I573f974d07f9b44c7e2a0fbbea0648f1b9ee19a5
    Signed-off-by: parkeryang <Parker.Yang@mediatek.com>

--- sound/soc/mediatek/mt8195/mt8195-afe-pcm.c
+++ sound/soc/mediatek/mt8195/mt8195-afe-pcm.c
@@ -3092,6 +3092,7 @@ static const dai_register_cb dai_register_cbs[] = {
 	mt8195_dai_dmic_register,
 	mt8195_dai_etdm_register,
 	mt8195_dai_pcm_register,
+	mt8195_dai_multi_in_register,
 	mt8195_dai_memif_register,
 };
 
