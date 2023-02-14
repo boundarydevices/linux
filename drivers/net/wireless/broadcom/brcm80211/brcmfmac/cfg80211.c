2e9e5241c2e5370b68deb86ee61a3d9a83dbb8b2
Shelley Yang <shelley.yang@infineon.com>
2023-02-14 03:08:42 -0600

    brcmfmac: set up wrong status when use internal supplicant on FT-PSK
    -use_fwsup should be set up BRCMF_PROFILE_FWSUP_PSK if use psk
    -fixes: SWWLAN-145694
    
    Signed-off-by: Shelley Yang <shelley.yang@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
@@ -2352,7 +2352,10 @@ brcmf_set_key_mgmt(struct net_device *ndev, struct cfg80211_connect_params *sme)
 		case WLAN_AKM_SUITE_FT_PSK:
 			val = WPA2_AUTH_PSK | WPA2_AUTH_FT;
 			profile->is_ft = true;
-			profile->use_fwsup = BRCMF_PROFILE_FWSUP_ROAM;
+			if (brcmf_feat_is_enabled(ifp, BRCMF_FEAT_FWSUP))
+				profile->use_fwsup = BRCMF_PROFILE_FWSUP_PSK;
+			else
+				profile->use_fwsup = BRCMF_PROFILE_FWSUP_ROAM;
 			break;
 		case WLAN_AKM_SUITE_DPP:
 			val = WFA_AUTH_DPP;
