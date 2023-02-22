dc150eb3aecc8be80f6a6e7a2c031e383ef98e29
Jindong Yue <jindong.yue@nxp.com>
2023-02-22 16:32:44 +0800

    MA-21060 soc: imx: imx8mp-blk-ctrl: Fix typo of imx8m_blk_ctrl_of_match
    Fix typo and add GPL license to support building as module.
    
    Change-Id: Ie5cfcedeae45ae9389080845841bf4651f0c6948
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>

--- drivers/soc/imx/imx8mp-blk-ctrl.c
+++ drivers/soc/imx/imx8mp-blk-ctrl.c
@@ -858,7 +858,7 @@ static const struct of_device_id imx8mp_blk_ctrl_of_match[] = {
 		/* Sentinel */
 	}
 };
-MODULE_DEVICE_TABLE(of, imx8m_blk_ctrl_of_match);
+MODULE_DEVICE_TABLE(of, imx8mp_blk_ctrl_of_match);
 
 static struct platform_driver imx8mp_blk_ctrl_driver = {
 	.probe = imx8mp_blk_ctrl_probe,
@@ -870,3 +870,4 @@ static struct platform_driver imx8mp_blk_ctrl_driver = {
 	},
 };
 module_platform_driver(imx8mp_blk_ctrl_driver);
+MODULE_LICENSE("GPL");
