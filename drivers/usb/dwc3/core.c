d7a7ba43b26b9a09f87c6ff0cd477cd69ca5ff21
Troy Kisky <troy.kisky@boundarydevices.com>
2023-02-24 16:48:52 -0800

    usb: dwc3: core: allow multiple reset gpios
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1740,8 +1740,8 @@ static int dwc3_probe(struct platform_device *pdev)
 	struct device		*dev = &pdev->dev;
 	struct resource		*res, dwc_res;
 	struct dwc3		*dwc;
-	struct gpio_desc	*reset_gpio;
-
+	struct gpio_descs	*reset_gpios;
+	int			i;
 	int			ret;
 
 	void __iomem		*regs;
@@ -1780,10 +1780,9 @@ static int dwc3_probe(struct platform_device *pdev)
 
 	dwc3_get_properties(dwc);
 
-	reset_gpio = devm_gpiod_get_optional(dev, "reset", GPIOD_OUT_HIGH);
-	if (IS_ERR(reset_gpio))
-		return PTR_ERR(reset_gpio);
-
+	reset_gpios = devm_gpiod_get_array_optional(dev, "reset", GPIOD_OUT_HIGH);
+	if (IS_ERR(reset_gpios))
+		return PTR_ERR(reset_gpios);
 	dwc->reset = devm_reset_control_array_get_optional_shared(dev);
 	if (IS_ERR(dwc->reset))
 		return PTR_ERR(dwc->reset);
@@ -1847,7 +1846,8 @@ static int dwc3_probe(struct platform_device *pdev)
 		goto err2;
 	}
 
-	gpiod_set_value_cansleep(reset_gpio, 0);
+	for (i = 0; i < reset_gpios->ndescs; i++)
+		gpiod_set_value_cansleep(reset_gpios->desc[i], 0);
 
 	ret = dwc3_get_dr_mode(dwc);
 	if (ret)
