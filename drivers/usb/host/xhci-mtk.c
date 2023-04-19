cb0061e7e3556249d0b0188e3eed408d372857ca
Jiacheng Liu <jiacheng.liu@mediatek.com>
2023-04-19 15:31:42 +0800

    [ALPS08032511] XHCI: keep runtime wakeup irq balance
    [Problem]
    After system resumed from suspend, xhci port cannot runtime
    resume when device plug in.
    
    [Solution]
    keep wakeup irq enable/disable count balance with:
    1. add enable_irq in xhci_mtk_resume
    2. add disable_irq_nosync in xhci_mtk_runtime_resume
    
    [Test]
    After system resumed, host port can runtime resume.
    
    MTK-Commit-Id: ba4f27b1ea714b147181fb22fbcf982696021823
    
    Signed-off-by: Jiacheng Liu <jiacheng.liu@mediatek.com>
    CR-Id: ALPS08032511
    Feature: usb_ssusb
    Change-Id: I6dfd475f92a36016029bd60ef67276fa9ac3222e

--- drivers/usb/host/xhci-mtk.c
+++ drivers/usb/host/xhci-mtk.c
@@ -1023,6 +1023,9 @@ static int __maybe_unused xhci_mtk_resume(struct device *dev)
 			goto enable_wakeup;
 	}
 
+	if (mtk->wakeup_irq > 0)
+		enable_irq(mtk->wakeup_irq);
+
 	ret = xhci_mtk_host_enable(mtk);
 	if (ret)
 		goto disable_clks;
@@ -1088,6 +1091,9 @@ static int __maybe_unused xhci_mtk_runtime_resume(struct device *dev)
 		return -ESHUTDOWN;
 
 	if (device_may_wakeup(dev)) {
+		if (mtk->wakeup_irq > 0)
+			disable_irq_nosync(mtk->wakeup_irq);
+
 		if (!mtk->has_ippc) {
 			parent = dev->parent;
 			if (parent && parent->driver) {
