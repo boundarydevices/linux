164b32177a4b66d97298f3197cfdd820c57b8f05
Richard Liu <xuegang.liu@nxp.com>
2023-05-22 21:08:02 +0800

    MA-21276 [#imx-3151] Disable GPU TX gating to avoid GPU hang on i.mx8mq
    Two issues found recently on Android13 + GPU 6.4.11.p1 driver on i.mx8mq,
    one is random GPU hang and dump when run CtsDeqpTestCases, another is GPU
    deadlock due to interrupt lost when run media streaming auto test, these
    two issues will disappear if disable GPU TX gating. Root cause is still
    under debugging, here disable GPU TX gating as a temporary solution.
    
    Change-Id: If268d246e599222400e1a91cc8ef8555c0d7f0df
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>

--- drivers/mxc/gpu-viv/hal/kernel/arch/gc_hal_kernel_hardware.c
+++ drivers/mxc/gpu-viv/hal/kernel/arch/gc_hal_kernel_hardware.c
@@ -2625,6 +2625,7 @@ gckHARDWARE_InitializeHardware(IN gckHARDWARE Hardware)
         _IsHardwareMatch(Hardware, gcv7000, 0x6202) ||
         _IsHardwareMatch(Hardware, gcv7000, 0x6203) ||
         _IsHardwareMatch(Hardware, gcv7000, 0x6204) ||
+        _IsHardwareMatch(Hardware, gcv7000, 0x6214) ||
         (gckHARDWARE_IsFeatureAvailable(Hardware, gcvFEATURE_TX_DESCRIPTOR) &&
          !gckHARDWARE_IsFeatureAvailable(Hardware, gcvFEATURE_TX_DESC_CACHE_CLOCKGATE_FIX))) {
         if (regPMC == 0) {
