5270d1c94aad7da6c6893c5c91387ffa2a97a1f4
parkeryang <Parker.Yang@mediatek.com>
2023-05-25 09:42:10 +0800

    GENIO: FIXUP: ASoC: mediatek: mt8195: fix PCM_BE dai-link
    PCM_BE supports playback so this property should be added.
    
    Change-Id: I5cbd2bf1e22e9ddd0a56fcc4019ab2e02c71f80e
    Signed-off-by: parkeryang <Parker.Yang@mediatek.com>

--- sound/soc/mediatek/mt8195/mt8395-evk.c
+++ sound/soc/mediatek/mt8195/mt8395-evk.c
@@ -774,6 +774,7 @@ static struct snd_soc_dai_link mt8195_mt6359_dai_links[] = {
 		.dai_fmt = SND_SOC_DAIFMT_I2S |
 			SND_SOC_DAIFMT_NB_NF |
 			SND_SOC_DAIFMT_CBS_CFS,
+		.dpcm_playback = 1,
 		.dpcm_capture = 1,
 		SND_SOC_DAILINK_REG(PCM1_BE),
 	},
