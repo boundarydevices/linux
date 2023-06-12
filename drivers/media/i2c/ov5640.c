23602899154b351c776479cbdec04f7fd2a77492
Mirela Rabulea <mirela.rabulea@nxp.com>
2023-06-12 11:28:42 +0300

    MA-12234: media: ov5640: Temporarily disable vblank control
    Low resolutions (160x120, 176x144, 320x240) in DVP mode can only run once,
    because the vblank value is restored from it's init value, when there is
    no mode change. The init value comes from VGA mode, and the mentioned
    low resolution mdes get stuck with it.
    
    Change-Id: I0660307db6a98d2bffacf00e308bcf2d82152bdd
    Signed-off-by: Mirela Rabulea <mirela.rabulea@nxp.com>

--- drivers/media/i2c/ov5640.c
+++ drivers/media/i2c/ov5640.c
@@ -3330,6 +3330,7 @@ static int ov5640_set_ctrl_vflip(struct ov5640_dev *sensor, int value)
 			      (BIT(2) | BIT(1)) : 0);
 }
 
+#if 0
 static int ov5640_set_ctrl_vblank(struct ov5640_dev *sensor, int value)
 {
 	const struct ov5640_mode_info *mode = sensor->current_mode;
@@ -3338,6 +3339,7 @@ static int ov5640_set_ctrl_vblank(struct ov5640_dev *sensor, int value)
 	return ov5640_write_reg16(sensor, OV5640_REG_TIMING_VTS,
 				  mode->height + value);
 }
+#endif
 
 static int ov5640_g_volatile_ctrl(struct v4l2_ctrl *ctrl)
 {
@@ -3372,7 +3374,7 @@ static int ov5640_s_ctrl(struct v4l2_ctrl *ctrl)
 	const struct ov5640_mode_info *mode = sensor->current_mode;
 	const struct ov5640_timings *timings;
 	unsigned int exp_max;
-	int ret;
+	int ret = 0;
 
 	/* v4l2_ctrl_lock() locks our own mutex */
 
@@ -3428,7 +3430,8 @@ static int ov5640_s_ctrl(struct v4l2_ctrl *ctrl)
 		ret = ov5640_set_ctrl_vflip(sensor, ctrl->val);
 		break;
 	case V4L2_CID_VBLANK:
-		ret = ov5640_set_ctrl_vblank(sensor, ctrl->val);
+		/* Temporarily disable vblank control */
+		/* ret = ov5640_set_ctrl_vblank(sensor, ctrl->val); */
 		break;
 	default:
 		ret = -EINVAL;
