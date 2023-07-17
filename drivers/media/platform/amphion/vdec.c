4d6c5ee3f69a4dd10da16aa23309fd2989168a8a
Jindong Yue <jindong.yue@nxp.com>
2023-07-17 16:13:41 +0800

    MA-21476 media: amphion: decoder implement V4L2_CID_MPEG_VIDEO_HEADER_MODE
    If the header mode is set to V4L2_MPEG_VIDEO_HEADER_MODE_SEPARATE,
    client may enqueue a buffer that only contains codec header,
    with flag V4L2_BUF_FLAG_HEADERS_ONLY.
    
    Change-Id: Ib8124b05ecb1fbcc23db12f38c35361eb2844bea
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>

--- drivers/media/platform/amphion/vdec.c
+++ drivers/media/platform/amphion/vdec.c
@@ -203,6 +203,9 @@ static int vdec_op_s_ctrl(struct v4l2_ctrl *ctrl)
 	case V4L2_CID_MPEG_VIDEO_DEC_DISPLAY_DELAY:
 		vdec->params.display_delay = ctrl->val;
 		break;
+	case V4L2_CID_MPEG_VIDEO_HEADER_MODE:
+		inst->header_separate = ctrl->val == V4L2_MPEG_VIDEO_HEADER_MODE_SEPARATE ? 1 : 0;
+		break;
 	default:
 		ret = -EINVAL;
 		break;
@@ -233,6 +236,13 @@ static int vdec_ctrl_init(struct vpu_inst *inst)
 	v4l2_ctrl_new_std(&inst->ctrl_handler, &vdec_ctrl_ops,
 			  V4L2_CID_MPEG_VIDEO_DEC_DISPLAY_DELAY_ENABLE,
 			  0, 1, 1, 0);
+
+	v4l2_ctrl_new_std_menu(&inst->ctrl_handler, &vdec_ctrl_ops,
+			  V4L2_CID_MPEG_VIDEO_HEADER_MODE,
+			  V4L2_MPEG_VIDEO_HEADER_MODE_JOINED_WITH_1ST_FRAME,
+			  ~((1 << V4L2_MPEG_VIDEO_HEADER_MODE_SEPARATE) |
+				  (1 << V4L2_MPEG_VIDEO_HEADER_MODE_JOINED_WITH_1ST_FRAME)),
+			  V4L2_MPEG_VIDEO_HEADER_MODE_JOINED_WITH_1ST_FRAME);
 
 	ctrl = v4l2_ctrl_new_std(&inst->ctrl_handler, &vdec_ctrl_ops,
 				 V4L2_CID_MIN_BUFFERS_FOR_CAPTURE, 1, 32, 1, 2);
