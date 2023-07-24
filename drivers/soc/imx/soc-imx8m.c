92bb7f69603a2560d6b0d8655f1e198cf36e4505
Alice Guo <alice.guo@nxp.com>
2023-07-24 08:19:42 +0000

    LF-9646-1: soc: imx: remove the part related to i.MX9 from soc-imx8m.c
    i.MX9 no longer reuses i.MX8M SoC driver so that delete code related to
    i.MX9.
    
    Signed-off-by: Alice Guo <alice.guo@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>

--- drivers/soc/imx/soc-imx8m.c
+++ drivers/soc/imx/soc-imx8m.c
@@ -37,11 +37,8 @@
 #define IMX8MP_OCOTP_UID_OFFSET		0x10
 #define IMX8MP_OCOTP_UID_HIGH		0xE00
 
-#define IMX93_OCOTP_UID_OFFSET		0x80c0
-
 /* Same as ANADIG_DIGPROG_IMX7D */
 #define ANADIG_DIGPROG_IMX8MM	0x800
-#define ANADIG_DIGPROG_IMX93	0x800
 
 struct imx8_soc_data {
 	char *name;
@@ -163,53 +160,6 @@ static u32 __init imx8mm_soc_revision(void)
 	return rev;
 }
 
-static void __init imx93_soc_uid(void)
-{
-	void __iomem *ocotp_base;
-	struct device_node *np;
-
-	np = of_find_compatible_node(NULL, NULL, "fsl,imx93-ocotp");
-	if (!np)
-		return;
-
-	ocotp_base = of_iomap(np, 0);
-	WARN_ON(!ocotp_base);
-
-	soc_uid = readl_relaxed(ocotp_base + IMX93_OCOTP_UID_OFFSET + 0x8);
-	soc_uid <<= 32;
-	soc_uid |= readl_relaxed(ocotp_base + IMX93_OCOTP_UID_OFFSET + 0xC);
-
-	soc_uid_h = readl_relaxed(ocotp_base + IMX93_OCOTP_UID_OFFSET + 0x0);
-	soc_uid_h <<= 32;
-	soc_uid_h |= readl_relaxed(ocotp_base + IMX93_OCOTP_UID_OFFSET + 0x4);
-
-	iounmap(ocotp_base);
-	of_node_put(np);
-}
-
-static u32 __init imx93_soc_revision(void)
-{
-	struct device_node *np;
-	void __iomem *anatop_base;
-	u32 rev;
-
-	np = of_find_compatible_node(NULL, NULL, "fsl,imx93-anatop");
-	if (!np)
-		return 0;
-
-	anatop_base = of_iomap(np, 0);
-	WARN_ON(!anatop_base);
-
-	rev = readl_relaxed(anatop_base + ANADIG_DIGPROG_IMX93);
-
-	iounmap(anatop_base);
-	of_node_put(np);
-
-	imx93_soc_uid();
-
-	return rev;
-}
-
 static const struct imx8_soc_data imx8mq_soc_data = {
 	.name = "i.MX8MQ",
 	.soc_revision = imx8mq_soc_revision,
@@ -230,17 +180,11 @@ static const struct imx8_soc_data imx8mp_soc_data = {
 	.soc_revision = imx8mm_soc_revision,
 };
 
-static const struct imx8_soc_data imx91p_soc_data = {
-	.name = "i.MX91P",
-	.soc_revision = imx93_soc_revision,
-};
-
 static __maybe_unused const struct of_device_id imx8_soc_match[] = {
 	{ .compatible = "fsl,imx8mq", .data = &imx8mq_soc_data, },
 	{ .compatible = "fsl,imx8mm", .data = &imx8mm_soc_data, },
 	{ .compatible = "fsl,imx8mn", .data = &imx8mn_soc_data, },
 	{ .compatible = "fsl,imx8mp", .data = &imx8mp_soc_data, },
-	{ .compatible = "fsl,imx91p", .data = &imx91p_soc_data, },
 	{ }
 };
 
