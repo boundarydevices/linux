2f75f3180051d5aafd7429e63c85efe56102546d
parkeryang <Parker.Yang@mediatek.com>
2023-08-04 11:39:06 +0800

    GENIO: ASoC: mediatek: mt8195: add dmic hw gain control
    Each DMIC gain module can be enabled/disabled and config independently.
    
    - target gain value:
     eg. 10dB: 10^(10/20)*0x80000 = 1657944
    amixer -c 0 cset name='DMIC1_HW_GAIN_TARGET' 16579442
    amixer -c 0 cset name='DMIC4_HW_GAIN_TARGET' 16579442
    
    - current gain value:
     default 0dB: 10^(0/20)*0x80000 = 524288
    amixer -c 0 cset name='DMIC1_HW_GAIN_CURRENT' 524288
    amixer -c 0 cset name='DMIC4_HW_GAIN_CURRENT' 524288
    
    - gain up per step:
     default 0.25dB: 10^(0.25/20)*0x80000 = 539597
    amixer -c 0 cset name='DMIC1_HW_GAIN_UP_STEP' 539597
    amixer -c 0 cset name='DMIC4_HW_GAIN_UP_STEP' 539597
    
    - samples per step:
    amixer -c 0 cset name='DMIC1_HW_GAIN_SAMPLE_PER_STEP' 200
    amixer -c 0 cset name='DMIC4_HW_GAIN_SAMPLE_PER_STEP' 200
    
    - enable dmic gain
    amixer -c 0 cset name='DMIC1_HW_GAIN_EN' 1
    amixer -c 0 cset name='DMIC4_HW_GAIN_EN' 1
    
    Change-Id: Ia8e83904d83663f9fa7ade228533ac4d87baad10
    Signed-off-by: parkeryang <Parker.Yang@mediatek.com>
    (cherry picked from commit 965c8e0ed9b7acc5d8144a23f08404d4ad3ca729)

--- sound/soc/mediatek/mt8195/mt8195-dai-dmic.c
+++ sound/soc/mediatek/mt8195/mt8195-dai-dmic.c
@@ -5,6 +5,7 @@
  * Copyright (c) 2020 MediaTek Inc.
  * Author: Bicycle Tsai <bicycle.tsai@mediatek.com>
  *         Trevor Wu <trevor.wu@mediatek.com>
+ *         Parker Yang <parker.yang@mediatek.com>
  */
 
 #include <linux/delay.h>
@@ -15,6 +16,17 @@
 #include "mt8195-reg.h"
 
 #define DMIC_MAX_CH (8)
+
+/* DMIC HW Gain configuration maximum value. */
+#define DMIC_GAIN_MAX_STEP	GENMASK(19, 0)
+#define DMIC_GAIN_MAX_PER_STEP	GENMASK(7, 0)
+#define DMIC_GAIN_MAX_TARGET	GENMASK(27, 0)
+#define DMIC_GAIN_MAX_CURRENT	GENMASK(27, 0)
+
+enum {
+	SUPPLY_SEQ_DMIC_GAIN,
+	SUPPLY_SEQ_DMIC_CK,
+};
 
 enum {
 	DMIC0,
@@ -28,10 +40,16 @@ struct mtk_dai_dmic_ctrl_reg {
 	unsigned int con0;
 };
 
+struct mtk_dai_dmic_hw_gain_ctrl_reg {
+	unsigned int bypass;
+	unsigned int con0;
+};
+
 struct mtk_dai_dmic_priv {
 	unsigned int clk_phase_sel_ch[2];
 	unsigned int dmic_src_sel[DMIC_MAX_CH];
 	unsigned int iir_on;
+	unsigned int gain_on[DMIC_NUM];
 	unsigned int two_wire_mode;
 	unsigned int channels;
 	unsigned int dmic_clk_mono;
@@ -59,6 +77,119 @@ static const struct mtk_dai_dmic_ctrl_reg *get_dmic_ctrl_reg(int id)
 		return NULL;
 
 	return &dmic_ctrl_regs[id];
+}
+
+static const struct mtk_dai_dmic_hw_gain_ctrl_reg
+	dmic_hw_gain_ctrl_regs[DMIC_NUM] = {
+	[DMIC0] = {
+		.bypass = DMIC_BYPASS_HW_GAIN,
+		.con0 = DMIC_GAIN1_CON0,
+	},
+	[DMIC1] = {
+		.bypass = DMIC_BYPASS_HW_GAIN,
+		.con0 = DMIC_GAIN2_CON0,
+	},
+	[DMIC2] = {
+		.bypass = DMIC_BYPASS_HW_GAIN,
+		.con0 = DMIC_GAIN3_CON0,
+	},
+	[DMIC3] = {
+		.bypass = DMIC_BYPASS_HW_GAIN,
+		.con0 = DMIC_GAIN4_CON0,
+	},
+};
+
+static const struct mtk_dai_dmic_hw_gain_ctrl_reg
+	*get_dmic_hw_gain_ctrl_reg(struct mtk_base_afe *afe, int id)
+{
+	if ((id < 0) || (id >= DMIC_NUM)) {
+		dev_dbg(afe->dev, "%s invalid id\n", __func__);
+		return NULL;
+	}
+
+	return &dmic_hw_gain_ctrl_regs[id];
+}
+
+static void mtk_dai_dmic_hw_gain_byass(struct mtk_base_afe *afe,
+				       unsigned int id, bool bypass)
+{
+	const struct mtk_dai_dmic_hw_gain_ctrl_reg *reg;
+	unsigned int msk;
+
+	reg = get_dmic_hw_gain_ctrl_reg(afe, id);
+	if (!reg)
+		return;
+
+	switch (id) {
+	case DMIC0:
+		msk = DMIC_BYPASS_HW_GAIN_DMIC1_BYPASS;
+		break;
+	case DMIC1:
+		msk = DMIC_BYPASS_HW_GAIN_DMIC2_BYPASS;
+		break;
+	case DMIC2:
+		msk = DMIC_BYPASS_HW_GAIN_DMIC3_BYPASS;
+		break;
+	case DMIC3:
+		msk = DMIC_BYPASS_HW_GAIN_DMIC4_BYPASS;
+		break;
+	default:
+		return;
+	}
+
+	if (bypass)
+		regmap_set_bits(afe->regmap, reg->bypass, msk);
+	else
+		regmap_clear_bits(afe->regmap, reg->bypass, msk);
+}
+
+static void mtk_dai_dmic_hw_gain_on(struct mtk_base_afe *afe,
+				    unsigned int id, bool on)
+{
+	const struct mtk_dai_dmic_hw_gain_ctrl_reg *reg;
+	unsigned int msk = DMIC_GAIN_CON0_GAIN_ON;
+
+	reg = get_dmic_hw_gain_ctrl_reg(afe, id);
+	if (!reg)
+		return;
+
+	if (on)
+		regmap_set_bits(afe->regmap, reg->con0, msk);
+	else
+		regmap_clear_bits(afe->regmap, reg->con0, msk);
+}
+
+static void mtk_dai_dmic_hw_gain_one_heart(struct mtk_base_afe *afe,
+					   unsigned int id, bool on)
+{
+	const struct mtk_dai_dmic_hw_gain_ctrl_reg *reg;
+	unsigned int msk;
+
+	reg = get_dmic_hw_gain_ctrl_reg(afe, id);
+	if (!reg)
+		return;
+
+	switch (id) {
+	case DMIC0:
+		/* dmic0 is group leader and no one heart bit */
+		return;
+	case DMIC1:
+		msk = DMIC_BYPASS_HW_GAIN2_ONE_HEART;
+		break;
+	case DMIC2:
+		msk = DMIC_BYPASS_HW_GAIN3_ONE_HEART;
+		break;
+	case DMIC3:
+		msk = DMIC_BYPASS_HW_GAIN4_ONE_HEART;
+		break;
+	default:
+		return;
+	}
+
+	if (on)
+		regmap_set_bits(afe->regmap, reg->bypass, msk);
+	else
+		regmap_clear_bits(afe->regmap, reg->bypass, msk);
 }
 
 static const struct reg_sequence mtk_dai_dmic_iir_coeff_reg_defaults[] = {
@@ -123,6 +254,74 @@ static int mtk_dai_dmic_configure_array(struct snd_soc_dai *dai)
 	      PWR2_TOP_CON_DMIC1_SRC_SEL_VAL(dmic_src_sel[0]);
 
 	regmap_update_bits(afe->regmap, PWR2_TOP_CON0, mask, val);
+
+	return 0;
+}
+
+static void mtk_dai_dmic_hw_gain_enable(struct mtk_base_afe *afe,
+					unsigned int channels, bool enable)
+{
+	struct mt8195_afe_private *afe_priv = afe->platform_priv;
+	struct mtk_dai_dmic_priv *dmic_priv;
+	unsigned int ch_base;
+	unsigned int end;
+	unsigned int id;
+	unsigned int reorder_id;
+
+	if (!channels)
+		return;
+
+	dmic_priv = afe_priv->dai_priv[MT8195_AFE_IO_DMIC_IN];
+	ch_base = dmic_priv->dmic_clk_mono ? 1 : 2;
+	if (channels > ch_base * 3)
+		end = 3;
+	else if (channels > ch_base * 2)
+		end = 2;
+	else if (channels > ch_base)
+		end = 1;
+	else if (channels > 0)
+		end = 0;
+
+	for (id = 0; id <= end; id++) {
+		reorder_id = dmic_priv->clk_index[id];
+		if (enable && dmic_priv->gain_on[reorder_id]) {
+			mtk_dai_dmic_hw_gain_one_heart(afe, reorder_id, enable);
+			mtk_dai_dmic_hw_gain_byass(afe, reorder_id, !enable);
+			mtk_dai_dmic_hw_gain_on(afe, reorder_id, enable);
+		} else {
+			mtk_dai_dmic_hw_gain_one_heart(afe, reorder_id, !enable);
+			mtk_dai_dmic_hw_gain_on(afe, reorder_id, enable);
+			mtk_dai_dmic_hw_gain_byass(afe, reorder_id, !enable);
+		}
+	}
+}
+
+static int mtk_dmic_gain_event(struct snd_soc_dapm_widget *w,
+			       struct snd_kcontrol *kcontrol,
+			       int event)
+{
+	struct snd_soc_component *cmpnt = snd_soc_dapm_to_component(w->dapm);
+	struct mtk_base_afe *afe = snd_soc_component_get_drvdata(cmpnt);
+	struct mt8195_afe_private *afe_priv = afe->platform_priv;
+	struct mtk_dai_dmic_priv *dmic_priv = NULL;
+	unsigned int channels;
+
+	dev_dbg(afe->dev, "%s(), name %s, event 0x%x\n",
+		__func__, w->name, event);
+
+	dmic_priv = afe_priv->dai_priv[MT8195_AFE_IO_DMIC_IN];
+	channels = dmic_priv->channels;
+
+	switch (event) {
+	case SND_SOC_DAPM_PRE_PMU:
+		mtk_dai_dmic_hw_gain_enable(afe, channels, true);
+		break;
+	case SND_SOC_DAPM_POST_PMD:
+		mtk_dai_dmic_hw_gain_enable(afe, channels, false);
+		break;
+	default:
+		break;
+	}
 
 	return 0;
 }
@@ -400,13 +599,16 @@ static const struct snd_soc_dapm_widget mtk_dai_dmic_widgets[] = {
 	SND_SOC_DAPM_MIXER("I010", SND_SOC_NOPM, 0, 0, NULL, 0),
 	SND_SOC_DAPM_MIXER("I011", SND_SOC_NOPM, 0, 0, NULL, 0),
 
-	SND_SOC_DAPM_SUPPLY("DMIC_CK_ON",
-			    PWR2_TOP_CON1,
-			    PWR2_TOP_CON1_DMIC_CKDIV_ON_SHIFT, 0,
-			    mtk_dmic_event,
-			    SND_SOC_DAPM_PRE_POST_PMU |
-			    SND_SOC_DAPM_PRE_POST_PMD),
-
+	SND_SOC_DAPM_SUPPLY_S("DMIC_GAIN_ON", SUPPLY_SEQ_DMIC_GAIN,
+			      SND_SOC_NOPM, 0, 0,
+			      mtk_dmic_gain_event,
+			      SND_SOC_DAPM_PRE_PMU | SND_SOC_DAPM_POST_PMD),
+	SND_SOC_DAPM_SUPPLY_S("DMIC_CK_ON", SUPPLY_SEQ_DMIC_CK,
+			      PWR2_TOP_CON1,
+			      PWR2_TOP_CON1_DMIC_CKDIV_ON_SHIFT, 0,
+			      mtk_dmic_event,
+			      SND_SOC_DAPM_PRE_POST_PMU |
+			      SND_SOC_DAPM_PRE_POST_PMD),
 	SND_SOC_DAPM_INPUT("DMIC_INPUT"),
 };
 
@@ -421,7 +623,127 @@ static const struct snd_soc_dapm_route mtk_dai_dmic_routes[] = {
 	{"I011", NULL, "DMIC Capture"},
 
 	{"DMIC Capture", NULL, "DMIC_CK_ON"},
+	{"DMIC Capture", NULL, "DMIC_GAIN_ON"},
 	{"DMIC Capture", NULL, "DMIC_INPUT"},
+};
+
+static const char * const mt8195_dmic_gain_enable_text[] = {
+	"Bypass", "Connect",
+};
+
+static SOC_ENUM_SINGLE_EXT_DECL(dmic_gain_on_enum,
+				mt8195_dmic_gain_enable_text);
+
+static int mtk_dai_dmic_hw_gain_ctrl_put(struct snd_kcontrol *kcontrol,
+					 struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_component *component = snd_kcontrol_chip(kcontrol);
+	struct soc_enum *e = (struct soc_enum *)kcontrol->private_value;
+	struct mtk_base_afe *afe = snd_soc_component_get_drvdata(component);
+	struct mt8195_afe_private *afe_priv = afe->platform_priv;
+	struct mtk_dai_dmic_priv *dmic_priv;
+	unsigned int source = ucontrol->value.enumerated.item[0];
+	unsigned int *cached;
+
+	if (source >= e->items)
+		return -EINVAL;
+
+	dmic_priv = afe_priv->dai_priv[MT8195_AFE_IO_DMIC_IN];
+
+	if (!strcmp(kcontrol->id.name, "DMIC1_HW_GAIN_EN"))
+		cached = &dmic_priv->gain_on[0];
+	else if (!strcmp(kcontrol->id.name, "DMIC2_HW_GAIN_EN"))
+		cached = &dmic_priv->gain_on[1];
+	else if (!strcmp(kcontrol->id.name, "DMIC3_HW_GAIN_EN"))
+		cached = &dmic_priv->gain_on[2];
+	else if (!strcmp(kcontrol->id.name, "DMIC4_HW_GAIN_EN"))
+		cached = &dmic_priv->gain_on[3];
+
+	if (source == *cached)
+		return 0;
+
+	*cached = source;
+	return 1;
+}
+
+static int mtk_dai_dmic_hw_gain_ctrl_get(struct snd_kcontrol *kcontrol,
+					 struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_component *component =
+		snd_soc_kcontrol_component(kcontrol);
+	struct mtk_base_afe *afe = snd_soc_component_get_drvdata(component);
+	struct mt8195_afe_private *afe_priv = afe->platform_priv;
+	struct mtk_dai_dmic_priv *dmic_priv;
+	unsigned int val = 0;
+
+	dmic_priv = afe_priv->dai_priv[MT8195_AFE_IO_DMIC_IN];
+
+	if (!strcmp(kcontrol->id.name, "DMIC1_HW_GAIN_EN"))
+		val = dmic_priv->gain_on[0];
+	else if (!strcmp(kcontrol->id.name, "DMIC2_HW_GAIN_EN"))
+		val = dmic_priv->gain_on[1];
+	else if (!strcmp(kcontrol->id.name, "DMIC3_HW_GAIN_EN"))
+		val = dmic_priv->gain_on[2];
+	else if (!strcmp(kcontrol->id.name, "DMIC4_HW_GAIN_EN"))
+		val = dmic_priv->gain_on[3];
+
+	ucontrol->value.enumerated.item[0] = val;
+	return 0;
+}
+
+static const struct snd_kcontrol_new mtk_dai_dmic_controls[] = {
+	SOC_ENUM_EXT("DMIC1_HW_GAIN_EN", dmic_gain_on_enum,
+		     mtk_dai_dmic_hw_gain_ctrl_get,
+		     mtk_dai_dmic_hw_gain_ctrl_put),
+	SOC_ENUM_EXT("DMIC2_HW_GAIN_EN", dmic_gain_on_enum,
+		     mtk_dai_dmic_hw_gain_ctrl_get,
+		     mtk_dai_dmic_hw_gain_ctrl_put),
+	SOC_ENUM_EXT("DMIC3_HW_GAIN_EN", dmic_gain_on_enum,
+		     mtk_dai_dmic_hw_gain_ctrl_get,
+		     mtk_dai_dmic_hw_gain_ctrl_put),
+	SOC_ENUM_EXT("DMIC4_HW_GAIN_EN", dmic_gain_on_enum,
+		     mtk_dai_dmic_hw_gain_ctrl_get,
+		     mtk_dai_dmic_hw_gain_ctrl_put),
+	SOC_SINGLE("DMIC1_HW_GAIN_TARGET", DMIC_GAIN1_CON1,
+		   0, DMIC_GAIN_MAX_TARGET, 0),
+	SOC_SINGLE("DMIC2_HW_GAIN_TARGET", DMIC_GAIN2_CON1,
+		   0, DMIC_GAIN_MAX_TARGET, 0),
+	SOC_SINGLE("DMIC3_HW_GAIN_TARGET", DMIC_GAIN3_CON1,
+		   0, DMIC_GAIN_MAX_TARGET, 0),
+	SOC_SINGLE("DMIC4_HW_GAIN_TARGET", DMIC_GAIN4_CON1,
+		   0, DMIC_GAIN_MAX_TARGET, 0),
+	SOC_SINGLE("DMIC1_HW_GAIN_CURRENT", DMIC_GAIN1_CUR,
+		   0, DMIC_GAIN_MAX_CURRENT, 0),
+	SOC_SINGLE("DMIC2_HW_GAIN_CURRENT", DMIC_GAIN2_CUR,
+		   0, DMIC_GAIN_MAX_CURRENT, 0),
+	SOC_SINGLE("DMIC3_HW_GAIN_CURRENT", DMIC_GAIN3_CUR,
+		   0, DMIC_GAIN_MAX_CURRENT, 0),
+	SOC_SINGLE("DMIC4_HW_GAIN_CURRENT", DMIC_GAIN4_CUR,
+		   0, DMIC_GAIN_MAX_CURRENT, 0),
+	SOC_SINGLE("DMIC1_HW_GAIN_UP_STEP", DMIC_GAIN1_CON3,
+		   0, DMIC_GAIN_MAX_STEP, 0),
+	SOC_SINGLE("DMIC2_HW_GAIN_UP_STEP", DMIC_GAIN2_CON3,
+		   0, DMIC_GAIN_MAX_STEP, 0),
+	SOC_SINGLE("DMIC3_HW_GAIN_UP_STEP", DMIC_GAIN3_CON3,
+		   0, DMIC_GAIN_MAX_STEP, 0),
+	SOC_SINGLE("DMIC4_HW_GAIN_UP_STEP", DMIC_GAIN4_CON3,
+		   0, DMIC_GAIN_MAX_STEP, 0),
+	SOC_SINGLE("DMIC1_HW_GAIN_DOWN_STEP", DMIC_GAIN1_CON2,
+		   0, DMIC_GAIN_MAX_STEP, 0),
+	SOC_SINGLE("DMIC2_HW_GAIN_DOWN_STEP", DMIC_GAIN2_CON2,
+		   0, DMIC_GAIN_MAX_STEP, 0),
+	SOC_SINGLE("DMIC3_HW_GAIN_DOWN_STEP", DMIC_GAIN3_CON2,
+		   0, DMIC_GAIN_MAX_STEP, 0),
+	SOC_SINGLE("DMIC4_HW_GAIN_DOWN_STEP", DMIC_GAIN4_CON2,
+		   0, DMIC_GAIN_MAX_STEP, 0),
+	SOC_SINGLE("DMIC1_HW_GAIN_SAMPLE_PER_STEP", DMIC_GAIN1_CON0,
+		   DMIC_GAIN_CON0_SAMPLE_PER_STEP_SHIFT, DMIC_GAIN_MAX_PER_STEP, 0),
+	SOC_SINGLE("DMIC2_HW_GAIN_SAMPLE_PER_STEP", DMIC_GAIN2_CON0,
+		   DMIC_GAIN_CON0_SAMPLE_PER_STEP_SHIFT, DMIC_GAIN_MAX_PER_STEP, 0),
+	SOC_SINGLE("DMIC3_HW_GAIN_SAMPLE_PER_STEP", DMIC_GAIN3_CON0,
+		   DMIC_GAIN_CON0_SAMPLE_PER_STEP_SHIFT, DMIC_GAIN_MAX_PER_STEP, 0),
+	SOC_SINGLE("DMIC4_HW_GAIN_SAMPLE_PER_STEP", DMIC_GAIN4_CON0,
+		   DMIC_GAIN_CON0_SAMPLE_PER_STEP_SHIFT, DMIC_GAIN_MAX_PER_STEP, 0),
 };
 
 static int init_dmic_priv_data(struct mtk_base_afe *afe)
@@ -498,11 +820,12 @@ int mt8195_dai_dmic_register(struct mtk_base_afe *afe)
 
 	dai->dai_drivers = mtk_dai_dmic_driver;
 	dai->num_dai_drivers = ARRAY_SIZE(mtk_dai_dmic_driver);
-
 	dai->dapm_widgets = mtk_dai_dmic_widgets;
 	dai->num_dapm_widgets = ARRAY_SIZE(mtk_dai_dmic_widgets);
 	dai->dapm_routes = mtk_dai_dmic_routes;
 	dai->num_dapm_routes = ARRAY_SIZE(mtk_dai_dmic_routes);
+	dai->controls = mtk_dai_dmic_controls;
+	dai->num_controls = ARRAY_SIZE(mtk_dai_dmic_controls);
 
 	return init_dmic_priv_data(afe);
 }
