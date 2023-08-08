76dceb11f20315d7ae26f6204748cae6d4e84e19
Kevenny Hsieh <kevenny.hsieh@mediatek.com>
2023-08-08 23:08:05 +0800

    FROMLIST: virt: geniezone: Enable PTP for synchronizing time between host and guest VMs
    Enabled Precision Time Protocol (PTP) for improved host-guest VM time
    synchronization, optimizing operations needing precise clock sync in
    virtual environment.
    
    Signed-off-by: Kevenny Hsieh <kevenny.hsieh@mediatek.com>
    Signed-off-by: Yi-De Wu <yi-de.wu@mediatek.com>
    Signed-off-by: Liju Chen <liju-clr.chen@mediatek.com>
    Change-Id: Id236519e48e44713b20d75b80524921ddc0abc3f
    Bug: 379230831
    Link: https://lore.kernel.org/lkml/20241114100802.4116-22-liju-clr.chen@mediatek.com/

--- arch/arm64/geniezone/gzvm_arch_common.h
+++ arch/arm64/geniezone/gzvm_arch_common.h
@@ -83,6 +83,8 @@ int gzvm_hypcall_wrapper(unsigned long a0, unsigned long a1,
  * @__pad: add an explicit '__u32 __pad;' in the middle to make it clear
  *         what the actual layout is.
  * @lr: The array of LRs(list registers).
+ * @vtimer_offset: The offset maintained by hypervisor that is host cycle count
+ *                 when guest VM startup.
  *
  * - Keep the same layout of hypervisor data struct.
  * - Sync list registers back for acking virtual device interrupt status.
@@ -91,6 +93,7 @@ struct gzvm_vcpu_hwstate {
 	__le32 nr_lrs;
 	__le32 __pad;
 	__le64 lr[GIC_V3_NR_LRS];
+	__le64 vtimer_offset;
 };
 
 static inline unsigned int
