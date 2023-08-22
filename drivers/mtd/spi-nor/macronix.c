1bf849437a44122ddac30714bcbf9d0e70feec42
Han Xu <han.xu@nxp.com>
2023-08-22 15:58:20 -0500

    LF-9906 spi-nor: macronix: fix the macronix octal dtr function
    fix the build issue that cause by unaligned octal dtr function for
    macronix spi-nor chips.
    
    Signed-off-by: Han Xu <han.xu@nxp.com>

--- drivers/mtd/spi-nor/macronix.c
+++ drivers/mtd/spi-nor/macronix.c
@@ -46,7 +46,7 @@ static const struct spi_nor_fixups mx25l25635_fixups = {
  *
  * Return: 0 on success, -errno otherwise.
  */
-static int spi_nor_macronix_octal_dtr_enable(struct spi_nor *nor, bool enable)
+static int spi_nor_macronix_set_octal_dtr(struct spi_nor *nor, bool enable)
 {
 	struct spi_mem_op op;
 	u8 *buf = nor->bouncebuf, i;
@@ -111,7 +111,7 @@ static int spi_nor_macronix_octal_dtr_enable(struct spi_nor *nor, bool enable)
 
 static void octaflash_default_init(struct spi_nor *nor)
 {
-	nor->params->octal_dtr_enable = spi_nor_macronix_octal_dtr_enable;
+	nor->params->set_octal_dtr = spi_nor_macronix_set_octal_dtr;
 }
 
 static struct spi_nor_fixups octaflash_fixups = {
