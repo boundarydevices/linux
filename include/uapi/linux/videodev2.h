6cbc10a4b60a2915a14e186766db40a49536e3d2
Aymen Sghaier <aymen.sghaier@nxp.com>
2023-12-14 17:17:28 +0100

    ILIE-12: include: videodev2.h: Add meta formats used for NXP NEOISP sub-system
    Add NXP NEOISP meta-data v4l2 new formats:
    - V4L2_META_FMT_NEO_ISP_PARAMS
    - V4L2_META_FMT_NEO_ISP_STATS
    
    Reviewed-by: Julien Vuillaumier <julien.vuillaumier@nxp.com>
    Signed-off-by: Aymen Sghaier <aymen.sghaier@nxp.com>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -840,6 +840,10 @@ struct v4l2_pix_format {
 #define V4L2_META_FMT_RK_ISP1_PARAMS	v4l2_fourcc('R', 'K', '1', 'P') /* Rockchip ISP1 3A Parameters */
 #define V4L2_META_FMT_RK_ISP1_STAT_3A	v4l2_fourcc('R', 'K', '1', 'S') /* Rockchip ISP1 3A Statistics */
 
+/* Vendor specific - used for NXP NEOISP sub-system */
+#define V4L2_META_FMT_NEO_ISP_PARAMS	v4l2_fourcc('N', 'N', 'I', 'P') /* NXP NEOISP Parameters */
+#define V4L2_META_FMT_NEO_ISP_STATS	v4l2_fourcc('N', 'N', 'I', 'S') /* NXP NEOISP Statistics */
+
 /* priv field value to indicates that subsequent fields are valid. */
 #define V4L2_PIX_FMT_PRIV_MAGIC		0xfeedcafe
 
