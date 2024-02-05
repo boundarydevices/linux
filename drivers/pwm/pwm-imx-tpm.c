de55e87525c40019a729eb4eef21837e8f201317
Viorel Suman <viorel.suman@nxp.com>
2024-02-05 09:44:30 +0200

    LF-11492-1 pwm: imx-tpm: reset module on probe
    Reset Timer PWM module on probe if "pwm-rst"
    property is provided in DTS node.
    
    Signed-off-by: Viorel Suman <viorel.suman@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Acked-by: Jason Liu <jason.hui.liu@nxp.com>

--- drivers/pwm/pwm-imx-tpm.c
+++ drivers/pwm/pwm-imx-tpm.c
@@ -33,6 +33,7 @@
 #define PWM_IMX_TPM_CnV(n)	(0x24 + (n) * 0x8)
 
 #define PWM_IMX_TPM_PARAM_CHAN			GENMASK(7, 0)
+#define PWM_IMX_TPM_GLOBAL_RST			BIT(1)
 
 #define PWM_IMX_TPM_SC_PS			GENMASK(2, 0)
 #define PWM_IMX_TPM_SC_CMOD			GENMASK(4, 3)
@@ -337,6 +338,7 @@ static const struct pwm_ops imx_tpm_pwm_ops = {
 
 static int pwm_imx_tpm_probe(struct platform_device *pdev)
 {
+	struct device_node *np = pdev->dev.of_node;
 	struct imx_tpm_pwm_chip *tpm;
 	int ret;
 	u32 val;
@@ -369,6 +371,12 @@ static int pwm_imx_tpm_probe(struct platform_device *pdev)
 	/* get number of channels */
 	val = readl(tpm->base + PWM_IMX_TPM_PARAM);
 	tpm->chip.npwm = FIELD_GET(PWM_IMX_TPM_PARAM_CHAN, val);
+
+	if (of_property_read_bool(np, "pwm-rst")) {
+		/* Resets all internal logic and registers */
+		writel(PWM_IMX_TPM_GLOBAL_RST, tpm->base + PWM_IMX_TPM_GLOBAL);
+		writel(0, tpm->base + PWM_IMX_TPM_GLOBAL);
+	}
 
 	mutex_init(&tpm->lock);
 
