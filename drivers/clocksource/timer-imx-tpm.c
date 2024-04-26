14ae78fa64e971c511b774406d60df6ace24ede2
Jacky Bai <ping.bai@nxp.com>
2024-04-26 14:03:20 +0800

    Revert "LF-11518-3: drivers: clocksource: timer imx tpm: multiple instances"
    This reverts commit 61e99a73f0068b2e29938aca5ceca886158f657c.

--- drivers/clocksource/timer-imx-tpm.c
+++ drivers/clocksource/timer-imx-tpm.c
@@ -6,7 +6,6 @@
 #include <linux/clk.h>
 #include <linux/clockchips.h>
 #include <linux/clocksource.h>
-#include <linux/cpuhotplug.h>
 #include <linux/delay.h>
 #include <linux/interrupt.h>
 #include <linux/module.h>
@@ -36,53 +35,37 @@
 #define TPM_C0V				0x24
 
 static int counter_width __ro_after_init;
-//static void __iomem *timer_base __ro_after_init;
+static void __iomem *timer_base __ro_after_init;
 
-struct imx_tpm_clockevent {
-	void __iomem *base;
-	int counter_width;
-	struct timer_of to_tpm;
-};
-
-struct imx_tpm_clockevent *global_tpm;
-
-struct imx_tpm_clockevent* to_tpm_clkevt(struct clock_event_device *evt)
-{
-	struct timer_of *to_tpm;
-
-	to_tpm = container_of(evt, struct timer_of, clkevt);
-	return container_of(to_tpm, struct imx_tpm_clockevent, to_tpm);
-}
-
-static inline void tpm_timer_disable(struct imx_tpm_clockevent *tpm)
+static inline void tpm_timer_disable(void)
 {
 	unsigned int val;
 
 	/* channel disable */
-	val = readl(tpm->base + TPM_C0SC);
+	val = readl(timer_base + TPM_C0SC);
 	val &= ~(TPM_C0SC_MODE_MASK | TPM_C0SC_CHIE);
-	writel(val, tpm->base + TPM_C0SC);
+	writel(val, timer_base + TPM_C0SC);
 }
 
-static inline void tpm_timer_enable(struct imx_tpm_clockevent *tpm)
+static inline void tpm_timer_enable(void)
 {
 	unsigned int val;
 
 	/* channel enabled in sw compare mode */
-	val = readl(tpm->base + TPM_C0SC);
+	val = readl(timer_base + TPM_C0SC);
 	val |= (TPM_C0SC_MODE_SW_COMPARE << TPM_C0SC_MODE_SHIFT) |
 	       TPM_C0SC_CHIE;
-	writel(val, tpm->base + TPM_C0SC);
+	writel(val, timer_base + TPM_C0SC);
 }
 
-static inline void tpm_irq_acknowledge(struct imx_tpm_clockevent *tpm)
+static inline void tpm_irq_acknowledge(void)
 {
-	writel(TPM_STATUS_CH0F, tpm->base + TPM_STATUS);
+	writel(TPM_STATUS_CH0F, timer_base + TPM_STATUS);
 }
 
-static inline unsigned long tpm_read_counter(struct imx_tpm_clockevent *tpm)
+static inline unsigned long tpm_read_counter(void)
 {
-	return readl(tpm->base + TPM_CNT);
+	return readl(timer_base + TPM_CNT);
 }
 
 #if defined(CONFIG_ARM)
@@ -90,32 +73,31 @@ static struct delay_timer tpm_delay_timer;
 
 static unsigned long tpm_read_current_timer(void)
 {
-	return tpm_read_counter(global_tpm);
+	return tpm_read_counter();
 }
-#endif
 
 static u64 notrace tpm_read_sched_clock(void)
 {
-	return tpm_read_counter(global_tpm);
+	return tpm_read_counter();
 }
+#endif
 
 static int tpm_set_next_event(unsigned long delta,
 				struct clock_event_device *evt)
 {
 	unsigned long next, prev, now;
-	struct imx_tpm_clockevent *tpm = to_tpm_clkevt(evt);
 
-	prev = tpm_read_counter(tpm);
+	prev = tpm_read_counter();
 	next = prev + delta;
-	writel(next, tpm->base + TPM_C0V);
-	now = tpm_read_counter(tpm);
+	writel(next, timer_base + TPM_C0V);
+	now = tpm_read_counter();
 
 	/*
 	 * Need to wait CNT increase at least 1 cycle to make sure
 	 * the C0V has been updated into HW.
 	 */
-	if ((next & 0xffffffff) != readl(tpm->base + TPM_C0V))
-		while (now == tpm_read_counter(tpm))
+	if ((next & 0xffffffff) != readl(timer_base + TPM_C0V))
+		while (now == tpm_read_counter())
 			;
 
 	/*
@@ -129,17 +111,14 @@ static int tpm_set_next_event(unsigned long delta,
 
 static int tpm_set_state_oneshot(struct clock_event_device *evt)
 {
-	struct imx_tpm_clockevent *tpm = to_tpm_clkevt(evt);
-	tpm_timer_enable(tpm);
+	tpm_timer_enable();
 
 	return 0;
 }
 
 static int tpm_set_state_shutdown(struct clock_event_device *evt)
 {
-	struct imx_tpm_clockevent *tpm = to_tpm_clkevt(evt);
-
-	tpm_timer_disable(tpm);
+	tpm_timer_disable();
 
 	return 0;
 }
@@ -147,74 +126,64 @@ static int tpm_set_state_shutdown(struct clock_event_device *evt)
 static irqreturn_t tpm_timer_interrupt(int irq, void *dev_id)
 {
 	struct clock_event_device *evt = dev_id;
-	struct imx_tpm_clockevent *tpm = to_tpm_clkevt(evt);
 
-	tpm_irq_acknowledge(tpm);
+	tpm_irq_acknowledge();
 
 	evt->event_handler(evt);
 
 	return IRQ_HANDLED;
 }
 
-static int __init tpm_clocksource_init(struct timer_of *to_tpm)
-{
-	struct imx_tpm_clockevent *tpm = container_of(to_tpm, struct imx_tpm_clockevent, to_tpm);
+static struct timer_of to_tpm = {
+	.flags = TIMER_OF_IRQ | TIMER_OF_BASE | TIMER_OF_CLOCK,
+	.clkevt = {
+		.name			= "i.MX TPM Timer",
+		.rating			= 200,
+		.features		= CLOCK_EVT_FEAT_ONESHOT | CLOCK_EVT_FEAT_DYNIRQ,
+		.set_state_shutdown	= tpm_set_state_shutdown,
+		.set_state_oneshot	= tpm_set_state_oneshot,
+		.set_next_event		= tpm_set_next_event,
+		.cpumask		= cpu_possible_mask,
+	},
+	.of_irq = {
+		.handler		= tpm_timer_interrupt,
+		.flags			= IRQF_TIMER,
+	},
+	.of_clk = {
+		.name = "per",
+	},
+};
 
+static int __init tpm_clocksource_init(void)
+{
 #if defined(CONFIG_ARM)
 	tpm_delay_timer.read_current_timer = &tpm_read_current_timer;
-	tpm_delay_timer.freq = timer_of_rate(to_tpm) >> 3;
+	tpm_delay_timer.freq = timer_of_rate(&to_tpm) >> 3;
 	register_current_timer_delay(&tpm_delay_timer);
-#endif
 
 	sched_clock_register(tpm_read_sched_clock, counter_width,
-			     timer_of_rate(to_tpm) >> 3);
+			     timer_of_rate(&to_tpm) >> 3);
+#endif
 
-	return clocksource_mmio_init(tpm->base + TPM_CNT,
+	return clocksource_mmio_init(timer_base + TPM_CNT,
 				     "imx-tpm",
-				     timer_of_rate(to_tpm) >> 3,
-				     to_tpm->clkevt.rating,
-				    counter_width,
+				     timer_of_rate(&to_tpm) >> 3,
+				     to_tpm.clkevt.rating,
+				     counter_width,
 				     clocksource_mmio_readl_up);
 }
 
-static void __init tpm_clockevent_init(struct timer_of *to_tpm)
+static void __init tpm_clockevent_init(void)
 {
-	clockevents_config_and_register(&to_tpm->clkevt,
-					timer_of_rate(to_tpm) >> 3,
+	clockevents_config_and_register(&to_tpm.clkevt,
+					timer_of_rate(&to_tpm) >> 3,
 					300,
 					GENMASK(counter_width - 1,
 					1));
 }
 
-static int tpm;
-/* percpu timer */
-static DEFINE_PER_CPU(struct imx_tpm_clockevent, tpm_percpu_timer);
-
-static int imx_tpm_starting_cpu(unsigned int cpu)
-{
-        struct imx_tpm_clockevent *clkevt;
-        clkevt = per_cpu_ptr(&tpm_percpu_timer, cpu);
-        tpm_clockevent_init(&clkevt->to_tpm);
-        irq_force_affinity(clkevt->to_tpm.clkevt.irq, cpumask_of(cpu));
-
-        return 0;
-}
-
-static int imx_tpm_dying_cpu(unsigned int cpu)
-{
-        struct imx_tpm_clockevent *clkevt;
-        clkevt = per_cpu_ptr(&tpm_percpu_timer, cpu);
-
-        disable_percpu_irq(timer_of_irq(&clkevt->to_tpm));
-
-        return 0;
-}
-
 static int __init tpm_timer_init(struct device_node *np)
 {
-	struct imx_tpm_clockevent *clkevt;
-	void __iomem *timer_base;
-	uint32_t rating = 0;
 	struct clk *ipg;
 	int ret;
 
@@ -231,59 +200,16 @@ static int __init tpm_timer_init(struct device_node *np)
 		return ret;
 	}
 
-	clkevt = per_cpu_ptr(&tpm_percpu_timer, tpm);
-
-	clkevt->to_tpm.flags = TIMER_OF_IRQ | TIMER_OF_BASE | TIMER_OF_CLOCK;
-
-	if (tpm == 0)
-		clkevt->to_tpm.clkevt = (struct clock_event_device) {
-			.name			= "i.MX TPM Timer 0",
-			.rating			= 200,
-			.features		= CLOCK_EVT_FEAT_ONESHOT,
-			.set_state_shutdown	= tpm_set_state_shutdown,
-			.set_state_oneshot	= tpm_set_state_oneshot,
-			.set_next_event		= tpm_set_next_event,
-			.cpumask		= cpumask_of(tpm),
-		};
-	else
-		clkevt->to_tpm.clkevt = (struct clock_event_device) {
-			.name			= "i.MX TPM Timer 1",
-			.rating			= 200,
-			.features		= CLOCK_EVT_FEAT_ONESHOT,
-			.set_state_shutdown	= tpm_set_state_shutdown,
-			.set_state_oneshot	= tpm_set_state_oneshot,
-			.set_next_event		= tpm_set_next_event,
-			.cpumask		= cpumask_of(tpm),
-		};
-
-	clkevt->to_tpm.of_irq = (struct of_timer_irq) {
-		.handler		= tpm_timer_interrupt,
-		.flags			= IRQF_TIMER,
-	};
-
-	clkevt->to_tpm.of_clk = (struct of_timer_clk) {
-		.name = "per",
-	};
-
-	ret = timer_of_init(np, &clkevt->to_tpm);
+	ret = timer_of_init(np, &to_tpm);
 	if (ret)
 		return ret;
 
-	timer_base = timer_of_base(&clkevt->to_tpm);
-	clkevt->base = timer_base;
+	timer_base = timer_of_base(&to_tpm);
 
 	counter_width = (readl(timer_base + TPM_PARAM)
 		& TPM_PARAM_WIDTH_MASK) >> TPM_PARAM_WIDTH_SHIFT;
-
-	of_property_read_u32(np, "timer-rating", &rating);
-
-	if (rating) {
-		/* override timer rating if device tree property is found */
-		clkevt->to_tpm.clkevt.rating = rating;
-	} else {
-		/* use rating 200 for 32-bit counter and 150 for 16-bit counter */
-		clkevt->to_tpm.clkevt.rating = counter_width == 0x20 ? 200 : 150;
-	}
+	/* use rating 200 for 32-bit counter and 150 for 16-bit counter */
+	to_tpm.clkevt.rating = counter_width == 0x20 ? 200 : 150;
 
 	/*
 	 * Initialize tpm module to a known state
@@ -313,18 +239,9 @@ static int __init tpm_timer_init(struct device_node *np)
 	/* set MOD register to maximum for free running mode */
 	writel(GENMASK(counter_width - 1, 0), timer_base + TPM_MOD);
 
-	if (!tpm) {
-		tpm++;
-		global_tpm = clkevt;
+	tpm_clockevent_init();
 
-		cpuhp_setup_state(CPUHP_AP_NXP_TIMER_STARTING,
-				  "clockevents/nxp/tpm_timer:starting",
-				  imx_tpm_starting_cpu, imx_tpm_dying_cpu);
-
-		return tpm_clocksource_init(&clkevt->to_tpm);
-	}
-	else
-		return 0;
+	return tpm_clocksource_init();
 }
 #ifdef MODULE
 static int tpm_timer_probe(struct platform_device *pdev)
