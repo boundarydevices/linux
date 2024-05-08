477552059940deb49335c124c89e432e1eef44ee
Suhrid Subramaniam <suhrid.subramaniam@mediatek.com>
2024-05-08 19:06:45 +0000

    GENIO: media: platform: mtk-mdp3: Add RGBA format support
    Add support for MDP3 to process and output RGBA format data for mt8183,
    mt8188 and mt8195.
    
    Based on commit 652336cbf6ac from mtk-mdp-driver (OOT):
    GENIO:mtk-mdp-driver: Add RGBA format support
    
    Change-Id: Ib8af6484b6d61400f7d65e22eafda7997b2e4034
    Signed-off-by: Suhrid Subramaniam <suhrid.subramaniam@mediatek.com>

--- drivers/media/platform/mediatek/mdp3/mt8183/mdp3-plat-mt8183.h
+++ drivers/media/platform/mediatek/mdp3/mt8183/mdp3-plat-mt8183.h
@@ -205,6 +205,13 @@ static const struct mdp_format mt8183_formats[] = {
 		.num_planes	= 1,
 		.flags		= MDP_FMT_FLAG_OUTPUT | MDP_FMT_FLAG_CAPTURE,
 	}, {
+		.pixelformat	= V4L2_PIX_FMT_RGBA32,
+		.mdp_color	= MDP_COLOR_RGBA8888,
+		.depth		= { 32 },
+		.row_depth	= { 32 },
+		.num_planes	= 1,
+		.flags		= MDP_FMT_FLAG_OUTPUT | MDP_FMT_FLAG_CAPTURE,
+	}, {
 		.pixelformat	= V4L2_PIX_FMT_ABGR32,
 		.mdp_color	= MDP_COLOR_BGRA8888,
 		.depth		= { 32 },
