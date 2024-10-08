453bbe91025c627511fca54592b679e125dfdda3
Pablo Sun <pablo.sun@mediatek.com>
2024-10-08 15:24:21 +0800

    GENIO: iommu/mediatek: Implement probe/release for APU iommu
    Implement the probe and release for MTK_IOMMU_TYPE_APU
    iommu entries. The API iommu has multiple hardware unit
    that must be probe and released together.
    
    Link: https://patchwork.kernel.org/project/linux-mediatek/patch/20211210172605.30618-8-flora.fu@mediatek.com/)
    Change-Id: I4d15b681187478f22b6e26a23b97903fd9281c6c
    Signed-off-by: Pablo Sun <pablo.sun@mediatek.com>

--- drivers/iommu/mtk_iommu.c
+++ drivers/iommu/mtk_iommu.c
@@ -845,7 +845,7 @@ static phys_addr_t mtk_iommu_iova_to_phys(struct iommu_domain *domain,
 static struct iommu_device *mtk_iommu_probe_device(struct device *dev)
 {
 	struct iommu_fwspec *fwspec = dev_iommu_fwspec_get(dev);
-	struct mtk_iommu_data *data;
+	struct mtk_iommu_data *data, *curdata;
 	struct device_link *link;
 	struct device *larbdev;
 	unsigned int larbid, larbidx, i;
@@ -855,34 +855,45 @@ static struct iommu_device *mtk_iommu_probe_device(struct device *dev)
 
 	data = dev_iommu_priv_get(dev);
 
-	if (!MTK_IOMMU_IS_TYPE(data->plat_data, MTK_IOMMU_TYPE_MM))
-		return &data->iommu;
-
-	/*
-	 * Link the consumer device with the smi-larb device(supplier).
-	 * The device that connects with each a larb is a independent HW.
-	 * All the ports in each a device should be in the same larbs.
-	 */
-	larbid = MTK_M4U_TO_LARB(fwspec->ids[0]);
-	if (larbid >= MTK_LARB_NR_MAX)
-		return ERR_PTR(-EINVAL);
-
-	for (i = 1; i < fwspec->num_ids; i++) {
-		larbidx = MTK_M4U_TO_LARB(fwspec->ids[i]);
-		if (larbid != larbidx) {
-			dev_err(dev, "Can only use one larb. Fail@larb%d-%d.\n",
-				larbid, larbidx);
-			return ERR_PTR(-EINVAL);
+	if (MTK_IOMMU_IS_TYPE(data->plat_data, MTK_IOMMU_TYPE_APU)) {
+		/*
+		 * The APU IOMMU HWs must work together. The consumer device
+		 * must connect with all the apu iommu HWs at the same time.
+		 */
+		for_each_m4u(curdata, data->hw_list) {
+			link = device_link_add(dev, curdata->dev, DL_FLAG_PM_RUNTIME |
+				DL_FLAG_STATELESS);
+			if (!link)
+				dev_err(dev, "Unable to link %s\n", dev_name(curdata->dev));
 		}
-	}
-	larbdev = data->larb_imu[larbid].dev;
-	if (!larbdev)
-		return ERR_PTR(-EINVAL);
+	} else if (MTK_IOMMU_IS_TYPE(data->plat_data, MTK_IOMMU_TYPE_MM)) {
+		/*
+		 * Link the consumer device with the smi-larb device(supplier).
+		 * The device that connects with each a larb is a independent HW.
+		 * All the ports in each a device should be in the same larbs.
+		 */
+		larbid = MTK_M4U_TO_LARB(fwspec->ids[0]);
+		if (larbid >= MTK_LARB_NR_MAX)
+			return ERR_PTR(-EINVAL);
 
-	link = device_link_add(dev, larbdev,
-			       DL_FLAG_PM_RUNTIME | DL_FLAG_STATELESS);
-	if (!link)
-		dev_err(dev, "Unable to link %s\n", dev_name(larbdev));
+		for (i = 1; i < fwspec->num_ids; i++) {
+			larbidx = MTK_M4U_TO_LARB(fwspec->ids[i]);
+			if (larbid != larbidx) {
+				dev_err(dev, "Can only use one larb. Fail@larb%d-%d.\n",
+					larbid, larbidx);
+				return ERR_PTR(-EINVAL);
+			}
+		}
+		larbdev = data->larb_imu[larbid].dev;
+		if (!larbdev)
+			return ERR_PTR(-EINVAL);
+
+		link = device_link_add(dev, larbdev,
+				       DL_FLAG_PM_RUNTIME | DL_FLAG_STATELESS);
+		if (!link)
+			dev_err(dev, "Unable to link %s\n", dev_name(larbdev));
+
+	}
 	return &data->iommu;
 }
 
@@ -890,6 +901,7 @@ static void mtk_iommu_release_device(struct device *dev)
 {
 	struct iommu_fwspec *fwspec = dev_iommu_fwspec_get(dev);
 	struct mtk_iommu_data *data;
+	struct list_head *head;
 	struct device *larbdev;
 	unsigned int larbid;
 
@@ -898,6 +910,10 @@ static void mtk_iommu_release_device(struct device *dev)
 		larbid = MTK_M4U_TO_LARB(fwspec->ids[0]);
 		larbdev = data->larb_imu[larbid].dev;
 		device_link_remove(dev, larbdev);
+	} else if (MTK_IOMMU_IS_TYPE(data->plat_data, MTK_IOMMU_TYPE_APU)) {
+		head = data->hw_list;
+		for_each_m4u(data, head)
+			device_link_remove(dev, data->dev);
 	}
 }
 
