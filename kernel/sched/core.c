70cc4a97617cb06179adc6599ab6dffb8b002f78
Peter Zijlstra <peterz@infradead.org>
2024-10-10 08:28:36 +0000

    UPSTREAM: sched/core: Dequeue PSI signals for blocked tasks that are delayed
    psi_dequeue() in for blocked task expects psi_sched_switch() to clear
    the TSK_.*RUNNING PSI flags and set the TSK_IOWAIT flags however
    psi_sched_switch() uses "!task_on_rq_queued(prev)" to detect if the task
    is blocked or still runnable which is no longer true with DELAY_DEQUEUE
    since a blocking task can be left queued on the runqueue.
    
    This can lead to PSI splats similar to:
    
        psi: inconsistent task state! task=... cpu=... psi_flags=4 clear=0 set=4
    
    when the task is requeued since the TSK_RUNNING flag was not cleared
    when the task was blocked.
    
    Explicitly communicate that the task was blocked to psi_sched_switch()
    even if it was delayed and is still on the runqueue.
    
      [ prateek: Broke off the relevant part from [1], commit message ]
    
    Fixes: 152e11f6df29 ("sched/fair: Implement delayed dequeue")
    Closes: https://lore.kernel.org/lkml/20240830123458.3557-1-spasswolf@web.de/
    Closes: https://lore.kernel.org/all/cd67fbcd-d659-4822-bb90-7e8fbb40a856@molgen.mpg.de/
    Change-Id: I1ee2f5dfd85abc58fdc8707d4d21e74ae5f9902d
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Not-yet-signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Johannes Weiner <hannes@cmpxchg.org>
    Link: https://lore.kernel.org/lkml/20241004123506.GR18071@noisy.programming.kicks-ass.net/ [1]
    (cherry picked from commit f5aaff7bfa11fb0b2ee6b8fd7bbc16cfceea2ad3)
    Signed-off-by: John Stultz <jstultz@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6646,6 +6646,7 @@ static void __sched notrace __schedule(int sched_mode)
 	 * as a preemption by schedule_debug() and RCU.
 	 */
 	bool preempt = sched_mode > SM_NONE;
+	bool block = false;
 	unsigned long *switch_count;
 	unsigned long prev_state;
 	struct rq_flags rf;
@@ -6731,6 +6732,7 @@ static void __sched notrace __schedule(int sched_mode)
 			 * After this, schedule() must not care about p->state any more.
 			 */
 			block_task(rq, prev, flags);
+			block = true;
 		}
 		switch_count = &prev->nvcsw;
 	}
@@ -6777,7 +6779,7 @@ picked:
 
 		migrate_disable_switch(rq, prev);
 		psi_account_irqtime(rq, prev, next);
-		psi_sched_switch(prev, next, !task_on_rq_queued(prev));
+		psi_sched_switch(prev, next, block);
 
 		trace_sched_switch(preempt, prev, next, prev_state);
 
