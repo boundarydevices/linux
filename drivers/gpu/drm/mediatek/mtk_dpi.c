a1f2cecfb8c77e671823affac8a87b3ed14eabbc
Tommy Chen <tommyyl.chen@mediatek.com>
2024-11-26 15:43:06 +0800

    GENIO: drm: mediatek: dpi: add dpix clock control for lvds
    During the suspend process, the bridge disabling first disables the
    LVDS module and then the DPI module. Specifically, mtk_lvds_poweroff()
    is called first, followed by mtk_dpi_power_off().
    
    However, disabling the dpix in mtk_lvds_poweroff() causes
    mtk_dpi_disable(), which is called in mtk_dpi_power_off(), to hang.
    
    As a result, the dpix clock must be disabled after mtk_dpi_disable() is
    called. Therefore, disabling the dpix clock is moved to the DPI module.
    
    (cherry picked from commit a3663b4ce36c7ff3c02c543c0c930a30de740d55)
    
    Change-Id: Icc2e2bf4d56b0b5938b1559e7a6958fa0611a02d
    CR-Id: AUTO00364628
    Signed-off-by: Tommy Chen <tommyyl.chen@mediatek.com>

--- drivers/gpu/drm/mediatek/mtk_dpi.c
+++ drivers/gpu/drm/mediatek/mtk_dpi.c
@@ -78,6 +78,7 @@ struct mtk_dpi {
 	struct clk *dpi_sel_clk;
 	struct clk *tvd_clk;
 	struct clk *pclk_src[5];
+	struct clk *dpix;
 	int irq;
 	struct drm_display_mode mode;
 	const struct mtk_dpi_conf *conf;
@@ -498,6 +499,7 @@ static void mtk_dpi_power_off(struct mtk_dpi *dpi)
 
 	reset_control_rearm(dpi->reset_ctl);
 
+	clk_disable_unprepare(dpi->dpix);
 	clk_disable_unprepare(dpi->pixel_clk);
 	clk_disable_unprepare(dpi->engine_clk);
 	clk_disable_unprepare(dpi->tvd_clk);
@@ -541,10 +543,18 @@ static int mtk_dpi_power_on(struct mtk_dpi *dpi)
 		goto err_pixel;
 	}
 
+	ret = clk_prepare_enable(dpi->dpix);
+	if (ret) {
+		dev_err(dpi->dev, "Failed to enable dpix clock: %d\n", ret);
+		goto err_dpix;
+	}
+
 	reset_control_reset(dpi->reset_ctl);
 
 	return 0;
 
+err_dpix:
+	clk_disable_unprepare(dpi->dpix);
 err_pixel:
 	clk_disable_unprepare(dpi->dpi_ck_cg);
 err_ck_cg:
@@ -1268,6 +1278,14 @@ static int mtk_dpi_probe(struct platform_device *pdev)
 	if (IS_ERR(dpi->dpi_sel_clk)) {
 		return dev_err_probe(dev, PTR_ERR(dpi->dpi_sel_clk),
 				     "Failed to get dpi_sel_clk clock\n");
+	}
+
+	dpi->dpix = devm_clk_get_optional(dev, "dpix");
+	if (IS_ERR(dpi->dpix)) {
+		ret = PTR_ERR(dpi->dpix);
+		dev_err(dev, "Failed to get vpll_dpix clock: %d\n", ret);
+
+		return ret;
 	}
 
 	//TODO: TVDPLL_D* are parent clock divisors which needs to be
