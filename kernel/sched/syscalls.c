44301f95d971eba2b0c8146cade500e47de6b6ba
Zhou Jian <quic_jianzhou@quicinc.com>
2024-12-08 23:43:34 -0800

    ANDROID: sched: add sched_setattr export
    commit 616d91b68cd5 ("sched: Remove sched_setscheduler*() EXPORTs")
    remove several exports, and commit 7a8142aaf68c ("Reapply "Revert
    sched: Remove sched_setscheduler*() EXPORTs"") does not add
    sched_setattr back.
    
    Add it as some vendor modules need to set explicit priorities for
    kernel threads.
    
    Bug: 163613349
    Change-Id: I082de83c6add074803f798ed710adeacf25f3563
    Signed-off-by: Zhou Jian <quic_jianzhou@quicinc.com>

--- kernel/sched/syscalls.c
+++ kernel/sched/syscalls.c
@@ -823,6 +823,7 @@ int sched_setattr(struct task_struct *p, const struct sched_attr *attr)
 {
 	return __sched_setscheduler(p, attr, true, true);
 }
+EXPORT_SYMBOL_GPL(sched_setattr);
 
 int sched_setattr_nocheck(struct task_struct *p, const struct sched_attr *attr)
 {
