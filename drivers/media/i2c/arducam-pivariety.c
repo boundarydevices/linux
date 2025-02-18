7048c3c0e21262b28e03868d379e84dcff607800
Yuriy Pasichnyk <yurijpasichnyk11@gmail.com>
2025-02-18 16:20:31 +0200

    media: i2c: arducam-pivariety: Fix mutex init and NULL pointer
    The mutex used in arducam-pivariety was not properly initialized,
    which could lead to undefined behavior. This also caused a NULL
    pointer dereference under certain conditions.
    
    This patch ensures the mutex is correctly initialized during probe
    and prevents NULL pointer dereferences.
    
    Signed-off-by: Yuriy Pasichnyk <yurijpasichnyk11@gmail.com>

--- drivers/media/i2c/arducam-pivariety.c
+++ drivers/media/i2c/arducam-pivariety.c
@@ -1210,6 +1210,8 @@ static int pivariety_enum_controls(struct pivariety *pivariety)
 	if (ret)
 		return ret;
 
+	mutex_init(&pivariety->mutex);
+
 	index = 0;
 	while (1) {
 		ret = pivariety_write(pivariety, CTRL_INDEX_REG, index);
@@ -1297,6 +1299,7 @@ static int pivariety_enum_controls(struct pivariety *pivariety)
 	v4l2_ctrl_handler_setup(ctrl_hdlr);
 	return 0;
 err:
+	mutex_destroy(&pivariety->mutex);
 	return -ENODEV;
 }
 
