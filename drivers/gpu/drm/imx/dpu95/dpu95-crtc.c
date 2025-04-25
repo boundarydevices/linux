2eea56d8924e3506f75076f361a947e0fd8d9b52
Liu Ying <victor.liu@nxp.com>
2025-04-25 15:48:49 +0800

    LF-15170 drm/imx: dpu95-crtc: Queue state event early for page flip
    Page flip is done usually with DRM vblank event.  To ensure the page flip
    case works efficiently, move queueing state event prior to waiting for
    ExtDst SHDLD done instead of the other way around so that DRM vblank event
    can be handled in vblank IRQ handler as soon as possible because this
    enables DisEngCFG frame complete IRQ(vblank IRQ) early enough.  Note that
    DisEngCFG frame complete IRQ comes after ExtDst SHDLD IRQ comes about a
    time duration of an entire vblank, so timing is critical if the vblank time
    duration is short.  A problematic case caused by missing the critical
    timing is that low page flip frame rate is seen on i.MX95 B0 EVK with
    RM692C9 MIPI DSI panel.
    
    Reviewed-by: Laurentiu Palcu <laurentiu.palcu@oss.nxp.com>
    Signed-off-by: Liu Ying <victor.liu@nxp.com>
    Acked-by: Jason Liu <jason.hui.liu@nxp.com>

--- drivers/gpu/drm/imx/dpu95/dpu95-crtc.c
+++ drivers/gpu/drm/imx/dpu95/dpu95-crtc.c
@@ -366,6 +366,18 @@ static void dpu95_crtc_atomic_flush(struct drm_crtc *crtc,
 	dpu95_crtc_atomic_flush_scalers(crtc);
 
 	if (!need_modeset && crtc->state->active) {
+		/*
+		 * Queue state event prior to waiting for ExtDst SHDLD done.
+		 * This makes sure that the event can be handled in the vblank
+		 * IRQ handler as soon as possible because vblank is enabled
+		 * early enough and otherwise it could be enabled too
+		 * late(after ExtDst SHDLD done). Note that DisEngCFG frame
+		 * complete IRQ(vblank IRQ) comes after ExtDst SHDLD IRQ comes
+		 * about a time duration of an entire vblank, so timing is
+		 * critical if the vblank time duration is short.
+		 */
+		dpu95_crtc_queue_state_event(crtc);
+
 		enable_irq(dpu_crtc->ed_cont_shdld_irq);
 
 		/*
@@ -379,8 +391,6 @@ static void dpu95_crtc_atomic_flush(struct drm_crtc *crtc,
 		disable_irq(dpu_crtc->ed_cont_shdld_irq);
 
 		DPU95_CRTC_CHECK_FRAMEGEN_PRIMARY_FIFO(dpu_crtc->fg);
-
-		dpu95_crtc_queue_state_event(crtc);
 	} else {
 		/*
 		 * Simply flush and hope that any update takes effect
