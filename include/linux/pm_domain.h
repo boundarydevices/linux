6985b7c48af8acd8bb0a927fe1104901722de725
Gary Bisson <gary.bisson@ezurio.com>
2025-05-07 17:09:22 +0200

    Revert "MA-20138-9 ABI: Remove power domain changes"
    This reverts commit 6caf6cf59962b27bc3648eca7a717c3d947ba1eb.

--- include/linux/pm_domain.h
+++ include/linux/pm_domain.h
@@ -18,6 +18,7 @@
 #include <linux/spinlock.h>
 #include <linux/cpumask.h>
 #include <linux/time64.h>
+#include <linux/clk.h>
 
 /*
  * Flags to control the behaviour when attaching a device to its PM domains.
@@ -92,6 +93,9 @@ struct dev_pm_domain_list {
  * GENPD_FLAG_OPP_TABLE_FW:	The genpd provider supports performance states,
  *				but its corresponding OPP tables are not
  *				described in DT, but are given directly by FW.
+ *
+ * GENPD_FLAG_PM_PD_CLK:	Instructs genpd to enable/disable PD clocks when
+ *				powering on/off domain.
  */
 #define GENPD_FLAG_PM_CLK	 (1U << 0)
 #define GENPD_FLAG_IRQ_SAFE	 (1U << 1)
@@ -100,7 +104,8 @@ struct dev_pm_domain_list {
 #define GENPD_FLAG_CPU_DOMAIN	 (1U << 4)
 #define GENPD_FLAG_RPM_ALWAYS_ON (1U << 5)
 #define GENPD_FLAG_MIN_RESIDENCY (1U << 6)
-#define GENPD_FLAG_OPP_TABLE_FW	 (1U << 7)
+#define GENPD_FLAG_PM_PD_CLK	 (1U << 7)
+#define GENPD_FLAG_OPP_TABLE_FW	 (1U << 8)
 
 enum gpd_status {
 	GENPD_STATE_ON = 0,	/* PM domain is on */
@@ -202,6 +207,8 @@ struct generic_pm_domain {
 #if !defined(CONFIG_IMX_GKI_FIX) || defined(CONFIG_IMX_GKI_8Q_FIX)
 	unsigned int state_idx_saved; /* saved power state for recovery after system suspend/resume */
 #endif
+	struct clk_bulk_data *clks;
+	int num_clks;
 };
 
 static inline struct generic_pm_domain *pd_to_genpd(struct dev_pm_domain *pd)
@@ -265,6 +272,7 @@ int pm_genpd_remove_subdomain(struct generic_pm_domain *genpd,
 			      struct generic_pm_domain *subdomain);
 int pm_genpd_init(struct generic_pm_domain *genpd,
 		  struct dev_power_governor *gov, bool is_off);
+int pm_genpd_of_add_clks(struct generic_pm_domain *genpd, struct device *dev);
 int pm_genpd_remove(struct generic_pm_domain *genpd);
 int dev_pm_genpd_set_performance_state(struct device *dev, unsigned int state);
 int dev_pm_genpd_add_notifier(struct device *dev, struct notifier_block *nb);
@@ -307,6 +315,12 @@ static inline int pm_genpd_init(struct generic_pm_domain *genpd,
 				struct dev_power_governor *gov, bool is_off)
 {
 	return -ENOSYS;
+}
+static inline int pm_genpd_of_add_clks(struct generic_pm_domain *genpd,
+				       struct device *dev)
+{
+	return 0;
+
 }
 static inline int pm_genpd_remove(struct generic_pm_domain *genpd)
 {
